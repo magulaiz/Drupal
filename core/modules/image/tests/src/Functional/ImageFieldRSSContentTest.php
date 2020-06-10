b2e526417dcd320a0fbffa1c885e1fcbecf41282
James Williams <18895-jameswilliams@users.noreply.drupalcode.org>
2020-06-10 12:05:00 +0200

    Issue #3150318: patch 2

---
+++ core/modules/image/tests/src/Functional/ImageFieldRSSContentTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\Tests\image\Functional;
+
+use Drupal\file\Entity\File;
+use Drupal\Tests\TestFileCreationTrait;
+
+/**
+ * Ensure that images added to nodes appear correctly in RSS feeds.
+ *
+ * @group image
+ */
+class ImageFieldRSSContentTest extends ImageFieldTestBase {
+
+  use TestFileCreationTrait {
+    getTestFiles as drupalGetTestFiles;
+  }
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['node', 'views'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests RSS enclosure formatter display for RSS feeds.
+   */
+  public function testFileFieldRSSContent() {
+    $node_storage = $this->container->get('entity_type.manager')->getStorage('node');
+    $field_name = strtolower($this->randomMachineName());
+    $type_name = 'article';
+
+    $this->createImageField($field_name, $type_name);
+
+    // RSS display must be added manually.
+    $this->drupalGet("admin/structure/types/manage/$type_name/display");
+    $edit = [
+      "display_modes_custom[rss]" => '1',
+    ];
+    $this->drupalPostForm(NULL, $edit, t('Save'));
+
+    // Change the format to 'RSS enclosure'.
+    $this->drupalGet("admin/structure/types/manage/$type_name/display/rss");
+    $edit = [
+      "fields[$field_name][type]" => 'file_rss_enclosure',
+      "fields[$field_name][region]" => 'content',
+    ];
+    $this->drupalPostForm(NULL, $edit, t('Save'));
+
+    $test_files = $this->drupalGetTestFiles('image');
+
+    // Create a new node with the uploaded image. Promote to frontpage needs to
+    // be set so this node will appear in the RSS feed.
+    $nid = $this->uploadNodeImage($test_files[0], $field_name, $type_name, $this->randomMachineName());
+    $node_storage->resetCache([$nid]);
+    $node = $node_storage->load($nid);
+    $node->promote = 1;
+    $node->save();
+
+    // Get the uploaded file from the node.
+    $node_file = File::load($node->{$field_name}->target_id);
+
+    // Check that the RSS enclosure appears in the RSS feed.
+    $this->drupalGet('rss.xml');
+    $uploaded_filename = str_replace('public://', '', $node_file->getFileUri());
+    $selector = sprintf(
+      'enclosure[@url="%s"][@length="%s"][@type="%s"]',
+      file_create_url("public://$uploaded_filename", ['absolute' => TRUE]),
+      $node_file->getSize(),
+      $node_file->getMimeType()
+    );
+    $this->assertNotNull($this->getSession()->getDriver()->find('xpath', $selector), 'Image field RSS enclosure is displayed when viewing the RSS feed.');
+  }
+
+}
