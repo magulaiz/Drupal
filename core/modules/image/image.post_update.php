78cf1375ceb0e2174d2c42496c4fde8261d8f4e9
Claudiu Cristea <clau.cristea@gmail.com>
2020-11-20 11:21:15 +0200

    Patch #2061377-386.

--- core/modules/image/image.post_update.php
+++ core/modules/image/image.post_update.php
@@ -8,6 +8,7 @@
 use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\image\ImageConfigUpdater;
+use Drupal\filter\FilterFormatInterface;
 
 /**
  * Implements hook_removed_post_updates().
@@ -26,5 +27,29 @@ function image_post_update_image_loading_attribute(?array &$sandbox = NULL): voi
   $image_config_updater = \Drupal::classResolver(ImageConfigUpdater::class);
   \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'entity_view_display', function (EntityViewDisplayInterface $view_display) use ($image_config_updater): bool {
     return $image_config_updater->processImageLazyLoad($view_display);
+  });
+}
+
+/**
+ * Update filter formats to allow the use of the image style filter.
+ */
+function image_post_update_enable_filter_image_style(array &$sandbox) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'filter_format', function (FilterFormatInterface $format) {
+    /** @var \Drupal\filter\Plugin\FilterInterface $filter */
+    if (!($filter = $format->filters('filter_html')) || !$filter->status) {
+      return FALSE;
+    }
+
+    $config = $filter->getConfiguration();
+    $allowed_html = !empty($config['settings']['allowed_html']) ? $config['settings']['allowed_html'] : NULL;
+    $matches = [];
+    if ($allowed_html && preg_match('/<img([^>]*)>/', $allowed_html, $matches)) {
+      $attributes = array_filter(preg_split('/\s/', $matches[1]));
+      $attributes[] = 'data-image-style';
+      $config['settings']['allowed_html'] = preg_replace('/<img([^>]*)>/', '<img ' . implode(' ', array_unique($attributes)) . '>', $allowed_html);
+      $format->setFilterConfig('filter_html', $config);
+      return TRUE;
+    }
+    return FALSE;
   });
 }
