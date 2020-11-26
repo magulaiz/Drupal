4c847c26e36ee5f82e8bdd69f978bd91cf11ad49
voleger <dev.vegera@gmail.com>
2020-11-26 18:18:19 +0200

    Issue #2059375 by alesr, voleger, Xilis, alansaviolobo, wturrell: Update notification messages as an option in settings

---
+++ core/modules/update/tests/src/Functional/UpdateSettingsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\update\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the Update Manager settings.
+ *
+ * @group update
+ */
+class UpdateSettingsTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['update_test', 'update', 'language', 'block'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $admin_user = $this->drupalCreateUser(['administer site configuration', 'administer modules', 'administer themes']);
+    $this->drupalLogin($admin_user);
+  }
+
+  /**
+   * Ensures that the notifications enabled by the default.
+   */
+  public function testNotificationsSetting() {
+    $this->drupalGet(Url::fromRoute('update.settings'));
+    // Checkbox should be checked so expected message on the page should be:
+    $this->assertText('Uncheck to hide system notifications for available updates. Hiding it may have security implications.');
+
+    $this->drupalPostForm(
+      Url::fromRoute('update.settings'),
+      ['update_system_notifications' => 0],
+      'Save configuration'
+    );
+
+    // Checkbox should be unchecked so expected message on the page should be:
+    $this->assertText('Check to show system notifications for available updates. Hiding it may have security implications.');
+  }
+
+}
