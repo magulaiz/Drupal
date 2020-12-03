3fffd1c65f0f77a3bbb1030f97ab5444cb62bd4a
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-03 19:56:41 +0200

    Modernize the post update script.

--- core/modules/node/node.post_update.php
+++ core/modules/node/node.post_update.php
@@ -6,7 +6,8 @@
  */
 
 use Drupal\Core\Field\Entity\BaseFieldOverride;
-use Drupal\node\Entity\NodeType;
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+use Drupal\node\NodeTypeInterface;
 use Drupal\views\Entity\View;
 
 /**
@@ -52,15 +53,14 @@ function node_post_update_glossary_view_published() {
 /**
  * Add plural label variants to node-type entities.
  */
-function node_post_update_plural_variants() {
-  /** @var \Drupal\node\NodeTypeInterface $node_type */
-  foreach (NodeType::loadMultiple() as $node_type) {
+function node_post_update_plural_variants(array &$sandbox): void {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'node_type', function (NodeTypeInterface $node_type): bool {
     $node_type
       ->set('label_singular', NULL)
       ->set('label_plural', NULL)
-      ->set('label_count', NULL)
-      ->save();
-  }
+      ->set('label_count', NULL);
+    return TRUE;
+  });
 }
 
 /**
