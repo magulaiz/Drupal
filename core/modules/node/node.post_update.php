8dcbc551020fad84fe6881a5bb17d12d61e21c21
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-03 19:56:41 +0200

    Modernize the post update script.

--- core/modules/node/node.post_update.php
+++ core/modules/node/node.post_update.php
@@ -5,7 +5,9 @@
  * Post update functions for Node.
  */
 
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\node\Entity\NodeType;
+use Drupal\node\NodeTypeInterface;
 
 /**
  * Implements hook_removed_post_updates().
@@ -24,13 +26,12 @@ function node_removed_post_updates() {
 /**
  * Add plural label variants to node-type entities.
  */
-function node_post_update_plural_variants() {
-  /** @var \Drupal\node\NodeTypeInterface $node_type */
-  foreach (NodeType::loadMultiple() as $node_type) {
+function node_post_update_plural_variants(array &$sandbox): void {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'node_type', function (NodeTypeInterface $node_type): bool {
     $node_type
       ->set('label_singular', NULL)
       ->set('label_plural', NULL)
-      ->set('label_count', NULL)
-      ->save();
-  }
+      ->set('label_count', NULL);
+    return TRUE;
+  });
 }
