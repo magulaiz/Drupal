1c663b386be83aa397818ddbe23633be21608ac9
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-03 17:50:49 +0200

    Improve documentation.

--- core/lib/Drupal/Core/Config/Entity/EntityBundleWithPluralLabelsInterface.php
+++ core/lib/Drupal/Core/Config/Entity/EntityBundleWithPluralLabelsInterface.php
@@ -11,7 +11,7 @@ interface EntityBundleWithPluralLabelsInterface {
    * Returns the singular label of the bundle.
    *
    * @return string|null
-   *   The singular label or NULL if it cannot be computed.
+   *   The singular label or NULL if it was not set.
    */
   public function getSingularLabel(): ?string;
 
@@ -19,7 +19,7 @@ interface EntityBundleWithPluralLabelsInterface {
    * Returns the plural label of the bundle.
    *
    * @return string|null
-   *   The plural label or NULL if it cannot be computed.
+   *   The plural label or NULL if it was not set.
    */
   public function getPluralLabel(): ?string;
 
@@ -28,12 +28,12 @@ interface EntityBundleWithPluralLabelsInterface {
    *
    * @param int $count
    *   The item count to display if the plural form was requested.
-   * @param string $context|null
-   *   (optional) The optional context of the count label. A bundle entity can
+   * @param string|null $variant
+   *   (optional) The optional variant of the count label. A bundle entity can
    *   define unlimited definite singular/plural count labels in order to cover
-   *   various contexts where they are used. Pass the context, as a string
-   *   identifier, to get appropriate version of the count label. Omit the
-   *   context if there's only one version of the definite singular/plural count
+   *   various contexts where they are used. Pass the variant, as a string
+   *   identifier, to get the appropriate version of the count label. Omit the
+   *   variant if there's only one version of the definite singular/plural count
    *   label. Defaults to NULL.
    *
    * @return string|null
