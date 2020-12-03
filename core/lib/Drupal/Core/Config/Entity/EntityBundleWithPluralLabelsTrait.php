ede95d9c2ac230892ab6e31e708a1d26b103bbd0
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-03 12:36:47 +0200

    Patch #2765065-101

---
+++ core/lib/Drupal/Core/Config/Entity/EntityBundleWithPluralLabelsTrait.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\Core\Config\Entity;
+
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Allows bundle configuration entities to support label plural variants.
+ */
+trait EntityBundleWithPluralLabelsTrait {
+
+  /**
+   * The indefinite singular name of the bundle.
+   *
+   * @var string
+   */
+  protected $label_singular;
+
+  /**
+   * The indefinite plural name of the bundle.
+   *
+   * @var string
+   */
+  protected $label_plural;
+
+  /**
+   * A list of definite singular/plural count label versions.
+   *
+   * Unlimited definite singular/plural count labels can be defined in order to
+   * cover various contexts where they are used. The array keys are strings,
+   * identifying the context. For example, a site might need two or more
+   * versions of the count labels:
+   * - singular '1 item', plural '@count items',
+   * - singular '1 item was found', plural '@count items were found'.
+   * For this case the value of this property is:
+   * @code
+   * [
+   *   'default' => "1 item\x03@count items",
+   *   'items_found' => "1 item was found\x03@count items were found",
+   * ]
+   * @endcode
+   * Note that the context ('default', 'items_found') is an arbitrary string
+   * identifier used to retrieve the desired version. If there's only one
+   * context, the context identifier can be omitted:
+   * @code
+   * [
+   *   "1 item\x03@count items",
+   * ]
+   * @endcode
+   * Each value is a definite singular/plural count label with the plural
+   * variants separated by ETX (PluralTranslatableMarkup::DELIMITER).
+   *
+   * @var string[]|null
+   *
+   * @see \Drupal\Core\StringTranslation\PluralTranslatableMarkup::DELIMITER
+   */
+  protected $label_count;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSingularLabel() {
+    // Provide a fallback in case label_singular is not set yet.
+    if (empty($this->label_singular)) {
+      if ($label = $this->label()) {
+        $this->label_singular = mb_strtolower($label);
+      }
+    }
+    return $this->label_singular;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluralLabel() {
+    // Provide a fallback in case label_plural is not set yet.
+    if (empty($this->label_plural)) {
+      if ($label = $this->label()) {
+        $arguments = ['@label' => mb_strtolower($label)];
+        $options = ['langcode' => $this->language()->getId()];
+        $this->label_plural = new TranslatableMarkup('@label items', $arguments, $options);
+      }
+    }
+    return $this->label_plural;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCountLabel($count, $context = NULL) {
+    $label_count_versions = (array) $this->label_count;
+
+    // If the context was not passed, pickup the first version of count label.
+    $context = $context ?: key($label_count_versions);
+
+    $index = static::getPluralIndex($count);
+    if ($index === -1) {
+      // If the index cannot be computed, fallback to a single plural variant.
+      $index = $count > 1 ? 1 : 0;
+    }
+
+    $label_count = empty($label_count_versions[$context]) ? [] : explode(PluralTranslatableMarkup::DELIMITER, $label_count_versions[$context]);
+    if (!empty($label_count[$index])) {
+      return new FormattableMarkup($label_count[$index], ['@count' => $count]);
+    }
+    if (($singular = $this->getSingularLabel()) && ($plural = $this->getPluralLabel())) {
+      $arguments = ['@singular' => $singular, '@plural' => $plural];
+      return new PluralTranslatableMarkup($count, '1 @singular', '@count @plural', $arguments);
+    }
+    return NULL;
+  }
+
+  /**
+   * Gets the plural index through the gettext formula.
+   *
+   * @param int $count
+   *   Number to return plural for.
+   *
+   * @return int
+   *   The numeric index of the plural variant to use for this $langcode and
+   *   $count combination or -1 if the language was not found or does not have a
+   *   plural formula.
+   *
+   * @todo Remove this method when https://www.drupal.org/node/2766857 gets in.
+   */
+  protected static function getPluralIndex($count) {
+    // We have to test both if the function and the service exist since in
+    // certain situations it is possible that locale code might be loaded but
+    // the service does not exist. For example, where the parent test site has
+    // locale installed but the child site does not.
+    // @todo Refactor in https://www.drupal.org/node/2660338 so this code does
+    //   not depend on knowing that the Locale module exists.
+    if (function_exists('locale_get_plural') && \Drupal::hasService('locale.plural.formula')) {
+      return locale_get_plural($count);
+    }
+    return -1;
+  }
+
+}
--- core/modules/node/node.post_update.php
+++ core/modules/node/node.post_update.php
@@ -5,6 +5,8 @@
  * Post update functions for Node.
  */
 
+use Drupal\node\Entity\NodeType;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -17,4 +19,18 @@ function node_removed_post_updates() {
     'node_post_update_modify_base_field_author_override' => '10.0.0',
     'node_post_update_set_node_type_description_and_help_to_null' => '11.0.0',
   ];
+}
+
+/**
+ * Add plural label variants to node-type entities.
+ */
+function node_post_update_plural_variants() {
+  /** @var \Drupal\node\NodeTypeInterface $node_type */
+  foreach (NodeType::loadMultiple() as $node_type) {
+    $node_type
+      ->set('label_singular', NULL)
+      ->set('label_plural', NULL)
+      ->set('label_count', NULL)
+      ->save();
+  }
 }
