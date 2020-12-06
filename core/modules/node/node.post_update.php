b6b56ca303ed4631076913f6ec407681ad54667e
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-06 11:35:57 +0200

    Add back the bundle entity getters.

--- core/modules/node/node.post_update.php
+++ core/modules/node/node.post_update.php
@@ -26,11 +26,7 @@ function node_removed_post_updates() {
  * Add plural label variants to node-type entities.
  */
 function node_post_update_plural_variants(array &$sandbox): void {
-  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'node_type', function (NodeTypeInterface $node_type): bool {
-    $node_type
-      ->set('label_singular', NULL)
-      ->set('label_plural', NULL)
-      ->set('label_count', NULL);
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'node_type', function (): bool {
     return TRUE;
   });
 }
