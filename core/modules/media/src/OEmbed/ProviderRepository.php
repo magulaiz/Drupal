4ab6363c8c36efc314121b19a35097cd4a7163ee
Phéna Proxima <adam@phenaproxima.net>
2020-12-06 11:19:48 -0500

    Fix bad variable assignment in ProviderRepository constructor.

--- core/modules/media/src/OEmbed/ProviderRepository.php
+++ core/modules/media/src/OEmbed/ProviderRepository.php
@@ -65,6 +65,16 @@ class ProviderRepository implements ProviderRepositoryInterface {
     $this->time = $time;
     $this->cacheBackend = $cache_backend;
     $this->maxAge = (int) $max_age;
+    if (!($key_value instanceof KeyValueFactoryInterface)) {
+      @trigger_error('The keyvalue service should be passed to ProviderRepository::__construct() since 9.2.0. This will be required in Drupal 10.0.0. See https://www.drupal.org/node/3186186', E_USER_DEPRECATED);
+      $key_value = \Drupal::service('keyvalue');
+    }
+    if (is_null($logger_factory)) {
+      @trigger_error('The logger.factory service should be passed to ProviderRepository::__construct() since 9.2.0. This will be required in Drupal 10.0.0. See https://www.drupal.org/node/3186186', E_USER_DEPRECATED);
+      $logger_factory = \Drupal::service('logger.factory');
+    }
+    $this->keyValue = $key_value->get('media.oembed');
+    $this->logger = $logger_factory->get('media');
   }
 
   /**
