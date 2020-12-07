21ea18dd9b41c89ffb9a7bb9f033d1cf64328503
Alexey Murz Korepov <MurzNN@gmail.com>
2020-12-08 00:56:48 +0300

    Keep initial value to pass EntityDefinitionUpdateTest

--- core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorageSchema.php
+++ core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorageSchema.php
@@ -2129,6 +2129,7 @@ class SqlContentEntityStorageSchema implements DynamicallyFieldableEntityStorage
       // Use the initial value of the field storage, if available.
       if ($initial_value && isset($initial_value[$field_column_name])) {
         $schema['fields'][$schema_field_name]['default'] = SqlContentEntityStorageSchema::castValue($column_schema, $initial_value[$field_column_name]);
+        $schema['fields'][$schema_field_name]['initial'] = SqlContentEntityStorageSchema::castValue($column_schema, $initial_value[$field_column_name]);
       }
       if (!empty($initial_value_from_field)) {
         $schema['fields'][$schema_field_name]['initial_from_field'] = $initial_value_from_field[$field_column_name];
