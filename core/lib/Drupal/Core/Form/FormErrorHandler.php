64563ed5e18f3768b20d3073e890ccfebf27ca28
Liam Morland <lkmorlan@uwaterloo.ca>
2020-12-11 12:24:56 -0500

    Issue #3083103: Set @aria-invalid when @aria-errormessage is set

--- core/lib/Drupal/Core/Form/FormErrorHandler.php
+++ core/lib/Drupal/Core/Form/FormErrorHandler.php
@@ -221,6 +221,7 @@ class FormErrorHandler implements FormErrorHandlerInterface {
     // Add aria-describedby attribute to the form element.
     if (($elements['#errors']) !== NULL) {
       $elements['#attributes']['aria-errormessage'] = $elements['#id'] . '--error-message';
+      $elements['#attributes']['aria-invalid'] = TRUE;
     }
   }
 
