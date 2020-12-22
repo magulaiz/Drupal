76b2487f906304e0e2169463cdad4fe23cdb3fda
voleger <dev.vegera@gmail.com>
2020-12-22 10:01:01 +0200

    Deprecate legacy definition of service_name for the specific defined queue

--- core/lib/Drupal/Core/Annotation/QueueWorker.php
+++ core/lib/Drupal/Core/Annotation/QueueWorker.php
@@ -63,4 +63,20 @@ class QueueWorker extends Plugin {
    */
   public $cron;
 
+  /**
+   * The service name which handle the defined reliable queue.
+   *
+   * @var string (optional)
+   */
+  public $queue_reliable_service;
+
+  /**
+   * The service name which handle the defined queue.
+   *
+   * The 'queue.database' service used by the default.
+   *
+   * @var string (optional)
+   */
+  public $queue_service = 'queue.database';
+
 }
