a146c2c02ecfad717d84c449718cbe609fe7d929
Sasa Jovanovic <office@sakiland.org>
2021-01-04 20:39:34 +0100

    Issue #3040556: It is not possible to react to an entity being duplicated
    Initial quick solution without crating hook support and move logic into entity storage.

--- core/tests/Drupal/KernelTests/Core/Entity/EntityDuplicateTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityDuplicateTest.php
@@ -67,7 +67,7 @@ class EntityDuplicateTest extends EntityKernelTestBase {
     $duplicate_entity->save();
 
     $this->assertNotEmpty($duplicate_entity->duplicateSource);
-    $this->assertEquals($duplicate_entity->duplicateSource, $entity);
+    $this->assertIdenticalObject($duplicate_entity->duplicateSource, $entity);
   }
 
 }
