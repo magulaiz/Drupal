1abfa05c397f25abfbe09ecb6e71a3b6a1945576
Dave Long <dave@longwaveconsulting.com>
2021-04-16 12:58:24 +0100

    Modernise test and fix failures.

--- core/modules/system/tests/src/Kernel/Theme/TwigDebugMarkupTest.php
+++ core/modules/system/tests/src/Kernel/Theme/TwigDebugMarkupTest.php
@@ -22,7 +22,7 @@ class TwigDebugMarkupTest extends KernelTestBase {
    *
    * @var array
    */
-  public static $modules = ['theme_test', 'node', 'system', 'user'];
+  protected static $modules = ['theme_test', 'node', 'system', 'user'];
 
   /**
    * The twig config to apply during container rebuils.
@@ -34,7 +34,7 @@ class TwigDebugMarkupTest extends KernelTestBase {
     'cache' => TRUE,
   ];
 
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     $this->installSchema('system', 'sequences');
@@ -49,7 +49,7 @@ class TwigDebugMarkupTest extends KernelTestBase {
   public function register(ContainerBuilder $container) {
     parent::register($container);
 
-    $container->setParameter('twig.config',  $this->twigConfig);
+    $container->setParameter('twig.config', $this->twigConfig);
   }
 
   /**
@@ -59,7 +59,7 @@ class TwigDebugMarkupTest extends KernelTestBase {
     /** @var \Drupal\Core\Render\RendererInterface $renderer */
     $renderer = $this->container->get('renderer');
     $extension = '.html.twig';
-    \Drupal::service('theme_handler')->install(['test_theme']);
+    \Drupal::service('theme_installer')->install(['test_theme']);
     $this->config('system.theme')->set('default', 'test_theme')->save();
 
     NodeType::create([
@@ -92,24 +92,24 @@ class TwigDebugMarkupTest extends KernelTestBase {
       'uid' => $user,
     ]);
     $node->save();
-    $build = node_view($node);
+    $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    $build = $builder->view($node);
     $output = $renderer->renderRoot($build);
-    $this->assertTrue(strpos($output, '<!-- THEME DEBUG -->') !== FALSE, 'Twig debug markup found in theme output when debug is enabled.');
-    $this->assertTrue(strpos($output, "THEME HOOK: 'node'") !== FALSE, 'Theme call information found.');
-    $this->assertContains(
-<<<'TWIG'
-* node--1--full.html.twig
-x node--1.html.twig
-* node--page--full.html.twig
-* node--page.html.twig
-* node--full.html.twig
-* node--{<script type="text/javascript">alert('yo');</script>.html.twig}
-* node.html.twig
-TWIG
-, (string) $output, 'Suggested template files found in order and node ID specific template shown as current template.');
-
+    $this->assertStringContainsString('<!-- THEME DEBUG -->', $output, 'Twig debug markup found in theme output when debug is enabled.');
+    $this->assertStringContainsString("THEME HOOK: 'node'", $output, 'Theme call information found.');
+    $escaped = Html::escape('node--<script type="text/javascript">alert(\'yo\');</script>');
+    $expected = <<<TWIG
+   * node--1--full$extension
+   x node--1$extension
+   * node--page--full$extension
+   * node--page$extension
+   * node--full$extension
+   * $escaped$extension
+   * node$extension
+TWIG;
+    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and node ID specific template shown as current template.');
     $template_filename = $templates['node__1']['path'] . '/' . $templates['node__1']['template'] . $extension;
-    $this->assertTrue(strpos($output, "BEGIN OUTPUT from '$template_filename'") !== FALSE, 'Full path to current template file found.');
+    $this->assertStringContainsString("BEGIN OUTPUT from '$template_filename'", $output, 'Full path to current template file found.');
 
     // Create another node and make sure the template suggestions shown in the
     // debug markup are correct.
@@ -119,9 +119,18 @@ TWIG
       'title' => 'test2',
     ]);
     $node2->save();
-    $build = node_view($node2);
+    $build = $builder->view($node2);
     $output = $renderer->renderRoot($build);
-    $this->assertTrue(strpos($output, '* node--2--full' . $extension . PHP_EOL . '   * node--2' . $extension . PHP_EOL . '   * node--page--full' . $extension . PHP_EOL . '   * node--page' . $extension . PHP_EOL . '   * node--full' . $extension . PHP_EOL . '   x node' . $extension) !== FALSE, 'Suggested template files found in order and base template shown as current template.');
+    $expected = <<<TWIG
+   * node--2--full$extension
+   * node--2$extension
+   * node--page--full$extension
+   * node--page$extension
+   * node--full$extension
+   * $escaped$extension
+   x node$extension
+TWIG;
+    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and base template shown as current template.');
 
     // Create another node and make sure the template suggestions shown in the
     // debug markup are correct.
@@ -132,21 +141,35 @@ TWIG
     ]);
     $node3->save();
     $build = ['#theme' => 'node__foo__bar'];
-    $build += node_view($node3);
+    $build += $builder->view($node3);
     $output = $renderer->renderRoot($build);
-    $this->assertTrue(strpos($output, "THEME HOOK: 'node__foo__bar'") !== FALSE, 'Theme call information found.');
-    $this->assertTrue(strpos($output, '* node--foo--bar' . $extension . PHP_EOL . '   * node--foo' . $extension . PHP_EOL . '   * node--&lt;script type=&quot;text/javascript&quot;&gt;alert(&#039;yo&#039;);&lt;/script&gt;' . $extension . PHP_EOL . '   * node--3--full' . $extension . PHP_EOL . '   * node--3' . $extension . PHP_EOL . '   * node--page--full' . $extension . PHP_EOL . '   * node--page' . $extension . PHP_EOL . '   * node--full' . $extension . PHP_EOL . '   x node' . $extension) !== FALSE, 'Suggested template files found in order and base template shown as current template.');
+    $this->assertStringContainsString("THEME HOOK: 'node__foo__bar'", $output, 'Theme call information found.');
+    $expected = <<<TWIG
+   * node--foo--bar$extension
+   * node--foo$extension
+   * node--3--full$extension
+   * node--3$extension
+   * node--page--full$extension
+   * node--page$extension
+   * node--full$extension
+   * $escaped$extension
+   x node$extension
+TWIG;
+    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and base template shown as current template.');
 
     // Disable debug, rebuild the service container, and clear all caches.
     $parameters = $this->container->getParameter('twig.config');
     $parameters['debug'] = FALSE;
-    $this->setContainerParameter('twig.config', $parameters);
-    $this->rebuildContainer();
-    $this->resetAll();
+    $this->twigConfig = $parameters;
 
-    $build = node_view($node);
+    $kernel = $this->container->get('kernel');
+    assert($kernel instanceof DrupalKernelInterface);
+    $kernel->rebuildContainer();
+    $this->container = $kernel->getContainer();
+
+    $build = $builder->view($node);
     $output = $renderer->renderRoot($build);
-    $this->assertFalse(strpos($output, '<!-- THEME DEBUG -->') !== FALSE, 'Twig debug markup not found in theme output when debug is disabled.');
+    $this->assertStringNotContainsString('<!-- THEME DEBUG -->', $output, 'Twig debug markup not found in theme output when debug is disabled.');
   }
 
 }
