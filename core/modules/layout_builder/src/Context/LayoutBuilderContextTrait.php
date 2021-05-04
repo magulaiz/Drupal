4da016b456333e4ec9bb277ea3c184e0bb8e4b5a
Claudiu Cristea <clau.cristea@gmail.com>
2021-05-04 09:54:41 +0200

    Add a 'section_delta' context
    Section::toRenderArray() should be aware of which section is being rendered. We
    pass the section's delta as context.

--- core/modules/layout_builder/src/Context/LayoutBuilderContextTrait.php
+++ core/modules/layout_builder/src/Context/LayoutBuilderContextTrait.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\layout_builder\Context;
 
+use Drupal\Core\Plugin\Context\Context;
+use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\Context\ContextInterface;
 use Drupal\layout_builder\SectionStorageInterface;
 
@@ -59,7 +61,7 @@ trait LayoutBuilderContextTrait {
    * @return \Drupal\Core\Plugin\Context\ContextInterface[]
    *   The array of context objects.
    */
-  protected function getPopulatedContexts(SectionStorageInterface $section_storage): array {
+  protected function getPopulatedContexts(SectionStorageInterface $section_storage, int $delta = NULL): array {
     // Get all known globally available contexts IDs.
     $available_context_ids = array_keys($this->contextRepository()->getAvailableContexts());
     // Filter to those that are populated.
@@ -69,6 +71,14 @@ trait LayoutBuilderContextTrait {
 
     // Add in the per-section_storage contexts.
     $contexts += $section_storage->getContextsDuringPreview();
+
+    if ($delta !== NULL) {
+      $contexts['section_delta'] = new Context(new ContextDefinition('integer'), $delta);
+    }
+    else {
+      @trigger_error('Calling LayoutBuilderContextTrait::getAvailableContexts() without the $delta argument is deprecated in drupal:9.2.0 and the $delta argument will be required in drupal:10.0.0. See https://www.drupal.org/node/3210520', E_USER_DEPRECATED);
+    }
+
     return $contexts;
   }
 
