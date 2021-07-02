a9b69ad71cdd1123146fd2ed1f55a94e6e0b8fd1
voleger <dev.vegera@gmail.com>
2021-07-02 12:04:26 +0300

    Fix PHPCS issues

--- core/lib/Drupal/Core/Batch/BatchProcessor.php
+++ core/lib/Drupal/Core/Batch/BatchProcessor.php
@@ -455,7 +455,7 @@ class BatchProcessor implements BatchProcessorInterface {
         // If possible, estimate remaining processing time.
         '@estimate'   => ($current > 0) ? $this->dateFormatter->formatInterval(($elapsed * ($total - $current) / $current) / 1000) : '-',
       ];
-      $message = strtr($progress_message, $values);
+      $message    = strtr($progress_message, $values);
       if (!empty($task_message)) {
         $label = $task_message;
       }
