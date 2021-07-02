3204cd3b5c73c2a88aa9b2db4e3858277806c842
voleger <dev.vegera@gmail.com>
2021-07-02 11:08:03 +0300

    Reroll #35

--- core/lib/Drupal/Core/Form/FormSubmitter.php
+++ core/lib/Drupal/Core/Form/FormSubmitter.php
@@ -60,7 +60,11 @@ class FormSubmitter implements FormSubmitterInterface {
       $batch['form_state'] = $form_state;
 
       $batch['progressive'] = !$form_state->isProgrammed();
-      $response = batch_process();
+
+      /** @var \Drupal\Core\Batch\BatchProcessorInterface $batch_processor */
+      $batch_processor = \Drupal::service('batch.processor');
+
+      $response = $batch_processor->process();
       // If the batch has been completed and _batch_finished() called then
       // $batch will be NULL.
       if ($batch && $batch['progressive']) {
@@ -150,7 +154,9 @@ class FormSubmitter implements FormSubmitterInterface {
    * Wraps batch_get().
    */
   protected function &batchGet() {
-    return batch_get();
+    /** @var \Drupal\Core\Batch\BatchProcessorInterface $batch_processor */
+    $batch_processor = \Drupal::service('batch.processor');
+    return $batch_processor->getCurrentBatch();
   }
 
 }
--- core/modules/config/src/Form/ConfigSingleImportForm.php
+++ core/modules/config/src/Form/ConfigSingleImportForm.php
@@ -421,6 +421,9 @@ class ConfigSingleImportForm extends ConfirmFormBase {
       return;
     }
 
+    /** @var \Drupal\Core\Batch\BatchProcessorInterface $batch_processor */
+    $batch_processor = \Drupal::service('batch.processor');
+
     /** @var \Drupal\Core\Config\ConfigImporter $config_importer */
     $config_importer = $form_state->get('config_importer');
     if ($config_importer->alreadyImporting()) {
@@ -438,7 +441,7 @@ class ConfigSingleImportForm extends ConfirmFormBase {
         foreach ($sync_steps as $sync_step) {
           $batch_builder->addOperation([ConfigImporterBatch::class, 'process'], [$config_importer, $sync_step]);
         }
-        batch_set($batch_builder->toArray());
+        $batch_processor->queue($batch_builder->toArray());
       }
       catch (ConfigImporterException $e) {
         // There are validation errors.
