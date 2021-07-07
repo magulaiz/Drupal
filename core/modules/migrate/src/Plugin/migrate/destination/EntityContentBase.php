808f1b72ce1374eccf8496521b0fca612522a803
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-07-07 16:15:30 +0100

    Fixed incorrect assumptions when default_bundle isn’t specified.

--- core/modules/migrate/src/Plugin/migrate/destination/EntityContentBase.php
+++ core/modules/migrate/src/Plugin/migrate/destination/EntityContentBase.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\FieldTypePluginManagerInterface;
 use Drupal\Core\Session\AccountSwitcherInterface;
@@ -115,6 +116,13 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
   protected $accountSwitcher;
 
   /**
+   * Entity type bundle info.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
+   */
+  protected $entityTypeBundleInfo;
+
+  /**
    * Constructs a content entity.
    *
    * @param array $configuration
@@ -135,8 +143,10 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
    *   The field type plugin manager service.
    * @param \Drupal\Core\Session\AccountSwitcherInterface $account_switcher
    *   The account switcher service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle info service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, EntityStorageInterface $storage, array $bundles, EntityFieldManagerInterface $entity_field_manager, FieldTypePluginManagerInterface $field_type_manager, AccountSwitcherInterface $account_switcher = NULL) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, EntityStorageInterface $storage, array $bundles, EntityFieldManagerInterface $entity_field_manager, FieldTypePluginManagerInterface $field_type_manager, AccountSwitcherInterface $account_switcher = NULL, EntityTypeBundleInfoInterface $entity_type_bundle_info) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $storage, $bundles);
     $this->entityFieldManager = $entity_field_manager;
     $this->fieldTypeManager = $field_type_manager;
@@ -145,6 +155,7 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
       $account_switcher = \Drupal::service('account_switcher');
     }
     $this->accountSwitcher = $account_switcher;
+    $this->entityTypeBundleInfo = $entity_type_bundle_info;
   }
 
   /**
@@ -161,7 +172,8 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
       array_keys($container->get('entity_type.bundle.info')->getBundleInfo($entity_type)),
       $container->get('entity_field.manager'),
       $container->get('plugin.manager.field.field_type'),
-      $container->get('account_switcher')
+      $container->get('account_switcher'),
+      $container->get('entity_type.bundle.info')
     );
   }
 
@@ -425,9 +437,33 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
       return [];
     }
 
-    $field_definitions = $this->entityFieldManager->getBaseFieldDefinitions($entity_type->id());
-    $bundle = empty($this->configuration['default_bundle']) ? $entity_type->id() : $this->configuration['default_bundle'];
-    $field_definitions += $this->entityFieldManager->getFieldDefinitions($entity_type->id(), $bundle);
+    // Try to determine the bundle to use when getting fields.
+    $bundle_info = $this->entityTypeBundleInfo->getBundleInfo($entity_type->id());
+
+    if (!empty($this->configuration['default_bundle'])) {
+      // If the migration destination configuration specifies a default_bundle,
+      // then use that.
+      $bundle = $this->configuration['default_bundle'];
+
+      if (!isset($bundle_info[$bundle])) {
+        throw new MigrateException(sprintf("The default_bundle value '%s' is not a valid bundle for the destination entity type '%s'.", $bundle, $entity_type->id()));
+      }
+    }
+    else {
+      // If the destination entity type has only one bundle, use that.
+      if (count($bundle_info) == 1) {
+        $bundle = array_key_first($bundle_info);
+      }
+    }
+
+    if (isset($bundle)) {
+      // If we have a bundle, get all the fields.
+      $this->entityFieldManager->getFieldDefinitions($entity_type->id(), $bundle);
+    }
+    else {
+      // Without a bundle, we can only get the base fields.
+      $field_definitions = $this->entityFieldManager->getBaseFieldDefinitions($entity_type->id());
+    }
 
     $fields = [];
     foreach ($field_definitions as $field_name => $definition) {
