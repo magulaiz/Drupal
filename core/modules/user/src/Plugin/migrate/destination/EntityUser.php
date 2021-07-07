d61470e58197ba17ebf4db7a565068cfc41d062c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-07-07 14:44:03 +0100

    Patch from Drupal.org. Comment: 77; URL: https://www.drupal.org/node/2630732#comment-13827131; file: 2630732-77.patch; fid: 6385202. Automatic commit by dorgflow.

--- core/modules/user/src/Plugin/migrate/destination/EntityUser.php
+++ core/modules/user/src/Plugin/migrate/destination/EntityUser.php
@@ -119,8 +119,7 @@ class EntityUser extends EntityContentBase {
       $container->get('entity_field.manager'),
       $container->get('plugin.manager.field.field_type'),
       $container->get('password'),
-      $container->get('account_switcher'),
-      $container->get('entity_field.manager')
+      $container->get('account_switcher')
     );
   }
 
