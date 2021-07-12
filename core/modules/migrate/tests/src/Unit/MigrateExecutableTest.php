2fb2b2a332059b74aef0fdcc43f136bdfed04858
Tim Plunkett <git@plnktt.com>
2021-07-12 09:31:26 -0400

    Patch from #107

--- core/modules/migrate/tests/src/Unit/MigrateExecutableTest.php
+++ core/modules/migrate/tests/src/Unit/MigrateExecutableTest.php
@@ -12,6 +12,7 @@ use Drupal\migrate\Plugin\MigrateIdMapInterface;
 use Drupal\migrate\MigrateException;
 use Drupal\migrate\Row;
 use Prophecy\Argument;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * @coversDefaultClass \Drupal\migrate\MigrateExecutable
@@ -431,6 +432,23 @@ class MigrateExecutableTest extends MigrateTestCase {
     }
     $this->assertCount(2, $row->getDestination());
     $this->assertSame(['test2'], $row->getEmptyDestinationProperties());
+  }
+
+  /**
+   * Tests error is triggered when using a deprecated migration.
+   *
+   * @group legacy
+   */
+  public function testMigrationDeprecation() {
+    $this->expectDeprecation('foo');
+
+    $this->migration->method('isDeprecated')
+      ->willReturn(TRUE);
+    $this->migration->method('getDeprecationMessage')
+      ->willReturn('foo');
+
+    $event_dispatcher = $this->createMock(EventDispatcherInterface::class);
+    new TestMigrateExecutable($this->migration, $this->message, $event_dispatcher);
   }
 
   /**
