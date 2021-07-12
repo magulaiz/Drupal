93ad275ed2c72e59eaa1083f619b6dcea0637f73
Tim Plunkett <git@plnktt.com>
2021-07-12 15:45:37 -0400

    Replace specific conditional with one checking deprecation message

--- core/modules/block/src/BlockForm.php
+++ core/modules/block/src/BlockForm.php
@@ -237,6 +237,12 @@ class BlockForm extends EntityForm {
         continue;
       }
 
+      // Don't display the deprecated node type condition unless it has existing
+      // settings.
+      if (isset($definition['deprecation_message']) && !isset($visibility[$condition_id])) {
+        continue;
+      }
+
       /** @var \Drupal\Core\Condition\ConditionInterface $condition */
       $condition = $this->manager->createInstance($condition_id, $visibility[$condition_id] ?? []);
       $form_state->set(['conditions', $condition_id], $condition);
