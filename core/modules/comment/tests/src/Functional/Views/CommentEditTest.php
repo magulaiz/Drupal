b088f87d8893109352dc18fbc84dac9863c1465e
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2021-07-23 16:51:12 -0400

    Issue #3219959 adjust expected value of submit button in CommentEditTest

--- core/modules/comment/tests/src/Functional/Views/CommentEditTest.php
+++ core/modules/comment/tests/src/Functional/Views/CommentEditTest.php
@@ -22,7 +22,7 @@ class CommentEditTest extends CommentBrowserTestBase {
   public function testCommentEdit() {
     $this->drupalLogin($this->adminUser);
     // Post a comment to node.
-    $node_comment = $this->postComment($this->node, $this->randomMachineName(), $this->randomMachineName(), TRUE);
+    $node_comment = $this->postComment($this->node, $this->randomMachineName(), $this->randomMachineName(), TRUE, 'comment', 'Post comment');
     $this->drupalGet('admin/content/comment');
     $this->assertSession()->pageTextContains($this->adminUser->label());
     $this->drupalGet($node_comment->toUrl('edit-form'));
