0fc038cc0c1301b808193c87bd5e7fd31e400abd
Frank Ebbers <sheetmusicfrank!@gmail.com>
2021-07-25 16:10:53 +0200

    Remove most of the changes in interdiff 55-59

--- core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
@@ -558,7 +558,6 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
     $expected = $this->getExpectedNormalizedEntity();
     static::recursiveKSort($expected);
     $actual = $this->serializer->decode((string) $response->getBody(), static::$format);
-    $actual = static::castToString($actual);
     static::recursiveKSort($actual);
     $this->assertEqualsCanonicalizing($expected, $actual);
 
@@ -1422,8 +1421,7 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
           // as strings in PHP memory.
           $expected_stored_data = static::castToString($expected_stored_data);
         }
-        $expected_field_normalization = static::castToString($field_normalization);
-        $this->assertEntityArraySubset($expected_field_normalization, static::castToString($this->serializer->normalize($modified_entity->get($field_name)->getValue())));
+        $this->assertEntityArraySubset($expected_stored_data, $modified_entity->get($field_name)->getValue());
       }
     }
   }
