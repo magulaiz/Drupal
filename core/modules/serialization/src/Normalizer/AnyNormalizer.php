4faec2e4adc88dd1b22c10ecbbc22cba2290988f
Frank Ebbers <sheetmusicfrank!@gmail.com>
2021-07-26 16:11:58 +0200

    Fixed comment typo

--- core/modules/serialization/src/Normalizer/AnyNormalizer.php
+++ core/modules/serialization/src/Normalizer/AnyNormalizer.php
@@ -21,7 +21,7 @@ class AnyNormalizer extends NormalizerBase {
     $this->addCacheableDependency($context, $object);
     $value = $object->getValue();
     // If the value is object or array, continue normalize it, object must be
-    // normalizable. Traversable object is one of the object which can be
+    // normalizable. Traversable object is one of the objects which can be
     // normalized, field type with typed data 'any' property definition which
     // stored the traversable object is advised to convert posted data to object
     // in setValue(), so that data can be denormalized automatically without
