55ec0466c1581bd4c1f3b52608cfedef3c298374
Dezső BICZÓ <mxr576@gmail.com>
2021-07-27 15:16:20 +0200

    Fix MenuLinkContentCacheabilityBubblingTest

--- core/modules/menu_link_content/tests/src/Kernel/MenuLinkContentCacheabilityBubblingTest.php
+++ core/modules/menu_link_content/tests/src/Kernel/MenuLinkContentCacheabilityBubblingTest.php
@@ -146,7 +146,7 @@ class MenuLinkContentCacheabilityBubblingTest extends KernelTestBase {
     $build = $menu_tree->build($tree);
     $renderer->renderRoot($build);
     $expected_cacheability = $expected_cacheability->merge($default_menu_cacheability);
-    $this->assertEquals($expected_cacheability, BubbleableMetadata::createFromRenderArray($build));
+    $this->assertEqualsCanonicalizing($expected_cacheability, BubbleableMetadata::createFromRenderArray($build));
   }
 
 }
