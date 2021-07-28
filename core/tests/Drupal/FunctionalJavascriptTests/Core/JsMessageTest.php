d9fba817f08b077aa5f4bc3509a44879b96817b3
Marcos Cano <marcoscano@1288796.no-reply.drupal.org>
2021-07-28 10:35:17 +0200

    3212975: Inline the loop to compare message strings instead of having a separate method for this

--- core/tests/Drupal/FunctionalJavascriptTests/Core/JsMessageTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Core/JsMessageTest.php
@@ -118,29 +118,12 @@ class JsMessageTest extends WebDriverTestBase {
         $current_messages[] = $message_div->getText();
       }
     }
-    $this->assertStringsArrayContainsStringsArray($expected_messages, $current_messages);
-  }
-
-  /**
-   * Applies ::assertStringContainsString() to arrays of needles + haystacks.
-   *
-   * This will loop over all elements of the arrays passed in, asserting that
-   * the N-th element of the $haystacks array contains the string defined in
-   * the N-th element of the $needles array.
-   *
-   * @param array $needles
-   *   An indexed array of strings to be used as needle.
-   * @param array $haystacks
-   *   An indexed array of strings to be used as haystack.
-   */
-  protected function assertStringsArrayContainsStringsArray(array $needles, array $haystacks) {
-    $needles = array_values($needles);
-    $haystacks = array_values($haystacks);
-    if (count($needles) !== count($haystacks)) {
-      $this->fail('The needles array contains a different number of values than the haystacks array.');
+    // Check that each message text contains the expected text.
+    if (count($expected_messages) !== count($current_messages)) {
+      $this->fail('The expected messages array contains a different number of values than the current messages array.');
     }
-    for ($i = 0; $i < count($needles); $i++) {
-      $this->assertStringContainsString($needles[$i], $haystacks[$i]);
+    for ($i = 0; $i < count($expected_messages); $i++) {
+      $this->assertStringContainsString($expected_messages[$i], $current_messages[$i]);
     }
   }
 
