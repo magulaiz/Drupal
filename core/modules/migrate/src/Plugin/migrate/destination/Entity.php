dc46564b71d8771964a83e41f5f8c084bbf049cd
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-08-02 12:58:39 +0100

    Simplified wording.

--- core/modules/migrate/src/Plugin/migrate/destination/Entity.php
+++ core/modules/migrate/src/Plugin/migrate/destination/Entity.php
@@ -16,8 +16,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * Available configuration keys:
  * - default_bundle: (optional) The bundle to use for this row if 'bundle' is
- *   not defined on the row. Note that setting this also allows the fields()
- *   method to return bundle fields as well as base fields.
+ *   not defined on the row. Setting this also allows the fields() method to
+ *   return bundle fields as well as base fields.
  *
  * Examples:
  *
