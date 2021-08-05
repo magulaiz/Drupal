2debc55a5cd9e2cbb72d02239bdc94d112da0b4f
Dave Long <dave@longwaveconsulting.com>
2021-08-05 12:34:35 +0100

    Move _filter_html_escape() to plugin.

--- core/modules/filter/src/Plugin/Filter/FilterHtmlEscape.php
+++ core/modules/filter/src/Plugin/Filter/FilterHtmlEscape.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\filter\Plugin\Filter;
 
+use Drupal\Component\Utility\Html;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
 
@@ -21,7 +22,7 @@ class FilterHtmlEscape extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
-    return new FilterProcessResult(_filter_html_escape($text));
+    return new FilterProcessResult(trim(Html::escape($text)));
   }
 
   /**
