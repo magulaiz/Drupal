2354e2d13d689677500250058747ce0231457790
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-08-11 11:27:24 +0100

    Added @covers to tests.

--- core/tests/Drupal/Tests/Core/Entity/Sql/SqlContentEntityStorageTest.php
+++ core/tests/Drupal/Tests/Core/Entity/Sql/SqlContentEntityStorageTest.php
@@ -1449,6 +1449,8 @@ class SqlContentEntityStorageTest extends UnitTestCase {
    * @param mixed $bad_id
    *   A badly-formed entity ID.
    *
+   * @covers ::loadMultiple
+   *
    * @dataProvider providerTestLoadMultipleException
    */
   public function testLoadMultipleException($bad_id) {
