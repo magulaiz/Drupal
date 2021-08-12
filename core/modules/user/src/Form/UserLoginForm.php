ad7d8987f0d8a697edb973714a1a544d4cdc827f
Brent <brent@dropsolid.com>
2021-08-12 15:32:39 +0200

    remove username query parameter

--- core/modules/user/src/Form/UserLoginForm.php
+++ core/modules/user/src/Form/UserLoginForm.php
@@ -246,7 +246,7 @@ class UserLoginForm extends FormBase {
         // handlers that ran earlier than this one.
         $user_input = $form_state->getUserInput();
         $query = isset($user_input['name']) ? ['name' => $user_input['name']] : [];
-        $form_state->setErrorByName('name', $this->t('Unrecognized username or password. <a href=":password">Forgot your password?</a>', [':password' => Url::fromRoute('user.pass', [], ['query' => $query])->toString()]));
+        $form_state->setErrorByName('name', $this->t('Unrecognized username or password. <a href=":password">Forgot your password?</a>', [':password' => Url::fromRoute('user.pass')->toString()]));
         $accounts = $this->userStorage->loadByProperties(['name' => $form_state->getValue('name')]);
         if (!empty($accounts)) {
           $this->logger('user')->notice('Login attempt failed for %user.', ['%user' => $form_state->getValue('name')]);
