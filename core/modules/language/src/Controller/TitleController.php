2ef69c1f4c010de510ac97028d103f58f99e7c37
pmagunia <pmagunia@763420.no-reply.drupal.org>
2021-08-18 20:31:48 -0400

    Issue #2228799: Change page title "Content language" when Content Translation module is enabled

---
+++ core/modules/language/src/Controller/TitleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\language\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Return response for Language route.
+ */
+class TitleController extends ControllerBase {
+
+  /**
+   * The _title_callback for the language.content_settings_page route.
+   *
+   * @return string
+   *   The page title.
+   */
+  public function setTitle() {
+    if (\Drupal::moduleHandler()->moduleExists('content_translation')) {
+      return t('Content language and translation');
+    }
+    else {
+      return t('Content language'); 
+    }
+  }
+
+}
