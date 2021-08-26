9c3b6e3fd5c3f96e8e0cf20ead2dc892f7a85160
mondrake <mondrake.org@gmail.com>
2021-08-26 10:02:16 +0200

    10

--- core/lib/Drupal/Core/Database/Driver/pgsql/Schema.php
+++ core/lib/Drupal/Core/Database/Driver/pgsql/Schema.php
@@ -289,11 +289,12 @@ EOD;
 
     $sql_keys = [];
     if (!empty($table['primary key']) && is_array($table['primary key'])) {
-      $this->ensureNotNullPrimaryKey($table['primary key'], $table['fields']);
+      $this->validatePrimaryKeySchema($table['primary key'], $table['fields']);
       $sql_keys[] = 'CONSTRAINT ' . $this->ensureIdentifiersLength($name, '', 'pkey') . ' PRIMARY KEY (' . $this->createPrimaryKeySql($table['primary key']) . ')';
     }
     if (isset($table['unique keys']) && is_array($table['unique keys'])) {
       foreach ($table['unique keys'] as $key_name => $key) {
+        $this->validateUniqueKeySchema($key, $table['fields']);
         $sql_keys[] = 'CONSTRAINT ' . $this->ensureIdentifiersLength($name, $key_name, 'key') . ' UNIQUE (' . implode(', ', $key) . ')';
       }
     }
@@ -646,9 +647,15 @@ EOD;
     }
 
     // Fields that are part of a PRIMARY KEY must be added as NOT NULL.
-    $is_primary_key = isset($new_keys['primary key']) && in_array($field, $new_keys['primary key'], TRUE);
+    $is_primary_key = isset($new_keys['primary key']) && in_array($field, $this->fieldNames($new_keys['primary key']), TRUE);
     if ($is_primary_key) {
-      $this->ensureNotNullPrimaryKey($new_keys['primary key'], [$field => $spec]);
+      $this->validatePrimaryKeySchema($new_keys['primary key'], [$field => $spec]);
+    }
+
+    if (!empty($new_keys['unique keys'])) {
+      foreach ($new_keys['unique keys'] as $key => $key_fields) {
+        $this->validateUniqueKeySchema($key_fields, [$field => $spec]);
+      }
     }
 
     $fixnull = FALSE;
@@ -899,8 +906,13 @@ EOD;
     if (($field != $field_new) && $this->fieldExists($table, $field_new)) {
       throw new SchemaObjectExistsException("Cannot rename field '$table.$field' to '$field_new': target field already exists.");
     }
-    if (isset($new_keys['primary key']) && in_array($field_new, $new_keys['primary key'], TRUE)) {
-      $this->ensureNotNullPrimaryKey($new_keys['primary key'], [$field_new => $spec]);
+    if (isset($new_keys['primary key']) && in_array($field_new, $this->fieldNames($new_keys['primary key']), TRUE)) {
+      $this->validatePrimaryKeySchema($new_keys['primary key'], [$field_new => $spec]);
+    }
+    if (!empty($new_keys['unique keys'])) {
+      foreach ($new_keys['unique keys'] as $key => $key_fields) {
+        $this->validateUniqueKeySchema($key_fields, [$field_new => $spec]);
+      }
     }
 
     $spec = $this->processField($spec);
