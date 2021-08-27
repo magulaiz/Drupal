735d4b945089d06ff68f91f8459c738a6c1d57d9
voleger <dev.vegera@gmail.com>
2021-08-27 16:44:59 +0300

    Add permissions to the update.module to hide warnings

--- core/modules/update/tests/src/Functional/UpdateSemverCoreTest.php
+++ core/modules/update/tests/src/Functional/UpdateSemverCoreTest.php
@@ -364,6 +364,15 @@ class UpdateSemverCoreTest extends UpdateSemverTestBase {
     $this->drupalGet('admin/modules');
     $this->assertSession()->pageTextContains('There are updates available for your version of Drupal.');
     $this->assertSession()->pageTextNotContains('There is a security update available for your version of Drupal.');
+
+    // Test missing "updates notification" permission for the user.
+    $this->drupalLogin($this->drupalCreateUser([
+      'administer site configuration',
+      'administer modules',
+      'administer themes',
+    ]));
+    $this->drupalGet('admin/modules');
+    $this->assertSession()->pageTextNotContains('There are updates available for your version of Drupal.');
   }
 
   /**
--- core/modules/update/tests/src/Functional/UpdateSemverTestBase.php
+++ core/modules/update/tests/src/Functional/UpdateSemverTestBase.php
@@ -41,6 +41,7 @@ abstract class UpdateSemverTestBase extends UpdateTestBase {
     parent::setUp();
     $admin_user = $this->drupalCreateUser([
       'administer site configuration',
+      'updates notifications',
     ]);
     $this->drupalLogin($admin_user);
     $this->drupalPlaceBlock('local_actions_block');
