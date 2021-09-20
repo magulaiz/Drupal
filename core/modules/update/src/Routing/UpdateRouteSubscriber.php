82e683e1793e0f35f9eeedaf688f9d91cf93d1a7
Ted Bowman <ted@tedbow.com>
2021-09-20 11:39:04 -0400

    change to _update_message with skip or verbose values

--- core/modules/update/src/Routing/UpdateRouteSubscriber.php
+++ core/modules/update/src/Routing/UpdateRouteSubscriber.php
@@ -6,7 +6,7 @@ use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
- * Sets the _omit_update_message for the system.status routes.
+ * Sets the _update_message route option for system routes.
  */
 class UpdateRouteSubscriber extends RouteSubscriberBase {
 
@@ -14,8 +14,17 @@ class UpdateRouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    $status_route = $collection->get('system.status');
-    $status_route->setOption('_omit_update_message', TRUE);
+    // The status report page already displays the available updates.
+    if ($status_route = $collection->get('system.status')) {
+      $status_route->setOption('_update_message', 'skip');
+    }
+    // If we are on the appearance or modules list, display a detailed report
+    // of the update status.
+    foreach (['system.modules_list', 'system.themes_page'] as $verbose_route) {
+      if ($route = $collection->get($verbose_route)) {
+        $route->setOption('_update_message', 'verbose');
+      }
+    }
   }
 
 }
