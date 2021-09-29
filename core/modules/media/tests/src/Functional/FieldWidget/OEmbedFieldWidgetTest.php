ba52799cfb7164eb0dd68163b4c45b795bc96587
Phéna Proxima <adam@phenaproxima.net>
2021-09-29 15:47:53 -0400

    Streamline the test

--- core/modules/media/tests/src/Functional/FieldWidget/OEmbedFieldWidgetTest.php
+++ core/modules/media/tests/src/Functional/FieldWidget/OEmbedFieldWidgetTest.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\Tests\media\Functional\FieldWidget;
 
-use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\field\Entity\FieldConfig;
-use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\Tests\media\Functional\MediaFunctionalTestBase;
 
 /**
@@ -17,15 +15,6 @@ class OEmbedFieldWidgetTest extends MediaFunctionalTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = [
-    'field_ui',
-    'link',
-    'media_test_oembed',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
   protected $defaultTheme = 'stark';
 
   /**
@@ -33,13 +22,7 @@ class OEmbedFieldWidgetTest extends MediaFunctionalTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-
-    \Drupal::configFactory()
-      ->getEditable('media.settings')
-      ->set('standalone_url', TRUE)
-      ->save(TRUE);
-
-    $this->container->get('router.builder')->rebuild();
+    $this->config('media.settings')->set('standalone_url', TRUE)->save();
   }
 
   /**
@@ -49,32 +32,17 @@ class OEmbedFieldWidgetTest extends MediaFunctionalTestBase {
     $this->drupalLogin($this->rootUser);
 
     $media_type = $this->createMediaType('oembed:video');
+    $source_field = $media_type->getSource()
+      ->getSourceFieldDefinition($media_type)
+      ->getName();
 
-    $field_storage = FieldStorageConfig::create([
-      'field_name' => 'field_media_oembed_video_test',
-      'entity_type' => 'media',
-      'type' => 'string',
-      'cardinality' => 1,
-    ]);
-    $field_storage->save();
-    FieldConfig::create([
-      'field_storage' => $field_storage,
-      'bundle' => $media_type->id(),
-      'description' => 'This is help text for oEmbed field.',
-    ])->save();
-
-    $entity_form_display = EntityFormDisplay::load("media.{$media_type->id()}.default");
-    $entity_form_display->setComponent('field_media_oembed_video_test', [
-      'type' => 'oembed_textfield',
-      'settings' => [
-        'size' => 60,
-        'region' => 'content',
-      ],
-    ]);
-    $entity_form_display->save();
+    /** @var \Drupal\field\Entity\FieldConfig $field */
+    $field = FieldConfig::loadByName('media', $media_type->id(), $source_field);
+    $field->setDescription('This is help text for oEmbed field.')
+      ->save();
 
     $this->drupalGet('media/add/' . $media_type->id());
-    $this->assertSession()->pageTextContains('This is help text for oEmbed field.');
+    $this->assertSession()->pageTextContains($field->getDescription());
   }
 
 }
