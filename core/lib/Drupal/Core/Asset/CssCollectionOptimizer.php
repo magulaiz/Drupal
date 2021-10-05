a5ef56615bb02d760fe063b003eb0fd1fdd0c5c0
Théodore BIADALA <theodore@biadala.net>
2021-10-05 10:00:09 +0200

    woops

--- core/lib/Drupal/Core/Asset/CssCollectionOptimizer.php
+++ core/lib/Drupal/Core/Asset/CssCollectionOptimizer.php
@@ -124,7 +124,7 @@ class CssCollectionOptimizer implements AssetCollectionOptimizerInterface {
               // http://www.w3.org/TR/REC-CSS2/cascade.html#at-import, @import
               // rules must precede any other style, so we move those to the
               // top.
-              $regexp = '/@import\s*(\'(?:\\\'|.)*\'|"(?:\\"|.)*"|url\(\s*(?:\\[\)\\\'\"]|[^\'")])*\s*\)|url\(\s*\'(?:\\\'|.)*\'\s*\)|url\(\s*"(?:\\"|.)*"\s*\)).*;/ig';
+              $regexp = '/@import\s*(?:\'(?:\\\'|.)*\'|"(?:\\"|.)*"|url\(\s*(?:\\[\)\\\'\"]|[^\'")])*\s*\)|url\(\s*\'(?:\\\'|.)*\'\s*\)|url\(\s*"(?:\\"|.)*"\s*\)).*;/igU';
               preg_match_all($regexp, $data, $matches);
               $data = preg_replace($regexp, '', $data);
               $data = implode('', $matches[0]) . $data;
