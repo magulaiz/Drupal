aec91268c74482401b759e8f14fc67a3e123e775
mondrake <mondrake.org@gmail.com>
2021-10-06 19:29:01 +0200

    Move more code into ::attachDatabase()

--- core/lib/Drupal/Core/Database/Driver/sqlite/Connection.php
+++ core/lib/Drupal/Core/Database/Driver/sqlite/Connection.php
@@ -87,27 +87,16 @@ class Connection extends DatabaseConnection {
     // Attach one database for each registered prefix.
     $prefixes = $this->prefixes;
     foreach ($prefixes as &$prefix) {
-      // Empty prefix means query the main database -- no need to attach anything.
-      if (!empty($prefix)) {
-        // Only attach the database once.
-        if (!isset($this->attachedDatabases[$prefix])) {
-          if ($connection_options['database'] === ':memory:') {
-            // In memory database use ':memory:' as database name. According to
-            // http://www.sqlite.org/inmemorydb.html it will open a unique
-            // database so attaching it twice is not a problem.
-            $this->attachedDatabases[$prefix] = $prefix;
-            $this->query('ATTACH DATABASE :database AS :prefix', [':database' => $connection_options['database'], ':prefix' => $prefix]);
-          }
-          else {
-            $this->attachDatabase($prefix);
-          }
-        }
-
+      // Empty prefix means query the main database -- no need to attach
+      // anything.
+      if ($prefix !== '') {
+        $this->attachDatabase($prefix);
         // Add a ., so queries become prefix.table, which is proper syntax for
         // querying an attached database.
         $prefix .= '.';
       }
     }
+    
     // Regenerate the prefixes replacement table.
     $this->setPrefix($prefixes);
   }
@@ -215,8 +204,19 @@ class Connection extends DatabaseConnection {
    * {@inheritdoc}
    */
   public function attachDatabase(string $database): void {
-    $this->attachedDatabases[$database] = $database;
-    $this->query('ATTACH DATABASE :database_file AS :database', [':database_file' => $this->connectionOptions['database'] . '-' . $database, ':database' => $database]);
+    // Only attach the database once.
+    if (!isset($this->attachedDatabases[$database])) {
+      $this->attachedDatabases[$database] = $database;
+      if ($this->connectionOptions['database'] === ':memory:') {
+        // In memory database use ':memory:' as database name. According to
+        // http://www.sqlite.org/inmemorydb.html it will open a unique
+        // database so attaching it twice is not a problem.
+        $this->query('ATTACH DATABASE :database_file AS :database', [':database_file' => $this->connectionOptions['database'], ':database' => $database]);
+      }
+      else {
+        $this->query('ATTACH DATABASE :database_file AS :database', [':database_file' => $this->connectionOptions['database'] . '-' . $database, ':database' => $database]);
+      }
+    }
   }
 
   /**
