d93f05eaa12c1b32cf63c2fcc0e4daa29adb9fb1
Chris Burge <ChrisBurge@1826152.no-reply.drupal.org>
2021-10-06 18:23:18 -0500

    Remove state API from Drupal\Tests\Core\Extension\ThemeExtensionListTest

--- core/tests/Drupal/Tests/Core/Extension/ThemeExtensionListTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ThemeExtensionListTest.php
@@ -13,7 +13,6 @@ use Drupal\Core\Extension\ThemeEngineExtensionList;
 use Drupal\Core\Extension\ThemeExtensionList;
 use Drupal\Core\KeyValueStore\KeyValueMemoryFactory;
 use Drupal\Core\Lock\NullLockBackend;
-use Drupal\Core\State\State;
 use Drupal\Tests\UnitTestCase;
 use Prophecy\Argument;
 
@@ -65,8 +64,6 @@ class ThemeExtensionListTest extends UnitTestCase {
       ->alter('system_info', Argument::type('array'), Argument::type(Extension::class), Argument::any())
       ->shouldBeCalled();
 
-    $state = new State(new KeyValueMemoryFactory(), new MemoryBackend(), new NullLockBackend());
-
     $config_factory = $this->getConfigFactoryStub([
       'core.extension' => [
         'module' => [],
@@ -78,10 +75,10 @@ class ThemeExtensionListTest extends UnitTestCase {
       ],
     ]);
 
-    $theme_engine_list = new TestThemeEngineExtensionList($this->root, 'theme_engine', new NullBackend('test'), $info_parser->reveal(), $module_handler->reveal(), $state, $config_factory, 'testing');
+    $theme_engine_list = new TestThemeEngineExtensionList($this->root, 'theme_engine', new NullBackend('test'), $info_parser->reveal(), $module_handler->reveal(), $config_factory, 'testing');
     $theme_engine_list->setExtensionDiscovery($extension_discovery->reveal());
 
-    $theme_list = new TestThemeExtensionList($this->root, 'theme', new NullBackend('test'), $info_parser->reveal(), $module_handler->reveal(), $state, $config_factory, $theme_engine_list, 'testing');
+    $theme_list = new TestThemeExtensionList($this->root, 'theme', new NullBackend('test'), $info_parser->reveal(), $module_handler->reveal(), $config_factory, $theme_engine_list, 'testing');
     $theme_list->setExtensionDiscovery($extension_discovery->reveal());
 
     $theme_data = $theme_list->reset()->getList();
@@ -121,10 +118,9 @@ class ThemeExtensionListTest extends UnitTestCase {
   public function testGetBaseThemes(array $themes, $theme, array $expected) {
     // Mocks and stubs.
     $module_handler = $this->prophesize(ModuleHandlerInterface::class);
-    $state = new State(new KeyValueMemoryFactory(), new MemoryBackend(), new NullLockBackend());
     $config_factory = $this->getConfigFactoryStub([]);
     $theme_engine_list = $this->prophesize(ThemeEngineExtensionList::class);
-    $theme_listing = new ThemeExtensionList($this->root, 'theme', new NullBackend('test'), new InfoParser($this->root), $module_handler->reveal(), $state, $config_factory, $theme_engine_list->reveal(), 'test');
+    $theme_listing = new ThemeExtensionList($this->root, 'theme', new NullBackend('test'), new InfoParser($this->root), $module_handler->reveal(), $config_factory, $theme_engine_list->reveal(), 'test');
 
     $base_themes = $theme_listing->getBaseThemes($themes, $theme);
 
