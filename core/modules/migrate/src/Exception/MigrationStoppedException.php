2506e1260856e29aa53a2d7cd29472d0a114a7ea
KenowaX <arthur@webstanz.be>
2021-10-07 17:49:51 +0200

    Adding full stop to comment
    Contents :
    
    - MigrationStoppedException.php
    
    Description needs full stop at the end.

--- core/modules/migrate/src/Exception/MigrationStoppedException.php
+++ core/modules/migrate/src/Exception/MigrationStoppedException.php
@@ -5,7 +5,7 @@ namespace Drupal\migrate\Exception;
 use Drupal\migrate\MigrateException;
 
 /**
- * To throw when Migration was requested to stop by user
+ * To throw when Migration was requested to stop by user.
  */
 class MigrationStoppedException extends MigrateException {
 
