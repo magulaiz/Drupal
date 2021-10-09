dd0c20379d53a3c1f1b3aa1f2e5b0dc0afa66c06
daffie <daffie@116368.no-reply.drupal.org>
2021-10-09 16:15:49 +0000

    Update DeprecatedJqueryUiAssetsTest.php for the hash of jquery.ui.widget.

--- core/tests/Drupal/KernelTests/Core/Asset/DeprecatedJqueryUiAssetsTest.php
+++ core/tests/Drupal/KernelTests/Core/Asset/DeprecatedJqueryUiAssetsTest.php
@@ -29,7 +29,7 @@ class DeprecatedJqueryUiAssetsTest extends KernelTestBase {
       'jquery.ui.mouse' => '8251c5dcf578abcc4d009c1f78ad0112',
       'jquery.ui.position' => '857689f6555a1f1380e57bc71e75da37',
       'jquery.ui.resizable' => '35324c9358d605fcf1ca754390373035',
-      'jquery.ui.widget' => 'eacd675de09572383b58e52309ba2245',
+      'jquery.ui.widget' => 'd5e3782353b947531960733cb9f997cf',
     ];
     // DrupalCI uses a precision of 100 in certain environments which breaks
     // this test.
