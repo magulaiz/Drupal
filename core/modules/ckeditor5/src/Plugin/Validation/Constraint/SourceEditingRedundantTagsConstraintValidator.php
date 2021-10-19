6f5078bf066c9684b8fd876e19a73dfcc75c5d66
Wim Leers <wim.leers@acquia.com>
2021-10-19 14:33:26 +0200

    Convert SourceEditingRedundantTagsConstraintValidator.

--- core/modules/ckeditor5/src/Plugin/Validation/Constraint/SourceEditingRedundantTagsConstraintValidator.php
+++ core/modules/ckeditor5/src/Plugin/Validation/Constraint/SourceEditingRedundantTagsConstraintValidator.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\ckeditor5\Plugin\Validation\Constraint;
 
-use Drupal\ckeditor5\HTMLRestrictionsUtilities;
+use Drupal\ckeditor5\HTMLRestrictions;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -40,24 +40,25 @@ class SourceEditingRedundantTagsConstraintValidator extends ConstraintValidator 
     unset($enabled_plugins['ckeditor5_sourceEditing']);
 
     // An array of tags enabled by every plugin other than Source Editing.
-    $enabled_plugin_tags = $this->pluginManager->getProvidedElements(array_keys($enabled_plugins));
-    $disabled_plugin_tags = $this->pluginManager->getProvidedElements(array_keys($disabled_plugins));
+    $enabled_plugin_tags = new HTMLRestrictions($this->pluginManager->getProvidedElements(array_keys($enabled_plugins)));
+    $disabled_plugin_tags = new HTMLRestrictions($this->pluginManager->getProvidedElements(array_keys($disabled_plugins)));
 
-    // An array of just the tags enabled by Source Editing.
-    $source_enabled_tags = HTMLRestrictionsUtilities::allowedElementsStringToHtmlFilterArray($value);
-    $enabled_plugin_overlap = array_intersect_key($enabled_plugin_tags, $source_enabled_tags);
-    $disabled_plugin_overlap = array_intersect_key($disabled_plugin_tags, $source_enabled_tags);
+    // The single tag for which source editing is enabled, which we are checking
+    // now.
+    $source_enabled_tags = HTMLRestrictions::parse($value);
+    assert($source_enabled_tags->count() <= 1);
+    // This validation constraint currently only validates tags, not attributes.
+    // @todo Expand this validation constraint to also cover elements other than tags.
+    $source_enabled_elements = $source_enabled_tags->getAllowedElements();
+    if (reset($source_enabled_elements) !== FALSE) {
+      return;
+    }
 
+    $enabled_plugin_overlap = $enabled_plugin_tags->intersect($source_enabled_tags);
+    $disabled_plugin_overlap = $disabled_plugin_tags->intersect($source_enabled_tags);
     foreach ([$enabled_plugin_overlap, $disabled_plugin_overlap] as &$overlap) {
       $checking_enabled = $overlap === $enabled_plugin_overlap;
-      if (!empty($overlap)) {
-        foreach ($overlap as $overlapping_tag => $overlapping_config) {
-          if (is_array($source_enabled_tags[$overlapping_tag])) {
-            unset($overlap[$overlapping_tag]);
-          }
-        }
-      }
-      if (!empty($overlap)) {
+      if ($overlap->count() > 0) {
         $plugins_to_check_against = $checking_enabled ? $enabled_plugins : $disabled_plugins;
         $tags_plugin_report = $this->pluginsSupplyingTagsMessage($overlap, $plugins_to_check_against);
         $message = $checking_enabled ? $constraint->enabledPluginsMessage : $constraint->availablePluginsMessage;
@@ -71,7 +72,7 @@ class SourceEditingRedundantTagsConstraintValidator extends ConstraintValidator 
   /**
    * Creates a message listing plugins and the overlapping tags they provide.
    *
-   * @param array $tags
+   * @param \Drupal\ckeditor5\HTMLRestrictions $overlap
    *   An array of overlapping tags.
    * @param \Drupal\ckeditor5\Plugin\CKEditor5PluginDefinition[] $plugin_definitions
    *   An array of plugin definitions where overlap was found.
@@ -79,16 +80,14 @@ class SourceEditingRedundantTagsConstraintValidator extends ConstraintValidator 
    * @return string
    *   A list of plugins that provide the overlapping tags.
    */
-  private function pluginsSupplyingTagsMessage(array $tags, array $plugin_definitions): string {
+  private function pluginsSupplyingTagsMessage(HTMLRestrictions $overlap, array $plugin_definitions): string {
     $message_array = [];
     $message_string = '';
     foreach ($plugin_definitions as $definition) {
       if ($definition->hasElements()) {
-        $elements_array = HTMLRestrictionsUtilities::allowedElementsStringToHtmlFilterArray(implode('', $definition->getElements()));
-        foreach ($elements_array as $tag_name => $tag_config) {
-          if (isset($tags[$tag_name])) {
-            $message_array[(string) $definition->label()][] = "<$tag_name>";
-          }
+        $plugin_capabilities = HTMLRestrictions::parse($definition->getElements());
+        foreach ($plugin_capabilities->intersect($overlap)->toCKEditor5ElementsArray() as $element) {
+          $message_array[(string) $definition->label()][] = $element;
         }
       }
     }
