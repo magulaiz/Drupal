567d6f8c9082b4317fd5660b4703832fe487ee29
Konrad Szymczak <kndr@inforion.pl>
2021-10-20 19:24:51 +0200

    Fix the code quality error.

--- core/modules/toolbar/js/toolbar.es6.js
+++ core/modules/toolbar/js/toolbar.es6.js
@@ -61,7 +61,7 @@
         // Indicate on the toolbarModel that the page has finished loading
         // and Big Pipe has populated all its placeholders.
         Drupal.toolbar.checkReadyState = setInterval(() => {
-          if (document.readyState === "complete") {
+          if (document.readyState === 'complete') {
             clearInterval(Drupal.toolbar.checkReadyState);
             model.set('isPageLoaded', true);
           }
