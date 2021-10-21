6316950259d79e9ea070b6190f102429248fc154
Konrad Szymczak <kndr@inforion.pl>
2021-10-21 10:49:55 +0200

    Use a window.onload event to indicate on the toolbarModel that the page has finished loading.

--- core/modules/toolbar/js/toolbar.es6.js
+++ core/modules/toolbar/js/toolbar.es6.js
@@ -60,12 +60,9 @@
 
         // Indicate on the toolbarModel that the page has finished loading
         // and Big Pipe has populated all its placeholders.
-        Drupal.toolbar.checkReadyState = setInterval(() => {
-          if (document.readyState === 'complete') {
-            clearInterval(Drupal.toolbar.checkReadyState);
-            model.set('isPageLoaded', true);
-          }
-        }, 100);
+        window.addEventListener('load', () => {
+          Drupal.toolbar.models.toolbarModel.set('isPageLoaded', true);
+        });
 
         // Attach a listener to the configured media query breakpoints.
         // Executes it before Drupal.toolbar.views to avoid extra rendering.
