c19db83324f23675770b6baef49faf8c7574b6b3
Aaron Manire <amanire@gmail.com>
2021-10-25 16:52:52 -0400

    Check $item_id['plugin_id'] for undefined index

--- core/modules/views/src/Plugin/ViewsHandlerManager.php
+++ core/modules/views/src/Plugin/ViewsHandlerManager.php
@@ -101,7 +101,12 @@ class ViewsHandlerManager extends DefaultPluginManager implements FallbackPlugin
       }
 
       // @todo This is crazy. Find a way to remove the override functionality.
-      $plugin_id = $override ?: $item['plugin_id'] ?: $definition['id'];
+      if (isset($item_id['plugin_id'])) {
+        $plugin_id = $override ?: $item['plugin_id'];
+      }
+      else {
+        $plugin_id = $override ?: $definition['id'];
+      }
 
       // When the definition id is field there will always be an override set.
       if ($definition['id'] === 'field' && $item['plugin_id'] !== 'field') {
