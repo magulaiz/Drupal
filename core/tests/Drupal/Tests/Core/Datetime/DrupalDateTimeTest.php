f3e3cf7f23ad2cadc7f9c00ddaa6d75cb01f95ed
Artem  Dmitriiev <a.dmitriiev@1xinternet.de>
2021-10-26 09:12:38 +0200

    Issue #3187004 by a.dmitriiev, mohit_aghera: DrupalDateTime serialization issue

--- core/tests/Drupal/Tests/Core/Datetime/DrupalDateTimeTest.php
+++ core/tests/Drupal/Tests/Core/Datetime/DrupalDateTimeTest.php
@@ -239,4 +239,23 @@ class DrupalDateTimeTest extends UnitTestCase {
     $this->assertEquals('America/New_York', $drupaldatetime->getTimezone()->getName());
   }
 
+  /**
+   * Test to avoid serialization of formatTranslationCache
+   */
+  public function testSleep() {
+    $tz = new \DateTimeZone(date_default_timezone_get());
+    $date = new DrupalDateTime('now', $tz, ['langcode' => 'en']);
+
+    // Override timestamp before serialize.
+    $date->setTimestamp(12345678);
+
+    $vars = $date->__sleep();
+    $this->assertContains('langcode', $vars);
+    $this->assertContains('dateTimeObject', $vars);
+    $this->assertNotContains('formatTranslationCache', $vars);
+
+    $unserialized_date = unserialize(serialize($date));
+    $this->assertSame(12345678, $unserialized_date->getTimestamp());
+  }
+
 }
