e2111ed0b5bf6611fd4943caaeae506af8e22790
Yogesh Pawar <yogesh.pawar01@gmail.com>
2021-10-27 15:26:57 +0530

    Resolved CS issues.

--- core/modules/migrate/src/MigrateExecutable.php
+++ core/modules/migrate/src/MigrateExecutable.php
@@ -180,10 +180,10 @@ class MigrateExecutable implements MigrateExecutableInterface {
 
     $this->migration->setStatus(MigrationInterface::STATUS_IMPORTING);
     $source = $this->getSource();
-    if (!empty($this->idlist)) {
+    if (!empty($this->getIdList())) {
       $migrate_source = $this->migration->getSourcePlugin();
       if ($migrate_source instanceof MigrateIdFilterInterface) {
-        $migrate_source->setIdList($this->idlist);
+        $migrate_source->setIdList($this->getIdList());
       }
     }
 
