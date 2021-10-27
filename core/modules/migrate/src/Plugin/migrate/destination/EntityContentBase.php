50d8ab0e3477f02bcab7ff0a9e138c6acc70e595
Omar <omar.lopesino@metadrop.net>
2021-10-27 12:02:58 +0200

    Issue #2988382 by SylvainM: Add option to create new revision in entity migration

--- core/modules/migrate/src/Plugin/migrate/destination/EntityContentBase.php
+++ core/modules/migrate/src/Plugin/migrate/destination/EntityContentBase.php
@@ -328,6 +328,10 @@ class EntityContentBase extends Entity implements HighestIdInterface, MigrateVal
       $entity->$field_name = NULL;
     }
 
+    if (isset($this->configuration['new_revision']) && $entity instanceof RevisionableInterface && $this->storage instanceof TranslatableRevisionableStorageInterface) {
+      $this->storage->createRevision($entity);
+    }
+
     $this->setRollbackAction($row->getIdMap(), $rollback_action);
 
     // We might have a different (translated) entity, so return it.
