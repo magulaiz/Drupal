c2e9ee6a184f0661c340ee636e5badced70e4859
Srilakshmi R <srilakshmier@gmail.com>
2021-11-03 16:28:09 +0530

    Removed |null from docblock for @result

--- core/modules/migrate/src/Plugin/migrate/id_map/Sql.php
+++ core/modules/migrate/src/Plugin/migrate/id_map/Sql.php
@@ -109,7 +109,7 @@ class Sql extends PluginBase implements MigrateIdMapInterface, ContainerFactoryP
   /**
    * The result.
    *
-   * @var \Drupal\Core\Database\StatementInterface|null
+   * @var \Drupal\Core\Database\StatementInterface
    */
   protected $result;
 
