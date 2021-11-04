617e526c6719717ac6e09bc4c5750980f05ba009
Omar <omar.lopesino@metadrop.net>
2021-11-04 15:42:25 +0100

    Issue #2780839: Do not call getIdList() on migrateExecutable

--- core/modules/migrate/src/MigrateExecutable.php
+++ core/modules/migrate/src/MigrateExecutable.php
@@ -180,7 +180,7 @@ class MigrateExecutable implements MigrateExecutableInterface {
 
     $this->migration->setStatus(MigrationInterface::STATUS_IMPORTING);
     $source = $this->getSource();
-    if (!empty($this->getIdList())) {
+    if (!empty($this->idlist)) {
       $migrate_source = $this->migration->getSourcePlugin();
       if ($migrate_source instanceof MigrateIdFilterInterface) {
         $migrate_source->setIdList($this->getIdList());
