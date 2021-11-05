055462c5db8607c25c7291759cce0ae204128f05
Michael Lutz <mlutz531@gmail.com>
2021-11-05 14:20:24 -0400

    fix: use MigrateException in callback plugin

--- core/modules/migrate/src/Plugin/migrate/process/Callback.php
+++ core/modules/migrate/src/Plugin/migrate/process/Callback.php
@@ -73,10 +73,10 @@ class Callback extends ProcessPluginBase {
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     if (!isset($configuration['callable'])) {
-      throw new \InvalidArgumentException('The "callable" must be set.');
+      throw new MigrateException('The "callable" must be set.');
     }
     elseif (!is_callable($configuration['callable'])) {
-      throw new \InvalidArgumentException('The "callable" must be a valid function or method.');
+      throw new MigrateException('The "callable" must be a valid function or method.');
     }
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
