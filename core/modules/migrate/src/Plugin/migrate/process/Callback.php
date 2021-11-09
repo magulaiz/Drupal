ae31127a18d230ed34972441c16a3f1731fb426e
Michael Lutz <mlutz531@gmail.com>
2021-11-09 09:37:20 -0500

    fix: move exception to the transform method

--- core/modules/migrate/src/Plugin/migrate/process/Callback.php
+++ core/modules/migrate/src/Plugin/migrate/process/Callback.php
@@ -71,20 +71,13 @@ class Callback extends ProcessPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    if (!isset($configuration['callable'])) {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!isset($this->configuration['callable'])) {
       throw new MigrateException('The "callable" must be set.');
     }
-    elseif (!is_callable($configuration['callable'])) {
+    if (!is_callable($this->configuration['callable'])) {
       throw new MigrateException('The "callable" must be a valid function or method.');
     }
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     if (!empty($this->configuration['unpack_source'])) {
       if (!is_array($value)) {
         throw new MigrateException(sprintf("When 'unpack_source' is set, the source must be an array. Instead it was of type '%s'", gettype($value)));
