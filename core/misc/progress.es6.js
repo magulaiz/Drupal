74fc7e02ad0f082b40db827e16b4a6d095440c5a
Michael Strelan <mstrelan@gmail.com>
2021-11-12 11:58:03 +1000

    Replace usages of jQuery.css() in misc/progress.es6.js

--- core/misc/progress.es6.js
+++ core/misc/progress.es6.js
@@ -76,7 +76,9 @@
         if (percentage >= 0 && percentage <= 100) {
           $(this.element)
             .find('div.progress__bar')
-            .css('width', `${percentage}%`);
+            .forEach((el) => {
+              el.style.width = `${percentage}%`;
+            });
           $(this.element)
             .find('div.progress__percentage')
             .html(`${percentage}%`);
--- core/misc/progress.js
+++ core/misc/progress.js
@@ -21,7 +21,9 @@
   $.extend(Drupal.ProgressBar.prototype, {
     setProgress: function setProgress(percentage, message, label) {
       if (percentage >= 0 && percentage <= 100) {
-        $(this.element).find('div.progress__bar').css('width', "".concat(percentage, "%"));
+        $(this.element).find('div.progress__bar').forEach(function (el) {
+          el.style.width = "".concat(percentage, "%");
+        });
         $(this.element).find('div.progress__percentage').html("".concat(percentage, "%"));
       }
 
