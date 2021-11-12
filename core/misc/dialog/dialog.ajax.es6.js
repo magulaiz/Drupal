730d547107c7eae415caa2ef4e99c49f6426b155
Michael Strelan <mstrelan@gmail.com>
2021-11-12 11:18:18 +1000

    Replace usages of jQuery.css() in misc/dialog/dialog.ajax.es6.js

--- core/misc/dialog/dialog.ajax.es6.js
+++ core/misc/dialog/dialog.ajax.es6.js
@@ -65,7 +65,8 @@
         '.form-actions input[type=submit], .form-actions a.button',
       );
       $buttons.each(function () {
-        const $originalButton = $(this).css({ display: 'none' });
+        const $originalButton = $(this);
+        this.style.display = 'none';
         buttons.push({
           text: $originalButton.html() || $originalButton.attr('value'),
           class: $originalButton.attr('class'),
