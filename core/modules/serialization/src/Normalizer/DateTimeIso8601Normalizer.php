07939626f342f1edcd6cb7954e2f61e3b5dc190d
bjorn <bjorn@swis.nl>
2021-11-12 11:03:47 +0100

    Simplify even more with simple is elseif suggested by @longwave

--- core/modules/serialization/src/Normalizer/DateTimeIso8601Normalizer.php
+++ core/modules/serialization/src/Normalizer/DateTimeIso8601Normalizer.php
@@ -56,12 +56,13 @@ class DateTimeIso8601Normalizer extends DateTimeNormalizer {
    */
   public function denormalize($data, $class, $format = NULL, array $context = []) {
     // @todo Move the date-only handling out of here in https://www.drupal.org/project/drupal/issues/2958416.
-    $field_definition = $context['field_definition'] ?? NULL;
     if (isset($context['target_instance'])) {
       $field_definition = $context['target_instance']->getFieldDefinition();
     }
-
-    if ($field_definition === NULL) {
+    elseif (isset($context['field_definition'])) {
+      $field_definition = $context['field_definition'];
+    }
+    else {
       throw new InvalidArgumentException('$context[\'target_instance\'] or $context[\'field_definition\'] must be set to denormalize with the DateTimeIso8601Normalizer');
     }
 
