ad97c3c968e4871790d53bfd41b60ef1bb02f4e7
jurgenhaas <jurgenhaas@168924.no-reply.drupal.org>
2021-11-16 09:28:59 +0000

    Update core/modules/comment/src/Form/CommentAdminOverview.php

--- core/modules/comment/src/Form/CommentAdminOverview.php
+++ core/modules/comment/src/Form/CommentAdminOverview.php
@@ -10,6 +10,7 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
