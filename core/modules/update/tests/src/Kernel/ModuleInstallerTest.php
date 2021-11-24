8a41423c1218e0fe766b54226bfd6598e7ddfb6e
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2021-11-24 20:16:43 +0530

    added test for testing module installer

---
+++ core/modules/update/tests/src/Kernel/ModuleInstallerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\update\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the module_installer service.
+ *
+ * @group update
+ */
+class ModuleInstallerTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'update',
+  ];
+
+  /**
+   * Tests the module_installer service.
+   */
+  public function testModuleInstaller(){
+    $keyvalue = $this->container->get('keyvalue.expirable')->get('update');
+    $keyvalue->set('key', 'some value');
+    $this->container->get('module_installer')->install(['help']);
+    $this->assertNull($keyvalue->get('key'));
+  }
+}
