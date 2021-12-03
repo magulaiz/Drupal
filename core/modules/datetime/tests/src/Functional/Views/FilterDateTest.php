6c086885804ae4770a9b704f695453a0c4495e16
Ivan Doroshenko <Matroskeen@3426249.no-reply.drupal.org>
2021-12-03 16:21:04 +0200

    Moved helper method to date test class.

--- core/modules/datetime/tests/src/Functional/Views/FilterDateTest.php
+++ core/modules/datetime/tests/src/Functional/Views/FilterDateTest.php
@@ -180,4 +180,22 @@ class FilterDateTest extends ViewTestBase {
     $this->assertIds([2]);
   }
 
+  /**
+   * Ensures that a given list of items appear on the view result.
+   *
+   * @param array $expected_ids
+   *   An array of IDs.
+   */
+  protected function assertIds(array $expected_ids = []): void {
+    // First verify the count.
+    $elements = $this->cssSelect('.view-content span.field-content');
+    $this->assertCount(count($expected_ids), $elements);
+
+    $actual_ids = [];
+    foreach ($elements as $element) {
+      $actual_ids[] = (int) $element->getText();
+    }
+    $this->assertEquals($expected_ids, $actual_ids);
+  }
+
 }
--- core/modules/views/tests/src/Functional/ViewTestBase.php
+++ core/modules/views/tests/src/Functional/ViewTestBase.php
@@ -110,24 +110,6 @@ abstract class ViewTestBase extends BrowserTestBase {
   }
 
   /**
-   * Ensures that a given list of items appear on the view result.
-   *
-   * @param array $expected_ids
-   *   An array of IDs.
-   */
-  protected function assertIds(array $expected_ids = [], $selector = '.view-content span.field-content'): void {
-    // First verify the count.
-    $elements = $this->cssSelect($selector);
-    $this->assertCount(count($expected_ids), $elements);
-
-    $actual_ids = [];
-    foreach ($elements as $element) {
-      $actual_ids[] = (int) $element->getText();
-    }
-    $this->assertEquals($expected_ids, $actual_ids);
-  }
-
-  /**
    * Executes a view.
    *
    * @param \Drupal\views\ViewExecutable $view
