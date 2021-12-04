9f23eaf4d4c73ce83eef8c5a9133818128baea53
Rick Hawkins <rick.hawkins@slalom.com>
2021-12-03 17:22:13 -0800

    Add tests for using Callback process with null value

--- core/modules/migrate/tests/src/Unit/process/CallbackTest.php
+++ core/modules/migrate/tests/src/Unit/process/CallbackTest.php
@@ -35,6 +35,28 @@ class CallbackTest extends MigrateProcessTestCase {
   }
 
   /**
+   * Test callback with valid "callable" and null value.
+   *
+   * @dataProvider providerCallbackNullValue
+   */
+  public function testCallbackNullValue($callable) {
+    $configuration = ['callable' => $callable];
+    $this->plugin = new Callback($configuration, 'map', []);
+    $value = $this->plugin->transform(NULL, $this->migrateExecutable, $this->row, 'destination_property');
+    $this->assertSame(M_PI, $value);
+  }
+
+  /**
+   * Data provider for ::testCallbackNullValue().
+   */
+  public function providerCallbackNullValue() {
+    return [
+      'function' => ['pi'],
+      'class method' => [[self::class, 'pi']],
+    ];
+  }
+
+  /**
    * Test callback with valid "callable" and multiple arguments.
    *
    * @dataProvider providerCallbackArray
@@ -125,6 +147,18 @@ class CallbackTest extends MigrateProcessTestCase {
    */
   public static function strToLower($string) {
     return mb_strtolower($string);
+  }
+
+  /**
+   * Returns an approximation of pi for testing purposes.
+   *
+   * @return float
+   *   The approximation of pi.
+   *
+   * @see \Drupal\Tests\migrate\Unit\process\CallbackTest::providerCallbackNullValue()
+   */
+  public static function pi() {
+    return pi();
   }
 
 }
