c692766d63ca25ed411239ea30ccf9900b9167cd
Ted Bowman <ted@tedbow.com>
2021-12-07 12:02:23 -0500

    UpdateServerProjectInfo comments

--- core/modules/update/src/UpdateServerProjectInfo.php
+++ core/modules/update/src/UpdateServerProjectInfo.php
@@ -2,28 +2,58 @@
 
 namespace Drupal\update;
 
+/**
+ * Update server project information.
+ */
 class UpdateServerProjectInfo {
 
 
   /**
-   * @var array
+   * The project data from the server.
+   *
+   * @var array[]
    */
   protected $data;
 
+  /**
+   * Constructs a UpdateServerProjectInfo object.
+   *
+   * @param array $data
+   *   The project data from the Update XML.
+   */
   private function __construct(array $data) {
     $this->data = $data;
   }
 
+  /**
+   * Creates a UpdateServerProjectInfo object.
+   *
+   * @param array $available
+   *   The project data from the Update XML.
+   *
+   * @return \Drupal\update\UpdateServerProjectInfo
+   *   The UpdateServerProjectInfo instances.
+   */
   public static function createFromArray(array $available): UpdateServerProjectInfo {
     return new UpdateServerProjectInfo($available);
   }
 
-
-
+  /**
+   * Gets the project status.
+   *
+   * @return string|null
+   *   The project status if available, otherwise NULL.
+   */
   public function getProjectStatus(): ?string {
     return $available['project_status'] ?? NULL;
   }
 
+  /**
+   * Gets the supported branches.
+   *
+   * @return string[]
+   *   The supported branches.
+   */
   public function getSupportBranches(): array {
     if (isset($this->data['supported_branches'])) {
       return explode(',', $this->data['supported_branches']);
@@ -31,7 +61,13 @@ class UpdateServerProjectInfo {
     return [];
   }
 
-  public function getReleases() {
+  /**
+   * Gets the project releases.
+   *
+   * @return array[]
+   *   The project releases.
+   */
+  public function getReleases(): array {
     return $this->data['releases'] ?? [];
   }
 
