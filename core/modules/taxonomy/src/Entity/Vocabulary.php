f4f905896377016ff457e8fa2f2756361cabd3ce
Benji Fisher <benji@FisherFam.org>
2021-12-11 13:21:18 -0500

    Enable the permissions form for core entity types

--- core/modules/taxonomy/src/Entity/Vocabulary.php
+++ core/modules/taxonomy/src/Entity/Vocabulary.php
@@ -31,6 +31,7 @@ use Drupal\taxonomy\VocabularyInterface;
  *     },
  *     "route_provider" = {
  *       "html" = "Drupal\taxonomy\Entity\Routing\VocabularyRouteProvider",
+ *       "permissions" = "Drupal\user\Entity\UserPermissionsRouteProvider",
  *     }
  *   },
  *   admin_permission = "administer taxonomy",
@@ -47,6 +48,7 @@ use Drupal\taxonomy\VocabularyInterface;
  *     "reset-form" = "/admin/structure/taxonomy/manage/{taxonomy_vocabulary}/reset",
  *     "overview-form" = "/admin/structure/taxonomy/manage/{taxonomy_vocabulary}/overview",
  *     "edit-form" = "/admin/structure/taxonomy/manage/{taxonomy_vocabulary}",
+ *     "permission-form" = "/admin/structure/taxonomy/manage/{taxonomy_vocabulary}/overview/permissions",
  *     "collection" = "/admin/structure/taxonomy",
  *   },
  *   config_export = {
