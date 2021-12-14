c05f3f946d62e60062f3f24a7592291016bf71ee
JeroenT <jeroent@2228934.no-reply.drupal.org>
2021-12-14 20:03:54 +0000

    Update core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php, core/modules/ckeditor5/ckeditor5.ckeditor5.yml, core/core.libraries.yml, core/assets/vendor/ckeditor5/autoformat.js

--- core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
+++ core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
@@ -15,7 +15,7 @@ use org\bovigo\vfs\vfsStream;
 use Symfony\Component\DependencyInjection\ParameterBag\FrozenParameterBag;
 use Symfony\Component\Yaml\Yaml;
 
-// cspell:ignore layercake everyblock justblockquote autoformat
+// cspell:ignore layercake everyblock justblockquote
 
 /**
  * Tests different ways of enabling CKEditor 5 plugins.
