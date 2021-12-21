df64684f3fd3066a9c2623e14cd3abe100c392f3
Ana Julia Gomes Przsiczny <CIANDT\anagomes@lnb029377cps.ciandt.global>
2021-12-21 18:02:59 -0300

    remove jquery from argument list

--- core/misc/date.js
+++ core/misc/date.js
@@ -11,7 +11,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-(function ($, Modernizr, Drupal, once) {
+(function (Modernizr, Drupal, once) {
   Drupal.behaviors.date = {
     attach: function attach(context, settings) {
       if (Modernizr.inputtypes.date === false) {
