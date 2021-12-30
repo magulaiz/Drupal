d17753bc433bf36db19bc867baa7ee0b0c80dae1
voleger <dev.vegera@gmail.com>
2021-12-30 17:35:29 +0200

    Add test of file includes for disabled extansions

---
+++ core/tests/Drupal/KernelTests/Core/Module/ModuleHandlerDisabledExtensionsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Module;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests ModuleHandler with disabled extensions.
+ *
+ * @group ModuleHandler
+ */
+class ModuleHandlerDisabledExtensionsTest extends KernelTestBase {
+
+  /**
+   * Test inclusion of the files for disabled extensions.
+   */
+  public function testLoadIncludeWithDisabledModule() {
+    /** @var \Drupal\Core\Extension\ModuleHandler $module_handler */
+    $module_handler = \Drupal::service('module_handler');
+    // Module extension.
+    $this->assertFalse(function_exists('module_test_system_info_alter'), 'Test function already defined during runtime');
+    $module_handler->loadInclude('module_test', 'module');
+    $this->assertTrue(function_exists('module_test_system_info_alter'), 'Module handler unable to include file of uninstalled extensions');
+    // Engine extension.
+    $this->assertFalse(function_exists('nyan_cat_extension'), 'Test function already defined during runtime');
+    $module_handler->loadInclude('nyan_cat', 'engine');
+    $this->assertTrue(function_exists('nyan_cat_extension'), 'Module handler unable to include file of uninstalled extensions');
+    // Theme extension.
+    $this->assertFalse(function_exists('test_theme_library_info_alter'), 'Test function already defined during runtime');
+    $module_handler->loadInclude('test_theme', 'theme');
+    $this->assertTrue(function_exists('test_theme_library_info_alter'), 'Module handler unable to include file of uninstalled extensions');
+    // Profile extension.
+    $this->assertFalse(function_exists('testing_requirements_requirements'), 'Test function already defined during runtime');
+    $module_handler->loadInclude('testing_requirements', 'install');
+    $this->assertTrue(function_exists('testing_requirements_requirements'), "Module handler unable to include file of uninstalled extensions");
+  }
+
+}
