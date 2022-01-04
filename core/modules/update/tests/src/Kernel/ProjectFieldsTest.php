09c02a1d6e5f51dc2578c3400e24a37ea58a33ed
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2022-01-04 16:43:48 +0530

    added test coverage for more values of $project[status]

--- core/modules/update/tests/src/Kernel/ProjectFieldsTest.php
+++ core/modules/update/tests/src/Kernel/ProjectFieldsTest.php
@@ -31,7 +31,6 @@ class ProjectFieldsTest extends KernelTestBase {
     $this->installConfig('update');
     $this->installConfig('update_test');
     $this->setCoreVersion('8.0.1');
-    $this->setReleaseMetadata(__DIR__ . '/../../fixtures/release-history/drupal.revoked.0.2.xml');
 
   }
 
@@ -64,15 +63,48 @@ class ProjectFieldsTest extends KernelTestBase {
   }
 
   /**
+   * Provides fixture data for test scenarios testing project status field.
+   *
+   * The test cases rely on the following fixtures:
+   * - drupal.revoked.0.2.xml : Project_status is 'revoked'.
+   * - drupal.insecure.0.2.xml :  Project_status is 'insecure'.
+   * - drupal.unsupported.0.2.xml : Project_status is 'unsupported'.
+   *
+   * @return array[]
+   *   Test data.
+   */
+  public function fixturesProvider(): array {
+    return [
+        'revoked' => [
+          'fixture' => '/../../fixtures/release-history/drupal.revoked.0.2.xml',
+          'label' => 'Project revoked',
+          'exp_error_message' => 'This project has been revoked, and is no longer available for download. Disabling everything included by this project is strongly recommended!',
+        ],
+        'insecure' => [
+          'fixture' => '/../../fixtures/release-history/drupal.insecure.0.2.xml',
+          'label' => 'Project not secure',
+          'exp_error_message' => 'This project has been labeled insecure by the Drupal security team, and is no longer available for download. Immediately disabling everything included by this project is strongly recommended!',
+        ],
+        'unsupported' => [
+        'fixture' => '/../../fixtures/release-history/drupal.unsupported.0.2.xml',
+        'label' => 'Project not supported',
+        'exp_error_message' => 'This project is no longer supported, and is no longer available for download. Disabling everything included by this project is strongly recommended!',
+      ]
+    ];
+  }
+  /**
+   * @dataProvider fixturesProvider
+   *
    * Tests the project_status field of the project.
    */
-  public function testProjectStatusField() {
+  public function testProjectStatusField($fixture, $label, $exp_error_message  ) {
     update_storage_clear();
+    $this->setReleaseMetadata(__DIR__ . $fixture);
     $available = update_get_available(TRUE);
     $new = update_calculate_project_data($available);
     self::assertArrayHasKey('extra', $new['drupal']);
-    self::assertEquals('Project revoked', $new['drupal']['extra']['0']['label']);
-    self::assertEquals('This project has been revoked, and is no longer available for download. Disabling everything included by this project is strongly recommended!', $new['drupal']['extra']['0']['data']);
+    self::assertEquals($label, $new['drupal']['extra']['0']['label']);
+    self::assertEquals($exp_error_message, $new['drupal']['extra']['0']['data']);
   }
 
 }
