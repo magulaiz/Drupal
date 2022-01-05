e43c7003a40437f999ab4ff3fc052dc1a294bf3b
Beatriz de Oliveira Rodrigues <CIANDT\beatrizrodrigues@lnb028750cps.ciandt.global>
2022-01-05 11:37:53 -0300

    setting empty string value when the connection_options array is empty

--- core/modules/mysql/src/Driver/Database/mysql/Connection.php
+++ core/modules/mysql/src/Driver/Database/mysql/Connection.php
@@ -100,6 +100,10 @@ class Connection extends DatabaseConnection {
     // modes as well.
     //
     // @see https://dev.mysql.com/doc/refman/5.7/en/sql-mode.html#sqlmode_ansi_quotes
+    
+    if (empty($connection_options)) {
+      $connection_options = '';
+    }
     $ansi_quotes_modes = ['ANSI_QUOTES', 'ANSI', 'DB2', 'MAXDB', 'MSSQL', 'ORACLE', 'POSTGRESQL'];
     $is_ansi_quotes_mode = FALSE;
     foreach ($ansi_quotes_modes as $mode) {
