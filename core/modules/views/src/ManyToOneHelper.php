fa81d737e86ce004dca6912f87924660d41c16b5
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2022-01-07 16:24:34 -0500

    Issue #2829178 by chertzog: Views Term ID has broken filters ("All of", "Is none of") and contextual filters "allow multiple"

--- core/modules/views/src/ManyToOneHelper.php
+++ core/modules/views/src/ManyToOneHelper.php
@@ -4,6 +4,7 @@ namespace Drupal\views;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\HandlerBase;
+use Drupal\views\Plugin\views\join\JoinPluginBase;
 
 /**
  * This many to one helper object is used on both arguments and filters.
@@ -73,6 +74,10 @@ class ManyToOneHelper {
     // See if there's a chain between us and the base relationship. If so, we need
     // to create a new relationship to use.
     $relationship = $this->handler->relationship;
+    if ($relationship) {
+      $relationship = $this->handler->query->relationships[$relationship]['link'];
+    }
+    $base_relationship = $relationship;
 
     // Determine the primary table to seek
     if (empty($this->handler->query->relationships[$relationship])) {
@@ -82,15 +87,18 @@ class ManyToOneHelper {
       $base_table = $this->handler->query->relationships[$relationship]['base'];
     }
 
+    if (!$join instanceof JoinPluginBase) {
+      return $this->handler->relationship;
+    }
     // Cycle through the joins. This isn't as error-safe as the normal
     // ensurePath logic. Perhaps it should be.
     $r_join = clone $join;
-    while ($r_join->leftTable != $base_table) {
+    while (isset($r_join->leftTable) && ($r_join->leftTable != $base_table)) {
       $r_join = HandlerBase::getTableJoin($r_join->leftTable, $base_table);
     }
     // If we found that there are tables in between, add the relationship.
     if ($r_join->table != $join->table) {
-      $relationship = $this->handler->query->addRelationship($this->handler->table . '_' . $r_join->table, $r_join, $r_join->table, $this->handler->relationship);
+      $relationship = $this->handler->query->addRelationship($this->handler->table . '_' . $r_join->table, $r_join, $r_join->table, $base_relationship);
     }
 
     // And now add our table, using the new relationship if one was used.
@@ -172,21 +180,23 @@ class ManyToOneHelper {
         }
         else {
           $join = $this->getJoin();
-          $join->type = 'LEFT';
-          if (!empty($this->handler->view->many_to_one_tables[$field])) {
-            foreach ($this->handler->view->many_to_one_tables[$field] as $value) {
-              $join->extra = [
-                [
-                  'field' => $this->handler->realField,
-                  'operator' => '!=',
-                  'value' => $value,
-                  'numeric' => !empty($this->handler->definition['numeric']),
-                ],
-              ];
+          if (isset($join)) {
+            $join->type = 'LEFT';
+            if (!empty($this->handler->view->many_to_one_tables[$field])) {
+              foreach ($this->handler->view->many_to_one_tables[$field] as $value) {
+                $join->extra = [
+                  [
+                    'field' => $this->handler->realField,
+                    'operator' => '!=',
+                    'value' => $value,
+                    'numeric' => !empty($this->handler->definition['numeric']),
+                  ],
+                ];
+              }
             }
-          }
 
-          $this->handler->tableAlias = $this->addTable($join);
+            $this->handler->tableAlias = $this->addTable($join);
+          }
         }
 
         return $this->handler->tableAlias;
@@ -199,16 +209,18 @@ class ManyToOneHelper {
         $this->handler->tableAliases = [];
         foreach ($this->handler->value as $value) {
           $join = $this->getJoin();
-          if ($this->handler->operator == 'and') {
+          if (isset($join) && $this->handler->operator == 'and') {
             $join->type = 'INNER';
           }
-          $join->extra = [
-            [
-              'field' => $this->handler->realField,
-              'value' => $value,
-              'numeric' => !empty($this->handler->definition['numeric']),
-            ],
-          ];
+          if (isset($join)) {
+            $join->extra = [
+              [
+                'field' => $this->handler->realField,
+                'value' => $value,
+                'numeric' => !empty($this->handler->definition['numeric']),
+              ],
+            ];
+          }
 
           // The table alias needs to be unique to this value across the
           // multiple times the filter or argument is called by the view.
@@ -231,18 +243,23 @@ class ManyToOneHelper {
       // the query phase to ensure that $table.$field IS NULL.
       else {
         $join = $this->getJoin();
-        $join->type = 'LEFT';
-        $join->extra = [];
-        $join->extraOperator = 'OR';
-        foreach ($this->handler->value as $value) {
-          $join->extra[] = [
-            'field' => $this->handler->realField,
-            'value' => $value,
-            'numeric' => !empty($this->handler->definition['numeric']),
-          ];
-        }
+        if (isset($join)) {
+          $join->type = 'LEFT';
+          $join->extra = [];
+          $join->extraOperator = 'OR';
+          foreach ($this->handler->value as $value) {
+            $join->extra[] = [
+              'field' => $this->handler->realField,
+              'value' => $value,
+              'numeric' => !empty($this->handler->definition['numeric']),
+            ];
+          }
 
-        $this->handler->tableAlias = $this->addTable($join);
+          $this->handler->tableAlias = $this->addTable($join);
+        }
+        else {
+          $this->handler->tableAlias = $this->handler->query->ensureTable($this->handler->table, $this->handler->relationship, $join);
+        }
       }
     }
     return $this->handler->tableAlias;
@@ -275,8 +292,13 @@ class ManyToOneHelper {
     // is set to TRUE, conditions will be added.
     $add_condition = TRUE;
     if ($operator == 'not') {
-      $value = NULL;
-      $operator = 'IS NULL';
+      if (count($value) > 1) {
+        $operator = 'NOT IN';
+      }
+      else {
+        $value = is_array($value) ? array_pop($value) : $value;
+        $operator = '!=';
+      }
       $add_condition = FALSE;
     }
     elseif ($operator == 'or' && empty($options['reduce_duplicates'])) {
@@ -310,18 +332,18 @@ class ManyToOneHelper {
           $placeholder .= '[]';
 
           if ($operator == 'IS NULL') {
-            $this->handler->query->addWhereExpression(0, "$field $operator");
+            $this->handler->query->addWhereExpression($options['group'], "$field $operator");
           }
           else {
-            $this->handler->query->addWhereExpression(0, "$field $operator($placeholder)", [$placeholder => $value]);
+            $this->handler->query->addWhereExpression($options['group'], "$field $operator($placeholder)", [$placeholder => $value]);
           }
         }
         else {
           if ($operator == 'IS NULL') {
-            $this->handler->query->addWhereExpression(0, "$field $operator");
+            $this->handler->query->addWhereExpression($options['group'], "$field $operator");
           }
           else {
-            $this->handler->query->addWhereExpression(0, "$field $operator $placeholder", [$placeholder => $value]);
+            $this->handler->query->addWhereExpression($options['group'], "$field $operator $placeholder", [$placeholder => $value]);
           }
         }
       }
