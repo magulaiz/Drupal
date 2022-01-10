fe85039885882fcb0092e3edbf32cf214743d200
Michael Lutz <mlutz531@gmail.com>
2022-01-10 10:50:07 -0500

    fix: respect ->skip() in prepare_row hooks

--- core/modules/migrate/src/Plugin/migrate/source/SourcePluginBase.php
+++ core/modules/migrate/src/Plugin/migrate/source/SourcePluginBase.php
@@ -292,11 +292,17 @@ abstract class SourcePluginBase extends PluginBase implements MigrateSourceInter
   public function prepareRow(Row $row) {
     $result = TRUE;
     try {
-      $result_hook = $this->getModuleHandler()->invokeAll('migrate_prepare_row', [$row, $this, $this->migration]);
-      $result_named_hook = $this->getModuleHandler()->invokeAll('migrate_' . $this->migration->id() . '_prepare_row', [$row, $this, $this->migration]);
+      $result_hook = $this->getModuleHandler()
+        ->invokeAll('migrate_prepare_row', [$row, $this, $this->migration]);
+      $result_named_hook = $this->getModuleHandler()
+        ->invokeAll('migrate_' . $this->migration->id() . '_prepare_row',
+          [$row, $this, $this->migration]);
       // We will skip if any hook returned FALSE.
-      $skip = ($result_hook && in_array(FALSE, $result_hook)) || ($result_named_hook && in_array(FALSE, $result_named_hook));
-      $save_to_map = TRUE;
+      $skip = ($result_hook && in_array(FALSE, $result_hook)) || ($result_named_hook && in_array(FALSE, $result_named_hook)) || $row->shouldSkip();
+      $save_to_map = $row->shouldSkip() ? $row->saveToMapOnSkip() : TRUE;
+      if ($row->skipMessage()) {
+        $this->idMap->saveMessage($row->getSourceIdValues(), $row->skipMessage(), MigrationInterface::MESSAGE_INFORMATIONAL);
+      }
     }
     catch (MigrateSkipRowException $e) {
       $skip = TRUE;
