72b8a46e229238d007f1e44cfed1ac243e505ce7
dpi <pro@danielph.in>
2022-01-11 19:31:01 +0800

    Convert mailmanager to proper hook call.

--- core/lib/Drupal/Core/Mail/MailManager.php
+++ core/lib/Drupal/Core/Mail/MailManager.php
@@ -268,12 +268,18 @@ class MailManager extends DefaultPluginManager implements MailManagerInterface {
     $message['headers'] = $headers;
 
     // Build the email (get subject and body, allow additional headers) by
-    // invoking hook_mail() on this module. We cannot use
-    // moduleHandler()->invoke() as we need to have $message by reference in
-    // hook_mail().
-    if (function_exists($function = $module . '_mail')) {
-      $function($key, $message, $params);
-    }
+    // invoking hook_mail() on this module. We use
+    // moduleHandler()->invokeAllWith() as we need to have $message by reference
+    // in hook_mail().
+    $this->moduleHandler->invokeAllWith(
+      'mail',
+      function (callable $hook, string $moduleHook) use ($key, &$message, $params, $module) {
+        // Only call this module.
+        if ($moduleHook === $module) {
+          $hook($key, $message, $params);
+        }
+      }
+    );
 
     // Invoke hook_mail_alter() to allow all modules to alter the resulting
     // email.
