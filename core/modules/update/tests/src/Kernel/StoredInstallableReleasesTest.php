fa2f55a909b52401b04780b634b6e3449322b701
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2022-01-11 14:20:57 +0530

    improved test and fixture

--- core/modules/update/tests/src/Kernel/StoredInstallableReleasesTest.php
+++ core/modules/update/tests/src/Kernel/StoredInstallableReleasesTest.php
@@ -31,7 +31,7 @@ class StoredInstallableReleasesTest extends KernelTestBase {
     // fake release metadata to be fetched.
     $this->installConfig('update');
     $this->installConfig('update_test');
-    $this->setCoreVersion('9.8.0');
+    $this->setCoreVersion('8.1.1');
     $this->setReleaseMetadata(__DIR__ . '/../../fixtures/release-history/drupal_8.2_8.1_8.0.xml');
 
   }
@@ -71,17 +71,29 @@ class StoredInstallableReleasesTest extends KernelTestBase {
     update_storage_clear();
     $available = update_get_available(TRUE);
     $new = update_calculate_project_data($available);
-    $this->assertArrayHasKey('8.2.3', $new['drupal']['releases']);
-    $this->assertArrayHasKey('8.2.1', $new['drupal']['releases']);
-    $this->assertArrayHasKey('8.1.3', $new['drupal']['releases']);
-    $this->assertArrayHasKey('8.1.1', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.0.2', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.0.1', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.0.0', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.2.2', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.2.0', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.1.2', $new['drupal']['releases']);
-    $this->assertArrayNotHasKey('8.1.0', $new['drupal']['releases']);
+    $expected_versions = [
+      '8.2.3',
+      '8.2.1',
+      '8.1.3',
+      '8.1.1',
+    ];
+    $not_expected_versions = [
+      '8.0.2',
+      '8.0.1',
+      '8.0.0',
+      '8.1.2',
+      '8.1.0',
+      '8.2.2',
+      '8.2.0',
+    ];
+    foreach ($expected_versions as $version){
+      $this->assertArrayHasKey($version, $new['drupal']['releases']);
+      $this->assertArrayHasKey('status', $new['drupal']['releases'][$version]);
+      $this->assertArrayHasKey('version', $new['drupal']['releases'][$version]);
+    }
+    foreach ($not_expected_versions as $version){
+      $this->assertArrayNotHasKey($version, $new['drupal']['releases']);
+    }
   }
 
 }
