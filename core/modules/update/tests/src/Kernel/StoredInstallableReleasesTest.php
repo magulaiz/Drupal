bfc4ba1a7a7534843b0a2056244d664d36ff3ee1
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2022-01-12 13:30:16 +0530

    improved test and fixture

--- core/modules/update/tests/src/Kernel/StoredInstallableReleasesTest.php
+++ core/modules/update/tests/src/Kernel/StoredInstallableReleasesTest.php
@@ -77,22 +77,11 @@ class StoredInstallableReleasesTest extends KernelTestBase {
       '8.1.3',
       '8.1.1',
     ];
-    $not_expected_versions = [
-      '8.0.2',
-      '8.0.1',
-      '8.0.0',
-      '8.1.2',
-      '8.1.0',
-      '8.2.2',
-      '8.2.0',
-    ];
+    $this->assertSame($expected_versions, array_keys($new['drupal']['releases']));
     foreach ($expected_versions as $version) {
       $this->assertArrayHasKey($version, $new['drupal']['releases']);
-      $this->assertArrayHasKey('status', $new['drupal']['releases'][$version]);
-      $this->assertArrayHasKey('version', $new['drupal']['releases'][$version]);
-    }
-    foreach ($not_expected_versions as $version) {
-      $this->assertArrayNotHasKey($version, $new['drupal']['releases']);
+      $this->assertEquals($new['drupal']['releases'][$version]['status'], 'published');
+      $this->assertEquals($new['drupal']['releases'][$version]['version'], $version);
     }
   }
 
