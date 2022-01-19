00b085297e2816448b9e9fc53d0ef4d2a095a03e
alex-gor <alex-gor@3071411.no-reply.drupal.org>
2022-01-19 02:55:29 +0200

    Issue #3256060 by chmez:
    1. Combine versions list of core and versions list of modules into one
    
    2. Use the right arrow instead of greater than sign between old and new versions
    
    3. Ensure that recommended version of the core is available
    
    4. Add field to the settings page
    
    5. Make message hideable

--- core/modules/update/src/UpdateSettingsForm.php
+++ core/modules/update/src/UpdateSettingsForm.php
@@ -110,6 +110,15 @@ class UpdateSettingsForm extends ConfigFormBase implements ContainerInjectionInt
       ),
     ];
 
+    $form['news'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('News mode'),
+      '#default_value' => $config->get('news'),
+      '#description' => $this->t('Display a message about available updates only
+when installed and/or recommended version of at least one extension has been
+changed.'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -162,6 +171,7 @@ class UpdateSettingsForm extends ConfigFormBase implements ContainerInjectionInt
       ->set('check.interval_days', $form_state->getValue('update_check_frequency'))
       ->set('notification.emails', $form_state->get('notify_emails'))
       ->set('notification.threshold', $form_state->getValue('update_notification_threshold'))
+      ->set('news', $form_state->getValue('news'))
       ->save();
 
     parent::submitForm($form, $form_state);
