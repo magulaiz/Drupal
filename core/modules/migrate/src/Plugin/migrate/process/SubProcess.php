e32535259c9e63aa9a555b92c88c5b4c32b49e28
Beatriz de Oliveira Rodrigues <CIANDT\beatrizrodrigues@lnb028750cps.ciandt.global>
2022-01-20 13:55:37 -0300

    fixing phpcs

--- core/modules/migrate/src/Plugin/migrate/process/SubProcess.php
+++ core/modules/migrate/src/Plugin/migrate/process/SubProcess.php
@@ -20,7 +20,7 @@ use Drupal\migrate\Row;
  *     pipeline is ignored.
  *   - include_source: (optional) If TRUE, all source row values and field
  *     values will be copied into the sub-processed row in a new property named
- *     for the source_key configuration value (see below). Defaults to FALSE. 
+ *     for the source_key configuration value (see below). Defaults to FALSE.
  *   - source_key: (optional) If include_source is TRUE, this
  *     is the name of the property of the sub-processed row which will contain
  *     the source configuration and values. Ignored if include_source is
