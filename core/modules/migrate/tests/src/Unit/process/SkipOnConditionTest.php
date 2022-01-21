249de86f4fea55da8246a16b313298bcee5d25e8
danflanagan8 <danflanagan8@gmail.com>
2022-01-21 15:51:32 -0600

    Update the unit test for skip_on_condition so it applies to the condition-plugin-based version.

--- core/modules/migrate/tests/src/Unit/process/SkipOnConditionTest.php
+++ core/modules/migrate/tests/src/Unit/process/SkipOnConditionTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\migrate\Unit\process;
 
 use Drupal\migrate\MigrateSkipProcessException;
+use Drupal\migrate\MigrateSkipRowException;
 use Drupal\migrate\Plugin\migrate\process\SkipOnCondition;
 
 /**
@@ -13,52 +14,135 @@ use Drupal\migrate\Plugin\migrate\process\SkipOnCondition;
  */
 class SkipOnConditionTest extends MigrateProcessTestCase {
 
-  public function testConfigurationValidation() {
+  /**
+   * Tests configuration validation in constructor.
+   */
+  public function testSkipOnConditionConstructor() {
+    $condition = $this->getMockBuilder('\Drupal\migrate\Plugin\MigrateProcessConditionPluginInterface')
+      ->getMock();
+    $process_condition_manager = $this->getMockBuilder('\Drupal\Component\Plugin\PluginManagerInterface')
+      ->getMock();
+    $process_condition_manager->expects($this->any())
+      ->method('createInstance')
+      ->will($this->returnValue($condition));
+
     $configuration = [];
     $this->expectException(\InvalidArgumentException::class);
-    $this->expectExceptionMessage('The "operation" must be set.');
-    (new SkipOnCondition($configuration, 'skip_on_condition', []))
-      ->transform('', $this->migrateExecutable, $this->row, 'destination_property');
-
-    $configuration = ['operation' => 'hello'];
-    $this->expectException(\InvalidArgumentException::class);
-    $this->expectExceptionMessage('The "hello" operation is not supported.');
-    (new SkipOnCondition($configuration, 'skip_on_condition', []))
-      ->transform('', $this->migrateExecutable, $this->row, 'destination_property');
-
-    $configuration = ['operation' => '<'];
-    $this->expectException(\InvalidArgumentException::class);
-    $this->expectExceptionMessage('The "<" operation requires "compared_value" to be set.');
-    (new SkipOnCondition($configuration, 'skip_on_condition', []))
+    $this->expectExceptionMessage('The "condition" must be set.');
+    (new SkipOnCondition($configuration, 'skip_on_condition', [], $process_condition_manager))
       ->transform('', $this->migrateExecutable, $this->row, 'destination_property');
   }
 
   /**
+   * @covers ::row
    * @covers ::process
-   * @dataProvider providerTestEmptyOperation
+   * @dataProvider providerTestSkipOnCondition
    */
-  public function testEmptyOperation($value, $negate) {
-    $configuration = [
-      'method' => 'process',
-      'operation' => 'empty',
-      'negate' => $negate,
-    ];
-    $this->expectException(MigrateSkipProcessException::class);
+  public function testSkipOnCondition($will_skip, $method, $evaluate, $negate, $message) {
+    $condition = $this->getMockBuilder('\Drupal\migrate\Plugin\MigrateProcessConditionPluginInterface')
+      ->getMock();
+    $condition->expects($this->once())
+      ->method('evaluate')
+      ->will($this->returnValue($evaluate));
+    $process_condition_manager = $this->getMockBuilder('\Drupal\Component\Plugin\PluginManagerInterface')
+      ->getMock();
+    $process_condition_manager->expects($this->once())
+      ->method('createInstance')
+      ->will($this->returnValue($condition));
 
-    (new SkipOnCondition($configuration, 'skip_on_condition', []))
-      ->transform($value, $this->migrateExecutable, $this->row, 'destination_property');
+    $value = 123;
+    $configuration = [
+      'method' => $method,
+      'condition' => 'foo',
+      'negate' => $negate,
+      'message' => $message,
+    ];
+    if ($will_skip) {
+      if ($method === 'process') {
+        $this->expectException(MigrateSkipProcessException::class);
+      }
+      else {
+        $this->expectException(MigrateSkipRowException::class);
+        $this->expectExceptionMessage($message);
+      }
+      (new SkipOnCondition($configuration, 'skip_on_condition', [], $process_condition_manager))
+        ->transform($value, $this->migrateExecutable, $this->row, 'destination_property');
+    }
+    else {
+      $pass_through = (new SkipOnCondition($configuration, 'skip_on_condition', [], $process_condition_manager))
+        ->transform($value, $this->migrateExecutable, $this->row, 'destination_property');
+      $this->assertSame($value, $pass_through);
+    }
   }
 
   /**
-   * Data provider for ::testEmptyOperation().
+   * Data provider for ::testSkipOnCondition().
    */
-  public function providerTestEmptyOperation() {
+  public function providerTestSkipOnCondition() {
     return [
-      'string' => ['', FALSE],
-      'array' => [[], FALSE],
-      'array_negate' => [['banana', 'apple'], TRUE],
-      'bool_negate' => [TRUE, TRUE],
-      'null' => [NULL, FALSE],
+      'skip row no message' => [
+        'will_skip' => TRUE,
+        'method' => 'row',
+        'evaluate' => TRUE,
+        'negate' => FALSE,
+        'message' => '',
+      ],
+      'skip row with message' => [
+        'will_skip' => TRUE,
+        'method' => 'row',
+        'evaluate' => TRUE,
+        'negate' => FALSE,
+        'message' => 'My message',
+      ],
+      'skip row using negate' => [
+        'will_skip' => TRUE,
+        'method' => 'row',
+        'evaluate' => FALSE,
+        'negate' => TRUE,
+        'message' => '',
+      ],
+      'skip process' => [
+        'will_skip' => TRUE,
+        'method' => 'process',
+        'evaluate' => TRUE,
+        'negate' => FALSE,
+        'message' => '',
+      ],
+      'skip process using negate' => [
+        'will_skip' => TRUE,
+        'method' => 'process',
+        'evaluate' => FALSE,
+        'negate' => TRUE,
+        'message' => '',
+      ],
+      'pass through row' => [
+        'will_skip' => FALSE,
+        'method' => 'row',
+        'evaluate' => FALSE,
+        'negate' => FALSE,
+        'message' => '',
+      ],
+      'pass through row by negation' => [
+        'will_skip' => FALSE,
+        'method' => 'row',
+        'evaluate' => TRUE,
+        'negate' => TRUE,
+        'message' => '',
+      ],
+      'pass through process' => [
+        'will_skip' => FALSE,
+        'method' => 'process',
+        'evaluate' => FALSE,
+        'negate' => FALSE,
+        'message' => '',
+      ],
+      'pass through process by negation' => [
+        'will_skip' => FALSE,
+        'method' => 'process',
+        'evaluate' => TRUE,
+        'negate' => TRUE,
+        'message' => '',
+      ],
     ];
   }
 
