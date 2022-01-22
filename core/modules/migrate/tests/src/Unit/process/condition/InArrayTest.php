07362856027a085b476471289cdaa017992303e5
danflanagan8 <danflanagan8@gmail.com>
2022-01-22 11:48:21 -0600

    Add test coverage for in_array, is_null, isset, and less_than.

---
+++ core/modules/migrate/tests/src/Unit/process/condition/InArrayTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Drupal\Tests\migrate\Unit\process\condition;
+
+use Drupal\migrate\Plugin\migrate\process\condition\InArray;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\migrate\Plugin\migrate\process\condition\InArray
+ * @group migrate
+ */
+class InArrayTest extends UnitTestCase {
+
+  /**
+   * @covers ::__construct()
+   * @dataProvider providerTestConfigurationValidation
+   */
+  public function testConfigurationValidation($configuration, $message) {
+    $this->expectException(\InvalidArgumentException::class);
+    $this->expectExceptionMessage($message);
+    $condition = new InArray($configuration, 'in_array', []);
+  }
+
+  /**
+   * Data provider for ::testConfigurationValidation().
+   */
+  public function providerTestConfigurationValidation() {
+    return [
+      [
+        'configuration' => [
+          'value' => 5,
+          'property' => 'my_property',
+        ],
+        'message' => 'Exactly one of value and property must be set when using the in_array process condition.',
+      ],
+      [
+        'configuration' => [],
+        'message' => 'Exactly one of value and property must be set when using the in_array process condition.',
+      ],
+      [
+        'configuration' => [
+          'property' => 123,
+        ],
+        'message' => 'The property configuration must be a string when using the in_array process condition.',
+      ],
+    ];
+  }
+
+  /**
+   * @covers ::evaluate
+   * @dataProvider providerTestEvaluate
+   */
+  public function testEvaluate($configuration, $source, $property_value, $expected) {
+    $row = $this->getMockBuilder('Drupal\migrate\Row')
+      ->disableOriginalConstructor()
+      ->getMock();
+    if (isset($configuration['property'])) {
+      $row->expects($this->any())
+        ->method('get')
+        ->willReturn($property_value);
+    }
+    $condition = new InArray($configuration, 'in_array', []);
+    $this->assertSame($expected, $condition->evaluate($source, $row));
+  }
+
+  /**
+   * Data provider for ::testEvaluate().
+   */
+  public function providerTestEvaluate() {
+    return [
+      [
+        'configuration' => [
+          'value' => 'string',
+        ],
+        'source' => 'string',
+        'property_value' => NULL,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'value' => 'string',
+        ],
+        'source' => 'something else',
+        'property_value' => NULL,
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, 3],
+        ],
+        'source' => 2,
+        'property_value' => NULL,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, 3],
+        ],
+        'source' => 4,
+        'property_value' => NULL,
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, 3],
+        ],
+        'source' => [1, 3, 3],
+        'property_value' => NULL,
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, [1, 2, 3]],
+        ],
+        'source' => [1, 2, 3],
+        'property_value' => NULL,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'property' => 'my_property',
+        ],
+        'source' => 2,
+        'property_value' => [1, 2, 3],
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'property' => 'my_property',
+        ],
+        'source' => 4,
+        'property_value' => [1, 2, 3],
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [
+          'property' => 'my_property',
+        ],
+        'source' => 2,
+        'property_value' => 2,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, 3],
+        ],
+        'source' => '2',
+        'property_value' => NULL,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'value' => [1, 2, 3],
+          'strict' => TRUE,
+        ],
+        'source' => '2',
+        'property_value' => NULL,
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [
+          'value' => ['key' => 'something'],
+        ],
+        'source' => 'something',
+        'property_value' => NULL,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [
+          'value' => ['key' => 'something'],
+        ],
+        'source' => 'key',
+        'property_value' => NULL,
+        'expected' => FALSE,
+      ],
+    ];
+  }
+
+}
---
+++ core/modules/migrate/tests/src/Unit/process/condition/IssetConditionTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\Tests\migrate\Unit\process\condition;
+
+use Drupal\migrate\Plugin\migrate\process\condition\IssetCondition;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\migrate\Plugin\migrate\process\condition\IssetCondition
+ * @group migrate
+ */
+class IssetConditionTest extends UnitTestCase {
+
+  /**
+   * @covers ::evaluate
+   * @dataProvider providerTestEvaluate
+   */
+  public function testEvaluate($configuration, $source, $expected) {
+    $row = $this->getMockBuilder('Drupal\migrate\Row')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $condition = new IssetCondition($configuration, 'isset', []);
+    $this->assertSame($expected, $condition->evaluate($source, $row));
+  }
+
+  /**
+   * Data provider for ::testEvaluate().
+   */
+  public function providerTestEvaluate() {
+    return [
+      [
+        'configuration' => [],
+        'source' => 'string',
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => 123,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => [1, 2, 3],
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => TRUE,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => 0,
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => '',
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => [],
+        'expected' => TRUE,
+      ],
+      [
+        'configuration' => [],
+        'source' => NULL,
+        'expected' => FALSE,
+      ],
+      [
+        'configuration' => [],
+        'source' => FALSE,
+        'expected' => TRUE,
+      ],
+    ];
+  }
+
+}
