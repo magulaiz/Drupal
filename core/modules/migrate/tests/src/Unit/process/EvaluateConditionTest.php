875e807a625e5ae3a7fcbdb9c59526e98495dbc3
danflanagan8 <danflanagan8@gmail.com>
2022-01-24 10:29:24 -0600

    Add evaluate_condition process plugin.

---
+++ core/modules/migrate/tests/src/Unit/process/EvaluateConditionTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\Tests\migrate\Unit\process;
+
+use Drupal\migrate\Plugin\migrate\process\EvaluateCondition;
+
+/**
+ * Tests the evaluate_condition process plugin.
+ *
+ * @group migrate
+ * @coversDefaultClass \Drupal\migrate\Plugin\migrate\process\EvaluateCondition
+ */
+class EvaluateConditionTest extends MigrateProcessTestCase {
+
+  /**
+   * Tests configuration validation in constructor.
+   */
+  public function testSkipOnConditionConstructor() {
+    $condition = $this->getMockBuilder('\Drupal\migrate\Plugin\MigrateProcessConditionPluginInterface')
+      ->getMock();
+    $process_condition_manager = $this->getMockBuilder('\Drupal\Component\Plugin\PluginManagerInterface')
+      ->getMock();
+    $process_condition_manager->expects($this->any())
+      ->method('createInstance')
+      ->will($this->returnValue($condition));
+
+    $configuration = [];
+    $this->expectException(\InvalidArgumentException::class);
+    $this->expectExceptionMessage('The "condition" must be set.');
+    (new EvaluateCondition($configuration, 'evaluate_condition', [], $process_condition_manager))
+      ->transform('', $this->migrateExecutable, $this->row, 'destination_property');
+  }
+
+  /**
+   * @covers ::row
+   * @covers ::process
+   * @dataProvider providerTestEvaluateCondition
+   */
+  public function testEvaluateCondition($evaluate, $negate, $expected) {
+    $condition = $this->getMockBuilder('\Drupal\migrate\Plugin\MigrateProcessConditionPluginInterface')
+      ->getMock();
+    $condition->expects($this->once())
+      ->method('evaluate')
+      ->will($this->returnValue($evaluate));
+    $process_condition_manager = $this->getMockBuilder('\Drupal\Component\Plugin\PluginManagerInterface')
+      ->getMock();
+    $process_condition_manager->expects($this->once())
+      ->method('createInstance')
+      ->will($this->returnValue($condition));
+
+    $value = 123;
+    $configuration = [
+      'condition' => 'foo',
+      'negate' => $negate,
+    ];
+    $evaluated = (new EvaluateCondition($configuration, 'evaluate_condition', [], $process_condition_manager))
+      ->transform($value, $this->migrateExecutable, $this->row, 'destination_property');
+    $this->assertSame($expected, $evaluated);
+  }
+
+  /**
+   * Data provider for ::testEvaluateCondition().
+   */
+  public function providerTestEvaluateCondition() {
+    return [
+      'true not negated' => [
+        'evaluate' => TRUE,
+        'negate' => FALSE,
+        'expected' => TRUE,
+      ],
+      'true negated' => [
+        'evaluate' => TRUE,
+        'negate' => TRUE,
+        'expected' => FALSE,
+      ],
+      'false not negated' => [
+        'evaluate' => FALSE,
+        'negate' => FALSE,
+        'expected' => FALSE,
+      ],
+      'false negated' => [
+        'evaluate' => FALSE,
+        'negate' => TRUE,
+        'expected' => TRUE,
+      ],
+    ];
+  }
+
+}
