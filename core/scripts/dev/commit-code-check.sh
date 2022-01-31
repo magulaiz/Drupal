03f28aa7861a63c4ebf57dfc29f01a4b901e0770
Tim Mallezie <tim.mallezie@gmail.com>
2022-01-31 09:18:32 +0100

    Run partial phstan local, full on CI.

--- core/scripts/dev/commit-code-check.sh
+++ core/scripts/dev/commit-code-check.sh
@@ -208,9 +208,17 @@ printf "\n"
 printf -- '-%.0s' {1..100}
 printf "\n"
 
-# Run PHPStan on all files. APCu is disabled to
+# Run PHPStan on all files on DrupalCI. APCu is disabled to
 # ensure that the composer classmap is not corrupted.
-php -d apc.enabled=0 -d apc.enable_cli=0 vendor/bin/phpstan analyze --no-progress --configuration="$TOP_LEVEL/core/phpstan.neon.dist"
+if [[ $DRUPALCI" == "1" ]]; then
+  printf "\nRunning PHPStan on *all* files.\n"
+  php -d apc.enabled=0 -d apc.enable_cli=0 vendor/bin/phpstan analyze --no-progress --configuration="$TOP_LEVEL/core/phpstan.neon.dist"
+else
+  # Only run PHPStan on changed files locally.
+  printf "\nRunning PHPStan on changed files.\n"
+  php -d apc.enabled=0 -d apc.enable_cli=0 vendor/bin/phpstan analyze --no-progress --configuration="$TOP_LEVEL/core/phpstan-partial.neon" $ABS_FILES
+fi
+
 if [ "$?" -ne "0" ]; then
   # If there are failures set the status to a number other than 0.
   FINAL_STATUS=1
