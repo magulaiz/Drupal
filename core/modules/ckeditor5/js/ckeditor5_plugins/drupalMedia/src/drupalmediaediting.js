de8dcc64a883acb2ebe65cd106e7e6219216e600
Lauri Eskola <lauri.eskola@acquia.com>
2022-02-01 13:17:57 +0200

    Remove comment about image styles

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalmediaediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalmediaediting.js
@@ -127,8 +127,6 @@ export default class DrupalMediaEditing extends Plugin {
       dispatcher.on(
         'attribute:drupalMediaStyle:drupalMedia',
         (evt, data, conversionApi) => {
-          // Re-use image style plugin CSS classes for rendering the alignment
-          // in the editing view to have equivalent functionality in the editor.
           const alignMapping = {
             alignLeft: 'drupal-media-style-align-left',
             alignRight: 'drupal-media-style-align-right',
