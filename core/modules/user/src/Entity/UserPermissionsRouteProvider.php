f9f44ed7955430b1ef8ec1da118d9fd96bcac188
Benji Fisher <benji@FisherFam.org>
2022-02-04 14:06:14 -0500

    Change bundle to entity in the route name

--- core/modules/user/src/Entity/UserPermissionsRouteProvider.php
+++ core/modules/user/src/Entity/UserPermissionsRouteProvider.php
@@ -50,7 +50,7 @@ class UserPermissionsRouteProvider implements EntityRouteProviderInterface, Enti
     $entity_type_id = $entity_type->id();
 
     if ($bundle_permissions_route = $this->getEntityPermissionsRoute($entity_type)) {
-      $collection->add("entity.$entity_type_id.bundle_permissions_form", $bundle_permissions_route);
+      $collection->add("entity.$entity_type_id.entity_permissions_form", $bundle_permissions_route);
     }
 
     return $collection;
