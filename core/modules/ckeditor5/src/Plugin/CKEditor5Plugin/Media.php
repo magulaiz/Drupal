5d684d9978350afb40d97d2f5dedb7c3a8c756f7
HARUMI JANG <harumi.jang@acquia.com>
2022-02-09 13:56:20 -0500

    added view mode info to editor settings

--- core/modules/ckeditor5/src/Plugin/CKEditor5Plugin/Media.php
+++ core/modules/ckeditor5/src/Plugin/CKEditor5Plugin/Media.php
@@ -7,6 +7,7 @@ namespace Drupal\ckeditor5\Plugin\CKEditor5Plugin;
 use Drupal\ckeditor5\Plugin\CKEditor5PluginDefault;
 use Drupal\Core\Url;
 use Drupal\editor\EditorInterface;
+use Drupal\media\Entity\MediaType;
 
 /**
  * CKEditor 5 Media plugin.
@@ -29,6 +30,15 @@ class Media extends CKEditor5PluginDefault {
       ->setRouteParameter('filter_format', $editor->getFilterFormat()->id())
       ->toString(TRUE)
       ->getGeneratedUrl();
+
+    $media_embed_filter = $editor->getFilterFormat()->filters('media_embed');
+    $this->entityDisplayRepository = \Drupal::service('entity_display.repository');
+
+    $media_bundles = MediaType::loadMultiple();
+    foreach (array_keys($media_bundles) as $bundle) {
+      $view_mode_options = array_intersect_key($this->entityDisplayRepository->getViewModeOptionsByBundle('media', $bundle), $media_embed_filter->settings['allowed_view_modes']);
+      $dynamic_plugin_config['drupalMedia']['viewModes'][$bundle] = $view_mode_options;
+    }
     $dynamic_plugin_config['drupalMedia']['isMediaUrl'] = self::getUrlWithReplacedCsrfTokenPlaceholder(
       Url::fromRoute('ckeditor5.media_image')
         ->setRouteParameter('editor', $editor->id())
