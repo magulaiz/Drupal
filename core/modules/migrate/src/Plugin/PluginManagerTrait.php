9ba8bcf8cc1c32a8a51b9ed801da8aff17535c80
Benji Fisher <benji.FisherFam.org>
2022-02-09 22:16:55 -0500

    Add the optional $manager parameter
    When the method is called from a class that is not a plugin manager,
    this argument should be supplied.

--- core/modules/migrate/src/Plugin/PluginManagerTrait.php
+++ core/modules/migrate/src/Plugin/PluginManagerTrait.php
@@ -3,6 +3,7 @@
 namespace Drupal\migrate\Plugin;
 
 use Drupal\Component\Plugin\PluginBase;
+use Drupal\Component\Plugin\PluginManagerInterface;
 
 /**
  * Provides functionality for plugin managers.
@@ -19,14 +20,24 @@ trait PluginManagerTrait {
    * we've expanded the dependencies it will depend on d6_node:page,
    * d6_node:story and so on, for other derivative migrations.
    *
+   * @param array $migration_ids
+   *   A list of plugin IDs.
+   * @param PluginManagerInterface $manager
+   *   (optional) A plugin manager. Defaults to $this, so it should be supplied
+   *   explicitly except in a class that implements PluginManagerInterface.
+   *
    * @return array
-   *   An array of expanded plugin ids.
+   *   An expanded array of plugin ids. Include the original list and add all
+   *   derivatives of plugins in the original list.
    */
-  protected function expandPluginIds(array $migration_ids) {
+  protected function expandPluginIds(array $migration_ids, ?PluginManagerInterface $manager = NULL) {
+    if ($manager === NULL) {
+      $manager = $this;
+    }
     $plugin_ids = [];
     foreach ($migration_ids as $id) {
-      $plugin_ids += preg_grep('/^' . preg_quote($id, '/') . PluginBase::DERIVATIVE_SEPARATOR . '/', array_keys($this->getDefinitions()));
-      if ($this->hasDefinition($id)) {
+      $plugin_ids += preg_grep('/^' . preg_quote($id, '/') . PluginBase::DERIVATIVE_SEPARATOR . '/', array_keys($manager->getDefinitions()));
+      if ($manager->hasDefinition($id)) {
         $plugin_ids[] = $id;
       }
     }
