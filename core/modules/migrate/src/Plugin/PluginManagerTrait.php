659c3f98d8daae6c8dff65507ee47d5ec7faf8e0
Benji Fisher <benji.FisherFam.org>
2022-02-09 22:21:19 -0500

    Code cleanup: call getDefinitions() outside the loop
    This also makes a long line a little shorter (still over 80 characters).

--- core/modules/migrate/src/Plugin/PluginManagerTrait.php
+++ core/modules/migrate/src/Plugin/PluginManagerTrait.php
@@ -35,8 +35,9 @@ trait PluginManagerTrait {
       $manager = $this;
     }
     $plugin_ids = [];
+    $all_ids = array_keys($manager->getDefinitions()));
     foreach ($migration_ids as $id) {
-      $plugin_ids += preg_grep('/^' . preg_quote($id, '/') . PluginBase::DERIVATIVE_SEPARATOR . '/', array_keys($manager->getDefinitions()));
+      $plugin_ids += preg_grep('/^' . preg_quote($id, '/') . PluginBase::DERIVATIVE_SEPARATOR . '/', $all_ids);
       if ($manager->hasDefinition($id)) {
         $plugin_ids[] = $id;
       }
