8ebb3c23ba0b517a2b158e865ef5286f1d5b8b61
Claudiu Cristea <clau.cristea@gmail.com>
2022-02-11 17:34:37 +0200

    Patch #186

--- core/modules/views/views.post_update.php
+++ core/modules/views/views.post_update.php
@@ -37,3 +37,14 @@ function views_removed_post_updates() {
     'views_post_update_image_lazy_load' => '10.0.0',
   ];
 }
+
+/**
+ * Add the actions order option to all bulk form field configurations.
+ */
+function views_post_update_bulk_form_action_order(?array &$sandbox = NULL): void {
+  /** @var \Drupal\views\ViewsConfigUpdater $view_config_updater */
+  $view_config_updater = \Drupal::classResolver(ViewsConfigUpdater::class);
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'view', function (ViewEntityInterface $view) use ($view_config_updater): bool {
+    return $view_config_updater->needsBulkFormActionsOrderUpdate($view);
+  });
+}
