12494e3d54a786a86404b3040ac6e1a90cb2153a
Lauri Eskola <lauri.eskola@acquia.com>
2022-02-14 12:54:51 +0200

    cspell

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/mediaimagetextalternative/mediaimagetextalternativeediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/mediaimagetextalternative/mediaimagetextalternativeediting.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-/* cspell:words mediaimagetextalternativecommand textalternativeformview drupalmediametadatarepository */
+/* cspell:words mediaimagetextalternativecommand textalternativeformview drupalmediametadatarepository insertdrupalmediacommand */
 
 import { Plugin } from 'ckeditor5/src/core';
 import { TooltipView, Template } from 'ckeditor5/src/ui';
