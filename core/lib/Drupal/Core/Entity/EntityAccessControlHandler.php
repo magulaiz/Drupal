3baf141e21f2ef78ac11b85cab4427a65e370548
dpi <pro@danielph.in>
2022-02-17 00:25:18 +0800

    test fixes + fix for 9.4 merge

--- core/lib/Drupal/Core/Entity/EntityAccessControlHandler.php
+++ core/lib/Drupal/Core/Entity/EntityAccessControlHandler.php
@@ -340,14 +340,16 @@ class EntityAccessControlHandler extends EntityHandlerBase implements EntityAcce
     $default = $default->andIf($entity_default);
 
     // Invoke hook and collect grants/denies for field access from other
-    // modules. Our default access flag is masked under the ':default' key.
-    $grants = [':default' => $default];
+    // modules.
+    $grants = [];
     $this->moduleHandler()->invokeAllWith(
       'entity_field_access',
       function (callable $hook, string $module) use ($operation, $field_definition, $account, $items, &$grants) {
-        $grants[$module] = $hook($operation, $field_definition, $account, $items);
+        $grants[] = [$module => $hook($operation, $field_definition, $account, $items)];
       }
     );
+    // Our default access flag is masked under the ':default' key.
+    $grants = array_merge([':default' => $default], ...$grants);
 
     // Also allow modules to alter the returned grants/denies.
     $context = [
