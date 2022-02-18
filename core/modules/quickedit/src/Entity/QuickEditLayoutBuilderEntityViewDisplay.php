e644e9b9d9531784d31932e0884ea767c89abcb3
Michael Strelan <mstrelan@gmail.com>
2022-02-18 13:44:31 +1000

    Clean up unused use statements

--- core/modules/quickedit/src/Entity/QuickEditLayoutBuilderEntityViewDisplay.php
+++ core/modules/quickedit/src/Entity/QuickEditLayoutBuilderEntityViewDisplay.php
@@ -2,10 +2,7 @@
 
 namespace Drupal\quickedit\Entity;
 
-use Drupal\Component\Plugin\ConfigurableInterface;
-use Drupal\Component\Plugin\DerivativeInspectionInterface;
 use Drupal\layout_builder\Entity\LayoutBuilderEntityViewDisplay;
-use Drupal\quickedit\LayoutBuilderIntegration;
 
 /**
  * Provides an entity view display entity that has a layout with quickedit.
