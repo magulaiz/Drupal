a462acbe8f492c6f2ea4159e52bb4198b1ba3a78
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2022-02-21 13:28:03 +0530

    added @see in doc

--- core/modules/update/tests/src/Traits/ProjectTestTrait.php
+++ core/modules/update/tests/src/Traits/ProjectTestTrait.php
@@ -12,6 +12,8 @@ trait ProjectTestTrait {
    *
    * @param array $system_info
    *   The system information.
+   *
+   * @see update_test_system_info_alter()
    */
   protected function setProjectsInfo(array $system_info): void {
     $this->config('update_test.settings')->set('system_info', $system_info)->save();
