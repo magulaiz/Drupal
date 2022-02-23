6b9f78bedd52f7a7075db496f314faaa25d70dac
dpi <pro@danielph.in>
2022-02-23 22:48:13 +0800

    add coverage for implementsHook via daffie

--- core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
@@ -306,7 +306,7 @@ class ModuleHandlerTest extends UnitTestCase {
   /**
    * Tests implementations methods when module is enabled.
    *
-   * @covers ::implementsHook
+   * @covers ::hasImplementations
    * @covers ::loadAllIncludes
    */
   public function testImplementsHookModuleEnabled() {
@@ -321,7 +321,7 @@ class ModuleHandlerTest extends UnitTestCase {
   }
 
   /**
-   * Tests getImplementations.
+   * Tests deprecation of the ::getImplementations method.
    *
    * @covers ::getImplementations
    * @covers ::getImplementationInfo
@@ -332,6 +332,18 @@ class ModuleHandlerTest extends UnitTestCase {
   public function testGetImplementations() {
     $this->expectDeprecation('ModuleHandlerInterface::getImplementations() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead you should use ModuleHandlerInterface::invokeAllWith() for hook invocations, or you should use ModuleHandlerInterface::hasImplementations() to determine if hooks implementations exist. See https://www.drupal.org/node/3000490');
     $this->assertEquals(['module_handler_test'], $this->getModuleHandler()->getImplementations('hook'));
+  }
+
+  /**
+   * Tests deprecation of the ::implementsHook method.
+   *
+   * @covers ::implementsHook
+   *
+   * @group legacy
+   */
+  public function testImplementsHook() {
+    $this->expectDeprecation('ModuleHandlerInterface::implementsHook() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead you should use ModuleHandlerInterface::hasImplementations()  with the $modules argument. See https://www.drupal.org/node/3000490');
+    $this->assertTrue($this->getModuleHandler()->implementsHook('module_handler_test', 'hook'));
   }
 
   /**
