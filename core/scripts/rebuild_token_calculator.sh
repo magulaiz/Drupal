dd4e22b91376998c78e1692028fa3f85fae446db
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-02-24 15:40:34 +0000

    Fixed inclusion of autoloader in scripts.

--- core/scripts/rebuild_token_calculator.sh
+++ core/scripts/rebuild_token_calculator.sh
@@ -15,7 +15,7 @@ if (PHP_SAPI !== 'cli') {
   return;
 }
 
-$autoloader = require __DIR__ . '/../../autoload.php';
+$autoloader = require_once dirname(exec("pwd") . '/' . trim($_SERVER['argv'][0], './'), 3) . '/autoload.php';
 
 $request = Request::createFromGlobals();
 Settings::initialize(DRUPAL_ROOT, DrupalKernel::findSitePath($request), $autoloader);
