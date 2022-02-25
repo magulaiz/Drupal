ca7746f8f8021c07dc78bc66fb295a0a91bd0b8d
Wim Leers <wim.leers@acquia.com>
2022-02-25 17:30:29 +0100

    Revert the "realtime validation errors" infrastructure back to the state in HEAD.

--- core/modules/ckeditor5/src/Plugin/Editor/CKEditor5.php
+++ core/modules/ckeditor5/src/Plugin/Editor/CKEditor5.php
@@ -270,11 +270,8 @@ class CKEditor5 extends EditorBase implements ContainerFactoryPluginInterface {
     // AJAX validation errors should appear visually close to the text editor
     // since this is a very long form: otherwise they would not be noticed.
     $form['real_time_validation_errors_location'] = [
-      '#type' => 'status_messages',
+      '#type' => 'container',
       '#id' => 'ckeditor5-realtime-validation-messages-container',
-      '#attributes' => [
-        'data-drupal-messages' => TRUE,
-      ],
     ];
 
     $form['toolbar'] = [
