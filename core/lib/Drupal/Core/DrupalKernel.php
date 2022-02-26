6854f0f5b0b504e5f4639bb067ba8037220b1e16
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-02-26 15:37:56 +0000

    Renamed DrupalKernel::guessApplicationRoot() since it by default doesn’t guess.

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -325,9 +325,23 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
     }
 
     if ($app_root === NULL) {
-      $app_root = static::guessApplicationRoot();
+      $app_root = static::getApplicationRoot();
     }
     $this->root = $app_root;
+  }
+
+  /**
+   * Returns the application root directory.
+   *
+   * @deprecated in drupal:9.4.0 and is removed from drupal:11.0.0. Use
+   *   getApplicationRoot() instead.
+   *
+   * @see https://www.drupal.org/node/3267862
+   */
+  protected static function guessApplicationRoot() {
+    @trigger_error('The guessApplicationRoot() method is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Use getApplicationRoot() instead.', E_USER_DEPRECATED);
+
+    return static::getApplicationRoot();
   }
 
   /**
@@ -346,7 +360,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
    *
    * @see \Drupal\Composer\Plugin\Scaffold\GenerateAutoloadReferenceFile
    */
-  protected static function guessApplicationRoot() {
+  protected static function getApplicationRoot() {
     if (class_exists(DrupalLocation::class)) {
       return DrupalLocation::APP_ROOT;
     }
@@ -421,7 +435,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
     }
 
     if ($app_root === NULL) {
-      $app_root = static::guessApplicationRoot();
+      $app_root = static::getApplicationRoot();
     }
 
     // Check for a test override.
@@ -1014,7 +1028,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
 
     // Determine the application root if it's not supplied.
     if ($app_root === NULL) {
-      $app_root = static::guessApplicationRoot();
+      $app_root = static::getApplicationRoot();
     }
 
     error_reporting(E_ALL);
