6308f79f0cc28bc3dfec83cf35a5dd73b515aa7a
Lauri Eskola <lauri.eskola@acquia.com>
2022-03-02 18:30:36 +0200

    Fix merge

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalelementstyle/drupalelementstylecommand.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalelementstyle/drupalelementstylecommand.js
@@ -84,21 +84,25 @@ export default class DrupalElementStyleCommand extends Command {
 
     if (this.isEnabled) {
       this.value = element.getAttribute('drupalElementStyle');
-    } else {
-      this.value = false;
-      // Check if there is a default style to apply to the element.
-      // eslint-disable-next-line no-restricted-syntax
-      for (const [name, style] of this._styles.entries()) {
-        if (style.isDefault) {
-          const appliesToCurrentElement = style.modelElements.find(
-            (modelElement) => element.is('element', modelElement),
-          );
-          if (appliesToCurrentElement) {
-            this.value = name;
-            break;
+
+      // If value is falsy, check if there is a default style to apply to the
+      // element.
+      if (!this.value) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [name, style] of this._styles.entries()) {
+          if (style.isDefault) {
+            const appliesToCurrentElement = style.modelElements.find(
+              (modelElement) => element.is('element', modelElement),
+            );
+            if (appliesToCurrentElement) {
+              this.value = name;
+              break;
+            }
           }
         }
       }
+    } else {
+      this.value = false;
     }
   }
 
