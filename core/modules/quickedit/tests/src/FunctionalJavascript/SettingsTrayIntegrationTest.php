b37153e5174dc02a3ee0a3c2f3f223b06feafca1
tstoeckler <tstoeckler@107158.no-reply.drupal.org>
2022-03-02 13:04:32 +0000

    Revert "Revert failing SettingsTrayIntegration change"
    This reverts commit 25cefefbffa3044d46384ce5bd80fab6e42c629a

--- core/modules/quickedit/tests/src/FunctionalJavascript/SettingsTrayIntegrationTest.php
+++ core/modules/quickedit/tests/src/FunctionalJavascript/SettingsTrayIntegrationTest.php
@@ -88,11 +88,7 @@ class SettingsTrayIntegrationTest extends SettingsTrayTestBase {
 
       $this->enableTheme($theme);
 
-      $block = $this->placeBlock($block_plugin, [
-        // @todo Remove this when
-        //   https://www.drupal.org/project/drupal/issues/3257504 is fixed.
-        'region' => 'sidebar_first',
-      ]);
+      $block = $this->placeBlock($block_plugin);
       $block_selector = $this->getBlockSelector($block);
       // Load the same page twice.
       foreach ([1, 2] as $page_load_times) {
