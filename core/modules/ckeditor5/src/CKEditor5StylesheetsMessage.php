488277198d70c74e4ee5bcb66a62c4294441c2eb
Wim Leers <wim.leers@acquia.com>
2022-03-03 13:28:36 +0100

    Strict types.

--- core/modules/ckeditor5/src/CKEditor5StylesheetsMessage.php
+++ core/modules/ckeditor5/src/CKEditor5StylesheetsMessage.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\ckeditor5;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
--- core/modules/ckeditor5/src/EventSubscriber/CKEditor5CacheTag.php
+++ core/modules/ckeditor5/src/EventSubscriber/CKEditor5CacheTag.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\ckeditor5\EventSubscriber;
 
 use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
