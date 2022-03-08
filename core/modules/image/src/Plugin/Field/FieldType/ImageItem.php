3b283615abc44f83dc10ec85782807b4cb2be34c
Dieter Holvoet <dieter.holvoet@gmail.com>
2022-03-08 17:29:50 +0100

    Use set method instead of setting non-existing properties

--- core/modules/image/src/Plugin/Field/FieldType/ImageItem.php
+++ core/modules/image/src/Plugin/Field/FieldType/ImageItem.php
@@ -326,19 +326,29 @@ class ImageItem extends FileItem {
   public function onChange($property_name, $notify = TRUE) {
     parent::onChange($property_name, $notify);
 
-    $width = $this->width;
-    $height = $this->height;
-
     // Determine the dimensions if necessary.
-    if ($this->entity instanceof EntityInterface) {
-      if (empty($width) || empty($height)) {
-        $image = \Drupal::service('image.factory')->get($this->entity->getFileUri());
-        if ($image->isValid()) {
-          $this->width = $image->getWidth();
-          $this->height = $image->getHeight();
-        }
-      }
+    if (!$this->entity instanceof EntityInterface) {
+        return;
     }
+
+    if ($property_name === 'width' || $property_name === 'height') {
+        return;
+    }
+
+    $width = $this->get('width')->getValue();
+    $height = $this->get('height')->getValue();
+
+    if ($width !== NULL && $height !== NULL) {
+        return;
+    }
+
+    $image = \Drupal::service('image.factory')->get($this->entity->getFileUri());
+    if (!$image->isValid()) {
+        return;
+    }
+
+    $this->set('width', $image->getWidth());
+    $this->set('height', $image->getHeight());
   }
 
   /**
@@ -350,24 +360,6 @@ class ImageItem extends FileItem {
     if (!$this->entity instanceof EntityInterface) {
       $this->getLogger('image')->warning("Missing file with ID %id.", ['%id' => $this->target_id]);
     }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getValue() {
-    $values = parent::getValue();
-
-    // If the image is invalid and no width/height was calculated,
-    // include NULL values to prevent undefined index notices.
-    if ($this->width === NULL) {
-      $values['width'] = NULL;
-    }
-    if ($this->height === NULL) {
-      $values['height'] = NULL;
-    }
-
-    return $values;
   }
 
   /**
