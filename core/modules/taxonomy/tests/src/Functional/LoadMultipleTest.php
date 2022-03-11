d874df898b4e00f9bfc8827bf13a354e6e77269b
Daniel Veza <daniel@veza.nz>
2022-03-11 18:37:16 +1300

    Issue #3269018: Fix obsolete reference to entity_load_multiple

--- core/modules/taxonomy/tests/src/Functional/LoadMultipleTest.php
+++ core/modules/taxonomy/tests/src/Functional/LoadMultipleTest.php
@@ -24,7 +24,7 @@ class LoadMultipleTest extends TaxonomyTestBase {
 
   /**
    * Create a vocabulary and some taxonomy terms, ensuring they're loaded
-   * correctly using entity_load_multiple().
+   * correctly using Term::loadMultiple().
    */
   public function testTaxonomyTermMultipleLoad() {
     // Create a vocabulary.
