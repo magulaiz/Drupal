a5190888dc7b2a190d66184e37caf29d5d40d0ed
Théodore BIADALA <theodore@biadala.net>
2022-03-14 11:33:15 +0100

    Script to check build CKE5 plugins

---
+++ core/scripts/js/ckeditor5-check-plugins.js
@@ -0,0 +1,40 @@
+/**
+ * @file
+ *
+ * Provides the `check:ckeditor5` command.
+ *
+ * Check that the plugins are built with the appropriate dependencies.
+ *
+ *
+ * @internal This file is part of the core javascript build process and is only
+ * meant to be used in that context.
+ */
+
+"use strict";
+
+const glob = require("glob");
+const log = require("./log");
+const fs = require("fs").promises;
+const child_process = require("child_process");
+
+async function getContents(files) {
+  return Object.fromEntries(
+    await Promise.all(
+      files.map(async (file) => [file, (await fs.readFile(file)).toString()])
+    )
+  );
+}
+
+(async () => {
+  const files = glob.sync("./modules/ckeditor5/js/build/*.js");
+
+  const pluginsBefore = await getContents(files);
+  // Execute the plugin build script.
+  child_process.execSync("yarn run build:ckeditor5");
+  const pluginsAfter = await getContents(files);
+
+  if (JSON.stringify(pluginsBefore) !== JSON.stringify(pluginsAfter)) {
+    log("Mismatch in CKEditor 5 plugins build artefacts.");
+    process.exitCode = 1;
+  }
+})();
