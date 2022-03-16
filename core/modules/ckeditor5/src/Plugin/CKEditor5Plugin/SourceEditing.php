39f8b747e77896804aa9005e337ef0642a527229
bnjmnm <bnjmnm@2369194.no-reply.drupal.org>
2022-03-16 11:50:56 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/ckeditor5/src/Plugin/CKEditor5Plugin/SourceEditing.php
+++ core/modules/ckeditor5/src/Plugin/CKEditor5Plugin/SourceEditing.php
@@ -77,7 +77,7 @@ class SourceEditing extends CKEditor5PluginDefault implements CKEditor5PluginCon
   public function getDynamicPluginConfig(array $static_plugin_config, EditorInterface $editor): array {
     $restrictions = HTMLRestrictions::fromString(implode(' ', $this->configuration['allowed_tags']));
     // Only handle concrete HTML elements to allow the Wildcard HTML support
-    // plugin handle the wildcards.
+    // plugin to handle wildcards.
     // @see \Drupal\ckeditor5\Plugin\CKEditor5Plugin\WildcardHtmlSupport
     $concrete_restrictions = $restrictions->getConcreteSubset();
     return [
