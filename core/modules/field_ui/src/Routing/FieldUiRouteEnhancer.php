35f7150d9d3304a497ef27529d63a1ded08363db
Yogesh Pawar <yogesh.pawar01@gmail.com>
2022-03-16 23:54:53 +0530

    Resolved CS errors.

--- core/modules/field_ui/src/Routing/FieldUiRouteEnhancer.php
+++ core/modules/field_ui/src/Routing/FieldUiRouteEnhancer.php
@@ -4,8 +4,6 @@ namespace Drupal\field_ui\Routing;
 
 @trigger_error('The ' . __NAMESPACE__ . '\EntityBundleRouteEnhancer is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead, use \Drupal\Core\Entity\Enhancer\EntityBundleRouteEnhancer. See https://www.drupal.org/node/3245017', E_USER_DEPRECATED);
 
-use Drupal\Core\Entity\Enhancer\EntityBundleRouteEnhancer;
-
 /**
  * Enhances Field UI routes by adding proper information about the bundle name.
  *
