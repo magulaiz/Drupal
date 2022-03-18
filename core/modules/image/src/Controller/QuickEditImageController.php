4e109cf85ff12389601eee1b8754d6430bd8613a
xjm <xjm@65776.no-reply.drupal.org>
2022-03-18 15:04:12 -0500

    Fix coding standards issue so the tests will run.

--- core/modules/image/src/Controller/QuickEditImageController.php
+++ core/modules/image/src/Controller/QuickEditImageController.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\image\Controller;
 
+use Drupal\quickedit\Controller\QuickEditImageController;
+
 @trigger_error(__NAMESPACE__ . '\QuickEditImageController is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead, use \Drupal\quickedit\QuickEditImageController. See https://www.drupal.org/node/3265140', E_USER_DEPRECATED);
 
-class QuickEditImageController extends \Drupal\quickedit\Controller\QuickEditImageController {}
+class QuickEditImageController extends QuickEditImageController {}
