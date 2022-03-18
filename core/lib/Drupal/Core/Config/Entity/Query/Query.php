7d5d703e513096d6ad08de045ca6c2b940813a11
quietone <quietone@2572884.no-reply.drupal.org>
2022-03-19 00:04:11 +1300

    quick fix in Query.php

--- core/lib/Drupal/Core/Config/Entity/Query/Query.php
+++ core/lib/Drupal/Core/Config/Entity/Query/Query.php
@@ -129,6 +129,7 @@ class Query extends QueryBase implements QueryInterface {
     $prefix_length = strlen($prefix);
 
     // Search the conditions for restrictions on configuration object names.
+    $restrictions = FALSE;
     $filter_by_names = [];
     $id_condition = NULL;
     $id_key = $this->entityType->getKey('id');
@@ -138,6 +139,7 @@ class Query extends QueryBase implements QueryInterface {
       foreach ($conditions as $condition_key => $condition) {
         $operator = $condition['operator'] ?: (is_array($condition['value']) ? 'IN' : '=');
         if (is_string($condition['field']) && ($operator == 'IN' || $operator == '=')) {
+          $restrictions = TRUE;
           // Special case ID lookups.
           if ($condition['field'] == $id_key) {
             $ids = (array) $condition['value'];
@@ -176,7 +178,7 @@ class Query extends QueryBase implements QueryInterface {
     }
 
     // If no restrictions on IDs were found, we need to parse all records.
-    if ($filter_by_names === []) {
+    if ($restrictions === FALSE) {
       $filter_by_names = $this->configFactory->listAll($prefix);
     }
     else {
