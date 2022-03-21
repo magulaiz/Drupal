b75618bbba904b2e3f3d2ed689bcf66736ed9df3
Wim Leers <wim.leers@acquia.com>
2022-03-21 12:31:15 +0100

    Remove CKEditor 4 dependency from Editor config entity REST+JSON:API tests.

--- core/modules/editor/tests/src/Functional/Rest/EditorResourceTestBase.php
+++ core/modules/editor/tests/src/Functional/Rest/EditorResourceTestBase.php
@@ -14,7 +14,7 @@ abstract class EditorResourceTestBase extends ConfigEntityResourceTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['ckeditor', 'editor'];
+  protected static $modules = ['ckeditor5', 'editor'];
 
   /**
    * {@inheritdoc}
@@ -59,7 +59,7 @@ abstract class EditorResourceTestBase extends ConfigEntityResourceTestBase {
     // Create a "Camelids" editor.
     $camelids = Editor::create([
       'format' => 'llama',
-      'editor' => 'ckeditor',
+      'editor' => 'ckeditor5',
     ]);
     $camelids
       ->setImageUploadSettings([
@@ -87,10 +87,10 @@ abstract class EditorResourceTestBase extends ConfigEntityResourceTestBase {
           'filter.format.llama',
         ],
         'module' => [
-          'ckeditor',
+          'ckeditor5',
         ],
       ],
-      'editor' => 'ckeditor',
+      'editor' => 'ckeditor5',
       'format' => 'llama',
       'image_upload' => [
         'status' => FALSE,
--- core/modules/jsonapi/tests/src/Functional/EditorTest.php
+++ core/modules/jsonapi/tests/src/Functional/EditorTest.php
@@ -18,7 +18,7 @@ class EditorTest extends ConfigEntityResourceTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['filter', 'editor', 'ckeditor'];
+  protected static $modules = ['filter', 'editor', 'ckeditor5'];
 
   /**
    * {@inheritdoc}
@@ -73,7 +73,7 @@ class EditorTest extends ConfigEntityResourceTestBase {
     // Create a "Camelids" editor.
     $camelids = Editor::create([
       'format' => 'llama',
-      'editor' => 'ckeditor',
+      'editor' => 'ckeditor5',
     ]);
     $camelids
       ->setImageUploadSettings([
@@ -120,10 +120,10 @@ class EditorTest extends ConfigEntityResourceTestBase {
               'filter.format.llama',
             ],
             'module' => [
-              'ckeditor',
+              'ckeditor5',
             ],
           ],
-          'editor' => 'ckeditor',
+          'editor' => 'ckeditor5',
           'image_upload' => [
             'status' => FALSE,
             'scheme' => 'public',
@@ -223,7 +223,7 @@ class EditorTest extends ConfigEntityResourceTestBase {
 
     $entity = Editor::create([
       'format' => 'pachyderm',
-      'editor' => 'ckeditor',
+      'editor' => 'ckeditor5',
     ]);
 
     $entity->setImageUploadSettings([
