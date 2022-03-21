092db2f95a280d6b2632b1ec09d21fa064f98d81
Ravi Shankar <ravi.s@opensenselabs.com>
2022-03-21 08:15:11 +0530

    Made changes as per suggestions of #13.

--- core/modules/taxonomy/src/TermStorageInterface.php
+++ core/modules/taxonomy/src/TermStorageInterface.php
@@ -87,7 +87,7 @@ interface TermStorageInterface extends ContentEntityStorageInterface {
    *   numbers of terms. Defaults to FALSE.
    *
    * @return object[]|\Drupal\taxonomy\TermInterface[]
-   *   An numerically-indexed array of term objects that are the children of the
+   *   A numerically indexed array of term objects that are the children of the
    *   vocabulary $vid.
    */
   public function loadTree($vid, $parent = 0, $max_depth = NULL, $load_entities = FALSE);
