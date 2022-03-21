d3a540100a134d8905fefca7852dc84263013220
Wim Leers <wim.leers@acquia.com>
2022-03-21 15:27:17 +0100

    Use default CKEditor 5 settings instead of default CKEditor 4 settings in REST+JSON:API test expectations.

--- core/modules/editor/tests/src/Functional/Rest/EditorResourceTestBase.php
+++ core/modules/editor/tests/src/Functional/Rest/EditorResourceTestBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\editor\Functional\Rest;
 
+use Drupal\ckeditor5\Plugin\CKEditor5Plugin\Heading;
 use Drupal\editor\Entity\Editor;
 use Drupal\filter\Entity\FilterFormat;
 use Drupal\Tests\rest\Functional\EntityResource\ConfigEntityResourceTestBase;
@@ -105,49 +106,10 @@ abstract class EditorResourceTestBase extends ConfigEntityResourceTestBase {
       'langcode' => 'en',
       'settings' => [
         'toolbar' => [
-          'rows' => [
-            [
-              [
-                'name' => 'Formatting',
-                'items' => [
-                  'Bold',
-                  'Italic',
-                ],
-              ],
-              [
-                'name' => 'Links',
-                'items' => [
-                  'DrupalLink',
-                  'DrupalUnlink',
-                ],
-              ],
-              [
-                'name' => 'Lists',
-                'items' => [
-                  'BulletedList',
-                  'NumberedList',
-                ],
-              ],
-              [
-                'name' => 'Media',
-                'items' => [
-                  'Blockquote',
-                  'DrupalImage',
-                ],
-              ],
-              [
-                'name' => 'Tools',
-                'items' => [
-                  'Source',
-                ],
-              ],
-            ],
-          ],
+          'items' => ['heading', 'bold', 'italic'],
         ],
         'plugins' => [
-          'language' => [
-            'language_list' => 'un',
-          ],
+          'ckeditor5_heading' => Heading::DEFAULT_CONFIGURATION,
         ],
       ],
       'status' => TRUE,
