e4a523e8348f1e2b35117706e913e2ce55755c54
HARUMI JANG <harumi.jang@acquia.com>
2022-03-29 10:50:46 -0400

    removed build files from commit

--- core/modules/ckeditor5/webpack.config.js
+++ core/modules/ckeditor5/webpack.config.js
@@ -11,10 +11,13 @@ function getDirectories(srcpath) {
 }
 
 module.exports = [];
+prodArray = [];
+devArray = [];
+
 // Loop through every subdirectory in ckeditor5_plugins, which should be a different
 // plugin, and build them all in ./build.
 getDirectories(path.resolve(__dirname, './js/ckeditor5_plugins')).forEach((dir) => {
-  const bc = {
+  const prod = {
     mode: 'production',
     optimization: {
       minimize: true,
@@ -59,5 +62,49 @@ getDirectories(path.resolve(__dirname, './js/ckeditor5_plugins')).forEach((dir) 
     },
   };
 
-  module.exports.push(bc);
+  const dev = {
+    devtool: false,
+    mode: 'development',
+    optimization: {
+      minimize: false,
+      moduleIds: 'named',
+    },
+    entry: {
+      path: path.resolve(
+        __dirname,
+        'js/ckeditor5_plugins',
+        dir,
+        'src/index.js',
+      ),
+    },
+    output: {
+      path: path.resolve(__dirname, './js/build'),
+      filename: `${dir}.js`,
+      library: ['CKEditor5', dir],
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+    },
+    plugins: [
+      new webpack.BannerPlugin('cspell:disable'),
+      new webpack.DllReferencePlugin({
+        manifest: require(path.resolve(__dirname, '../../node_modules/ckeditor5/build/ckeditor5-dll.manifest.json')), // eslint-disable-line global-require, import/no-unresolved
+        scope: 'ckeditor5/src',
+        name: 'CKEditor5.dll',
+      }),
+    ],
+    module: {
+      rules: [{ test: /\.svg$/, use: 'raw-loader' }],
+    },
+  }
+  prodArray.push(prod);
+  devArray.push(dev);
 });
+
+module.exports = (env, argv) => {
+  // Files aren't minified in build with the development flag.
+  if (argv.mode === 'development') {
+    return devArray;
+  } else {
+    return prodArray;
+  }
+}
