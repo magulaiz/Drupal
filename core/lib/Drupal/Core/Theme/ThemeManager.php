857cb35ee093759032bdc44d302966d44915d15e
Tatiana Glushchenko <tatianag@catalyst.net.nz>
2022-03-30 20:14:04 +1300

    Issue #2994975 by mdolnik, tatianag: Fix the incorrect order of theme hooks

--- core/lib/Drupal/Core/Theme/ThemeManager.php
+++ core/lib/Drupal/Core/Theme/ThemeManager.php
@@ -428,6 +428,7 @@ class ThemeManager implements ThemeManagerInterface {
     }
 
     $theme_keys = array_keys($theme->getBaseThemeExtensions());
+    $theme_keys = array_reverse($theme_keys);
     $theme_keys[] = $theme->getName();
     $functions = [];
     foreach ($theme_keys as $theme_key) {
