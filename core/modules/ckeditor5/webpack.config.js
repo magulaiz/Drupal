2b60f4660c1801ac1edbc955f80683dcba95184e
HARUMI JANG <harumi.jang@acquia.com>
2022-03-31 14:38:25 -0400

    change dev properties in new copy of object

--- core/modules/ckeditor5/webpack.config.js
+++ core/modules/ckeditor5/webpack.config.js
@@ -10,14 +10,13 @@ function getDirectories(srcpath) {
     .filter((item) => fs.statSync(path.join(srcpath, item)).isDirectory());
 }
 
-module.exports = [];
-prodArray = [];
-devArray = [];
+let prodPluginBuilds = [];
+let devPluginBuilds = [];
 
 // Loop through every subdirectory in ckeditor5_plugins, which should be a different
 // plugin, and build them all in ./build.
 getDirectories(path.resolve(__dirname, './js/ckeditor5_plugins')).forEach((dir) => {
-  const prod = {
+  const bc = {
     mode: 'production',
     optimization: {
       minimize: true,
@@ -62,49 +61,17 @@ getDirectories(path.resolve(__dirname, './js/ckeditor5_plugins')).forEach((dir) 
     },
   };
 
-  const dev = {
-    devtool: false,
-    mode: 'development',
-    optimization: {
-      minimize: false,
-      moduleIds: 'named',
-    },
-    entry: {
-      path: path.resolve(
-        __dirname,
-        'js/ckeditor5_plugins',
-        dir,
-        'src/index.js',
-      ),
-    },
-    output: {
-      path: path.resolve(__dirname, './js/build'),
-      filename: `${dir}.js`,
-      library: ['CKEditor5', dir],
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-    },
-    plugins: [
-      new webpack.BannerPlugin('cspell:disable'),
-      new webpack.DllReferencePlugin({
-        manifest: require(path.resolve(__dirname, '../../node_modules/ckeditor5/build/ckeditor5-dll.manifest.json')), // eslint-disable-line global-require, import/no-unresolved
-        scope: 'ckeditor5/src',
-        name: 'CKEditor5.dll',
-      }),
-    ],
-    module: {
-      rules: [{ test: /\.svg$/, use: 'raw-loader' }],
-    },
-  }
-  prodArray.push(prod);
-  devArray.push(dev);
+  const dev = {...bc, mode: 'development', optimization: {...bc.optimization, minimize: false}, devtool: false};
+
+  prodPluginBuilds.push(bc);
+  devPluginBuilds.push(dev);
 });
 
 module.exports = (env, argv) => {
   // Files aren't minified in build with the development flag.
   if (argv.mode === 'development') {
-    return devArray;
+    return devPluginBuilds;
   } else {
-    return prodArray;
+    return prodPluginBuilds;
   }
 }
