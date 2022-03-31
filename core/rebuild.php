e5f7ea35e767b7c6908c4e278f7f3e084082953b
Roman Paska <roman.paska@lemberg.co.uk>
2022-03-31 12:59:54 +0300

    Ensure that there is a trailing slash when running in a subdirectory

--- core/rebuild.php
+++ core/rebuild.php
@@ -55,4 +55,5 @@ if (Settings::get('rebuild_access', FALSE) ||
   drupal_rebuild($autoloader, $request);
   \Drupal::messenger()->addStatus('Cache rebuild complete.');
 }
-header('Location: ' . $request->getUriForPath('/'));
+$base_path = dirname($request->getBaseUrl(), 2);
+header('Location: ' . $request->getSchemeAndHttpHost() . ($base_path === '/' ? $base_path : $base_path . '/'));
