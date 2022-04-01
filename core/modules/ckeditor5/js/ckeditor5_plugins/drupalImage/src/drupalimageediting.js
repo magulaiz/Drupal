f0bc4aec457c17e2ed4c409561c75e97b61314a3
Théodore BIADALA <theodore@biadala.net>
2022-04-01 11:22:21 +0200

    cspell

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// cSpell:words conversionutils downcasted linkimageediting
+// cSpell:words conversionutils downcasted linkimageediting emptyelement downcastdispatcher
 import { Plugin } from 'ckeditor5/src/core';
 import { setViewAttributes } from '@ckeditor/ckeditor5-html-support/src/conversionutils';
 
