0b69f8b11862e0db91a8e2b95cdd23b2168bf16f
HARUMI JANG <harumi.jang@acquia.com>
2022-04-05 18:55:53 -0400

    [WIP] filled out plugin stubs

---
+++ core/modules/ckeditor5/src/Plugin/CKEditor5Plugin/ListOrder.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ckeditor5\Plugin\CKEditor5Plugin;
+
+use Drupal\ckeditor5\Plugin\CKEditor5PluginConfigurableInterface;
+use Drupal\ckeditor5\Plugin\CKEditor5PluginDefault;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * CKEditor 5 List Order plugin.
+ *
+ * @internal
+ *   Plugin classes are internal.
+ */
+class ListOrder extends CKEditor5PluginDefault implements CKEditor5PluginConfigurableInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+//  public function setConfiguration(array $configuration) {
+//    // TODO: Implement setConfiguration() method.
+//  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['reverse' => TRUE, 'startIndex' => TRUE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['reverse'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Allow the user to reverse a numbered list'),
+      '#default_value' => $this->configuration['reverse'],
+    ];
+    $form['startIndex'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Allow the user to specify the start index of a list'),
+      '#default_value' => $this->configuration['startIndex'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $form_value = $form_state->getValue('reverse');
+    $form_state->setValue('reverse', (bool) $form_value);
+    $form_value = $form_state->getValue('startIndex');
+    $form_state->setValue('startIndex', (bool) $form_value);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['reverse'] = $form_state->getValue('reverse');
+    $this->configuration['startIndex'] = $form_state->getValue('startIndex');
+  }
+
+}
