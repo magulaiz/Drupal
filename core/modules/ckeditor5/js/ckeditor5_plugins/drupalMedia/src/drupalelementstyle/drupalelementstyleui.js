d37e1b36655718e79cf81bca6bd0c3fa3642a0e9
HARUMI JANG <harumi.jang@acquia.com>
2022-04-05 11:10:26 -0400

    added back modelElement check

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalelementstyle/drupalelementstyleui.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalelementstyle/drupalelementstyleui.js
@@ -225,12 +225,9 @@ export default class DrupalElementStyleUi extends Plugin {
 
     const filteredDefinedStyles = definedStyles.filter(function (item) {
       // @todo this filter should also check the model element.
-      if (!modelElement) {
-        return false;
-      }
       // eslint-disable-next-line no-restricted-syntax
       for (const [key, value] of toMap(item.modelAttributes)) {
-        if (modelElement.hasAttribute(key)) {
+        if (modelElement && modelElement.hasAttribute(key)) {
           return value.includes(modelElement.getAttribute(key));
         }
       }
