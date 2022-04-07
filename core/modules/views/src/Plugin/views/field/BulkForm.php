eb11ff753e95f3787b13624ff59197b6f2133482
Claudiu Cristea <clau.cristea@gmail.com>
2022-04-07 12:19:38 +0200

    Ensure a 'no action' as default option

--- core/modules/views/src/Plugin/views/field/BulkForm.php
+++ core/modules/views/src/Plugin/views/field/BulkForm.php
@@ -377,6 +377,11 @@ class BulkForm extends FieldPluginBase implements CacheableDependencyInterface, 
 
       // Replace the form submit button label.
       $form['actions']['submit']['#value'] = $this->t('Apply to selected items');
+      $form['actions']['submit']['#states'] = [
+        'disabled' => [
+          'select[name="action"]' => ['value' => ''],
+        ],
+      ];
 
       // Ensure a consistent container for filters/operations in the view header.
       $form['header'] = [
@@ -394,6 +399,7 @@ class BulkForm extends FieldPluginBase implements CacheableDependencyInterface, 
         '#title' => $this->options['action_title'],
         '#options' => $this->getBulkOptions(),
         '#empty_option' => $this->t('- Select -'),
+        '#required' => TRUE,
       ];
 
       // Duplicate the form actions into the action container in the header.
