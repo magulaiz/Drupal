3c9ca2266fe7d17b96af10aa53d6c7f6271ff9e9
Roman Paska <roman.paska@lemberg.co.uk>
2022-04-07 13:22:51 +0300

    and what about 10s

--- core/lib/Drupal/Core/Test/TestSetupTrait.php
+++ core/lib/Drupal/Core/Test/TestSetupTrait.php
@@ -170,7 +170,7 @@ trait TestSetupTrait {
         $connection_info[$target]['prefix'] = $value['prefix'] . $this->databasePrefix;
       }
       if ($connection_info['default']['driver'] === 'sqlite') {
-        $connection_info['default']['init_commands']['busy_timeout'] = 'PRAGMA busy_timeout=2500';
+        $connection_info['default']['init_commands']['busy_timeout'] = 'PRAGMA busy_timeout=10000';
       }
 
       Database::addConnectionInfo('default', 'default', $connection_info['default']);
