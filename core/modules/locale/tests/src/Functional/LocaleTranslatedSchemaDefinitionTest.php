baf343c58cbf10c4f04496bac925d0bd934c1b34
Johnny Wesley da Silva Santos <CIANDT\jwesley@lnb035237cps.ciandt.global>
2022-04-08 15:22:34 -0300

    changed classy for stark in locale module and removed nodde class from cssSelect

--- core/modules/locale/tests/src/Functional/LocaleTranslatedSchemaDefinitionTest.php
+++ core/modules/locale/tests/src/Functional/LocaleTranslatedSchemaDefinitionTest.php
@@ -25,7 +25,7 @@ class LocaleTranslatedSchemaDefinitionTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'classy';
+  protected $defaultTheme = 'stark';
 
   /**
    * {@inheritdoc}
