824f6ce3ab6f444b89496d2891b0b1c1b199ce46
Claudiu Cristea <clau.cristea@gmail.com>
2022-04-12 17:04:09 +0300

    Avoid unneeded computation

--- core/tests/Drupal/FunctionalJavascriptTests/Core/Field/TimestampFormatterWithTimeDiffTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Core/Field/TimestampFormatterWithTimeDiffTest.php
@@ -83,9 +83,9 @@ class TimestampFormatterWithTimeDiffTest extends WebDriverTestBase {
     // Unit testing Drupal.timeDiff.format(). Not using @dataProvider mechanism
     // here in order to avoid installing the site for each case.
     foreach ($this->getFormatDiffTestCases() as $case) {
-      $from = \DateTime::createFromFormat(\DateTimeInterface::RFC3339, $case['from'])->getTimestamp() * 1000;
-      $to = \DateTime::createFromFormat(\DateTimeInterface::RFC3339, $case['to'])->getTimestamp() * 1000;
-      $diff = ($to - $from) / 1000;
+      $from = \DateTime::createFromFormat(\DateTimeInterface::RFC3339, $case['from'])->getTimestamp();
+      $to = \DateTime::createFromFormat(\DateTimeInterface::RFC3339, $case['to'])->getTimestamp();
+      $diff = $to - $from;
       $options = json_encode($case['options']);
       $expected_value = json_encode($case['expected_value']);
       $expected_formatted_value = $case['expected_formatted_value'];
