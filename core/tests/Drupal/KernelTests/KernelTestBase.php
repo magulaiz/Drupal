4953481db1969884d5a7211cb6149c3d6fc58d07
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2022-04-13 20:38:41 +0000

    Use LoggingTrait in KernelTestBase

--- core/tests/Drupal/KernelTests/KernelTestBase.php
+++ core/tests/Drupal/KernelTests/KernelTestBase.php
@@ -19,6 +19,7 @@ use Drupal\Core\Language\Language;
 use Drupal\Core\Site\Settings;
 use Drupal\Core\Test\EventSubscriber\FieldStorageCreateCheckSubscriber;
 use Drupal\Core\Test\TestDatabase;
+use Drupal\Tests\Traits\Core\LoggingTrait;
 use Drupal\Tests\ConfigTestTrait;
 use Drupal\Tests\ExtensionListTestTrait;
 use Drupal\Tests\RandomGeneratorTrait;
@@ -104,6 +105,7 @@ abstract class KernelTestBase extends TestCase implements ServiceProviderInterfa
   use PhpUnitCompatibilityTrait;
   use ProphecyTrait;
   use ExpectDeprecationTrait;
+  use LoggingTrait;
 
   /**
    * {@inheritdoc}
