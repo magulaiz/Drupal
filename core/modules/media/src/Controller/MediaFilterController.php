3ef0969611f38b0874bc3f631c9b59a68559fcef
Lauri Eskola <lauri.eskola@acquia.com>
2022-04-13 17:47:24 +0300

    Revert "don't cache previews"
    This reverts commit e8828df1c5c9b123aacfa71bcf5b17b6d64fec09.

--- core/modules/media/src/Controller/MediaFilterController.php
+++ core/modules/media/src/Controller/MediaFilterController.php
@@ -125,8 +125,8 @@ class MediaFilterController implements ContainerInjectionInterface {
     return (new Response($html, 200, $headers))
       // Do not allow any intermediary to cache the response, only the end user.
       ->setPrivate()
-      // Do not cache the response to avoid reusing outdated responses.
-      ->setMaxAge(-1);
+      // Allow the end user to cache it for up to 5 minutes.
+      ->setMaxAge(300);
   }
 
   /**
