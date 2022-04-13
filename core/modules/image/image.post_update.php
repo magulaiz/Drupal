ad72fab93974a0115d8e5aa68640c4071490ac5a
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2022-04-13 09:27:06 -0500

    Revert "remove return type hints"
    This reverts commit d9ecd721aebe53ebc719f2689cbb69d762fc3ecc.

--- core/modules/image/image.post_update.php
+++ core/modules/image/image.post_update.php
@@ -22,9 +22,9 @@ function image_removed_post_updates() {
 /**
  * Add the image loading attribute setting to image field formatter instances.
  */
-function image_post_update_image_loading_attribute(array &$sandbox = NULL) {
+function image_post_update_image_loading_attribute(array &$sandbox = NULL): void {
   $image_config_updater = \Drupal::classResolver(ImageConfigUpdater::class);
-  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'entity_view_display', function (EntityViewDisplayInterface $view_display) use ($image_config_updater) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'entity_view_display', function (EntityViewDisplayInterface $view_display) use ($image_config_updater): bool {
     return $image_config_updater->processImageLazyLoad($view_display);
   });
 }
