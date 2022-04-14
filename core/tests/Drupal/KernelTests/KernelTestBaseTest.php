fdb90920d5db18dfdede505ab85611f723787caa
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2022-04-14 11:22:27 +0000

    Fix errors in tests

--- core/tests/Drupal/KernelTests/KernelTestBaseTest.php
+++ core/tests/Drupal/KernelTests/KernelTestBaseTest.php
@@ -338,7 +338,7 @@ class KernelTestBaseTest extends KernelTestBase {
    * @covers ::log
    */
   public function testExpectedLogging() {
-    $this->expectNoLog(RfcLogLevel::ERROR);
+    $this->expectNoLogMoreSevereThan(RfcLogLevel::ERROR);
     this->expectLog(RfcLogLevel::ERROR, 'test');
     \Drupal::logger('test')->error('a test error');
   }
@@ -347,7 +347,7 @@ class KernelTestBaseTest extends KernelTestBase {
    * @covers ::log
    */
   public function testUnexpectedLogging() {
-    $this->expectNoLog(RfcLogLevel::ERROR);
+    $this->expectNoLogMoreSevereThan(RfcLogLevel::ERROR);
     $this->expectException(ExpectationFailedException::class);
     \Drupal::logger('test')->error('a test error');
   }
