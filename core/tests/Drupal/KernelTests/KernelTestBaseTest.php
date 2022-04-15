0adfa0f5cbbf1a2b67e30cf44def8b1cbd01f787
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2022-04-15 10:17:08 +0000

    Syntax in tests

--- core/tests/Drupal/KernelTests/KernelTestBaseTest.php
+++ core/tests/Drupal/KernelTests/KernelTestBaseTest.php
@@ -388,7 +388,7 @@ class KernelTestBaseTest extends KernelTestBase {
    */
   public function testExpectedLogging() {
     $this->expectNoLogMoreSevereThan(RfcLogLevel::ERROR);
-    this->expectLog(RfcLogLevel::ERROR, 'test');
+    $this->expectLog(RfcLogLevel::ERROR, 'test');
     \Drupal::logger('test')->error('a test error');
   }
 
