6a99e615207941458ba931dc8c674435fdc13c97
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2022-04-17 12:10:34 +0000

    Coding standards

--- core/tests/Drupal/KernelTests/KernelTestBase.php
+++ core/tests/Drupal/KernelTests/KernelTestBase.php
@@ -1046,7 +1046,7 @@ abstract class KernelTestBase extends TestCase implements ServiceProviderInterfa
   }
 
   /**
-   * Get an active instance of the AssertableLogger.
+   * Get the AssertableLogger.
    *
    * @return \Drupal\KernelTests\AssertableLogger|null
    */
