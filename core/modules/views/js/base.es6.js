c644c6cba7df4b9cf14f0a997774635b151b2d7f
Andrei Vesterli <andrei.vesterli@indrivo.com>
2022-04-18 23:34:36 +0300

    Make sure that get argument is set.

--- core/modules/views/js/base.es6.js
+++ core/modules/views/js/base.es6.js
@@ -29,7 +29,7 @@
     for (let i = 0; i < pairs.length; i++) {
       pair = pairs[i].split('=');
       // Ignore the 'q' path argument, if present.
-      if (pair[0] !== 'q' && pair[1]) {
+      if (pair[0] !== 'q' && pair[1] !== undefined) {
         args[decodeURIComponent(pair[0].replace(/\+/g, ' '))] =
           decodeURIComponent(pair[1].replace(/\+/g, ' '));
       }
