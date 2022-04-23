0623833317786cca8ba79d0e9ff10fb61befe578
claudiu.cristea <claudiucristea@56348.no-reply.drupal.org>
2022-04-23 10:05:43 +0000

    Fix tests

--- core/tests/Drupal/KernelTests/Core/Entity/EntityBundleLabelTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityBundleLabelTest.php
@@ -29,7 +29,7 @@ class EntityBundleLabelTest extends KernelTestBase {
       'label' => 'Alterable label',
     ]);
     $bundle_entity->save();
-    $this->expectDeprecation('Using hook_entity_bundle_info_alter() to alter the label of bundles stored as config entities is deprecated in drupal:9.2.0 and is not permitted in drupal:10.0.0. Label altered bundles: bundle_with_alterable_label (entity_test_with_bundle). Use different methods to alter the label for bundles stored as config entities. See https://www.drupal.org/node/3186694');
+    $this->expectDeprecation('Using hook_entity_bundle_info_alter() to alter the label of bundles stored as config entities is deprecated in drupal:9.4.0 and is not permitted in drupal:10.0.0. Label altered bundles: bundle_with_alterable_label (entity_test_with_bundle). Use different methods to alter the label for bundles stored as config entities. See https://www.drupal.org/node/3186694');
     $bundle_info = \Drupal::service('entity_type.bundle.info')->getBundleInfo('entity_test_with_bundle')['bundle_with_alterable_label'];
     $this->assertNotSame($bundle_info['label'], $bundle_entity->label());
     $this->assertSame('Alterable label', $bundle_entity->label());
