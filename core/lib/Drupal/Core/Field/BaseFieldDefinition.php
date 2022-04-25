490cb8aea3359df973a9116457646dae35942473
Johnny Wesley da Silva Santos <CIANDT\jwesley@lnb035237cps.ciandt.global>
2022-04-25 14:19:12 -0300

    changed options parameter to be optional with default weight equals to zero

--- core/lib/Drupal/Core/Field/BaseFieldDefinition.php
+++ core/lib/Drupal/Core/Field/BaseFieldDefinition.php
@@ -377,7 +377,7 @@ class BaseFieldDefinition extends ListDataDefinition implements FieldDefinitionI
    * @return static
    *   The object itself for chaining.
    */
-  public function setDisplayOptions($display_context, array $options) {
+  public function setDisplayOptions($display_context, array $options = ['weight' => 0]) {
     $this->definition['display'][$display_context]['options'] = $options;
     return $this;
   }
