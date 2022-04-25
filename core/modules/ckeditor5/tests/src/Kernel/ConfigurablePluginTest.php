21c571688b488a2d2b82cc86114612d82df5020e
HARUMI JANG <harumi.jang@acquia.com>
2022-04-25 15:13:00 -0400

    removed extra white line

--- core/modules/ckeditor5/tests/src/Kernel/ConfigurablePluginTest.php
+++ core/modules/ckeditor5/tests/src/Kernel/ConfigurablePluginTest.php
@@ -50,7 +50,6 @@ class ConfigurablePluginTest extends KernelTestBase {
     $configurable_definitions = array_filter($all_definitions, function (CKEditor5PluginDefinition $definition): bool {
       return $definition->isConfigurable();
     });
-
     $default_plugin_settings = [];
     foreach (array_keys($configurable_definitions) as $plugin_name) {
       $default_plugin_settings[$plugin_name] = $this->manager->getPlugin($plugin_name, NULL)->defaultConfiguration();
