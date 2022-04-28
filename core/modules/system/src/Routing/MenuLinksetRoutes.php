b1e3b6603859fd614e745d35c17ad4596fab563a
Brian Perry <brian.perry@pantheon.io>
2022-04-28 15:30:50 -0700

    Refactor MenuLinksetRoutes to use dependency injection

--- core/modules/system/src/Routing/MenuLinksetRoutes.php
+++ core/modules/system/src/Routing/MenuLinksetRoutes.php
@@ -2,12 +2,41 @@
 
 namespace Drupal\system\Routing;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Route;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
- * Dynamically defines routes for menu linkset endpoints.
+ * Defines a route subscriber to register a url for serving image styles.
  */
-class MenuLinksetRoutes {
+class MenuLinksetRoutes implements ContainerInjectionInterface {
+
+  /**
+   * The system.linkset config object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * Constructs a new MenuLinksetRoutes object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->config = $config_factory->get('system.linkset');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * Returns an array of route objects.
@@ -19,7 +48,7 @@ class MenuLinksetRoutes {
     $routes = [];
 
     // Only enable linkset routes if the related config option is enabled.
-    if (\Drupal::config('system.linkset')->get('enable_endpoint')) {
+    if ($this->config->get('enable_endpoint')) {
       $routes['system.menu.linkset'] = new Route(
         '/system/menu/{menu}/linkset',
         [
