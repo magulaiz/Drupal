6caaf774fd3670b0e905c2835be148f177758cce
Brian Perry <brian.perry@pantheon.io>
2022-04-28 16:23:09 -0700

    Refactor event subscriber to use dependency injection

--- core/modules/system/src/Routing/EventSubscriber.php
+++ core/modules/system/src/Routing/EventSubscriber.php
@@ -6,6 +6,7 @@ namespace Drupal\system\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * Routing event subscriber.
@@ -27,13 +28,23 @@ final class EventSubscriber extends RouteSubscriberBase {
   protected $providerIds;
 
   /**
+   * The system.linkset config object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
    * EventSubscriber constructor.
    *
    * @param string[] $authentication_providers
    *   An array of authentication providers, keyed by ID.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
    */
-  public function __construct(array $authentication_providers) {
+  public function __construct(array $authentication_providers, ConfigFactoryInterface $config_factory) {
     $this->providerIds = array_keys($authentication_providers);
+    $this->config = $config_factory->get('system.linkset');
   }
 
   /**
@@ -49,7 +60,7 @@ final class EventSubscriber extends RouteSubscriberBase {
    *   A collection of routes.
    */
   public function alterRoutes(RouteCollection $collection) {
-    if (\Drupal::config('system.linkset')->get('enable_endpoint')) {
+    if ($this->config->get('enable_endpoint')) {
       $collection->get('system.menu.linkset')->setOption('_auth', $this->providerIds);
     }
   }
