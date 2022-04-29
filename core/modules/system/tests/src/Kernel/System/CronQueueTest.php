bb6940f1fa4e11023e9e0762b2b83b96356c5055
Clayton Liddell <clayton.liddell@civicactions.com>
2022-04-29 14:36:00 -0500

    Add service dependencies

--- core/modules/system/tests/src/Kernel/System/CronQueueTest.php
+++ core/modules/system/tests/src/Kernel/System/CronQueueTest.php
@@ -64,7 +64,7 @@ class CronQueueTest extends KernelTestBase {
 
     $realQueueFactory = $this->container->get('queue');
     $queue_factory = $this->prophesize(get_class($realQueueFactory));
-    $database = new DatabaseQueue('cron_queue_test_database_delay_exception', $this->connection);
+    $database = new DatabaseQueue('cron_queue_test_database_delay_exception', $this->connection, $time);
     $memory = new Memory('cron_queue_test_memory_delay_exception');
     $queue_factory->get('cron_queue_test_database_delay_exception', Argument::cetera())->willReturn($database);
     $queue_factory->get('cron_queue_test_memory_delay_exception', Argument::cetera())->willReturn($memory);
