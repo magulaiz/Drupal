af87133607ebe9552ec7804e4f59b1498aaf43ef
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-04-30 07:19:33 +0100

    Added link plugin derivers.

---
+++ core/lib/Drupal/Core/Entity/Plugin/Derivative/EntityMenuLinkDeriver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\Core\Entity\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides derivative menu links for entity types.
+ *
+ * @see \Drupal\Core\Entity\Menu\BaseEntityLinksProvider
+ * @see plugin_api
+ */
+class EntityMenuLinkDeriver extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs an EntityMenuLinkDeriver instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $plugin_definitions = [];
+
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type) {
+      if (!$entity_type->hasLinkProvider()) {
+        continue;
+      }
+
+      $link_provider_handler = $this->entityTypeManager->getHandler($entity_type->id(), 'link_provider');
+
+      $plugin_definitions += $link_provider_handler->getMenuLinks($base_plugin_definition);
+    }
+
+    return $plugin_definitions;
+  }
+
+}
