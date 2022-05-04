1ad93aa598fc8e68b63bd2578f1551dab80bc074
HARUMI JANG <harumi.jang@acquia.com>
2022-05-04 18:08:20 -0400

    move eslint disable line

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimagecaption.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimagecaption.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 // cSpell:words downcasted
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Plugin } from 'ckeditor5/src/core';
 
 /**
