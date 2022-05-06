4bca22616231eea2f9baa19f0a052c59ced9325f
Brad Jones <brad@bradjonesllc.com>
2022-05-06 09:56:23 -0600

    Update docblocks.

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldType/EntityReferenceItemInterface.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldType/EntityReferenceItemInterface.php
@@ -5,7 +5,7 @@ namespace Drupal\Core\Field\Plugin\Field\FieldType;
 use Drupal\Core\Field\FieldDefinitionInterface;
 
 /**
- * Interface definition for EntityReference items.
+ * Interface definition for field items referencing other entities.
  */
 interface EntityReferenceItemInterface {
 
@@ -17,9 +17,10 @@ interface EntityReferenceItemInterface {
    *   types and bundles.
    *
    * @return array
-   *   An array keyed by entity type IDs where the values are an indexed
-   *   array of bundle IDs or entity type ID if the entity type does not have
-   *   bundles.
+   *   An array of referenceable bundles where the array is keyed by the entity
+   *   type ID, with values an array of bundle names. (It is a single-value
+   *   array with the entity type ID if the entity type does not implement
+   *   bundles.)
    */
   public static function getReferenceableBundles(FieldDefinitionInterface $field_definition);
 
