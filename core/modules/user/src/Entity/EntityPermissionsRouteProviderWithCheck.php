4fd8945d9fbc879e58b7cd02b47a4aadb7521e1d
Benji Fisher <benji@FisherFam.org>
2022-05-06 18:28:15 -0400

    Document the difference between the two route providers

--- core/modules/user/src/Entity/EntityPermissionsRouteProviderWithCheck.php
+++ core/modules/user/src/Entity/EntityPermissionsRouteProviderWithCheck.php
@@ -7,6 +7,13 @@ use Symfony\Component\Routing\Route;
 
 /**
  * Provides routes for the entity permissions form with a custom access check.
+ *
+ * Use this class or EntityPermissionsRouteProvider as a route provider for an
+ * entity type such as BlockContentType. Either one will provide routes for the
+ * entity permissions form. This class provides a custom access check: it denies
+ * access if there are no entity-specific permissions. If you know that each
+ * entity has permissions, or if the check is too expensive, then use
+ * EntityPermissionsRouteProvider instead of this class.
  */
 class EntityPermissionsRouteProviderWithCheck extends EntityPermissionsRouteProvider {
 
