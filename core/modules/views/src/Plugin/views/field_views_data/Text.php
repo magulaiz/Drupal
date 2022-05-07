8a39e6857970907591c8763f3a38ca763ad198d6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-05-07 08:50:42 +0100

    Changed text plugin to handle other text type fields as well.

--- core/modules/views/src/Plugin/views/field_views_data/Text.php
+++ core/modules/views/src/Plugin/views/field_views_data/Text.php
@@ -2,16 +2,40 @@
 
 namespace Drupal\views\Plugin\views\field_views_data;
 
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+
 /**
- * Defines views data for fields of type "text".
+ * Defines views data for text fields.
  *
  * @ViewsFieldData(
  *   id = "text",
  *   field_types = {
  *     "text",
+ *     "text_long",
+ *     "text_with_summary",
+ *   },
+ *   argument = {
+ *     "id" = "string",
+ *   },
+ *   filter = {
+ *     "id" = "string",
  *   }
  * )
  */
-class Text extends TextLong {
+class Text extends FieldViewsDataPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData(FieldStorageDefinitionInterface $field_storage, $column_name) {
+    $views_field = parent::getViewsData($field_storage, $column_name);
+
+    // Connect the text field to its formatter.
+    if ($column_name == 'value') {
+      $views_field['field']['format'] = $field_storage->getName() . '__format';
+    }
+
+    return $views_field;
+  }
 
 }
