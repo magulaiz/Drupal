b5baee3369ed76581fd5994a1fcbf77dc415ed0c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-05-08 07:05:09 +0100

    Added type hints to protected methods on link providers.

--- core/lib/Drupal/Core/Entity/Menu/DefaultConfigEntityLinksProvider.php
+++ core/lib/Drupal/Core/Entity/Menu/DefaultConfigEntityLinksProvider.php
@@ -12,7 +12,7 @@ class DefaultConfigEntityLinksProvider extends BaseEntityLinksProvider implement
   /**
    * {@inheritdoc}
    */
-  protected function getCollectionMenuLink($base_plugin_definition) {
+  protected function getCollectionMenuLink(array $base_plugin_definition) {
     if ($this->routeExists($this->getRouteName('collection'))) {
       // Config entity types that have a collection route get a menu link that
       // is placed in the 'admin > structure' section.
@@ -32,7 +32,7 @@ class DefaultConfigEntityLinksProvider extends BaseEntityLinksProvider implement
   /**
    * {@inheritdoc}
    */
-  protected function getEditFormTaskLink($base_plugin_definition) {
+  protected function getEditFormTaskLink(array $base_plugin_definition) {
     // Config entities have just one task here, but for entity types which are
     // bundles (such as node_type), Field UI module hangs its own tabs for the
     // associated content entity's fields off the bundle entity's edit form
