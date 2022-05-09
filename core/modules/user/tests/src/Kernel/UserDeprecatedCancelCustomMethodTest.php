eef4b83e0147b3539b27ff949193dc5eb21cf029
Claudiu Cristea <clau.cristea@gmail.com>
2022-05-09 15:39:16 +0200

    Properly deprecate procedural code

--- core/modules/user/tests/src/Kernel/UserDeprecatedCancelCustomMethodTest.php
+++ core/modules/user/tests/src/Kernel/UserDeprecatedCancelCustomMethodTest.php
@@ -7,6 +7,7 @@ use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\user\Controller\UserController;
 use Drupal\user\Form\UserCancelForm;
 use Drupal\user\Form\UserMultipleCancelConfirm;
+use Drupal\user\UserInterface;
 
 /**
  * Tests deprecation of procedural custom user account cancelling method.
@@ -45,6 +46,22 @@ class UserDeprecatedCancelCustomMethodTest extends KernelTestBase {
     $account = $this->createUser();
     $this->expectDeprecation('The deprecated hook hook_user_cancel() is implemented in these functions: user_cancel_test_user_cancel(). The hook is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. In order to act on user account cancellation provide an event subscriber that listens to the \Drupal\user\Event\AccountCancelEvent event. The event subscriber can be defined with a priority higher than the core subscribers in order to cancel them by using AccountCancelEvent::stopPropagation(). See https://www.drupal.org/node/3279455');
     $this->container->get('user.account_cancellation')->cancel($account->id(), 'user_cancel_test');
+  }
+
+  /**
+   * Tests deprecation of procedural code.
+   *
+   * @covers \user_cancel
+   * @covers \_user_cancel
+   * @covers \_user_cancel_session_regenerate
+   */
+  public function testProceduralCodeDeprecations(): void {
+    $this->expectDeprecation("user_cancel is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. Use the method ::cancel() from the 'user.account_cancellation' service instead. See https://www.drupal.org/node/3279455");
+    user_cancel([], 123, 'abc');
+    $this->expectDeprecation("_user_cancel is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. Use the method ::doCancelAccount() from the 'user.account_cancellation' service instead. See https://www.drupal.org/node/3279455");
+    _user_cancel([], $this->prophesize(UserInterface::class)->reveal(), 'abc');
+    $this->expectDeprecation("_user_cancel_session_regenerate is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. Use the method ::regenerateSession() from the 'user.account_cancellation' service instead. See https://www.drupal.org/node/3279455");
+    _user_cancel_session_regenerate();
   }
 
   /**
