154979b7150ceafe22a2c11f621bfc3bc2155bf6
Phéna Proxima <adam@phenaproxima.net>
2022-05-10 14:48:41 -0400

    Remove unnecessary check from table assert

--- core/modules/update/tests/src/Functional/UpdateSemverTestBase.php
+++ core/modules/update/tests/src/Functional/UpdateSemverTestBase.php
@@ -156,7 +156,6 @@ abstract class UpdateSemverTestBase extends UpdateTestBase {
           $this->standardTests();
           $this->assertUpdateTableTextNotContains('Security update required!');
           $this->assertUpdateTableElementContains(Link::fromTextAndUrl('9.0.0', Url::fromUri("http://example.com/{$this->updateProject}-9-0-0-release"))->toString());
-          $this->assertUpdateTableElementNotContains("http://example.com/{$this->updateProject}-9-0-0.tar.gz");
           $this->assertUpdateTableElementContains(Link::fromTextAndUrl('Release notes', Url::fromUri("http://example.com/{$this->updateProject}-9-0-0-release"))->toString());
           $this->assertUpdateTableTextNotContains('Up to date');
           $this->assertUpdateTableTextContains('Not supported!');
