c9c8381413cf438e74a221fcbafa9b7fefe428e2
Andy Blum <andy.blum.01@gmail.com>
2022-05-15 18:16:02 -0400

    3085219-58: compile & commit assets

--- core/themes/claro/js/mobile.install.js
+++ core/themes/claro/js/mobile.install.js
@@ -27,7 +27,7 @@
       var header = document.querySelector('header[role="banner"]');
       var stepIndicator = document.createElement('div');
       stepIndicator.className = 'step-indicator';
-      stepIndicator.innerHTML = "\n        <span class=\"visually-hidden\">Install Step ".concat(findActiveStep(steps), " of ").concat(steps.length, "</span>\n        <span aria-hidden>").concat(findActiveStep(steps), "/").concat(steps.length, "</span>\n      ");
+      stepIndicator.innerHTML = "\n        <span class=\"visually-hidden\">Install Step ".concat(findActiveStep(steps), " of ").concat(steps.length, "</span>\n        <span aria-hidden=\"true\">").concat(findActiveStep(steps), "/").concat(steps.length, "</span>\n      ");
       header.appendChild(stepIndicator);
     }
   }
