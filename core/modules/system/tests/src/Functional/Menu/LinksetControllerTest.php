9f82ed48292665e6c6cfcc9c0eb0a967d7232b25
Ravi Shankar <ravi.s@opensenselabs.com>
2022-05-18 15:24:55 +0530

    Removed content entity save.

--- core/modules/system/tests/src/Functional/Menu/LinksetControllerTest.php
+++ core/modules/system/tests/src/Functional/Menu/LinksetControllerTest.php
@@ -439,7 +439,6 @@ final class LinksetControllerTest extends BrowserTestBase {
     }
     $link_content = MenuLinkContent::create($values);
     assert($link_content instanceof MenuLinkContentInterface);
-    $link_content->save();
     return $link_content;
   }
 
