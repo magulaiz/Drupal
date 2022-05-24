1b209497948eeb979a57cf9271fcea8471102bf5
Wim Leers <wim.leers@acquia.com>
2022-05-24 17:29:07 +0200

    Update SmartDefaultSettings::addToolbarItemsToMatchHtmlElementsInFormat() to get and select candidates in two phases.

--- core/modules/ckeditor5/src/SmartDefaultSettings.php
+++ core/modules/ckeditor5/src/SmartDefaultSettings.php
@@ -656,11 +656,36 @@ final class SmartDefaultSettings {
     $enabled_plugins = array_keys($enabled_definitions);
     $provided_elements = $this->pluginManager->getProvidedElements($enabled_plugins);
     $provided = new HTMLRestrictions($provided_elements);
-    $still_needed = HTMLRestrictions::fromTextFormat($format)->diff($provided);
+    $needed = HTMLRestrictions::fromTextFormat($format);
+    $still_needed = $needed->diff($provided);
+
+    // Plugin supporting <tag attr> cannot create the tag, for that they need to
+    // support plain <tag> too. Break down what is provided and is needed based
+    // on this.
+    // @see \Drupal\ckeditor5\Plugin\CKEditor5PluginDefinition::getCreatableElements()
+    // TRICKY: the HTMLRestrictions value object can only convey complete
+    // restrictions: merging <foo> and <foo bar> results in just <foo bar>. The
+    // list of already provided plain tags must hence be constructed separately.
+    $provided_plain_tags = new HTMLRestrictions(
+      $this->pluginManager->getProvidedElements($enabled_plugins, NULL, FALSE, TRUE)
+    );
+    $still_needed_plain_tags = $needed->extractPlainTagsSubset()->diff($provided_plain_tags);
+    $still_needed_attributes = $still_needed->diff($still_needed_plain_tags);
+    // Merging $still_needed_plain_tags with $still_needed_attributes must
+    // always equal $still_needed.
+    assert($still_needed_plain_tags->merge($still_needed_attributes)->diff($still_needed)->allowsNothing());
 
     if (!$still_needed->allowsNothing()) {
-      $plugin_candidates = self::getCandidates($provided, $still_needed, $disabled_definitions);
-      $selected_plugins = self::selectCandidate($plugin_candidates, $still_needed, array_keys($provided->getAllowedElements()));
+      // Select plugins for supporting the still needed plain tags.
+      $plugin_candidates_plain_tags = self::getCandidates($provided_plain_tags, $still_needed_plain_tags, $disabled_definitions);
+      $selected_plugins_plain_tags = self::selectCandidate($plugin_candidates_plain_tags, $still_needed_plain_tags, array_keys($provided_plain_tags->getAllowedElements()));
+
+      // Select plugins for supporting the still needed attributes.
+      $plugin_candidates_attributes = self::getCandidates($provided, $still_needed_attributes, $disabled_definitions);
+      $selected_plugins_attributes = self::selectCandidate($plugin_candidates_attributes, $still_needed, array_keys($provided->getAllowedElements()));
+
+      // Combine the selection.
+      $selected_plugins = array_merge_recursive($selected_plugins_plain_tags, $selected_plugins_attributes);
 
       // If additional plugins need to be enable to support attribute config,
       // loop through the list to enable the plugins and build a UI message that
