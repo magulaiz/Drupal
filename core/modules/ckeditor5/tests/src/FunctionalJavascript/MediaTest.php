bbcb1f4e902e80253c7a7349ab971471238aebf3
Wim Leers <wim.leers@acquia.com>
2022-05-31 16:42:36 +0200

    MediaTest::testDrupalMediaStyleWithClass(): not all tags are creatable with current fixture.

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaTest.php
@@ -1168,13 +1168,22 @@ class MediaTest extends WebDriverTestBase {
     $editor->setSettings([
       'toolbar' => [
         'items' => [
+          'heading',
           'sourceEditing',
           'simpleBox',
         ],
       ],
       'plugins' => [
+        'ckeditor5_heading' => [
+          'enabled_headings' => [
+            'heading1',
+          ],
+        ],
         'ckeditor5_sourceEditing' => [
-          'allowed_tags' => [],
+          'allowed_tags' => [
+            '<div>',
+            '<section>',
+          ],
         ],
         'media_media' => [
           'allow_view_mode_override' => TRUE,
