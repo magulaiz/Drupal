409d1294f1ef822c15ecee0a5f008f676f72a53d
Claudiu Cristea <clau.cristea@gmail.com>
2022-06-01 13:32:29 +0300

    Typehinting & CS

--- core/modules/views/tests/modules/action_bulk_test/src/Form/TestActionConfirmForm.php
+++ core/modules/views/tests/modules/action_bulk_test/src/Form/TestActionConfirmForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\action_bulk_test\Form;
 
+use Drupal\Component\Render\MarkupInterface;
 use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
@@ -14,27 +15,28 @@ class TestActionConfirmForm extends ConfirmFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId(): string {
     return 'test_action_confirm_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getCancelUrl() {
+  public function getCancelUrl(): Url {
     return Url::fromRoute('<front>');
   }
 
   /**
-   * @inheritdoc
+   * {@inheritdoc}
    */
-  public function getQuestion() {
+  public function getQuestion(): MarkupInterface {
     return $this->t('Do you agree?');
   }
 
   /**
-   * @inheritdoc
+   * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {}
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+  }
 
 }
