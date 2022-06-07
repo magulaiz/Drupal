e6866996ba24ad10e7af93e83626c7cdda632239
Andy Blum <andy.blum.01@gmail.com>
2022-06-07 08:50:14 -0400

    3085219-82: apply changes to Drupal 10

--- core/themes/claro/js/mobile.install.js
+++ core/themes/claro/js/mobile.install.js
@@ -27,7 +27,10 @@
       const header = document.querySelector('header[role="banner"]');
       const stepIndicator = document.createElement('div');
       stepIndicator.className = 'step-indicator';
-      stepIndicator.innerHTML = `${findActiveStep(steps)}/${steps.length}`;
+      stepIndicator.innerHTML = `
+        <span class="visually-hidden">Install Step ${findActiveStep(steps)} of ${steps.length}</span>
+        <span aria-hidden="true">${findActiveStep(steps)}/${steps.length}</span>
+      `;
       header.appendChild(stepIndicator);
     }
   }
