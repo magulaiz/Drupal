d30a4ec40fa5bc2424732ba415809f6b660226d2
Prabhat Burnwal <prabhat.burnwal@innoraft.com>
2022-06-13 14:20:31 +0530

    Added condition for cache bodting if css/js file are aggregated.

--- core/lib/Drupal/Core/Asset/JsCollectionRenderer.php
+++ core/lib/Drupal/Core/Asset/JsCollectionRenderer.php
@@ -4,6 +4,7 @@ namespace Drupal\Core\Asset;
 
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\State\StateInterface;
+use Drupal\Core\Site\Settings;
 
 /**
  * Renders JavaScript assets.
@@ -75,9 +76,9 @@ class JsCollectionRenderer implements AssetCollectionRendererInterface {
           $query_string = $js_asset['version'] == -1 ? $default_query_string : 'v=' . $js_asset['version'];
           $query_string_separator = (strpos($js_asset['data'], '?') !== FALSE) ? '&' : '?';
           $element['#attributes']['src'] = file_url_transform_relative(file_create_url($js_asset['data']));
-          // Only add the cache-busting query string if this isn't an aggregate
-          // file.
-          if (!isset($js_asset['preprocessed'])) {
+          // Add the cache-busting query string if this is an aggregate file
+          // and settings enabled or if this isn't an aggregate file.
+          if (Settings::get('css_js_query_string_for_compressed', FALSE) || !isset($js_asset['preprocessed'])) {
             $element['#attributes']['src'] .= $query_string_separator . ($js_asset['cache'] ? $query_string : REQUEST_TIME);
           }
           break;
