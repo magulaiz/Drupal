3a66efc87bd1e8a207d5ee0cca74b6640af310d7
Mike Herchel <mike@herchel.com>
2022-06-13 09:07:17 -0400

    Revert "Merge branch '9.4.x' into redesign-install-update-maintenance-pages"
    This reverts commit 9d20ead1a28d70023fe5b42d085ad63986390dc1, reversing
    changes made to 1a2873e81f874a69576d890bc5783c5fdc108372.

--- core/modules/system/tests/src/Functional/Theme/ThemeUpdateTest.php
+++ core/modules/system/tests/src/Functional/Theme/ThemeUpdateTest.php
@@ -36,10 +36,6 @@ class ThemeUpdateTest extends BrowserTestBase {
     $this->assertFalse(\Drupal::moduleHandler()->moduleExists('test_another_module_required_by_theme'));
     $this->runUpdates();
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('test_another_module_required_by_theme'));
-
-    $this->assertSession()->addressEquals('update.php/results');
-    $this->assertSession()->responseContains('test_theme_depending_on_modules theme');
-    $this->assertSession()->responseContains('Post update message from theme post update function');
   }
 
   /**
@@ -47,7 +43,7 @@ class ThemeUpdateTest extends BrowserTestBase {
    */
   protected function doSelectionTest() {
     // Ensure that the theme's post update appears as expected.
-    $this->assertSession()->responseContains('test_theme_depending_on_modules theme');
+    $this->assertSession()->responseContains('test_theme_depending_on_modules module');
     $this->assertSession()->responseContains('Install a dependent module.');
   }
 
