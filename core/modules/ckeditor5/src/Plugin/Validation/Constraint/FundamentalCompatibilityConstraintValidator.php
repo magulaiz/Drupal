e3a2aa4db73ef8cca97efdb3c95b1853afabf384
Wim Leers <wim.leers@acquia.com>
2022-06-17 18:14:40 +0200

    Implement what @lauriii and I discussed.

--- core/modules/ckeditor5/src/Plugin/Validation/Constraint/FundamentalCompatibilityConstraintValidator.php
+++ core/modules/ckeditor5/src/Plugin/Validation/Constraint/FundamentalCompatibilityConstraintValidator.php
@@ -6,6 +6,7 @@ namespace Drupal\ckeditor5\Plugin\Validation\Constraint;
 
 use Drupal\ckeditor5\HTMLRestrictions;
 use Drupal\ckeditor5\Plugin\CKEditor5PluginDefinition;
+use Drupal\ckeditor5\Plugin\CKEditor5PluginElementsSubsetInterface;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\editor\EditorInterface;
 use Drupal\filter\FilterFormatInterface;
@@ -236,9 +237,14 @@ class FundamentalCompatibilityConstraintValidator extends ConstraintValidator im
           ->setParameter('%plugin', $plugin_definition->label())
           ->setParameter('@attributes_on_tag', implode(', ', $attributes_on_tag->toCKEditor5ElementsArray()));
 
+        // If this plugin has a configurable subset, associate the violation
+        // with the property path pointing to this plugin's settings form.
+        if (is_a($plugin_definition->getClass(), CKEditor5PluginElementsSubsetInterface::class, TRUE)) {
+          $violation->atPath(sprintf('settings.plugins.%s', $plugin_definition->id()));
+        }
         // If this plugin is associated with a toolbar item, associate the
         // violation with the property path pointing to the active toolbar item.
-        if ($plugin_definition->hasToolbarItems()) {
+        elseif ($plugin_definition->hasToolbarItems()) {
           $toolbar_items = $plugin_definition->getToolbarItems();
           $active_toolbar_items = array_intersect(
             $text_editor->getSettings()['toolbar']['items'],
