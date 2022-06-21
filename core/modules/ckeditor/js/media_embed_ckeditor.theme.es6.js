9bab8d69084e4647e419acd69e3d092bbc50e939
Wim Leers <wim.leers@acquia.com>
2022-06-21 13:57:04 +0200

    Split up media_embed_ckeditor.theme.js in two parts: one that is text editor-agnostic, one that is CKEditor 4-specific.

--- core/modules/ckeditor/js/media_embed_ckeditor.theme.es6.js
+++ core/modules/ckeditor/js/media_embed_ckeditor.theme.es6.js
@@ -5,22 +5,6 @@
 
 ((Drupal) => {
   /**
-   * Themes the error displayed when the media embed preview fails.
-   *
-   * @param {string} error
-   *   The error message to display
-   *
-   * @return {string}
-   *   A string representing a DOM fragment.
-   *
-   * @see media-embed-error.html.twig
-   */
-  Drupal.theme.mediaEmbedPreviewError = () =>
-    `<div class="media-embed-error media-embed-error--preview-error">${Drupal.t(
-      'An error occurred while trying to preview the media. Please save your work and reload this page.',
-    )}</div>`;
-
-  /**
    * Themes the edit button for a media embed.
    *
    * @return {string}
--- core/modules/ckeditor/js/media_embed_ckeditor.theme.js
+++ core/modules/ckeditor/js/media_embed_ckeditor.theme.js
@@ -6,10 +6,6 @@
 **/
 
 (function (Drupal) {
-  Drupal.theme.mediaEmbedPreviewError = function () {
-    return "<div class=\"media-embed-error media-embed-error--preview-error\">".concat(Drupal.t('An error occurred while trying to preview the media. Please save your work and reload this page.'), "</div>");
-  };
-
   Drupal.theme.mediaEmbedEditButton = function () {
     return "<button class=\"media-library-item__edit\">".concat(Drupal.t('Edit media'), "</button>");
   };
