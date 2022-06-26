07fcdb5920d3a99c2f1fc1505b7c292ab5960a0c
Claudiu Cristea <clau.cristea@gmail.com>
2022-06-26 12:34:21 +0300

    Move to Drupal 10 leftovers

--- core/modules/user/user.api.php
+++ core/modules/user/user.api.php
@@ -36,7 +36,7 @@ use Drupal\user\UserInterface;
  * @param string $method
  *   The account cancellation method.
  *
- * @deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. In order to
+ * @deprecated in drupal:10.0.0 and is removed from drupal:11.0.0. In order to
  *   act on user account cancellation provide an event subscriber that listens
  *   to the \Drupal\user\Event\AccountCancelEvent event. The event subscriber
  *   can be defined with a priority higher than the core subscribers in order to
