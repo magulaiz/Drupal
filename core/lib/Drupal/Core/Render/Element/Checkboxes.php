0610a0c9259924ffbdeefbab8893ddda81e5b9c7
Murz <murz@157092.no-reply.drupal.org>
2022-06-30 11:07:20 +0000

    Cast all integer keys to strings

--- core/lib/Drupal/Core/Render/Element/Checkboxes.php
+++ core/lib/Drupal/Core/Render/Element/Checkboxes.php
@@ -69,10 +69,10 @@ class Checkboxes extends FormElement {
       foreach ($element['#options'] as $key => $choice) {
         // Integer 0 is not a valid #return_value, so use '0' instead.
         // @see \Drupal\Core\Render\Element\Checkbox::valueCallback().
-        // @todo For Drupal 8, cast all integer keys to strings for consistency
+        // Cast all integer keys to strings for consistency
         //   with \Drupal\Core\Render\Element\Radios::processRadios().
-        if ($key === 0) {
-          $key = '0';
+        if (is_int($key)) {
+          $key = strval($key);
         }
         // Maintain order of options as defined in #options, in case the element
         // defines custom option sub-elements, but does not define all option
