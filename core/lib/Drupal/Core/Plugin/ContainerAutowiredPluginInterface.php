0900a4ff67ad985d2d299b44e869e4c0313ea36e
Dave Long <dave@longwaveconsulting.com>
2022-07-05 20:28:33 +0100

    Prototype.

---
+++ core/lib/Drupal/Core/Plugin/ContainerAutowiredPluginInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Drupal\Core\Plugin;
+
+/**
+ * Defines an interface for autowiring plugin dependencies from the container.
+ */
+interface ContainerAutowiredPluginInterface {}
--- core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
+++ core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
@@ -3,9 +3,11 @@
 namespace Drupal\Core\Plugin\Factory;
 
 use Drupal\Component\Plugin\Factory\DefaultFactory;
+use Drupal\Core\Plugin\Attribute\Service;
+use Drupal\Core\Plugin\ContainerAutowiredPluginInterface;
 
 /**
- * Plugin factory which passes a container to a create method.
+ * Plugin factory that creates plugin instances with dependent services.
  */
 class ContainerFactory extends DefaultFactory {
 
@@ -15,6 +17,20 @@ class ContainerFactory extends DefaultFactory {
   public function createInstance($plugin_id, array $configuration = []) {
     $plugin_definition = $this->discovery->getDefinition($plugin_id);
     $plugin_class = static::getPluginClass($plugin_id, $plugin_definition, $this->interface);
+
+    // If the plugin provides service attributes, construct it from those.
+    if (is_subclass_of($plugin_class, ContainerAutowiredPluginInterface::class)) {
+      $constructor = new \ReflectionMethod($plugin_class, '__construct');
+      $args = [$configuration, $plugin_id, $plugin_definition];
+      foreach ($constructor->getParameters() as $pos => $parameter) {
+        foreach ($parameter->getAttributes() as $attribute) {
+          if ($attribute->getName() === Service::class) {
+            $args[$pos] = \Drupal::service($attribute->newInstance()->service);
+          }
+        }
+      }
+      return new $plugin_class(...$args);
+    }
 
     // If the plugin provides a factory method, pass the container to it.
     if (is_subclass_of($plugin_class, 'Drupal\Core\Plugin\ContainerFactoryPluginInterface')) {
