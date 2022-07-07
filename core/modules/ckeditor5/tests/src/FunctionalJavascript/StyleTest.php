b59ab33570f3c522c516fa3dd0c4fffc11b6163d
Théodore BIADALA <theodore@biadala.net>
2022-07-07 11:56:03 +0200

    cspell

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/StyleTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/StyleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+// cspell:ignore sourceediting
+
 namespace Drupal\Tests\ckeditor5\FunctionalJavascript;
 
 use Drupal\ckeditor5\Plugin\Editor\CKEditor5;
@@ -123,7 +125,7 @@ JS;
     $page->clickLink('Source editing');
     $this->assertNotNull($assert_session->waitForElementVisible('css', '[data-drupal-selector="edit-editor-settings-plugins-ckeditor5-sourceediting-allowed-tags"]'));
     $javascript = <<<JS
-      const allowedTags = document.querySelector('[data-drupal-selector="edit-editor-settings-plugins-ckeditor5-sourceediting-allowed-tags"]');
+      const allowedTags = document.querySelector('[data-drupal-selector="edit-editor-settings-plugins-ckeditor5-sourceEditing-allowed-tags"]');
       allowedTags.value = '<aside class>';
       allowedTags.dispatchEvent(new Event('input'));
 JS;
