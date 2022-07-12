d2088f9a8de6d9fd45d1bbdf1cc016955bb71bb5
Ravi Shankar <ravi.s@opensenselabs.com>
2022-07-12 10:53:33 +0530

    Added typehint to method.

--- core/tests/Drupal/KernelTests/Core/Database/ConnectionTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/ConnectionTest.php
@@ -212,7 +212,7 @@ class ConnectionTest extends DatabaseTestBase {
    *
    * @group legacy
    */
-  public function testDeprecatedTablePrefix() {
+  public function testDeprecatedTablePrefix(): void {
     $this->expectDeprecation('Drupal\Core\Database\Connection::tablePrefix() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Instead, you should just use Connection::getPrefix(). See https://www.drupal.org/node/3260849');
     $this->assertIsString($this->connection->tablePrefix());
   }
