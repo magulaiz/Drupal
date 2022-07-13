5ad25e80a45644a8c6fdb6b31c7e46eec3ff5e47
claudiu.cristea <claudiucristea@56348.no-reply.drupal.org>
2022-07-13 07:56:51 +0000

    Fix @file description. Remove redundant docs.

--- core/includes/dev_class_aliases.php
+++ core/includes/dev_class_aliases.php
@@ -2,11 +2,7 @@
 
 /**
  * @file
- * Development class aliases.
+ * Aliasing classes when dev dependencies are installed.
  */
 
-// Provides a workaround for a bug in BrowserKitDriver that wrongly considers
-// as text of the page, pieces of texts inside the <head> section.
-// @see https://github.com/minkphp/MinkBrowserKitDriver/issues/153
-// @see https://www.drupal.org/project/drupal/issues/3175718
 class_alias('\Drupal\Tests\DocumentElement', '\Behat\Mink\Element\DocumentElement', TRUE);
