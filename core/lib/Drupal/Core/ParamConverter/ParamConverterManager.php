a538ab84c3ed72edff3e1e0749075f13af4226b6
Dezső BICZÓ <dezso.biczo@pronovix.com>
2022-07-22 15:43:59 +0200

    Add static cache to ParamConverterManager

--- core/lib/Drupal/Core/ParamConverter/ParamConverterManager.php
+++ core/lib/Drupal/Core/ParamConverter/ParamConverterManager.php
@@ -21,6 +21,13 @@ class ParamConverterManager implements ParamConverterManagerInterface {
   protected $converters = [];
 
   /**
+   * Stores already upcasted parameters in the current request.
+   *
+   * @var array
+   */
+  protected $cached = [];
+
+  /**
    * {@inheritdoc}
    */
   public function addConverter(ParamConverterInterface $param_converter, $id) {
@@ -97,15 +104,59 @@ class ParamConverterManager implements ParamConverterManagerInterface {
       // If a converter returns NULL it means that the parameter could not be
       // converted.
       $value = $defaults[$name];
-      $defaults[$name] = $this->getConverter($definition['converter'])->convert($value, $definition, $name, $defaults);
-      if (!isset($defaults[$name])) {
-        $message = 'The "%s" parameter was not converted for the path "%s" (route name: "%s")';
-        $route_name = $defaults[RouteObjectInterface::ROUTE_NAME];
-        throw new ParamNotConvertedException(sprintf($message, $name, $route->getPath(), $route_name), 0, NULL, $route_name, [$name => $value]);
+
+      $cache_id = $this->generateParameterCacheId($name, $value, $definition, $defaults);
+      if (array_key_exists($cache_id, $this->cached)) {
+        $defaults[$name] = $this->cached[$cache_id];
       }
+      else {
+        $defaults[$name] = $this->getConverter($definition['converter'])->convert($value, $definition, $name, $defaults);
+        if (isset($defaults[$name])) {
+          $this->cached[$cache_id] = $defaults[$name];
+        }
+        else {
+          $message = 'The "%s" parameter was not converted for the path "%s" (route name: "%s")';
+          $route_name = $defaults[RouteObjectInterface::ROUTE_NAME];
+          throw new ParamNotConvertedException(sprintf($message, $name, $route->getPath(), $route_name), 0, NULL, $route_name, [$name => $value]);
+        }
+      }
+
     }
 
     return $defaults;
+  }
+
+  /**
+   * Generates a unique cache id for the given parameter upcast.
+   *
+   * @param string $name
+   *   The name of the parameter.
+   * @param mixed $value
+   *   The raw value.
+   * @param mixed $definition
+   *   The parameter definition provided in the route options.
+   * @param array $defaults
+   *   The route defaults array.
+   *
+   * @return string
+   *   The unique cache id.
+   *
+   * @throws \JsonException
+   */
+  private function generateParameterCacheId(string $name, $value, $definition, array $defaults): string {
+    $cache_id_parts = [];
+    $cache_id_parts[] = $name . '-' . (string) $value;
+    $cache_id_parts[] = json_encode($definition, JSON_THROW_ON_ERROR);
+    $cleaned_defaults = $defaults;
+    // Deduplicate, remove the parameter in question, it is already part of
+    // the cache id.
+    unset($cleaned_defaults[$name]);
+    // Remove internal properties.
+    $cleaned_defaults = array_filter($cleaned_defaults, static function (string $key): bool {
+      return strpos($key, '_') !== 0;
+    }, ARRAY_FILTER_USE_KEY);
+    $cache_id_parts[] = json_encode($cleaned_defaults, JSON_THROW_ON_ERROR);
+    return md5(implode('.', $cache_id_parts));
   }
 
 }
