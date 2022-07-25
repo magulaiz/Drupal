baf38e912bea0df32952e89f8123cce94da8c26e
immaculatexavier <immaculate.x@3591395.no-reply.drupal.org>
2022-07-25 16:16:12 +0000

    Update core/modules/views/src/Plugin/views/cache/CachePluginBase.php

--- core/modules/views/src/Plugin/views/cache/CachePluginBase.php
+++ core/modules/views/src/Plugin/views/cache/CachePluginBase.php
@@ -201,7 +201,7 @@ abstract class CachePluginBase extends PluginBase {
       foreach (['query', 'count_query'] as $index) {
         // If the default query back-end is used generate SQL query strings from
         // the query objects.
-        if ($build_info[$index] instanceof Select) {
+        if ($build_info[$index] instanceof SelectInterface) {
           $query = clone $build_info[$index];
           $query->preExecute();
           $build_info[$index] = [
