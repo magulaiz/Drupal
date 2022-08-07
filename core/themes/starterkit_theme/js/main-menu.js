bfaa84f1ebab883858baed6e870fcf7277b9791f
Mike Herchel <mike@herchel.com>
2022-08-07 10:19:12 -0400

    WIP

--- core/themes/starterkit_theme/js/main-menu.js
+++ core/themes/starterkit_theme/js/main-menu.js
@@ -6,7 +6,7 @@
 ((Drupal) => {
   const mobileNavigationButton = document.querySelector('[data-drupal-selector="main-nav__mobile-button"]');
   function isDesktopNav() {
-    return mobileNavigationButton.clientHeight > 0;
+    return mobileNavigationButton.clientHeight === 0;
   }
 
   const buttonSelector = '[data-drupal-selector="main-nav-submenu-toggle-button"], button[data-drupal-selector="main-nav-menu-link-has-children"]';
@@ -64,8 +64,8 @@
     if (!isDesktopNav()) return;
 
     setTimeout(() => {
-      const menuParentItem = e.target.closest('[data-drupal-selector="primary-nav-menu-item-has-children"]');
-      if (!menuParentItem.contains(document.activeElement)) {
+      const menuParentItem = e.target.closest('[data-drupal-selector="main-nav-menu-item-has-children"]');
+      if (!menuParentItem?.contains(document.activeElement)) {
         toggleSubNav(menuParentItem, false);
       }
     }, 200);
