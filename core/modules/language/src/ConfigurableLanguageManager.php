fcb70853aabf519235fdeddef510a744f3d6fb73
Kobe Wright <info@wrightko.be>
2022-08-10 13:22:35 +0200

    Issue #2410579: Allows setting the current language programmatically.

--- core/modules/language/src/ConfigurableLanguageManager.php
+++ core/modules/language/src/ConfigurableLanguageManager.php
@@ -238,6 +238,16 @@ class ConfigurableLanguageManager extends LanguageManager implements Configurabl
   /**
    * {@inheritdoc}
    */
+  public function setCurrentLanguage(LanguageInterface $language, $type = LanguageInterface::TYPE_INTERFACE) {
+    $current_language = $this->getCurrentLanguage();
+    $this->negotiatedLanguages[$type] = $language;
+
+    return $current_language;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function reset($type = NULL) {
     if (!isset($type)) {
       $this->initialized = FALSE;
