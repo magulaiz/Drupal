0befe032ea898eaa1378226be717a4230c079c1f
Alex Pott <alex.a.pott@googlemail.com>
2022-08-23 03:53:38 -0700

    @joachim and @quietone review

--- core/tests/Drupal/Tests/Core/Entity/EntityFieldManagerTest.php
+++ core/tests/Drupal/Tests/Core/Entity/EntityFieldManagerTest.php
@@ -11,13 +11,13 @@ use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
-use Drupal\Core\Config\Entity\ConfigEntityStorageInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\ContentEntityTypeInterface;
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityFieldManager;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
@@ -275,20 +275,20 @@ class EntityFieldManagerTest extends UnitTestCase {
     EntityTypeManagerTestEntity::$bundleFieldDefinitions = $bundle_fields;
     EntityTypeManagerTestEntityBundle::$bundleClassFieldDefinitions = $bundle_class_fields;
 
-    // Test that only base fields are retrieved
+    // Test that only base fields are retrieved.
     $expected = ['id' => $field_definition];
     $this->assertSame($expected, $this->entityFieldManager->getFieldDefinitions('test_entity_type', 'some_other_bundle'));
 
-    // Test that base fields, and bundle fields from the default Entity
-    // class are retrieved
+    // Test that base fields and bundle fields from the default entity class are
+    // retrieved.
     $expected = [
       'id' => $field_definition,
       'id_bundle' => $bundle_fields['the_entity_id']['test_entity_bundle']['id_bundle'],
     ];
     $this->assertSame($expected, $this->entityFieldManager->getFieldDefinitions('test_entity_type', 'test_entity_bundle'));
 
-    // Test that base fields, and bundle fields from the Bundle Class
-    // and default Entity class are retrieved
+    // Test that base fields and bundle fields from the bundle class and
+    // entity class are retrieved
     $expected = [
       'id' => $field_definition,
       'some_extra_field' => $bundle_fields['the_entity_id']['test_entity_bundle_class']['some_extra_field'],
@@ -621,7 +621,7 @@ class EntityFieldManagerTest extends UnitTestCase {
     $this->entityType = $this->prophesize(EntityTypeInterface::class);
     $this->setUpEntityTypeDefinitions(['test_entity_type' => $this->entityType, 'base_field_override' => $override_entity_type]);
 
-    $storage = $this->prophesize(ConfigEntityStorageInterface::class);
+    $storage = $this->prophesize(EntityStorageInterface::class);
     $storage->loadMultiple(Argument::type('array'))->willReturn([]);
 
     // By default, make the storage entity class lookup return the
