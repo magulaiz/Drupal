3c83116cd0675d2c2f3e13a1e3b144c502c9c193
Théodore BIADALA <theodore@biadala.net>
2022-08-24 15:58:34 +0200

    test the input url widget

---
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/ImageUrlTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Drupal\Tests\ckeditor5\FunctionalJavascript;
+
+use Drupal\Component\Utility\Html;
+use Drupal\editor\Entity\Editor;
+use Drupal\file\Entity\File;
+use Drupal\filter\Entity\FilterFormat;
+use Drupal\node\Entity\Node;
+use Drupal\Tests\TestFileCreationTrait;
+use Drupal\Tests\ckeditor5\Traits\CKEditor5TestTrait;
+use Drupal\ckeditor5\Plugin\Editor\CKEditor5;
+use Symfony\Component\Validator\ConstraintViolation;
+
+// cspell:ignore imageresize imageupload
+
+/**
+ * @coversDefaultClass \Drupal\ckeditor5\Plugin\CKEditor5Plugin\Image
+ * @group ckeditor5
+ * @internal
+ */
+class ImageUrlTest extends CKEditor5TestBase {
+
+  use CKEditor5TestTrait;
+  use TestFileCreationTrait;
+
+  /**
+   * The user to use during testing.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * The sample image File entity to embed.
+   *
+   * @var \Drupal\file\FileInterface
+   */
+  protected $file;
+
+  /**
+   * A host entity with a body field to embed images in.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $host;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'ckeditor5',
+    'node',
+    'text',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    FilterFormat::create([
+      'format' => 'test_format',
+      'name' => 'Test format',
+      'filters' => [
+        'filter_html' => [
+          'status' => TRUE,
+          'settings' => [
+            'allowed_html' => '<p> <br> <em> <a href> <img alt height width src>',
+          ],
+        ],
+        'filter_align' => ['status' => TRUE],
+        'filter_caption' => ['status' => TRUE],
+      ],
+    ])->save();
+    Editor::create([
+      'editor' => 'ckeditor5',
+      'format' => 'test_format',
+      'settings' => [
+        'toolbar' => [
+          'items' => [
+            'drupalInsertImage',
+            'sourceEditing',
+            'link',
+            'italic',
+          ],
+        ],
+        'plugins' => [
+          'ckeditor5_sourceEditing' => [
+            'allowed_tags' => [],
+          ],
+          'ckeditor5_imageResize' => [
+            'allow_resize' => TRUE,
+          ],
+        ],
+      ],
+      'image_upload' => [
+        'status' => FALSE,
+      ],
+    ])->save();
+    $this->assertSame([], array_map(
+      function (ConstraintViolation $v) {
+        return (string) $v->getMessage();
+      },
+      iterator_to_array(CKEditor5::validatePair(
+        Editor::load('test_format'),
+        FilterFormat::load('test_format')
+      ))
+    ));
+    $this->adminUser = $this->drupalCreateUser([
+      'use text format test_format',
+      'bypass node access',
+      'administer filters',
+    ]);
+
+    $this->host = $this->createNode([
+      'type' => 'page',
+      'title' => 'Animals with strange names',
+      'body' => [
+        'value' => '<p>The pirate is irate.</p>',
+        'format' => 'test_format',
+      ],
+    ]);
+    $this->host->save();
+
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests the Drupal image URL widget.
+   */
+  public function testImageUrlWidget(): void {
+    $assert_session = $this->assertSession();
+    $page = $this->getSession()->getPage();
+
+    $image_selector = '.ck-widget.image-inline';
+    $src = '/core/misc/druplicon.png';
+
+    $this->drupalGet($this->host->toUrl('edit-form'));
+    $this->waitForEditor();
+
+    $this->pressEditorButton('Insert image');
+    $panel = $page->find('css', '.ck-dropdown__panel.ck-image-insert__panel');
+    $src_input = $panel->find('css', 'input[type=text]');
+    $src_input->setValue('/core/misc/druplicon.png');
+    $panel->find('xpath', "//button[span[text()='Insert']]")->click();
+
+    $this->assertNotEmpty($assert_session->waitForElementVisible('css', $image_selector));
+    $this->click($image_selector);
+    $this->assertVisibleBalloon('[aria-label="Image toolbar"]');
+
+    $this->pressEditorButton('Insert image');
+    $panel = $page->find('css', '.ck-dropdown__panel.ck-image-insert__panel');
+    $src_input = $panel->find('css', 'input[type=text]');
+    $this->assertEquals($src, $src_input->getValue());
+  }
+
+}
