2355b4e7c0e76d09b737f9da3a39389ad8eb8346
Lauri Eskola <lauri.eskola@acquia.com>
2022-08-25 13:51:07 +0300

    Make \Drupal\Tests\ckeditor5\FunctionalJavascript\ImageUrlTest::testAltTextRequired pass

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/imagealternativetext/drupalimagealternativetextui.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/imagealternativetext/drupalimagealternativetextui.js
@@ -61,22 +61,30 @@ export default class DrupalImageAlternativeTextUi extends Plugin {
     this._createForm();
     this._createMissingAltTextComponent();
 
+    const showAlternativeTextForm = () => {
+      const imageUtils = this.editor.plugins.get('ImageUtils');
+      // Show form after upload if there's image widget in the current
+      // selection.
+      if (
+        imageUtils.getClosestSelectedImageWidget(
+          this.editor.editing.view.document.selection,
+        )
+      ) {
+        this._showForm();
+      }
+    };
+
+
+    if (this.editor.commands.get('insertImage')) {
+      const insertImage = this.editor.commands.get('insertImage');
+      insertImage.on('execute', showAlternativeTextForm);
+    }
     if (this.editor.plugins.has('ImageUploadEditing')) {
       const imageUploadEditing = this.editor.plugins.get('ImageUploadEditing');
-      const imageUtils = this.editor.plugins.get('ImageUtils');
-      imageUploadEditing.on('uploadComplete', () => {
-        // Show form after upload if there's image widget in the current
-        // selection.
-        if (
-          imageUtils.getClosestSelectedImageWidget(
-            this.editor.editing.view.document.selection,
-          )
-        ) {
-          this._showForm();
-        }
-      });
+      imageUploadEditing.on('uploadComplete', showAlternativeTextForm);
     }
   }
+
 
   /**
    * Creates a missing alt text view which can be displayed within image widgets
