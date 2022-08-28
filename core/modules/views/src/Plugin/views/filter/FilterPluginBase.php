88cf9606cfc87b1a45a7f3cb989132c5c2910355
purushotam.rai <purushotam.rai@3193859.no-reply.drupal.org>
2022-08-28 11:36:34 +0000

    Fix: Remove unnecessary validation for any_label_override.

--- core/modules/views/src/Plugin/views/filter/FilterPluginBase.php
+++ core/modules/views/src/Plugin/views/filter/FilterPluginBase.php
@@ -571,6 +571,9 @@ abstract class FilterPluginBase extends HandlerBase implements CacheableDependen
           'invisible' => [
             ':input[name="options[expose][any_label_override]"]' => ['checked' => FALSE],
           ],
+          'required' => [
+            ':input[name="options[expose][any_label_override]"]' => ['checked' => FALSE],
+          ],
         ],
       ];
     }
@@ -699,11 +702,6 @@ abstract class FilterPluginBase extends HandlerBase implements CacheableDependen
       $form_state->setError(
         $form['expose']['operator_list'],
         $this->t('You selected the "@operator" operator as the default value but is not included in the list of limited operators.', ['@operator' => $this->operatorOptions()[$selected_operator]]));
-    }
-    $any_label_override =  $form_state->getValue(['options', 'expose', 'any_label_override']);
-    $any_label =  $form_state->getValue(['options', 'expose', 'any_label']);
-    if (!empty($any_label_override) && empty($any_label)) {
-      $form_state->setError($form['expose']['any_label'], $this->t('Any label is required.'));
     }
   }
 
@@ -1332,10 +1330,14 @@ abstract class FilterPluginBase extends HandlerBase implements CacheableDependen
     }
 
     if ($type == 'value' && empty($this->always_required) && empty($this->options['expose']['required']) && $form['#type'] == 'select' && empty($form['#multiple'])) {
-      $any_label = \Drupal::config('views.settings')->get('ui.exposed_filter_any_label') == 'old_any' ? t('<Any>') : t('- Any -');
+      $any_label = t('- Any -');
       if ($this->options['expose']['any_label_override']) {
         $label = $this->options['expose']['any_label'];
         $any_label = $label;
+      }
+      else {
+        $config = \Drupal::config('views.settings')->get('ui.exposed_filter_any_label');
+        $any_label = $config == 'old_any' ? t('<Any>') : $any_label;
       }
       $form['#options'] = ['All' => $any_label] + $form['#options'];
       $form['#default_value'] = 'All';
