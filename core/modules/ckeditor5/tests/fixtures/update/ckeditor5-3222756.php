dcb2e7fb0c211c7a1a496d46c47458bd614b3d71
Wim Leers <wim.leers@acquia.com>
2022-08-30 10:31:33 +0200

    Switch to the 9.4 fixture to allow an identical commit to the `9.5.x` and `10.0.x` branches. And take into account hook_removed_post_updates().

--- core/modules/ckeditor5/tests/fixtures/update/ckeditor5-3222756.php
+++ core/modules/ckeditor5/tests/fixtures/update/ckeditor5-3222756.php
@@ -25,6 +25,27 @@ $connection->update('config')
   ->condition('name', 'core.extension')
   ->execute();
 
+// Add all ckeditor5_removed_post_updates() as existing updates.
+require_once __DIR__ . '/../../../../ckeditor5/ckeditor5.post_update.php';
+if (function_exists('ckeditor5_removed_post_updates')) {
+  $existing_updates = $connection->select('key_value')
+    ->fields('key_value', ['value'])
+    ->condition('collection', 'post_update')
+    ->condition('name', 'existing_updates')
+    ->execute()
+    ->fetchField();
+  $existing_updates = unserialize($existing_updates);
+  $existing_updates = array_merge(
+    $existing_updates,
+    array_keys(ckeditor5_removed_post_updates()),
+  );
+  $connection->update('key_value')
+    ->fields(['value' => serialize($existing_updates)])
+    ->condition('collection', 'post_update')
+    ->condition('name', 'existing_updates')
+    ->execute();
+}
+
 $test_format_image_format = Yaml::decode(file_get_contents(__DIR__ . '/filter.format.test_format_image.yml'));
 $connection->insert('config')
   ->fields([
