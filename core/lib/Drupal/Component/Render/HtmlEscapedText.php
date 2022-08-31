3a6576b22df6e80660367a6c9ac84fea4da480ae
Billme <vas-alyfantis@3705281.no-reply.drupal.org>
2022-08-31 11:08:04 +0000

    Issue #2877953 by brtamas, Sivaji_Ganesh_Jojodae: HtmlEscapedText object to string conversion error

--- core/lib/Drupal/Component/Render/HtmlEscapedText.php
+++ core/lib/Drupal/Component/Render/HtmlEscapedText.php
@@ -28,6 +28,13 @@ class HtmlEscapedText implements MarkupInterface, \Countable {
    *   The string to escape. This value will be cast to a string.
    */
   public function __construct($string) {
+    if (is_object($string)) {
+      if (method_exists($string, 'toString')) {
+        $string = $string->toString();
+      } else {
+        $string = '';
+      }
+    }
     $this->string = (string) $string;
   }
 
