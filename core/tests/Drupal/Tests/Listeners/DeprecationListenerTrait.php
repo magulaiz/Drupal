c1c59ba2b56b834b89c74a04b420a33540e466c7
Wim Leers <wim.leers@acquia.com>
2022-08-31 15:35:10 +0200

    Update DeprecationListenerTrait to *better* ignore the `core/ckeditor` deprecation in tests.

--- core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
+++ core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
@@ -143,6 +143,7 @@ trait DeprecationListenerTrait {
       'The "core/drupal.date" asset library is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. There is no replacement. See https://www.drupal.org/node/3258267',
       "Drupal\Core\Render\Element\Date::processDate() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. There is no replacement. See https://www.drupal.org/node/3258267",
       // The CKEditor 4 asset library is deprecated.
+      'The "core/ckeditor" asset library is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. There is no replacement. See https://www.drupal.org/node/3304481',
       'Theme "core" is extending a deprecated library. The "core/ckeditor" asset library is deprecated in drupal:9.5.0 and is removed from drupal:10.0.0. There is no replacement. See https://www.drupal.org/node/3304481',
     ];
   }
