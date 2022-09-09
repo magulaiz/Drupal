d1c387f1ef2907649ad00b384ddb3cf95edb2fec
Phéna Proxima <adam@phenaproxima.net>
2022-09-09 12:31:40 -0400

    Temporarily add SettingsTrayBlockFormTest methods to legacy group

--- core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
+++ core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
@@ -162,6 +162,11 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
 
   /**
    * Creates tests for ::testBlocks().
+   *
+   * @group legacy
+   *
+   * @todo Remove this from the `legacy` group once Stable is removed from
+   *   core.
    */
   public function getBlockTests() {
     $blocks = [];
@@ -229,6 +234,11 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
 
   /**
    * Tests enabling and disabling Edit Mode.
+   *
+   * @group legacy
+   *
+   * @todo Remove this from the `legacy` group once Stable is removed from
+   *   core.
    */
   public function testEditModeEnableDisable() {
     foreach ($this->getTestThemes() as $theme) {
@@ -264,6 +274,11 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
 
   /**
    * Tests that validation errors appear in the off-canvas dialog.
+   *
+   * @group legacy
+   *
+   * @todo Remove this from the `legacy` group once Stable is removed from
+   *   core.
    */
   public function testValidationMessages() {
     $page = $this->getSession()->getPage();
