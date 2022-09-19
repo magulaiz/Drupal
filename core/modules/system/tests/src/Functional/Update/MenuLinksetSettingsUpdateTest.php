de75b9714e5a30772972e80567168584b7ef02d7
Brian Perry <brian.perry@pantheon.io>
2022-09-19 13:32:50 +0200

    Revert "Update fixture for MenuLinksetSettingsUpdateTest"
    This reverts commit df78f3d0fefe108ecd5fc4c406552b10e8e0e95f.

--- core/modules/system/tests/src/Functional/Update/MenuLinksetSettingsUpdateTest.php
+++ core/modules/system/tests/src/Functional/Update/MenuLinksetSettingsUpdateTest.php
@@ -18,7 +18,7 @@ class MenuLinksetSettingsUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles() {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../fixtures/update/drupal-9.4.0.bare.standard.php.gz',
+      __DIR__ . '/../../../fixtures/update/drupal-9.3.0.bare.standard.php.gz',
     ];
   }
 
