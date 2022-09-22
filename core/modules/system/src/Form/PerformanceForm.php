9d062608e5f0eaf8833aead58a411985ca963f13
Zequi Vazquez <zequi@lullabot.com>
2022-09-22 15:08:54 +0200

    #3278493: Move theme debug checkbox to Performance form

--- core/modules/system/src/Form/PerformanceForm.php
+++ core/modules/system/src/Form/PerformanceForm.php
@@ -3,11 +3,13 @@
 namespace Drupal\system\Form;
 
 use Drupal\Core\Asset\AssetCollectionOptimizerInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Site\Settings;
+use Drupal\Core\State\StateInterface;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -47,6 +49,20 @@ class PerformanceForm extends ConfigFormBase {
   protected $moduleHandler;
 
   /**
+   * The state.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * The settings.
+   *
+   * @var \Drupal\Core\Site\Settings
+   */
+  protected $settings;
+
+  /**
    * Constructs a PerformanceForm object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
@@ -59,14 +75,20 @@ class PerformanceForm extends ConfigFormBase {
    *   The JavaScript asset collection optimizer service.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state.
+   * @param \Drupal\Core\Site\Settings $settings
+   *   The settings.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, DateFormatterInterface $date_formatter, AssetCollectionOptimizerInterface $css_collection_optimizer, AssetCollectionOptimizerInterface $js_collection_optimizer, ModuleHandlerInterface $module_handler) {
+  public function __construct(ConfigFactoryInterface $config_factory, DateFormatterInterface $date_formatter, AssetCollectionOptimizerInterface $css_collection_optimizer, AssetCollectionOptimizerInterface $js_collection_optimizer, ModuleHandlerInterface $module_handler, StateInterface $state, Settings $settings) {
     parent::__construct($config_factory);
 
     $this->dateFormatter = $date_formatter;
     $this->cssCollectionOptimizer = $css_collection_optimizer;
     $this->jsCollectionOptimizer = $js_collection_optimizer;
     $this->moduleHandler = $module_handler;
+    $this->state = $state;
+    $this->settings = $settings;
   }
 
   /**
@@ -78,7 +100,9 @@ class PerformanceForm extends ConfigFormBase {
       $container->get('date.formatter'),
       $container->get('asset.css.collection_optimizer'),
       $container->get('asset.js.collection_optimizer'),
-      $container->get('module_handler')
+      $container->get('module_handler'),
+      $container->get('state'),
+      $container->get('settings')
     );
   }
 
@@ -166,6 +190,24 @@ class PerformanceForm extends ConfigFormBase {
       '#disabled' => $disabled,
     ];
 
+    $form['theme_development'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Development'),
+      '#open' => TRUE,
+      '#description' => $this->t('Enable theme debug mode. This option will turn Twig debug mode on, and set Null cache backend for page, dynamic_page, and render cache bins. Cache will be rebuilt after changing this setting.'),
+    ];
+
+    // Do not allow change this setting from UI when it is overridden in
+    // settings.php file
+    $theme_debug_overridden = $this->settings->get('theme_debug');
+    $form['theme_development']['theme_debug'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Theme debug mode'),
+      '#default_value' => $theme_debug_overridden ?? $this->state->get('theme_debug', FALSE),
+      '#disabled' => $theme_debug_overridden !== NULL,
+      '#description' => $theme_debug_overridden !== NULL ? $this->t('Overridden in settings file.') : '',
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -181,6 +223,17 @@ class PerformanceForm extends ConfigFormBase {
       ->set('css.preprocess', $form_state->getValue('preprocess_css'))
       ->set('js.preprocess', $form_state->getValue('preprocess_js'))
       ->save();
+
+    // Save the theme debug value to state, if not overridden in settings file.
+    if ($this->settings->get('theme_debug') === NULL) {
+      $theme_debug = $this->state->get('theme_debug', 0);
+
+      // Save and rebuild caches if value has actually changed.
+      if ($theme_debug !== $form_state->getValue('theme_debug')) {
+        $this->state->set('theme_debug', $form_state->getValue('theme_debug'));
+        drupal_flush_all_caches();
+      }
+    }
 
     parent::submitForm($form, $form_state);
   }
