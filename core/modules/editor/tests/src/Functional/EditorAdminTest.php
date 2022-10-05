57de81b7033ac5bcf9d92a294f4d2543d0030388
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2022-10-05 12:21:00 -0600

    add test coverage for lazy filter after reference

--- core/modules/editor/tests/src/Functional/EditorAdminTest.php
+++ core/modules/editor/tests/src/Functional/EditorAdminTest.php
@@ -157,6 +157,27 @@ class EditorAdminTest extends BrowserTestBase {
   }
 
   /**
+   * Tests lazy load must go after file reference filter.
+   */
+  public function testLazyAfterReferenceFilter(): void {
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('admin/config/content/formats/manage/filtered_html');
+
+    $page = $this->getSession()->getPage();
+    $page->checkField('Track images uploaded via a Text Editor');
+    $page->checkField('Lazy load tracked images uploaded via a Text Editor');
+    $page->selectFieldOption('edit-filters-editor-file-reference-weight', 10);
+    $page->selectFieldOption('edit-filters-editor-image-lazy-load-weight', 0);
+    $page->pressButton('Save configuration');
+    $assert = $this->assertSession();
+    $assert->pageTextContains('The Track images uploaded via a Text Editor must be enabled and placed before the Lazy load tracked images uploaded via a Text Editor filter.');
+    $page->selectFieldOption('edit-filters-editor-file-reference-weight', 0);
+    $page->selectFieldOption('edit-filters-editor-image-lazy-load-weight', 10);
+    $page->pressButton('Save configuration');
+    $assert->pageTextContains('The text format Filtered HTML has been updated.');
+  }
+
+  /**
    * Tests switching text editor to none does not throw a TypeError.
    */
   public function testSwitchEditorToNone() {
