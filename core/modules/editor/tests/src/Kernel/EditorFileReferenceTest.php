eae0fcd2aa79783e69d82974d457f1469b27bd1e
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2022-10-06 08:23:05 -0600

    fix failing test

--- core/modules/editor/tests/src/Kernel/EditorFileReferenceTest.php
+++ core/modules/editor/tests/src/Kernel/EditorFileReferenceTest.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Drupal\Tests\editor\Unit;
+namespace Drupal\Tests\editor\Kernel;
 
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Image\ImageFactory;
 use Drupal\editor\Plugin\Filter\EditorFileReference;
-use Drupal\Tests\UnitTestCase;
+use Drupal\KernelTests\KernelTestBase;
 
 /**
  * Tests editor file reference filter plugin.
@@ -15,7 +15,7 @@ use Drupal\Tests\UnitTestCase;
  * @coversDefaultClass \Drupal\editor\Plugin\Filter\EditorFileReference
  * @group editor
  */
-class EditorFileReferenceTest extends UnitTestCase {
+class EditorFileReferenceTest extends KernelTestBase {
 
   /**
    * Tests EditorFileReference image factory deprecation.
@@ -28,8 +28,9 @@ class EditorFileReferenceTest extends UnitTestCase {
     $plugin_definition = ['provider' => 'editor'];
     $entity_repository = $this->prophesize(EntityRepositoryInterface::class);
     $image_factory = $this->prophesize(ImageFactory::class);
-    $this->expectDeprecation('Calling Drupal\editor\Plugin\Filter\EditorFileReference::__construct() with the $image_factory argument is deprecated in drupal:10.1.0 and is removed in drupal:11.0.0. See https://www.drupal.org/node/3173719');
-    new EditorFileReference($configuration, $plugin_id, $plugin_definition, $entity_repository->reveal(), $image_factory->reveal());
+    $this->expectDeprecation('The property imageFactory (image.factory service) is deprecated in Drupal\editor\Plugin\Filter\EditorFileReference and will be removed before Drupal 11.0.0.');
+    $editor_file_reference = new EditorFileReference($configuration, $plugin_id, $plugin_definition, $entity_repository->reveal(), $image_factory->reveal());
+    $this->assertInstanceOf(ImageFactory::class, $editor_file_reference->imageFactory);
   }
 
 }
