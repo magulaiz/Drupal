7099a88094e4aa6a39392c54a526ed1ba43f2086
Carly Gerard <gerardc@wwu.edu>
2022-10-06 16:38:19 -0700

    remove unneeded variable, add label element parameters to style test

--- core/modules/views/src/Plugin/views/style/StylePluginBase.php
+++ core/modules/views/src/Plugin/views/style/StylePluginBase.php
@@ -628,7 +628,6 @@ abstract class StylePluginBase extends PluginBase {
         // While iterating, parent groups, that do not exist yet, are added.
         $set = &$sets;
         foreach ($groupings as $level => $info) {
-          $info['grouping_label_element'] = '';
           $field = $info['field'];
           $rendered = $info['rendered'] ?? $group_rendered;
           $rendered_strip = $info['rendered_strip'] ?? FALSE;
