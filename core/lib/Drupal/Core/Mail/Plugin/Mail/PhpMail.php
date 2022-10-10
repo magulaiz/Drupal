dcf27d8db99abf1a65297162685410e31b7ffd2c
Osvaldo Villarroel Maraon <vachisinio@gmail.com>
2022-10-10 11:06:06 -0400

    Fix unvalidated get SERVER_SOFTWARE when is empty

--- core/lib/Drupal/Core/Mail/Plugin/Mail/PhpMail.php
+++ core/lib/Drupal/Core/Mail/Plugin/Mail/PhpMail.php
@@ -77,7 +77,7 @@ class PhpMail implements MailInterface {
     // If 'Return-Path' isn't already set in php.ini, we pass it separately
     // as an additional parameter instead of in the header.
     if (isset($message['headers']['Return-Path'])) {
-      $return_path_set = strpos((string)ini_get('sendmail_path'), ' -f');
+      $return_path_set = strpos(ini_get('sendmail_path'), ' -f');
       if (!$return_path_set) {
         $message['Return-Path'] = $message['headers']['Return-Path'];
         unset($message['headers']['Return-Path']);
@@ -109,7 +109,8 @@ class PhpMail implements MailInterface {
     // We suppress warnings and notices from mail() because of issues on some
     // hosts. The return value of this method will still indicate whether mail
     // was sent successfully.
-    if (!$request->server->has('WINDIR') && strpos((string)$request->server->get('SERVER_SOFTWARE'), 'Win32') === FALSE) {
+    $server_software = empty($request->server->get('SERVER_SOFTWARE')) ? PHP_OS : $request->server->get('SERVER_SOFTWARE');
+    if (!$request->server->has('WINDIR') && strpos($server_software, 'Win32') === FALSE) {
       // On most non-Windows systems, the "-f" option to the sendmail command
       // is used to set the Return-Path. There is no space between -f and
       // the value of the return path.
