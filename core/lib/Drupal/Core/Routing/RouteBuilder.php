06d1500df785243563fef4e6e2588ee5913879e8
Alex Pott <alex.a.pott@googlemail.com>
2022-10-11 18:16:13 +0100

    WHoops

--- core/lib/Drupal/Core/Routing/RouteBuilder.php
+++ core/lib/Drupal/Core/Routing/RouteBuilder.php
@@ -130,7 +130,7 @@ class RouteBuilder implements RouteBuilderInterface, DestructableInterface {
     $this->building = TRUE;
     $collection = new RouteCollection();
 
-    // DYNAMIC is supposed to be used to add new routes based upon all the
+    // STATIC is supposed to be used to add new routes based upon all the
     // static defined ones.
     $this->dispatcher->dispatch(new RouteBuildEvent($collection), RoutingEvents::STATIC);
 
