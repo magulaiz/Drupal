4c2cbd0c3ec026de77e0eaa82b8fd5e86927af4e
Adaucy Junior <adaucyj@ciandt.com>
2022-10-19 15:12:57 -0300

    Issue #3296811 by imustakim, jmizarela, GersonJL, adaucyj: ResourceResponseTrait needs a setData() method to be consistent with the API

--- core/modules/rest/src/ResourceResponseInterface.php
+++ core/modules/rest/src/ResourceResponseInterface.php
@@ -4,6 +4,8 @@ namespace Drupal\rest;
 
 /**
  * Defines a common interface for resource responses.
+ *
+ * @see \Drupal\rest\ResourceResponseTrait
  */
 interface ResourceResponseInterface {
 
@@ -14,5 +16,16 @@ interface ResourceResponseInterface {
    *   Response data that should be serialized.
    */
   public function getResponseData();
+
+  /**
+   * Sets the response content.
+   *
+   * @param mixed $data
+   *   Response data that should be serialized.
+   *
+   * @return Drupal\rest\ResourceResponseInterface
+   *   Interface for resource responses.
+   */
+  public function setResponseData($data): ResourceResponseInterface;
 
 }
