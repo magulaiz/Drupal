a0d6c24b426591a2f86d6c31d7ef54a3249cf343
Wim Leers <wim.leers@acquia.com>
2022-10-27 13:49:14 +0200

    `yarn run prettier`

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalEntityLinkSuggestions/src/linksuggestionediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalEntityLinkSuggestions/src/linksuggestionediting.js
@@ -20,32 +20,35 @@ export default class DrupalEntityLinkSuggestionsEditing extends Plugin {
       editor.conversion.for('downcast').attributeToElement({
         model: attribute,
         view: (value, { writer }) => {
-          const linkViewElement = writer.createAttributeElement('a', {
-            [attribute]: value
-          }, { priority: 5 });
+          const linkViewElement = writer.createAttributeElement(
+            'a',
+            {
+              [attribute]: value,
+            },
+            { priority: 5 },
+          );
 
           // Without it the isLinkElement() will not recognize the link and the UI will not show up
           // when the user clicks a link.
           writer.setCustomProperty('link', true, linkViewElement);
 
           return linkViewElement;
-        }
+        },
       });
 
       // View (DOM/DATA) -> Model
-      editor.conversion.for('upcast')
-        .elementToAttribute({
-          view: {
-            name: 'a',
-            attributes: {
-              [attribute]: true,
-            }
+      editor.conversion.for('upcast').elementToAttribute({
+        view: {
+          name: 'a',
+          attributes: {
+            [attribute]: true,
           },
-          model: {
-            key: attribute,
-            value: viewElement => viewElement.getAttribute(attribute),
-          }
-        });
+        },
+        model: {
+          key: attribute,
+          value: (viewElement) => viewElement.getAttribute(attribute),
+        },
+      });
     });
   }
 
@@ -54,61 +57,76 @@ export default class DrupalEntityLinkSuggestionsEditing extends Plugin {
     const linkCommand = editor.commands.get('link');
     let linkCommandExecuting = false;
 
-    linkCommand.on('execute', (evt, args) => {
-      // Custom handling is only required if an extra attribute was passed into
-      // editor.execute( 'link', ... ).
-      if (args.length < 3) {
-        return;
-      }
-      if (linkCommandExecuting) {
-        linkCommandExecuting = false;
-        return;
-      }
+    linkCommand.on(
+      'execute',
+      (evt, args) => {
+        // Custom handling is only required if an extra attribute was passed into
+        // editor.execute( 'link', ... ).
+        if (args.length < 3) {
+          return;
+        }
+        if (linkCommandExecuting) {
+          linkCommandExecuting = false;
+          return;
+        }
 
-      // If the additional attribute was passed, we stop the default execution
-      // of the LinkCommand. We're going to create Model#change() block for undo
-      // and execute the LinkCommand together with setting the extra attribute.
-      evt.stop();
+        // If the additional attribute was passed, we stop the default execution
+        // of the LinkCommand. We're going to create Model#change() block for undo
+        // and execute the LinkCommand together with setting the extra attribute.
+        evt.stop();
 
-      // Prevent infinite recursion by keeping records of when link command is
-      // being executed by this function.
-      linkCommandExecuting = true;
-      const extraAttributeValues = args[args.length - 1];
-      const model = this.editor.model;
-      const selection = model.document.selection;
+        // Prevent infinite recursion by keeping records of when link command is
+        // being executed by this function.
+        linkCommandExecuting = true;
+        const extraAttributeValues = args[args.length - 1];
+        const model = this.editor.model;
+        const selection = model.document.selection;
 
-      // Wrapping the original command execution in a model.change() block to
-      // make sure there's a single undo step when the extra attribute is added.
-      model.change((writer) => {
-        editor.execute('link', ...args);
+        // Wrapping the original command execution in a model.change() block to
+        // make sure there's a single undo step when the extra attribute is added.
+        model.change((writer) => {
+          editor.execute('link', ...args);
 
-        const firstPosition = selection.getFirstPosition();
+          const firstPosition = selection.getFirstPosition();
 
-        this.attrs.forEach((attribute) => {
-          if (selection.isCollapsed) {
-            const node = firstPosition.textNode || firstPosition.nodeBefore;
+          this.attrs.forEach((attribute) => {
+            if (selection.isCollapsed) {
+              const node = firstPosition.textNode || firstPosition.nodeBefore;
 
-            if (extraAttributeValues[attribute]) {
-              writer.setAttribute(attribute, extraAttributeValues[attribute], writer.createRangeOn(node));
-            } else {
-              writer.removeAttribute(attribute, writer.createRangeOn(node));
-            }
-
-            writer.removeSelectionAttribute(attribute);
-          } else {
-            const ranges = model.schema.getValidRanges(selection.getRanges(), attribute);
-
-            for (const range of ranges) {
               if (extraAttributeValues[attribute]) {
-                writer.setAttribute(attribute, extraAttributeValues[attribute], range);
+                writer.setAttribute(
+                  attribute,
+                  extraAttributeValues[attribute],
+                  writer.createRangeOn(node),
+                );
               } else {
-                writer.removeAttribute(attribute, range);
+                writer.removeAttribute(attribute, writer.createRangeOn(node));
+              }
+
+              writer.removeSelectionAttribute(attribute);
+            } else {
+              const ranges = model.schema.getValidRanges(
+                selection.getRanges(),
+                attribute,
+              );
+
+              for (const range of ranges) {
+                if (extraAttributeValues[attribute]) {
+                  writer.setAttribute(
+                    attribute,
+                    extraAttributeValues[attribute],
+                    range,
+                  );
+                } else {
+                  writer.removeAttribute(attribute, range);
+                }
               }
             }
-          }
+          });
         });
-      });
-    }, { priority: 'high' } );
+      },
+      { priority: 'high' },
+    );
   }
 
   _removeExtraAttributesOnUnlinkCommandExecute() {
@@ -120,54 +138,63 @@ export default class DrupalEntityLinkSuggestionsEditing extends Plugin {
     let isUnlinkingInProgress = false;
 
     // Make sure all changes are in a single undo step so cancel the original unlink first in the high priority.
-    unlinkCommand.on('execute', evt => {
-      if (isUnlinkingInProgress) {
-        return;
-      }
+    unlinkCommand.on(
+      'execute',
+      (evt) => {
+        if (isUnlinkingInProgress) {
+          return;
+        }
 
-      evt.stop();
+        evt.stop();
 
-      // This single block wraps all changes that should be in a single undo step.
-      model.change(() => {
-        // Now, in this single "undo block" let the unlink command flow naturally.
-        isUnlinkingInProgress = true;
+        // This single block wraps all changes that should be in a single undo step.
+        model.change(() => {
+          // Now, in this single "undo block" let the unlink command flow naturally.
+          isUnlinkingInProgress = true;
 
-        // Do the unlinking within a single undo step.
-        editor.execute('unlink');
+          // Do the unlinking within a single undo step.
+          editor.execute('unlink');
 
-        // Let's make sure the next unlinking will also be handled.
-        isUnlinkingInProgress = false;
+          // Let's make sure the next unlinking will also be handled.
+          isUnlinkingInProgress = false;
 
-        // The actual integration that removes the extra attribute.
-        model.change(writer => {
-          // Get ranges to unlink.
-          let ranges;
+          // The actual integration that removes the extra attribute.
+          model.change((writer) => {
+            // Get ranges to unlink.
+            let ranges;
 
-          this.attrs.forEach((attribute) => {
-            if (selection.isCollapsed) {
-              ranges = [findAttributeRange(
-                selection.getFirstPosition(),
-                attribute,
-                selection.getAttribute(attribute),
-                model
-              )];
-            } else {
-              ranges = model.schema.getValidRanges(selection.getRanges(), attribute);
-            }
+            this.attrs.forEach((attribute) => {
+              if (selection.isCollapsed) {
+                ranges = [
+                  findAttributeRange(
+                    selection.getFirstPosition(),
+                    attribute,
+                    selection.getAttribute(attribute),
+                    model,
+                  ),
+                ];
+              } else {
+                ranges = model.schema.getValidRanges(
+                  selection.getRanges(),
+                  attribute,
+                );
+              }
 
-            // Remove the extra attribute from specified ranges.
-            for (const range of ranges) {
-              writer.removeAttribute(attribute, range);
-            }
+              // Remove the extra attribute from specified ranges.
+              for (const range of ranges) {
+                writer.removeAttribute(attribute, range);
+              }
+            });
           });
         });
-      });
-    }, { priority: 'high' });
+      },
+      { priority: 'high' },
+    );
   }
 
   _refreshExtraAttributeValues() {
     const editor = this.editor;
-    const attributes = this.attrs
+    const attributes = this.attrs;
     const linkCommand = editor.commands.get('link');
     const model = this.editor.model;
     const selection = model.document.selection;
