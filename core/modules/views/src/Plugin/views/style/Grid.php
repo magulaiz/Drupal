f5cc2cc8bb13f624daaed0450f2319cde6991cdf
Carly Gerard <gerardc@wwu.edu>
2022-11-01 14:18:02 -0700

    add grouping label element support for grid displays

--- core/modules/views/src/Plugin/views/style/Grid.php
+++ core/modules/views/src/Plugin/views/style/Grid.php
@@ -45,6 +45,22 @@ class Grid extends StylePluginBase {
    */
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
     parent::buildOptionsForm($form, $form_state);
+    // Add support for selecting an HTML label to use for the grouping
+    $c = count($this->options['grouping']);
+    // Add a form for every grouping, plus one.
+    for ($i = 0; $i <= $c; $i++) {
+      $grouping = !empty($this->options['grouping'][$i]) ? $this->options['grouping'][$i] : [];
+      $grouping += ['field' => '', 'rendered' => TRUE, 'rendered_strip' => FALSE, 'grouping_label_element' => ''];
+      
+      $form['grouping'][$i]['grouping_label_element'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Grouping Label Tag', ['@number' => $i + 1]),
+        '#options' => $this->getLabelElements(),
+        '#default_value' => $grouping['grouping_label_element'],
+        '#description' => $this->t('You may specify a wrapper tag by which to group the records.'),
+      ];
+    }
+    
     $form['columns'] = [
       '#type' => 'number',
       '#title' => $this->t('Number of columns'),
