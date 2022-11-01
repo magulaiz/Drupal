405b03dcfc122c83b15fb8646797decf45f051e1
Carly Gerard <gerardc@wwu.edu>
2022-11-01 14:12:36 -0700

    add grouping label element options to default style

--- core/modules/views/src/Plugin/views/style/DefaultStyle.php
+++ core/modules/views/src/Plugin/views/style/DefaultStyle.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\views\Plugin\views\style;
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Unformatted style plugin to render rows.
  *
@@ -30,5 +32,24 @@ class DefaultStyle extends StylePluginBase {
    * @var bool
    */
   protected $usesRowClass = TRUE;
+
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    // Add support for selecting an HTML label to use for the grouping
+    $c = count($this->options['grouping']);
+    // Add a form for every grouping, plus one.
+    for ($i = 0; $i <= $c; $i++) {
+      $grouping = !empty($this->options['grouping'][$i]) ? $this->options['grouping'][$i] : [];
+      $grouping += ['field' => '', 'rendered' => TRUE, 'rendered_strip' => FALSE, 'grouping_label_element' => ''];
+      
+      $form['grouping'][$i]['grouping_label_element'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Grouping Label Tag', ['@number' => $i + 1]),
+        '#options' => $this->getLabelElements(),
+        '#default_value' => $grouping['grouping_label_element'],
+        '#description' => $this->t('You may specify a wrapper tag by which to group the records.'),
+      ];
+    }
+  }
 
 }
