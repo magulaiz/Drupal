a59a8d56de322b84aeb1a5bfd77d2ffb6f1a605c
Carly Gerard <gerardc@wwu.edu>
2022-11-01 17:26:50 -0700

    remove separating white space

--- core/modules/views/src/Plugin/views/style/Grid.php
+++ core/modules/views/src/Plugin/views/style/Grid.php
@@ -58,7 +58,6 @@ class Grid extends StylePluginBase {
     for ($i = 0; $i <= $c; $i++) {
       $grouping = !empty($this->options['grouping'][$i]) ? $this->options['grouping'][$i] : [];
       $grouping += ['field' => '', 'rendered' => TRUE, 'rendered_strip' => FALSE, 'grouping_label_element' => ''];
-
       $form['grouping'][$i]['grouping_label_element'] = [
         '#type' => 'select',
         '#title' => $this->t('Grouping Label Tag', ['@number' => $i + 1]),
@@ -67,7 +66,6 @@ class Grid extends StylePluginBase {
         '#description' => $this->t('You may specify a wrapper tag by which to group the records.'),
       ];
     }
-
     $form['columns'] = [
       '#type' => 'number',
       '#title' => $this->t('Number of columns'),
--- core/modules/views/src/Plugin/views/style/HtmlList.php
+++ core/modules/views/src/Plugin/views/style/HtmlList.php
@@ -82,7 +82,6 @@ class HtmlList extends StylePluginBase {
     for ($i = 0; $i <= $c; $i++) {
       $grouping = !empty($this->options['grouping'][$i]) ? $this->options['grouping'][$i] : [];
       $grouping += ['field' => '', 'rendered' => TRUE, 'rendered_strip' => FALSE, 'grouping_label_element' => ''];
-
       $form['grouping'][$i]['grouping_label_element'] = [
         '#type' => 'select',
         '#title' => $this->t('Grouping Label Tag', ['@number' => $i + 1]),
