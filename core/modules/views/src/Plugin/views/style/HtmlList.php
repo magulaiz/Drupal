9d91924a56f10c5d4499484c5014da5b2dd62c18
Carly Gerard <gerardc@wwu.edu>
2022-11-03 15:13:15 -0700

    htmllist uses grouping label element, formatting error fix

--- core/modules/views/src/Plugin/views/style/HtmlList.php
+++ core/modules/views/src/Plugin/views/style/HtmlList.php
@@ -59,7 +59,10 @@ class HtmlList extends StylePluginBase {
     $form['type'] = [
       '#type' => 'radios',
       '#title' => $this->t('List type'),
-      '#options' => ['ul' => $this->t('Unordered list'), 'ol' => $this->t('Ordered list')],
+      '#options' => [
+        'ul' => $this->t('Unordered list'),
+        'ol' => $this->t('Ordered list'),
+      ],
       '#default_value' => $this->options['type'],
     ];
     $form['wrapper_class'] = [
@@ -76,20 +79,6 @@ class HtmlList extends StylePluginBase {
       '#size' => '30',
       '#default_value' => $this->options['class'],
     ];
-    // Add support for selecting an HTML label to use for the grouping.
-    $c = count($this->options['grouping']);
-    // Add a form for every grouping, plus one.
-    for ($i = 0; $i <= $c; $i++) {
-      $grouping = !empty($this->options['grouping'][$i]) ? $this->options['grouping'][$i] : [];
-      $grouping += ['field' => '', 'rendered' => TRUE, 'rendered_strip' => FALSE, 'grouping_label_element' => ''];
-      $form['grouping'][$i]['grouping_label_element'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Grouping Label Tag', ['@number' => $i + 1]),
-        '#options' => $this->getLabelElements(),
-        '#default_value' => $grouping['grouping_label_element'],
-        '#description' => $this->t('You may specify a wrapper tag by which to group the records.'),
-      ];
-    }
   }
 
 }
