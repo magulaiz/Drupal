55d449f1a3100da5f1bc9c45ec6a840a827ac075
Carly Gerard <gerardc@wwu.edu>
2022-11-10 14:12:03 -0800

    only set grouping_label vars if grouping label is used

--- core/modules/views/src/Plugin/views/style/StylePluginBase.php
+++ core/modules/views/src/Plugin/views/style/StylePluginBase.php
@@ -625,7 +625,9 @@ abstract class StylePluginBase extends PluginBase {
           $rendered = $info['rendered'] ?? $group_rendered;
           $rendered_strip = $info['rendered_strip'] ?? FALSE;
           $grouping = '';
-          $grouping_label_element = $info['grouping_label_element'] ?? '';
+          if ($this->usesGroupingLabelElement()) {
+            $grouping_label_element = $info['grouping_label_element'];
+          }
           $group_content = '';
           // Group on the rendered version of the field, not the raw.  That way,
           // we can control any special formatting of the grouping field through
@@ -657,7 +659,9 @@ abstract class StylePluginBase extends PluginBase {
             $set[$grouping]['group'] = $group_content;
             $set[$grouping]['level'] = $level;
             $set[$grouping]['rows'] = [];
-            $set[$grouping]['grouping_label_element'] = $grouping_label_element;
+            if ($this->usesGroupingLabelElement()) {
+              $set[$grouping]['grouping_label_element'] = $grouping_label_element;
+            }
           }
 
           // Move the set reference into the row set of the group we just determined.
