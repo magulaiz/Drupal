44025ba286bc1d42fee1d38b6a67f420b99c245d
Spokje <Spokje@3386132.no-reply.drupal.org>
2022-11-18 12:03:30 +0100

    Point to current CR
    (cherry picked from commit c833041054dc335114b099600ef2632d1fb1820f)

--- core/lib/Drupal/Core/Password/PhpassHashedPassword.php
+++ core/lib/Drupal/Core/Password/PhpassHashedPassword.php
@@ -8,5 +8,5 @@ namespace Drupal\Core\Password;
  * @deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use
  *   \Drupal\Core\Password\LegacyPassword instead.
  *
- * @see https://www.drupal.org/project/drupal/issues/1845004
+ * @see https://www.drupal.org/project/drupal/issues/3322420
  */
