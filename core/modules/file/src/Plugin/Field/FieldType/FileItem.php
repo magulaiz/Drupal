fae75d7b10fbad0e918fce3bbb181f48618191ff
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2022-11-20 18:13:51 +0530

    Issue #2157945: Fix CS.

--- core/modules/file/src/Plugin/Field/FieldType/FileItem.php
+++ core/modules/file/src/Plugin/Field/FieldType/FileItem.php
@@ -189,7 +189,7 @@ class FileItem extends EntityReferenceItem {
       '#type' => 'textfield',
       '#title' => $this->t('Maximum upload size'),
       '#default_value' => $settings['max_filesize'],
-      '#description' => t('Enter a value like "512" (bytes), "80 KB" (kilobytes) or "50 MB" (megabytes) in order to restrict the allowed file size. If left empty the file sizes will be limited only by PHP\'s maximum post and file upload sizes (current limit <strong>%limit</strong>).', [
+      '#description' => $this->t('Enter a value like "512" (bytes), "80 KB" (kilobytes) or "50 MB" (megabytes) in order to restrict the allowed file size. If left empty the file sizes will be limited only by PHP\'s maximum post and file upload sizes (current limit <strong>%limit</strong>).', [
           '%limit' => ByteSizeMarkup::create(Environment::getUploadMaxSize()),
       ]),
     ];
