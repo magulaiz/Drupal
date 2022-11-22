89515faefe876f0d2ebd6218bd2e0652908e5a63
Bhanu951 <bhanu951@3607219.no-reply.drupal.org>
2022-11-22 13:29:54 +0000

    Issue #3323041: Update Test.

--- core/modules/mysql/tests/src/Functional/Mysql8RequirePrimaryKeyUpdateTest.php
+++ core/modules/mysql/tests/src/Functional/Mysql8RequirePrimaryKeyUpdateTest.php
@@ -58,7 +58,7 @@ class Mysql8RequirePrimaryKeyUpdateTest extends UpdatePathTestBase {
 
     // Ensure that after updating a user can be created and do a basic test that
     // the site is available by logging in.
-    $this->drupalLogin($this->createUser(['administer site configuration']));
+    $this->drupalLogin($this->createUser());
     $this->assertSession()->statusCodeEquals(200);
   }
 
