9da00ee6eccfd2f4601f40f82a2c4949ce07ecab
Phéna Proxima <adam@phenaproxima.net>
2022-11-28 13:56:09 -0500

    Validate that the listed config exists.

--- core/modules/system/tests/src/Kernel/Entity/ConfigEntityValidationTest.php
+++ core/modules/system/tests/src/Kernel/Entity/ConfigEntityValidationTest.php
@@ -15,12 +15,14 @@ class ConfigEntityValidationTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['user'];
+  protected static $modules = ['system', 'user'];
 
   /**
    * Tests validation of config dependencies.
    */
   public function testConfigDependenciesValidation(): void {
+    $this->installConfig(['system', 'user']);
+
     /** @var \Drupal\Core\Entity\EntityViewModeInterface $entity */
     $entity = EntityViewMode::create([
       'id' => 'user.test',
@@ -42,7 +44,7 @@ class ConfigEntityValidationTest extends KernelTestBase {
     // purposes of this test, these dependencies don't need to actually exist.
     $data['dependencies']['theme'][] = 'stark';
     $data['dependencies']['module'][] = 'system';
-    $data['dependencies']['config'][] = 'system.site';
+    $data['dependencies']['config'][] = 'user.settings';
     $data['dependencies']['content'][] = 'node:some-random-uuid';
     $this->assertCount(0, $typed_config->createFromNameAndData($name, $data)->validate());
 
@@ -51,6 +53,21 @@ class ConfigEntityValidationTest extends KernelTestBase {
     $violations = $typed_config->createFromNameAndData($name, $data)->validate();
     $this->assertCount(1, $violations);
     $this->assertSame("'fun_stuff' is not a supported key.", (string) $violations->get(0)->getMessage());
+
+    // Adding a dependency on non-existent config should raise an error.
+    unset($data['dependencies']['fun_stuff']);
+    $data['dependencies']['config'][] = 'node.settings';
+    $violations = $typed_config->createFromNameAndData($name, $data)->validate();
+    $this->assertCount(1, $violations);
+    $this->assertSame("The 'node.settings' config does not exist.", (string) $violations->get(0)->getMessage());
+
+    // Trying to put an empty string in config dependencies should also raise an
+    // error.
+    array_splice($data['dependencies']['config'], -1, NULL, ['']);
+    $violations = $typed_config->createFromNameAndData($name, $data)->validate();
+    $this->assertCount(2, $violations);
+    $this->assertSame('This value should not be blank.', (string) $violations->get(0)->getMessage());
+    $this->assertSame("The '' config does not exist.", (string) $violations->get(1)->getMessage());
   }
 
 }
