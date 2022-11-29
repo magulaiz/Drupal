48346f63d31b3c550862d96dc4ee239b7249487e
Phéna Proxima <adam@phenaproxima.net>
2022-11-29 13:26:32 -0500

    cs for real this time

--- core/modules/comment/tests/src/Kernel/CommentTypeValidationTest.php
+++ core/modules/comment/tests/src/Kernel/CommentTypeValidationTest.php
@@ -7,7 +7,7 @@ use Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase;
 
 /**
  * Tests validation of comment_type entities.
- * 
+ *
  * @group comment
  */
 class CommentTypeValidationTest extends ConfigEntityValidationTestBase {
