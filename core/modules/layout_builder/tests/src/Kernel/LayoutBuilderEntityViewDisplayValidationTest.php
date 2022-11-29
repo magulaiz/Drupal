efc70ca79eb65794bdc4f89116c23b957fbcbf9e
Phéna Proxima <adam@phenaproxima.net>
2022-11-29 11:55:20 -0500

    Test LB entity view displays

---
+++ core/modules/layout_builder/tests/src/Kernel/LayoutBuilderEntityViewDisplayValidationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Tests\layout_builder\Kernel;
+
+use Drupal\Core\Entity\Entity\EntityViewMode;
+use Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase;
+use Drupal\layout_builder\Entity\LayoutBuilderEntityViewDisplay;
+
+/**
+ * Tests validation of Layout Builder's entity_view_display entities.
+ *
+ * @group layout_builder
+ */
+class LayoutBuilderEntityViewDisplayValidationTest extends ConfigEntityValidationTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['layout_builder', 'user'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $mode = EntityViewMode::create([
+      'id' => 'user.layout',
+      'label' => 'Layout',
+      'targetEntityType' => 'user',
+    ]);
+    $mode->save();
+
+    $this->entity = LayoutBuilderEntityViewDisplay::create([
+      'mode' => 'layout',
+      'label' => 'Layout',
+      'targetEntityType' => 'user',
+      'bundle' => 'user',
+    ]);
+    $this->entity->save();
+  }
+
+}
