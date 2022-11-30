5cd3d14239cf00c5a7231ce5d67bb0759c1d43f8
Wim Leers <wim.leers@acquia.com>
2022-11-30 15:30:52 +0100

    `StringParts` constraint.

---
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/StringPartsConstraintValidator.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Core\Validation\Plugin\Validation\Constraint;
+
+use Drupal\Core\Config\Schema\Mapping;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+/**
+ * Validates the StringParts constraint.
+ */
+class StringPartsConstraintValidator extends ConstraintValidator {
+
+  use TreeAwareConstraintTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate(mixed $value, Constraint $constraint) {
+    if (!is_string($value)) {
+      throw new UnexpectedTypeException($value, 'string');
+    }
+
+    // Find the parent mapping.
+    $mapping = $this->getParentProperty();
+    // If it's not a Mapping, it's a logical error in the config schema, not the
+    // concrete config.
+    if (!$mapping instanceof Mapping) {
+      throw new \LogicException('This constraint can only be set on `type: mapping`.');
+    }
+
+    // Verify the required parts are present; if not, that's a logical error in
+    // the config schema, not in concrete config.
+    $elements = $mapping->getElements();
+    $missing_elements = array_diff($constraint->parts, array_keys($elements));
+    if (!empty($missing_elements)) {
+      throw new \LogicException(sprintf('This validation constraint is configured to inspect the mapping elements %s, but some do not exist: %s.',
+        implode(', ', $constraint->parts),
+        implode(', ', $missing_elements)
+      ));
+    }
+
+    // Retrieve the parts of the expected string.
+    $expected_string_parts = [];
+    foreach ($constraint->parts as $part) {
+      $part_value = $elements[$part]->getValue();
+      if (!is_string($part_value)) {
+        throw new \LogicException();
+      }
+      $expected_string_parts[] = $part_value;
+    }
+    $expected_string = implode($constraint->separator, $expected_string_parts);
+
+    if ($expected_string !== $value) {
+      $expected_format = implode(
+        $constraint->separator,
+        array_map(function (string $v) {
+          return sprintf('<%s>', $v);
+        }, $constraint->parts)
+      );
+      $this->context->addViolation($constraint->message, [
+        '@value' => $value,
+        '@expected_string' => $expected_string,
+        '@expected_format' => $expected_format,
+      ]);
+    }
+  }
+
+}
---
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/TreeAwareConstraintTrait.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Core\Validation\Plugin\Validation\Constraint;
+
+use Drupal\Core\Config\Schema\ArrayElement;
+use Drupal\Core\Config\Schema\Element;
+
+/**
+ * Helper methods for tree-aware validation constraints.
+ */
+trait TreeAwareConstraintTrait {
+
+  /**
+   * Find the parent property.
+   *
+   * @return \Drupal\Core\Config\Schema\Element
+   *   The parent property.
+   */
+  private function getParentProperty(): Element {
+    $parent_property_path = array_slice(explode('.', $this->context->getPropertyPath()), 0, -1);
+    return self::findPropertyForPath($this->context->getRoot(), $parent_property_path);
+  }
+
+  /**
+   * Finds the specified property path in the given tree.
+   *
+   * @todo consider adopting Symfony's PropertyAccess component.
+   *
+   * @param \Drupal\Core\Config\Schema\ArrayElement $tree
+   *   A config schema (sub)tree.
+   * @param string[] $property_path
+   *   A property path, in array form.
+   *
+   * @return \Drupal\Core\Config\Schema\Element
+   *   The element found at the specified property path.
+   *
+   * @throws \OutOfRangeException
+   *   When requesting a non-existent property path.
+   */
+  private static function findPropertyForPath(ArrayElement $tree, array $property_path): Element {
+    // Edge case: root is requested.
+    if (empty($property_path)) {
+      return $tree;
+    }
+
+    $elements = $tree->getElements();
+    $name = array_shift($property_path);
+
+    // Check if it exists.
+    if (!isset($elements[$name])) {
+      throw new \OutOfRangeException();
+    }
+
+    // If it does, and there are no more parts of the property path, we're done.
+    if (empty($parts)) {
+      return $elements[$name];
+    }
+    // Otherwise, recurse.
+    else {
+      return self::findPropertyForPath($elements[$name], $parts);
+    }
+  }
+
+}
