f756506e68a3e0b4a2bc9e5d922aefc6608c91dd
Wim Leers <wim.leers@acquia.com>
2022-11-30 17:30:28 +0100

    `MachineName` constraint.

---
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/MachineNameConstraint.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Core\Validation\Plugin\Validation\Constraint;
+
+/**
+ * @Constraint(
+ *   id = "MachineName",
+ *   label = @Translation("A valid machine name", context = "Validation")
+ * )
+ *
+ * Note: this does not check uniqueness, only the format. Add an additional
+ * constraint to ensure unique machine names; how to do that depends on the
+ * configuration. For config entities, the `UniqueField` is usually possible.
+ *
+ * @see \Drupal\Core\Validation\Plugin\Validation\Constraint\UniqueFieldConstraint
+ */
+class MachineNameConstraint extends RegexConstraint {
+
+  /**
+   * {@inheritdoc}
+   */
+  public $message = 'Invalid machine name.';
+
+  /**
+   * The pattern to use.
+   *
+   * @var string
+   *
+   * @see \Drupal\Core\Render\Element\MachineName::validateMachineName
+   */
+  const PATTERN = '/^[a-z0-9_]+$/';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array|string|null $pattern, string $message = NULL, string $htmlPattern = NULL, bool $match = NULL, callable $normalizer = NULL, array $groups = NULL, mixed $payload = NULL, array $options = []) {
+    parent::__construct(self::PATTERN, $message, $htmlPattern, $match, $normalizer, $groups, $payload, $options);
+  }
+
+
+}
