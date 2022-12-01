8aedf4617bb9104160ccfae8e6c8a321212de549
Carly Gerard <gerardc@wwu.edu>
2022-12-01 13:09:56 -0800

    set #grouping_label_elm if uses function returns true

--- core/modules/views/src/Plugin/views/style/StylePluginBase.php
+++ core/modules/views/src/Plugin/views/style/StylePluginBase.php
@@ -542,7 +542,7 @@ abstract class StylePluginBase extends PluginBase {
       }
 
       $single_output['#grouping_level'] = $level;
-      if ($this->options['grouping']) {
+      if ($this->usesGroupingLabelElement()) {
         $single_output['#grouping_label_element'] = $this->options['grouping'][$level]['grouping_label_element'];
       }
       else {
