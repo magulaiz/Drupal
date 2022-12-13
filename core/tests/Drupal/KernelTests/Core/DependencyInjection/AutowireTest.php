2f4e0a94fd76517b8a81984c0a7da592c7900d46
Dave Long <dave@longwaveconsulting.com>
2022-12-13 22:30:39 +0000

    Ignore abstract services in AutowireTest.

--- core/tests/Drupal/KernelTests/Core/DependencyInjection/AutowireTest.php
+++ core/tests/Drupal/KernelTests/Core/DependencyInjection/AutowireTest.php
@@ -48,7 +48,7 @@ class AutowireTest extends KernelTestBase {
       if (is_string($service)) {
         $aliases[$id] = substr($service, 1);
       }
-      elseif (isset($service['class']) && class_exists($service['class'])) {
+      elseif (isset($service['class']) && class_exists($service['class']) && empty($service['abstract'])) {
         // Ignore certain tagged services.
         if (isset($service['tags'])) {
           foreach ($service['tags'] as $tag) {
