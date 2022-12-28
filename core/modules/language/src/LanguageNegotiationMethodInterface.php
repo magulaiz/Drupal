1cb042df9f433510291024be7001e2c5e2f842bc
xjm <xjm@65776.no-reply.drupal.org>
2022-12-28 05:10:07 -0600

    Clean up coding standards and fix typo.

--- core/modules/language/src/LanguageNegotiationMethodInterface.php
+++ core/modules/language/src/LanguageNegotiationMethodInterface.php
@@ -39,19 +39,20 @@ interface LanguageNegotiationMethodInterface {
 
   /**
    * Performs language negotiation.
-   * 
-   * @todo Determine whether string|false or string|null should be the normalized
-   *   result across all implementations and update @result and its comment
-   *   accordingly.
-   * @see https://www.drupal.org/node/3329952
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   (optional) The current request. Defaults to NULL if it has not been
    *   initialized yet.
    *
    * @return string|null|false
-   *   A valid language code if the negotiation was successful and either NULL or
-   *   FALSE otherwise.
+   *   A valid language code if the negotiation was successful and either NULL
+   *    or FALSE otherwise.
+   *
+   * @todo Determine whether string|false or string|null should be the
+   *   normalized result across all implementations and update the @return and
+   *   its comment accordingly.
+   *
+   * @see https://www.drupal.org/node/3329952
    */
   public function getLangcode(Request $request = NULL);
 
