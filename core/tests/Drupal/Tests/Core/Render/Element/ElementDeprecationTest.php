a4c9d7f48a8c3aa84b2824370ef0a2f7607ed3df
Jess <45385-xjm@users.noreply.drupalcode.org>
2022-12-29 04:50:17 +0000

    Updated Comments.

--- core/tests/Drupal/Tests/Core/Render/Element/ElementDeprecationTest.php
+++ core/tests/Drupal/Tests/Core/Render/Element/ElementDeprecationTest.php
@@ -6,7 +6,7 @@ use Drupal\Core\Render\Element;
 use Drupal\Tests\UnitTestCase;
 
 /**
- * Element Deprecation Test class.
+ * Tests the deprecation of global rendering functions.
  *
  * @group Render
  * @group legacy
@@ -16,26 +16,26 @@ class ElementDeprecationTest extends UnitTestCase {
   /**
    * {@inheritdoc}
    */
-  public function setUp():void {
+  public function setUp(): void {
     parent::setUp();
     include_once $this->root . '/core/includes/common.inc';
   }
 
   /**
-   * Tests deprecation of show() function.
+   * Tests the deprecation of the global show() function.
    */
-  public function testShowDeprecation() {
-    $this->expectDeprecation('show() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::show() instead. See https://www.drupal.org/node/3261271');
+  public function testShowDeprecation(): void {
+    $this->expectDeprecation('The global show() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::show() instead. See https://www.drupal.org/node/3261271');
     $element1 = [];
     $element2 = [];
     $this->assertEquals(show($element1), Element::show($element2));
   }
 
   /**
-   * Tests deprecation of hide() function.
+   * Tests the deprecation of the global hide() function.
    */
-  public function testHideDeprecation() {
-    $this->expectDeprecation('hide() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::hide() instead. See https://www.drupal.org/node/3261271');
+  public function testHideDeprecation(): void {
+    $this->expectDeprecation('The global hide() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::hide() instead. See https://www.drupal.org/node/3261271');
     $element1 = [];
     $element2 = [];
     $this->assertEquals(hide($element1), Element::hide($element2));
