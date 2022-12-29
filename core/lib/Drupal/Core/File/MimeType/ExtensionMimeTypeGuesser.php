9e6455cfb5ed9f03424eb6149f83a0dddc352fe6
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2022-12-29 21:04:33 +0530

    2311679: Add Deprecation messages.

--- core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
+++ core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
@@ -120,6 +120,8 @@ class ExtensionMimeTypeGuesser implements MimeTypeGuesserInterface {
    * @see https://www.drupal.org/project/drupal/issues/2311679
    */
   public function setMapping(array $mapping = NULL) {
+    @trigger_error(__CLASS__ . '::setMapping() is deprecated in drupal:10.1.0, and will be removed in drupal:11.0.0. Use \Drupal\Core\File\MimeType\MimeTypeMapper::setMapping() instead. See https://www.drupal.org/project/drupal/issues/2311679.', E_USER_DEPRECATED);
+    $this->mapper->setMapping($mapping);
     $this->mapping = $mapping;
   }
 
