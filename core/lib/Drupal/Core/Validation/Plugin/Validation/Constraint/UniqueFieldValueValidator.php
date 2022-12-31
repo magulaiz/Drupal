9ab60e635e19363818dd18b7489ebd766dc29559
Wim Leers <wim.leers@acquia.com>
2022-12-31 12:48:28 +0100

    Generalize `UniqueFieldValueValidator` to work for both content and config entity types.

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/UniqueFieldValueValidator.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/UniqueFieldValueValidator.php
@@ -7,19 +7,18 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Validates that a field is unique for the given entity type.
+ *
+ * Supports top-level properties on both content and config entity types.
  */
 class UniqueFieldValueValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
    */
-  public function validate($items, Constraint $constraint) {
-    if (!$item = $items->first()) {
-      return;
-    }
-    $field_name = $items->getFieldDefinition()->getName();
+  public function validate($items_or_value, Constraint $constraint) {
+    $field_name = $this->context->getPropertyPath();
     /** @var \Drupal\Core\Entity\EntityInterface $entity */
-    $entity = $items->getEntity();
+    $entity = $this->context->getRoot()->getValue();
     $entity_type_id = $entity->getEntityTypeId();
     $id_key = $entity->getEntityType()->getKey('id');
 
@@ -33,17 +32,20 @@ class UniqueFieldValueValidator extends ConstraintValidator {
       $query->condition($id_key, $entity_id, '<>');
     }
 
+    $value = is_array($items_or_value) ? $items_or_value->first()->value : $items_or_value;
     $value_taken = (bool) $query
-      ->condition($field_name, $item->value)
+      ->condition($field_name, $value)
       ->range(0, 1)
       ->count()
       ->execute();
 
     if ($value_taken) {
       $this->context->addViolation($constraint->message, [
-        '%value' => $item->value,
+        '%value' => $value,
         '@entity_type' => $entity->getEntityType()->getSingularLabel(),
-        '@field_name' => $items->getFieldDefinition()->getLabel(),
+        '@field_name' => is_array($items_or_value)
+          ? $items_or_value->getFieldDefinition()->getLabel()
+          : $field_name,
       ]);
     }
   }
