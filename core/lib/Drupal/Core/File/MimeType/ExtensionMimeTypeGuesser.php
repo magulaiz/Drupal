ec5b28285c86fb15d7757b311a1d8b82c55ec95d
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2023-01-02 20:18:20 +0530

    Issue #2311679 : Deprecation Test Added.

--- core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
+++ core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
@@ -921,9 +921,9 @@ class ExtensionMimeTypeGuesser implements MimeTypeGuesserInterface {
     ModuleHandlerInterface $module_handler,
     MimeTypeMapperInterface $mapper = NULL
   ) {
-    if (!$mapper) {
+    if ($mapper === NULL) {
       @trigger_error(
-        'The file.mime_type.mapper service must be passed to ExtensionMimeTypeGuesser::__construct(), it is required before drupal:11.0.0.',
+        'Calling ' . __METHOD__ . '() without the $mapper argument is deprecated in drupal:10.1.0 and will be required before drupal:11.0.0. See https://www.drupal.org/node/2311679.',
         E_USER_DEPRECATED
       );
       $mapper = \Drupal::service('file.mime_type.mapper');
