d69c40b307ae7b0086031e0ad28ca0001ed25d08
Spokje <Spokje@3386132.no-reply.drupal.org>
2023-01-02 14:54:51 +0100

    Wut?

--- core/lib/Drupal/Core/PageCache/ChainRequestPolicy.php
+++ core/lib/Drupal/Core/PageCache/ChainRequestPolicy.php
@@ -41,7 +41,7 @@ class ChainRequestPolicy implements ChainRequestPolicyInterface {
       elseif ($result === static::ALLOW) {
         $final_result = $result;
       }
-      else {
+      elseif (isset($result)) {
         throw new \UnexpectedValueException('Return value of RequestPolicyInterface::check() must be one of RequestPolicyInterface::ALLOW, RequestPolicyInterface::DENY or NULL');
       }
     }
