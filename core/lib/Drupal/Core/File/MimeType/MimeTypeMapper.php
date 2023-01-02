4a26b630a8e4ac3fac3a4bed320411a19bbe4d15
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2023-01-02 19:20:46 +0530

    Issue #2311679 by mondrake, Arla, ParisLiakos, yogeshmpawar, pcambra, Bhanu951: Separate MIME type mapping from ExtensionMimeTypeGuesser

--- core/lib/Drupal/Core/File/MimeType/MimeTypeMapper.php
+++ core/lib/Drupal/Core/File/MimeType/MimeTypeMapper.php
@@ -866,9 +866,13 @@ class MimeTypeMapper implements MimeTypeMapperInterface {
 
 
   /**
-   * The MIME types mapping array after going through the module handler.
+   * MIME extension mapping.
+   *
+   * This is the runtime mapping array, altered by
+   * hook_file_mimetype_mapping_alter or by calling the class' methods.
    *
    * @var array
+   *   Array of mimetypes correlated to the extensions that relate to them.
    */
   protected $mapping;
 
@@ -878,16 +882,6 @@ class MimeTypeMapper implements MimeTypeMapperInterface {
    * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
   protected $moduleHandler;
-
-  /**
-   * Constructs a new ExtensionMimeTypeGuesser.
-   *
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   */
-  public function __construct(ModuleHandlerInterface $module_handler) {
-    $this->moduleHandler = $module_handler;
-  }
 
   /**
    * {@inheritdoc}
@@ -997,42 +991,17 @@ class MimeTypeMapper implements MimeTypeMapperInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets the default mimetypes/extension mapping.
+   *
+   * @return array
+   *   The default mimetypes/extension mapping array.
+   *
+   * @internal
+   *
+   * @todo supports BC for ExtensionMimeTypeGuesser. Remove in Drupal 11.0.0.
    */
-  public function guessMimeType($path): ?string {
-    if ($this->mapping === NULL) {
-      $mapping = $this->defaultMapping;
-      // Allow modules to alter the default mapping.
-      $this->moduleHandler->alter('file_mimetype_mapping', $mapping);
-      $this->mapping = $mapping;
-    }
-
-    $extension = '';
-    $file_parts = explode('.', \Drupal::service('file_system')->basename($path));
-
-    // Remove the first part: a full filename should not match an extension.
-    array_shift($file_parts);
-
-    // Iterate over the file parts, trying to find a match.
-    // For my.awesome.image.jpeg, we try:
-    // - jpeg
-    // - image.jpeg, and
-    // - awesome.image.jpeg.
-    while ($additional_part = array_pop($file_parts)) {
-      $extension = strtolower($additional_part . ($extension ? '.' . $extension : ''));
-      if (isset($this->mapping['extensions'][$extension])) {
-        return $this->mapping['mimetypes'][$this->mapping['extensions'][$extension]];
-      }
-    }
-
-    return 'application/octet-stream';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isGuesserSupported(): bool {
-    return TRUE;
+  public function getDefaultMapping() {
+    return static::$defaultMapping;
   }
 
 }
