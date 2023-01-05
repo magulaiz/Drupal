88ab2cf113a735ddc9b9987a4e98f9d1618b67ee
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2023-01-05 21:47:38 +0530

    Issue #2422443 : Update baseline.

--- core/modules/comment/tests/src/Unit/CommentStatisticsUnitTest.php
+++ core/modules/comment/tests/src/Unit/CommentStatisticsUnitTest.php
@@ -3,8 +3,10 @@
 namespace Drupal\Tests\comment\Unit;
 
 use Drupal\comment\CommentStatistics;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Tests\UnitTestCase;
+use Drupal\user\EntityOwnerInterface;
+
+// cspell:ignore testcomment
 
 /**
  * @coversDefaultClass \Drupal\comment\CommentStatistics
@@ -27,11 +29,67 @@ class CommentStatisticsUnitTest extends UnitTestCase {
   protected $select;
 
   /**
+   * Mock insert object.
+   *
+   * @var \Drupal\Core\Database\Query\Insert
+   */
+  protected $insert;
+
+  /**
    * Mock database connection.
    *
    * @var \Drupal\Core\Database\Connection
    */
   protected $database;
+
+  /**
+   * Mock account interface for the user calling CommentStatistics methods.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * User ID returned by the mock user object. Should be set by tests.
+   *
+   * @var int
+   */
+  protected $currentUserId;
+
+  /**
+   * Values to be added to the mock database row store.
+   *
+   * @var array
+   */
+  protected $stagedInsertValues;
+
+  /**
+   * Mock database row store for comment_statistics table.
+   *
+   * @var array
+   */
+  protected $mockDatabaseStorage;
+
+  /**
+   * Mock entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * Mock state service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
 
   /**
    * CommentStatistics service under test.
@@ -45,12 +103,18 @@ class CommentStatisticsUnitTest extends UnitTestCase {
    *
    * @var int
    */
-  protected $calls_to_fetch;
+  protected $callsToFetch;
 
   /**
    * Sets up required mocks and the CommentStatistics service under test.
    */
   protected function setUp(): void {
+
+    $this->database = $this->getMockBuilder('Drupal\Core\Database\Connection')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    // Set up mock objects for database select.
     $this->statement = $this->getMockBuilder('Drupal\sqlite\Driver\Database\sqlite\Statement')
       ->disableOriginalConstructor()
       ->getMock();
@@ -75,15 +139,52 @@ class CommentStatisticsUnitTest extends UnitTestCase {
       ->method('execute')
       ->willReturn($this->statement);
 
-    $this->database = $this->getMockBuilder('Drupal\Core\Database\Connection')
+    $this->database->expects($this->any())
+      ->method('select')
+      ->will($this->returnValue($this->select));
+
+    // Set up mock objects for database insert.
+    $this->insert = $this->getMockBuilder('Drupal\Core\Database\Query\Insert')
       ->disableOriginalConstructor()
       ->getMock();
 
-    $this->database->expects($this->once())
-      ->method('select')
-      ->willReturn($this->select);
+    $this->insert->expects($this->any())
+      ->method('fields')
+      ->will($this->returnSelf());
 
-    $this->commentStatistics = new CommentStatistics($this->database, $this->createMock('Drupal\Core\Session\AccountInterface'), $this->createMock(EntityTypeManagerInterface::class), $this->createMock('Drupal\Core\State\StateInterface'), $this->database);
+    $this->insert->expects($this->any())
+      ->method('values')
+      ->will($this->returnCallback([$this, 'stageValuesCallback']));
+
+    $this->insert->expects($this->any())
+      ->method('execute')
+      ->will($this->returnCallback([$this, 'updateValuesCallback']));
+
+    $this->database->expects($this->any())
+      ->method('insert')
+      ->will($this->returnValue($this->insert));
+
+    // Set up mock account which can be told what ID it has.
+    $this->currentUser = $this->getMockBuilder('Drupal\Core\Session\AccountInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $this->currentUser->expects($this->any())
+      ->method('id')
+      ->will($this->returnCallback([$this, 'currentUserIdCallback']));
+
+    $this->entityTypeManager = $this->getMockBuilder('Drupal\Core\Entity\EntityTypeManagerInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->state = $this->getMockBuilder('Drupal\Core\State\StateInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->time = $this->getMockBuilder('Drupal\Component\Datetime\TimeInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->commentStatistics = new CommentStatistics($this->database, $this->currentUser, $this->entityTypeManager, $this->state, NULL, $this->time);
   }
 
   /**
@@ -95,9 +196,118 @@ class CommentStatisticsUnitTest extends UnitTestCase {
    * @group Comment
    */
   public function testRead() {
-    $this->calls_to_fetch = 0;
+    $this->callsToFetch = 0;
     $results = $this->commentStatistics->read(['1' => 'boo', '2' => 'foo'], 'snafus');
     $this->assertEquals(['something', 'something-else'], $results);
+  }
+
+  /**
+   * Tests the create method, by a mock class with EntityOwnerInterface.
+   *
+   * @see \Drupal\comment\CommentStatistics::create()
+   *
+   * @group Drupal
+   * @group Comment
+   */
+  public function testCreateEntityWithOwnerInterface() {
+    // Test creating with different user IDs: 0, 1, 2.
+    // The entity IDs are random. We increment them but probably don't need to.
+    $entity_id = 233;
+    foreach ([0, 1, 2] as $uid) {
+      $entity = $this->getMockBuilder('Drupal\node\Entity\Node')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+      if ($this->createEntity($entity, 'node', ++$entity_id, $uid)) {
+        // Statistics should get the entity's owner id as last_comment_uid.
+        $this->assertEquals($uid, $this->mockDatabaseStorage["node:$entity_id:field_testcomment"]['last_comment_uid']);
+      }
+    }
+  }
+
+  /**
+   * Tests the create method, by a mock class without EntityOwnerInterface.
+   *
+   * @see \Drupal\comment\CommentStatistics::create()
+   *
+   * @group Drupal
+   * @group Comment
+   */
+  public function testCreateEntityWithoutOwnerInterface() {
+    // Initialize 'current user' value to use while inserting statistics record.
+    $this->currentUserId = 5;
+    // Test creating with different user IDs: 0, 1, 2.
+    // The entity IDs are random. We increment them but probably don't need to.
+    $entity_id = 344;
+    foreach ([0, 1, 2] as $uid) {
+      $entity = $this->getMockBuilder('Drupal\user\UserInterface')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+      if ($this->createEntity($entity, 'user', ++$entity_id, $uid)) {
+        // Statistics should get current user id as last_comment_uid.
+        $this->assertEquals(5, $this->mockDatabaseStorage["user:$entity_id:field_testcomment"]['last_comment_uid']);
+      }
+    }
+  }
+
+  /**
+   * Inserts a mock statistics row for an entity.
+   *
+   * @param object $entity
+   *   Entity mock object.
+   * @param string $type
+   *   Entity type.
+   * @param int $id
+   *   Entity ID.
+   * @param int $uid
+   *   Entity owner ID.
+   *
+   * @return bool
+   *   TRUE if no tests failed inside this method.
+   *
+   * @see \Drupal\comment\CommentStatistics::create()
+   *
+   * @group Drupal
+   * @group Comment
+   */
+  protected function createEntity($entity, $type, $id, $uid) {
+
+    // Assign various values. OwnerID is for the test; others are unimportant.
+    $entity->expects($this->any())
+      ->method('getEntityTypeId')
+      ->will($this->returnValue($type));
+
+    if ($entity instanceof EntityOwnerInterface) {
+      $entity->expects($this->any())
+        ->method('getOwnerId')
+        ->will($this->returnValue($uid));
+    }
+
+    $entity->expects($this->any())
+      ->method('id')
+      ->will($this->returnValue($id));
+
+    $entity->expects($this->any())
+      ->method('getChangedTime')
+      ->will($this->returnValue(time()));
+
+    // Every field we pass to create, will 'be present in $entity'.
+    $entity->expects($this->any())
+      ->method('hasField')
+      ->will($this->returnValue(TRUE));
+
+    // We assume $fields values are not used; the create() call signature is
+    // like it is, only because it mirrors hook_entity_storage_load().
+    $fields = ['field_testcomment' => NULL];
+    $this->commentStatistics->create($entity, $fields);
+
+    if (!isset($this->mockDatabaseStorage["$type:$id:field_testcomment"]['last_comment_uid'])) {
+      $this->fail("No statistics record or last comment ID was stored for $type:$id:field_testcomment.");
+      return FALSE;
+    }
+
+    return TRUE;
   }
 
   /**
@@ -108,8 +318,8 @@ class CommentStatisticsUnitTest extends UnitTestCase {
    *   other calls to function.
    */
   public function fetchObjectCallback() {
-    $this->calls_to_fetch++;
-    switch ($this->calls_to_fetch) {
+    $this->callsToFetch++;
+    switch ($this->callsToFetch) {
       case 1:
         return 'something';
 
@@ -119,6 +329,44 @@ class CommentStatisticsUnitTest extends UnitTestCase {
       default:
         return FALSE;
     }
+  }
+
+  /**
+   * Return value callback for values() function on mocked Insert.
+   *
+   * @return \Drupal\Core\Database\Query\Insert
+   *   The mocked Insert object on which values() was called.
+   */
+  public function stageValuesCallback($values) {
+    $this->stagedInsertValues = $values;
+    // Return 'self'.
+    return $this->insert;
+  }
+
+  /**
+   * Return value callback for execute() function on mocked Insert object.
+   *
+   * @return int
+   *   Zero. (An invalid 'insert id'. It's expected not to be used.)
+   */
+  public function updateValuesCallback() {
+    // This method just assumes it's always being called immediately after
+    // another 'stage values' callback, that all required values are present,
+    // and that an insert won't try to set duplicate rows.
+    $vals = $this->stagedInsertValues;
+    $pkey = "$vals[entity_type]:$vals[entity_id]:$vals[field_name]";
+    $this->mockDatabaseStorage[$pkey] = $vals;
+    return 0;
+  }
+
+  /**
+   * Return value callback for id() function on mocked User.
+   *
+   * @return int
+   *   A user ID, which should be set beforehand by a test method as needed.
+   */
+  public function currentUserIdCallback() {
+    return isset($this->currentUserId) ? $this->currentUserId : 999;
   }
 
 }
