b4816791b76f5fddb8492eee6505aab374334ff0
Wim Leers <wim.leers@acquia.com>
2023-01-11 10:49:10 +0100

    @larowlan review: `static` return type.

--- core/lib/Drupal/Core/Config/Plugin/Validation/Constraint/ImmutableFieldsConstraintValidator.php
+++ core/lib/Drupal/Core/Config/Plugin/Validation/Constraint/ImmutableFieldsConstraintValidator.php
@@ -31,7 +31,7 @@ class ImmutableFieldsConstraintValidator extends ConstraintValidator implements 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container): static {
     return new static(
       $container->get('entity_type.manager')
     );
