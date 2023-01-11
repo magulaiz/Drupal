9e2221d0f59693f2fafd7d8da5b542ee6403c6b9
Phéna Proxima <adam@phenaproxima.net>
2023-01-11 13:47:29 -0500

    validate contact form IDs

--- core/modules/contact/tests/src/Kernel/ContactFormValidationTest.php
+++ core/modules/contact/tests/src/Kernel/ContactFormValidationTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\contact\Kernel;
 
 use Drupal\contact\Entity\ContactForm;
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase;
 
 /**
@@ -28,6 +29,25 @@ class ContactFormValidationTest extends ConfigEntityValidationTestBase {
       'label' => 'Test',
     ]);
     $this->entity->save();
+  }
+
+  /**
+   * Tests that the content form's ID is validated as a machine name.
+   *
+   * @param string $invalid_id
+   *   An invalid machine name that should raise a validation error.
+   *
+   * @testWith ["invalid name"]
+   *  ["invalid-name"]
+   *  ["Invalid_Name"]
+   */
+  public function testMachineName(string $invalid_id): void {
+    $this->entity->set('id', $invalid_id);
+    $this->assertValidationErrors(['This value is not valid.']);
+
+    $max_length = EntityTypeInterface::BUNDLE_MAX_LENGTH;
+    $this->entity->set('id', mb_strtolower($this->randomMachineName($max_length + 2)));
+    $this->assertValidationErrors(['This value is too long. It should have <em class="placeholder">' . $max_length . '</em> characters or less.']);
   }
 
 }
