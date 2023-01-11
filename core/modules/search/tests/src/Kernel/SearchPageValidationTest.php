0cd595567a487fd7aaad681b0b63ba48f9d123ce
Phéna Proxima <adam@phenaproxima.net>
2023-01-11 16:19:22 -0500

    validate search page IDs

--- core/modules/search/tests/src/Kernel/SearchPageValidationTest.php
+++ core/modules/search/tests/src/Kernel/SearchPageValidationTest.php
@@ -31,4 +31,22 @@ class SearchPageValidationTest extends ConfigEntityValidationTestBase {
     $this->entity->save();
   }
 
+  /**
+   * Tests that the search page's ID is validated as a machine name.
+   *
+   * @param string $invalid_id
+   *   An invalid machine name that should raise a validation error.
+   *
+   * @testWith ["invalid name"]
+   *  ["invalid-name"]
+   *  ["Invalid_Name"]
+   */
+  public function testMachineName(string $invalid_id): void {
+    $this->entity->set('id', $invalid_id);
+    $this->assertValidationErrors(['This value is not valid.']);
+
+    $this->entity->set('id', mb_strtolower($this->randomMachineName(68)));
+    $this->assertValidationErrors(['This value is too long. It should have <em class="placeholder">64</em> characters or less.']);
+  }
+
 }
