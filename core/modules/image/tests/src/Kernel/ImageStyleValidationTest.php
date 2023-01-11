ee59d43dc8450b43cc411d9bbd5a5c238f1a45a7
Phéna Proxima <adam@phenaproxima.net>
2023-01-11 14:16:26 -0500

    Validate image style IDs

--- core/modules/image/tests/src/Kernel/ImageStyleValidationTest.php
+++ core/modules/image/tests/src/Kernel/ImageStyleValidationTest.php
@@ -30,4 +30,22 @@ class ImageStyleValidationTest extends ConfigEntityValidationTestBase {
     $this->entity->save();
   }
 
+  /**
+   * Tests that the action entity's ID is validated as a machine name.
+   *
+   * @param string $invalid_id
+   *   An invalid machine name that should raise a validation error.
+   *
+   * @testWith ["invalid name"]
+   *  ["invalid-name"]
+   *  ["Invalid_Name"]
+   */
+  public function testMachineName(string $invalid_id): void {
+    $this->entity->set('name', $invalid_id);
+    $this->assertValidationErrors(['This value is not valid.']);
+
+    $this->entity->set('name', mb_strtolower($this->randomMachineName(68)));
+    $this->assertValidationErrors(['This value is too long. It should have <em class="placeholder">64</em> characters or less.']);
+  }
+
 }
