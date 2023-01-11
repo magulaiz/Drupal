96f137b3d9ebdb5b87b338842eff36d623993d42
Phéna Proxima <adam@phenaproxima.net>
2023-01-11 12:42:13 -0500

    Use data providers

--- core/modules/system/tests/src/Kernel/Entity/ActionValidationTest.php
+++ core/modules/system/tests/src/Kernel/Entity/ActionValidationTest.php
@@ -28,13 +28,32 @@ class ActionValidationTest extends ConfigEntityValidationTestBase {
   }
 
   /**
-   * Tests that the action entity's ID is validated as a machine name.
+   * Data provider for ::testMachineName().
+   *
+   * @return array[]
+   *   The test cases.
    */
-  public function testMachineName(): void {
+  public function providerMachineName(): array {
+    return [
+      ['invalid name'],
+      ['invalid-name'],
+      ['Invalid_Name'],
+    ];
+  }
+
+  /**
+   * Tests that the action entity's ID is validated as a machine name.
+   *
+   * @param string $invalid_id
+   *   An invalid machine name that should raise a validation error.
+   *
+   * @dataProvider providerMachineName
+   */
+  public function testMachineName(string $invalid_id): void {
     // The entity should be valid to begin with.
     $this->assertValidationErrors([]);
 
-    $this->entity->set('id', 'invalid name');
+    $this->entity->set('id', $invalid_id);
     $this->assertValidationErrors(['This value is not valid.']);
 
     $this->entity->set('id', mb_strtolower($this->randomMachineName(68)));
