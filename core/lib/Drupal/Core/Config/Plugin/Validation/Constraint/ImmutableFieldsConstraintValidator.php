0c47622e7283a2d29202c5e75a749480cf0c6711
Wim Leers <wim.leers@acquia.com>
2023-01-11 10:48:42 +0100

    @larowlan review: use constructor promotion.

--- core/lib/Drupal/Core/Config/Plugin/Validation/Constraint/ImmutableFieldsConstraintValidator.php
+++ core/lib/Drupal/Core/Config/Plugin/Validation/Constraint/ImmutableFieldsConstraintValidator.php
@@ -20,20 +20,12 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
 class ImmutableFieldsConstraintValidator extends ConstraintValidator implements ContainerInjectionInterface {
 
   /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected EntityTypeManagerInterface $entityTypeManager;
-
-  /**
    * Constructs an ImmutableFieldsConstraintValidator object.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   The entity type manager service.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
-    $this->entityTypeManager = $entity_type_manager;
+  public function __construct(protected EntityTypeManagerInterface $entityTypeManager) {
   }
 
   /**
