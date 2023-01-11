e782478ddd0af8e5c07e1ded0672950e140516b6
Phéna Proxima <adam@phenaproxima.net>
2023-01-11 16:22:33 -0500

    validate shortcut set IDs

--- core/modules/shortcut/tests/src/Kernel/ShortcutSetValidationTest.php
+++ core/modules/shortcut/tests/src/Kernel/ShortcutSetValidationTest.php
@@ -26,10 +26,31 @@ class ShortcutSetValidationTest extends ConfigEntityValidationTestBase {
     $this->installEntitySchema('shortcut');
 
     $this->entity = ShortcutSet::create([
-      'id' => 'test',
+      'id' => 'test-shortcut-set',
       'label' => 'Test',
     ]);
     $this->entity->save();
+  }
+
+  /**
+   * Tests that the shortcut set's ID is validated as a machine name.
+   *
+   * @param string $invalid_id
+   *   An invalid machine name that should raise a validation error.
+   *
+   * @testWith ["invalid name"]
+   *  ["invalid_name"]
+   *  ["Invalid-Name"]
+   */
+  public function testMachineName(string $invalid_id): void {
+    $this->entity->set('id', $invalid_id);
+    $this->assertValidationErrors(['This value is not valid.']);
+
+    $max_length = 23;
+    $this->entity->set('id', mb_strtolower($this->randomMachineName($max_length + 2)));
+    $this->assertValidationErrors([
+      'This value is too long. It should have <em class="placeholder">' . $max_length . '</em> characters or less.',
+    ]);
   }
 
 }
