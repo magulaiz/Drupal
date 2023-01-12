27c4bd50c50eafec9e8f0f58a3131e63cc36efec
Fran Garcia-Linares <fjgarlin@gmail.com>
2023-01-12 10:56:53 +0100

    Adding test.

---
+++ core/modules/comment/tests/src/Functional/CommentEntityReferenceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Tests\comment\Functional;
+
+use Drupal\comment\Entity\Comment;
+use Drupal\Tests\field\Traits\EntityReferenceTestTrait;
+
+/**
+ * Tests that comments behave correctly when added as entity references.
+ *
+ * @group comment
+ */
+class CommentEntityReferenceTest extends CommentTestBase {
+
+  use EntityReferenceTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that comments are correctly saved as entity references.
+   */
+  public function testCommentAsEntityReference() {
+    $this->drupalLogin($this->webUser);
+    $comment = $this->postComment($this->node, $this->randomMachineName(), $this->randomMachineName());
+    $this->assertInstanceOf(Comment::class, $comment);
+
+    $this->createEntityReferenceField(
+      'node',
+      'article',
+      'entity_reference_comment',
+      'Entity Reference Comment',
+      'comment'
+    );
+
+    $this->node->set('entity_reference_comment', $comment->id())->save();
+    $this->assertNotEmpty($this->node->get('entity_reference_comment'), 'Reference field is saved.');
+    $this->assertEquals($this->node->get('entity_reference_comment')[0]['target_id'], $comment->id(), 'Reference field is saved.');
+  }
+
+}
