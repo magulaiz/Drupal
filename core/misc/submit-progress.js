235c685ae64eb1d21b8aa236b16c486d473c820b
Lauri Eskola <lauri.eskola@acquia.com>
2023-01-16 13:23:17 +0200

    Fix submission

--- core/misc/submit-progress.js
+++ core/misc/submit-progress.js
@@ -1,28 +1,16 @@
 ((Drupal, once, { focusable }) => {
-  console.log('hi buddy');
   Drupal.behaviors.submitButtonProgress = {
     attach(context) {
       once('submit-buttons', '[data-progress-message]', context).forEach(
         (button) => {
           const parentForm = button.form;
-          button.addEventListener('click', (e) => {
-            return;
-            // @todo making any changes within the form here seems to prevent
-            // the form from submitting.
-            // If I open a modal dialog instead of modifying form contents, the
-            // form will submit.
-            // eslint-disable-next-line no-unreachable
+          parentForm.addEventListener('submit', (e) => {
             button.disabled = true;
             const throbber = Drupal.theme(
               'ajaxProgressThrobber',
               button.getAttribute('data-progress-message'),
             );
             button.parentNode.innerHTML += throbber;
-            if (parentForm) {
-              focusable(parentForm).forEach((focusableElement) => {
-                // focusableElement.disabled = true;
-              });
-            }
           });
         },
       );
