0f0762666f7043dfe3dbd08552e7af979d9c3366
Marios Anagnostopoulos <55973-MariosAnagnostopoulos@users.noreply.drupalcode.org>
2023-01-19 15:21:06 +0000

    Removes redundant check, since getBaseFieldDefinition can not return null.

--- core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
+++ core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
@@ -144,8 +144,7 @@ class BaseFieldOverride extends FieldConfigBase {
    * {@inheritdoc}
    */
   public function isInternal() {
-    $base_field_definition = $this->getBaseFieldDefinition();
-    return $base_field_definition ? $base_field_definition->isInternal() : NULL;
+    $base_field_definition = $this->getBaseFieldDefinition()->isInternal();
   }
 
   /**
