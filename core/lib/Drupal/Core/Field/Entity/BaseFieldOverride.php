magulaiz/Drupal

commit 0f0762666f7043dfe3dbd08552e7af979d9c3366

Removes redundant check, since getBaseFieldDefinition can not return null.

core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
@@ -144,8 +144,7 @@ class BaseFieldOverride extends FieldConfigBase {
    * {@inheritdoc}
    */
   public function isInternal() {
-    $base_field_definition = $this->getBaseFieldDefinition();
-    return $base_field_definition ? $base_field_definition->isInternal() : NULL;
+    $base_field_definition = $this->getBaseFieldDefinition()->isInternal();
   }
 
   /**

commit 7d009986048a9af582dfab1d66f685a30a04ff55

Fixes issue introduced in 0f0762666f7043dfe3dbd08552e7af979d9c3366

core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
@@ -144,7 +144,7 @@ class BaseFieldOverride extends FieldConfigBase {
    * {@inheritdoc}
    */
   public function isInternal() {
-    $base_field_definition = $this->getBaseFieldDefinition()->isInternal();
+    return $this->getBaseFieldDefinition()->isInternal();
   }
 
   /**