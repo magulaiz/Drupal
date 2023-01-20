9e3cfc7f82c85ebf2c83d9798cd0bda448351eb1
Brad Jones <brad@bradjonesllc.com>
2023-01-19 19:05:10 -0700

    Update docblock

--- core/modules/user/src/EntityOwnerInterface.php
+++ core/modules/user/src/EntityOwnerInterface.php
@@ -15,8 +15,8 @@ interface EntityOwnerInterface {
   /**
    * Returns the entity owner's user entity.
    *
-   * @return \Drupal\user\UserInterface
-   *   The owner user entity.
+   * @return \Drupal\user\UserInterface|null
+   *   The owner user entity, or null if the user could not be loaded.
    */
   public function getOwner();
 
