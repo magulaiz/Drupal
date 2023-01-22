f66751f2c41b7dc658bcd9285566b0c90d9d30cc
Spokje <Spokje@3386132.no-reply.drupal.org>
2023-01-22 16:30:51 +0100

    Deprecate in constructor

--- core/lib/Drupal/Core/Database/Driver/sqlite/Statement.php
+++ core/lib/Drupal/Core/Database/Driver/sqlite/Statement.php
@@ -3,6 +3,7 @@
 namespace Drupal\Core\Database\Driver\sqlite;
 
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\StatementPrefetch;
 use Drupal\sqlite\Driver\Database\sqlite\Statement as SqliteStatement;
 
 /**
@@ -20,7 +21,7 @@ class Statement extends SqliteStatement {
    */
   public function __construct(\PDO $pdo_connection, Connection $connection, $query, array $driver_options = [], bool $row_count_enabled = FALSE) {
     @trigger_error('\Drupal\Core\Database\Driver\sqlite\Statement is deprecated in drupal:9.4.0 and is removed from drupal:11.0.0. The SQLite database driver has been moved to the sqlite module. See https://www.drupal.org/node/3129492', E_USER_DEPRECATED);
-    parent::__construct();
+    StatementPrefetch::__construct($pdo_connection, $connection, $query, $driver_options, $row_count_enabled);
   }
 
 }
