3f448c7ffffebb9916591597e739e9a89fcdfd4f
Jess <45385-xjm@users.noreply.drupalcode.org>
2023-01-24 22:24:09 +0000

    Make changes suggested by maintainer

--- core/modules/book/src/BookManagerInterface.php
+++ core/modules/book/src/BookManagerInterface.php
@@ -180,14 +180,16 @@ interface BookManagerInterface {
   public function updateOutline(NodeInterface $node);
 
   /**
-   * Saves a link of a single book entry.
+   * Saves a link for a single book entry to the book.
    *
    * @param array $link
    *   The link data to save. $link['nid'] must be set. Other keys in this array
-   *   get default values from ::getLinkDefaults(). The array keys available to
-   *   be set are documented in BookOutlineStorageInterface::loadMultiple().
+   *   get default values from
+   *   \Drupal\book\BookManagerInterface::getLinkDefaults(). The array keys
+   *   available to be set are documented in
+   *   \Drupal\book\BookOutlineStorageInterface::loadMultiple().
    * @param bool $new
-   *   Is this a new link.
+   *   Whether this is a link to a new book entry.
    *
    * @return array
    *   The book entry link information.
