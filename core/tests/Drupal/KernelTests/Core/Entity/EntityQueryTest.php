f064c2bc392a02ab8ff098c3cb06d756c5995d90
Lucas Cruz <lucassc@ciandt.com>
2023-01-25 11:11:19 -0300

    Issue #2950869 by amateescu, alexpott, Fabianx, abhishek-anand, ravi.shankar, adam.weingarten, Timmy_Cos, Rashmisoni, bryanmanalo, pooja saraah, _pratik_, corneboele, matsbla, Wim Leers, mheip, mariancalinro, effulgentsia, Krzysztof Domański, lucassc: Entity queries querying the latest revision very slow with lots of revisions

--- core/tests/Drupal/KernelTests/Core/Entity/EntityQueryTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityQueryTest.php
@@ -377,6 +377,30 @@ class EntityQueryTest extends EntityKernelTestBase {
     $assert = [4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9', 10 => '10', 11 => '11', 12 => '12', 20 => '12', 13 => '13', 21 => '13', 14 => '14', 22 => '14', 15 => '15', 23 => '15'];
     $this->assertSame($assert, $results);
 
+    $results = $this->queryResults = $this->storage
+      ->getQuery()
+      ->latestRevision()
+      ->notExists("$figures.color")
+      ->accessCheck(TRUE)
+      ->execute();
+    $expected = [16 => '4', 8 => '8', 20 => '12'];
+    $this->assertSame($expected, $results);
+
+    // Update an entity.
+    $entity = EntityTestMulRev::load(4);
+    $entity->setNewRevision();
+    $entity->$figures->color = 'red';
+    $entity->save();
+
+    $results = $this->queryResults = $this->storage
+      ->getQuery()
+      ->latestRevision()
+      ->notExists("$figures.color")
+      ->accessCheck(TRUE)
+      ->execute();
+    $expected = [8 => '8', 20 => '12'];
+    $this->assertSame($expected, $results);
+
     // Check that a query on the latest revisions without any condition returns
     // the correct results.
     $results = $this->storage
