44847364a17d2063ed3a62abb46e51b98d34b3ed
xjm <xjm@65776.no-reply.drupal.org>
2023-01-26 16:06:23 -0600

    Issue #2574981 by Manjit.Singh, dimaro, wengerk, darol100, izus, pguillard, vsujeetkumar, Himanshu5050, snehi, paulocs, rakesh.gectcr, jofitz, _utsavsharma, charginghawk, rajeshwari10, sdstyles, quietone, benjifisher, marcusvsouza, LoMo, joelpittet, justAChris, cilefen, YesCT, mradcliffe, spitzialist, annajl, jhodgdon, catch, alexpott, Sabbi0612, kattekrab, Mile23, antojose, leanderl, rodrigoac: Fix grammar ("an URL" should be "a URL") and consistent use of URL (not "url" / "Url") in documentation only

--- core/tests/Drupal/Tests/Component/Utility/UrlHelperTest.php
+++ core/tests/Drupal/Tests/Component/Utility/UrlHelperTest.php
@@ -82,7 +82,7 @@ class UrlHelperTest extends TestCase {
    * @covers ::isValid
    *
    * @param string $url
-   *   The url to test.
+   *   The URL to test.
    * @param string $scheme
    *   The scheme to test.
    */
@@ -137,7 +137,7 @@ class UrlHelperTest extends TestCase {
    * @covers ::isValid
    *
    * @param string $url
-   *   The url to test.
+   *   The URL to test.
    * @param string $scheme
    *   The scheme to test.
    */
@@ -171,7 +171,7 @@ class UrlHelperTest extends TestCase {
    * @covers ::isValid
    *
    * @param string $url
-   *   The url to test.
+   *   The URL to test.
    * @param string $prefix
    *   The prefix to test.
    */
@@ -202,7 +202,7 @@ class UrlHelperTest extends TestCase {
    * @covers ::isValid
    *
    * @param string $url
-   *   The url to test.
+   *   The URL to test.
    * @param string $prefix
    *   The prefix to test.
    */
@@ -254,7 +254,7 @@ class UrlHelperTest extends TestCase {
   }
 
   /**
-   * Tests url parsing.
+   * Tests URL parsing.
    *
    * @dataProvider providerTestParse
    * @covers ::parse
@@ -537,7 +537,7 @@ class UrlHelperTest extends TestCase {
   }
 
   /**
-   * Tests dangerous url protocol filtering.
+   * Tests dangerous URL protocol filtering.
    *
    * @dataProvider providerTestStripDangerousProtocols
    * @covers ::setAllowedProtocols
@@ -619,12 +619,12 @@ class UrlHelperTest extends TestCase {
    * Tests detecting external urls that point to local resources.
    *
    * @param string $url
-   *   The external url to test.
+   *   The external URL to test.
    * @param string $base_url
-   *   The base url.
+   *   The base URL.
    * @param bool $expected
    *   TRUE if an external URL points to this installation as determined by the
-   *   base url.
+   *   base URL.
    *
    * @covers ::externalIsLocal
    * @dataProvider providerTestExternalIsLocal
@@ -634,7 +634,7 @@ class UrlHelperTest extends TestCase {
   }
 
   /**
-   * Provider for local external url detection.
+   * Provider for local external URL detection.
    *
    * @see \Drupal\Tests\Component\Utility\UrlHelperTest::testExternalIsLocal()
    */
@@ -671,12 +671,12 @@ class UrlHelperTest extends TestCase {
   }
 
   /**
-   * Tests invalid url arguments.
+   * TTests invalid URL arguments.
    *
    * @param string $url
-   *   The url to test.
+   *   The URL to test.
    * @param string $base_url
-   *   The base url.
+   *   The base URL.
    *
    * @covers ::externalIsLocal
    * @dataProvider providerTestExternalIsLocalInvalid
@@ -687,7 +687,7 @@ class UrlHelperTest extends TestCase {
   }
 
   /**
-   * Provides invalid argument data for local external url detection.
+   * Provides invalid argument data for local external URL detection.
    *
    * @see \Drupal\Tests\Component\Utility\UrlHelperTest::testExternalIsLocalInvalid()
    */
