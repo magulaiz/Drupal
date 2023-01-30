b432e9efde42daa363f7ac78bc99cea3de0a60a5
Oleh Vehera <44159-voleger@users.noreply.drupalcode.org>
2023-01-30 03:47:17 +0000

    Deprecation Messages Updated.

--- core/lib/Drupal/Core/Form/EventSubscriber/FormAjaxSubscriber.php
+++ core/lib/Drupal/Core/Form/EventSubscriber/FormAjaxSubscriber.php
@@ -152,7 +152,7 @@ class FormAjaxSubscriber implements EventSubscriberInterface {
    * @return string
    *   The formatted size.
    *
-   * @deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Use
+   * @deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use
    *   \Drupal\Core\StringTranslation\ByteSizeMarkup::create() instead.
    *
    * @see https://www.drupal.org/node/2999981
--- core/tests/Drupal/KernelTests/Core/Common/SizeTest.php
+++ core/tests/Drupal/KernelTests/Core/Common/SizeTest.php
@@ -16,7 +16,7 @@ class SizeTest extends KernelTestBase {
    * Tests deprecation of the format_size() function.
    */
   public function testFormatSizeDeprecation(): void {
-    $this->expectDeprecation('format_size() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Use \Drupal\Core\StringTranslation\ByteSizeMarkup::create($size, $langcode) instead. See https://www.drupal.org/node/2999981');
+    $this->expectDeprecation('format_size() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\StringTranslation\ByteSizeMarkup::create($size, $langcode) instead. See https://www.drupal.org/node/2999981');
     $size = format_size(4053371676);
     $this->assertEquals('3.77 GB', $size);
     $this->assertEquals('@size GB', $size->getUntranslatedString());
