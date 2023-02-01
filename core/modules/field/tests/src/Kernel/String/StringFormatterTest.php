1f155c08c08c425370c07ae7953c3ee959834432
dpi <pro@danielph.in>
2023-02-01 19:08:58 +0800

    fix test: needs table installed for access check

--- core/modules/field/tests/src/Kernel/String/StringFormatterTest.php
+++ core/modules/field/tests/src/Kernel/String/StringFormatterTest.php
@@ -198,6 +198,7 @@ class StringFormatterTest extends KernelTestBase {
    */
   public function testLinkToContentForEntitiesWithNoCanonicalPath() {
     $this->enableModules(['entity_test']);
+    $this->installEntitySchema('entity_test_label');
     $field_name = 'test_field_name';
     $entity_type = $bundle = 'entity_test_label';
 
