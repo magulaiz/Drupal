3bb3b3f8f80c417aee7366d3635a231ce38338b4
Pucci, Murilo henrique [GTSBR NON-J&J] <mpucci@its.jnj.com>
2023-02-05 13:45:46 -0300

    Fix legacy test.

--- core/modules/system/tests/src/Kernel/Entity/EntityControllerLegacyTest.php
+++ core/modules/system/tests/src/Kernel/Entity/EntityControllerLegacyTest.php
@@ -23,7 +23,7 @@ class EntityControllerLegacyTest extends KernelTestBase {
    */
   public function testOptionalParameterDeprecation(): void {
 
-    $this->expectDeprecation('Calling Drupal\Core\Entity\Controller\EntityController::__construct() without the $route_match argument is deprecated in drupal:10.1.0 and it will be required in drupal:11.0.0. See https://www.drupal.org/node/2897251');
+    $this->expectDeprecation('Calling Drupal\Core\Entity\Controller\EntityController::__construct() without the $route_match argument is deprecated in drupal:10.1.0 and it will be required in drupal:11.0.0. See https://www.drupal.org/node/3337782');
 
     new EntityController(
       $this->container->get('entity_type.manager'),
