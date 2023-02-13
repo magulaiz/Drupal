507aae6a4270d779fe12b03a416c0fe628b103cb
Phéna Proxima <adam@phenaproxima.net>
2023-02-13 14:52:05 -0500

    Test all machine names against underscores

--- core/modules/system/tests/src/Kernel/Entity/MenuValidationTest.php
+++ core/modules/system/tests/src/Kernel/Entity/MenuValidationTest.php
@@ -25,15 +25,4 @@ class MenuValidationTest extends ConfigEntityValidationTestBase {
     $this->entity->save();
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function providerInvalidMachineNameCharacters(): array {
-    $data = parent::providerInvalidMachineNameCharacters();
-    // Menu machine names allow dashes, but not underscores.
-    unset($data['dash separated']);
-    $data['underscore separated'] = ['invalid_name'];
-    return $data;
-  }
-
 }
