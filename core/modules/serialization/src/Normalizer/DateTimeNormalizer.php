a0cfa3427ba0eee97860bae0487ffe6096831f25
Sourabh Jain <sourabh.j@valuebound.com>
2023-02-20 13:58:08 +0530

    Rerolled patch with latest changes.

--- core/modules/serialization/src/Normalizer/DateTimeNormalizer.php
+++ core/modules/serialization/src/Normalizer/DateTimeNormalizer.php
@@ -88,7 +88,7 @@ class DateTimeNormalizer extends NormalizerBase implements DenormalizerInterface
   public function denormalize($data, $class, $format = NULL, array $context = []): mixed {
     // This only knows how to denormalize datetime strings and timestamps. If
     // something else is received, let validation constraints handle this.
-    if (!is_string($data) && !is_numeric($data)) {
+    if (!is_string($data) && !is_numeric($data) || empty($data)) {
       return $data;
     }
 
