07fba89a0c616b2ef9ba033ebce73885883e0bba
Jonathan Shaw <20711-jonathanjfshaw@users.noreply.drupalcode.org>
2023-02-21 14:50:56 +0000

    HtmlResponse::setContent needs to convert RenderableElementInterface to string

--- core/lib/Drupal/Core/Render/HtmlResponse.php
+++ core/lib/Drupal/Core/Render/HtmlResponse.php
@@ -5,6 +5,7 @@ namespace Drupal\Core\Render;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Cache\CacheableResponseInterface;
 use Drupal\Core\Cache\CacheableResponseTrait;
+use Drupal\Core\Render\RenderableElementInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -54,7 +55,7 @@ class HtmlResponse extends Response implements CacheableResponseInterface, Attac
   public function setContent($content): static {
     // A render array can automatically be converted to a string and set the
     // necessary metadata.
-    if (is_array($content) && (isset($content['#markup']))) {
+    if ((is_array($content) || $content instanceof RenderableElementInterface) && (isset($content['#markup']))) {
       $content += [
         '#attached' => [
           'html_response_attachment_placeholders' => [],
