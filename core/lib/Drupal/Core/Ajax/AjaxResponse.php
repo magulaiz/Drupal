7fc35d414b722e5e4a6e667db8ffd7fbf7a87e86
Eashika Naznin <60945-eashika@users.noreply.drupalcode.org>
2023-02-22 21:01:02 +0000

    Update AjaxResponse.php

--- core/lib/Drupal/Core/Ajax/AjaxResponse.php
+++ core/lib/Drupal/Core/Ajax/AjaxResponse.php
@@ -69,7 +69,7 @@ class AjaxResponse extends JsonResponse implements AttachmentsInterface {
    * Merges ajax commands.
    *
    * @param array $commands
-   * Merges ajax command(s) with the current one(s) in this class.
+   *   Merges ajax command(s) with the current one(s) in this class.
    */
   public function setCommands(array $commands) {
     $this->commands = array_merge($this->commands, $commands);
