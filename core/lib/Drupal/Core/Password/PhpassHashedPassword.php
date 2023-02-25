21c6382b6f2fc7b8032cea37dedb9e03e64f26a9
znerol <znerol@63999.no-reply.drupal.org>
2023-02-25 19:35:28 +0100

    Move legacy password implementation to Drupal\Core\Password\LegacyPassword

--- core/lib/Drupal/Core/Password/PhpassHashedPassword.php
+++ core/lib/Drupal/Core/Password/PhpassHashedPassword.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Core\Password;
 
-use Drupal\phpass\Password\PhpassHashedPassword as PasswordCompatibility;
-
 @trigger_error('\Drupal\Core\Password\PhpassHashedPassword is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. The password compatibility service has been moved to the phpass module. Use \Drupal\phpass\Password\PhpassHashedPassword instead. See https://www.drupal.org/node/3322420', E_USER_DEPRECATED);
 
 /**
@@ -15,4 +13,4 @@ use Drupal\phpass\Password\PhpassHashedPassword as PasswordCompatibility;
  *
  * @see https://www.drupal.org/node/3322420
  */
-class PhpassHashedPassword extends PasswordCompatibility {}
+class PhpassHashedPassword extends LegacyPassword {}
