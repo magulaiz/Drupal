5e0ecacee910e72ac6853e0c965d74dddaafa3eb
Dezső BICZÓ <dezso.biczo@pronovix.com>
2023-02-27 15:56:12 +0100

    Add failing test

--- core/tests/Drupal/Tests/Core/DependencyInjection/DependencySerializationTest.php
+++ core/tests/Drupal/Tests/Core/DependencyInjection/DependencySerializationTest.php
@@ -39,7 +39,7 @@ class DependencySerializationTest extends UnitTestCase {
     $dependencySerialization = unserialize($string);
 
     $this->assertTrue($container->has(ReverseContainer::class));
-    $this->assertSame($service, $dependencySerialization->service);
+    $this->assertSame($service, $dependencySerialization->getService());
     $this->assertSame($container, $dependencySerialization->container);
     $this->assertEmpty($dependencySerialization->getServiceIds());
   }
@@ -47,18 +47,11 @@ class DependencySerializationTest extends UnitTestCase {
 }
 
 /**
- * Defines a test class which has a single service as dependency.
+ * Defines a base test class for dependency serialization testing.
  */
-class DependencySerializationTestDummy implements ContainerAwareInterface {
+abstract class DependencySerializationTestDummyBase implements ContainerAwareInterface {
 
   use DependencySerializationTrait;
-
-  /**
-   * A test service.
-   *
-   * @var object
-   */
-  public $service;
 
   /**
    * The container.
@@ -66,16 +59,6 @@ class DependencySerializationTestDummy implements ContainerAwareInterface {
    * @var \Symfony\Component\DependencyInjection\ContainerInterface
    */
   public $container;
-
-  /**
-   * Constructs a new TestClass object.
-   *
-   * @param object $service
-   *   A test service.
-   */
-  public function __construct(\stdClass $service) {
-    $this->service = $service;
-  }
 
   /**
    * {@inheritdoc}
@@ -92,6 +75,39 @@ class DependencySerializationTestDummy implements ContainerAwareInterface {
    */
   public function getServiceIds() {
     return $this->_serviceIds;
+  }
+
+}
+
+/**
+ * Defines a test class which has a single service as dependency.
+ */
+class DependencySerializationTestDummy extends DependencySerializationTestDummyBase {
+  /**
+   * A test service.
+   *
+   * @var object
+   */
+  private $service;
+
+  /**
+   * Constructs a new TestClass object.
+   *
+   * @param object $service
+   *   A test service.
+   */
+  public function __construct(\stdClass $service) {
+    $this->service = $service;
+  }
+
+  /**
+   * Returns the service.
+   *
+   * @return object
+   *   The service.
+   */
+  public function getService() {
+    return $this->service;
   }
 
 }
