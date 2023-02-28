1ad610ae1f0ace99b25876d1aaa854a8467e2431
Stephen Mustgrave <smustgrave@gmail.com>
2023-02-27 19:45:20 -0500

    BlockType admin perm

--- core/modules/block_content/src/Entity/BlockContentType.php
+++ core/modules/block_content/src/Entity/BlockContentType.php
@@ -32,7 +32,7 @@ use Drupal\block_content\BlockContentTypeInterface;
  *     },
  *     "list_builder" = "Drupal\block_content\BlockContentTypeListBuilder"
  *   },
- *   admin_permission = "administer block library",
+ *   admin_permission = "administer block types",
  *   config_prefix = "type",
  *   bundle_of = "block_content",
  *   entity_keys = {
