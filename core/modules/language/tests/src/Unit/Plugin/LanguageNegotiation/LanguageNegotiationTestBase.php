34563448042d0da2d1390be015ca1309a7bd12db
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2023-03-01 14:07:13 +0000

    Switch to using an base class instead of a trait

--- core/modules/language/tests/src/Unit/Plugin/LanguageNegotiation/LanguageNegotiationTestBase.php
+++ core/modules/language/tests/src/Unit/Plugin/LanguageNegotiation/LanguageNegotiationTestBase.php
@@ -2,12 +2,14 @@
 
 namespace Drupal\Tests\language\Unit\Plugin\LanguageNegotiation;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Tests\UnitTestCase;
 
 /**
- * Provides a class for creating plugins via factory.
+ * Base class used for testing the various LanguageNegotiation plugins.
+ *
+ * @group language
  */
-trait LanguageNegotiationFactoryTrait {
+abstract class LanguageNegotiationTestBase extends UnitTestCase {
 
   /**
    * Returns the plugin class to use for creating the language negotiation plugin.
@@ -16,13 +18,13 @@ trait LanguageNegotiationFactoryTrait {
    *   The plugin class name.
    */
   abstract protected function getPluginClass(): string;
-  
+
   /**
    * Creates a @LanguageNegotiation plugin using the factory ::create method.
    *
    * @return \Drupal\language\LanguageNegotiationMethodInterface
    */
-  private function createLanguageNegotiationPlugin(array $configuration = [], $plugin_definition = NULL) {
+  protected function createLanguageNegotiationPlugin(array $configuration = [], $plugin_definition = NULL) {
     $this->assertTrue(in_array(ContainerFactoryPluginInterface::class, class_implements($this->getPluginClass())));
     return $this->getPluginClass()::create(\Drupal::getContainer(), $configuration, $this->getPluginClass()::METHOD_ID, $plugin_definition);
   }
