d2edaf3fc27304bc4d6a8173c2c67a7173d392fc
Andy Postnikov <apostnikov@gmail.com>
2023-03-08 15:26:40 +0100

    Address comment 92

--- core/lib/Drupal/Core/Render/Element.php
+++ core/lib/Drupal/Core/Render/Element.php
@@ -229,15 +229,15 @@ class Element {
   /**
    * Shows a hidden element for later rendering.
    *
-   * You can also use render($element), which shows the element while rendering
-   * it.
+   * You can also use RendererInterface::render($element), which shows
+   * the element while rendering it.
    *
    * The first time RendererInterface::render() is called on an element tree,
    * as each element in the tree is rendered, it is marked with a #printed flag
    * and the rendered children of the element are cached. Subsequent calls to
    * RendererInterface::render() will not traverse the child tree of this element
    * again: they will just use the cached children. So if you want to show an
-   * element, be sure to call element::show() on the element before its parent
+   * element, be sure to call Element::show() on the element before its parent
    * tree is rendered for the first time, as it will have no effect on
    * subsequent renderings of the parent tree.
    *
