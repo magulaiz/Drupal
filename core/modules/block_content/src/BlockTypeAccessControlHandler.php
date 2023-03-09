c3d9297bdbeea3300950e1629e6d01b39283bbcc
Stephen Mustgrave <smustgrave@gmail.com>
2023-03-08 21:24:41 -0500

    Missing type label

---
+++ core/modules/block_content/src/BlockTypeAccessControlHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\block_content;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Defines the access control handler for the "Block Type" entity type.
+ *
+ * @see \Drupal\block_content\Entity\BlockContentType
+ */
+class BlockTypeAccessControlHandler extends EntityAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $viewLabelOperation = TRUE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    if ($operation === 'view label') {
+      return AccessResult::allowedIfHasPermission($account, 'access block library');
+    }
+    else {
+      return parent::checkAccess($entity, $operation, $account);
+    }
+  }
+
+}
