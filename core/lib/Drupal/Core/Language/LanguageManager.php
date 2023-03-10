92c51af8e4d117205e2a680e8c50669e4cb2ec4e
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2023-03-10 00:07:42 +0000

    As there is only one language in LanguageManager, it cannot be changed. This should be a noop function.

--- core/lib/Drupal/Core/Language/LanguageManager.php
+++ core/lib/Drupal/Core/Language/LanguageManager.php
@@ -112,10 +112,7 @@ class LanguageManager implements LanguageManagerInterface {
    * {@inheritdoc}
    */
   public function setCurrentLanguage(LanguageInterface $language, $type = LanguageInterface::TYPE_INTERFACE): LanguageInterface {
-    $current_language = $this->getCurrentLanguage($type);
-    $this->defaultLanguage->set($language);
-
-    return $current_language;
+    return $this->getDefaultLanguage();
   }
 
   /**
