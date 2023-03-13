ebd4c2fc7416fa7f569326097d22715abe68cae8
Andy Postnikov <apostnikov@gmail.com>
2023-03-13 01:54:28 +0100

    Use to inline instead of replacement

--- core/tests/Drupal/Tests/Core/Render/Element/ProceduralApiDeprecationTest.php
+++ core/tests/Drupal/Tests/Core/Render/Element/ProceduralApiDeprecationTest.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\Tests\Core\Render\Element;
 
-use Drupal\Core\Render\Element;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -24,21 +23,12 @@ class ProceduralApiDeprecationTest extends UnitTestCase {
   /**
    * Tests the deprecation of the global show() function.
    */
-  public function testShowDeprecation(): void {
-    $this->expectDeprecation('The global show() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::show() instead. See https://www.drupal.org/node/3261271');
-    $element1 = [];
-    $element2 = [];
-    $this->assertEquals(show($element1), Element::show($element2));
-  }
-
-  /**
-   * Tests the deprecation of the global hide() function.
-   */
-  public function testHideDeprecation(): void {
-    $this->expectDeprecation('The global hide() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Render\Element::hide() instead. See https://www.drupal.org/node/3261271');
-    $element1 = [];
-    $element2 = [];
-    $this->assertEquals(hide($element1), Element::hide($element2));
+  public function testShowHideDeprecation(): void {
+    $element = [];
+    $this->expectDeprecation('The global hide() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use to inline #printed=TRUE instead. See https://www.drupal.org/node/3261271');
+    $this->assertEquals(['#printed' => TRUE], hide($element));
+    $this->expectDeprecation('The global show() function is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use to inline #printed=FALSE instead. See https://www.drupal.org/node/3261271');
+    $this->assertEquals(['#printed' => FALSE], show($element));
   }
 
 }
