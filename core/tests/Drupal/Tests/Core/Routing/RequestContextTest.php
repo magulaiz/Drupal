32ba93509decc4404d147ca3eccb9417273e8660
Klaus Purer <klaus.purer@protonmail.ch>
2023-03-13 16:44:26 +0100

    Add test case

---
+++ core/tests/Drupal/Tests/Core/Routing/RequestContextTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\Tests\Core\Routing;
+
+use Drupal\Core\Routing\RequestContext;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Routing\RequestContext
+ * @group Routing
+ */
+class RequestContextTest extends UnitTestCase {
+
+  /**
+   * Make sure there is no TypeError when passing an empty RequestStack.
+   *
+   * @covers ::fromRequestStack
+   */
+  public function testFromRequestStack() {
+    $request_context = new RequestContext();
+    $request_stack = new RequestStack();
+    $result = $request_context->fromRequestStack($request_stack);
+    $this->assertNotNull($result);
+  }
+
+}
