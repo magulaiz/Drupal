24520495bc5e4fd7ec273c6399d834df778a9dae
Kristiaan Van den Eynde <kristiaan@factorial.io>
2023-03-17 09:16:19 +0100

    Provide a bit of information on the interface.

--- core/lib/Drupal/Core/Session/PermissionCheckerInterface.php
+++ core/lib/Drupal/Core/Session/PermissionCheckerInterface.php
@@ -5,6 +5,11 @@ namespace Drupal\Core\Session;
 /**
  * Defines a permission checker interface.
  *
+ * This service can be swapped out or decorated by contrib modules to enable
+ * access logic more complex than simply checking permissions on roles. Please
+ * be careful when changing how this service works and provide ample automated
+ * tests when doing so as you may open your website up to security issues.
+ *
  * @ingroup user_api
  */
 interface PermissionCheckerInterface {
