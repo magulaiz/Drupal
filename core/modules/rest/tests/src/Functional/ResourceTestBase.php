04a466b65d49d6bf1da1e3c19887fba4888acfa6
Claudiu Cristea <clau.cristea@gmail.com>
2023-03-19 11:02:46 +0200

    Attempt to fix tests

--- core/modules/rest/tests/src/Functional/ResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/ResourceTestBase.php
@@ -412,14 +412,8 @@ abstract class ResourceTestBase extends BrowserTestBase {
       $this->assertTrue($response->hasHeader('X-Drupal-Cache'));
       $this->assertSame($expected_page_cache_header_value, $response->getHeader('X-Drupal-Cache')[0]);
     }
-    else {
-      if (NULL !== $response->hasHeader('X-Drupal-Cache')) {
-        $this->assertFalse($response->hasHeader('X-Drupal-Cache'));
-      }
-      else {
-        $this->assertTrue($response->hasHeader('X-Drupal-Cache'));
-      }
-      $this->stringStartsWith('0 UNCACHEABLE');
+    elseif ($response->hasHeader('X-Drupal-Cache')) {
+      $this->assertMatchesRegularExpression('#^UNCACHEABLE \((request|response) policy\)$#', $response->getHeader('X-Drupal-Cache')[0]);
     }
 
     // Expected Dynamic Page Cache header value: X-Drupal-Dynamic-Cache header.
@@ -427,17 +421,8 @@ abstract class ResourceTestBase extends BrowserTestBase {
       $this->assertTrue($response->hasHeader('X-Drupal-Dynamic-Cache'));
       $this->assertSame($expected_dynamic_page_cache_header_value, $response->getHeader('X-Drupal-Dynamic-Cache')[0]);
     }
-    else {
-      if ($expected_status_code === 403) {
-        if ($response->hasHeader('X-Drupal-Dynamic-Cache')) {
-          $this->assertTrue($response->hasHeader('X-Drupal-Dynamic-Cache'));
-          $this->stringStartsWith('UNCACHEABLE (', $response->getHeader('X-Drupal-Dynamic-Cache')[0]);
-        }
-      }
-      else {
-        $this->assertFalse($response->hasHeader('X-Drupal-Dynamic-Cache'));
-        $this->stringStartsWith('0 UNCACHEABLE');
-      }
+    elseif ($response->hasHeader('X-Drupal-Dynamic-Cache')) {
+      $this->assertMatchesRegularExpression('#^UNCACHEABLE \((no|poor) cacheability\)$#', $response->getHeader('X-Drupal-Cache')[0]);
     }
   }
 
