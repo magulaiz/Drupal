d5b628d24ed1eb4e0228f6e2679102c9283ac940
Arunkumar Kuppuswamy <arun.kumar1@srijan.net>
2023-03-22 16:26:43 +0530

    Test fail fix

--- core/modules/action/tests/src/Functional/ActionListTest.php
+++ core/modules/action/tests/src/Functional/ActionListTest.php
@@ -54,12 +54,17 @@ class ActionListTest extends BrowserTestBase {
   public function testCategoryInActionList() {
     // Create a user with permission to view the actions administration pages.
     $this->drupalLogin($this->drupalCreateUser(['administer actions']));
-    Action::create([
-      'id' => 'action_form_ajax_test',
-      'label' => $this->randomMachineName(),
-      'type' => 'system',
-      'plugin' => 'action_form_ajax_test',
-    ])->save();
+
+    // Make a POST request to admin/config/system/actions.
+    $edit = [];
+    $edit['id'] = 'action_form_ajax_test';
+    $edit['label'] = $this->randomMachineName();
+    $edit['type'] = 'system';
+    $edit['plugin'] = 'action_form_ajax_test';
+    $this->drupalGet('admin/config/system/actions');
+    $this->submitForm($edit, 'Create');
+    $this->assertSession()->statusCodeEquals(200);
+
     $this->drupalGet('/admin/config/system/actions');
     $this->assertSession()->pageTextContains('Test Action');
     $this->assertSession()->elementExists('css', 'optgroup[label="Test Action"] option[value="action_form_ajax_test"]');
