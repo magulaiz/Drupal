18f2c354c3f41d45c096704bee774b87727c193c
Joe Parsons <parsonsj@arizona.edu>
2023-03-23 22:53:00 -0700

    Coding standards fix.

--- core/modules/ckeditor5/tests/src/Functional/MediaEntityMetadataApiTest.php
+++ core/modules/ckeditor5/tests/src/Functional/MediaEntityMetadataApiTest.php
@@ -128,7 +128,7 @@ class MediaEntityMetadataApiTest extends BrowserTestBase {
               'view_mode_2' => 'view_mode_2',
             ],
             'allowed_media_types' => [],
-            'default_view_mode_10101'=> 'view_mode_1',
+            'default_view_mode_10101' => 'view_mode_1',
           ],
         ],
       ],
