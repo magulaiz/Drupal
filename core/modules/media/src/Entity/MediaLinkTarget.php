390c86a655bb67f56a52c2b2d97dd60163f8d6ed
Wim Leers <wim.leers@acquia.com>
2023-03-28 13:24:36 +0200

    Introduce `\Drupal\media\MediaSourceInterface::METADATA_ATTRIBUTE_LINK_TARGET` and support it in the `File` and `OEmbed` media source plugins.

--- core/modules/media/src/Entity/MediaLinkTarget.php
+++ core/modules/media/src/Entity/MediaLinkTarget.php
@@ -8,7 +8,8 @@ use Drupal\Core\Cache\Cache;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityLinkTargetInterface;
 use Drupal\Core\GeneratedUrl;
-use Drupal\file\FileInterface;
+use Drupal\media\MediaInterface;
+use Drupal\media\MediaSourceInterface;
 
 /**
  * Provides a Media link target handler.
@@ -24,7 +25,14 @@ use Drupal\file\FileInterface;
  * On the other hand, media entities that use the "file" media source plugin can
  * be linked regardless of that setting because the referenced files are still
  * linkable.
+ * Every media source plugin is free to implement the base implementation of
+ * :getMetadata() because a source plugin is guaranteed to know how the media is
+ * stored and linked.
  *
+ * @see \Drupal\media\MediaSourceInterface::METADATA_ATTRIBUTE_LINK_TARGET
+ * @see \Drupal\media\MediaSourceBase::getMetadata()
+ * @see \Drupal\media\Plugin\media\Source\File::getMetadata()
+ * @see \Drupal\media\Plugin\media\Source\OEmbed::getMetadata()
  * @see \Drupal\file\Entity\FileLinkTarget
  */
 class MediaLinkTarget implements EntityLinkTargetInterface {
@@ -33,26 +41,9 @@ class MediaLinkTarget implements EntityLinkTargetInterface {
    * {@inheritdoc}
    */
   public function getLinkTarget(EntityInterface $entity): GeneratedUrl {
-    // Media entities using the "file" media source plugin.
-    // @see \Drupal\media\Plugin\media\Source\File
-    $source_field = $entity->getSource()->getSourceFieldDefinition($entity->get('bundle')->entity);
-    if ($source_field && $entity->hasField($source_field->getName()) && $entity->get($source_field->getName())->entity instanceof FileInterface) {
-      $file = $entity->get($source_field->getName())->entity;
-      // Similar to the File entities special case, but subtly different.
-      $url = $file->createFileUrl(TRUE);
-      assert(is_string($url));
-      return (new GeneratedUrl())
-        ->setGeneratedUrl($url)
-        ->setCacheMaxAge(Cache::PERMANENT)
-        // The subtle but crucial difference compared to File entity.
-        ->addCacheableDependency($file);
-    }
-
-    // Media entities using a media source plugin other than "file" are only
-    // linkable if and only if standalone URLs are enabled: linking to their
-    // edit forms is meaningless.
-    if ($entity->getEntityType()->getLinkTemplate('canonical') !== $entity->getEntityType()->getLinkTemplate('edit-form')) {
-      return $entity->toUrl()->toString(TRUE);
+    assert($entity instanceof MediaInterface);
+    if ($link_target = $entity->getSource()->getMetadata($entity, MediaSourceInterface::METADATA_ATTRIBUTE_LINK_TARGET)) {
+      return $link_target;
     }
 
     // @todo Ensure that in the entity selection plugin logic only file media
