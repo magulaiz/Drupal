2b0f89984b99bd7a1f7f6a815ca1565c1594c2f4
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-03-31 10:24:27 +1000

    Use a lazy builder

--- core/modules/announcements_feed/src/LazyBuilders.php
+++ core/modules/announcements_feed/src/LazyBuilders.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Drupal\announcements_feed;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Messenger\MessengerInterface;
-use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Render\ElementInfoManagerInterface;
+use Drupal\Core\Security\TrustedCallbackInterface;
 use Drupal\Core\Url;
 
 /**
  * Defines a class for lazy building render arrays.
  */
-final class LazyBuilders {
+final class LazyBuilders implements TrustedCallbackInterface {
 
   /**
    * Constructs a new LazyBuilders.
@@ -21,10 +23,13 @@ final class LazyBuilders {
    *   User status service.
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   Messenger service.
+   * @param \Drupal\Core\Render\ElementInfoManagerInterface $elementInfo
+   *   Element info.
    */
   public function __construct(
     protected AnnounceUserStatus $userStatus,
     protected MessengerInterface $messenger,
+    protected ElementInfoManagerInterface $elementInfo,
   ) {
   }
 
@@ -34,7 +39,7 @@ final class LazyBuilders {
    * @return array
    *   Render array.
    */
-  public function renderAnouncements(): array {
+  public function renderAnnouncements(): array {
     $announce_icon = 'announce-default';
 
     // Check for new announcements for the current user.
@@ -49,15 +54,20 @@ final class LazyBuilders {
       $announce_icon = 'announce-new';
     }
 
-    return [
-        '#type' => 'link',
+    $build = [
+      '#type' => 'link',
+      '#cache' => [
+        'context' => ['user'],
+      ],
       '#title' => t('Announcements'),
       '#url' => Url::fromRoute('announcements_feed.announcement'),
+      '#id' => Html::getId('toolbar-item-announcement'),
       '#attributes' => [
         'title' => t('Announcements'),
         'data-drupal-announce-trigger' => '',
         'class' => [
           'toolbar-icon',
+          'toolbar-item',
           'toolbar-icon-announce',
           'use-ajax',
           'announce-canvas-link',
@@ -85,6 +95,18 @@ final class LazyBuilders {
       ],
     ];
 
+    // The renderer has already added element defaults by the time the lazy
+    // builder is run.
+    // @see https://www.drupal.org/project/drupal/issues/2609250
+    $build += $this->elementInfo->getInfo('link');
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks(): array {
+    return ['renderAnnouncements'];
   }
 
 }
