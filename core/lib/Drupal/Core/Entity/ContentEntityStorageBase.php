1ee708fac461806b20b226f7b9c426974b4a7025
Mariano D'Agostino <dagmar@154086.no-reply.drupal.org>
2023-04-01 23:42:48 -0300

    New method to ignore deletion of content when uninstalling dblog

--- core/lib/Drupal/Core/Entity/ContentEntityStorageBase.php
+++ core/lib/Drupal/Core/Entity/ContentEntityStorageBase.php
@@ -1208,4 +1208,11 @@ abstract class ContentEntityStorageBase extends EntityStorageBase implements Con
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function requireDeleteContentBeforeUninstall() : bool {
+    return TRUE;
+  }
+
 }
