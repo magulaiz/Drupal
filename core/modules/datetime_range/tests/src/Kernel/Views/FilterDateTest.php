afe955e2d0a2478b93614d50a6e032a91726a9ec
Sahil Goyal <ketangoyal1042@gmail.com>
2023-04-06 10:18:25 +0530

    updated variable names To fix CCF

--- core/modules/datetime_range/tests/src/Kernel/Views/FilterDateTest.php
+++ core/modules/datetime_range/tests/src/Kernel/Views/FilterDateTest.php
@@ -57,7 +57,7 @@ class FilterDateTest extends DateTimeHandlerTestBase {
     static::$date = $this->getUTCEquivalentOfUserNowAsTimestamp();
 
     // Change field storage to date-only.
-    $storage = FieldStorageConfig::load('node.' . static::$field_name);
+    $storage = FieldStorageConfig::load('node.' . static::$fieldName);
     $storage->setSetting('datetime_type', DateTimeItemInterface::DATETIME_TYPE_DATE);
     $storage->save();
 
