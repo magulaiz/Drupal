d2c8e6bb24eb9b11f3e339bc0d7c35e9c69c65b9
Merlin <merlin@geeks4change.net>
2023-04-09 18:37:07 +0200

    Add phpdoc

--- core/lib/Drupal/Core/Entity/Sql/StorageMapperDelegatorTrait.php
+++ core/lib/Drupal/Core/Entity/Sql/StorageMapperDelegatorTrait.php
@@ -2,10 +2,22 @@
 
 namespace Drupal\Core\Entity\Sql;
 
+/**
+ * Delegate ::mapColumnsOnLoad to the field item class.
+ */
 trait StorageMapperDelegatorTrait {
 
+  /**
+   * Get the field item class.
+   *
+   * @return string
+   *   The field item class.
+   */
   abstract protected function getFieldItemClass();
 
+  /**
+   * {@inheritDoc}
+   */
   public function mapColumnsOnLoad(array $columns): ?array {
     $fieldItemClass = $this->getFieldItemClass();
     if (is_subclass_of($fieldItemClass, FieldItemStorageMapperInterface::class)) {
