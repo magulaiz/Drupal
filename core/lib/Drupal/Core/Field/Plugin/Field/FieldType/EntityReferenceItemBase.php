a2f06debfc3da1606a23bb31ad5ac6997fa38ad1
Adam Bramley <adam.bramley@previousnext.com.au>
2023-04-11 10:27:59 +1000

    Add base class

---
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldType/EntityReferenceItemBase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Core\Field\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+
+/**
+ * Base class for field items referencing other entities.
+ */
+abstract class EntityReferenceItemBase extends FieldItemBase implements EntityReferenceItemInterface {
+
+}
