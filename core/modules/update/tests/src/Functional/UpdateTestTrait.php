d9bbe2ce3d6044de05538f5082e2a07910cb1e2b
omkar podey <omkar.podey@acquia.com>
2023-04-12 14:32:49 +0530

    better comments + removed the trait use statement

--- core/modules/update/tests/src/Functional/UpdateTestTrait.php
+++ core/modules/update/tests/src/Functional/UpdateTestTrait.php
@@ -2,32 +2,54 @@
 
 namespace Drupal\Tests\update\Functional;
 
-use Drupal\Core\Test\TestSetupTrait;
+use Drupal\Core\Config\Config;
 
 /**
- * Provides a trait to set system info and xml mappings.
+ * Provides a trait to set system info and XML mappings.
  */
 trait UpdateTestTrait {
-  use TestSetupTrait;
+
+  /**
+   * The dependency injection container used in the test.
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface
+   */
+  protected $container;
 
   /**
    * Sets system info.
    *
-   * @param array $system_info
+   * It expects information about the installed modules.
+   *
+   * @param string[] $system_info
    *   The system info.
+   *   In the format as the key to be the project name and an array of sub keys
+   *   as value such as 'project' (which is just the project name), 'version',
+   *   'hidden', for example:
+   *   'drupal' => [
+   *     'project' => 'drupal',
+   *     'version' => '8.0.0',
+   *     'hidden' => FALSE,
+   *   ].
    */
   public function setSystemInfo(array $system_info): void {
     $this->config('update_test.settings')->set('system_info', $system_info)->save();
   }
 
   /**
-   * Sets xml mappings.
+   * Sets XML mappings.
    *
-   * @param array $xml_map
-   *   The xml mappings.
+   * The array that maps project names to availability scenarios to
+   * fetch.
+   *
+   * @param string[] $xml_map
+   *   The XML mappings.
+   *   In the format as the key to be the project name and available release as
+   *   its value, for example:
+   *   'aaa_update_test' => '8.x-1.2'.
    */
   public function setXmlMap(array $xml_map): void {
-    $this->config('update_test.settings')->set('system_info', $xml_map)->save();
+    $this->config('update_test.settings')->set('xml_map', $xml_map)->save();
   }
 
   /**
@@ -39,7 +61,7 @@ trait UpdateTestTrait {
    * @return \Drupal\Core\Config\Config
    *   The configuration object with original configuration data.
    */
-  protected function config($name) {
+  protected function config($name): Config {
     return $this->container->get('config.factory')->getEditable($name);
   }
 
