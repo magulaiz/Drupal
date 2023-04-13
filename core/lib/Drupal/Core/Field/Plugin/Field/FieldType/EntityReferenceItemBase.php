945fdbba5c16cceed50ff3929501c8fe85077bc7
Adam Bramley <adam.bramley@previousnext.com.au>
2023-04-13 13:02:41 +1000

    Add more documentation

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldType/EntityReferenceItemBase.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldType/EntityReferenceItemBase.php
@@ -6,6 +6,10 @@ use Drupal\Core\Field\FieldItemBase;
 
 /**
  * Base class for field items referencing other entities.
+ *
+ * Any field type that is an entity reference should extend from this class in
+ * order to remain backwards compatible with any changes added in the future
+ * to EntityReferenceItemInterface.
  */
 abstract class EntityReferenceItemBase extends FieldItemBase implements EntityReferenceItemInterface {
 
