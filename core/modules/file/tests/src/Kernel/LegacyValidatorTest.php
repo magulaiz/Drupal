2aeb02ebd448dc61190c17a16db879ac0ea2d7dd
Olivier Briat <olivier.briat@kleegroup.com>
2023-04-13 16:04:08 +0200

    Add filesize attribut on image object creation.

--- core/modules/file/tests/src/Kernel/LegacyValidatorTest.php
+++ core/modules/file/tests/src/Kernel/LegacyValidatorTest.php
@@ -37,6 +37,7 @@ class LegacyValidatorTest extends FileManagedUnitTestBase {
     /** @var \Drupal\Core\File\FileSystemInterface $file_system */
     $file_system = \Drupal::service('file_system');
     $this->image->setFilename($file_system->basename($this->image->getFileUri()));
+    $this->image->setSize(@filesize($this->image->getFileUri()));
 
     $this->nonImage = File::create();
     $this->nonImage->setFileUri('core/assets/vendor/jquery/jquery.min.js');
