be19326767630ed96edde3962ce3dfa9b6ac7de1
Rolando Payán Mosqueda <rpayanm@gmail.com>
2023-04-20 17:48:22 -0300

    2395797-10.1.x

--- core/modules/toolbar/js/toolbar.js
+++ core/modules/toolbar/js/toolbar.js
@@ -46,10 +46,12 @@
       // Process the administrative toolbar.
       once('toolbar', '#toolbar-administration', context).forEach((toolbar) => {
         // Establish the toolbar models and views.
-        const defaultLock = (options.default_orientation == 'vertical');
-        const userLocked = JSON.parse(localStorage.getItem('Drupal.toolbar.trayVerticalLocked'));
+        const defaultLock = options.default_orientation === 'vertical';
+        const userLocked = JSON.parse(
+          localStorage.getItem('Drupal.toolbar.trayVerticalLocked'),
+        );
         const model = new Drupal.toolbar.ToolbarModel({
-          locked: (userLocked != null) ? userLocked : defaultLock,
+          locked: userLocked != null ? userLocked : defaultLock,
           activeTab: document.getElementById(
             JSON.parse(localStorage.getItem('Drupal.toolbar.activeTabID')),
           ),
--- core/modules/toolbar/src/Form/ToolbarSettingsForm.php
+++ core/modules/toolbar/src/Form/ToolbarSettingsForm.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @file
- * Contains \Drupal\toolbar\Form\ToolbarSettingsForm.
- */
 
 namespace Drupal\toolbar\Form;
 
@@ -18,77 +14,78 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ToolbarSettingsForm extends ConfigFormBase {
 
   /**
-     * The module handler.
-     *
-     * @var \Drupal\Core\Extension\ModuleHandlerInterface
-     */
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
   protected $moduleHandler;
 
   /**
-     * Constructs a \Drupal\user\ToolbarSettingsForm object.
-     *
-     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-     *   The factory for configuration objects.
-     * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-     *   The module handler.
-     */
+   * Constructs a \Drupal\user\ToolbarSettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
   public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $module_handler) {
-        parent::__construct($config_factory);
+    parent::__construct($config_factory);
 
-        $this->moduleHandler = $module_handler;
-      }
+    $this->moduleHandler = $module_handler;
+  }
 
   /**
-     * {@inheritdoc}
-     */
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container) {
-        return new static(
-              $container->get('config.factory'),
+    return new static(
+      $container->get('config.factory'),
       $container->get('module_handler')
-          );
+    );
   }
 
   /**
-     * {@inheritdoc}
-     */
+   * {@inheritdoc}
+   */
   public function getFormId() {
-        return 'toolbar_settings_form';
+    return 'toolbar_settings_form';
   }
 
   /**
-     * {@inheritdoc}
-     */
+   * {@inheritdoc}
+   */
   protected function getEditableConfigNames() {
-        return ['toolbar.settings'];
+    return ['toolbar.settings'];
   }
 
   /**
-     * {@inheritdoc}
-     */
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('toolbar.settings');
+    $config = $this->config('toolbar.settings');
 
-        $form['toolbar_default_orientation'] = [
-            '#type' => 'select',
-            '#title' => t('Toolbar Default Orientation'),
-            '#default_value' => $config->get('toolbar_default_orientation'),
-            '#options' => [
-                'horizontal' => t('Horizontal'),
-                'vertical' => t('Vertical'),
-              ],
-          ];
+    $form['toolbar_default_orientation'] = [
+      '#type' => 'select',
+      '#title' => t('Toolbar Default Orientation'),
+      '#default_value' => $config->get('toolbar_default_orientation'),
+      '#options' => [
+        'horizontal' => t('Horizontal'),
+        'vertical' => t('Vertical'),
+      ],
+    ];
 
-        return parent::buildForm($form, $form_state);
+    return parent::buildForm($form, $form_state);
   }
 
   /**
-     * {@inheritdoc}
-     */
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-        $this->config('toolbar.settings')
-          ->set('toolbar_default_orientation', $form_state->getValue('toolbar_default_orientation'))
-          ->save();
+    $this->config('toolbar.settings')
+      ->set('toolbar_default_orientation', $form_state->getValue('toolbar_default_orientation'))
+      ->save();
 
     parent::submitForm($form, $form_state);
   }
+
 }
