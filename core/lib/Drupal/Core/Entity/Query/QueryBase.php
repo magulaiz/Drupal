2fd7fda3f4cadd867562c26d4abb2a232064d0eb
rohan.sinha <rohan.sinha@srijan.net>
2023-04-24 10:36:31 +0530

    Fixes

--- core/lib/Drupal/Core/Entity/Query/QueryBase.php
+++ core/lib/Drupal/Core/Entity/Query/QueryBase.php
@@ -468,7 +468,7 @@ abstract class QueryBase implements QueryInterface {
   /**
    * Gets a list of namespaces of the ancestors of a class.
    *
-   * This method returns a list of namespaces that includes the namespace of the
+   * Returns a list of namespaces that includes the namespace of the
    * class, as well as the namespaces of its parent class and ancestors. This
    * is useful for locating classes in a hierarchy of namespaces, such as when
    * searching for the appropriate query class for an entity type.
@@ -491,8 +491,7 @@ abstract class QueryBase implements QueryInterface {
   /**
    * Finds a class in a list of namespaces.
    *
-   * This method searches in the order of the array, and
-   * returns the first one it finds.
+   * Searches in the order of the array, and returns the first one it finds.
    *
    * @param array $namespaces
    *   A list of namespaces.
