6dcded14668cfca07f1b660204c101ff61fd5407
Merlin <merlin@geeks4change.net>
2023-04-29 15:30:30 +0200

    See if code is exec at all

--- core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
+++ core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
@@ -1418,6 +1418,7 @@ class SqlContentEntityStorage extends ContentEntityStorageBase implements SqlEnt
               $field_name,
               $storage_definition->mapColumnsOnSave($item_value)
             );
+            dump([$maybe_mapped_columns, $item_value, $item]);
           }
           if (isset($maybe_mapped_columns)) {
             $record += $maybe_mapped_columns;
