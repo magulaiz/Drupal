89ef6243de626e708c0fd5e7a16bd94697b110d0
Dave Long <dave@longwaveconsulting.com>
2023-04-30 16:31:30 +0100

    Fix unused use statement.

--- core/modules/migrate_drupal/src/MigrateDrupalServiceProvider.php
+++ core/modules/migrate_drupal/src/MigrateDrupalServiceProvider.php
@@ -4,7 +4,6 @@ namespace Drupal\migrate_drupal;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
-use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Alters container services.
