56e661375f323edbc6980b45994c7d9e004a2e7d
Dave Long <dave@longwaveconsulting.com>
2023-04-30 16:15:14 +0100

    Fix locale tests.

--- core/modules/locale/tests/src/Kernel/LocaleStringIsSafeTest.php
+++ core/modules/locale/tests/src/Kernel/LocaleStringIsSafeTest.php
@@ -16,7 +16,7 @@ class LocaleStringIsSafeTest extends KernelTestBase {
    *
    * @var array
    */
-  protected static $modules = ['locale', 'locale_test'];
+  protected static $modules = ['language', 'locale', 'locale_test'];
 
   /**
    * Tests for locale_string_is_safe().
