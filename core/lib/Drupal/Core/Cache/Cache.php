007b8f13c4a40cbc8326c45b51169fd4f5ed7da8
quietone <quietone@2572884.no-reply.drupal.org>
2023-05-05 21:19:08 +1200

    Find and fix remaining failures

--- core/lib/Drupal/Core/Cache/Cache.php
+++ core/lib/Drupal/Core/Cache/Cache.php
@@ -148,6 +148,7 @@ class Cache {
    *   A hash of the query arguments.
    */
   public static function keyFromQuery(SelectInterface $query) {
+    // phpcs:ignore Drupal.Semantics.FunctionTriggerError
     @trigger_error(__METHOD__ . ' is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. No replacement provided. https://www.drupal.org/node/3322044', E_USER_DEPRECATED);
     $query->preExecute();
     $keys = [(string) $query, $query->getArguments()];
--- core/modules/workspaces/src/WorkspaceOperationFactory.php
+++ core/modules/workspaces/src/WorkspaceOperationFactory.php
@@ -85,6 +85,7 @@ class WorkspaceOperationFactory {
     $this->workspaceAssociation = $workspace_association;
     $this->cacheTagsInvalidator = $cache_tags_invalidator;
     if (!$event_dispatcher) {
+      // phpcs:ignore Drupal.Semantics.FunctionTriggerError
       @trigger_error('The event dispatcher service should be passed to WorkspaceOperationFactory::__construct() since 10.1.0. This will be required in Drupal 11.0.0.', E_USER_DEPRECATED);
       $event_dispatcher = \Drupal::service('event_dispatcher');
     }
