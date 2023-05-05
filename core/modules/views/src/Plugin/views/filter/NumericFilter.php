56afcd081c3e404bca2a7526bad64e82c6c1b30f
Bharath Kondeti <bharath122334@gmail.com>
2023-05-05 18:25:41 +0530

    Fixed few more warnings due to grouped filters.

--- core/modules/views/src/Plugin/views/filter/NumericFilter.php
+++ core/modules/views/src/Plugin/views/filter/NumericFilter.php
@@ -423,12 +423,15 @@ class NumericFilter extends FilterPluginBase {
       if (!empty($info[$this->operator]['values'])) {
         switch ($info[$this->operator]['values']) {
           case 1:
-            if ($value['value'] === '') {
+            if (!isset($value['value']) || $value['value'] === '') {
               return FALSE;
             }
             break;
 
           case 2:
+            if (!isset($value['min']) || !isset($value['max'])) {
+              return FALSE;
+            }
             if ($value['min'] === '' && $value['max'] === '') {
               return FALSE;
             }
