7c66afa2dcb29308e839468aef78f417aecb3404
andrew farquharson <farquharson.a@gmail.com>
2023-05-07 16:35:54 +0100

    fix 2 lint errors

--- core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
+++ core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
@@ -12,7 +12,9 @@
     // CKEditor 5 in modals can work as expected.
     // @see https://api.jqueryui.com/dialog/#method-_allowInteraction
     _allowInteraction(event) {
-      if(typeof event.target.classList != 'undefined'){ return event.target.classList.contains('ck') || this._super(event);} 
+      if (typeof event.target.classList !== 'undefined') {
+        return event.target.classList.contains('ck') || this._super(event);
+      }
     },
   });
 })(jQuery);
