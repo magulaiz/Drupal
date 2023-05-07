f6fcdb2f3cca0f1936459f9acab1874d266e63a2
andrew farquharson <farquharson.a@gmail.com>
2023-05-07 15:52:17 +0100

    check whether typeof undefined

--- core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
+++ core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
@@ -12,7 +12,7 @@
     // CKEditor 5 in modals can work as expected.
     // @see https://api.jqueryui.com/dialog/#method-_allowInteraction
     _allowInteraction(event) {
-      return event.target.classList.contains('ck') || this._super(event);
+      if(typeof event.target.classList != 'undefined'){ return event.target.classList.contains('ck');} else {return this._super(event);}
     },
   });
 })(jQuery);
