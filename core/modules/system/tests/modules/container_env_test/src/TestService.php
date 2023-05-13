b246b25bd28ac99719dc08727d7c04e0b291ec93
Dua <duadua@gmail.com>
2023-05-13 10:43:23 +0100

    Test passing parameter to service

---
+++ core/modules/system/tests/modules/container_env_test/src/TestService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\container_env_test;
+
+class TestService {
+
+  public function __construct(public string $parameter) {
+  }
+
+}
--- core/tests/Drupal/KernelTests/Core/DependencyInjection/EnvironmentVariableTest.php
+++ core/tests/Drupal/KernelTests/Core/DependencyInjection/EnvironmentVariableTest.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\KernelTests\Core\DependencyInjection;
 
+use Drupal\container_env_test\TestService;
 use Drupal\KernelTests\KernelTestBase;
 
+/**
+ * Tests integration of the container with environment variables.
+ */
 class EnvironmentVariableTest extends KernelTestBase {
 
   /**
@@ -12,18 +16,21 @@ class EnvironmentVariableTest extends KernelTestBase {
   protected static $modules = ['container_env_test'];
 
   protected function setUp(): void {
-    $name = $this->getName();
-
     $_ENV['CONTAINER_ENV_TEST'] = 'test-variable';
     $_ENV['CONTAINER_ENV_TEST_WITH_DEFAULT2'] = 'override-default';
     parent::setUp();
   }
 
 
-  public function testWithEnvironmentVariable(): void {
+  public function testEnvironmentVariable(): void {
     $this->assertEquals('test-variable', $this->container->getParameter('container_env_test'));
     $this->assertEquals('some-test', $this->container->getParameter('container_env_test_with_default'));
     $this->assertEquals('override-default', $this->container->getParameter('container_env_test_with_default2'));
+
+    $service = \Drupal::service(TestService::class);
+    assert($service instanceof TestService);
+    $this->assertEquals('test-variable', $service);
+
   }
 
 }
