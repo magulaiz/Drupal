b98ef25e670025cffddec9c5be7ae44f28aec470
pratik <pratik@specbee.com>
2023-05-17 10:34:46 +0530

    PHPCS fix.

--- core/modules/user/tests/src/Unit/Form/UserPermissionsFormTest.php
+++ core/modules/user/tests/src/Unit/Form/UserPermissionsFormTest.php
@@ -2,24 +2,10 @@
 
 namespace Drupal\Tests\user\Unit\Form;
 
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Tests\UnitTestCase;
-use Drupal\user\PermissionHandlerInterface;
-use Drupal\user\RoleStorageInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Tests the user permissions administration form.
  *
  * @coversDefaultClass \Drupal\user\Form\UserPermissionsForm
  * @group user
  */
-class UserPermissionsFormTest extends UnitTestCase {
-
-  /**
-   *
-   */
-
-}
+class UserPermissionsFormTest extends UnitTestCase {}
