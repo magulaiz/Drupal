27367e94fc367c8f4554b3ffaa82b884e7f4ff20
Ricardo Sanz <ricardo@metadrop.net>
2023-05-22 13:05:58 +0200

    Fix typo (implies changing lines because one line is now longer than 80 chars)

--- core/lib/Drupal/Core/Render/Element/Select.php
+++ core/lib/Drupal/Core/Render/Element/Select.php
@@ -41,8 +41,8 @@ use Drupal\Core\Render\Element;
  * - #empty_value: (optional) The value for the first default option, which is
  *   used to determine whether the user submitted a value or not.
  *   - If #required is TRUE, this defaults to '' (an empty string). Warning:
- *     the value in #options will be overriden by the value of #empty_option if
- *     #empty_value is set to any value already present in the keys of
+ *     the value in #options will be overridden by the value of #empty_option
+ *     if #empty_value is set to any value already present in the keys of
  *     #options.
  *   - If #required is not TRUE and this value isn't set, then no extra option
  *     is added to the select control, leaving the control in a slightly
@@ -82,12 +82,14 @@ use Drupal\Core\Render\Element;
  *
  * @FormElement("select")
  */
-class Select extends FormElement {
+class Select extends FormElement
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getInfo() {
+  public function getInfo()
+  {
     $class = static::class;
     return [
       '#input' => TRUE,
@@ -126,7 +128,8 @@ class Select extends FormElement {
    *
    * @see _form_validate()
    */
-  public static function processSelect(&$element, FormStateInterface $form_state, &$complete_form) {
+  public static function processSelect(&$element, FormStateInterface $form_state, &$complete_form)
+  {
     // #multiple select fields need a special #name.
     if ($element['#multiple']) {
       $element['#attributes']['multiple'] = 'multiple';
@@ -164,7 +167,8 @@ class Select extends FormElement {
   /**
    * {@inheritdoc}
    */
-  public static function valueCallback(&$element, $input, FormStateInterface $form_state) {
+  public static function valueCallback(&$element, $input, FormStateInterface $form_state)
+  {
     if ($input !== FALSE) {
       if (isset($element['#multiple']) && $element['#multiple']) {
         // If an enabled multi-select submits NULL, it means all items are
@@ -172,8 +176,7 @@ class Select extends FormElement {
         // default value should be used.
         if (empty($element['#disabled'])) {
           return (is_array($input)) ? array_combine($input, $input) : [];
-        }
-        else {
+        } else {
           return (isset($element['#default_value']) && is_array($element['#default_value'])) ? $element['#default_value'] : [];
         }
       }
@@ -185,8 +188,7 @@ class Select extends FormElement {
       // is selected, set the value to match the empty value exactly.
       elseif (isset($element['#empty_value']) && $input === (string) $element['#empty_value']) {
         return $element['#empty_value'];
-      }
-      else {
+      } else {
         return $input;
       }
     }
@@ -195,10 +197,10 @@ class Select extends FormElement {
   /**
    * Prepares a select render element.
    */
-  public static function preRenderSelect($element) {
+  public static function preRenderSelect($element)
+  {
     Element::setAttributes($element, ['id', 'name', 'size']);
     static::setAttributes($element, ['form-select']);
     return $element;
   }
-
 }
