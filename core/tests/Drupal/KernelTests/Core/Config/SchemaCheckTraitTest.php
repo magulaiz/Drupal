f57ed08b2d00f89637856557a62ef16ee157a2eb
Wim Leers <wim.leers@acquia.com>
2023-05-27 19:41:10 +0200

    Test: expect validation errors for missing keys, even if the values are nullable. IOW: the key may be required while the value is optional!

--- core/tests/Drupal/KernelTests/Core/Config/SchemaCheckTraitTest.php
+++ core/tests/Drupal/KernelTests/Core/Config/SchemaCheckTraitTest.php
@@ -82,7 +82,7 @@ class SchemaCheckTraitTest extends KernelTestBase {
     $this->assertEquals($expected, $ret);
 
     // Omit all data, this should trigger validation errors for required keys
-    // missing.
+    // missing. No matter whether the value is nullable or not.
     $config_data = [];
     $ret = $this->checkConfigSchema($this->typedConfig, 'config_test.types', $config_data);
     $expected = [
@@ -95,6 +95,16 @@ class SchemaCheckTraitTest extends KernelTestBase {
       "[] 'int' is a required key.",
       "[] 'string' is a required key.",
       "[] 'string_int' is a required key.",
+      "[] 'nullable_array' is a required key.",
+      "[] 'nullable_boolean' is a required key.",
+      "[] 'nullable_exp' is a required key.",
+      "[] 'nullable_float' is a required key.",
+      "[] 'nullable_float_as_integer' is a required key.",
+      "[] 'nullable_hex' is a required key.",
+      "[] 'nullable_int' is a required key.",
+      "[] 'nullable_octal' is a required key.",
+      "[] 'nullable_string' is a required key.",
+      "[] 'nullable_string_int' is a required key.",
     ];
     $this->assertEquals($expected, $ret);
   }
