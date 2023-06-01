0dbb80af0153d5ca0922dfcd78ddf7b6e901da81
Sweetchuck <Sweetchuck@23728.no-reply.drupal.org>
2023-06-01 12:51:30 +0200

    WIP

--- core/modules/tour/src/TipPluginBase.php
+++ core/modules/tour/src/TipPluginBase.php
@@ -53,7 +53,7 @@ abstract class TipPluginBase extends PluginBase implements TipPluginInterface {
    * {@inheritdoc}
    */
   public function get($key) {
-    return $this->configuration[$key] ?? NULL;
+    return $this->configuration[$key] ?? '';
   }
 
   /**
@@ -71,7 +71,7 @@ abstract class TipPluginBase extends PluginBase implements TipPluginInterface {
 
     // The location values accepted by PopperJS, the library used for
     // positioning the tip.
-    assert(in_array(trim($location ?? ''), [
+    assert(in_array(trim($location), [
       'auto',
       'auto-start',
       'auto-end',
