0ba8a34e6d57288ad86caef5d80f74590932fe2a
bnjmnm <benm@umich.edu>
2023-06-01 13:36:34 -0400

    temporary button

--- core/modules/toolbar/src/Element/Toolbar.php
+++ core/modules/toolbar/src/Element/Toolbar.php
@@ -139,6 +139,7 @@ class Toolbar extends RenderElement {
           '#type' => 'html_tag',
           '#tag' => 'a',
           '#aria-label' => t('...'),
+          '#value' => '😎',
           '#attributes' => [
             'type' => 'button',
             'data-toolbar-extra-item-toggle-button' => TRUE,
