7d4d066f9111551a4a7a759b229bf954f3a200b4
Wim Leers <wim.leers@acquia.com>
2023-06-01 11:43:47 +0200

    Now we can expect explicit validation error for the invalid key-value pair.

--- core/tests/Drupal/KernelTests/Core/Config/SchemaCheckTraitTest.php
+++ core/tests/Drupal/KernelTests/Core/Config/SchemaCheckTraitTest.php
@@ -59,6 +59,7 @@ class SchemaCheckTraitTest extends KernelTestBase {
       'config_test.types:new_key' => 'missing schema',
       'config_test.types:new_array' => 'missing schema',
       'config_test.types:boolean' => 'non-scalar value but not defined as an array (such as mapping or sequence)',
+      0 => '[boolean] This value should be of the correct primitive type.',
     ];
     $this->assertEquals($expected, $ret);
   }
