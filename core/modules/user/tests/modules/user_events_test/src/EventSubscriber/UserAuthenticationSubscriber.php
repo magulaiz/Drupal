2035a86c098d0dc2b54007758a24ba84915c614a
Kostia Bohach <k.bohach@dev-branch.com>
2023-06-07 19:40:43 +0300

    Fixed coding standards for function comment

--- core/modules/user/tests/modules/user_events_test/src/EventSubscriber/UserAuthenticationSubscriber.php
+++ core/modules/user/tests/modules/user_events_test/src/EventSubscriber/UserAuthenticationSubscriber.php
@@ -12,7 +12,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class UserAuthenticationSubscriber implements EventSubscriberInterface {
 
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
     return [
