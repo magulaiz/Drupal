80f497ee9f9ef5a4c935255264c8338f5e134184
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-06-08 12:59:27 +0530

    Small nit

--- core/modules/options/src/Plugin/Field/FieldType/ListItemBase.php
+++ core/modules/options/src/Plugin/Field/FieldType/ListItemBase.php
@@ -139,12 +139,6 @@ abstract class ListItemBase extends FieldItemBase implements OptionsProviderInte
     });
     $current_keys = array_keys($allowed_values);
     for ($delta = 0; $delta <= $max; $delta++) {
-      if ($delta === 0) {
-        $required_field = TRUE;
-      }
-      else {
-        $required_field = FALSE;
-      }
       $key = $current_keys[$delta] ?? '';
       $element['allowed_values']['table'][$delta] = [
         '#attributes' => [
@@ -158,7 +152,7 @@ abstract class ListItemBase extends FieldItemBase implements OptionsProviderInte
           '#title' => $this->t('Name'),
           '#weight' => -30,
           '#default_value' => isset($current_keys[$delta]) ? $allowed_values[$current_keys[$delta]] : '',
-          '#required' => $required_field,
+          '#required' => $delta == 0,
         ],
       ];
       if ($field_type == 'list_string') {
@@ -188,7 +182,7 @@ abstract class ListItemBase extends FieldItemBase implements OptionsProviderInte
             '#default_value' => $current_keys[$delta] ?? '',
             '#step' => ($field_type == 'list_float' ? 'any' : ''),
             '#weight' => -20,
-            '#required' => $required_field,
+            '#required' => $delta == 0,
           ],
         ];
       }
