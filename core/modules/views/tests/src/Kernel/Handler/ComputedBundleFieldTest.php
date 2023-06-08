abf3e784f654af4961a8e3a36e86309060cd628c
Owen Bush <owen.bush@lullabot.com>
2023-06-08 14:10:51 -0400

    Revert normalizer test, remove unused use statement

--- core/modules/views/tests/src/Kernel/Handler/ComputedBundleFieldTest.php
+++ core/modules/views/tests/src/Kernel/Handler/ComputedBundleFieldTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\views\Kernel\Handler;
 
 use Drupal\entity_test\Entity\EntityTestComputedBundleField;
-use Drupal\entity_test\Entity\EntityTestComputedField;
 use Drupal\Tests\views\Kernel\ViewsKernelTestBase;
 use Drupal\views\Views;
 
