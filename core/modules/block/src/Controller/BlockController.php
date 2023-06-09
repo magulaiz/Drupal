262c8f49c8cb791a2da890eda18bc3172d8c4dfd
Benji Fisher <benji@FisherFam.org>
2023-06-08 21:22:18 -0400

    Use PathChangedHelper to simplify the controller

--- core/modules/block/src/Controller/BlockController.php
+++ core/modules/block/src/Controller/BlockController.php
@@ -3,6 +3,7 @@
 namespace Drupal\block\Controller;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Routing\PathChangedHelper;
 use Drupal\block\BlockInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Extension\ThemeHandlerInterface;
@@ -142,52 +143,19 @@ class BlockController extends ControllerBase {
    */
   public function blockLayoutRedirect(RouteMatchInterface $route_match, Request $request, ?BlockInterface $block = NULL): RedirectResponse {
     @trigger_error('The path /admin/structure/block, with its child paths, is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/appearance/block. See https://www.drupal.org/node/3320855.', E_USER_DEPRECATED);
-
     $change_record = 'https://www.drupal.org/node/3320855';
-    return $this->redirectWithWarning(
-      $route_match,
-      $request,
-      $change_record,
-      $this->getLogger('block'),
-      $this->messenger()
-    );
-  }
-
-  /**
-   * Provides a redirect and optionally adds warning messages.
-   *
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The current route match. The route name should end in '.bc'.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The current request.
-   * @param string $change_record
-   *   The URL of the change record, to be included in the log message.
-   * @param \Psr\Log\LoggerInterface|null $logger
-   *   (optional) A logger for the warning message.
-   * @param \Drupal\Core\Messenger\MessengerInterface|null $messenger
-   *   (optional) A messenger for the warning message.
-   *
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse
-   */
-  protected function redirectWithWarning(RouteMatchInterface $route_match, Request $request, string $change_record, ?LoggerInterface $logger = NULL, ?MessengerInterface $messenger = NULL): RedirectResponse {
-    $args = $route_match->getRawParameters()->all();
-    // Strip '.bc' from the end of the route name.
-    $route_name = substr($route_match->getRouteName(), 0, -3);
-
+    $helper = new PathChangedHelper($route_match, $request);
     $params = [
-      '%old_path' => Url::fromRoute("$route_name.bc", $args)->toString(),
-      '%new_path' => Url::fromRoute($route_name, $args)->toString(),
+      '%old_path' => $helper->oldPath(),
+      '%new_path' => $helper->newPath(),
       '%change_record' => $change_record,
     ];
-    if ($logger) {
-      $logger->warning('A user was redirected from %old_path. This redirect will be removed in a future version of Drupal. Update links, shortcuts, and bookmarks to use %new_path. See %change_record for more information.', $params);
-    }
-    if ($messenger) {
-      $messenger->addWarning($this->t('You have been redirected from %old_path. Update links, shortcuts, and bookmarks to use %new_path.', $params));
-    }
+    $warning_message = $this->t('You have been redirected from %old_path. Update links, shortcuts, and bookmarks to use %new_path.', $params);
+    $this->messenger()->addWarning($warning_message);
+    $this->getLogger('block_content')
+      ->warning('A user was redirected from %old_path. This redirect will be removed in a future version of Drupal. Update links, shortcuts, and bookmarks to use %new_path. See %change_record for more information.', $params);
 
-    $query = $request->query->all();
-    return $this->redirect($route_name, $args, ['query' => $query], 301);
+    return $helper->redirect();
   }
 
 }
