6f70c2fd6f9012e03e4d73bd75deb18d69313d26
Dave Reid <dave@davereid.net>
2023-06-17 09:19:08 -0500

    Fixed extra space.

--- core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
+++ core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
@@ -207,7 +207,7 @@ class LibraryDiscoveryParser {
           elseif ($type == 'css') {
             $options['group'] = $extension_type == 'theme' ? CSS_AGGREGATE_THEME : CSS_AGGREGATE_DEFAULT;
           }
-          // All library assets are files by default, except  external URLs.
+          // All library assets are files by default, except external URLs.
           if (!isset($options['type'])) {
             $options['type'] = UrlHelper::isExternal($source) ? 'external' : 'file';
           }
