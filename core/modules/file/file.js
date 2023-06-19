45da493fadae034ce1b5a2029282420fa39978f1
Yash Rode <yashrode774@gmail.com>
2023-06-19 15:30:45 +0530

    phpcs fix

--- core/modules/file/file.js
+++ core/modules/file/file.js
@@ -260,10 +260,7 @@
         // Replace the name with the required identifier to ensure that this
         // element sends its upload identifier. The upload identifier is the
         // last string in the name that does not contain square brackets.
-        $progressId.attr(
-          'name',
-          originalName.match(/[^\[\]]+(?!.*[^\[\]]+)/)[0],
-        );
+        $progressId.attr('name', originalName.match(/[^\[\]]+(?!.*[^\[\]]+)/)[0]);
 
         // Restore the original name after the upload begins to prevent this
         // element from sending its upload identifier with another element’s
