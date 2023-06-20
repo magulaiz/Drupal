ad7ad2645e3f420279a3ff767ef51ec352b2baa5
quietone <quietone@2572884.no-reply.drupal.org>
2023-06-21 11:45:56 +1200

    Fix missed items during rebase

--- core/modules/block_content/src/Controller/BlockContentController.php
+++ core/modules/block_content/src/Controller/BlockContentController.php
@@ -187,15 +187,9 @@ class BlockContentController extends ControllerBase {
    *
    * @see https://www.drupal.org/node/3320855
    */
-<<<<<<< HEAD
   public function blockLibraryRedirect(RouteMatchInterface $route_match, Request $request) {
     @trigger_error('The path /admin/structure/block/block-content is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block. See https://www.drupal.org/node/3320855.', E_USER_DEPRECATED);
     $helper = new PathChangedHelper($route_match, $request);
-=======
-  public function blockLibraryRedirect() {
-    @trigger_error('The path /admin/structure/block/block-content is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block. See https://www.drupal.org/node/3320855', E_USER_DEPRECATED);
-    $route = 'entity.block_content.collection';
->>>>>>> 4390bb58aa (Find and fix remaining failures)
     $params = [
       '%old_path' => $helper->oldPath(),
       '%new_path' => $helper->newPath(),
@@ -225,48 +219,11 @@ class BlockContentController extends ControllerBase {
    *   /admin/content/block/{block_content} directly instead of
    *   /block/{block_content}.
    *
-<<<<<<< HEAD
    * @see https://www.drupal.org/node/3320855
    */
   public function editRedirect(RouteMatchInterface $route_match, Request $request, BlockContentInterface $block_content): RedirectResponse {
     @trigger_error('The path /block/{block_content} is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block/{block_content}. See https://www.drupal.org/node/3320855.', E_USER_DEPRECATED);
     $helper = new PathChangedHelper($route_match, $request);
-=======
-   * @see https://www.drupal.org/node/2317981
-   */
-  public function editRedirect(BlockContentInterface $block_content): RedirectResponse {
-    @trigger_error('The path /block/{block_content} is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block/{block_content}. See https://www.drupal.org/node/2317981', E_USER_DEPRECATED);
-    $route = 'entity.block_content.edit_form';
-    $params = [
-      '%old_path' => Url::fromRoute("$route.bc", ['block_content' => $block_content->id()])->toString(),
-      '%new_path' => Url::fromRoute($route, ['block_content' => $block_content->id()])->toString(),
-      '%change_record' => 'https://www.drupal.org/node/3320855',
-    ];
-    $warning_message = $this->t('You have been redirected from %old_path. Update links, shortcuts, and bookmarks to use %new_path.', $params);
-    $this->messenger()->addWarning($warning_message);
-    $this->getLogger('block_content')->warning('A user was redirected from %old_path to %new_path. This redirect will be removed in a future version of Drupal. Update links, shortcuts, and bookmarks to use %new_path. See %change_record for more information.', $params);
-
-    return $this->redirect($route, ['block_content' => $block_content->id()], [], 301);
-  }
-
-  /**
-   * Provides a redirect to block delete page.
-   *
-   * @param Drupal\block_content\BlockContentInterface $block_content
-   *   The block to be deleted.
-   *
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse
-   *
-   * @deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use
-   *   /admin/content/block/{block_content}/delete directly instead of
-   *   /block/{block_content}/delete.
-   *
-   * @see https://www.drupal.org/node/2317981
-   */
-  public function deleteRedirect(BlockContentInterface $block_content): RedirectResponse {
-    @trigger_error('The path /block/{block_content}/delete is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block/{block_content}/delete. See https://www.drupal.org/node/2317981', E_USER_DEPRECATED);
-    $route = 'entity.block_content.delete_form';
->>>>>>> 4390bb58aa (Find and fix remaining failures)
     $params = [
       '%old_path' => $helper->oldPath(),
       '%new_path' => $helper->newPath(),
