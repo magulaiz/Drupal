1fbd4a6fac9efa8603d28605d42aacf65819f29c
bnjmnm <benm@umich.edu>
2023-06-22 06:21:16 -0400

    11x toggletip

--- core/lib/Drupal/Core/Render/Element/HtmlTag.php
+++ core/lib/Drupal/Core/Render/Element/HtmlTag.php
@@ -3,6 +3,7 @@
 namespace Drupal\Core\Render\Element;
 
 use Drupal\Component\Render\MarkupInterface;
+use Drupal\Component\Serialization\Json;
 use Drupal\Component\Utility\Html as HtmlUtility;
 use Drupal\Core\Render\Attribute\RenderElement;
 use Drupal\Core\Render\Markup;
@@ -82,6 +83,9 @@ class HtmlTag extends RenderElementBase {
    */
   public static function preRenderHtmlTag($element) {
     $attributes = isset($element['#attributes']) ? new Attribute($element['#attributes']) : '';
+    if (isset($element['#toggletip']['content'])) {
+      $attributes['data-drupal-toggletip'] = Json::encode([...$element['#toggletip'], 'rendered' => TRUE]);
+    }
 
     // An HTML tag should not contain any special characters. Escape them to
     // ensure this cannot be abused.
---
+++ core/misc/toggletip.js
@@ -0,0 +1,415 @@
+/**
+ * @file
+ * Attaches behaviors for adding toggle tip functionality.
+ */
+// cspell:ignore UIDOM Wolfson
+
+((
+  Drupal,
+  displace,
+  { computePosition, flip, shift, offset, autoUpdate, arrow },
+) => {
+  // Keeps track of generated ids to ensure no duplicates are created.
+  const toggleTipIds = new Set();
+
+  /**
+   * Get the line height of an element.
+   *
+   * @param {element} node
+   *   The element to get the line height of.
+   * @return {Number}
+   *
+   *   The pixel count of the line height.
+   *
+   * This is code based on the project https://github.com/twolfson/line-height,
+   * copyright (c) 2013 Todd Wolfson.  These classes are licensed under the MIT
+   * license. For additional details, see
+   * https://raw.githubusercontent.com/twolfson/line-height/master/LICENSE-MIT.
+   */
+  const getLineHeight = (node) => {
+    // Grab the line-height via style
+    let lineHeightString = getComputedStyle(node)['line-height'];
+    let lineHeight = parseFloat(lineHeightString, 10);
+
+    // If the lineHeight did not contain a unit (i.e. it was numeric), convert it to ems (e.g. '2.3' === '2.3em')
+    if (lineHeightString === `${lineHeight}`) {
+      // Save the old lineHeight style and update the em unit to the element
+      const _lineHeightStyle = node.style.lineHeight;
+      node.style.lineHeight = `${lineHeightString}em`;
+
+      // Calculate the em based height.
+      lineHeightString = getComputedStyle(node)['line-height'];
+      lineHeight = parseFloat(lineHeightString, 10);
+
+      // Revert the lineHeight style
+      if (_lineHeightStyle) {
+        node.style.lineHeight = _lineHeightStyle;
+      } else {
+        delete node.style.lineHeight;
+      }
+    }
+
+    lineHeight = Math.round(lineHeight);
+
+    // If the line-height is "normal", calculate by font-size.
+    if (lineHeightString === 'normal') {
+      // Create a temporary node
+      const { nodeName } = node;
+      const _node = document.createElement(nodeName);
+      _node.innerHTML = '&nbsp;';
+      _node.style.fontSize = getComputedStyle(node)['font-size'];
+
+      // Remove default padding/border which can affect offset height.
+      _node.style.padding = '0px';
+      _node.style.border = '0px';
+
+      // Append it to the body.
+      document.body.appendChild(_node);
+
+      // Assume the line height of the element is the height.
+      lineHeight = _node.offsetHeight;
+
+      // Remove the temporary node from the DOM.
+      document.body.removeChild(_node);
+    }
+
+    // Return the calculated height.
+    return lineHeight;
+  };
+
+  const placeButton = (button, tipElement, config) => {
+    const { offsetHeight, offsetWidth } = button;
+    tipElement.style.position = 'relative';
+    button.style.position = 'absolute';
+    const places = config.place.split('-');
+
+    const mainPosition = {
+      left: {
+        style: 'inset-inline-start',
+        offset: 0 - offsetWidth * 1.5 - config.offset / 4,
+      },
+      right: {
+        style: 'inset-inline-end',
+        offset: 0 - offsetWidth * 1.5 - config.offset / 4,
+      },
+      top: { style: 'inset-block-start', offset: 0 - offsetHeight },
+      bottom: { style: 'inset-block-end', offset: 0 - offsetHeight },
+    }[places[0]];
+
+    button.style[mainPosition.style] = `${mainPosition.offset}px`;
+
+    if (places[1]) {
+      const secondaryPosition = {
+        start: ['left', 'right'].includes(places[0])
+          ? { style: 'inset-block-start', offset: 0 }
+          : {
+              style: 'inset-inline-start',
+              offset: 0 - offsetWidth / 2,
+            },
+        end: ['left', 'right'].includes(places[0])
+          ? { style: 'inset-block-end', offset: 0 }
+          : { style: 'inset-inline-end', offset: 0 - offsetWidth / 2 },
+      }[places[1]];
+      button.style[secondaryPosition.style] = `${secondaryPosition.offset}px`;
+    } else {
+      const properties = ['left', 'right'].includes(places[0])
+        ? { start: 'top', end: 'bottom' }
+        : { start: 'left', end: 'right' };
+
+      button.style[`margin-${properties.start}`] = 'auto';
+      button.style[`margin-${properties.end}`] = 'auto';
+      button.style[properties.start] = '0';
+      button.style[properties.end] = '0';
+    }
+  };
+
+  /**
+   * Toggle tip.
+   *
+   * @see https://codepen.io/aardrian/pen/NWpoVQd
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.toggleTip = {
+    attach(context) {
+      const generateId = () => {
+        const id = Math.random().toString(36).substring(2, 15);
+        if (toggleTipIds.has(id)) {
+          return generateId();
+        }
+        toggleTipIds.add(id);
+        return id;
+      };
+
+      /**
+       * Closes every open toggletip on the page.
+       *
+       * @param {string} exceptId
+       *   Optional. Will not close a toggletip with an id that matches this.
+       */
+      const closeAllToggletips = (exceptId) => {
+        document
+          .querySelectorAll(
+            '[data-drupal-toggletip-toggle-button][aria-expanded="true"]',
+          )
+          .forEach((toggleButton) => {
+            if (toggleButton.id === exceptId) {
+              return;
+            }
+            toggleButton.setAttribute('aria-expanded', 'false');
+            document.getElementById(
+              toggleButton.getAttribute('aria-controls'),
+            ).hidden = true;
+          });
+      };
+
+      const defaultConfig = {
+        atDescription: Drupal.t('More info about this'),
+        placement: 'top-end',
+        offset: 24,
+        positionOffsetPrimary: 0,
+        positionOffsetSecondary: 0,
+        shiftPadding: 5,
+      };
+
+      once('drupal-tip', '[data-drupal-toggletip]', context).forEach(
+        (tipElement) => {
+          const toggletipConfig = JSON.parse(
+            tipElement.getAttribute('data-drupal-toggletip'),
+          );
+          if (!toggletipConfig.content) {
+            return;
+          }
+
+          tipElement.classList.add('toggletip');
+          const config = { ...defaultConfig, ...toggletipConfig };
+          // Create unique ids for each part of the toggletip.
+          const id = generateId();
+          const toggleId = `${id}-toggle`;
+          const descriptionId = `${id}-description`;
+          const tipId = `${id}-tip`;
+
+          // Create the toggle button.
+          const button = document.createElement('button');
+          button.type = 'button';
+          button.setAttribute('data-drupal-toggletip-toggle-button', true);
+          button.setAttribute('aria-expanded', false);
+          button.setAttribute('aria-labelledby', descriptionId);
+          button.setAttribute('aria-controls', tipId);
+          button.classList.add('toggletip__toggle');
+          button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path fill="#55565B" d="M7.194 2.073a1.729 1.729 0 0 0-.131.677c0 .24.042.463.127.67.085.205.214.394.384.566.171.17.361.3.568.386.209.085.436.128.68.128.24 0 .461-.044.666-.13a1.74 1.74 0 0 0 .562-.389c.172-.172.3-.362.385-.566a1.71 1.71 0 0 0 .127-.665c0-.243-.042-.47-.128-.675a1.7 1.7 0 0 0-.384-.564 1.79 1.79 0 0 0-.564-.382A1.72 1.72 0 0 0 8.822 1a1.73 1.73 0 0 0-1.236.51 1.696 1.696 0 0 0-.392.563ZM7.689 12.582l.001-.006 2.091-7.1.004-.025a.078.078 0 0 0-.08-.076l-4.935.159a.076.076 0 0 0-.074.058l-.254.95-.005.027c0 .043.035.079.077.079h.966c.475 0 .666.29.613.738-.032.268-.122.555-.192.816l-1.106 3.441c-.22.722-.447 1.561-.17 2.3.442 1.168 1.98 1.231 2.954.844.35-.14.691-.35 1.025-.627l.005-.004c.333-.277.662-.627.986-1.05.322-.419.64-.91.956-1.473a.08.08 0 0 0-.02-.106l-.684-.523a.075.075 0 0 0-.105.028c-.202.347-.396.658-.58.928a8.133 8.133 0 0 1-.536.708 2.95 2.95 0 0 1-.45.441c-.122.09-.223.137-.303.137-.365 0-.246-.436-.184-.664Z"/></svg> <span id=${descriptionId} class="visually-hidden">${config.atDescription}</span>`;
+          button.id = toggleId;
+
+          // Create the toggletip content container.
+          const tip = document.createElement('div');
+          tip.classList.add('toggletip__tip');
+          tip.setAttribute('tabindex', '0');
+          tip.setAttribute('role', 'region');
+          tip.setAttribute('hidden', 'true');
+          tip.setAttribute('data-drupal-toggletip-tip', true);
+          tip.id = tipId;
+          tip.innerHTML = toggletipConfig.content;
+
+          // Create the arrow that points to the tip's disclosure button.
+          const tipArrow = document.createElement('div');
+          tipArrow.classList.add('toggletip__arrow');
+          tip.append(tipArrow);
+
+          // Disclosure button click behavior.
+          button.addEventListener('click', () => {
+            const expanded = button.getAttribute('aria-expanded') === 'true';
+            button.setAttribute('aria-expanded', !expanded);
+            tip.hidden = !!expanded;
+
+            // Close all toggletips other than the one just opened.
+            if (expanded) {
+              closeAllToggletips(button.id);
+            }
+          });
+
+          // When the disclosure button is blurred, close all toggletips unless
+          // focus has moved to the tip the disclosure button opens.
+          button.addEventListener('blur', () => {
+            // Wrap in a timeout so there is sufficient time for focus to have
+            // moved.
+            setTimeout(() => {
+              if (document.activeElement.id !== tip.id) {
+                closeAllToggletips();
+              }
+            });
+          });
+
+          // When the toggletip is blurred, close all toggletips unless focus has
+          // moved to that tip's disclosure button.
+          tip.addEventListener('blur', () => {
+            // Wrap in a timeout so there is sufficient time for focus to have
+            // moved.
+            setTimeout(() => {
+              if (document.activeElement.id !== button.id) {
+                closeAllToggletips();
+              }
+            });
+          });
+
+          // When a toggletip option is directly added to a details element, the
+          // button is appended to its summary. To add a toggletip inside a
+          // details element, add it to a child render array.
+          if (
+            tipElement.tagName === 'DETAILS' &&
+            tipElement.querySelector('summary')
+          ) {
+            const summary = tipElement.querySelector('summary');
+            summary.append(button);
+            summary.append(tip);
+          } else if (
+            tipElement.hasAttribute('data-drupal-toggletip-form-element')
+          ) {
+            // If a toggletip is attached to a form element, the button should
+            // append to that element's label.
+            const label = tipElement
+              .closest('.js-form-item')
+              .querySelector('label');
+            if (label) {
+              label.append(button);
+              label.append(tip);
+            }
+          } else {
+            tipElement.append(button);
+            tipElement.append(tip);
+          }
+
+          // Determine the line height so the toggle button can be vertically
+          // centered.
+          const elementLineHeight = getLineHeight(tipElement);
+
+          if (elementLineHeight > button.offsetHeight && !config.place) {
+            // If the element receiving the toggle button has a larger line
+            // height than the toggle button, vertically position the button
+            // based on the element's line height. This ensures vertical
+            // centering even if the text wraps.
+            button.style.position = 'relative';
+
+            const halfTallerDivider =
+              elementLineHeight / 2 > button.clientHeight ? 1 : 2;
+            const pixel = -Math.abs(
+              elementLineHeight / 2 -
+                button.clientHeight / halfTallerDivider -
+                2,
+            );
+
+            button.style.top = `${pixel}px`;
+          }
+
+          // If the button placement is explicitly set, parse the positioning
+          // string into styles that place the toggle button in the expected
+          // position.
+          if (config.place) {
+            placeButton(button, tipElement, config);
+          }
+
+          // Position the toggletip.
+          autoUpdate(button, tip, () => {
+            displace();
+            computePosition(button, tip, {
+              placement: config.placement,
+              middleware: [
+                offset(config.offset),
+                // Account for Drupal's top offset so the toggletip is not hidden
+                // under the toolbar.
+                flip({
+                  padding: {
+                    top: displace.offsets.top
+                      ? displace.offsets.top + config.offset
+                      : 0,
+                  },
+                  crossAxis: false,
+                }),
+                shift({ padding: config.shiftPadding }),
+                arrow({ element: tipArrow }),
+              ],
+              // eslint-disable-next-line max-nested-callbacks
+            }).then(({ x, y, placement, middlewareData }) => {
+              // Position the tip.
+              const { left, right } = displace.offsets;
+
+              // If the default X position is less than the left offset, the x position
+              // should begin at the left offset.
+              let offsetX = x < left ? left : x;
+
+              // If the default X position is less than 0, then just add the
+              // offset.
+              if (x < 0) {
+                offsetX = left + x;
+              }
+              const { marginLeft, marginRight } = window.getComputedStyle(tip);
+              const marginOffset =
+                parseInt(marginLeft.replace(/\D/g, ''), 10) +
+                parseInt(marginRight.replace(/\D/g, ''), 10);
+              Object.assign(tip.style, {
+                left: `${offsetX}px`,
+                top: `${y}px`,
+                position: 'absolute',
+                width:
+                  left || right
+                    ? `${
+                        document.body.offsetWidth -
+                        // Subtract right because right offset does not change
+                        // the body offset width.
+                        right -
+                        config.offset -
+                        config.shiftPadding -
+                        marginOffset
+                      }px`
+                    : 'auto',
+              });
+
+              // Use middleware to dynamically position the arrow.
+              const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+              // Placement will the opposite of the tip's primary axis.
+              const staticSide = {
+                top: 'bottom',
+                right: 'left',
+                bottom: 'top',
+                left: 'right',
+              }[placement.split('-')[0]];
+
+              let offsetArrowX = arrowX - marginOffset / 2;
+
+              if (offsetX !== x) {
+                const addToOffset = x > 0 && arrowX > left ? x : 0;
+                offsetArrowX = Math.abs(arrowX - left) + addToOffset;
+              }
+
+              Object.assign(tipArrow.style, {
+                left: arrowX != null ? `${offsetArrowX}px` : '',
+                top: arrowY != null ? `${arrowY}px` : '',
+                right: '',
+                bottom: '',
+                [staticSide]: '-4px',
+              });
+            });
+          });
+        },
+      );
+
+      once('toggletip-escape', document.body).forEach(() => {
+        // All toggletips close if escape is pressed.
+        document.addEventListener('keydown', (e) => {
+          if (e.code === 'Escape') {
+            document
+              .querySelectorAll(
+                '[data-drupal-toggletip-toggle-button][aria-expanded="true"]',
+              )
+              .forEach((toggleButton) => {
+                toggleButton.setAttribute('aria-expanded', 'false');
+                document.getElementById(
+                  toggleButton.getAttribute('aria-controls'),
+                ).hidden = true;
+              });
+          }
+        });
+      });
+    },
+  };
+})(Drupal, Drupal.displace, window.FloatingUIDOM);
