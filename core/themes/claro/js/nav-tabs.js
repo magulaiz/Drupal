fa6c4770ffc92267f16f89dd97d14d9fa497c051
Sagar Chauhan <sagarchauhan@live.in>
2023-06-23 12:53:51 +0530

    Issue #3225621: Rebase the branch and make required changes.

--- core/themes/claro/js/nav-tabs.js
+++ core/themes/claro/js/nav-tabs.js
@@ -72,10 +72,9 @@
     });
 
     $tab.on('click.tabs', '[data-drupal-nav-tabs-trigger]', openMenu);
-    $(window)
-      // @todo use a media query event listener https://www.drupal.org/project/drupal/issues/3225621
-      .on('resize.tabs', Drupal.debounce(toggleCollapsed, 150))
-      .trigger('resize.tabs');
+    const mql = window.matchMedia('(min-width: 48em)');
+    mql.addEventListener('change', Drupal.debounce(toggleCollapsed, 150));
+    toggleCollapsed(mql);
   }
   /**
    * Initialize the tabs JS.
