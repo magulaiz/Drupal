5bcbe341d8595d32984c893e988d135323542bc8
Wim Leers <wim.leers@acquia.com>
2023-06-26 14:26:06 +0200

    Introduce `view` and `download` `link_target` handlers, and update the filter plugin to use the `download` one when a `download` attribute is present.

--- core/modules/ckeditor5/src/Controller/EntityLinkSuggestionsController.php
+++ core/modules/ckeditor5/src/Controller/EntityLinkSuggestionsController.php
@@ -214,8 +214,7 @@ class EntityLinkSuggestionsController implements ContainerInjectionInterface {
       // @see \Drupal\filter\Plugin\Filter\EntityLinks
       'path' => sprintf('entity:%s/%s', $entity->getEntityTypeId(), $entity->id()),
       'exposed_attributes' => [
-        // @todo ⚠️ COMPUTE THIS INSTEAD OF HARDCODING!
-        'download' => $entity->getEntityTypeId() === 'file' || ($entity->getEntityTypeId() === 'media' && $entity->bundle() === 'document'),
+        'download' =>  $entity->getEntityType()->hasHandlerClass('link_target', 'download'),
       ],
     ];
   }
