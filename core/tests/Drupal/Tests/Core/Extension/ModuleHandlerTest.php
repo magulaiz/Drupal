6df0849fec41ae3131fd8f245395bc15517e87fe
Andreas Hennings <andreas@dqxtech.net>
2023-06-26 15:17:36 +0200

    Issue #3120298: Cover removal of implementations by hook_module_implements_alter().

--- core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
@@ -752,6 +752,38 @@ class ModuleHandlerTest extends UnitTestCase {
         'module_handler_test2_subtype_alter',
       ],
     ];
+    $datasets['remove'] = [
+      [
+        'module_handler_test1',
+        'module_handler_test2',
+        // Add a *_module_implements_alter() to remove *_test.
+        'module_handler_test_remove',
+      ],
+      [
+        'module_handler_test_custom_order',
+        'module_handler_test2_custom_order',
+      ],
+      [
+        'module_handler_test_type_alter',
+        'module_handler_test2_type_alter',
+      ],
+      [
+        'module_handler_test_type_alter',
+        'module_handler_test_subtype_alter',
+        'module_handler_test_unaltered_alter',
+        'module_handler_test2_type_alter',
+        'module_handler_test2_subtype_alter',
+        'module_handler_test2_unaltered_alter',
+      ],
+      [
+        'module_handler_test_unaltered_alter',
+        'module_handler_test_subtype_alter',
+        'module_handler_test1_unaltered_alter',
+        'module_handler_test1_subtype_alter',
+        'module_handler_test2_unaltered_alter',
+        'module_handler_test2_subtype_alter',
+      ],
+    ];
     // Use hook_module_implements_alter() to insert implementations for fake
     // modules early, late and between.
     // The test covers the _current_ behavior, which might not be ideal in all
