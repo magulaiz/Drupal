e32189f90e8acb00d2b644ca7526f61c725b108e
bjorn <bjorn@swis.nl>
2023-06-30 10:04:05 +0200

    Update deprecation target

--- core/modules/jsonapi/src/ResourceType/ResourceTypeRepository.php
+++ core/modules/jsonapi/src/ResourceType/ResourceTypeRepository.php
@@ -447,7 +447,7 @@ class ResourceTypeRepository implements ResourceTypeRepositoryInterface {
     }
     else {
       @trigger_error(
-        sprintf('Entity reference field items not implementing %s is deprecated in drupal:10.1.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3279140', EntityReferenceItemInterface::class),
+        sprintf('Entity reference field items not implementing %s is deprecated in drupal:10.2.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3279140', EntityReferenceItemInterface::class),
         E_USER_DEPRECATED
       );
       $handler_settings = $item_definition->getSetting('handler_settings');
