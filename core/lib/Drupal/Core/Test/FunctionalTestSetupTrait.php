5dc0903f50a602a43f749f35ff0b52d9de159ac7
Bhanu <50266-Bhanu951@users.noreply.drupalcode.org>
2023-07-05 15:34:32 +0530

    Issue #3309104: Replace REQUEST_TIME in Functional/FunctionalJavascript tests.

--- core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
+++ core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
@@ -262,13 +262,7 @@ trait FunctionalTestSetupTrait {
     $server = array_merge($request->server->all(), $override_server_vars);
 
     $request = Request::create($request_path, 'GET', [], [], [], $server);
-<<<<<<< ours
-    // Ensure the request time is REQUEST_TIME to ensure that API calls
-    // in the test use the right timestamp.
-    $request->server->set('REQUEST_TIME', REQUEST_TIME);
-=======
     $this->container->get('request_stack')->push($request);
->>>>>>> theirs
 
     $this->container->get('request_stack')->push($request);
     // The request context is normally set by the router_listener from within
