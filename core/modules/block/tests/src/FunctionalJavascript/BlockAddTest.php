1fb164aa3cd6c7b73ecdc1c8cfac6997cf1c33af
Eduardo Morales <eduardo.morales@metadrop.net>
2023-07-07 15:44:25 +0200

    Issue #2783897 by albeorte, andregp, Chi, Eduardo Morales Alberti, bucefal91: Drupal Current theme condition plugin should provide an option to select all themes

--- core/modules/block/tests/src/FunctionalJavascript/BlockAddTest.php
+++ core/modules/block/tests/src/FunctionalJavascript/BlockAddTest.php
@@ -37,13 +37,11 @@ class BlockAddTest extends WebDriverTestBase {
     $this->drupalGet('admin/structure/block/add/system_powered_by_block');
     $assert_session = $this->assertSession();
 
-    // Click on Pages condition because, current theme
-    // have the same "Theme" label.
-    $assert_session->linkExists('Pages');
-    $this->clickLink('Pages');
+    // Using selector because the label collides with current theme condition.
+    $theme_select = $this->cssSelect('.block-form > .form-item-theme select');
+    $this->assertCount(1, $theme_select);
+    $theme_select[0]->selectOption('claro');
 
-    // Pick a theme with a region that does not exist in another theme.
-    $assert_session->selectExists('Theme')->selectOption('claro');
     $assert_session->assertWaitOnAjaxRequest();
     $assert_session->selectExists('Region')->selectOption('pre_content');
     $assert_session->assertWaitOnAjaxRequest();
