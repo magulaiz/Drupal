5fb12cdd28f4ed7ecaffb218719ef51921e84cae
Andreas Hennings <andreas@dqxtech.net>
2023-07-08 05:43:39 +0200

    Issue #3368812: Conversion: workspaces_entity_update(), part I.

--- core/modules/workspaces/src/EntityOperations.php
+++ core/modules/workspaces/src/EntityOperations.php
@@ -196,9 +196,8 @@ class EntityOperations implements ContainerInjectionInterface {
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity that was just saved.
-   *
-   * @see hook_entity_update()
    */
+  #[Hook('entity_update')]
   public function entityUpdate(EntityInterface $entity) {
     // Only run if the entity type can belong to a workspace and we are in a
     // non-default workspace.
