ff1f2633e72bc96afd2a97bfbda251e22b463202
Andreas Hennings <andreas@dqxtech.net>
2023-07-08 05:57:17 +0200

    Issue #3368812: Conversion: workspaces_workspace_predelete(), part II.

--- core/modules/workspaces/src/WorkspaceRepository.php
+++ core/modules/workspaces/src/WorkspaceRepository.php
@@ -145,7 +145,7 @@ class WorkspaceRepository implements WorkspaceRepositoryInterface {
   /**
    * {@inheritdoc}
    */
-  #[Hook('workspace_(insert|update)')]
+  #[Hook('workspace_(insert|update|predelete)')]
   public function resetCache() {
     $this->cache->invalidate('workspace_tree');
     $this->tree = NULL;
