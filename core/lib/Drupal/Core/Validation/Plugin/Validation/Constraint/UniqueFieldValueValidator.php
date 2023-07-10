2557c24153c8a29631958b7ec64c872d760a9b68
Wim Leers <wim.leers@acquia.com>
2023-07-10 14:32:22 +0200

    Revert changes to `UniqueFieldValueValidator`.

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/UniqueFieldValueValidator.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/UniqueFieldValueValidator.php
@@ -7,18 +7,19 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Validates that a field is unique for the given entity type.
- *
- * Supports top-level properties on both content and config entity types.
  */
 class UniqueFieldValueValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
    */
-  public function validate($items_or_value, Constraint $constraint) {
-    $field_name = $this->context->getPropertyPath();
+  public function validate($items, Constraint $constraint) {
+    if (!$item = $items->first()) {
+      return;
+    }
+    $field_name = $items->getFieldDefinition()->getName();
     /** @var \Drupal\Core\Entity\EntityInterface $entity */
-    $entity = $this->context->getRoot()->getValue();
+    $entity = $items->getEntity();
     $entity_type_id = $entity->getEntityTypeId();
     $id_key = $entity->getEntityType()->getKey('id');
 
@@ -32,35 +33,18 @@ class UniqueFieldValueValidator extends ConstraintValidator {
       $query->condition($id_key, $entity_id, '<>');
     }
 
-    $value = is_array($items_or_value) ? $items_or_value->first()->value : $items_or_value;
-    $matching_entity_ids = $query
-      ->condition($field_name, $value)
+    $value_taken = (bool) $query
+      ->condition($field_name, $item->value)
+      ->range(0, 1)
+      ->count()
       ->execute();
 
-    if (empty($matching_entity_ids)) {
-      return;
-    }
-
-    if ($entity->isNew()) {
+    if ($value_taken) {
       $this->context->addViolation($constraint->message, [
-        '%value' => $value,
+        '%value' => $item->value,
         '@entity_type' => $entity->getEntityType()->getSingularLabel(),
-        '@field_name' => is_array($items_or_value)
-          ? $items_or_value->getFieldDefinition()->getLabel()
-          : $field_name,
+        '@field_name' => $items->getFieldDefinition()->getLabel(),
       ]);
-    }
-    // If not new, the only match must be *this* entity.
-    elseif ($entity_id != reset($matching_entity_ids)) {
-      // @todo decide how to handle this without breaking BC — I think logging? For now, just an exception to allow making it reliable.
-      throw new \Exception(sprintf(
-        'The existing %s entity (ID: %s) is being validated and it violates a uniqueness constraint: %s have the same value for the "%s" field: "%s".',
-        $entity_type_id,
-        $entity_id,
-        implode($matching_entity_ids),
-        $field_name,
-        (string) $value
-      ));
     }
   }
 
