b8221c0d4c882dec64bffb975ad1ceb8aded576b
andrew farquharson <farquharson.a@gmail.com>
2023-07-11 01:04:24 +0100

    add inline explanatory comments

--- core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
+++ core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
@@ -12,6 +12,8 @@
     // CKEditor 5 in modals can work as expected.
     // @see https://api.jqueryui.com/dialog/#method-_allowInteraction
     _allowInteraction(event) {
+      // Fixes a console error message generated in Firefox only
+      // @see https://www.drupal.org/project/drupal/issues/3351600
       if (event.target.classList === undefined) {
         return this._super(event);
       }
