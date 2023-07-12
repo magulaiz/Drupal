757df66f88c20295752aa7f07e909f9f734efba4
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-07-11 22:22:30 -0500

    Fixed another test.

--- core/modules/layout_builder/tests/modules/layout_builder_element_test/src/EventSubscriber/TestPrepareLayout.php
+++ core/modules/layout_builder/tests/modules/layout_builder_element_test/src/EventSubscriber/TestPrepareLayout.php
@@ -75,7 +75,8 @@ class TestPrepareLayout implements EventSubscriberInterface {
 
       // Node 1 or 2: Append a block to the layout.
       if (in_array($entity->id(), ['1', '2'])) {
-        $section = new Section('layout_onecol');
+        $section = (new Section('layout_onecol'))
+          ->setUuid(\Drupal::service('uuid')->generate());
         $section->appendComponent(new SectionComponent('fake-uuid', 'content', [
           'id' => 'static_block',
           'label' => 'Test static block title',
@@ -108,7 +109,8 @@ class TestPrepareLayout implements EventSubscriberInterface {
 
       // Node 1, 2, or 3: Append a block to the layout.
       if (in_array($entity->id(), ['1', '2', '3'])) {
-        $section = new Section('layout_onecol');
+        $section = (new Section('layout_onecol'))
+          ->setUuid(\Drupal::service('uuid')->generate());
         $section->appendComponent(new SectionComponent('fake-uuid', 'content', [
           'id' => 'static_block_two',
           'label' => 'Test second static block title',
