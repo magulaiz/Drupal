4f480cc3336482d85100b06aa7fbd655d545980b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-07-14 17:26:25 +0100

    Fixed deprecation message.

--- core/modules/user/src/EntityOwnerTrait.php
+++ core/modules/user/src/EntityOwnerTrait.php
@@ -89,7 +89,7 @@ trait EntityOwnerTrait {
    * @see https://www.drupal.org/node/3374738
    */
   public static function getDefaultEntityOwner() {
-    @trigger_error('The ::getCurrentUserId method is deprecated in 11.0.x and will be removed before 12.0.0.', E_USER_DEPRECATED);
+    @trigger_error('The ::getDefaultEntityOwner method is deprecated in 10.2.x and will be removed before 11.0.0.', E_USER_DEPRECATED);
     return \Drupal::currentUser()->id();
   }
 
