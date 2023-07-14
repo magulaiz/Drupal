c383d0f2385b984266c069751eeb0417be789e7a
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-07-15 07:21:56 +1000

    Fix test

--- core/modules/forum/tests/src/Kernel/ForumIndexTest.php
+++ core/modules/forum/tests/src/Kernel/ForumIndexTest.php
@@ -46,7 +46,12 @@ final class ForumIndexTest extends KernelTestBase {
   public function testForumIndexIndex(): void {
     $schema = \Drupal::database()->schema();
     $this->assertTrue($schema->tableExists('forum_index'));
-    $this->assertTrue($schema->indexExists('forum_index', 'PRIMARY'));
+    // We can't reliably call ::indexExists for each database driver as sqlite
+    // doesn't have named indexes for primary keys like mysql (PRIMARY) and
+    // pgsql (pkey).
+    $find_primary_key_columns = new \ReflectionMethod(get_class($schema), 'findPrimaryKeyColumns');
+    $find_primary_key_columns->setAccessible(TRUE);
+    $this->assertEquals(['nid', 'tid'], $find_primary_key_columns->invoke($schema, 'forum_index'));
   }
 
 }
