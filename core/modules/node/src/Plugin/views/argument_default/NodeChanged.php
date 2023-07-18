d4fc4a1550c4cc427cdcac36ff4054ab1bc9798f
Lennard Swaneveld <lennard@ezcompany.nl>
2023-07-18 20:00:43 +0200

    Add base class for node date argument defaults

--- core/modules/node/src/Plugin/views/argument_default/NodeChanged.php
+++ core/modules/node/src/Plugin/views/argument_default/NodeChanged.php
@@ -2,14 +2,7 @@
 
 namespace Drupal\node\Plugin\views\argument_default;
 
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Cache\CacheableDependencyInterface;
-use Drupal\Core\Datetime\DateFormatterInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\node\NodeInterface;
-use Drupal\views\Plugin\views\argument\Date;
-use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the 'changed' time of the current node as default argument value.
@@ -21,67 +14,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   title = @Translation("Current node 'changed' time")
  * )
  */
-class NodeChanged extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
-
-  /**
-   * Constructs a new NodeChanged instance.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
-   *   The route match.
-   * @param \Drupal\Core\Datetime\DateFormatterInterface $dateFormatter
-   *   The date formatter service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, protected RouteMatchInterface $routeMatch, protected DateFormatterInterface $dateFormatter) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
+class NodeChanged extends NodeDateArgumentDefaultPluginBase {
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('current_route_match'),
-      $container->get('date.formatter')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getArgument(): string|bool {
-    // Return the current node changed time if a current node can be found.
-    if (($node = $this->routeMatch->getParameter('node')) && $node instanceof NodeInterface) {
-
-      // The Date argument handlers provide their own format strings, otherwise
-      // use a default.
-      $format = $this->argument instanceof Date ? $this->argument->getArgFormat() : 'Y-m-d';
-
-      return $this->dateFormatter->format($node->getChangedTime(), 'custom', $format);
-    }
-    return FALSE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheContexts() {
-    return ['url'];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheMaxAge() {
-    return Cache::PERMANENT;
+  protected function getNodeDateValue(NodeInterface $node): int {
+    return $node->getChangedTime();
   }
 
 }
--- core/modules/node/src/Plugin/views/argument_default/NodeCreated.php
+++ core/modules/node/src/Plugin/views/argument_default/NodeCreated.php
@@ -2,14 +2,7 @@
 
 namespace Drupal\node\Plugin\views\argument_default;
 
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Cache\CacheableDependencyInterface;
-use Drupal\Core\Datetime\DateFormatterInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\node\NodeInterface;
-use Drupal\views\Plugin\views\argument\Date;
-use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the created time of the current node as default argument value.
@@ -21,84 +14,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   title = @Translation("Current node 'created' time")
  * )
  */
-class NodeCreated extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
-
-  /**
-   * The route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
-   * The date formatter service.
-   *
-   * @var \Drupal\Core\Datetime\DateFormatterInterface
-   */
-  protected $dateFormatter;
-
-  /**
-   * Constructs a new NodeCreated instance.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The route match.
-   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
-   *   The date formatter service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match, DateFormatterInterface $date_formatter) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->routeMatch = $route_match;
-    $this->dateFormatter = $date_formatter;
-  }
+class NodeCreated extends NodeDateArgumentDefaultPluginBase {
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('current_route_match'),
-      $container->get('date.formatter')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getArgument() {
-    // Return the current node creation time if a current node can be found.
-    if (($node = $this->routeMatch->getParameter('node')) && $node instanceof NodeInterface) {
-
-      // The Date argument handlers provide their own format strings, otherwise
-      // use a default.
-      $format = $this->argument instanceof Date ? $this->argument->getArgFormat() : 'Y-m-d';
-
-      return $this->dateFormatter->format($node->getCreatedTime(), 'custom', $format);
-    }
-    return FALSE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheContexts() {
-    return ['url'];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheMaxAge() {
-    return Cache::PERMANENT;
+  protected function getNodeDateValue(NodeInterface $node): int {
+    return $node->getCreatedTime();
   }
 
 }
