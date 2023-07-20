d2c164f62b75f6eeb75224852a3a64bb3a1478a0
znerol <znerol@63999.no-reply.drupal.org>
2023-07-20 21:41:13 +0200

    Moving around sleeps a bit

--- core/modules/workspaces/tests/src/Functional/PathWorkspacesTest.php
+++ core/modules/workspaces/tests/src/Functional/PathWorkspacesTest.php
@@ -93,10 +93,6 @@ class PathWorkspacesTest extends BrowserTestBase {
     $path = $edit['path[0][alias]'];
     $this->assertAccessiblePaths([$path]);
 
-    // The \Drupal\path_alias\AliasWhitelist service performs cache clears after
-    // Drupal has flushed the response to the client; wait for this to finish.
-    sleep(1);
-
     // Check that the 'preload-paths' cache includes the active workspace ID in
     // the cache key.
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:stage:/node/1'));
@@ -110,6 +106,11 @@ class PathWorkspacesTest extends BrowserTestBase {
     // Publish the workspace and check that the alias can be accessed in Live.
     $stage->publish();
     $this->assertAccessiblePaths([$path]);
+
+    // The \Drupal\path_alias\AliasWhitelist service performs cache clears after
+    // Drupal has flushed the response to the client; wait for this to finish.
+    sleep(1);
+
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:/node/1'));
   }
 
@@ -137,10 +138,6 @@ class PathWorkspacesTest extends BrowserTestBase {
     $path = $edit['path[0][alias]'];
     $this->assertAccessiblePaths([$path]);
 
-    // The \Drupal\path_alias\AliasWhitelist service performs cache clears after
-    // Drupal has flushed the response to the client; wait for this to finish.
-    sleep(1);
-
     // Check that the 'preload-paths' cache includes the active workspace ID in
     // the cache key.
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:stage:/node/1'));
@@ -155,8 +152,14 @@ class PathWorkspacesTest extends BrowserTestBase {
     // Publish the workspace and check that the alias can be accessed in Live.
     $this->drupalLogin($this->rootUser);
     $stage->publish();
+
     $this->drupalLogout();
     $this->assertAccessiblePaths([$path]);
+
+    // The \Drupal\path_alias\AliasWhitelist service performs cache clears after
+    // Drupal has flushed the response to the client; wait for this to finish.
+    sleep(1);
+
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:/node/1'));
   }
 
