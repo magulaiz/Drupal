f6a893aad2ba222766e702cbc4d8274aba47cfc2
mondrake <mondrake.org@gmail.com>
2023-07-23 18:25:05 +0200

    fix typo

--- core/modules/search/src/SearchQueryFactory.php
+++ core/modules/search/src/SearchQueryFactory.php
@@ -42,7 +42,7 @@ class SearchQueryFactory {
     // @todo remove this BC layer in drupal:11.0.0.
     $class = $connection->getConnectionOptions()['namespace'] . '\\SearchQuery';
     if (class_exists($class)) {
-      @trigger_error("Invoking {$class} outside of a backend overrideable service is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Include the driver class in a backend overrideable service instead. See https://www.drupal.org/node/3217534", E_USER_DEPRECATED);
+      @trigger_error("Invoking {$class} outside of a backend overridable service is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Include the driver class in a backend overridable service instead. See https://www.drupal.org/node/3217534", E_USER_DEPRECATED);
       return new $class($query, $connection);
     }
     // @todo end
--- core/tests/Drupal/KernelTests/Core/Database/SelectExtenderTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/SelectExtenderTest.php
@@ -166,7 +166,7 @@ class SelectExtenderTest extends KernelTestBase {
    * @group legacy
    */
   public function testExtendWithLocalClasses(string $expected, string $namespace, string $extend): void {
-    $this->expectDeprecation('Invoking Drupal\\corefake\\Driver\\Database\\corefakeWithAllCustomClasses\\%A outside of a backend overrideable service is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Include the driver class in a backend overrideable service instead. See https://www.drupal.org/node/3217534');
+    $this->expectDeprecation('Invoking Drupal\\corefake\\Driver\\Database\\corefakeWithAllCustomClasses\\%A outside of a backend overridable service is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Include the driver class in a backend overridable service instead. See https://www.drupal.org/node/3217534');
     $this->expectDeprecation('Calling %A::__construct without the %A argument is deprecated in drupal:9.4.0 and will be required in drupal:10.0.0. Use the relevant service to instantiate extenders. See https://www.drupal.org/node/3218001');
 
     $additional_class_loader = new ClassLoader();
