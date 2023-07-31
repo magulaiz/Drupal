f2298a1521ebbfd953fc36030a6700fee1af3b39
bnjmnm <benm@umich.edu>
2023-07-31 13:23:51 -0400

    document callback

--- core/modules/user/src/Event/PermissionsListFilterEvent.php
+++ core/modules/user/src/Event/PermissionsListFilterEvent.php
@@ -28,6 +28,27 @@ class PermissionsListFilterEvent extends Event {
    *
    * @param callable $callback
    *   The filter callback.
+   *
+   *   This is a callback used by array_filter and applied to a permissions
+   *   array. The ARRAY_FILTER_USE_BOTH option is used, so the function
+   *   is effectively `callable(array, string): bool`.
+   *      * @code
+   * // Example:
+   * function filterDeleteAndBlockContent($permission_array, $permission_name) {
+   *   // Remove any permission with a name that starts with 'delete'
+   *   if (strpos($permission_name, 'delete') === 0) {
+   *     return FALSE;
+   *   }
+   *   // Remove any permission from the Block Content module.
+   *   if ($permission_array['provider'] === 'block_content) {
+   *     return FALSE;
+   *   }
+   *   return TRUE;
+   * }
+   *
+   * $permission_list_filter_event->filter('filterDeleteAndBlockContent');
+   * @endcode
+   *
    */
   public function filter(callable $callback): void {
     $this->permissions = array_filter($this->permissions, $callback, ARRAY_FILTER_USE_BOTH);
