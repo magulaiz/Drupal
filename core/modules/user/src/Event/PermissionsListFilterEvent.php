e8eb906637c3fbe0985e4d1a6a2a76a9446affd0
bnjmnm <benm@umich.edu>
2023-07-31 13:39:12 -0400

    indent

--- core/modules/user/src/Event/PermissionsListFilterEvent.php
+++ core/modules/user/src/Event/PermissionsListFilterEvent.php
@@ -32,21 +32,22 @@ class PermissionsListFilterEvent extends Event {
    *   This is a callback used by array_filter and applied to a permissions
    *   array. The ARRAY_FILTER_USE_BOTH option is used, so the function
    *   is effectively `callable(array, string): bool`.
-   * @code
-   * // Example:
-   * function filterDeleteAndBlockContent($permission_array, $permission_name) {
-   *   // Remove any permission with a name that starts with 'delete'
-   *   if (strpos($permission_name, 'delete') === 0) {
-   *     return FALSE;
-   *   }
-   *   // Remove any permission from the Block Content module.
-   *   if ($permission_array['provider'] === 'block_content) {
-   *     return FALSE;
-   *   }
-   *   return TRUE;
-   * }
    *
-   * $permission_list_filter_event->filter('filterDeleteAndBlockContent');
+   *    @code
+   *   // Example:
+   *   function filterDeleteAndBlockContent($permission_array, $permission_name) {
+   *     // Remove any permission with a name that starts with 'delete'
+   *     if (strpos($permission_name, 'delete') === 0) {
+   *       return FALSE;
+   *     }
+   *     // Remove any permission from the Block Content module.
+   *     if ($permission_array['provider'] === 'block_content) {
+   *       return FALSE;
+   *     }
+   *     return TRUE;
+   *   }
+   *
+   *   $permission_list_filter_event->filter('filterDeleteAndBlockContent');
    * @endcode
    *
    */
