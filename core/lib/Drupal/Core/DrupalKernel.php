09c9190bde35b6ba771770cf33b62cac5ea5aba4
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2023-08-01 10:57:06 +0530

    Removed unnecessary code

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -584,9 +584,6 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
 
     $this->loadLegacyIncludes();
 
-    // Load all enabled modules.
-    // Uncomment $this->container->get('module_handler')->loadAll();
-
     // Register stream wrappers.
     $this->container->get('stream_wrapper_manager')->register();
 
--- core/lib/Drupal/Core/Theme/ThemeManager.php
+++ core/lib/Drupal/Core/Theme/ThemeManager.php
@@ -130,14 +130,6 @@ class ThemeManager implements ThemeManagerInterface {
     static $default_attributes;
 
     $active_theme = $this->getActiveTheme();
-
-    // If called before all modules are loaded, we do not necessarily have a
-    // full theme registry to work with, and therefore cannot process the theme
-    // request properly. See also \Drupal\Core\Theme\Registry::get().
-    if (!$this->moduleHandler->isLoaded() && !defined('MAINTENANCE_MODE')) {
-      // Uncomment throw new \Exception('The theme implementations may not be rendered until all modules are loaded.');
-    }
-
     $theme_registry = $this->themeRegistry->getRuntime();
 
     // If an array of hook candidates were passed, use the first one that has an
