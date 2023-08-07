9d096ec4f2c80ba5594715d80078174c20b82037
Stephen Mustgrave <smustgrave@gmail.com>
2023-08-07 13:45:01 -0400

    Add test back

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/PerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/PerformanceTest.php
@@ -26,4 +26,16 @@ class PerformanceTest extends PerformanceTestBase {
     $this->assertSame(1, $this->scriptCount);
   }
 
+  /**
+   * Load the front page as a user with access to Toolbar.
+   */
+  public function testFrontPagePerformance(): void {
+    $admin_user = $this->drupalCreateUser(['access toolbar']);
+    $this->drupalLogin($admin_user);
+    $this->drupalGet('<front>');
+    $this->assertSession()->pageTextContains('Umami');
+    $this->assertSame(2, $this->stylesheetCount);
+    $this->assertSame(1, $this->scriptCount);
+  }
+
 }
