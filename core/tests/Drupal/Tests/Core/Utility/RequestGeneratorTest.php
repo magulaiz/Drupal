f63676df1a86235d7effd5c17c026a17cea0bab7
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2023-08-08 18:53:26 +0530

    add new test for the service

---
+++ core/tests/Drupal/Tests/Core/Utility/RequestGeneratorTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Drupal\Tests\Core\Utility;
+
+use Drupal\Core\Routing\RouteObjectInterface;
+use Drupal\Core\Utility\RequestGenerator;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\HttpFoundation\InputBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Route;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Utility\RequestGenerator
+ * @group Utility
+ */
+class RequestGeneratorTest extends UnitTestCase {
+
+  /**
+   * The request generator.
+   *
+   * @var \Drupal\Core\Utility\RequestGenerator
+   */
+  protected $requestGenerator;
+
+  /**
+   * The mocked path processor.
+   *
+   * @var \Drupal\Core\PathProcessor\InboundPathProcessorInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  protected $pathProcessor;
+
+  /**
+   * The mocked current path.
+   *
+   * @var \Drupal\Core\Path\CurrentPathStack|\PHPUnit\Framework\MockObject\MockObject
+   */
+  protected $currentPath;
+
+  /**
+   * The request matching mock object.
+   *
+   * @var \Symfony\Component\Routing\Matcher\RequestMatcherInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  protected $requestMatcher;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    $this->pathProcessor = $this->createMock('\Drupal\Core\PathProcessor\InboundPathProcessorInterface');
+    $this->currentPath = $this->createMock('Drupal\Core\Path\CurrentPathStack');
+    $this->requestMatcher = $this->createMock('\Symfony\Component\Routing\Matcher\RequestMatcherInterface');
+    $this->requestGenerator = new RequestGenerator(
+      $this->pathProcessor,
+      $this->currentPath,
+      $this->requestMatcher,
+    );
+    parent::setUp();
+  }
+
+  /**
+   * Data provider for testGenerateRequestForPath().
+   *
+   * @return array
+   *   The test cases.
+   */
+  public function providerTestGenerateRequestForPath() {
+    $path = '/any/path';
+    return [
+      "request for a path with no paths to skip" => [
+        $path,
+        [],
+        ['processInbound' => 1, 'matchRequest' => 1],
+        TRUE,
+      ],
+      "request for a path which needs to be skipped " => [
+        $path,
+        [$path => TRUE],
+        ['processInbound' => 0, 'matchRequest' => 0],
+        FALSE,
+      ],
+      "request for a path with other path which needs to be skipped" => [
+        $path,
+        ['/other/path' => TRUE],
+        ['processInbound' => 1, 'matchRequest' => 1],
+        TRUE,
+      ],
+    ];
+  }
+
+  /**
+   * Tests generateRequestForPath().
+   *
+   * @dataProvider providerTestGenerateRequestForPath
+   * @covers ::generateRequestForPath
+   */
+  public function testGenerateRequestForPath($path, $exclude, $methods_called, $request_generated) {
+    $route = new Route($path);
+    $this->pathProcessor->expects($this->exactly($methods_called['processInbound']))
+      ->method('processInbound')
+      ->willReturn($path);
+    $this->requestMatcher->expects($this->exactly($methods_called['matchRequest']))
+      ->method('matchRequest')
+      ->willReturnCallback(function (Request $request) use ($route, $path) {
+        if ($request->getPathInfo() == $path) {
+          return [
+            RouteObjectInterface::ROUTE_NAME => 'User Example',
+            RouteObjectInterface::ROUTE_OBJECT => $route,
+            '_raw_variables' => new InputBag([]),
+          ];
+        }
+      });
+    $request = $this->requestGenerator->generateRequestForPath($path, $exclude);
+    if ($request_generated) {
+      $this->assertNotNull($request);
+      $this->assertSame($request->getPathInfo(), $path);
+    }
+    else {
+      $this->assertNull($request);
+    }
+  }
+
+  /**
+   * Data provider for testGenerateRequestForPathWithException().
+   *
+   * @return array
+   *   The test cases.
+   */
+  public function providerTestGenerateRequestForPathWithException() {
+    return [
+      'ResourceNotFoundException' => [ResourceNotFoundException::class, ''],
+      'MethodNotAllowedException' => [MethodNotAllowedException::class, []],
+      'AccessDeniedHttpException' => [AccessDeniedHttpException::class, ''],
+    ];
+  }
+
+  /**
+   * Tests generateRequestForPath() with exceptions during request matching.
+   *
+   * @dataProvider providerTestGenerateRequestForPathWithException
+   * @covers ::generateRequestForPath
+   */
+  public function testGenerateRequestForPathWithException($exception_class, $exception_argument) {
+    $path = '/example';
+    $exclude = [];
+    $this->pathProcessor->expects($this->any())
+      ->method('processInbound')
+      ->willReturn($path);
+    $this->requestMatcher->expects($this->once())
+      ->method('matchRequest')
+      ->will($this->throwException(new $exception_class($exception_argument)));
+    $request = $this->requestGenerator->generateRequestForPath($path, $exclude);
+    $this->assertNull($request);
+  }
+
+}
