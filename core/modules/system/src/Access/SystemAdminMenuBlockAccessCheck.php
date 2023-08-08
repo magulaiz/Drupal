37f2d554781a48c3a1cdbba1aab2367f68c4b87b
omkar podey <omkar.podey@acquia.com>
2023-08-08 16:58:16 +0530

    revert allowed

--- core/modules/system/src/Access/SystemAdminMenuBlockAccessCheck.php
+++ core/modules/system/src/Access/SystemAdminMenuBlockAccessCheck.php
@@ -70,7 +70,7 @@ class SystemAdminMenuBlockAccessCheck implements AccessInterface {
       if ($route) {
         return AccessResult::allowedIf(empty($route->getRequirement('_access_admin_menu_block_page')));
       }
-      return AccessResult::allowed();
+      return AccessResult::neutral();
     }
 
     foreach ($tree as $menu_link_route_id => $element) {
