83f5df8b9cbeeb069908d150210b29c00ae4a170
Lauri Eskola <lauri.eskola@acquia.com>
2023-08-14 14:57:33 +0300

    cspell 🤷‍♂️

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// cSpell:words downcasted linkimageediting emptyelement downcastdispatcher
+// cSpell:words datafilter downcasted linkimageediting emptyelement downcastdispatcher
 import { Plugin } from 'ckeditor5/src/core';
 import { setViewAttributes } from '@ckeditor/ckeditor5-html-support/src/utils';
 
