d8e2136ff07b9692c749f0b251deaa7d9f381de6
Ted Bowman <ted+git@tedbow.com>
2023-08-15 10:50:52 -0400

    get link for route to check

--- core/modules/system/src/Access/SystemAdminMenuBlockAccessCheck.php
+++ core/modules/system/src/Access/SystemAdminMenuBlockAccessCheck.php
@@ -5,6 +5,8 @@ namespace Drupal\system\Access;
 use Drupal\Core\Access\AccessManagerInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Menu\MenuLinkInterface;
+use Drupal\Core\Menu\MenuLinkManagerInterface;
 use Drupal\Core\Menu\MenuLinkTreeInterface;
 use Drupal\Core\Menu\MenuTreeParameters;
 use Drupal\Core\Routing\Access\AccessInterface;
@@ -26,8 +28,10 @@ class SystemAdminMenuBlockAccessCheck implements AccessInterface {
    *   The menu link tree service.
    * @param \Drupal\Core\Routing\AccessAwareRouter $router
    *   The router service.
+   * @param \Drupal\Core\Menu\MenuLinkManagerInterface $menuLinkManager
+   *   The menu link manager service.
    */
-  public function __construct(private readonly AccessManagerInterface $accessManager, private readonly MenuLinkTreeInterface $menuLinkTree, private readonly AccessAwareRouter $router) {
+  public function __construct(private readonly AccessManagerInterface $accessManager, private readonly MenuLinkTreeInterface $menuLinkTree, private readonly AccessAwareRouter $router, private readonly MenuLinkManagerInterface $menuLinkManager) {
   }
 
   /**
@@ -42,23 +46,30 @@ class SystemAdminMenuBlockAccessCheck implements AccessInterface {
    *   The access result.
    */
   public function access(RouteMatchInterface $route_match, AccountInterface $account): AccessResultInterface {
-    return $this->hasAccessToChildRoutes($route_match->getRouteName(), $account)->cachePerPermissions();
+    // Load links matching this route.
+    $links = $this->menuLinkManager->loadLinksByRoute($route_match->getRouteName(), [], 'admin');
+    // Select the first matching link.
+    if (empty($links)) {
+      return AccessResult::neutral();
+    }
+    return $this->hasAccessToChildMenuItems(reset($links), $account)->cachePerPermissions();
   }
 
   /**
    * Check that the given route has access to one of it's child routes.
    *
-   * @param string $route_name
-   *   The route ID.
+   * @param \Drupal\Core\Menu\MenuLinkInterface $link
+   *   The menu link.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The account.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function hasAccessToChildRoutes(string $route_name, AccountInterface $account): AccessResultInterface {
+  protected function hasAccessToChildMenuItems(MenuLinkInterface $link, AccountInterface $account): AccessResultInterface {
     $parameters = new MenuTreeParameters();
-    $parameters->setRoot($route_name)
+    $link_id = $link->getPluginId();
+    $parameters->setRoot($link_id)
       ->excludeRoot()
       ->setTopLevelOnly()
       ->onlyEnabledLinks();
@@ -66,7 +77,7 @@ class SystemAdminMenuBlockAccessCheck implements AccessInterface {
     $tree = $this->menuLinkTree->load(NULL, $parameters);
 
     if (empty($tree)) {
-      $route = $this->router->getRouteCollection()->get($route_name);
+      $route = $this->router->getRouteCollection()->get($link->getRouteName());
       if ($route) {
         return AccessResult::allowedIf(empty($route->getRequirement('_access_admin_menu_block_page')));
       }
@@ -80,7 +91,7 @@ class SystemAdminMenuBlockAccessCheck implements AccessInterface {
       }
 
       // Check if it's again route with inaccessible children.
-      return AccessResult::allowedIf($this->hasAccessToChildRoutes($url->getRouteName(), $account)->isAllowed());
+      return AccessResult::allowedIf($this->hasAccessToChildMenuItems($element->link, $account)->isAllowed());
     }
 
     return AccessResult::neutral();
