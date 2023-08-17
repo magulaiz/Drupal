89eab1ea3e535dacbb555974160fa607d5b04a9b
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2023-08-17 14:28:21 +0530

    Changes done only for Link

--- core/lib/Drupal/Core/Render/HtmlResponseAttachmentsProcessor.php
+++ core/lib/Drupal/Core/Render/HtmlResponseAttachmentsProcessor.php
@@ -216,7 +216,7 @@ class HtmlResponseAttachmentsProcessor implements AttachmentsResponseProcessorIn
 
       // Remove duplicate headers.
       foreach ($attached['http_header'] as $header_key => $header_value) {
-        if (in_array($header_value[1], $headers)) {
+        if (in_array($header_value[1], $headers) && $header_value[0] === 'Link') {
           assert(isset($attached['http_header'][$header_key]), 'Duplicate header ' . $header_value[1] . ' found.');
           unset($attached['http_header'][$header_key]);
         }
