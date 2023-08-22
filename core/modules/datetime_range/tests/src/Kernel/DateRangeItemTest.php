f5aef0a83315fb557c8859f1d99402d9bce85bcc
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-08-22 13:51:56 +0530

    Created branch for 11.x

--- core/modules/datetime_range/tests/src/Kernel/DateRangeItemTest.php
+++ core/modules/datetime_range/tests/src/Kernel/DateRangeItemTest.php
@@ -103,4 +103,60 @@ class DateRangeItemTest extends FieldKernelTestBase {
     $this->assertEquals('12:00:00', $end_date->format('H:i:s'));
   }
 
+  /**
+   * Test optional end date.
+   */
+  public function testOptionalEndDate() {
+    $field_name = $this->fieldStorage->getName();
+
+    $this->fieldStorage->setSettings([
+      'datetime_type' => DateRangeItem::DATETIME_TYPE_DATE,
+      'optional_end_date' => FALSE,
+    ])
+      ->save();
+
+    $value = [
+      'value' => '2016-09-21',
+      'end_value' => NULL,
+    ];
+
+    $end_date_without_start_date_value = [
+      'value' => NULL,
+      'end_value' => '2016-09-23',
+    ];
+
+    // Verify entity without optional_end_date enabled.
+    $entity = EntityTest::create([
+      'name' => $this->randomString(),
+      $field_name => $value,
+    ]);
+    $this->assertNotEquals(count($entity->validate()), 0);
+
+    // Verify entity with the optional_end_date enabled.
+    $this->fieldStorage->setSetting('optional_end_date', TRUE)
+      ->save();
+    $entity = EntityTest::create([
+      'name' => $this->randomString(),
+      $field_name => $value,
+    ]);
+    $this->entityValidateAndSave($entity);
+
+    // Verify changing the date value.
+    $this->assertEquals($entity->{$field_name}->end_value, $value['end_value']);
+
+    // Verify entity with the optional_end_date enabled when setting an
+    // end_date but no start date.
+    $this->fieldStorage->setSetting('optional_end_date', TRUE)
+      ->save();
+    $entity = EntityTest::create([
+      'name' => $this->randomString(),
+      $field_name => $end_date_without_start_date_value,
+    ]);
+    $this->entityValidateAndSave($entity);
+
+    // Verify changing the date and end_date value.
+    $this->assertEquals($entity->{$field_name}->end_value, $value['end_value']);
+    $this->assertEquals($entity->{$field_name}->value, $value['value']);
+  }
+
 }
