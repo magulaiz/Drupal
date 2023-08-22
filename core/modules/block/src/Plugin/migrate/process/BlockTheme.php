c9cc699cd576994a22845bae91d5338b72c2ed63
Michael Strelan <mstrelan@gmail.com>
2023-08-23 09:17:08 +1000

    3382742

--- core/modules/block/src/Plugin/migrate/process/BlockTheme.php
+++ core/modules/block/src/Plugin/migrate/process/BlockTheme.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\block\Plugin\migrate\process;
 
+use Drupal\Component\Utility\DeprecationHelperTrait;
 use Drupal\Core\Config\Config;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
@@ -16,6 +17,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class BlockTheme extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  use DeprecationHelperTrait;
 
   /**
    * Contains the system.theme configuration object.
@@ -45,13 +48,12 @@ class BlockTheme extends ProcessPluginBase implements ContainerFactoryPluginInte
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, Config|MigrationInterface $theme_config, array|Config $themes) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    if ($theme_config instanceof MigrationInterface) {
-      @trigger_error('Calling ' . __CLASS__ . '::__construct() with the $migration argument is deprecated in drupal:10.1.0 and is removed in drupal:11.0.0. See https://www.drupal.org/node/3323212', E_USER_DEPRECATED);
-      $theme_config = func_get_arg(4);
-      $themes = func_get_arg(5);
-    }
-    $this->themeConfig = $theme_config;
-    $this->themes = $themes;
+    $this->removeDeprecatedParam(
+      func_get_args(),
+      3,
+      MigrationInterface::class,
+      'Calling ' . __CLASS__ . '::__construct() with the $migration argument is deprecated in drupal:10.1.0 and is removed in drupal:11.0.0. See https://www.drupal.org/node/3323212',
+    );
   }
 
   /**
