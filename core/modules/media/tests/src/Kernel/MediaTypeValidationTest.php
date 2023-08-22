8cee6664148124d13506220cc48d8047d6d386be
Phéna Proxima <adam@phenaproxima.net>
2023-08-22 09:12:41 -0400

    Test media sources

--- core/modules/media/tests/src/Kernel/MediaTypeValidationTest.php
+++ core/modules/media/tests/src/Kernel/MediaTypeValidationTest.php
@@ -27,4 +27,18 @@ class MediaTypeValidationTest extends ConfigEntityValidationTestBase {
     $this->entity = $this->createMediaType('test', ['id' => 'test_media']);
   }
 
+  /**
+   * Tests that the media source plugin's existence is validated.
+   */
+  public function testMediaSourceIsValidated(): void {
+    // We need to clear the current source configuration, or we will get
+    // validation errors because the old configuration is not supported by the
+    // new source.
+    $this->entity->set('source_configuration', [])
+      ->set('source', 'invalid');
+    $this->assertValidationErrors([
+      'source' => "The 'invalid' plugin does not exist.",
+    ]);
+  }
+
 }
