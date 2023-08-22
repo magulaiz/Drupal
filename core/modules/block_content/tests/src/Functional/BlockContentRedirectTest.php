89462ee42a246038ecdee142d59b43041b0474b9
Stephen Mustgrave <smustgrave@gmail.com>
2023-08-22 13:24:24 -0400

    Last thread

--- core/modules/block_content/tests/src/Functional/BlockContentRedirectTest.php
+++ core/modules/block_content/tests/src/Functional/BlockContentRedirectTest.php
@@ -63,7 +63,7 @@ class BlockContentRedirectTest extends BlockContentTestBase {
   public function testBlockContentDeleteRedirect(): void {
     $block = $this->createBlockContent();
     $this->drupalLogin($this->adminUser);
-    $this->expectDeprecation('The path /block/{block_content} is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block/{block_content}/delete. See https://www.drupal.org/node/3320855.');
+    $this->expectDeprecation('The path /block/{block_content}/delete is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use /admin/content/block/{block_content}/delete. See https://www.drupal.org/node/3320855.');
     $this->drupalGet("/block/{$block->id()}/delete");
     $this->assertSession()
       ->pageTextContains("You have been redirected from block/{$block->id()}/delete. Update links, shortcuts, and bookmarks to use admin/content/block/{$block->id()}/delete.");
