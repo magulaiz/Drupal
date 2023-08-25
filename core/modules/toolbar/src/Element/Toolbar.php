df933a5a4f03d4980fa150ebb8d5b60ebcd8ec12
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-08-25 07:54:25 +0530

    Revert the removing of #id

--- core/modules/toolbar/src/Element/Toolbar.php
+++ core/modules/toolbar/src/Element/Toolbar.php
@@ -134,6 +134,7 @@ class Toolbar extends RenderElement {
         '#wrapper_attributes' => [
           'data-drupal-selector' => 'toolbar-extra-item-toggle',
         ],
+        '#id' => 'extra-item-toggle',
         'tab' => [
           '#type' => 'html_tag',
           '#tag' => 'a',
