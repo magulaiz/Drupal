db0fbf9c12c63e5847cd69001ed41e3d545c8e59
Gaurav Mahlawat <gauravmahlawat@Gauravs-MacBook-Pro.local>
2023-08-25 08:09:44 +0530

    Fixed CC failures

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldWidget/OptionsButtonsWidget.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldWidget/OptionsButtonsWidget.php
@@ -59,7 +59,7 @@ class OptionsButtonsWidget extends OptionsWidgetBase {
     $bundles = $items->getItemDefinition()->getSettings();
     $type = implode('', $bundles['handler_settings']['target_bundles']);
     if (count($options) < 1) {
-      $element['#description'] = $this->t('There are no @type types available. Please contact a site administrator.', [
+      $element['#description'] = $this->t('There are no @type types available. Contact the site administrator.', [
         '@type' => $type,
       ]);
     }
