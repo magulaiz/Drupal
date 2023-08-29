fa8a934b4886abe94e0e9d81e7c0f3f225be3037
Kim Pepper <kim@previousnext.com.au>
2023-08-29 16:58:51 +1000

    Catch calling deprecation

--- core/modules/file/tests/src/Functional/SaveUploadTest.php
+++ core/modules/file/tests/src/Functional/SaveUploadTest.php
@@ -439,6 +439,8 @@ class SaveUploadTest extends FileManagedTestBase {
 
   /**
    * Tests file munge handling.
+   *
+   * @group legacy
    */
   public function testHandleFileMunge() {
     // Ensure insecure uploads are disabled for this test.
@@ -583,6 +585,9 @@ class SaveUploadTest extends FileManagedTestBase {
       'files[file_test_upload][]' => \Drupal::service('file_system')->realpath($this->image->getFileUri()),
       'allow_all_extensions' => 'empty_string',
     ];
+
+    // @todo Remove this once the deprecated function is removed.
+    $this->expectDeprecation('_file_save_upload_from_form() is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\file\Upload\FileElementHelper::saveFileUploads instead. See https://www.drupal.org/node/3382414');
 
     $this->drupalGet('file-test/save_upload_from_form_test');
     $this->submitForm($edit, 'Submit');
