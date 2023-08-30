50697e475f3633e6b6cad2db3f2ff52e7c3f8f9c
Ted Bowman <ted+git@tedbow.com>
2023-08-30 15:27:07 -0400

    use fallback for finding links

--- core/modules/system/tests/src/Functional/Menu/MenuAccessTest.php
+++ core/modules/system/tests/src/Functional/Menu/MenuAccessTest.php
@@ -119,12 +119,13 @@ class MenuAccessTest extends BrowserTestBase {
     // This user doesn't have access to any of the child pages, so the parent
     // pages should not be accessible.
     $this->drupalLogin($webUser);
-    $this->assertMenuItemRoutesAccess(403, 'admin/structure', 'admin/people');
-    // As menu_test adds a menu link under config.
-    $this->assertMenuItemRoutesAccess(200, 'admin/config');
+    $this->assertMenuItemRoutesAccess(403, 'admin/structure', 'admin/people', 'admin/config');
 
+    // The test cases below depend on routes, menu items and permissions added
+    // by the menu_test module. It is not enabled before this to ensure that any
+    // other configuration it provides that we don't need for these test cases
+    // does not affect the assertions above.
     $this->container->get('module_installer')->install(['menu_test']);
-
     // Test access to routes in the admin menu. The routes are in a menu tree
     // of the hierarchy:
     // menu_test.parent_test
