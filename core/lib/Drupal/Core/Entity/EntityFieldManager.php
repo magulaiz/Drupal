d35e04e4d4a4ad7c35ecd5235015e20e1d7c94ef
Parag Magunia <32683-pmagunia@users.noreply.drupalcode.org>
2023-08-30 09:50:46 -0400

    Change inheritDoc to inheritdoc as recommended by queue-bot.

--- core/lib/Drupal/Core/Entity/EntityFieldManager.php
+++ core/lib/Drupal/Core/Entity/EntityFieldManager.php
@@ -573,7 +573,7 @@ class EntityFieldManager implements EntityFieldManagerInterface {
   }
 
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
   public function rebuildBundleFieldMap() {
     $map = [];
