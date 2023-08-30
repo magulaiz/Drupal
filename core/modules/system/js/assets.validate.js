08c5100d0b52f845fcc15a34998c23319c1bdcf4
voleger <44159-voleger@users.noreply.drupalcode.org>
2023-08-30 16:49:07 +0300

    Fix prettier issue

--- core/modules/system/js/assets.validate.js
+++ core/modules/system/js/assets.validate.js
@@ -1,46 +1,57 @@
 ((Drupal, drupalSettings) => {
   Drupal.behaviors.systemPerformanceAssetsValidate = {
-    'attach': (context, settings) => {
+    attach: (context, settings) => {
       const validate = () => {
-        const messages = new Drupal.Message(context.querySelector('[data-assets-validate-messages]'));
-        drupalSettings.systemPerformanceAssetsChecklist.forEach(urlInfo => {
+        const messages = new Drupal.Message(
+          context.querySelector('[data-assets-validate-messages]'),
+        );
+        drupalSettings.systemPerformanceAssetsChecklist.forEach((urlInfo) => {
           messages.clear();
           try {
-            fetch(urlInfo['url'])
-              .then(response => {
+            fetch(urlInfo.url)
+              .then((response) => {
                 if (response.status === 404) {
-                  messages.error(Drupal.t(
-                    'Your server is not configured properly to access @name assets. Review Drupal server requirements.',
-                    { '@name': urlInfo['name'] },
-                  ));
+                  messages.error(
+                    Drupal.t(
+                      'Your server is not configured properly to access @name assets. Review Drupal server requirements.',
+                      { '@name': urlInfo.name },
+                    ),
+                  );
                 } else {
-                  messages.add(Drupal.t(
-                    'Your server is configured properly to access @name assets.',
-                    { '@name': urlInfo['name'] },
-                  ));
+                  messages.add(
+                    Drupal.t(
+                      'Your server is configured properly to access @name assets.',
+                      { '@name': urlInfo.name },
+                    ),
+                  );
                 }
               })
-              .catch(error => {
-                messages.warning(Drupal.t(
-                  'Unable to check server requirements for @name assets.',
-                  { '@name': urlInfo['name'] },
-                ));
+              .catch((error) => {
+                messages.warning(
+                  Drupal.t(
+                    'Unable to check server requirements for @name assets.',
+                    { '@name': urlInfo.name },
+                  ),
+                );
               });
           } catch (err) {
-            messages.warning(Drupal.t(
-              'Unable to check server requirements for @name assets.',
-              { '@name': urlInfo['name'] },
-            ));
+            messages.warning(
+              Drupal.t(
+                'Unable to check server requirements for @name assets.',
+                { '@name': urlInfo.name },
+              ),
+            );
           }
         });
       };
-      context.querySelectorAll('[data-assets-validate-button="init"]').forEach(btn => {
-        btn.setAttribute('data-assets-validate-button', '');
-        btn.setAttribute('type', 'button');
-        btn.addEventListener('keyup', validate);
-        btn.addEventListener('click', validate);
-
-      });
+      context
+        .querySelectorAll('[data-assets-validate-button="init"]')
+        .forEach((btn) => {
+          btn.setAttribute('data-assets-validate-button', '');
+          btn.setAttribute('type', 'button');
+          btn.addEventListener('keyup', validate);
+          btn.addEventListener('click', validate);
+        });
     },
   };
 })(Drupal, drupalSettings);
