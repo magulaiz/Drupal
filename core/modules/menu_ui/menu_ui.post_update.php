32be3bc7faf84952d50878959c0a3b505007e2ec
Kunal Sachdev <kunal.sachdev@3685163.no-reply.drupal.org>
2023-08-31 15:16:54 +0530

    fix post_update

--- core/modules/menu_ui/menu_ui.post_update.php
+++ core/modules/menu_ui/menu_ui.post_update.php
@@ -11,14 +11,12 @@
 function menu_ui_post_update_add_link_by_default() {
   $config_factory = \Drupal::configFactory();
   foreach ($config_factory->listAll('node.type.') as $node_type_config_name) {
-    if (!str_contains($node_type_config_name, 'third_party.menu_ui')) {
+    $node_type_settings = $config_factory->getEditable($node_type_config_name);
+    $node_type_third_party_menu_ui_settings = $node_type_settings->get('third_party_settings.menu_ui');
+    if (is_null($node_type_third_party_menu_ui_settings)) {
       continue;
     }
-    $node_type_third_party_menu_ui = $config_factory->getEditable($node_type_config_name);
-    $node_type_third_party_menu_ui_settings = $node_type_third_party_menu_ui->get('settings');
-
     $node_type_third_party_menu_ui_settings['link_by_default'] = FALSE;
-    $node_type_third_party_menu_ui->set('settings', $node_type_third_party_menu_ui_settings);
-    $node_type_third_party_menu_ui->save(TRUE);
+    $node_type_settings->set('third_party_settings.menu_ui', $node_type_third_party_menu_ui_settings)->save(TRUE);
   }
 }
