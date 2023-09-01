ed6eca22dd9cbb17be222993e57b82415a6153cc
omkar podey <omkar.podey@acquia.com>
2023-09-01 14:06:42 +0530

    ignore the deprecation

--- core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
+++ core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
@@ -1,5 +1,5 @@
 <?php
-
+// @phpcs:ignoreFile
 namespace Drupal\datetime_range\Plugin\Field\FieldType;
 
 use Drupal\Core\Field\FieldDefinitionInterface;
@@ -109,9 +109,7 @@ class DateRangeItem extends DateTimeItem {
 
     // Just pick a date in the past year. No guidance is provided by this Field
     // type.
-    // phpcs:disable
     $start = \Drupal::time()->getRequestTime() - mt_rand(0, 86400 * 365) - 86400;
-    // phpcs:enable
     $end = $start + 86400;
     if ($type == static::DATETIME_TYPE_DATETIME) {
       $values['value'] = gmdate(DateTimeItemInterface::DATETIME_STORAGE_FORMAT, $start);
