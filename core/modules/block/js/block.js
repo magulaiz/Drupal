10512ac935d11ce42733e417e86d799a3c10570a
Joao Paulo <joaopauloctga@gmail.com>
2023-09-04 13:37:22 -0300

    Adding unit tests for drag and drop blocks

--- core/modules/block/js/block.js
+++ core/modules/block/js/block.js
@@ -127,15 +127,16 @@
        *
        * @param {jQuery} table
        *   The jQuery object representing the table to inspect.
-       * @param {jQuery} rowObject
+       * @param {Drupal.tableDrag.row} rowObject
        *   The jQuery object representing the table row.
        */
       function updateLastPlaced(table, rowObject) {
         // Remove the color-success class from new block if applicable.
         table.find('.color-success').removeClass('color-success');
+        const $rowObject = $(rowObject);
         if (!rowObject.element.matches('.drag-previous')) {
           table.find('.drag-previous').removeClass('drag-previous');
-          rowObject.addClass('drag-previous');
+          $rowObject.addClass('drag-previous');
         }
       }
 
@@ -170,7 +171,7 @@
       // Add a handler for when a row is swapped, update empty regions.
       tableDrag.row.prototype.onSwap = function (swappedRow) {
         checkEmptyRegions(table, this);
-        updateLastPlaced(table, $(this));
+        updateLastPlaced(table, this);
       };
 
       // Add a handler so when a row is dropped, update fields dropped into
@@ -242,7 +243,7 @@
           // Modify empty regions with added or removed fields.
           checkEmptyRegions(table, tableDrag.rowObject);
           // Update last placed block indication.
-          updateLastPlaced(table, row);
+          updateLastPlaced(table, tableDrag.rowObject);
           // Show unsaved changes warning.
           if (!tableDrag.changed) {
             $(Drupal.theme('tableDragChangedWarning'))
