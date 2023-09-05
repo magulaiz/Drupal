4db6f09e8cfe827ad796d50550aa86de9085fdd7
Eduardo Morales Alberti <12516-EduardoMoralesAlberti@users.noreply.drupalcode.org>
2023-09-05 10:50:15 +0200

    Issue #3384789: Allow to choose the cancel options

--- core/modules/user/tests/src/Functional/Update/UserUpdateCancelOptionsTest.php
+++ core/modules/user/tests/src/Functional/Update/UserUpdateCancelOptionsTest.php
@@ -16,7 +16,7 @@ class UserUpdateCancelOptionsTest extends UpdatePathTestBase {
    * {@inheritdoc}
    */
   protected function setDatabaseDumpFiles() {
-    $this->databaseDumpFiles[] = __DIR__ . '/../../../../system/tests/fixtures/update/drupal-9.4.0.bare.standard.php.gz';
+    $this->databaseDumpFiles[] = __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.bare.standard.php.gz';
   }
 
   /**
