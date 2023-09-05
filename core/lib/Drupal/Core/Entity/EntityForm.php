312837918280953827f2a2776016c016ccae7897
Lauri Eskola <lauri.eskola@acquia.com>
2023-09-05 12:11:34 +0300

    Fix concequent changes to the form

--- core/lib/Drupal/Core/Entity/EntityForm.php
+++ core/lib/Drupal/Core/Entity/EntityForm.php
@@ -138,8 +138,8 @@ class EntityForm extends FormBase implements EntityFormInterface {
    */
   public function form(array $form, FormStateInterface $form_state) {
     // Add #process and #after_build callbacks.
-    $form['#process'][] = [$this, 'processForm'];
-    $form['#after_build'][] = [$this, 'afterBuild'];
+    $form['#process'][] = '::processForm';
+    $form['#after_build'][] = '::afterBuild';
 
     return $form;
   }
