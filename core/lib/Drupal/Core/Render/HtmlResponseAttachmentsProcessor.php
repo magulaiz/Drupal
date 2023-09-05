7f8ad4d6124fbe66787f73db35504966f731c55e
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-09-05 12:07:40 +0530

    Phpcs fixes

--- core/lib/Drupal/Core/Render/HtmlResponseAttachmentsProcessor.php
+++ core/lib/Drupal/Core/Render/HtmlResponseAttachmentsProcessor.php
@@ -316,7 +316,6 @@ class HtmlResponseAttachmentsProcessor implements AttachmentsResponseProcessorIn
     // Print styles - if present.
     if (isset($placeholders['styles'])) {
       // Optimize CSS if necessary, but only during normal site operation.
-//      $optimize_css = !$maintenance_mode &&  isset($development_settings['css.preprocess']) ? $development_settings['css.preprocess'] : TRUE;
       $optimize_css = !$maintenance_mode && $this->config->get('css.preprocess');
       $variables['styles'] = $this->cssCollectionRenderer->render($this->assetResolver->getCssAssets($assets, $optimize_css, $this->languageManager->getCurrentLanguage()));
     }
@@ -324,7 +323,6 @@ class HtmlResponseAttachmentsProcessor implements AttachmentsResponseProcessorIn
     // Print scripts - if any are present.
     if (isset($placeholders['scripts']) || isset($placeholders['scripts_bottom'])) {
       // Optimize JS if necessary, but only during normal site operation.
-      $optimize_js = !$maintenance_mode &&  isset($development_settings['js.preprocess']) ? $development_settings['js.preprocess'] : TRUE;
       $optimize_js = !$maintenance_mode && $this->config->get('js.preprocess');
       [$js_assets_header, $js_assets_footer] = $this->assetResolver->getJsAssets($assets, $optimize_js, $this->languageManager->getCurrentLanguage());
       $variables['scripts'] = $this->jsCollectionRenderer->render($js_assets_header);
