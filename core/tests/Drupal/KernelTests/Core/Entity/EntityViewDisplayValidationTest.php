e70c878f9ed28b192f8951f9fec96446a92dfb49
Phéna Proxima <adam@phenaproxima.net>
2023-09-05 11:38:42 -0400

    Test LB entity view displays

--- core/tests/Drupal/KernelTests/Core/Entity/EntityViewDisplayValidationTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityViewDisplayValidationTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\KernelTests\Core\Entity;
 
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\layout_builder\Entity\LayoutEntityDisplayInterface;
+use Drupal\layout_builder\Section;
 
 /**
  * Tests validation of entity_view_display entities.
@@ -30,6 +32,26 @@ class EntityViewDisplayValidationTest extends EntityViewModeValidationTest {
       'mode' => 'test',
     ]);
     $this->entity->save();
+  }
+
+  /**
+   * Tests that the plugin ID of a Layout Builder section is validated.
+   */
+  public function testLayoutSectionPluginIdIsValidated(): void {
+    $this->enableModules(['layout_builder', 'layout_discovery']);
+
+    $this->entity = $this->container->get('entity_display.repository')
+      ->getViewDisplay('user', 'user');
+    $this->assertInstanceOf(LayoutEntityDisplayInterface::class, $this->entity);
+    $this->entity->enableLayoutBuilder()->save();
+    $sections = array_map(fn (Section $section) => $section->toArray(), $this->entity->getSections());
+    $this->assertCount(1, $sections);
+    $sections[0]['layout_id'] = 'non_existent';
+
+    $this->entity->setThirdPartySetting('layout_builder', 'sections', $sections);
+    $this->assertValidationErrors([
+      'third_party_settings.layout_builder.sections.0.layout_id' => "The 'non_existent' plugin does not exist.",
+    ]);
   }
 
 }
