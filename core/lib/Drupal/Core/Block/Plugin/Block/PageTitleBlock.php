4ab610a3c6734116edf29e1dc211c93302d1bb7c
Lauri Eskola <lauri.eskola@acquia.com>
2023-09-06 11:10:15 +0300

    Allow #title to override title resolver and change the config key to base_route_title

--- core/lib/Drupal/Core/Block/Plugin/Block/PageTitleBlock.php
+++ core/lib/Drupal/Core/Block/Plugin/Block/PageTitleBlock.php
@@ -50,8 +50,8 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
    *   The route match.
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
    *   The request stack.
-   * @param \Drupal\Core\Controller\BaseRouteTitleResolver $baseRouteTitleResolver
-   *   The base route title.
+   * @param \Drupal\Core\Controller\TitleResolverInterface $baseRouteTitleResolver
+   *   The base route title resolver.
    */
   public function __construct(
     array $configuration,
@@ -60,7 +60,7 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
     protected TitleResolverInterface $titleResolver,
     protected RouteMatchInterface $routeMatch,
     protected RequestStack $requestStack,
-    protected BaseRouteTitleResolver $baseRouteTitleResolver,
+    protected TitleResolverInterface $baseRouteTitleResolver,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
@@ -94,7 +94,7 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
   public function defaultConfiguration() {
     return [
       'label_display' => FALSE,
-      'contextualize_title' => FALSE,
+      'base_route_title' => FALSE,
     ];
   }
 
@@ -103,7 +103,7 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
    */
   public function build() {
     $title = $this->title;
-    if ($this->configuration['contextualize_title']) {
+    if ($this->configuration['base_route_title']) {
       $contextualized_title = $this->getTitleBasedOnBaseRoute();
       if (!is_null($contextualized_title)) {
         $title = $contextualized_title;
@@ -119,11 +119,15 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form['contextualize_title'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Contextualize title based on page hierarchy'),
-      '#default_value' => $this->configuration['contextualize_title'],
-      '#description' => $this->t('Display the page title based on the current context opposed to the default behaviour which displays title based on the current page.'),
+    $form['base_route_title'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Title to be displayed'),
+      '#options' => [
+        0 => $this->t('Current page title'),
+        1 => $this->t('Section page title')
+      ],
+      '#default_value' => (int) $this->configuration['base_route_title'],
+      '#description' => $this->t('Choose whether to display the title of the current page or the current section. Displaying the section title is often preferred in admin interfaces.'),
     ];
     return $form;
   }
@@ -132,37 +136,54 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, Con
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['contextualize_title'] = $form_state->getValue('contextualize_title');
+    $this->configuration['base_route_title'] = (bool) $form_state->getValue('base_route_title');
   }
 
   /**
    * Gets title based on base route.
    *
-   * @return array|string|\Stringable|null
+   * @return array|string|\Stringable
    *   The title based on base route.
    */
-  private function getTitleBasedOnBaseRoute(): array|string|null|\Stringable {
+  private function getTitleBasedOnBaseRoute(): array|string|\Stringable {
+    $controller_title = $this->titleResolver->getTitle(\Drupal::requestStack()->getCurrentRequest(), \Drupal::routeMatch()->getRouteObject());
+
+    // Controller render arrays using `#title` take precedent over the title
+    // resolvers.
+    if ((string) $this->titleToString($controller_title) !== (string) $this->titleToString($this->title)) {
+      return $this->title;
+    }
+
     $base_route_title = $this->baseRouteTitleResolver->getTitle(\Drupal::requestStack()->getCurrentRequest(), \Drupal::routeMatch()->getRouteObject());
     if (!is_null($base_route_title)) {
-      if (is_array($base_route_title)) {
-        $base_route_title = \Drupal::service('renderer')->render($base_route_title);
-      }
-      $current_title = $this->title;
-      if (is_array($current_title)) {
-        $current_title = \Drupal::service('renderer')->render($current_title);
-      }
-
       // If the titles are equal, return the original title.
-      if ((string) $base_route_title === (string) $current_title) {
+      if ((string) $this->titleToString($base_route_title) === (string) $this->titleToString($this->title)) {
         return $this->title;
       }
 
       return $this->t('@section_title<span class="visually-hidden">: @current_title</span>', [
-        '@section_title' => $base_route_title,
-        '@current_title' => $current_title,
+        '@section_title' => $this->titleToString($base_route_title),
+        '@current_title' => $this->titleToString($this->title),
       ]);
     }
-    return NULL;
+
+    return $this->title;
+  }
+
+  /**
+   * Converts title to string.
+   *
+   * @param array|string|null|\Stringable $title
+   *   A title that could be an array, string or stringable object.
+   *
+   * @return string|\Stringable
+   */
+  private function titleToString(array|string|null|\Stringable $title): string|\Stringable {
+    if (is_array($title)) {
+       $title = \Drupal::service('renderer')->render($title);
+    }
+
+    return $title ?? '';
   }
 
 }
