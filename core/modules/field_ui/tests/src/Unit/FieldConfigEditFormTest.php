4af52e87b29ee31b852cd3d969902da5d9062763
srishtiee <srishtibankar@gmail.com>
2023-09-06 22:58:36 +0530

    PHPStan

--- core/modules/field_ui/tests/src/Unit/FieldConfigEditFormTest.php
+++ core/modules/field_ui/tests/src/Unit/FieldConfigEditFormTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\field_ui\Unit;
 
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
-use Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManagerInterface;
 use Drupal\Core\TempStore\PrivateTempStore;
 use Drupal\field_ui\Form\FieldConfigEditForm;
 use Drupal\Tests\UnitTestCase;
@@ -32,8 +31,7 @@ class FieldConfigEditFormTest extends UnitTestCase {
     $typed_data = $this->createMock('\Drupal\Core\TypedData\TypedDataManagerInterface');
     $temp_store = $this->createMock(PrivateTempStore::class);
     $entity_display_repository = $this->createMock(EntityDisplayRepositoryInterface::class);
-    $selection_manager = $this->createMock(SelectionPluginManagerInterface::class);
-    $this->fieldConfigEditForm = new FieldConfigEditForm($entity_type_bundle_info, $typed_data, $entity_display_repository, $temp_store, $selection_manager);
+    $this->fieldConfigEditForm = new FieldConfigEditForm($entity_type_bundle_info, $typed_data, $entity_display_repository, $temp_store);
   }
 
   /**
