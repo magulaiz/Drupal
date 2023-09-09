1bd7fb4a9d9968fa78a4dfbf75b3b9e9a28027e8
catch <catch@35733.no-reply.drupal.org>
2023-09-09 21:27:49 +0100

    Delete build test test because it's slowing us down.

--- core/tests/Drupal/Tests/Core/Test/TestDiscoveryTest.php
+++ core/tests/Drupal/Tests/Core/Test/TestDiscoveryTest.php
@@ -458,7 +458,6 @@ EOF;
     $data['core-kerneltest'] = ['\Drupal\KernelTests\KernelTestBaseTest', 'Kernel'];
     $data['core-functionaltest'] = ['\Drupal\FunctionalTests\ExampleTest', 'Functional'];
     $data['core-functionaljavascripttest'] = ['\Drupal\FunctionalJavascriptTests\ExampleTest', 'FunctionalJavascript'];
-    $data['core-buildtest'] = ['\Drupal\BuildTests\Framework\Tests\BuildTestTest', 'Build'];
 
     return $data;
   }
