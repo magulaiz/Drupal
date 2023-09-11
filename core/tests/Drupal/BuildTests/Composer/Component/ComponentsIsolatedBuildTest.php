bc2afc524601cef5ba4bc3d444b0d6984e4efcc1
catch <catch@35733.no-reply.drupal.org>
2023-09-11 14:21:51 +0100

    Drop the dataprovider

--- core/tests/Drupal/BuildTests/Composer/Component/ComponentsIsolatedBuildTest.php
+++ core/tests/Drupal/BuildTests/Composer/Component/ComponentsIsolatedBuildTest.php
@@ -22,7 +22,7 @@ class ComponentsIsolatedBuildTest extends ComposerBuildTestBase {
    * @return array
    *   An array with relative paths to the component paths.
    */
-  public function provideComponentPaths(): array {
+  protected function provideComponentPaths(): array {
     $data = [];
     // During the dataProvider phase, there is not a workspace directory yet.
     // So we will find relative paths and assemble them with the workspace
@@ -32,31 +32,32 @@ class ComponentsIsolatedBuildTest extends ComposerBuildTestBase {
 
     /** @var \Symfony\Component\Finder\SplFileInfo $path */
     foreach ($composer_json_finder->getIterator() as $path) {
-      $data[] = ['/' . $path->getRelativePath()];
+      $data[] = '/' . $path->getRelativePath();
     }
     return $data;
   }
 
   /**
    * Test whether components' composer.json can be installed in isolation.
-   *
-   * @dataProvider provideComponentPaths
    */
-  public function testComponentComposerJson(string $component_path): void {
+  public function testComponentComposerJson(): void {
     // Only copy the components. Copy all of them because some of them depend on
     // each other.
     $finder = $this->getCodebaseFinder();
     $finder->in($this->getDrupalRoot() . static::$componentsPath);
     $this->copyCodebase($finder->getIterator());
 
-    $working_dir = $this->getWorkingPath() . static::$componentsPath . $component_path;
+    $component_paths = $this->provideComponentPaths();
+    foreach ($component_paths as $component_path) {
+      $working_dir = $this->getWorkingPath() . static::$componentsPath . $component_path;
 
-    // We add path repositories so we can wire internal dependencies together.
-    $this->addExpectedRepositories($working_dir);
+      // We add path repositories so we can wire internal dependencies together.
+      $this->addExpectedRepositories($working_dir);
 
-    // Perform the installation.
-    $this->executeCommand("composer install --working-dir=$working_dir --no-interaction --no-progress");
-    $this->assertCommandSuccessful();
+      // Perform the installation.
+      $this->executeCommand("composer install --working-dir=$working_dir --no-interaction --no-progress");
+      $this->assertCommandSuccessful();
+    }
   }
 
   /**
