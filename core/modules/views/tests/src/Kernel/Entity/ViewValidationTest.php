ebe05dc0fa50f05ee336ef16f0ec527914ff750b
Phéna Proxima <adam@phenaproxima.net>
2023-09-12 09:33:26 -0400

    Test all views handler types

--- core/modules/views/tests/src/Kernel/Entity/ViewValidationTest.php
+++ core/modules/views/tests/src/Kernel/Entity/ViewValidationTest.php
@@ -35,20 +35,38 @@ class ViewValidationTest extends ConfigEntityValidationTestBase {
   /**
    * Tests that a various plugin IDs making up a view display are validated.
    *
-   * @testWith ["display_plugin"]
-   *   ["display_options", "pager", "type"]
-   *   ["display_options", "exposed_form", "type"]
-   *   ["display_options", "access", "type"]
-   *   ["display_options", "style", "type"]
-   *   ["display_options", "row", "type"]
-   *   ["display_options", "query", "type"]
-   *   ["display_options", "cache", "type"]
-   *   ["display_options", "header", "non_existent", "plugin_id"]
-   *   ["display_options", "footer", "non_existent", "plugin_id"]
+   * @param string[] $parents
+   *   The array parents of the property of the view's default display which
+   *   will be set to `non_existent`.
+   * @param string|null $handler_type
+   *   If the property being changed refers to a handler, the type of handler
+   *   that it is. Since Views handler plugins support fallbacks, this is used
+   *   to set up a mocked plugin manager which returns a non-existent plugin ID
+   *   as the fallback plugin ID, thus raising the expected validation error.
+   *
+   * @todo Remove the the $handler_type parameter and mocking the handler
+   *   manager when fallback plugin IDs are no longer allowed by Views' config
+   *   schema.
+   *
+   * @testWith [["display_plugin"], null]
+   *   [["display_options", "pager", "type"], null]
+   *   [["display_options", "exposed_form", "type"], null]
+   *   [["display_options", "access", "type"], null]
+   *   [["display_options", "style", "type"], null]
+   *   [["display_options", "row", "type"], null]
+   *   [["display_options", "query", "type"], null]
+   *   [["display_options", "cache", "type"], null]
+   *   [["display_options", "header", "non_existent", "plugin_id"], "area"]
+   *   [["display_options", "footer", "non_existent", "plugin_id"], "area"]
+   *   [["display_options", "empty", "non_existent", "plugin_id"], "area"]
+   *   [["display_options", "arguments", "non_existent", "plugin_id"], "argument"]
+   *   [["display_options", "sorts", "non_existent", "plugin_id"], "sort"]
+   *   [["display_options", "fields", "non_existent", "plugin_id"], "field"]
+   *   [["display_options", "filters", "non_existent", "plugin_id"], "filter"]
+   *   [["display_options", "relationships", "non_existent", "plugin_id"], "relationship"]
    */
-  public function testInvalidPluginId(...$parents): void {
-    $handler_types = ['area'];
-    foreach ($handler_types as $handler_type) {
+  public function testInvalidPluginId(array $parents, ?string $handler_type): void {
+    if ($handler_type) {
       $this->container->set("plugin.manager.views.$handler_type", new class (
         $handler_type,
         $this->container->get('container.namespaces'),
