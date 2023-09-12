2608bbc9a226a2bb8458cb4e8e8c34a7a4336fc6
catch <catch@35733.no-reply.drupal.org>
2023-09-12 07:45:30 +0100

    Add PathAliasGeneric test to show what happens for required, hidden.

---
+++ core/modules/path_alias/tests/src/Functional/PathAliasGenericTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\path_alias\Functional\Module;
+
+use Drupal\Tests\system\Functional\Module\GenericModuleTestBase;
+
+/**
+ * Generic module test for Path Alias.
+ *
+ * @group path_alias
+ */
+class PathAliasGenericTest extends GenericModuleTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $module = 'path_alias';
+
+}
