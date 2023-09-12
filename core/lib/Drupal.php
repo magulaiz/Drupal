f2fdbcf3f174a49fd4db1e1b34b6eb082c8d6ea0
Alex Pott <alex.a.pott@googlemail.com>
2023-09-12 01:23:29 +0100

    Revert changes for testing

--- core/lib/Drupal.php
+++ core/lib/Drupal.php
@@ -6,8 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Static Service Container wrapper.
  *
- * Thisisaspellingmistake.
- *
  * Generally, code in Drupal should accept its dependencies via either
  * constructor injection or setter method injection. However, there are cases,
  * particularly in legacy procedural code, where that is infeasible. This
