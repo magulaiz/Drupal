04fbf41f6a39147f7374d35dadc0e8a30d0122dc
Sean Blommaert <37342-seanpenn079@users.noreply.drupalcode.org>
2023-09-12 16:50:50 -0400

    Issue #3054641 by seanB: Reroll for 10.1

--- core/lib/Drupal/Core/Menu/Plugin/Block/LocalTasksBlock.php
+++ core/lib/Drupal/Core/Menu/Plugin/Block/LocalTasksBlock.php
@@ -88,13 +88,16 @@ class LocalTasksBlock extends BlockBase implements ContainerFactoryPluginInterfa
     $config = $this->configuration;
     $cacheability = new CacheableMetadata();
     $cacheability->addCacheableDependency($this->localTaskManager);
-    // If the current route belongs to an entity, include cache tags of that
-    // entity as well.
-    $route_parameters = $this->routeMatch->getParameters()->all();
-    foreach ($route_parameters as $parameter) {
-      if ($parameter instanceof CacheableDependencyInterface) {
-        $cacheability->addCacheableDependency($parameter);
-      }
+
+    $userAdminLangcode = \Drupal::currentUser()->getPreferredAdminLangcode(FALSE);
+    $negotiation_method_enabled = \Drupal::moduleHandler()->moduleExists('language') && \Drupal::service('language_negotiator')->isNegotiationMethodEnabled('language-user-admin');
+
+    $primary_attributes = [];
+    $secondary_attributes = [];
+    if ($negotiation_method_enabled && !empty($userAdminLangcode)) {
+      $language = \Drupal::languageManager()->getLanguage($userAdminLangcode);
+      $primary_attributes = ['dir' => $language->getDirection(), 'lang' => $language->getId()];
+      $secondary_attributes = $primary_attributes;
     }
 
     $tabs = [
@@ -108,6 +111,7 @@ class LocalTasksBlock extends BlockBase implements ContainerFactoryPluginInterfa
       // Do not display single tabs.
       $tabs += [
         '#primary' => count(Element::getVisibleChildren($links['tabs'])) > 1 ? $links['tabs'] : [],
+        '#primary_attributes' => $primary_attributes,
       ];
     }
     if ($config['secondary']) {
@@ -116,6 +120,7 @@ class LocalTasksBlock extends BlockBase implements ContainerFactoryPluginInterfa
       // Do not display single tabs.
       $tabs += [
         '#secondary' => count(Element::getVisibleChildren($links['tabs'])) > 1 ? $links['tabs'] : [],
+        '#secondary_attributes' => $secondary_attributes,
       ];
     }
 
