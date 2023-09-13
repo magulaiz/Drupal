881ac274d09d89333bafab87e6fadb1370b52af9
Utsav Sharma <git@users.noreply.drupalcode.org>
2023-09-13 09:27:43 -0400

    Issue #2894747: fix JS concatenation

--- core/misc/ajax.js
+++ core/misc/ajax.js
@@ -1032,7 +1032,7 @@
   Drupal.Ajax.prototype.success = function (response, status) {
     // Remove the progress element.
     if (this.progress.element) {
-      $('.' + $(this.progress.element).attr('class').replace(/\s/g, '.')).remove();
+      $(`.${$(this.progress.element).attr('class').replace(/\s/g, '.')}`).remove();
     }
     if (this.progress.object) {
       this.progress.object.stopMonitoring();
