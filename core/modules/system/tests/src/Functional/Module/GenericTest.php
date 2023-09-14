f8d9e64f28b679ea7302dae4e8b93884b4cabd2e
catch <catch@35733.no-reply.drupal.org>
2023-09-14 15:32:14 +0100

    Add forum and workspaces test coverage. Rename tests to a standard classname so they're easier to script.

--- core/modules/system/tests/src/Functional/Module/GenericTest.php
+++ core/modules/system/tests/src/Functional/Module/GenericTest.php
@@ -7,4 +7,4 @@ namespace Drupal\Tests\system\Functional\Module;
  *
  * @group system
  */
-class SystemGenericTest extends GenericModuleTestBase {}
+class GenericTest extends GenericModuleTestBase {}
