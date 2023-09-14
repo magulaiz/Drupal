1713f98a588651cedfeecbbf00bb413b1748f9e0
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-09-14 22:30:08 +0100

    Added helper autoload.php file in scripts folder.

--- core/scripts/generate-proxy-class.php
+++ core/scripts/generate-proxy-class.php
@@ -23,7 +23,7 @@ if (PHP_SAPI !== 'cli') {
 }
 
 // Bootstrap.
-$autoloader = require_once dirname(exec("pwd") . '/' . trim($_SERVER['argv'][0], './'), 3) . '/autoload.php';
+$autoloader = require_once 'autoload.php';
 $request = Request::createFromGlobals();
 DrupalKernel::createFromRequest($request, $autoloader, 'prod')->boot();
 
