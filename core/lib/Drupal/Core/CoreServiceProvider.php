d2070feb6214cc69593ff28ddf50695b22cab078
bjorn <bjorn@swis.nl>
2023-09-15 17:21:38 +0200

    Revert "a phpcs error"
    This reverts commit 514b0d9d0bc5be53488d888bd8827a0ae7ef5284.

--- core/lib/Drupal/Core/CoreServiceProvider.php
+++ core/lib/Drupal/Core/CoreServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\Core;
 
 use Drupal\Core\Cache\Context\CacheContextsPass;
@@ -29,6 +30,18 @@ use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * ServiceProvider class for mandatory core services.
+ *
+ * This is where Drupal core registers all of its compiler passes.
+ * The service definitions themselves are in core/core.services.yml with a
+ * few, documented exceptions (typically, install requirements).
+ *
+ * Modules wishing to register services to the container should use
+ * modulename.services.yml in their respective directories.
+ *
+ * @ingroup container
+ */
 class CoreServiceProvider implements ServiceProviderInterface, ServiceModifierInterface {
 
   /**
