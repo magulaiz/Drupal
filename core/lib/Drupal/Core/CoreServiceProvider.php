05d78d57d6639b70f82deeaa1d73109047e7b10f
bjorn <bjorn@swis.nl>
2023-09-15 17:21:39 +0200

    Revert "A phpstan error"
    This reverts commit 394a0ebcfb1526c26c8ff3e9f0a8436163f7df4b.

--- core/lib/Drupal/Core/CoreServiceProvider.php
+++ core/lib/Drupal/Core/CoreServiceProvider.php
@@ -50,8 +50,6 @@ class CoreServiceProvider implements ServiceProviderInterface, ServiceModifierIn
   public function register(ContainerBuilder $container) {
     $this->registerTest($container);
 
-    echo $green;
-
     // Only register the private file stream wrapper if a file path has been set.
     if (Settings::get('file_private_path')) {
       $container->register('stream_wrapper.private', 'Drupal\Core\StreamWrapper\PrivateStream')
