bcc4ce48658e341cead3bd3f8155d25edd9b6402
Kunal Sachdev <kunal.sachdev@acquia.com>
2023-09-15 16:50:11 +0530

    upgrade path to optional_end_date config in daterange field storage settings

--- core/modules/datetime_range/datetime_range.post_update.php
+++ core/modules/datetime_range/datetime_range.post_update.php
@@ -14,3 +14,24 @@ function datetime_range_removed_post_updates() {
     'datetime_range_post_update_views_string_plugin_id' => '9.0.0',
   ];
 }
+
+/**
+ * Adds optional_end_date config in daterange field storage settings.
+ */
+function datetime_range_post_update_add_optional_end_date()
+{
+  $config_factory = \Drupal::configFactory();
+  foreach ($config_factory->listAll('field.storage.node.') as $node_field_storage) {
+    $node_field_storage_settings = $config_factory->getEditable($node_field_storage);
+    if ($node_field_storage_settings->get('type') != 'daterange') {
+      continue;
+    }
+    $settings = $node_field_storage_settings->get('settings');
+    $settings['optional_end_date'] = FALSE;
+    $node_field_storage_settings->set('settings', $settings);
+
+    // Mark the resulting configuration as trusted data. This avoids issues with
+    // future schema changes.
+    $node_field_storage_settings->save(TRUE);
+  }
+}
