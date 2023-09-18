be8d5087873026ad971a843ce07829fb35352257
catch <catch@35733.no-reply.drupal.org>
2023-09-18 16:46:23 +0100

    Bring back generic test for help_topics.

---
+++ core/modules/help_topics/tests/src/Functional/GenericTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\Tests\help_topics\Functional;
+
+use Drupal\Tests\system\Functional\Module\GenericModuleTestBase;
+
+/**
+ * Generic module test for help_topics.
+ *
+ * @group help_topics
+ * @group legacy
+ */
+class GenericTest extends GenericModuleTestBase {}
