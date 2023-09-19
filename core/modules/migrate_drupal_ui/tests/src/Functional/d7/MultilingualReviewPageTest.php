2f1b7613f71f75848836219c280340a716a6eb32
catch <catch@35733.no-reply.drupal.org>
2023-09-19 16:18:29 +0100

    Add @group slow to tests over 45 seconds.

--- core/modules/migrate_drupal_ui/tests/src/Functional/d7/MultilingualReviewPageTest.php
+++ core/modules/migrate_drupal_ui/tests/src/Functional/d7/MultilingualReviewPageTest.php
@@ -13,6 +13,7 @@ use Drupal\Tests\migrate_drupal_ui\Functional\MultilingualReviewPageTestBase;
  *
  * @group migrate_drupal_7
  * @group migrate_drupal_ui
+ * @group #slow
  */
 class MultilingualReviewPageTest extends MultilingualReviewPageTestBase {
 
