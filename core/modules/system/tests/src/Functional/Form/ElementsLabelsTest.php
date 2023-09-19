19cf644b703a2b51b9db7e6de0a661a51566c98a
catch <catch@35733.no-reply.drupal.org>
2023-09-19 21:46:43 +0100

    Add @group #slow to ElementsLabelsTest

--- core/modules/system/tests/src/Functional/Form/ElementsLabelsTest.php
+++ core/modules/system/tests/src/Functional/Form/ElementsLabelsTest.php
@@ -9,6 +9,7 @@ use Drupal\Tests\BrowserTestBase;
  * Tests form element labels, required markers and associated output.
  *
  * @group Form
+ * @group #slow
  */
 class ElementsLabelsTest extends BrowserTestBase {
 
