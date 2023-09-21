aa9c99e3f05fda5db7538599ed06aab051658121
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-09-21 13:47:04 +1000

    Check for duplicates

--- core/modules/forum/tests/src/Functional/ForumIndexUpdateTest.php
+++ core/modules/forum/tests/src/Functional/ForumIndexUpdateTest.php
@@ -34,8 +34,26 @@ final class ForumIndexUpdateTest extends UpdatePathTestBase {
     $find_primary_key_columns->setAccessible(TRUE);
     $columns = $find_primary_key_columns->invoke($schema, 'forum_index');
     $this->assertEmpty($columns);
+    $count = \Drupal::database()->select('forum_index')->countQuery()->execute()->fetchField();
+    $this->assertEquals(6, $count);
+    $duplicates = \Drupal::database()->select('forum_index')->condition('nid', 1)->countQuery()->execute()->fetchField();
+    $this->assertEquals(2, $duplicates);
     $this->runUpdates();
     $this->assertEquals(['nid', 'tid'], $find_primary_key_columns->invoke($schema, 'forum_index'));
+    $count = \Drupal::database()->select('forum_index')->countQuery()->execute()->fetchField();
+    $this->assertEquals(5, $count);
+    $duplicates = \Drupal::database()->select('forum_index')->condition('nid', 1)->countQuery()->execute()->fetchField();
+    $this->assertEquals(1, $duplicates);
+    $entry = \Drupal::database()->select('forum_index', 'f')->fields('f')->condition('nid', 5)->execute()->fetchAssoc();
+    $this->assertEquals([
+      'nid' => 5,
+      'title' => 'AFL',
+      'tid' => 5,
+      'sticky' => 0,
+      'created' => 1695264369,
+      'last_comment_timestamp' => 1695264403,
+      'comment_count' => 1,
+    ], $entry);
   }
 
 }
