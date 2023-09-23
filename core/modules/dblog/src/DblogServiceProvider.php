b71308fbab65e99e2b6b59ff620baaec84ca866c
Mustakim Farooqui <fmustakim7@gmail.com>
2023-09-23 03:49:12 -0400

    3354081: Addressed #11 suggestions.

--- core/modules/dblog/src/DblogServiceProvider.php
+++ core/modules/dblog/src/DblogServiceProvider.php
@@ -4,12 +4,17 @@ namespace Drupal\dblog;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\Core\Config\BootstrapConfigStorageFactory;
 
 class DblogServiceProvider extends ServiceProviderBase {
-  public function alter(ContainerBuilder $container) {
-    if ($container->hasDefinition('logger.dblog')) {
-      $definition = $container->getDefinition('logger.dblog');
-      $definition->setClass('Drupal\dblog\Logger\Disabled');
+
+  public function register(ContainerBuilder $container) {
+
+    $config_storage = BootstrapConfigStorageFactory::get();
+    $dblog_config = $config_storage->read('dblog.settings');
+    if ($dblog_config['logging_paused']) {
+      return;
     }
+    $container->register('dblog.logger', 'Drupal\dblog\Logger\DbLog');
   }
 }
