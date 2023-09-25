f33100cd266828e06fbdd0679cef1c66f75db0fa
Kunal Sachdev <kunal.sachdev@acquia.com>
2023-09-25 16:21:27 +0530

    change variable names in update path

--- core/modules/datetime_range/datetime_range.post_update.php
+++ core/modules/datetime_range/datetime_range.post_update.php
@@ -20,17 +20,17 @@ function datetime_range_removed_post_updates() {
  */
 function datetime_range_post_update_add_optional_end_date() {
   $config_factory = \Drupal::configFactory();
-  foreach ($config_factory->listAll('field.storage.') as $node_field_storage) {
-    $node_field_storage_settings = $config_factory->getEditable($node_field_storage);
-    if ($node_field_storage_settings->get('type') != 'daterange') {
+  foreach ($config_factory->listAll('field.storage.') as $field_storage) {
+    $field_storage_settings = $config_factory->getEditable($field_storage);
+    if ($field_storage_settings->get('type') != 'daterange') {
       continue;
     }
-    $settings = $node_field_storage_settings->get('settings');
+    $settings = $field_storage_settings->get('settings');
     $settings['optional_end_date'] = FALSE;
-    $node_field_storage_settings->set('settings', $settings);
+    $field_storage_settings->set('settings', $settings);
 
     // Mark the resulting configuration as trusted data. This avoids issues with
     // future schema changes.
-    $node_field_storage_settings->save(TRUE);
+    $field_storage_settings->save(TRUE);
   }
 }
