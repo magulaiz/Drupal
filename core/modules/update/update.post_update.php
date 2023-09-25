bb3a76ac2cd1593cc1ab22d57324a6b90662b992
Yash Rode <yashrode774@gmail.com>
2023-09-25 14:27:23 +0530

    changed if condition

--- core/modules/update/update.post_update.php
+++ core/modules/update/update.post_update.php
@@ -29,9 +29,9 @@ function update_post_update_set_blank_fetch_url_to_null() {
 /**
  * Sets the default day for update.
  */
-function update_day_of_week() {
+function update_post_update_day_of_week() {
   $update_settings = \Drupal::configFactory()->getEditable('update.settings');
-  if ($update_settings->get('check.update_day') === 0) {
+  if ($update_settings->get('check.update_day') === NULL) {
     $update_settings
       ->set('check.update_day', 4)
       ->save(TRUE);
