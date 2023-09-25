831b25b3664cb00a62e2505d1b3f0c9acbea18ed
Yash Rode <yashrode774@gmail.com>
2023-09-25 12:14:16 +0530

    changed post update hook

--- core/modules/update/update.post_update.php
+++ core/modules/update/update.post_update.php
@@ -25,3 +25,11 @@ function update_post_update_set_blank_fetch_url_to_null() {
       ->save(TRUE);
   }
 }
+
+/**
+ * Sets the default day for update.
+ */
+function update_day_of_week() {
+  $update_settings = \Drupal::configFactory()->getEditable('update.settings');
+  $update_settings->set('check.update_day', 4);
+}
