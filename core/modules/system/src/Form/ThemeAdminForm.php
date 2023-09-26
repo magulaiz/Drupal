e717a0f037a28085bfe641769937f3f3b93cae4e
Phéna Proxima <adam@phenaproxima.net>
2023-09-26 13:53:02 -0400

    Revert ThemeAdminForm, since it does not call parent::buildForm()

--- core/modules/system/src/Form/ThemeAdminForm.php
+++ core/modules/system/src/Form/ThemeAdminForm.php
@@ -41,7 +41,7 @@ class ThemeAdminForm extends ConfigFormBase {
       '#options' => ['' => $this->t('Default theme')] + $theme_options,
       '#title' => $this->t('Administration theme'),
       '#description' => $this->t('Choose "Default theme" to always use the same theme as the rest of the site.'),
-      '#config_target' => 'system.theme:admin',
+      '#default_value' => $this->config('system.theme')->get('admin'),
     ];
     $form['admin_theme']['actions'] = ['#type' => 'actions'];
     $form['admin_theme']['actions']['submit'] = [
@@ -50,6 +50,14 @@ class ThemeAdminForm extends ConfigFormBase {
       '#button_type' => 'primary',
     ];
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    $this->config('system.theme')->set('admin', $form_state->getValue('admin_theme'))->save();
   }
 
 }
