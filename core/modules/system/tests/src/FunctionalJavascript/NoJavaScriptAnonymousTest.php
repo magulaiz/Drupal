fff9e895a832a2116c3aa59622d759ca08834ce0
catch <catch@35733.no-reply.drupal.org>
2023-09-26 14:14:04 +0100

    Reset group to Common

--- core/modules/system/tests/src/FunctionalJavascript/NoJavaScriptAnonymousTest.php
+++ core/modules/system/tests/src/FunctionalJavascript/NoJavaScriptAnonymousTest.php
@@ -11,7 +11,7 @@ use Drupal\node\NodeInterface;
  * This is tested with the core modules that are enabled in the 'standard'
  * profile.
  *
- * @group Performance
+ * @group Common
  */
 class NoJavaScriptAnonymousTest extends PerformanceTestBase {
 
