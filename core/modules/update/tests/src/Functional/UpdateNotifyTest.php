6ab7b204f9f405549ac58d9c2d4585dc9e93498f
Yash Rode <yashrode774@gmail.com>
2023-09-26 17:42:29 +0530

    Added test coverage

---
+++ core/modules/update/tests/src/Functional/UpdateNotifyTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\update\Functional;
+
+use Drupal\Core\Datetime\DateHelper;
+
+/**
+ * Tests that the update notification email is sent.
+ *
+ * @group update
+ */
+class UpdateNotifyTest extends UpdateSemverTestBase {
+
+  /**
+   * Sets the version to x.x.x when no project-specific mapping is defined.
+   *
+   * @param string $version
+   *   The version.
+   */
+  protected function setProjectInstalledVersion($version) {
+    $this->mockDefaultExtensionsInfo(['version' => $version]);
+  }
+
+  /**
+   * Checks that the update notification is sent on configured day.
+   */
+  public function testModulePageRegularUpdate() {
+    $this->setProjectInstalledVersion('8.0.0');
+    $this->mockReleaseHistory(['drupal' => '0.1']);
+
+    $request_time = \Drupal::time()->getRequestTime();
+    $day = (int) DateHelper::dayOfWeek(date('Y-m-d', $request_time));
+    $update_settings = \Drupal::configFactory()->getEditable('update.settings');
+    $update_settings
+      ->set('check.interval_days', 7)
+      ->set('check.update_day', $day)
+      ->set('notification.emails', ['abc@gmail.com'])
+      ->save(TRUE);
+    $cron = $this->container->get('cron');
+    $cron->run();
+    // Last email notification time will be set to the recent request time if
+    // the mail has been sent. @see _update_cron_notify().
+    $this->assertSame($request_time, \Drupal::state()->get('update.last_email_notification'));
+  }
+
+}
