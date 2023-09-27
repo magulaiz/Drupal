d7761102df73c27795d318050480ed1f192c39d9
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-09-27 14:19:54 +0530

    Added a functional js test for the feature included

---
+++ core/modules/views/tests/src/FunctionalJavascript/MenuSelectionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\Tests\views\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Tests the menu selection.
+ *
+ * @group mymodule
+ */
+class MenuSelectionTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['views_ui', 'menu_ui'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'starterkit_theme';
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->drupalLogin($this->rootUser);
+  }
+
+  /**
+   * Tests the behavior of parent link selection list based on selected menu.
+   */
+  public function testSelectLists() {
+
+    // Visit the edit form for the test menu link.
+    $this->drupalGet('admin/structure/menu/manage/account');
+    $this->clickLink('Edit');
+    // Select the tools option from the menu list.
+    $this->getSession()->getPage()->findField('menu_parent_menu')->selectOption('<Tools>');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->assertSession()->optionExists('Parent link', '-- Compose tips (disabled)');
+    $this->assertSession()->optionExists('Parent link', '<Tools>');
+
+  }
+
+}
