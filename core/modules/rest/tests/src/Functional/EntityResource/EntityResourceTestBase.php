3e9f1ad429681848a80b563281830880da7a6ac0
omkar podey <omkar.podey@acquia.com>
2023-09-27 18:12:06 +0530

    just unset

--- core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
@@ -1023,7 +1023,7 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
       $this->entity->set('rest_test_validation', 'ALWAYS_FAIL');
       $this->entity->save();
 
-      $valid_request_body['vid'][0] = $this->entity->getRevisionId();
+      unset($valid_request_body['vid']);
       $request_options[RequestOptions::BODY] = $this->serializer->serialize($valid_request_body, static::$format);
       // Information disclosure prevented: when a malicious user correctly
       // guesses the current invalid value of a field, ensure a 200 is not sent
