65288aabd869ac0e2c5d317188cbb6a579a04750
catch <catch@35733.no-reply.drupal.org>
2023-09-28 12:38:47 +0100

    Divide up the front vs node page tests into separate classes for future expansion (logged in etc.)

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
@@ -10,7 +10,7 @@ use Drupal\FunctionalJavascriptTests\PerformanceTestBase;
  * @group OpenTelemetry
  * @group #slow
  */
-class OpenTelemetryPerformanceTest extends PerformanceTestBase {
+class OpenTelemetryNodePagPerformanceTest extends PerformanceTestBase {
 
   /**
    * {@inheritdoc}
@@ -18,64 +18,12 @@ class OpenTelemetryPerformanceTest extends PerformanceTestBase {
   protected $profile = 'demo_umami';
 
   /**
-   * Logs front page tracing data with a cold cache.
-   */
-  public function testFrontPageColdCache() {
-    $this->telemetryServiceName = FALSE;
-    // @todo: Chromedriver doesn't collect tracing performance logs for the very
-    // first request in a test, so warm it up.
-    // @see https://www.drupal.org/project/drupal/issues/3379750
-    $this->drupalGet('user/login');
-    $this->rebuildAll();
-    $this->logTelemetry('umamiFrontPageColdCache', function () {
-      $this->drupalGet('<front>');
-    });
-    $this->assertSession()->pageTextContains('Umami');
-  }
-
-  /**
-   * Logs front page tracing data with a hot cache.
-   *
-   * Hot here means that all possible caches are warmed.
-   */
-  public function testFrontPageHotCache() {
-    // Request the page twice so that asset aggregates and image derivatives are
-    // definitely cached in the browser cache. The first response builds the
-    // file and serves from PHP with private, no-store headers. The second
-    // request will get the file served directly from disk by the browser with
-    // cacheable headers, so only the third request actually has the files
-    // in the browser cache.
-    $this->drupalGet('<front>');
-    $this->drupalGet('<front>');
-    $this->logTelemetry('umamiFrontPageWarmCache', function () {
-      $this->drupalGet('<front>');
-    });
-  }
-
-  /**
-   * Logs front page tracing data with a lukewarm cache.
-   *
-   * Cool here means that 'global' site caches are warm but anything
-   * specific to the front page is cold.
-   */
-  public function testFrontPageCoolCache() {
-    // First of all visit the front page to ensure the image style exists.
-    $this->drupalGet('<front>');
-    $this->rebuildAll();
-    // Now visit a different page to warm non-route-specific caches.
-    $this->drupalGet('/user/login');
-    $this->logTelemetry('umamiFrontPageLukewarmCache', function () {
-      $this->drupalGet('<front>');
-    });
-  }
-
-  /**
    * Logs node page tracing data with a cold cache.
    */
   public function testNodePageColdCache() {
     // @todo: Chromedriver doesn't collect tracing performance logs for the very
     // first request in a test, so warm it up.
-    // @see https://www.drupal.org/project/drupal/issues/3379750
+    // See https://www.drupal.org/project/drupal/issues/3379750
     $this->drupalGet('user/login');
     $this->rebuildAll();
     $this->logTelemetry('umamiNodePageColdCache', function () {
