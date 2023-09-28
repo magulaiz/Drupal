126f3a91052f912d88987740377b25e8b00e051d
omkar podey <omkar.podey@acquia.com>
2023-09-28 14:57:29 +0530

    expand comment

--- core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
@@ -1024,6 +1024,8 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
       $this->entity->save();
 
       // Whenever value is set it's probably out of sync so just unset it.
+      // We need to do this because we are making vid an administrative field,
+      // and we don't have the permissions to alter it here.
       if (isset($valid_request_body['vid'][0]['value'])) {
         unset($valid_request_body['vid']);
       }
