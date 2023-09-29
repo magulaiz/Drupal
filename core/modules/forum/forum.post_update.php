f9fa08b398293c5f684c3586539f76c52b5ab3ed
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-09-29 15:46:56 +1000

    Review suggestions

--- core/modules/forum/forum.post_update.php
+++ core/modules/forum/forum.post_update.php
@@ -7,16 +7,16 @@
 
 use Drupal\Core\Site\Settings;
 use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\node\NodeInterface;
 
 /**
  * Repopulate the forum index table.
  */
-function forum_post_update_recreate_forum_index_rows(&$sandbox = NULL): PluralTranslatableMarkup {
+function forum_post_update_recreate_forum_index_rows(&$sandbox = NULL): TranslatableMarkup {
   $entityStorage = \Drupal::entityTypeManager()->getStorage('node');
-  if (!isset($sandbox['progress'])) {
+  if (!isset($sandbox['ids'])) {
     // This must be the first run. Initialize the sandbox.
-    $sandbox['progress'] = 0;
     $sandbox['ids'] = \Drupal::state()->get('forum_update_10101_nids', []);
     $sandbox['max'] = count($sandbox['ids']);
   }
@@ -44,16 +44,16 @@ function forum_post_update_recreate_forum_index_rows(&$sandbox = NULL): PluralTr
       $entity->comment_forum->last_comment_timestamp,
       $entity->comment_forum->comment_count,
     ]);
-    $sandbox['progress']++;
   }
   if ($do_insert) {
     $insert->execute();
   }
-  $sandbox['#finished'] = empty($sandbox['max']) ? 1 : $sandbox['progress'] / $sandbox['max'];
+  $sandbox['#finished'] = empty($sandbox['max']) || empty($sandbox['ids']) ? 1 : ($sandbox['max'] - count($sandbox['ids'])) / $sandbox['max'];
   if ($sandbox['#finished'] === 1) {
     \Drupal::state()->delete('forum_update_10101_nids');
+    return new TranslatableMarkup('Finished updating forum index rows.');
   }
-  return new PluralTranslatableMarkup($sandbox['progress'],
+  return new PluralTranslatableMarkup($sandbox['max'] - count($sandbox['ids']),
     'Processed @count entry of @total.',
     'Processed @count entries of @total.',
     ['@total' => $sandbox['max']],
