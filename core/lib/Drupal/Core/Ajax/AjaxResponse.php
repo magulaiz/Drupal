8f0de01fb1129c89690a7ba4d44f9a65783b8dee
Tim Diels <tim.diels@calibrate.be>
2023-09-29 15:07:07 +0200

    Updated naming to be the same as the 10.1.x branch

--- core/lib/Drupal/Core/Ajax/AjaxResponse.php
+++ core/lib/Drupal/Core/Ajax/AjaxResponse.php
@@ -71,7 +71,7 @@ class AjaxResponse extends JsonResponse implements AttachmentsInterface {
    * @param array $commands
    *   Merges ajax command(s) with the current one(s) in this class.
    */
-  public function setCommands(array $commands) {
+  public function mergeCommands(array $commands) {
     $this->commands = array_merge($this->commands, $commands);
   }
 
