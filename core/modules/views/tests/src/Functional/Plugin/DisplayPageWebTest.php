36cb3669275ceba8154f76ea22c9a3900c0c60a2
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-10-03 15:33:30 +0530

    Fixed DisplayPageWebTest

--- core/modules/views/tests/src/Functional/Plugin/DisplayPageWebTest.php
+++ core/modules/views/tests/src/Functional/Plugin/DisplayPageWebTest.php
@@ -119,7 +119,7 @@ class DisplayPageWebTest extends ViewTestBase {
 
     // Update the menu link.
     $this->drupalGet("admin/structure/menu/link/views_view:views.test_page_display_menu.page_3/edit");
-    $this->getSession()->getPage()->findField('menu_parent')->selectOption('<Tools>');
+    $this->getSession()->getPage()->findField('menu_parent')->selectOption('tools:');
     $this->submitForm(['title' => 'New title'], 'Save');
 
     $this->drupalGet('<front>');
