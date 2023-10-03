3006fe3c4369e7bc48b25376f6d69b0a30aca208
omkar podey <omkar.podey@acquia.com>
2023-10-03 16:51:37 +0530

    keep in sync

--- core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
@@ -1023,13 +1023,13 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
       $this->entity->set('rest_test_validation', 'ALWAYS_FAIL');
       $this->entity->save();
 
-      // Whenever value is set it's probably out of sync so just unset it.
+      // The request options must remain in sync with the entities stored vid.
       // We need to do this because we are making vid an administrative field,
-      // and we don't have the permissions to alter it here.
+      // and we don't have the permissions to alter it.
       if (isset($valid_request_body['vid'][0]['value'])) {
-        unset($valid_request_body['vid']);
+        $valid_request_body['vid'][0] = $this->entity->getRevisionId();
+        $request_options[RequestOptions::BODY] = $this->serializer->serialize($valid_request_body, static::$format);
       }
-      $request_options[RequestOptions::BODY] = $this->serializer->serialize($valid_request_body, static::$format);
       // Information disclosure prevented: when a malicious user correctly
       // guesses the current invalid value of a field, ensure a 200 is not sent
       // because this would disclose to the attacker what the current value is.
