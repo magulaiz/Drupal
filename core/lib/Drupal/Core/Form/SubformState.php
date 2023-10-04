b70757075cec2217bc396881b91b56e449ae0d56
Lauri Eskola <lauri.eskola@acquia.com>
2023-10-04 16:29:33 +0300

    Use property promotion properly

--- core/lib/Drupal/Core/Form/SubformState.php
+++ core/lib/Drupal/Core/Form/SubformState.php
@@ -37,7 +37,7 @@ class SubformState extends FormStateDecoratorBase implements SubformStateInterfa
    * @param \Drupal\Core\Form\FormInterface|null $subformFormObject
    *    The subform form object when it's not the same as the parent form.
    */
-  protected function __construct(array &$subform, array &$parent_form, FormStateInterface $parent_form_state, readonly ?FormInterface $subformFormObject = NULL) {
+  protected function __construct(array &$subform, array &$parent_form, FormStateInterface $parent_form_state, protected readonly ?FormInterface $subformFormObject = NULL) {
     $this->decoratedFormState = $parent_form_state;
     $this->parentForm = $parent_form;
     $this->subform = $subform;
