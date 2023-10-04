8745650431d3b87745e4ea45c24ac21197352007
Dave Long <dave@longwaveconsulting.com>
2023-10-04 11:12:12 +0100

    Add comment.

--- core/lib/Drupal/Component/Utility/Html.php
+++ core/lib/Drupal/Component/Utility/Html.php
@@ -278,6 +278,9 @@ class Html {
     // Instantiate the HTML5 parser, but without the HTML5 namespace being
     // added to the DOM document.
     $html5 = new HTML5(['disable_html_ns' => TRUE, 'encoding' => 'UTF-8']);
+
+    // Attach the provided HTML inside the body. Rely on the HTML5 parser to
+    // close the body tag.
     return $html5->loadHTML('<body>' . $html);
   }
 
