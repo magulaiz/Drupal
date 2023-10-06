78e3b9a0fc911c999ea77a771be7c9bfd779ae8b
Andy Postnikov <apostnikov@gmail.com>
2023-10-06 15:14:00 +0200

    fix announcements_feed

--- core/modules/announcements_feed/src/RenderCallbacks.php
+++ core/modules/announcements_feed/src/RenderCallbacks.php
@@ -4,28 +4,22 @@ declare(strict_types=1);
 
 namespace Drupal\announcements_feed;
 
-use Drupal\Core\Security\TrustedCallbackInterface;
+use Drupal\Core\Security\Attribute\TrustedCallback;
 
 /**
  * Defines a class for render callbacks.
  *
  * @internal
  */
-final class RenderCallbacks implements TrustedCallbackInterface {
+final class RenderCallbacks {
 
   /**
    * Render callback.
    */
+  #[TrustedCallback]
   public static function removeTabAttributes(array $element): array {
     unset($element['tab']['#attributes']);
     return $element;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function trustedCallbacks(): array {
-    return ['removeTabAttributes'];
   }
 
 }
