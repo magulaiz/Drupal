f4309b7be801fadfc65a1bcf58c9d3653124171d
mondrake <mondrake.org@gmail.com>
2023-10-08 21:30:25 +0200

    Revert "fix"
    This reverts commit 79d70cdeacc950bfec2da25a34e921966d3024eb.

--- core/modules/mysqli/tests/src/Kernel/mysqli/LargeQueryTest.php
+++ core/modules/mysqli/tests/src/Kernel/mysqli/LargeQueryTest.php
@@ -16,10 +16,6 @@ class LargeQueryTest extends BaseMySqlTest {
 
   /**
    * Tests truncation of messages when max_allowed_packet exception occurs.
-   *
-   * Since the test breaks the database connection, it is run in a separate,
-   * additional, test connection so that the test teardown process can still
-   * be executed on the default test connection.
    */
   public function testMaxAllowedPacketQueryTruncating(): void {
     $connectionInfo = Database::getConnectionInfo('default');
@@ -41,9 +37,15 @@ class LargeQueryTest extends BaseMySqlTest {
       $this->fail("An exception should be thrown for queries larger than 'max_allowed_packet'");
     }
     catch (\Throwable $e) {
+      dump([$e, $max_allowed_packet]);
+      // throw $e;
+      // Close and re-open the connection. Otherwise we will run into error
+      // 2006 "MySQL server had gone away" afterwards.
       Database::closeConnection('testMaxAllowedPacketQueryTruncating');
+      // Database::getConnection();
       $this->assertInstanceOf(DatabaseExceptionWrapper::class, $e);
       // Got a packet bigger than 'max_allowed_packet' bytes exception thrown.
+      // NOTE *** This differs from the MySql PDO test.
       $this->assertEquals(1153, $e->getCode());
       // 'max_allowed_packet' exception message truncated.
       // Use strlen() to count the bytes exactly, not the unicode chars.
