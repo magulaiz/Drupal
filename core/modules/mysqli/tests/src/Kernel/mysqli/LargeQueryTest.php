b8ecfa01b3de2afe9c9f87dd92752e2b71b26dfb
mondrake <mondrake.org@gmail.com>
2023-10-08 17:59:20 +0200

    fix

--- core/modules/mysqli/tests/src/Kernel/mysqli/LargeQueryTest.php
+++ core/modules/mysqli/tests/src/Kernel/mysqli/LargeQueryTest.php
@@ -3,8 +3,8 @@
 namespace Drupal\Tests\mysqli\Kernel\mysqli;
 
 use Drupal\Component\Utility\Environment;
-use Drupal\Core\Database\Database;
-use Drupal\Core\Database\DatabaseException;
+// use Drupal\Core\Database\Database;
+// use Drupal\Core\Database\DatabaseException;
 use Drupal\Tests\mysql\Kernel\mysql\LargeQueryTest as BaseMySqlTest;
 
 /**
