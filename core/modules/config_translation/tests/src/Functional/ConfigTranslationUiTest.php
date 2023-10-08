0415eb2c75069cab5c5a1e133776d86f832e3554
catch <catch@35733.no-reply.drupal.org>
2023-10-08 15:00:06 +0100

    Move cspell:ignore too.

--- core/modules/config_translation/tests/src/Functional/ConfigTranslationUiTest.php
+++ core/modules/config_translation/tests/src/Functional/ConfigTranslationUiTest.php
@@ -5,7 +5,7 @@ namespace Drupal\Tests\config_translation\Functional;
 use Drupal\Core\Language\Language;
 use Drupal\Core\Language\LanguageInterface;
 
-// cspell:ignore libellé viewsviewfiles
+// cspell:ignore viewsviewfiles
 
 /**
  * Translate settings and entities to various languages.
