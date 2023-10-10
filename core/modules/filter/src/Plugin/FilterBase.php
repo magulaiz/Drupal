3444f7274f685ff435397dcf724c0d699e0c4c67
Alex Pott <alex.a.pott@googlemail.com>
2023-10-10 17:42:01 +0100

    Make FilterBase::setConfiguration() behave the same as other plugins

--- core/modules/filter/src/Plugin/FilterBase.php
+++ core/modules/filter/src/Plugin/FilterBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\filter\Plugin;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\PluginBase;
 
@@ -58,15 +59,13 @@ abstract class FilterBase extends PluginBase implements FilterInterface {
    * {@inheritdoc}
    */
   public function setConfiguration(array $configuration) {
-    if (isset($configuration['status'])) {
-      $this->status = (bool) $configuration['status'];
-    }
-    if (isset($configuration['weight'])) {
-      $this->weight = (int) $configuration['weight'];
-    }
-    if (isset($configuration['settings'])) {
-      $this->settings = (array) $configuration['settings'];
-    }
+    $configuration = NestedArray::mergeDeep(
+      $this->defaultConfiguration(),
+      $configuration
+    );
+    $this->status = (bool) $configuration['status'];
+    $this->weight = (int) $configuration['weight'];
+    $this->settings = (array) $configuration['settings'];
     return $this;
   }
 
