aad9af96a34328f4833dcb3ae715b22c488fcc93
mondrake <mondrake.org@gmail.com>
2023-10-11 21:03:56 +0200

    more

--- core/modules/mysqli/tests/src/Unit/NamedPlaceholderConverterTest.php
+++ core/modules/mysqli/tests/src/Unit/NamedPlaceholderConverterTest.php
@@ -181,40 +181,52 @@ class NamedPlaceholderConverterTest extends UnitTestCase {
 
     yield [
       'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, ARRAY[:foo])',
-      'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, ARRAY[{:foo}])',
+      [':foo' => 'qux'],
+      'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, ARRAY[?])',
+      ['qux'],
     ];
 
     yield [
       'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, array[:foo])',
-      'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, array[{:foo}])',
+      [':foo' => 'qux'],
+      'SELECT * FROM foo WHERE jsonb_exists_any(foo.bar, array[?])',
+      ['qux'],
     ];
 
     yield [
       "SELECT table.column1, ARRAY['3'] FROM schema.table table WHERE table.f1 = :foo AND ARRAY['3']",
-      "SELECT table.column1, ARRAY['3'] FROM schema.table table WHERE table.f1 = {:foo} AND ARRAY['3']",
+      [':foo' => 'qux'],
+      "SELECT table.column1, ARRAY['3'] FROM schema.table table WHERE table.f1 = ? AND ARRAY['3']",
+      ['qux'],
     ];
 
     yield [
       "SELECT table.column1, ARRAY['3']::integer[] FROM schema.table table"
         . " WHERE table.f1 = :foo AND ARRAY['3']::integer[]",
+      [':foo' => 'qux'],
       "SELECT table.column1, ARRAY['3']::integer[] FROM schema.table table"
-        . " WHERE table.f1 = {:foo} AND ARRAY['3']::integer[]",
+        . " WHERE table.f1 = ? AND ARRAY['3']::integer[]",
+      ['qux'],
     ];
 
     yield [
       "SELECT table.column1, ARRAY[:foo] FROM schema.table table WHERE table.f1 = :bar AND ARRAY['3']",
-      "SELECT table.column1, ARRAY[{:foo}] FROM schema.table table WHERE table.f1 = {:bar} AND ARRAY['3']",
+      [':foo' => 'qux', ':bar' => 'git'],
+      "SELECT table.column1, ARRAY[?] FROM schema.table table WHERE table.f1 = ? AND ARRAY['3']",
+      ['qux', 'git'],
     ];
 
     yield [
       'SELECT table.column1, ARRAY[:foo]::integer[] FROM schema.table table'
         . " WHERE table.f1 = :bar AND ARRAY['3']::integer[]",
-      'SELECT table.column1, ARRAY[{:foo}]::integer[] FROM schema.table table'
-        . " WHERE table.f1 = {:bar} AND ARRAY['3']::integer[]",
+      [':foo' => 'qux', ':bar' => 'git'],
+      'SELECT table.column1, ARRAY[?]::integer[] FROM schema.table table'
+        . " WHERE table.f1 = ? AND ARRAY['3']::integer[]",
+      ['qux', 'git'],
     ];
 
-        yield 'Quotes inside literals escaped by doubling' => [
-            <<<'SQL'
+    yield 'Quotes inside literals escaped by doubling' => [
+      <<<'SQL'
 SELECT * FROM foo
 WHERE bar = ':not_a_param1 ''":not_a_param2"'''
    OR bar=:a_param1
@@ -224,81 +236,93 @@ WHERE bar = ':not_a_param1 ''":not_a_param2"'''
    OR bar=:a_param3
 SQL
 ,
-            <<<'SQL'
+      [':a_param1' => 'qux', ':a_param2' => 'git', ':a_param3' => 'foo'],
+    <<<'SQL'
 SELECT * FROM foo
 WHERE bar = ':not_a_param1 ''":not_a_param2"'''
-   OR bar={:a_param1}
-   OR bar={:a_param2}||':not_a_param3'
+   OR bar=?
+   OR bar=?||':not_a_param3'
    OR bar=':not_a_param4 '':not_a_param5'' :not_a_param6'
    OR bar=''
-   OR bar={:a_param3}
+   OR bar=?
 SQL
 ,
-        ];
+      ['qux', 'git', 'foo'],
+    ];
 
-        yield [
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . " WHERE (data.description LIKE :condition_0 ESCAPE '\\\\')"
-                . " AND (data.description LIKE :condition_1 ESCAPE '\\\\') ORDER BY id ASC",
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . " WHERE (data.description LIKE {:condition_0} ESCAPE '\\\\')"
-                . " AND (data.description LIKE {:condition_1} ESCAPE '\\\\') ORDER BY id ASC",
-        ];
+    yield [
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . " WHERE (data.description LIKE :condition_0 ESCAPE '\\\\')"
+        . " AND (data.description LIKE :condition_1 ESCAPE '\\\\') ORDER BY id ASC",
+      [':condition_0' => 'qux', ':condition_1' => 'git'],
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . " WHERE (data.description LIKE ? ESCAPE '\\\\')"
+        . " AND (data.description LIKE ? ESCAPE '\\\\') ORDER BY id ASC",
+      ['qux', 'git'],
+    ];
 
-        yield [
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . ' WHERE (data.description LIKE :condition_0 ESCAPE "\\\\")'
-                . ' AND (data.description LIKE :condition_1 ESCAPE "\\\\") ORDER BY id ASC',
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . ' WHERE (data.description LIKE {:condition_0} ESCAPE "\\\\")'
-                . ' AND (data.description LIKE {:condition_1} ESCAPE "\\\\") ORDER BY id ASC',
-        ];
+    yield [
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . ' WHERE (data.description LIKE :condition_0 ESCAPE "\\\\")'
+        . ' AND (data.description LIKE :condition_1 ESCAPE "\\\\") ORDER BY id ASC',
+      [':condition_0' => 'qux', ':condition_1' => 'git'],
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . ' WHERE (data.description LIKE ? ESCAPE "\\\\")'
+        . ' AND (data.description LIKE ? ESCAPE "\\\\") ORDER BY id ASC',
+      ['qux', 'git'],
+    ];
 
-        yield 'Combined single and double quotes' => [
-            <<<'SQL'
+    yield 'Combined single and double quotes' => [
+      <<<'SQL'
 SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id
   FROM test_data data
  WHERE (data.description LIKE :condition_0 ESCAPE "\\")
    AND (data.description LIKE :condition_1 ESCAPE '\\') ORDER BY id ASC
 SQL
 ,
-            <<<'SQL'
+      [':condition_0' => 'qux', ':condition_1' => 'git'],
+      <<<'SQL'
 SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id
   FROM test_data data
- WHERE (data.description LIKE {:condition_0} ESCAPE "\\")
-   AND (data.description LIKE {:condition_1} ESCAPE '\\') ORDER BY id ASC
+ WHERE (data.description LIKE ? ESCAPE "\\")
+   AND (data.description LIKE ? ESCAPE '\\') ORDER BY id ASC
 SQL
 ,
-        ];
+      ['qux', 'git'],
+    ];
 
-        yield [
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . ' WHERE (data.description LIKE :condition_0 ESCAPE `\\\\`)'
-                . ' AND (data.description LIKE :condition_1 ESCAPE `\\\\`) ORDER BY id ASC',
-            'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
-                . ' WHERE (data.description LIKE {:condition_0} ESCAPE `\\\\`)'
-                . ' AND (data.description LIKE {:condition_1} ESCAPE `\\\\`) ORDER BY id ASC',
-        ];
+    yield [
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . ' WHERE (data.description LIKE :condition_0 ESCAPE `\\\\`)'
+        . ' AND (data.description LIKE :condition_1 ESCAPE `\\\\`) ORDER BY id ASC',
+      [':condition_0' => 'qux', ':condition_1' => 'git'],
+      'SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id FROM test_data data'
+        . ' WHERE (data.description LIKE ? ESCAPE `\\\\`)'
+        . ' AND (data.description LIKE ? ESCAPE `\\\\`) ORDER BY id ASC',
+      ['qux', 'git'],
+    ];
 
-        yield 'Combined single quotes and backticks' => [
-            <<<'SQL'
+    yield 'Combined single quotes and backticks' => [
+      <<<'SQL'
 SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id
   FROM test_data data
  WHERE (data.description LIKE :condition_0 ESCAPE '\\')
    AND (data.description LIKE :condition_1 ESCAPE `\\`) ORDER BY id ASC
 SQL
 ,
-            <<<'SQL'
+      [':condition_0' => 'qux', ':condition_1' => 'git'],
+      <<<'SQL'
 SELECT data.age AS age, data.id AS id, data.name AS name, data.id AS id
   FROM test_data data
- WHERE (data.description LIKE {:condition_0} ESCAPE '\\')
-   AND (data.description LIKE {:condition_1} ESCAPE `\\`) ORDER BY id ASC
+ WHERE (data.description LIKE ? ESCAPE '\\')
+   AND (data.description LIKE ? ESCAPE `\\`) ORDER BY id ASC
 SQL
 ,
-        ];
+      ['qux', 'git'],
+    ];
 
-        yield 'Placeholders inside comments' => [
-            <<<'SQL'
+    yield '? placeholders inside comments' => [
+      <<<'SQL'
 /*
  * test placeholder ?
  */
@@ -309,7 +333,8 @@ SELECT dummy as "dummy?"
    AND dummy = ?
 SQL
 ,
-            <<<'SQL'
+      ['baz'],
+      <<<'SQL'
 /*
  * test placeholder ?
  */
@@ -317,21 +342,51 @@ SELECT dummy as "dummy?"
   FROM DUAL
  WHERE '?' = '?'
 -- AND dummy <> ?
-   AND dummy = {?}
+   AND dummy = ?
 SQL
 ,
-        ];
+      ['baz'],
+    ];
 
-        yield 'Escaped question' => [
-            <<<'SQL'
+    yield 'Named placeholders inside comments' => [
+      <<<'SQL'
+/*
+ * test :placeholder
+ */
+SELECT dummy as "dummy?"
+  FROM DUAL
+ WHERE '?' = '?'
+-- AND dummy <> :dummy
+   AND dummy = :key
+SQL
+,
+      [':key' => 'baz'],
+      <<<'SQL'
+/*
+ * test :placeholder
+ */
+SELECT dummy as "dummy?"
+  FROM DUAL
+ WHERE '?' = '?'
+-- AND dummy <> :dummy
+   AND dummy = ?
+SQL
+,
+      ['baz'],
+    ];
+
+    yield 'Escaped question' => [
+      <<<'SQL'
 SELECT '{"a":null}'::jsonb ?? :key
 SQL
 ,
-            <<<'SQL'
-SELECT '{"a":null}'::jsonb ?? {:key}
+      [':key' => 'qux'],
+      <<<'SQL'
+SELECT '{"a":null}'::jsonb ?? ?
 SQL
 ,
-        ];
-    }
+      ['qux'],
+    ];
+  }
 
 }
