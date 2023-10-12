cb99fee9360bd7e21aea7e2cdf2696033b745105
catch <catch@35733.no-reply.drupal.org>
2023-10-12 10:47:31 +0100

    Add @return to suppress deprecation message.

--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
@@ -41,6 +41,8 @@ class WebDriverCurlService extends CurlService {
 
   /**
    * {@inheritdoc}
+   *
+   * @return array
    */
   public function execute($requestMethod, $url, $parameters = NULL, $extraOptions = []) {
     $extraOptions += [
