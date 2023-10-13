4d63557b1895d089bf9018653b51a13a6dd4eca2
Philipp Michael <michael@abgeordnetenwatch.de>
2023-10-13 12:12:25 +0200

    revert unintentional changes from last commit

--- core/modules/views/src/Plugin/views/pager/SqlBase.php
+++ core/modules/views/src/Plugin/views/pager/SqlBase.php
@@ -247,7 +247,7 @@ abstract class SqlBase extends PagerPluginBase implements CacheableDependencyInt
     // Only accept integer values.
     $error = FALSE;
     $exposed_options = $form_state->getValue(['pager_options', 'expose', 'items_per_page_options']);
-    if (str_contains($exposed_options, '.')) {
+    if (strpos($exposed_options, '.') !== FALSE) {
       $error = TRUE;
     }
     $options = explode(',', $exposed_options);
@@ -280,7 +280,7 @@ abstract class SqlBase extends PagerPluginBase implements CacheableDependencyInt
     if ($this->itemsPerPageExposed()) {
       $query = $this->view->getRequest()->query;
       $items_per_page = $query->get('items_per_page');
-      if ((int) $items_per_page > 0) {
+      if ($items_per_page > 0) {
         $this->options['items_per_page'] = $items_per_page;
       }
       elseif ($items_per_page == 'All' && $this->options['expose']['items_per_page_options_all']) {
