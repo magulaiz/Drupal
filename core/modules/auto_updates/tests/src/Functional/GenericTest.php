eb06231edd4083132369f439cd99cd281ab4d229
Ted Bowman <ted+git@tedbow.com>
2023-10-14 16:12:28 -0400

    Contrib: copy generic test to module - https://git.drupalcode.org/project/automatic_updates/-/commit/df5b2f78520843e8d96c1edf37292c6ed1df1945

---
+++ core/modules/auto_updates/tests/src/Functional/GenericTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Tests\auto_updates\Functional;
+
+use Drupal\Tests\system\Functional\Module\GenericModuleTestBase;
+
+/**
+ * Generic module test for auto_updates.
+ *
+ * @group auto_updates
+ */
+class GenericTest extends GenericModuleTestBase {}
