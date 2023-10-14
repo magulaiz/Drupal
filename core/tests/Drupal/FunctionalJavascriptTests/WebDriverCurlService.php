aac19ab03d4834bf257244ab540f7249c0b8b6cd
catch <catch@35733.no-reply.drupal.org>
2023-10-14 08:28:31 +0100

    Instead of adding @return, skip the deprecation.

--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
@@ -41,8 +41,6 @@ class WebDriverCurlService extends CurlService {
 
   /**
    * {@inheritdoc}
-   *
-   * @return array
    */
   public function execute($requestMethod, $url, $parameters = NULL, $extraOptions = []) {
     $extraOptions += [
