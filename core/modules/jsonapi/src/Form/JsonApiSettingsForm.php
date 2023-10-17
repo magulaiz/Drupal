95d77f1dbc5cf658befe7b2e66a90dbe617f5fc1
Phéna Proxima <adam@phenaproxima.net>
2023-10-17 11:02:58 -0400

    Convert jsonapi_settings to 0 and 1 rather than rw and r

--- core/modules/jsonapi/src/Form/JsonApiSettingsForm.php
+++ core/modules/jsonapi/src/Form/JsonApiSettingsForm.php
@@ -34,44 +34,14 @@ class JsonApiSettingsForm extends ConfigFormBase {
       '#type' => 'radios',
       '#title' => $this->t('Allowed operations'),
       '#options' => [
-        'r' => $this->t('Accept only JSON:API read operations.'),
-        'rw' => $this->t('Accept all JSON:API create, read, update, and delete operations.'),
+        1 => $this->t('Accept only JSON:API read operations.'),
+        0 => $this->t('Accept all JSON:API create, read, update, and delete operations.'),
       ],
-      '#config_target' => [
-        'target' => 'jsonapi.settings:read_only',
-        'load_callback' => '::loadReadOnlyValue',
-        'save_callback' => '::saveReadOnlyValue',
-      ],
+      '#config_target' => 'jsonapi.settings:read_only',
       '#description' => $this->t('Warning: Only enable all operations if the site requires it. <a href=":docs">Learn more about securing your site with JSON:API.</a>', [':docs' => 'https://www.drupal.org/docs/8/modules/jsonapi/security-considerations']),
     ];
 
     return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * Transformation callback for the read_only config value.
-   *
-   * @param bool $read_only
-   *   The value to transform.
-   *
-   * @return string
-   *   The transformed value.
-   */
-  public function loadReadOnlyValue(bool $read_only): string {
-    return $read_only ? 'r' : 'rw';
-  }
-
-  /**
-   * Transformation callback for the read_only config value.
-   *
-   * @param string $value
-   *   The value to transform.
-   *
-   * @return bool
-   *   The transformed value.
-   */
-  public function saveReadOnlyValue(string $value): bool {
-    return $value === 'r';
   }
 
 }
---
+++ core/modules/jsonapi/tests/src/Functional/SettingsFormTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\jsonapi\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * @covers \Drupal\jsonapi\Form\JsonApiSettingsForm
+ * @group jsonapi
+ */
+class SettingsFormTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['jsonapi'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests the JSON:API settings form.
+   */
+  public function testSettingsForm(): void {
+    $account = $this->drupalCreateUser(['administer site configuration']);
+    $this->drupalLogin($account);
+    $this->drupalGet('/admin/config/services/jsonapi');
+
+    $page = $this->getSession()->getPage();
+    $page->selectFieldOption('read_only', 0);
+    $page->pressButton('Save configuration');
+    $this->assertFalse($this->config('jsonapi.settings')->get('read_only'));
+
+    $page->selectFieldOption('read_only', 1);
+    $page->pressButton('Save configuration');
+    $this->container->get('config.factory')->clearStaticCache();
+    $this->assertTrue($this->config('jsonapi.settings')->get('read_only'));
+  }
+
+}
