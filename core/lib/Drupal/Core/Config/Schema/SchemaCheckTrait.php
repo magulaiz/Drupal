72dcd0625892c15d7d2e1b27db9b9f892adb552d
Joris Vercammen <botchcake@gmail.com>
2023-10-19 08:40:47 +0200

    views

--- core/lib/Drupal/Core/Config/Schema/SchemaCheckTrait.php
+++ core/lib/Drupal/Core/Config/Schema/SchemaCheckTrait.php
@@ -947,7 +947,8 @@ trait SchemaCheckTrait {
         "'multi_type' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name) \(see config schema type views\.field\..*",
         "'separator' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name) \(see config schema type views\.field\..*",
         "'settings' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name) \(see config schema type views\.field\..*",
-        "'type' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name) \(see config schema type views\.field\..*",
+        "'type' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name|boolean) \(see config schema type views\.field\..*",
+        "'type_custom_true' is a conditionally required key because display\..*\.fields\..*\.plugin_id is (field|term_name|boolean) \(see config schema type views\.field\..*",
         // @see \Drupal\views\Plugin\views\field\EntityLink
         // @see \Drupal\views\Plugin\views\field\EntityLinkDelete (inherited)
         // @see \Drupal\views\Plugin\views\field\EntityLinkEdit (inherited)
