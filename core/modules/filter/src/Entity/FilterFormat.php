17eca6566735b6054503ce5c0e9836e41c159141
Alex Pott <alex.a.pott@googlemail.com>
2023-10-20 17:45:52 +0200

    Copy stuff from Role

--- core/modules/filter/src/Entity/FilterFormat.php
+++ core/modules/filter/src/Entity/FilterFormat.php
@@ -203,8 +203,12 @@ class FilterFormat extends ConfigEntityBase implements FilterFormatInterface, En
    */
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
-    // Ensure the filters have been sorted before saving.
-    ksort($this->filters);
+    if (!$this->isSyncing() && $this->hasTrustedData()) {
+      // Filters are sorted by keys to ensure config export diffs are easy to
+      // read and there is a minimal changeset. If the save is not trusted then
+      // the configuration will be sorted by StorableConfigBase.
+      ksort($this->filters);
+    }
 
     assert(is_string($this->label()), 'Filter format label is expected to be a string.');
     $this->name = trim($this->label());
