f6ad1173690d905aa09682fcead4ac1e14a02637
Alex Pott <alex.a.pott@googlemail.com>
2023-10-21 16:23:34 +0200

    Run the test we're interesting in lots

--- core/scripts/run-tests.sh
+++ core/scripts/run-tests.sh
@@ -165,7 +165,8 @@ if (!Composer::upgradePHPUnitCheck(Version::id())) {
   exit(SIMPLETEST_SCRIPT_EXIT_FAILURE);
 }
 
-$test_list = simpletest_script_get_test_list();
+//$test_list = simpletest_script_get_test_list();
+$test_list = array_fill(0, 100, '\Drupal\FunctionalTests\Installer\InstallerExistingConfigTest');
 
 // Try to allocate unlimited time to run the tests.
 Environment::setTimeLimit(0);
