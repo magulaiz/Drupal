ba03649e474b275575952a1dd313cf19566341ca
devbranch-vitaliy <HitchShock@gmail.com>
2023-10-21 11:48:26 +0300

    [#3163940] Fixed typo

--- core/modules/views/src/Form/ViewsForm.php
+++ core/modules/views/src/Form/ViewsForm.php
@@ -89,7 +89,7 @@ class ViewsForm implements FormInterface, ContainerInjectionInterface {
    * @param \Drupal\Core\DependencyInjection\ClassResolverInterface $class_resolver
    *   The class resolver to get the subform form objects.
    * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
-   *   The url generator to generate the form action.
+   *   The URL generator to generate the form action.
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
    *   The request stack.
    * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
