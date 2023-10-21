2917d300b044b24c4e37b2c220cdfe54cb9a8d7d
mondrake <mondrake.org@gmail.com>
2023-10-21 16:02:37 +0200

    explicitly use a collation in testConcatWsFields

--- core/modules/mysqli/src/Driver/Database/mysqli/Connection.php
+++ core/modules/mysqli/src/Driver/Database/mysqli/Connection.php
@@ -135,8 +135,6 @@ class Connection extends BaseMySqlConnection {
 
     $connection_options['init_commands'] += [
       'sql_mode' => "SET sql_mode = 'ANSI,TRADITIONAL'",
-      // @todo Perhaps 'collation_connection' => "SET collation_connection =
-      //   'utf8mb4_0900_ai_ci'".
     ];
 
     // Execute initial commands.
--- core/modules/mysqli/tests/src/Kernel/mysqli/SyntaxTest.php
+++ core/modules/mysqli/tests/src/Kernel/mysqli/SyntaxTest.php
@@ -10,4 +10,17 @@ use Drupal\KernelTests\Core\Database\DriverSpecificSyntaxTestBase;
  * @group Database
  */
 class SyntaxTest extends DriverSpecificSyntaxTestBase {
+
+  /**
+   * Tests string concatenation with separator, with field values.
+   */
+  public function testConcatWsFields() {
+    $result = $this->connection->query("SELECT CONCAT_WS('-', :a1, [name], :a2, [age]) FROM {test} WHERE [age] = :age COLLATE utf8mb4_general_ci", [
+      ':a1' => 'name',
+      ':a2' => 'age',
+      ':age' => 25,
+    ]);
+    $this->assertSame('name-John-age-25', $result->fetchField());
+  }
+
 }
