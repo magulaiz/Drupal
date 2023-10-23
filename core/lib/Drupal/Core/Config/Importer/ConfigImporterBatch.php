1db88611e4469f8d89181a6b10b3a84b5bac81bd
Alex Pott <alex.a.pott@googlemail.com>
2023-10-23 13:01:17 +0100

    Okay...

--- core/lib/Drupal/Core/Config/Importer/ConfigImporterBatch.php
+++ core/lib/Drupal/Core/Config/Importer/ConfigImporterBatch.php
@@ -58,6 +58,7 @@ class ConfigImporterBatch {
         foreach ($results['errors'] as $error) {
           $messenger->addError($error);
           $logger->error($error);
+          trigger_error((string) $error, E_USER_ERROR);
         }
         $messenger->addWarning(t('The configuration was imported with errors.'));
       }
@@ -72,6 +73,7 @@ class ConfigImporterBatch {
       $error_operation = reset($operations);
       $message = t('An error occurred while processing %error_operation with arguments: @arguments', ['%error_operation' => $error_operation[0], '@arguments' => print_r($error_operation[1], TRUE)]);
       $messenger->addError($message);
+      trigger_error((string) $message, E_USER_ERROR);
     }
   }
 
