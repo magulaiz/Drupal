6a6ad30b0d11bd582666987c0517db9affaea83d
Andy Blum <andy.blum.01@gmail.com>
2023-10-23 08:17:07 -0400

    allow 3-character hexadecimal color inputs

--- core/themes/olivero/js/color-picker.js
+++ core/themes/olivero/js/color-picker.js
@@ -34,10 +34,10 @@
    * @param {HTMLElement} inputToSync input element to synchronize
    */
   function synchronizeInputs(changedInput, inputToSync) {
-    inputToSync.value = changedInput.value;
+    inputToSync.value = formatHex(changedInput.value);
 
-    changedInput.setAttribute('data-olivero-custom-color', changedInput.value);
-    inputToSync.setAttribute('data-olivero-custom-color', changedInput.value);
+    changedInput.setAttribute('data-olivero-custom-color', formatHex(changedInput.value));
+    inputToSync.setAttribute('data-olivero-custom-color', formatHex(changedInput.value));
 
     const colorSchemeSelect = document.querySelector(
       '[data-drupal-selector="edit-color-scheme"]',
@@ -130,7 +130,7 @@
       'form-element--type-color',
       'form-element--api-color',
     );
-    colorInput.value = textInput.value;
+    colorInput.value = formatHex(textInput.value);
     colorInput.setAttribute('name', `${textInput.name}_visual`);
     colorInput.setAttribute(
       'data-olivero-custom-color',
@@ -157,6 +157,28 @@
     colorInput.addEventListener('input', () => {
       synchronizeInputs(colorInput, textInput);
     });
+  }
+
+  /**
+   * Formats hexcode to full 6-character string for HTMLColorInput.
+   *
+   * @param {string} hex The hex code input
+   * @returns {string} The same hex code, formatted
+   */
+  function formatHex(hex) {
+    // Temporarily remove hash
+    if (hex.startsWith('#')) {
+      hex = hex.substring(1);
+    }
+
+    // Convert "abc" to "aabbcc".
+    if (hex.length === 3) {
+      hex = hex.split('').flatMap(character => [character, character]).join('');
+    }
+
+    hex = `#${hex}`;
+
+    return hex;
   }
 
   /**
