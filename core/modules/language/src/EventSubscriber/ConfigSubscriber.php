e8a4a9475d0ab65335554ffe091a0cdd46f4a9dc
Alex Pott <alex.a.pott@googlemail.com>
2023-10-23 12:45:31 +0100

    What's the original data

--- core/modules/language/src/EventSubscriber/ConfigSubscriber.php
+++ core/modules/language/src/EventSubscriber/ConfigSubscriber.php
@@ -95,7 +95,7 @@ class ConfigSubscriber implements EventSubscriberInterface {
         $this->languageManager->reset();
 
         if ($saved_config->getOriginal('default_langcode') === NULL) {
-          trigger_error(print_r($saved_config->get(), TRUE) . "\n" . (new \Exception())->getTraceAsString(), E_USER_ERROR);
+          trigger_error(print_r($saved_config->getOriginal(), TRUE) . "\n" . print_r($saved_config->get(), TRUE) . "\n" . (new \Exception())->getTraceAsString(), E_USER_ERROR);
         }
 
         // Do not do secondary writes to configuration during config sync.
