c488fdf25d3c1f7a53fab2a4af1dacaca8e87161
Alex Pott <alex.a.pott@googlemail.com>
2023-10-24 14:30:31 +0100

    Add back debug code

--- core/modules/field/src/FieldStorageConfigStorage.php
+++ core/modules/field/src/FieldStorageConfigStorage.php
@@ -159,6 +159,9 @@ class FieldStorageConfigStorage extends ConfigEntityStorage {
    */
   protected function mapFromStorageRecords(array $records) {
     foreach ($records as $id => &$record) {
+      if (!isset($record['type'])) {
+        trigger_error(print_r($record, TRUE) . "\n" . (new \Exception())->getTraceAsString(), E_USER_ERROR);
+      }
       try {
         $class = $this->fieldTypeManager->getPluginClass($record['type']);
       }
