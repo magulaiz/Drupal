25193b3f47eaf976e25904e12bc71760bfe6ee71
mondrake <mondrake.org@gmail.com>
2023-10-24 20:38:40 +0200

    add test for module visibility

--- core/modules/mysqli/tests/src/Functional/GenericTest.php
+++ core/modules/mysqli/tests/src/Functional/GenericTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\mysqli\Functional;
 
+use Drupal\Core\Extension\ExtensionLifecycle;
 use Drupal\Tests\system\Functional\Module\GenericModuleTestBase;
 
 /**
@@ -9,4 +10,18 @@ use Drupal\Tests\system\Functional\Module\GenericModuleTestBase;
  *
  * @group mysqli
  */
-class GenericTest extends GenericModuleTestBase {}
+class GenericTest extends GenericModuleTestBase {
+
+  /**
+   * Checks visibility of the module.
+   */
+  public function testMysqliModule(): void {
+    $module = $this->getModule();
+    \Drupal::service('module_installer')->install([$module]);
+    $info = \Drupal::service('extension.list.module')->getExtensionInfo($module);
+    dump($info);
+    $this->assertTrue($info['hidden']);
+    $this->assertSame(ExtensionLifecycle::EXPERIMENTAL, $info['lifecycle']);
+  }
+
+}
