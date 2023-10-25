3c35a2c6824e30e8730d9c35f2a560d188367107
Dave Long <dave@longwaveconsulting.com>
2023-10-25 23:30:03 +0100

    Add autowirable services test.

--- core/tests/Drupal/KernelTests/Core/DependencyInjection/AutowireTest.php
+++ core/tests/Drupal/KernelTests/Core/DependencyInjection/AutowireTest.php
@@ -54,9 +54,7 @@ class AutowireTest extends KernelTestBase {
     $services = [];
     $aliases = [];
 
-    $filenames = array_map(fn($module) => "core/modules/{$module[0]}/{$module[0]}.services.yml", $this->coreModuleListDataProvider());
-    $filenames[] = 'core/core.services.yml';
-    foreach (array_filter($filenames, 'file_exists') as $filename) {
+    foreach ($this->getCoreServiceFiles() as $filename) {
       foreach (Yaml::decode(file_get_contents($filename))['services'] as $id => $service) {
         if (is_string($service)) {
           $aliases[$id] = substr($service, 1);
@@ -174,6 +172,52 @@ class AutowireTest extends KernelTestBase {
     }
 
     $this->assertEmpty($autowire, 'The following core controllers can be autowired. Remove the create() method:' . PHP_EOL . implode(PHP_EOL, $autowire));
+  }
+
+  /**
+   * Tests that core services have aliases correctly defined where possible.
+   */
+  public function testCoreAutowiring(): void {
+    $services = [];
+    $aliases = [];
+    foreach ($this->getCoreServiceFiles() as $filename) {
+      foreach (Yaml::decode(file_get_contents($filename))['services'] as $id => $service) {
+        if (is_string($service)) {
+          $aliases[$id] = substr($service, 1);
+        }
+        elseif (isset($service['class']) && isset($service['arguments'])) {
+          $services[$id] = $service;
+        }
+      }
+    }
+
+    $autowirable = [];
+    foreach ($services as $id => $service) {
+      if (!method_exists($service['class'], '__construct')) {
+        continue;
+      }
+      $constructor = new \ReflectionMethod($service['class'], '__construct');
+      foreach ($constructor->getParameters() as $parameter) {
+        if (!isset($aliases[(string) $parameter->getType()])) {
+          continue 2;
+        }
+      }
+      $autowirable[] = $id;
+    }
+
+    $this->assertEmpty($autowirable, 'The following core services can be autowired. Remove their arguments from the services.yml file:' . PHP_EOL . implode(PHP_EOL, $autowirable));
+  }
+
+  /**
+   * Return a list of core service YAML files.
+   *
+   * @return string[]
+   *   An array of filenames.
+   */
+  private function getCoreServiceFiles() {
+    $filenames = array_map(fn($module) => "core/modules/{$module[0]}/{$module[0]}.services.yml", $this->coreModuleListDataProvider());
+    $filenames[] = 'core/core.services.yml';
+    return array_filter($filenames, 'file_exists');
   }
 
 }
