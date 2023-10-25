ff5eaef7bf9eb7e6d41061c263abda1cd624a1cb
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-10-25 14:32:48 +0530

    Resolved test failures

--- core/modules/views/tests/src/FunctionalJavascript/MenuSelectionTest.php
+++ core/modules/views/tests/src/FunctionalJavascript/MenuSelectionTest.php
@@ -31,10 +31,10 @@ class MenuSelectionTest extends WebDriverTestBase {
     $this->drupalGet('admin/structure/menu/manage/account');
     $this->clickLink('Edit');
     // Select the tools option from the menu list.
-    $this->getSession()->getPage()->findField('menu_parent_menu')->selectOption('<Tools>');
+    $this->getSession()->getPage()->findField('menu_parent_menu')->selectOption('Tools');
     $this->assertSession()->assertWaitOnAjaxRequest();
     $this->assertSession()->optionExists('Parent link', '-- Compose tips (disabled)');
-    $this->assertSession()->optionExists('Parent link', '<Tools>');
+    $this->assertSession()->optionExists('Parent link', 'Tools');
   }
 
 }
