713ed3da6f9b5e370183108277d5eef2f95000d6
ressa <53072-ressa@users.noreply.drupalcode.org>
2023-10-25 08:57:17 +0000

    Removing forbidden word Please

--- core/modules/migrate_drupal_ui/src/Form/IdConflictForm.php
+++ core/modules/migrate_drupal_ui/src/Form/IdConflictForm.php
@@ -142,7 +142,7 @@ class IdConflictForm extends MigrateUpgradeFormBase {
    */
   protected function i18nWarningForm(array &$form, array $conflicts) {
     $form['i18n'] = [
-      '#title' => $this->t('Please check if you have translated content of these types:'),
+      '#title' => $this->t('Check if you have translated content of these types:'),
       '#theme' => 'item_list',
       '#items' => $this->formatConflicts($conflicts),
     ];
