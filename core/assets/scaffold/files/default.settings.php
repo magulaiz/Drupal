9af3539dc305fbe4e62e00a3c1e5279f86ef101a
Scott Euser <scotteuser@gmail.com>
2023-10-26 07:58:39 +0000

    Update default setting description to clearly state the implications of changing the default timeout. Set the commented out default timeout to match CURL's default timeout.

--- core/assets/scaffold/files/default.settings.php
+++ core/assets/scaffold/files/default.settings.php
@@ -308,14 +308,19 @@ $settings['update_free_access'] = FALSE;
 /**
  * Outgoing HTTP request timeout in seconds.
  *
- * If you have outgoing HTTP requests for oEmbed resources, some custom
- * providers may require a longer timeout than the default 5 seconds.
+ * The default for all requests using the HTTP Client is 0, which means
+ * the requests will never time out. Requests for oEmbed resources are
+ * set to 5 second timeout by default. Setting this value overrides the
+ * default timeout for all HTTP Client requests, including those for
+ * oEmbed resources. For oEmbed resources, some custom providers may require
+ * a longer timeout than the default 5 seconds.
  * - $settings['http_client_config']['timeout'] = 10; Would increase this to
- *   10 seconds.
+ *   10 seconds as well as setting a default timeout for all HTTP Client
+ *   requests.
  * You could conditionally set this to higher for CLI commands by checking
  * if PHP_SAPI === 'cli'.
  */
-# $settings['http_client_config']['timeout'] = 10;
+# $settings['http_client_config']['timeout'] = 0;
 
 /**
  * External access proxy settings:
