18593a40f317e518582cf67511e9b39763d967b6
Wim Leers <wim.leers@acquia.com>
2023-10-27 11:03:35 +0200

    `cspell` 🙄

--- core/modules/ckeditor5/tests/src/Functional/Update/CKEditor5UpdateListMultiBlockTest.php
+++ core/modules/ckeditor5/tests/src/Functional/Update/CKEditor5UpdateListMultiBlockTest.php
@@ -7,6 +7,8 @@ namespace Drupal\Tests\ckeditor5\Functional\Update;
 use Drupal\editor\Entity\Editor;
 use Drupal\FunctionalTests\Update\UpdatePathTestBase;
 
+// cspell:ignore multiblock
+
 /**
  * @covers ckeditor5_post_update_list_multiblock
  * @group Update
