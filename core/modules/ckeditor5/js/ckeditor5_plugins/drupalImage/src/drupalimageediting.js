f8fd06a68b0dc3bd96944f0a0de0693eadb1dfad
Wim Leers <wim.leers@acquia.com>
2023-10-27 09:20:02 +0200

    `cspell`

--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalImage/src/drupalimageediting.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// cspell:ignore datafilter downcasted linkimageediting emptyelement downcastdispatcher
+// cspell:ignore datafilter downcasted linkimageediting emptyelement downcastdispatcher imageloadobserver
 import { Plugin } from 'ckeditor5/src/core';
 import { setViewAttributes } from '@ckeditor/ckeditor5-html-support/src/utils';
 import ImageLoadObserver from '@ckeditor/ckeditor5-image/src/image/imageloadobserver';
