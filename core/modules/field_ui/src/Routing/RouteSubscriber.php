e5af2ec687c0ea430b1fb99560b02705846b438d
Lauri Eskola <lauri.eskola@acquia.com>
2023-10-30 19:08:41 +0200

    Address phpcs violation

--- core/modules/field_ui/src/Routing/RouteSubscriber.php
+++ core/modules/field_ui/src/Routing/RouteSubscriber.php
@@ -7,7 +7,6 @@ use Drupal\Core\Routing\RouteSubscriberBase;
 use Drupal\Core\Routing\RoutingEvents;
 use Drupal\field_ui\Controller\FieldConfigAddController;
 use Drupal\field_ui\Controller\FieldTempStoreDeleteController;
-use Drupal\field_ui\Form\FieldStorageAddSubfieldForm;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
