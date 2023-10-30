836e57332823ac16f0513e92f9ed6de53fb901ff
srishtiee <srishtibankar@gmail.com>
2023-10-30 11:20:41 +0530

    Revert one more unnecessary change

--- core/modules/menu_link_content/tests/src/Functional/MenuLinkContentFormTest.php
+++ core/modules/menu_link_content/tests/src/Functional/MenuLinkContentFormTest.php
@@ -73,7 +73,7 @@ class MenuLinkContentFormTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
 
     // Test that other menus are available when editing existing menu link.
-    $this->assertSession()->optionExists('edit-menu-parent-menu', 'main:');
+    $this->assertSession()->optionExists('menu', 'main:');
 
     $this->drupalLogin($this->basicUser);
 
