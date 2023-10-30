4f06fe6c21e8975e31452f8cb7ca6e256986bb10
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-10-30 16:24:17 +0530

    Added failing tests

---
+++ core/modules/datetime/tests/src/FunctionalJavascript/AllowedValuesForDateFieldTypeTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\Tests\datetime\FunctionalJavascript;
+
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItem;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ *
+ *
+ * @group datetime
+ */
+class AllowedValuesForDateFieldTypeTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'field_ui',
+    'toolbar',
+    'node',
+    'datetime',
+    'field',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp():void {
+    parent::setUp();
+    $this->drupalCreateContentType([
+      'name' => 'Article',
+      'type' => 'article',
+    ]);
+    $fieldStorage = FieldStorageConfig::create([
+      'field_name' => 'field_date',
+      'entity_type' => 'node',
+      'type' => 'datetime',
+      'settings' => ['datetime_type' => DateTimeItem::DATETIME_TYPE_DATETIME],
+    ]);
+    $fieldStorage->save();
+    $field = FieldConfig::create([
+      'field_storage' => $fieldStorage,
+      'bundle' => 'article',
+      'required' => TRUE,
+    ]);
+    $field->save();
+    $this->drupalLogin($this->rootUser);
+  }
+
+  /**
+   * Test of viewing announcements by a user with appropriate permission.
+   */
+  public function testAnnounceFirstLogin() {
+    $this->drupalGet('/admin/structure/types/manage/article/fields/node.article.field_date');
+    $page = $this->getSession()->getPage();
+    $page->findField('edit-field-storage-subform-cardinality-number')->setValue('-11');
+    $page->findButton('Save settings')->click();
+
+    $this->assertSession()->pageTextContains('Limit must be higher than or equal to 1.');
+  }
+
+}
