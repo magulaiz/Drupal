33f4407de1343f068b6671021845e58737a62b86
Kunal Sachdev <kunal.sachdev@acquia.com>
2023-10-31 18:28:48 +0530

    Widget test for missing end date

---
+++ core/modules/datetime_range/tests/src/Functional/DateRangeWidgetTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Drupal\Tests\datetime_range\Functional;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\datetime_range\Plugin\Field\FieldType\DateRangeItem;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+
+/**
+ * Tests Daterange widgets functionality.
+ *
+ * @group datetime
+ */
+class DateRangeWidgetTest extends DateTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['datetime_range'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getTestFieldType() {
+    return 'daterange';
+  }
+
+  /**
+   * Tests daterange field with missing end date.
+   */
+  public function testMissingEndDate() {
+    // Create a test content type.
+    $this->drupalCreateContentType(['type' => 'daterange_content']);
+
+    $field_name = $this->randomMachineName();
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => $field_name,
+      'entity_type' => 'node',
+      'type' => 'daterange',
+      'settings' => ['datetime_type' => DateRangeItem::DATETIME_TYPE_DATE],
+    ]);
+    $field_storage->save();
+
+    $field = FieldConfig::create([
+      'field_storage' => $field_storage,
+      'field_name' => $field_name,
+      'bundle' => 'daterange_content',
+      'required' => TRUE,
+    ]);
+    $field->save();
+
+    $field->setSetting('optional_end_date', TRUE)->save();
+
+    \Drupal::service('entity_display.repository')
+      ->getFormDisplay('node', 'daterange_content')
+      ->setComponent($field_name, [
+        'type' => 'datetime_default',
+      ])
+      ->save();
+
+    //Add node with start date only.
+    $this->drupalGet('node/add/daterange_content');
+    $value = '2012-12-31 00:00:00';
+    $start_date = new DrupalDateTime($value, timezone_open(date_default_timezone_get()));
+    $date_format = DateFormat::load('html_date')->getPattern();
+    $title = $this->randomString();
+    $edit = [
+      'title[0][value]' => $title,
+      'body[0][value]' => $this->randomString(),
+      $field_name . '[0][value][date]' => $start_date->format($date_format),
+    ];
+    $this->submitForm($edit, 'Save');
+    $this->assertSession()->pageTextContains('daterange_content ' . $title . ' has been created.');
+    $node = $this->drupalGetNodeByTitle($title);
+
+    //Assert correct start date and end date is stored.
+    $this->assertEquals($start_date->format($date_format), $node->get($field_name)->offsetGet(0)->value);
+    $this->assertEquals(NULL, $node->get($field_name)->offsetGet(0)->end_value);
+  }
+
+}
