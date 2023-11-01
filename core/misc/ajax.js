e74ba4fa60c2a1b579dace3e9e01d0ff6a40075c
Théodore Biadala <30255-nod_@users.noreply.drupalcode.org>
2023-11-01 12:13:55 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/misc/ajax.js
+++ core/misc/ajax.js
@@ -41,7 +41,7 @@
         }
         // Use jQuery selector instead of a native selector for
         // backwards compatibility.
-        once('drupal-ajax', $(elementSettings.selector, context)).forEach((el) => {
+        once('drupal-ajax', $(context).find(elementSettings.selector)).forEach((el) => {
           elementSettings.element = el;
           elementSettings.base = base;
           Drupal.ajax(elementSettings);
