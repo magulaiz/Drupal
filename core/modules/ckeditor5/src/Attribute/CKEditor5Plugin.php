5faf6a5a45c34119f1772624bf5993089eebf434
Wim Leers <wim.leers@acquia.com>
2023-11-03 14:52:24 +0100

    Try to get this going: define the new Attribute.

---
+++ core/modules/ckeditor5/src/Attribute/CKEditor5Plugin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\ckeditor5\Attribute;
+
+use Drupal\ckeditor5\Annotation\CKEditor5AspectsOfCKEditor5Plugin;
+use Drupal\ckeditor5\Annotation\DrupalAspectsOfCKEditor5Plugin;
+use Drupal\ckeditor5\Plugin\CKEditor5PluginDefinition;
+use Drupal\Component\Plugin\Attribute\Plugin;
+
+/**
+ * The CKEditor5Plugin attribute.
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class CKEditor5Plugin extends Plugin {
+
+  /**
+   * Constructs a CKEditor5Plugin attribute.
+   *
+   * @param string $id
+   *   The plugin ID.
+   * @param \Drupal\ckeditor5\Annotation\CKEditor5AspectsOfCKEditor5Plugin $ckeditor5
+   *   The CKEditor 5 aspects of the plugin definition.
+   * @param \Drupal\ckeditor5\Annotation\DrupalAspectsOfCKEditor5Plugin $drupal
+   *   The Drupal aspects of the plugin definition.
+   */
+  public function __construct(
+    public readonly string $id,
+    public readonly CKEditor5AspectsOfCKEditor5Plugin $ckeditor5,
+    public readonly DrupalAspectsOfCKEditor5Plugin $drupal,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClass(): string {
+    return $this->drupal->getClass();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setClass($class): void {
+    $this->drupal->setClass($class);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get(): CKEditor5PluginDefinition {
+    // @todo Figure out how to do this.
+  }
+
+}
