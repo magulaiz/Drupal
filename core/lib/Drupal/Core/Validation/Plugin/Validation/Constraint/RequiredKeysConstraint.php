4b4104f3741a044eefb463ec0fa197fda3c742a6
Wim Leers <wim.leers@acquia.com>
2023-11-03 15:40:46 +0100

    Remove dead code from `RequiredKeysConstraint`.

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/RequiredKeysConstraint.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/RequiredKeysConstraint.php
@@ -31,13 +31,6 @@ class RequiredKeysConstraint extends Constraint {
   public string $dynamicMessage = "'@key' is a required key because @dynamic_type_property_path is @dynamic_type_property_value (see config schema type @resolved_dynamic_type).";
 
   /**
-   * The error message if a key is unknown.
-   *
-   * @var string
-   */
-  public string $unknownMessage = "'@key' is an unknown key because @dynamic_type_property_path is @dynamic_type_property_value (see config schema type @resolved_dynamic_type).";
-
-  /**
    * Keys which are required — only `<infer>` supported currently.
    *
    * @var string
