f12debd697213c471192275fe556a087f918290a
Phéna Proxima <adam@phenaproxima.net>
2023-11-03 11:10:31 -0400

    Make test fail, rightfully, due to wrong behavior

--- core/modules/system/tests/src/Functional/Form/NestedConfigTargetTest.php
+++ core/modules/system/tests/src/Functional/Form/NestedConfigTargetTest.php
@@ -24,11 +24,10 @@ class NestedConfigTargetTest extends BrowserTestBase {
     $assert_session = $this->assertSession();
     $assert_session->statusMessageContains('This value should not be blank.', 'error');
     // The parent element should be marked as invalid, along with its children.
-    // @todo This is wrong behavior - only the invalid element should be
-    //   marked invalid. But at least now we have the beginnings of a test.
     $assert_session->elementAttributeExists('css', '#edit-favorites', 'aria-invalid');
     $assert_session->elementAttributeExists('named', ['field', 'First choice'], 'aria-invalid');
-    $assert_session->elementAttributeExists('named', ['field', 'Second choice'], 'aria-invalid');
+    // The second choice is fine, so it should not be marked invalid.
+    $assert_session->elementAttributeNotExists('named', ['field', 'Second choice'], 'aria-invalid');
   }
 
 }
