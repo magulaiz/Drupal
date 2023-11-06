d01793d030e451cf964afd18307e20eb9c6335d1
Alex Pott <alex.a.pott@googlemail.com>
2023-11-06 11:21:22 +0000

    Add back missing test

--- core/modules/system/tests/src/Functional/Form/ConfigTargetTest.php
+++ core/modules/system/tests/src/Functional/Form/ConfigTargetTest.php
@@ -47,4 +47,22 @@ class ConfigTargetTest extends BrowserTestBase {
     $assert_session->elementAttributeExists('named', ['field', 'Missing key'], 'aria-invalid');
   }
 
+  /**
+   * Tests #config_target where there is not a 1:1 property to element.
+   */
+  public function testNested(): void {
+    $this->drupalGet('/form-test/nested-config-target');
+    $page = $this->getSession()->getPage();
+    $page->fillField('First choice', '');
+    $page->pressButton('Save configuration');
+    $assert_session = $this->assertSession();
+    $assert_session->statusMessageContains('This value should not be blank.', 'error');
+    // The parent element should be marked as invalid, along with its children.
+    // @todo This is wrong behavior - only the invalid element should be
+    //   marked invalid. But at least now we have the beginnings of a test.
+    $assert_session->elementAttributeExists('css', '#edit-favorites', 'aria-invalid');
+    $assert_session->elementAttributeExists('named', ['field', 'First choice'], 'aria-invalid');
+    $assert_session->elementAttributeExists('named', ['field', 'Second choice'], 'aria-invalid');
+  }
+
 }
