eb2db7fc333d46d2582e2cc50ca9ddad66d05387
Phéna Proxima <adam@phenaproxima.net>
2023-11-06 13:33:38 -0500

    Fix NegotiationBrowserForm

--- core/modules/language/src/Form/NegotiationBrowserForm.php
+++ core/modules/language/src/Form/NegotiationBrowserForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\language\Form;
 
+use Drupal\Core\Config\Config;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
@@ -188,15 +189,14 @@ class NegotiationBrowserForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  protected static function copyFormValuesToConfig(Config $config, FormStateInterface $form_state): void {
+    parent::copyFormValuesToConfig($config, $form_state);
+
+    assert($config->getName() === 'language.mappings');
     $mappings = $form_state->get('mappings');
     if (!empty($mappings)) {
-      $config = $this->config('language.mappings');
       $config->setData(['map' => $mappings]);
-      $config->save();
     }
-
-    parent::submitForm($form, $form_state);
   }
 
   /**
