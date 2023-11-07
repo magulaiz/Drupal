1ac53be5548ea4cb53e7d47f9cbb35f001c61786
Dave Long <24510-longwave@users.noreply.drupalcode.org>
2023-11-07 16:05:51 +0000

    Update CacheDecoratorInterface.php

--- core/lib/Drupal/Core/CacheDecorator/CacheDecoratorInterface.php
+++ core/lib/Drupal/Core/CacheDecorator/CacheDecoratorInterface.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\Core\CacheDecorator;
 
-@trigger_error('The ' . __NAMESPACE__ . '\CacheDecoratorInterface is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Last/only usage was removed in 2020. See https://www.drupal.org/node/3398182', E_USER_DEPRECATED);
+@trigger_error('The ' . __NAMESPACE__ . '\CacheDecoratorInterface is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3398182', E_USER_DEPRECATED);
 
 /**
  * Defines an interface for cache decorator implementations.
  *
  * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0.
- *  Last/only usage was removed in 2020.
+ *  There is no replacement.
  *
  * @see https://www.drupal.org/node/3398182
  */
