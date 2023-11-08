7184d5fcca9d76da5883471652cf8309b11a6717
Alex Pott <alex.a.pott@googlemail.com>
2023-11-08 13:04:29 +0000

    Move 401 route to attributes

--- core/modules/system/src/Controller/Http4xxController.php
+++ core/modules/system/src/Controller/Http4xxController.php
@@ -3,6 +3,7 @@
 namespace Drupal\system\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Controller for default HTTP 4xx responses.
@@ -27,6 +28,12 @@ class Http4xxController extends ControllerBase {
    * @return array
    *   A render array containing the message to display for 401 pages.
    */
+  #[Route(
+    path: '/system/401',
+    name: 'system.401',
+    requirements: ['_access' => 'TRUE'],
+    defaults: ['_title' => 'Unauthorized']
+  )]
   public function on401() {
     return [
       '#markup' => $this->t('Log in to access this page.'),
