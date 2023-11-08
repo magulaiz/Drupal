31d6fb55d866e0e9856b6050d548fc17800b0d1b
Wim Leers <44946-wimleers@users.noreply.drupalcode.org>
2023-11-08 15:04:53 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/system/tests/modules/router_test_directory/src/Controller/TestAttributes.php
+++ core/modules/system/tests/modules/router_test_directory/src/Controller/TestAttributes.php
@@ -5,14 +5,8 @@ namespace Drupal\router_test\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Test controller.
- */
 class TestAttributes extends ControllerBase {
 
-  /**
-   * Provides test content.
-   */
   #[Route('/test_method_attribute', requirements: ['_access' => 'TRUE'])]
   public function attributeMethod() {
     return ['#markup' => 'Testing method with a Route attribute'];
