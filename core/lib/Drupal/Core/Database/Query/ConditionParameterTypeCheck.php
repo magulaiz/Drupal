e6ad9e208373bca3626429aee09f06c86d0791aa
David Bekker <david.bekker@finalist.nl>
2023-11-09 23:13:04 +0000

    Remove test code

--- core/lib/Drupal/Core/Database/Query/ConditionParameterTypeCheck.php
+++ core/lib/Drupal/Core/Database/Query/ConditionParameterTypeCheck.php
@@ -60,7 +60,6 @@ class ConditionParameterTypeCheck {
    */
   public function checkParameterTypes(array $table_names, array $conditions): void {
     $schemas = $this->getSchemaTables($table_names);
-//$i = FALSE;
     foreach ($conditions as $condition) {
       if (isset($condition['field']) && $condition['field'] instanceof ConditionInterface) {
         // Recursively call this method to check all nested conditions.
@@ -93,14 +92,8 @@ class ConditionParameterTypeCheck {
           }
 
           if ($throw_error) {
-//if (!$i) {
-//  dump(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
-//  $i = TRUE;
-//}
-            dump(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+            //dump(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
             @trigger_error($error_message, E_USER_DEPRECATED);
-            throw new \Exception('STOP');
-
           }
         }
       }
