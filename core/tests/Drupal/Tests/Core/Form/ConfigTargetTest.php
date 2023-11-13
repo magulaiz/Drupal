f33799d2693f095da3c5a952bf5e2f161e01166b
Wim Leers <wim.leers@acquia.com>
2023-11-13 11:36:13 +0100

    Unit test for duplicate config target detection.

--- core/tests/Drupal/Tests/Core/Form/ConfigTargetTest.php
+++ core/tests/Drupal/Tests/Core/Form/ConfigTargetTest.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\Tests\Core\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\ConfigTarget;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\RedundantEditableConfigNamesTrait;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -10,6 +15,45 @@ use Drupal\Tests\UnitTestCase;
  * @group Form
  */
 class ConfigTargetTest extends UnitTestCase {
+
+  /**
+   * @covers \Drupal\Core\Form\ConfigFormBase::storeConfigKeyToFormElementMap
+   */
+  public function testDuplicateTargetsNotAllowed(): void {
+    $form = [
+      'test' => [
+        '#type' => 'text',
+        '#default_value' => 'A test',
+        '#config_target' => new ConfigTarget('system.site', 'admin_compact_mode', 'intval', 'boolval'),
+        '#name' => 'test',
+        '#array_parents' => ['test'],
+      ],
+      'duplicate' => [
+        '#type' => 'text',
+        '#config_target' => new ConfigTarget('system.site', 'admin_compact_mode', 'intval', 'boolval'),
+        '#name' => 'duplicate',
+        '#array_parents' => ['duplicate'],
+      ],
+    ];
+
+    $test_form = new class(
+      $this->prophesize(ConfigFactoryInterface::class)->reveal(),
+      $this->prophesize(TypedConfigManagerInterface::class)->reveal(),
+    ) extends ConfigFormBase {
+      use RedundantEditableConfigNamesTrait;
+
+      public function getFormId() {
+        return 'test';
+      }
+
+    };
+    $form_state = new FormState();
+    $test_form->storeConfigKeyToFormElementMap($form['test'], $form_state);
+
+    $this->expectException(\LogicException::class);
+    $this->expectExceptionMessage('Two #config_targets both target "admin_compact_mode" in the "system.site" config: `$form[\'test\']` and `$form[\'duplicate\']`.');
+    $test_form->storeConfigKeyToFormElementMap($form['duplicate'], $form_state);
+  }
 
   /**
    * @covers ::fromForm
