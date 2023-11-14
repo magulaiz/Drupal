3b80fa6e4e686e32bbba2c34d2a9e01ec11b68ea
Joao Paulo <joaopauloctga@gmail.com>
2023-11-14 16:52:43 -0300

    drupalcs

--- core/modules/block/src/BlockListBuilder.php
+++ core/modules/block/src/BlockListBuilder.php
@@ -255,8 +255,16 @@ class BlockListBuilder extends ConfigEntityListBuilder implements FormInterface 
         ],
       ];
 
+      $titleLink = $this->t('Toggle blocks on region @region', ['@region' => $region]);
+      $showFiltered = $this->t('Show filtered');
       $filter = [
-        '#markup' => '<a aria-hidden="true" title="' . $this->t('Toggle blocks on region @region', ["@region" => $region]) . '" aria-pressed="false" href="#" data-toggle-region="' . $region . '" class="button button--small region-filter-control">' . $this->t('Show filtered') . '</a>',
+        '#markup' => '<a
+          aria-hidden="true"
+          title="' . $titleLink . '"
+          aria-pressed="false"
+          href="#"
+          data-toggle-region="' . $region . '"
+          class="button button--small region-filter-control">' . $showFiltered . '</a>',
       ];
 
       $form['region-' . $region]['title'] = [
