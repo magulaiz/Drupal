9251ab10432ba05738aeee65f2a71199fefb77f9
Anushri Kumari <53583-anushri19@users.noreply.drupalcode.org>
2023-11-15 00:38:47 +0000

    #3401464: Updated date_time category for datetime module.

--- core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
+++ core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
@@ -16,6 +16,7 @@ use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
  * @FieldType(
  *   id = "daterange",
  *   label = @Translation("Date range"),
+ *   category = "date_time",
  *   description = @Translation("Create and store date ranges."),
  *   default_widget = "daterange_default",
  *   default_formatter = "daterange_default",
