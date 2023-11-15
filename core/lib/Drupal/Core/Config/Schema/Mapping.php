f6284010d77b1766ac7ac95fe45e79ec2df7c800
Wim Leers <wim.leers@acquia.com>
2023-11-15 18:21:46 +0100

    Add methods to `Mapping` and add Kernel test.

--- core/lib/Drupal/Core/Config/Schema/Mapping.php
+++ core/lib/Drupal/Core/Config/Schema/Mapping.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Core\Config\Schema;
 
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\TypedData\MapDataDefinition;
+
 /**
  * Defines a mapping configuration element.
  *
@@ -24,6 +27,248 @@ class Mapping extends ArrayElement {
     $value = $this->value[$key] ?? NULL;
     $definition = $this->definition['mapping'][$key] ?? [];
     return $this->buildDataDefinition($definition, $value, $key);
+  }
+
+  /**
+   * Gets all valid keys in this mapping.
+   *
+   * @return string[]
+   *   A list of valid keys given the values in this mapping.
+   */
+  public function getValidKeys(): array {
+    $all_keys = $this->getDefinedKeys();
+    return array_keys($all_keys);
+  }
+
+  /**
+   * Gets all required keys in this mapping.
+   *
+   * Keys are required by default, they can opt out by specifying
+   * `requiredKey: false`. Deprecated keys are also treated as optional.
+   *
+   * @return string[]
+   *   A list of required keys given the values in this mapping.
+   */
+  public function getRequiredKeys(): array {
+    $all_keys = $this->getDefinedKeys();
+    $required_keys = array_filter(
+      $all_keys,
+      fn (array $raw_schema_definition) => !array_key_exists('requiredKey', $raw_schema_definition) && !array_key_exists('deprecated', $raw_schema_definition),
+    );
+    return array_keys($required_keys);
+  }
+
+  /**
+   * Gets the keys defined for this mapping (locally defined + inherited).
+   *
+   * @return array
+   *   Raw schema definitions: keys are mapping keys, values are their
+   *   definitions.
+   */
+  protected function getDefinedKeys(): array {
+    $definition = $this->getDataDefinition();
+    assert($definition instanceof MapDataDefinition && self::validateMappingConfigSchemaDefinition($definition));
+    // f.e. when using `type: mapping`, no keys have been defined, but it's
+    // still possible to define keys under `mapping: {…}`.
+    return $definition->toArray()['mapping'];
+  }
+
+  /**
+   * Gets all dynamically valid keys.
+   *
+   * When the `type` of the mapping is dynamic itself. For example: the settings
+   * associated with a FieldConfig depends on which field plugin that field
+   * uses.
+   * Other examples:
+   * - CKEditor 5 uses 'ckeditor5.plugin.[%key]', but that uses no information
+   *   stored elsewhere: the chosen key (in a sequence) determines the type.
+   * - third party settings use '[%parent.%parent.%type].third_party.[%key]',
+   *   but the first variable value only causes the config entity type (at the
+   *   root) to be inherited (f.e. 'node.type.third_party.[%key]').
+   * - field instances 'field.value.[%parent.%parent.field_type]', which is
+   *   used to determine the type of the default field value, to ensure
+   *   matches the precise config schema type of the field type
+   * - views use 'views.filter.[plugin_id]' to allow the value itself to
+   *   determine which filter plugin it  uses, in the 'plugin_id' key.
+   *
+   * For each of these examples, the mapping at this property path may have keys
+   * that are dynamically valid. This means that depending on other values
+   * (hence "dynamically"), different sets of keys are considered valid.
+   *
+   * @return string[][]
+   *   A list of dynamically valid keys. An array with:
+   *   - a key for every possible resolved type
+   *   - the corresponding value an array of the additional mapping keys that
+   *     are supported for this resolved type
+   *
+   * @see \Drupal\Core\Config\TypedConfigManager::replaceName()
+   * @see \Drupal\Core\Config\TypedConfigManager::replaceVariable()
+   */
+  public function getDynamicallyValidKeys(): array {
+    $parent_data_def = $this->getParent()?->getDataDefinition();
+    if ($parent_data_def === NULL) {
+      return [];
+    }
+
+    // The parent mapping definition is used to determine the type used to
+    // determine the type of this mapping.
+    // f.e.:
+    // 1. `type: editor.settings.[%parent.editor]`
+    // 2. `type: editor.image_upload_settings.[status]`.
+    $original_mapping_type = match (TRUE) {
+      $parent_data_def instanceof MapDataDefinition => $parent_data_def->toArray()['mapping'][$this->getName()]['type'],
+      $parent_data_def instanceof SequenceDataDefinition => $parent_data_def->toArray()['sequence']['type'],
+      default => throw new \LogicException('Invalid config schema detected.'),
+    };
+
+    // If the original mapping type is not dynamic, there's no additional work.
+    if (!str_contains($original_mapping_type, ']')) {
+      return [];
+    }
+    // Prefix-based dynamic typing is used only by third party settings, which
+    // are by definition optional.
+    elseif (str_starts_with($original_mapping_type, '[')) {
+      return [];
+    }
+
+    // Find all possible types for the given original mapping type.
+    // f.e.:
+    // 1. `editor.settings.unicorn` or `editor.settings.trex`
+    // 2. `editor.image_upload_settings.*` or `editor.image_upload_settings.1`
+    $possible_types = $this->getPossibleTypes($original_mapping_type);
+
+    // TRICKY: it is tempting to not consider this a dynamic type if only one
+    // concrete type is installed. But this would lead to different validation
+    // errors when modules are installed or uninstalled.
+    assert(!empty($possible_types));
+
+    // Determine all valid keys across all possible types.
+    $all_type_definitions = $this->getTypedDataManager()->getDefinitions();
+    $possible_type_definitions = array_intersect_key($all_type_definitions, array_fill_keys($possible_types, TRUE));
+    // TRICKY: \Drupal\Core\Config\TypedConfigManager::getDefinition() does the
+    // necessary resolving, but TypedConfigManager::getDefinitions() does not! 🤷‍♂️
+    // @see \Drupal\Core\Config\TypedConfigManager::getDefinitionWithReplacements()
+    // @see ::getValidKeys()
+    $valid_keys_per_type = array_map(
+      fn (string $possible_type) => array_keys($this->getTypedDataManager()->getDefinition($possible_type)['mapping'] ?? []),
+      // Keep the original types, but array_map() does not allow using the keys,
+      // so pass the same information twice: this logic will replace the values.
+      array_combine(
+        array_keys($possible_type_definitions),
+        array_keys($possible_type_definitions),
+      ),
+    );
+
+    // From all valid keys across all types, get the ones for the fallback type:
+    // the keys in this mapping definition are inherited by all type definitions
+    // and are hence valid everywhere. Not all types have a fallback type.
+    // @see \Drupal\Core\Config\TypedConfigManager::getDefinitionWithReplacements()
+    $fallback_type = $this->getTypedDataManager()->findFallback($original_mapping_type);
+    $valid_keys_everywhere = array_intersect_key(
+      $valid_keys_per_type,
+      [$fallback_type => NULL],
+    );
+    assert(count($valid_keys_everywhere) <= 1);
+    $statically_required_keys = NestedArray::mergeDeepArray($valid_keys_everywhere);
+
+    // Now that statically valid keys are known, determine which valid keys are
+    // only valid in some cases: filter away the statically valid keys that are
+    // present in each per-type array of valid keys.
+    $valid_keys_some = array_diff_key($valid_keys_per_type, $valid_keys_everywhere);
+    $valid_keys_some_processed = array_map(
+      fn (array $keys) => array_values(array_filter($keys, fn (string $key) => !in_array($key, $statically_required_keys, TRUE))),
+      $valid_keys_some
+    );
+    return $valid_keys_some_processed;
+  }
+
+  /**
+   * Gets all optional keys in this mapping.
+   *
+   * @return string[]
+   *   A list of optional keys given the values in this mapping.
+   */
+  public function getOptionalKeys(): array {
+    return array_values(array_diff($this->getValidKeys(), $this->getRequiredKeys()));
+  }
+
+  /**
+   * Validates optional `requiredKey` flags in mappings.
+   *
+   * Validates that the values for either optional flag are correct. Does not
+   * validate the semantics, only the shapes.
+   *
+   * @param \Drupal\Core\TypedData\MapDataDefinition $definition
+   *   The config schema definition for a `type: mapping`.
+   *
+   * @return bool
+   */
+  protected static function validateMappingConfigSchemaDefinition(MapDataDefinition $definition): bool {
+    $definition = $definition->toArray();
+    assert(array_key_exists('mapping', $definition));
+
+    // Validates `requiredKey` flag in mapping definitions.
+    foreach ($definition['mapping'] as $options) {
+      if (!array_key_exists('requiredKey', $options)) {
+        // This flag is optional.
+        continue;
+      }
+      if ($options['requiredKey'] !== FALSE) {
+        throw new \LogicException('The `requiredKey` flag must either be omitted or have `false` as the value.');
+      }
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * Returns all possible types for the type with the given name.
+   *
+   * @param string $name
+   *   Configuration name or key.
+   *
+   * @return string[]
+   *   All possible types for a given type. For example,
+   *   `core_date_format_pattern.[%parent.locked]` will return:
+   *   - `core_date_format_pattern.0`
+   *   - `core_date_format_pattern.1`
+   *   If a fallback name is available, that will be returned too. In this
+   *   example, that would be `core_date_format_pattern.*`.
+   */
+  protected function getPossibleTypes(string $name): array {
+    // First, parse from e.g.
+    // `module.something.foo_[%parent.locked]`
+    // this:
+    // `[%parent.locked]`
+    // or from
+    // `[%parent.%parent.%type].third_party.[%key]`
+    // this:
+    // `[%parent.%parent.%type]` and `[%key]`.
+    // And collapse all these to just `[]`.
+    // @see \Drupal\Core\Config\TypedConfigManager::replaceVariable()
+    $matches = [];
+    if (preg_match_all('/(\[[^\]]+\])/', $name, $matches) >= 1) {
+      $name = str_replace($matches[0], '[]', $name);
+    }
+    // Then, replace all `[]` occurrences with `.*` and escape all periods for
+    // use in a regex. So:
+    // `module\.something\.foo_.*`
+    // or
+    // `.*\.third_party\..*`
+    $regex = str_replace(['.', '[]'], ['\.', '.*'], $name);
+    // Now find all possible types:
+    // 1. `module.something.foo_foo`, `module.something.foo_bar`, etc.
+    $possible_types = array_filter(
+      array_keys($this->getTypedDataManager()->getDefinitions()),
+      fn (string $type) => preg_match("/^$regex$/", $type) === 1
+    );
+    // 2. The fallback: `module.something.*` — if no concrete definition for it
+    // exists.
+    $fallback_type = $this->getTypedDataManager()->findFallback($name);
+    if ($fallback_type && !in_array($fallback_type, $possible_types, TRUE)) {
+      $possible_types[] = $fallback_type;
+    }
+    return $possible_types;
   }
 
 }
---
+++ core/tests/Drupal/KernelTests/Config/Schema/MappingTest.php
@@ -0,0 +1,282 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\KernelTests\Config\Schema;
+
+use Drupal\block\Entity\Block;
+use Drupal\Core\Config\Schema\Mapping;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Config\Schema\Mapping
+ * @group Config
+ */
+class MappingTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $configSchemaCheckerExclusions = [
+    'config_schema_deprecated_test.settings',
+  ];
+
+  /**
+   * @dataProvider providerMappingInterpretation
+   */
+  public function testMappingInterpretation(
+    string $config_name,
+    ?string $property_path,
+    array $expected_valid_keys,
+    array $expected_optional_keys,
+    array $expected_dynamically_valid_keys,
+  ): void {
+    // Some config needs some dependencies installed.
+    switch ($config_name) {
+      case 'block.block.branding':
+        $this->enableModules(['system', 'block']);
+        /** @var \Drupal\Core\Extension\ThemeInstallerInterface $theme_installer */
+        $theme_installer = $this->container->get('theme_installer');
+        $theme_installer->install(['stark']);
+        Block::create([
+          'id' => 'branding',
+          'plugin' => 'system_branding_block',
+          'theme' => 'stark',
+          'status' => TRUE,
+          'settings' => [
+            'use_site_logo' => TRUE,
+            'use_site_name' => TRUE,
+            'use_site_slogan' => TRUE,
+            'label_display' => FALSE,
+            // TRICKY: these 4 are inherited from `type: block_settings`.
+            'status' => TRUE,
+            'info' => '',
+            'view_mode' => 'full',
+            'context_mapping' => [],
+          ],
+        ])->save();
+        break;
+
+      case 'block.block.local_tasks':
+        $this->enableModules(['system', 'block']);
+        /** @var \Drupal\Core\Extension\ThemeInstallerInterface $theme_installer */
+        $theme_installer = $this->container->get('theme_installer');
+        $theme_installer->install(['stark']);
+        Block::create([
+          'id' => 'local_tasks',
+          'plugin' => 'local_tasks_block',
+          'theme' => 'stark',
+          'status' => TRUE,
+          'settings' => [
+            'primary' => TRUE,
+            'secondary' => FALSE,
+            // TRICKY: these 4 are inherited from `type: block_settings`.
+            'status' => TRUE,
+            'info' => '',
+            'view_mode' => 'full',
+            'context_mapping' => [],
+          ],
+        ])->save();
+        break;
+
+      case 'config_schema_deprecated_test.settings':
+        $this->enableModules(['config_schema_deprecated_test']);
+        $config = $this->config('config_schema_deprecated_test.settings');
+        // @see \Drupal\KernelTests\Core\Config\ConfigSchemaDeprecationTest
+        $config
+          ->set('complex_structure_deprecated.type', 'fruits')
+          ->set('complex_structure_deprecated.products', ['apricot', 'apple'])
+          ->save();
+        break;
+    }
+
+    /** @var \Drupal\Core\Config\TypedConfigManagerInterface $typed_config_manager */
+    $typed_config_manager = \Drupal::service('config.typed');
+    $mapping = $typed_config_manager->get($config_name);
+    if ($property_path) {
+      $mapping = $mapping->get($property_path);
+    }
+
+    assert($mapping instanceof Mapping);
+    $expected_required_keys = array_values(array_diff($expected_valid_keys, $expected_optional_keys));
+    $this->assertSame($expected_valid_keys, $mapping->getValidKeys());
+    $this->assertSame($expected_required_keys, $mapping->getRequiredKeys());
+    $this->assertSame($expected_dynamically_valid_keys, $mapping->getDynamicallyValidKeys());
+    $this->assertSame($expected_optional_keys, $mapping->getOptionalKeys());
+  }
+
+  public function providerMappingInterpretation(): \Generator {
+    $available_block_settings_types = [
+      'block.settings.field_block:*:*:*' => [
+        'formatter',
+      ],
+      'block.settings.extra_field_block:*:*:*' => [
+        'formatter',
+      ],
+      'block.settings.system_branding_block' => [
+        'use_site_logo',
+        'use_site_name',
+        'use_site_slogan',
+      ],
+      'block.settings.system_menu_block:*' => [
+        'level',
+        'depth',
+        'expand_all_items',
+      ],
+      'block.settings.local_tasks_block' => [
+        'primary',
+        'secondary',
+      ],
+    ];
+
+    // A simple config often is just a single Mapping object.
+    yield 'core.extension' => [
+      'core.extension',
+      NULL,
+      [
+        // Keys inherited from `type: config_object`.
+        // @see core/config/schema/core.data_types.schema.yml
+        '_core',
+        'langcode',
+        // Keys defined locally, in `type: core.extension`.
+        // @see core/config/schema/core.extension.schema.yml
+        'module',
+        'theme',
+        'profile',
+      ],
+      ['_core'],
+      [],
+    ];
+
+    // Special case: deprecated  is needed for deprecated config schema:
+    // - deprecated keys are treated as optional
+    // - if a deprecated property path is itself a mapping, then the keys inside
+    //   are not optional
+    yield 'config_schema_deprecated_test.settings' => [
+      'config_schema_deprecated_test.settings',
+      NULL,
+      [
+        // Keys inherited from `type: config_object`.
+        // @see core/config/schema/core.data_types.schema.yml
+        '_core',
+        'langcode',
+        // Keys defined locally, in `type: config_schema_deprecated_test.settings`.
+        // @see core/modules/config/tests/config_schema_deprecated_test/config/schema/config_schema_deprecated_test.schema.yml
+        'complex_structure_deprecated',
+      ],
+      ['_core', 'complex_structure_deprecated'],
+      [],
+    ];
+    yield 'config_schema_deprecated_test.settings:complex_structure_deprecated' => [
+      'config_schema_deprecated_test.settings',
+      'complex_structure_deprecated',
+      [
+        // Keys defined locally, in `type: config_schema_deprecated_test.settings`.
+        // @see core/modules/config/tests/config_schema_deprecated_test/config/schema/config_schema_deprecated_test.schema.yml
+        'type',
+        'products',
+      ],
+      [],
+      [],
+    ];
+
+    // A config entity is always a Mapping at the top level, but most nesting is
+    // also using Mappings (unless the keys are free to be chosen, then a
+    // Sequence would be used).
+    yield 'block.block.branding' => [
+      'block.block.branding',
+      NULL,
+      [
+        // Keys inherited from `type: config_entity`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'uuid',
+        'langcode',
+        'status',
+        'dependencies',
+        'third_party_settings',
+        '_core',
+        // Keys defined locally, in `type: block.block.*`.
+        // @see core/modules/block/config/schema/block.schema.yml
+        'id',
+        'theme',
+        'region',
+        'weight',
+        'provider',
+        'plugin',
+        'settings',
+        'visibility',
+      ],
+      ['third_party_settings', '_core'],
+      [],
+    ];
+
+    // An example of nested Mapping objects in config entities.
+    yield 'block.block.branding:dependencies' => [
+      'block.block.branding',
+      'dependencies',
+      [
+        // Keys inherited from `type: config_dependencies_base`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'config',
+        'content',
+        'module',
+        'theme',
+        // Keys defined locally, in `type: config_dependencies`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'enforced',
+      ],
+      // All these keys are optional!
+      ['config', 'content', 'module', 'theme', 'enforced'],
+      [],
+    ];
+
+    // And finally, two examples of dynamic typing in config schema, and the
+    // consequences on what keys are considered valid: it depends on the block
+    // plugin being used.
+    yield 'block.block.branding:settings' => [
+      'block.block.branding',
+      'settings',
+      [
+        // Keys inherited from `type: block.settings.*`, which in turn is
+        // inherited from `type: block_settings`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'id',
+        'label',
+        'label_display',
+        'provider',
+        'status',
+        'info',
+        'view_mode',
+        'context_mapping',
+        // Keys defined locally, in `type: block.settings.system_branding_block`.
+        // @see core/modules/block/config/schema/block.schema.yml
+        ...$available_block_settings_types['block.settings.system_branding_block'],
+      ],
+      [],
+      $available_block_settings_types,
+    ];
+    yield 'block.block.local_tasks:settings' => [
+      'block.block.local_tasks',
+      'settings',
+      [
+        // Keys inherited from `type: block.settings.*`, which in turn is
+        // inherited from `type: block_settings`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'id',
+        'label',
+        'label_display',
+        'provider',
+        'status',
+        'info',
+        'view_mode',
+        'context_mapping',
+        // Keys defined locally, in `type: block.settings.local_tasks_block`.
+        // @see core/modules/system/config/schema/system.schema.yml
+        ...$available_block_settings_types['block.settings.local_tasks_block'],
+      ],
+      [],
+      $available_block_settings_types,
+    ];
+  }
+
+}
