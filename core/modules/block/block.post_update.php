c793450f6158cef9a679b832cf3faba8992b12c1
Alastair Moore <alastair.moore@tpximpact.com>
2023-11-16 21:30:03 +0000

    Reverted block update hook, added post update hook to update existing label_display settings

--- core/modules/block/block.post_update.php
+++ core/modules/block/block.post_update.php
@@ -5,6 +5,9 @@
  * Post update functions for Block.
  */
 
+use Drupal\block\BlockInterface;
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -15,4 +18,17 @@ function block_removed_post_updates() {
     'block_post_update_fix_negate_in_conditions' => '9.0.0',
     'block_post_update_replace_node_type_condition' => '10.0.0',
   ];
+}
+
+/**
+ * Update Block config schema to change label_display type.
+ */
+function block_post_update_label_display_type(?array &$sandbox = NULL) {
+  $config_entity_updater = \Drupal::classResolver(ConfigEntityUpdater::class);
+  $config_entity_updater->update($sandbox, 'block', function (BlockInterface $block): bool {
+    $settings = $block->get('settings');
+    $settings['label_display'] = (bool) $settings['label_display'];
+    $block->set('settings', $settings);
+    return TRUE;
+  });
 }
