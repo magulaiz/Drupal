5f4870dad8f85a60986bdd66d3fdc6eb480dfbc2
Théodore BIADALA <theodore@biadala.net>
2023-11-16 01:29:30 +0100

    select elements do not just match the html string

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/CommandsTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/CommandsTest.php
@@ -35,8 +35,9 @@ class CommandsTest extends WebDriverTestBase {
 
     // Tests the 'add_css' command.
     $page->pressButton("AJAX 'add_css' command");
-    $this->assertWaitPageContains('my/file.css');
-    $this->assertWaitPageContains('https://example.com/css?family=Open+Sans');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->assertNotEmpty($this->cssSelect('link[href="my/file.css"]'), 'Local CSS loaded');
+    $this->assertNotEmpty($this->cssSelect('link[href="https://example.com/css?family=Open+Sans"]'), 'Remote CSS loaded');
 
     // Tests the 'after' command.
     $page->pressButton("AJAX 'After': Click to put something after the div");
