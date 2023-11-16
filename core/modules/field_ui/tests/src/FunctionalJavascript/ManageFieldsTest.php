fce6e7a45c36349657b2ea8ca914943f29bc40b6
Lauri Eskola <lauri.eskola@acquia.com>
2023-11-16 14:56:55 +0200

    phpcs

--- core/modules/field_ui/tests/src/FunctionalJavascript/ManageFieldsTest.php
+++ core/modules/field_ui/tests/src/FunctionalJavascript/ManageFieldsTest.php
@@ -184,8 +184,6 @@ class ManageFieldsTest extends WebDriverTestBase {
 
     $this->clickLink('Number');
     $assert_session->assertWaitOnAjaxRequest();
-//        $this->getSession()->executeScript('document.body.style.backgroundColor = "yellow"');
-//    $this->assertSession()->waitForElementVisible('css', ".test-go", 50000000);
     $assert_session->waitForText('Add field: Number');
     $buttons = $this->assertSession()->elementExists('css', '.ui-dialog-buttonpane');
     $buttons->pressButton('Continue');
