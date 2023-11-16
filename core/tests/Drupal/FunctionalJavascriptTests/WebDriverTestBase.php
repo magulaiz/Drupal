314e9b56902a18aec15cfcfa8c6b5a6eae9d97e6
Lauri Eskola <lauri.eskola@acquia.com>
2023-11-16 14:15:51 +0200

    Revert "phpcs"
    This reverts commit 63c98778318349d4834b009040d6b93a937fd214.

--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
@@ -3,6 +3,7 @@
 namespace Drupal\FunctionalJavascriptTests;
 
 use Behat\Mink\Exception\DriverException;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Tests\BrowserTestBase;
 use PHPUnit\Runner\BaseTestRunner;
 use Symfony\Component\DependencyInjection\ContainerInterface;
