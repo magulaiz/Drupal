e0f669e276d91ae8a66ae32d1ab231ee1a47c25b
Michael Strelan <mstrelan@gmail.com>
2023-11-16 15:49:23 +1000

    Fix field_ui.js

--- core/modules/field_ui/js/field_ui.js
+++ core/modules/field_ui/js/field_ui.js
@@ -71,19 +71,22 @@
   // eslint-disable-next-line func-names
   Drupal.Ajax.prototype.beforeSend = function () {
     // Disable the submit button on AJAX request initiation.
-    $('.field-config-edit-form [data-drupal-selector="edit-submit"]').prop(
-      'disabled',
-      true,
+    $('.field-config-edit-form [data-drupal-selector="edit-submit"]').each(
+      function () {
+        this.disabled = true;
+      },
     );
+
     // eslint-disable-next-line prefer-rest-params
     return originalAjaxBeforeSend.apply(this, arguments);
   };
   // Re-enable the submit button after AJAX request is completed.
   // eslint-disable-next-line
   $(document).on('ajaxComplete', () => {
-    $('.field-config-edit-form [data-drupal-selector="edit-submit"]').prop(
-      'disabled',
-      false,
+    $('.field-config-edit-form [data-drupal-selector="edit-submit"]').each(
+      function () {
+        this.disabled = false;
+      },
     );
   });
 
