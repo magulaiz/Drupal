3e20daeba6972da8ea9e47092f4071964f1c5fc5
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-11-16 19:50:17 +0000

    Moved common dumper setup code to a new parent class out of the test namespace.

--- core/tests/Drupal/TestTools/TestVarDumper.php
+++ core/tests/Drupal/TestTools/TestVarDumper.php
@@ -2,12 +2,9 @@
 
 namespace Drupal\TestTools;
 
-use Drupal\Core\Utility\Attribute\OmitFromDump;
-use ReflectionClass;
-use Symfony\Component\VarDumper\Cloner\Stub;
+use Drupal\Core\Utility\VarDumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
-use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 /**
  * Provides handlers for the Symfony VarDumper to work within tests.
@@ -15,7 +12,7 @@ use Symfony\Component\VarDumper\Dumper\HtmlDumper;
  * This allows the dump() function to produce output on the terminal without
  * causing PHPUnit to complain.
  */
-class TestVarDumper {
+class TestVarDumper extends VarDumper {
 
   /**
    * A CLI handler for \Symfony\Component\VarDumper\VarDumper.
@@ -23,14 +20,7 @@ class TestVarDumper {
   public static function cliHandler($var) {
     $cloner = new VarCloner();
 
-    if (is_object($var)) {
-      // Add a caster specifically for the class of the object being dumped, as
-      // defining a general caster doesn't seem to be supported.
-      $casters = [
-        get_class($var) => static::class . '::' . 'removePropertiesCaster',
-      ];
-      $cloner->addCasters($casters);
-    }
+    static::addCasters($cloner, $var);
 
     $dumper = new CliDumper();
     fwrite(STDOUT, "\n");
@@ -45,35 +35,6 @@ class TestVarDumper {
         }
       }
     );
-  }
-
-  /**
-   * Caster to remove properties from objects.
-   *
-   * Removes properties marked with the \Drupal\Core\Utility\OmitFromDump
-   * attribute.
-   */
-  public static function removePropertiesCaster($object, $array, Stub $stub, $isNested, $filter) {
-    if (is_object($object)) {
-      $reflection_class = new \ReflectionClass($object);
-      foreach ($reflection_class->getProperties() as $reflection_property) {
-        if ($reflection_property->getAttributes(OmitFromDump::class)) {
-          unset($array["\0*\0" . $reflection_property->getName()]);
-        }
-      }
-    }
-
-    return $array;
-  }
-
-
-  /**
-   * A HTML handler for \Symfony\Component\VarDumper\VarDumper.
-   */
-  public static function htmlHandler($var) {
-    $cloner = new VarCloner();
-    $dumper = new HtmlDumper();
-    $dumper->dump($cloner->cloneVar($var));
   }
 
 }
