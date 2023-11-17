780e4ae3aa64b7de531d4cb96eab526e976ee984
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-11-17 17:03:02 +0000

    Converted config entity translation list.

--- core/modules/config_translation/src/Controller/ConfigTranslationEntityListBuilder.php
+++ core/modules/config_translation/src/Controller/ConfigTranslationEntityListBuilder.php
@@ -40,7 +40,7 @@ class ConfigTranslationEntityListBuilder extends ConfigEntityListBuilder impleme
     ];
 
     $build['filters']['text'] = [
-      '#type' => 'search',
+      '#type' => 'list_filter',
       '#title' => $this->t('Search'),
       '#size' => 30,
       '#placeholder' => $filter['placeholder'],
@@ -50,11 +50,14 @@ class ConfigTranslationEntityListBuilder extends ConfigEntityListBuilder impleme
         'autocomplete' => 'off',
         'title' => $filter['description'],
       ],
+      '#list_container_id' => 'config-translation-entity-list',
+      '#list_item' => 'tbody tr',
+      '#list_text' => 'td.table-filter-text-source',
     ];
 
+    $build['table']['#attributes']['id'] = 'config-translation-entity-list';
     $build['table']['#attributes']['class'][] = 'config-translation-entity-list';
     $build['table']['#weight'] = 0;
-    $build['#attached']['library'][] = 'system/drupal.system.modules';
 
     return $build;
   }
