3335f33f1e53a941156b58197ed79a2d4826deb4
Simon Hobbs <simon@hobbs.id.au>
2023-11-20 00:51:02 +1100

    Revert "Preset a sensible dfeault like the link field does it."
    This reverts commit c94a5fafeb77630cd8088c63af44fd07fdbd291a.

--- core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
+++ core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
@@ -29,7 +29,7 @@ class DateRangeItem extends DateTimeItem {
    */
   public static function defaultFieldSettings() {
     return [
-      'optional_values' => static::OPTIONAL_NONE,
+      'optional_values' => FALSE,
     ] + parent::defaultFieldSettings();
   }
 
