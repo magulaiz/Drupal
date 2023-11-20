9ffb21955b992ee06dab063dc78682a2259ad4da
Wim Leers <wim.leers@acquia.com>
2023-11-20 15:29:50 +0100

    Explicit test coverage for [childkey]-based dynamic type selection.

--- core/tests/Drupal/KernelTests/Config/Schema/MappingTest.php
+++ core/tests/Drupal/KernelTests/Config/Schema/MappingTest.php
@@ -79,6 +79,41 @@ class MappingTest extends KernelTestBase {
         ])->save();
         break;
 
+      case 'block.block.positively_powered___alternate_reality_with_fallback_type___':
+        // $this->enableModules(['config_schema_add_fallback_type_test']);
+        $id = 'positively_powered___alternate_reality_with_fallback_type___';
+      case 'block.block.positively_powered':
+        $this->enableModules(['system', 'block']);
+        /** @var \Drupal\Core\Extension\ThemeInstallerInterface $theme_installer */
+        $theme_installer = $this->container->get('theme_installer');
+        $theme_installer->install(['stark']);
+        Block::create([
+          'id' => $id ?? 'positively_powered',
+          'plugin' => 'system_powered_by_block',
+          'theme' => 'stark',
+          'status' => TRUE,
+          'settings' => [
+            'label_display' => FALSE,
+            // TRICKY: these 4 are inherited from `type: block_settings`.
+            'status' => TRUE,
+            'info' => '',
+            'view_mode' => 'full',
+            'context_mapping' => [],
+          ],
+          // Avoid showing "Powered by Drupal" on 404 responses.
+          'visibility' => [
+            'I_CAN_CHOOSE_THIS' => [
+              // This is what determines the
+              'id' => 'response_status',
+              'negate' => FALSE,
+              'status_codes' => [
+                404,
+              ],
+            ],
+          ],
+        ])->save();
+        break;
+
       case 'config_schema_deprecated_test.settings':
         $this->enableModules(['config_schema_deprecated_test']);
         $config = $this->config('config_schema_deprecated_test.settings');
@@ -130,7 +165,7 @@ class MappingTest extends KernelTestBase {
     ];
 
     // A simple config often is just a single Mapping object.
-    yield 'core.extension' => [
+    yield 'No dynamic type: core.extension' => [
       'core.extension',
       NULL,
       [
@@ -152,7 +187,7 @@ class MappingTest extends KernelTestBase {
     // - deprecated keys are treated as optional
     // - if a deprecated property path is itself a mapping, then the keys inside
     //   are not optional
-    yield 'config_schema_deprecated_test.settings' => [
+    yield 'No dynamic type: config_schema_deprecated_test.settings' => [
       'config_schema_deprecated_test.settings',
       NULL,
       [
@@ -167,7 +202,7 @@ class MappingTest extends KernelTestBase {
       ['_core', 'complex_structure_deprecated'],
       [],
     ];
-    yield 'config_schema_deprecated_test.settings:complex_structure_deprecated' => [
+    yield 'No dynamic type: config_schema_deprecated_test.settings:complex_structure_deprecated' => [
       'config_schema_deprecated_test.settings',
       'complex_structure_deprecated',
       [
@@ -183,7 +218,7 @@ class MappingTest extends KernelTestBase {
     // A config entity is always a Mapping at the top level, but most nesting is
     // also using Mappings (unless the keys are free to be chosen, then a
     // Sequence would be used).
-    yield 'block.block.branding' => [
+    yield 'No dynamic type: block.block.branding' => [
       'block.block.branding',
       NULL,
       [
@@ -211,7 +246,7 @@ class MappingTest extends KernelTestBase {
     ];
 
     // An example of nested Mapping objects in config entities.
-    yield 'block.block.branding:dependencies' => [
+    yield 'No dynamic type: block.block.branding:dependencies' => [
       'block.block.branding',
       'dependencies',
       [
@@ -230,10 +265,11 @@ class MappingTest extends KernelTestBase {
       [],
     ];
 
-    // And finally, two examples of dynamic typing in config schema, and the
+    // Two examples of %parent-based dynamic typing in config schema, and the
     // consequences on what keys are considered valid: it depends on the block
-    // plugin being used.
-    yield 'block.block.branding:settings' => [
+    // plugin being used. See `type: block.block.*`, which uses
+    // `type: block.settings.[%parent.plugin]`.
+    yield 'Dynamic type with [%parent]: block.block.branding:settings' => [
       'block.block.branding',
       'settings',
       [
@@ -255,7 +291,7 @@ class MappingTest extends KernelTestBase {
       [],
       $available_block_settings_types,
     ];
-    yield 'block.block.local_tasks:settings' => [
+    yield 'Dynamic type with [%parent]: block.block.local_tasks:settings' => [
       'block.block.local_tasks',
       'settings',
       [
@@ -276,6 +312,97 @@ class MappingTest extends KernelTestBase {
       ],
       [],
       $available_block_settings_types,
+    ];
+
+    // An example of childkey-based dynamic mapping typing in config schema, for
+    // a mapping inside a sequence: the `id` key-value pair in the mapping
+    // determines the type of the mapping. The key in the sequence whose value
+    // is the mapping is irrelevant, it can be arbitrarily chosen. See
+    // `type: block.block.*` which uses `type: condition.plugin.[id]`.
+    yield 'Dynamic type with [childkey]: block.block.positively_powered:visibility.I_CAN_CHOOSE_THIS' => [
+      'block.block.positively_powered',
+      'visibility.I_CAN_CHOOSE_THIS',
+      [
+        // Keys inherited from `type: condition.plugin`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'id',
+        'negate',
+        'uuid',
+        'context_mapping',
+        // Keys defined locally, in `type: condition.plugin.response_status`.
+        // @see core/modules/system/config/schema/system.schema.yml
+        'status_codes',
+      ],
+      [],
+      // Note the presence of `id`, `negate`, `uuid` and `context_mapping` here.
+      // That's because there is no `condition.plugin.*` type that specifies
+      // defaults. Each individual condition plugin has the freedom to deviate
+      // from this approach!
+      [
+        'condition.plugin.entity_bundle:*' => [
+          'id',
+          'negate',
+          'uuid',
+          'context_mapping',
+          'bundles',
+        ],
+        'condition.plugin.request_path' => [
+          'id',
+          'negate',
+          'uuid',
+          'context_mapping',
+          'pages',
+        ],
+        'condition.plugin.response_status' => [
+          'id',
+          'negate',
+          'uuid',
+          'context_mapping',
+          'status_codes',
+        ],
+        'condition.plugin.current_theme' => [
+          'id',
+          'negate',
+          'uuid',
+          'context_mapping',
+          'theme',
+        ],
+      ],
+    ];
+    // Same, but what if `type: condition.plugin.*` would have existed?
+    // @see core/modules/config/tests/config_schema_add_fallback_type_test/config/schema/config_schema_add_fallback_type_test.schema.yml
+    yield 'Dynamic type with [childkey]: block.block.positively_powered___alternate_reality_with_fallback_type___:visibility' => [
+      'block.block.positively_powered___alternate_reality_with_fallback_type___',
+      'visibility.I_CAN_CHOOSE_THIS',
+      [
+        // Keys inherited from `type: condition.plugin`.
+        // @see core/config/schema/core.data_types.schema.yml
+        'id',
+        'negate',
+        'uuid',
+        'context_mapping',
+        // Keys defined locally, in `type: condition.plugin.response_status`.
+        // @see core/modules/system/config/schema/system.schema.yml
+        'status_codes',
+      ],
+      [],
+      // Note the ABSENCE of `id`, `negate`, `uuid` and `context_mapping`
+      // compared to the previous test case, because now the
+      // `condition.plugin.*` type does exist.
+      [
+        'condition.plugin.entity_bundle:*' => [
+          'bundles',
+        ],
+        'condition.plugin.request_path' => [
+          'pages',
+        ],
+        'condition.plugin.response_status' => [
+          'status_codes',
+        ],
+        'condition.plugin.current_theme' => [
+          'theme',
+        ],
+      ],
     ];
   }
 
