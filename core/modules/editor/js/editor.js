6b72061b0f86a447dae402e75f08aea00cd677fb
Nazar Velychenko <51416-dinazaur@users.noreply.drupalcode.org>
2023-11-20 12:43:56 +0200

    Issue #3319358 by Tim Bozeman, pooja saraah, JeffM2001: Trigger event when Text Editor is attached

--- core/modules/editor/js/editor.js
+++ core/modules/editor/js/editor.js
@@ -299,7 +299,15 @@
   Drupal.editorAttach = function (field, format) {
     if (format.editor) {
       // Attach the text editor.
-      Drupal.editors[format.editor].attach(field, format);
+      Drupal.editors[format.editor]
+        .attach(field, format)
+        .then((editor) => {
+            $(document).trigger('editor:attached', [editor]);
+          })
+        .catch((error) => {
+            // eslint-disable-next-line no-console
+              console.log(`Failed to attach editor.\n${error}`);
+          });
 
       // Ensures form.js' 'formUpdated' event is triggered even for changes that
       // happen within the text editor.
