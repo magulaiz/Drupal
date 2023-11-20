c7353a1c0f8173f19d464400803cf14324970eed
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-11-20 15:14:16 +0530

    Fixed tests

--- core/misc/dialog/dialog.position.js
+++ core/misc/dialog/dialog.position.js
@@ -102,22 +102,23 @@
           }
           adjustedOptions[option] = adjustedValue;
         }
-        if (
-          option === 'width' &&
-          parseInt(optionValue, 10) >
+        if (option === 'width') {
+          if (
+            parseInt(optionValue, 10) >
             $(window).width() - (displace.offsets.left + displace.offsets.right)
-        ) {
-          if (!event.data.settings.modal) {
-            adjustedValue =
-              $(window).width() -
-              40 -
-              (displace.offsets.left + displace.offsets.right);
+          ) {
+            if (!event.data.settings.modal) {
+              adjustedValue =
+                $(window).width() -
+                40 -
+                (displace.offsets.left + displace.offsets.right);
+            } else {
+              adjustedValue = $(window).width() - 40;
+            }
+            adjustedOptions[option] = adjustedValue;
           } else {
-            adjustedValue = $(window).width() - 40;
+            adjustedOptions[option] = parseInt(optionValue, 10);
           }
-          adjustedOptions[option] = adjustedValue;
-        } else {
-          adjustedOptions[option] = parseInt(optionValue, 10);
         }
       }
     }
