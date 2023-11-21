7892a7c55092605a650f39566e684bb452e088b1
Alastair Moore <alastair.moore@tpximpact.com>
2023-11-21 22:57:48 +0000

    Fixed label display schema test

--- core/modules/block/tests/src/Functional/BlockUpdateLabelDisplaySchemaTest.php
+++ core/modules/block/tests/src/Functional/BlockUpdateLabelDisplaySchemaTest.php
@@ -27,7 +27,7 @@ class BlockUpdateLabelDisplaySchemaTest extends UpdatePathTestBase {
 
     $block_config = $this->config('block.block.olivero_account_menu');
     $label_display = $block_config->get('settings.label_display');
-    $this->assertSame('0', $label_display);
+    $this->assertSame(false, $label_display);
     $this->assertIsString($label_display);
 
     $this->runUpdates();
