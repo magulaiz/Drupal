14eef8c955cefb60d2395be99c9f355bc71d3f34
Wim Leers <wim.leers@acquia.com>
2023-11-21 16:06:50 +0100

    Restore the `::setRequired()` call, but now only for those types that opt in.

--- core/lib/Drupal/Core/Config/TypedConfigManager.php
+++ core/lib/Drupal/Core/Config/TypedConfigManager.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Core\Config;
 
+// cspell:ignore validatable
+
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Cache\CacheBackendInterface;
@@ -11,6 +13,8 @@ use Drupal\Core\DependencyInjection\ClassResolverInterface;
 use Drupal\Core\Config\Schema\Undefined;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\TypedData\TypedDataManager;
+use Drupal\Core\Validation\Plugin\Validation\Constraint\FullyValidatableConstraint;
+use Symfony\Component\Validator\Constraint;
 
 /**
  * Manages config schema type plugins.
@@ -118,6 +122,34 @@ class TypedConfigManager extends TypedDataManager implements TypedConfigManagerI
     foreach ($definition as $key => $value) {
       if (!isset($data_definition[$key])) {
         $data_definition[$key] = $value;
+      }
+    }
+
+    // All values are optional by default, except for Mapping and Sequence. This
+    // means any value can be replaced with NULL. For Sequence, NULL is only
+    // valid when `nullable: true` is set on the config schema type definition.
+    // This is not what Drupal core documentation has prescribed, nor is it
+    // intuitive.
+    // @see https://www.drupal.org/node/2264179
+    // @see https://www.drupal.org/node/1978714
+    // To gradually evolve configuration schemas in the Drupal ecosystem to be
+    // validatable, this must be clarified in a non-disruptive way. Any config
+    // schema type definition (a top-level entry in a *.schema.yml file) can opt
+    // in to the stricter behavior (a property path is not allowed to have NULL
+    // as its value unless that property path's schema has `nullable: true`) by
+    // specifying the `FullyValidatable` validation constraint as a top-level
+    // validation constraint.
+    // @see https://www.drupal.org/node/3364108
+    // @see https://www.drupal.org/node/3364109
+    // @see \Drupal\Core\TypedData\TypedDataManager::getDefaultConstraints()
+    if ($parent) {
+      $root_type_has_opted_in = array_reduce(
+        $parent->getRoot()->getConstraints(),
+        fn (bool $carry, Constraint $c): bool => $carry || $c instanceof FullyValidatableConstraint,
+        FALSE
+      );
+      if ($root_type_has_opted_in) {
+        $data_definition->setRequired(!isset($data_definition['nullable']) || $data_definition['nullable'] === FALSE);
       }
     }
 
