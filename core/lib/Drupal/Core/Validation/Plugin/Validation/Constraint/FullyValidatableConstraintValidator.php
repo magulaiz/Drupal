2cea252b04ca8ad4d0aaae081675a567cd5a1196
Wim Leers <wim.leers@acquia.com>
2023-11-22 14:41:00 +0100

    Port `FullyVAlidatableConstraint(Validator)` from #3364109.

---
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/FullyValidatableConstraintValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Core\Validation\Plugin\Validation\Constraint;
+
+// cspell:ignore validatable
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * FullyValidatable constraint.
+ *
+ * @internal
+ */
+final class FullyValidatableConstraintValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate(mixed $value, Constraint $constraint) {
+    // No-op.
+  }
+
+}
