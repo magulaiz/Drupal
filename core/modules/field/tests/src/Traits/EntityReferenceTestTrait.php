86c0867cda3002996fc674efd5d86e8e00c742d1
Alexander Varwijk <alexander@getopensocial.com>
2023-11-22 21:04:05 +0100

    Issue #3403491: Rename EntityReferenceTestTrait breaks multi-version testing

---
+++ core/modules/field/tests/src/Traits/EntityReferenceTestTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\Tests\field\Traits;
+
+/**
+ * Provides common functionality for the EntityReference test classes.
+ *
+ * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use
+ *    \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait instead.
+ *
+ * @see https://www.drupal.org/node/3401941
+ */
+trait EntityReferenceTestTrait {
+
+  use EntityReferenceFieldCreationTrait;
+
+}
