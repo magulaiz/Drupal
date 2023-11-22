d55e1b781dea8aee3d16086659a31a79f5ca2ec7
Wim Leers <wim.leers@acquia.com>
2023-11-22 10:25:18 +0100

    The change to `ValidKeysConstraintValidator` is not (yet) necessary, so omit it from this MR.

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/ValidKeysConstraintValidator.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/ValidKeysConstraintValidator.php
@@ -20,12 +20,6 @@ class ValidKeysConstraintValidator extends ConstraintValidator {
     assert($constraint instanceof ValidKeysConstraint);
 
     if (!is_array($value)) {
-      // If the value is NULL, then the `NotNull` constraint validator will
-      // set the appropriate validation error message.
-      // @see \Drupal\Core\Validation\Plugin\Validation\Constraint\NotNullConstraintValidator
-      if ($value === NULL) {
-        return;
-      }
       throw new UnexpectedTypeException($value, 'array');
     }
 
