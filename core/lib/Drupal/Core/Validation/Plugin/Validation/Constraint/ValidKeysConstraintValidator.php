ed55887e12dfced8a91a0f16b8ab4c60dfccb1c4
Wim Leers <wim.leers@acquia.com>
2023-11-23 17:08:51 +0100

    `$constraint` was being overwritten! 🙈 (Also, fix typo.)

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/ValidKeysConstraintValidator.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/ValidKeysConstraintValidator.php
@@ -105,8 +105,8 @@ class ValidKeysConstraintValidator extends ConstraintValidator {
     // @see https://www.drupal.org/node/3364108
     // @see https://www.drupal.org/node/3364109
     $root_type_has_opted_in = FALSE;
-    foreach ($this->context->getRoot()->getConstraints() as $constraint) {
-      if ($constraint instanceof FullyValidatableConstraint) {
+    foreach ($this->context->getRoot()->getConstraints() as $c) {
+      if ($c instanceof FullyValidatableConstraint) {
         $root_type_has_opted_in = TRUE;
         break;
       }
