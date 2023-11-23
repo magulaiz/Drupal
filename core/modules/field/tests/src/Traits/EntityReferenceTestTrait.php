7079e0fbf82cf49132e75f5f8e01669d5d8beeeb
Ankshetty <ankithashetty16@gmail.com>
2023-11-23 11:31:05 +0530

    Issue #3403491: Added @trigger_error to the top of the file.

--- core/modules/field/tests/src/Traits/EntityReferenceTestTrait.php
+++ core/modules/field/tests/src/Traits/EntityReferenceTestTrait.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\field\Traits;
 
+@trigger_error('\Drupal\Tests\field\Traits\EntityReferenceTestTrait is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait instead. See https://www.drupal.org/node/3401941', E_USER_DEPRECATED);
+
 /**
  * Provides common functionality for the EntityReference test classes.
  *
