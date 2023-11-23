f2d0113532596aab23fd475e44b5480149813c90
Bram Driesen <4761-BramDriesen@users.noreply.drupalcode.org>
2023-11-23 08:51:16 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/field/tests/src/Traits/EntityReferenceTestTrait.php
+++ core/modules/field/tests/src/Traits/EntityReferenceTestTrait.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\Tests\field\Traits;
 
-@trigger_error('\Drupal\Tests\field\Traits\EntityReferenceTestTrait is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait instead. See https://www.drupal.org/node/3401941', E_USER_DEPRECATED);
+@trigger_error('The ' . __NAMESPACE__ . '\EntityReferenceTestTrait is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Instead, use \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait. See https://www.drupal.org/node/3401941', E_USER_DEPRECATED);
 
 /**
  * Provides common functionality for the EntityReference test classes.
