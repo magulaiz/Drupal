27b47f8d9c9477bc86530b7bc509881551e0ce3a
sascha <sascha.meissner@werk21.de>
2023-11-23 13:10:59 +0100

    Issue #3403706: After manual resize debounce will cause "choose a block" to not position correctly

--- core/misc/dialog/off-canvas/js/off-canvas.js
+++ core/misc/dialog/off-canvas/js/off-canvas.js
@@ -143,7 +143,7 @@
         .on(
           'resize.off-canvas',
           eventData,
-          debounce(Drupal.offCanvas.resetSize, 100, true),
+          Drupal.offCanvas.resetSize,
         )
         .trigger('resize.off-canvas');
     },
