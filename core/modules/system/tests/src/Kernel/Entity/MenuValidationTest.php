1d7f8516dd2474a6b92dbf7631b739d688230a7e
Wim Leers <wim.leers@acquia.com>
2023-11-24 09:55:37 +0100

    To match `MenuForm`: mark `system.menu.*:description` as nullable and limit it to 512 characters.

--- core/modules/system/tests/src/Kernel/Entity/MenuValidationTest.php
+++ core/modules/system/tests/src/Kernel/Entity/MenuValidationTest.php
@@ -48,4 +48,24 @@ class MenuValidationTest extends ConfigEntityValidationTestBase {
     return $cases;
   }
 
+  /**
+   * Tests that description is optional, and limited to 512 characters.
+   *
+   * phpcs:disable Drupal.Commenting
+   * cspell:disable
+   *
+   * @testWith [null, {}]
+   *           ["", {}]
+   *           ["This is an ASCII description.", {}]
+   *           ["This is an emoji in a description: 🕺.", []]
+   *           ["Iste et sunt ut cum. Suscipit officia molestias amet provident et sunt sit. Tenetur doloribus odit sapiente doloremque sequi id dignissimos. In rerum nihil voluptatibus architecto laborum. Repellendus eligendi laborum id nesciunt alias incidunt non. Tenetur deserunt facere voluptas nisi id. Aut ab eaque eligendi. Nihil quasi illum sit provident voluptatem repellat temporibus autem. Mollitia quisquam error facilis quasi voluptate. Dignissimos quis culpa nobis veritatis ut vel laudantium cumque. Rerum mollitia deleniti possimus placeat rerum. Reiciendis distinctio soluta voluptatem.", {"description": "This value is too long. It should have <em class=\"placeholder\">512</em> characters or less."}]
+   *
+   * cspell:enable
+   * phpcs:enable Drupal.Commenting
+   */
+  public function testDescription(?string $description, array $expected_errors): void {
+    $this->entity->set('description', $description);
+    $this->assertValidationErrors($expected_errors);
+  }
+
 }
