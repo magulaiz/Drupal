0ae9d80e421014a62158c0e36a0c1379fa424c61
Fran Garcia-Linares <fjgarlin@gmail.com>
2023-11-24 12:28:33 +0100

    Longer waits.

--- core/modules/field_ui/tests/src/FunctionalJavascript/ManageFieldsTest.php
+++ core/modules/field_ui/tests/src/FunctionalJavascript/ManageFieldsTest.php
@@ -344,8 +344,8 @@ class ManageFieldsTest extends WebDriverTestBase {
     $page = $this->getSession()->getPage();
     $page->findField('edit-field-storage-subform-cardinality-number')->setValue('-11');
     $page->findButton('Save settings')->click();
-    sleep(5);
     $this->assertSession()->assertWaitOnAjaxRequest();
+    sleep(10);
     $this->assertSession()->pageTextContains('Limit must be higher than or equal to 1.');
   }
 
