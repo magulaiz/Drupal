cf5b1ce08a9d2f4dde09210eef0195f4c21179b7
Nitin Shrivastava <61485-Nitin_shrivastava@users.noreply.drupalcode.org>
2023-11-24 05:52:14 +0000

    Update EntityFormDisplayTest.php

--- core/modules/jsonapi/tests/src/Functional/EntityFormDisplayTest.php
+++ core/modules/jsonapi/tests/src/Functional/EntityFormDisplayTest.php
@@ -149,6 +149,7 @@ class EntityFormDisplayTest extends ConfigEntityResourceTestBase {
                 'match_limit' => 10,
                 'size' => 60,
                 'placeholder' => '',
+                'show_id' => TRUE,
               ],
               'region' => 'content',
               'third_party_settings' => [],
