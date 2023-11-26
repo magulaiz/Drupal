5eb9de8caa2f024ace39036a9a48eb9d5e7ea854
Lap Pham <phthlaap@gmail.com>
2023-11-26 08:17:51 +0700

    Move the changes from patch 3127116-31.patch to a Git merge request, add comments to the tests, and incorporate any modifications suggested by the reviewer.

--- core/modules/image/tests/src/Functional/ImageAdminStylesTest.php
+++ core/modules/image/tests/src/Functional/ImageAdminStylesTest.php
@@ -529,4 +529,38 @@ class ImageAdminStylesTest extends ImageFieldTestBase {
     $this->assertSession()->pageTextContains("Select a new effect");
   }
 
+  /**
+   * Test preview image show with private scheme.
+   */
+  public function testPreviewImageShowInPrivateScheme() {
+    $this->config('system.file')->set('default_scheme', 'private')->save();
+
+    /** @var \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator */
+    $file_url_generator = \Drupal::service('file_url_generator');
+
+    // Get the original preview image file in core config.
+    $original_path = $this->config('image.settings')->get('preview_image');
+    $style = ImageStyle::create(['name' => 'test_foo', 'label' => 'test foo']);
+    $style->save();
+
+    // Build the derivative preview image file with the Image Style.
+    // @see core/modules/image/image.admin.inc:55
+    $preview_file = $style->buildUri($original_path);
+    $style->createDerivative($original_path, $preview_file);
+
+    // Check if derivative image exists.
+    $this->assertFileExists($preview_file);
+
+    // Generate itok token for the preview image.
+    $itok = $style->getPathToken('private://' . $original_path);
+
+    $url = $file_url_generator->generateAbsoluteString($preview_file);
+    $url .= '?itok=' . $itok;
+
+    // Check if the preview image with style is shown.
+    $this->drupalGet($url);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->responseHeaderContains('Content-Type', 'image/png');
+  }
+
 }
