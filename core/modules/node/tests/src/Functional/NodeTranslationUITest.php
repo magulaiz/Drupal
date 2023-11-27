8b6f4ecfe107d750a052dfd4e8392ac24be2f627
Marco Villegas <git@marvil07.net>
2023-11-27 18:07:59 -0500

    Use language test trait on NodeTranslationUITest

--- core/modules/node/tests/src/Functional/NodeTranslationUITest.php
+++ core/modules/node/tests/src/Functional/NodeTranslationUITest.php
@@ -3,11 +3,12 @@
 namespace Drupal\Tests\node\Functional;
 
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Tests\content_translation\Functional\ContentTranslationUITestBase;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Url;
-use Drupal\node\Entity\Node;
+use Drupal\Tests\content_translation\Functional\ContentTranslationUITestBase;
+use Drupal\Tests\language\Traits\LanguageTestTrait;
 use Drupal\language\Entity\ConfigurableLanguage;
+use Drupal\node\Entity\Node;
 
 /**
  * Tests the Node Translation UI.
@@ -16,6 +17,8 @@ use Drupal\language\Entity\ConfigurableLanguage;
  * @group #slow
  */
 class NodeTranslationUITest extends ContentTranslationUITestBase {
+
+  use LanguageTestTrait;
 
   /**
    * {@inheritdoc}
@@ -564,9 +567,7 @@ class NodeTranslationUITest extends ContentTranslationUITestBase {
     $this->submitForm($edit, 'Save');
 
     // Make the image field non-translatable.
-    $edit = ['settings[node][article][fields][field_image]' => FALSE];
-    $this->drupalGet('admin/config/regional/content-language');
-    $this->submitForm($edit, 'Save configuration');
+    self::setFieldTranslatable('node', 'article', 'field_image', FALSE);
 
     // Create a node.
     $nid = $this->createEntity(['title' => 'Node with multi-value image field en title'], 'en');
