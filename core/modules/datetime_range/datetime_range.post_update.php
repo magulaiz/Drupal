111f3db84763f44e1083e292f026d357383ad9c7
Kunal Sachdev <kunal.sachdev@acquia.com>
2023-11-29 18:57:22 +0530

    add hook_ENTITY_TYPE_presave() and change post_update

--- core/modules/datetime_range/datetime_range.post_update.php
+++ core/modules/datetime_range/datetime_range.post_update.php
@@ -5,7 +5,9 @@
  * Post-update functions for Datetime Range module.
  */
 
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\datetime_range\Plugin\Field\FieldType\DateRangeItem;
+use Drupal\field\FieldConfigInterface;
 
 /**
  * Implements hook_removed_post_updates().
@@ -20,21 +22,18 @@ function datetime_range_removed_post_updates() {
 /**
  * Adds optional_values config in daterange field settings.
  */
-function datetime_range_post_update_add_optional_values() {
-  $config_factory = \Drupal::configFactory();
-  foreach ($config_factory->listAll('field.field.') as $field_settings) {
-    $field_config = $config_factory->getEditable($field_settings);
-    if ($field_config->get('field_type') != 'daterange') {
-      continue;
+function datetime_range_post_update_add_optional_values(&$sandbox = NULL) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'field_config', function (FieldConfigInterface $fieldConfig) {
+    if ($fieldConfig->get('field_type') != 'daterange') {
+      return FALSE;
     }
-    $settings = $field_config->get('settings');
-    if (!isset($settings['optional_values'])) {
-      $settings['optional_values'] = DateRangeItem::OPTIONAL_NONE;
-      $field_config->set('settings', $settings);
-
-      // Mark the resulting configuration as trusted data. This avoids issues with
-      // future schema changes.
-      $field_config->save(TRUE);
+    $settings = $fieldConfig->get('settings');
+    if (!isset($settings['optional_alues'])) {
+      $settings['optional_alues'] = DateRangeItem::OPTIONAL_NONE;
+      $fieldConfig->set('settings', $settings);
+      $fieldConfig->save();
+      return TRUE;
     }
-  }
+    return FALSE;
+  });
 }
