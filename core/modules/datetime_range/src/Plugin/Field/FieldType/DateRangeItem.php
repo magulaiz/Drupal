5a8ad1613fe3a45b25214cb8a9f7845888cb8789
Ankshetty <ankithashetty16@gmail.com>
2023-11-29 12:41:58 +0530

    Issue #3401464: Updated description under annotation for daterange fieldtype.

--- core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
+++ core/modules/datetime_range/src/Plugin/Field/FieldType/DateRangeItem.php
@@ -18,10 +18,10 @@ use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
  *   label = @Translation("Date range"),
  *   category = "date_time",
  *   description = {
- *     @Translation("Users can specify a start date and an end date for the content, defining the duration of the event or period."),
- *     @Translation("With the date range field, users can easily search and filter content based on specific date criteria."),
+ *     @Translation("Ideal for storing durations that consist of start and end dates (and times)."),
+ *     @Translation("Choose between setting both date and time, or date only, for each duration."),
  *     @Translation("Field options: </br> <ol><li>Date and Time: Stores date and time component.(E.g Thu, 11/02/2023 - 09:00 - Thu, 11/16/2023 - 17:00).</li><li>Date - Stores date only and time is set 12:00 hrs as default. (E.g Thu, 11/02/2023 - 12:00 - Thu, 11/16/2023 - 12:00).</li><li>All Day -  Stores date and time, starting with 00:00 hrs of start date and 23:59 hrs of end date  (Thu, 11/02/2023 - 00:00 - Thu, 11/16/2023 - 23:59).</li></ol>"),
- *     @Translation("Basic validation that date range provides: </br> <ol><li>If the one date has value then other date cannot be NULL.</li><li>The end date will always be greater than start date.</li></ol>"),
+ *     @Translation("The system automatically validates that the end date (and time) is later than the start, and both fields are completed."),
  *   },
  *   default_widget = "daterange_default",
  *   default_formatter = "daterange_default",
