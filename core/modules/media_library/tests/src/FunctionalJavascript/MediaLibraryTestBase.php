ae705b9567b7211b9ba484ce2523b6a52cbad420
Fran Garcia-Linares <fjgarlin@gmail.com>
2023-11-30 18:28:04 +0100

    Wrong selector.

--- core/modules/media_library/tests/src/FunctionalJavascript/MediaLibraryTestBase.php
+++ core/modules/media_library/tests/src/FunctionalJavascript/MediaLibraryTestBase.php
@@ -279,7 +279,7 @@ abstract class MediaLibraryTestBase extends WebDriverTestBase {
 
     if (empty($this->assertSession()->waitForText('Add or select media'))) {
       // Maybe there were more buttons, try another selector.
-      $this->getSession()->getPage()->pressButton("{$field_name}_-media-library-open-button");
+      $this->getSession()->getPage()->pressButton("{$field_name}-media-library-open-button");
       if (empty($this->assertSession()->waitForText('Add or select media'))) {
         var_dump(htmlentities($this->getSession()->getPage()->getHtml()));
       }
