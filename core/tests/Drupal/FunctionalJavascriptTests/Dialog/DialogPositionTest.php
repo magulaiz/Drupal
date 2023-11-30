60e69264ebd8af41c2d50641516101f53b642c84
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-11-30 09:12:44 +0530

    Added test coverage

--- core/tests/Drupal/FunctionalJavascriptTests/Dialog/DialogPositionTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Dialog/DialogPositionTest.php
@@ -91,15 +91,22 @@ class DialogPositionTest extends WebDriverTestBase {
     $this->assertSession()->assertWaitOnAjaxRequest();
     $script = <<<SCRIPT
       (function() {
-        return document.querySelector('.modal-dialog').style.width;
+        return document.querySelector('.modal-dialog').clientWidth;
       }())
       SCRIPT;
 
     // Resize the window.
     $width_before = $this->getSession()->getDriver()->evaluateScript($script);
-    $this->getSession()->resizeWindow(785, 805);
+    $this->assertEquals('880', $width_before);
+    $this->getSession()->resizeWindow(500, 805);
     $width_after = $this->getSession()->getDriver()->evaluateScript($script);
-    $this->assertEquals($width_before, $width_after);
+    $this->assertEquals('460', $width_after);
+
+    // Resize the window.
+    $this->getSession()->resizeWindow(1300, 1300);
+    $width_after_resize = $this->getSession()->getDriver()->evaluateScript($script);
+    // Assert that the width is restored to full size.
+    $this->assertStringContainsString('880', $width_after_resize);
   }
 
 }
