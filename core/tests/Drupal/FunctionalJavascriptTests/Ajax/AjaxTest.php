3cf04b778b80baf494821922538f5ad75a075321
Fran Garcia-Linares <14157-fjgarlin@users.noreply.drupalcode.org>
2023-11-30 10:36:46 +0000

    Remove portion of test.

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxTest.php
@@ -324,19 +324,6 @@ JS;
     $has_focus_id = $this->getSession()->evaluateScript('document.activeElement.id');
     $this->assertEquals('edit-textfield-2', $has_focus_id);
 
-    // Test textfield with 'change' event listener with refocus-blur set to
-    // FALSE.
-    $textfield2->setValue('Llamas say yarhar');
-    // The above does a blur at the end, so the focus is gone already.
-    // Note that if the ajax call finishes before the "evaluateScript" we could
-    // still get a random fail.
-    $has_focus_id = $this->getSession()->evaluateScript('document.activeElement.id');
-    $this->assertNotEquals('edit-textfield-2', $has_focus_id);
-    $this->assertSession()->assertWaitOnAjaxRequest();
-    // The focus is now back after the request.
-    $has_focus_id = $this->getSession()->evaluateScript('document.activeElement.id');
-    $this->assertEquals('edit-textfield-2', $has_focus_id);
-
     // Test textfield with 'change' event.
     $textfield3->focus();
     $textfield3->setValue('Wasps buzz');
