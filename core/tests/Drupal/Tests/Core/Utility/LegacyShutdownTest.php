32c850fc93c4e3f50b5480a331c23cbb6f313b36
voleger <44159-voleger@users.noreply.drupalcode.org>
2023-12-02 05:40:03 +0200

    Fix PHPCS issue

--- core/tests/Drupal/Tests/Core/Utility/LegacyShutdownTest.php
+++ core/tests/Drupal/Tests/Core/Utility/LegacyShutdownTest.php
@@ -19,13 +19,13 @@ class LegacyShutdownTest extends UnitTestCase {
    * Test legacy drupal_register_shutdown_function() function.
    */
   public function testDrupalRegisterShutdownFunction() {
-    $a = static function(int $b = 0): int {
+    $a = static function (int $b = 0): int {
       static $a;
       return $a += $b;
     };
     $add = 10;
     $count = 0;
-    $this->expectDeprecation('drupal_register_shutdown_function() is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Shutdown\ShutdownHandler::get() or \Drupal\Core\Shutdown\ShutdownHandler::add() or \Drupal\Core\Shutdown\ShutdownHandler::reset() instead. See https://www.drupal.org/node/3053808');
+    $this->expectDeprecation('drupal_register_shutdown_function() is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Shutdown\ShutdownHandler::get() or \Drupal\Core\Shutdown\ShutdownHandler::add() or \Drupal\Core\Shutdown\ShutdownHandler::reset() instead. See https://www.drupal.org/node/3053808');
     $this->assertCount(++$count, drupal_register_shutdown_function($a, $add));
     $this->assertCount(++$count, $callbacks = &drupal_register_shutdown_function($a, $add));
     foreach ($callbacks as $callback_array) {
