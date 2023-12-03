388a340fcf429512e7b83ac95e74a96812297e04
Patrick Kenny <33362-ptmkenny@users.noreply.drupalcode.org>
2023-12-03 18:37:31 +0900

    add draft change record

--- core/modules/user/src/Plugin/rest/resource/UserRegistrationResource.php
+++ core/modules/user/src/Plugin/rest/resource/UserRegistrationResource.php
@@ -76,7 +76,7 @@ class UserRegistrationResource extends ResourceBase {
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, ImmutableConfig $user_settings, AccountInterface $current_user, PasswordGeneratorInterface $password_generator = NULL) {
     if (is_null($password_generator)) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $password_generator argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0.', E_USER_DEPRECATED);
+      @trigger_error('Calling ' . __METHOD__ . '() without the $password_generator argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3405799.', E_USER_DEPRECATED);
       $password_generator = \Drupal::service('password_generator');
     }
 
