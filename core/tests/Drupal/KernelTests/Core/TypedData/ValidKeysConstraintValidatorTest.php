f64086af7ee27d446313df01bb82cd1747ce40bd
Wim Leers <wim.leers@acquia.com>
2023-12-06 13:24:43 +0100

    Update `ValidKeys` test expectations.

--- core/tests/Drupal/KernelTests/Core/TypedData/ValidKeysConstraintValidatorTest.php
+++ core/tests/Drupal/KernelTests/Core/TypedData/ValidKeysConstraintValidatorTest.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\KernelTests\Core\TypedData;
 
-use Drupal\Core\TypedData\DataDefinition;
+// cspell:ignore validatable
+
+use Drupal\block\Entity\Block;
+use Drupal\Core\TypedData\MapDataDefinition;
 use Drupal\KernelTests\KernelTestBase;
+use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 /**
@@ -17,19 +21,142 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 class ValidKeysConstraintValidatorTest extends KernelTestBase {
 
   /**
+   * The config under test.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    // Install the Block module and create a Block config entity, so that we can
+    // test that the validator infers the required keys from a defined schema.
+    $this->enableModules(['system', 'block', 'config_schema_test']);
+    // Also install the block_test_fully_validatable module, to enable using
+    // Block config entities as the example in the test cases below. The opt-in
+    // aspect is already tested in
+    // @see \Drupal\KernelTests\Config\Schema\MappingTest::testMappingInterpretations()
+    \Drupal::state()->set('config_schema_test_block_fully_validatable', TRUE);
+    /** @var \Drupal\Core\Extension\ThemeInstallerInterface $theme_installer */
+    $theme_installer = $this->container->get('theme_installer');
+    $theme_installer->install(['stark']);
+
+    $block = Block::create([
+      'id' => 'branding',
+      'plugin' => 'system_branding_block',
+      'theme' => 'stark',
+      'status' => TRUE,
+      'settings' => [
+        'use_site_logo' => TRUE,
+        'use_site_name' => TRUE,
+        'use_site_slogan' => TRUE,
+        'label_display' => FALSE,
+        // TRICKY: these 4 are inherited from `type: block_settings`.
+        'status' => TRUE,
+        'info' => '',
+        'view_mode' => 'full',
+        'context_mapping' => [],
+      ],
+    ]);
+    $block->save();
+
+    $this->config = $this->container->get('config.typed')
+      ->get('block.block.branding');
+  }
+
+  /**
+   * Tests ValidKeys constraint validator detecting unsupported keys.
+   *
+   * @see \Drupal\Core\Validation\Plugin\Validation\Constraint\ValidKeysConstraint::$invalidKeyMessage
+   */
+  public function testSupportedKeys(): void {
+    // Start from the valid config.
+    $this->assertEmpty($this->config->validate());
+
+    // Then modify only one thing: generate a non-existent `label_display`
+    // setting.
+    $data = $this->config->toArray();
+    $data['settings']['foobar'] = TRUE;
+    $this->config = $this->container->get('config.typed')
+      ->createFromNameAndData('block.block.branding', $data);
+
+    // Now 1 validation error should be triggered: one for the unsupported key.
+    // @see \Drupal\system\Plugin\Block\SystemBrandingBlock::defaultConfiguration()
+    // @see \Drupal\system\Plugin\Block\SystemPoweredByBlock::defaultConfiguration()
+    $this->assertSame(
+      [
+        "'foobar' is not a supported key.",
+      ],
+      array_map(
+        fn (ConstraintViolation $v) => (string) $v->getMessage(),
+        iterator_to_array($this->config->validate()),
+      )
+    );
+  }
+
+  /**
+   * Tests ValidKeys constraint validator detecting unknown keys.
+   *
+   * @see \Drupal\Core\Validation\Plugin\Validation\Constraint\ValidKeysConstraint::$dynamicInvalidKeyMessage
+   */
+  public function testUnknownKeys(): void {
+    // Start from the valid config.
+    $this->assertEmpty($this->config->validate());
+
+    // Then modify only one thing: the block plugin that is being used.
+    $data = $this->config->toArray();
+    $data['plugin'] = 'system_powered_by_block';
+    $this->config = $this->container->get('config.typed')
+      ->createFromNameAndData('block.block.branding', $data);
+
+    // Now 3 validation errors should be triggered: one for each of the settings
+    // that exist in the "branding" block but not the "powered by" block.
+    // @see \Drupal\system\Plugin\Block\SystemBrandingBlock::defaultConfiguration()
+    // @see \Drupal\system\Plugin\Block\SystemPoweredByBlock::defaultConfiguration()
+    $this->assertSame(
+      [
+        "'use_site_logo' is an unknown key because plugin is system_powered_by_block (see config schema type block.settings.*).",
+        "'use_site_name' is an unknown key because plugin is system_powered_by_block (see config schema type block.settings.*).",
+        "'use_site_slogan' is an unknown key because plugin is system_powered_by_block (see config schema type block.settings.*).",
+      ],
+      array_map(
+        fn (ConstraintViolation $v) => (string) $v->getMessage(),
+        iterator_to_array($this->config->validate()),
+      )
+    );
+  }
+
+  /**
    * Tests the ValidKeys constraint validator.
    */
   public function testValidation(): void {
     // Create a data definition that specifies certain allowed keys.
-    $definition = DataDefinition::create('any')
+    $definition = MapDataDefinition::create('mapping')
       ->addConstraint('ValidKeys', ['north', 'south', 'west']);
+    $definition['mapping'] = [
+      'north' => ['type' => 'string', 'requiredKey' => FALSE],
+      'east' => ['type' => 'string', 'requiredKey' => FALSE],
+      'south' => ['type' => 'string', 'requiredKey' => FALSE],
+      'west' => ['type' => 'string', 'requiredKey' => FALSE],
+    ];
+    // @todo Remove this line in https://www.drupal.org/project/drupal/issues/3403782
+    $definition->setClass('Drupal\Core\Config\Schema\Mapping');
 
-    /** @var \Drupal\Core\TypedData\TypedDataManagerInterface $typed_data */
-    $typed_data = $this->container->get('typed_data_manager');
+    /** @var \Drupal\Core\TypedData\TypedDataManagerInterface $typed_config */
+    $typed_config = $this->container->get('config.typed');
+    // @see \Drupal\Core\Config\TypedConfigManager::buildDataDefinition()
+    // @see \Drupal\Core\TypedData\TypedDataManager::createDataDefinition()
+    $definition->setTypedDataManager($typed_config);
 
     // Passing a non-array value should raise an exception.
     try {
-      $typed_data->create($definition, 2501)->validate();
+      // TRICKY: we must clone the definition because the instance is modified
+      // when processing.
+      // @see \Drupal\Core\Config\Schema\Mapping::processRequiredKeyFlags()
+      $typed_config->create(clone $definition, 2501)->validate();
       $this->fail('Expected an exception but none was raised.');
     }
     catch (UnexpectedTypeException $e) {
@@ -37,21 +164,21 @@ class ValidKeysConstraintValidatorTest extends KernelTestBase {
     }
 
     // Empty arrays are valid.
-    $this->assertCount(0, $typed_data->create($definition, [])->validate());
+    $this->assertCount(0, $typed_config->create(clone $definition, [])->validate());
 
     // Indexed arrays are never valid.
-    $violations = $typed_data->create($definition, ['north', 'south'])->validate();
+    $violations = $typed_config->create(clone $definition, ['north', 'south'])->validate();
     $this->assertCount(1, $violations);
     $this->assertSame('Numerically indexed arrays are not allowed.', (string) $violations->get(0)->getMessage());
 
     // Arrays with automatically assigned keys, AND a valid key, should be
     // considered invalid overall.
-    $violations = $typed_data->create($definition, ['north', 'south' => 'west'])->validate();
+    $violations = $typed_config->create(clone $definition, ['north', 'south' => 'west'])->validate();
     $this->assertCount(1, $violations);
     $this->assertSame("'0' is not a supported key.", (string) $violations->get(0)->getMessage());
 
     // Associative arrays with an invalid key should be invalid.
-    $violations = $typed_data->create($definition, ['north' => 'south', 'east' => 'west'])->validate();
+    $violations = $typed_config->create(clone $definition, ['north' => 'south', 'east' => 'west'])->validate();
     $this->assertCount(1, $violations);
     $this->assertSame("'east' is not a supported key.", (string) $violations->get(0)->getMessage());
 
@@ -61,7 +188,16 @@ class ValidKeysConstraintValidatorTest extends KernelTestBase {
       'south' => 'Atlanta',
       'west' => 'San Francisco',
     ];
-    $violations = $typed_data->create($definition, $value)->validate();
+    $violations = $typed_config->create(clone $definition, $value)->validate();
+    $this->assertCount(0, $violations);
+
+    // If in the mapping definition some keys that do NOT have
+    // `requiredKey: false` set, then they MUST be set.
+    // First test without changing the value: no error should occur because all
+    // keys passed to the ValidKeys constraint have a value.
+    unset($definition['mapping']['south']['requiredKey']);
+    unset($definition['mapping']['east']['requiredKey']);
+    $violations = $typed_config->create(clone $definition, $value)->validate();
     $this->assertCount(0, $violations);
   }
 
