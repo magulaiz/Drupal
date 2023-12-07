80bb43eb8e14cd21a6ab4a2f2c143426d796d718
catch <catch@35733.no-reply.drupal.org>
2023-12-07 11:40:36 +0000

    More tweaks.

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -80,7 +80,8 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertLessThanOrEqual(39, $performance_data->getQueryCount());
     $this->assertGreaterThanOrEqual(87, $performance_data->getCacheGetCount());
     $this->assertLessThanOrEqual(88, $performance_data->getCacheGetCount());
-    $this->assertSame(20, $performance_data->getCacheSetCount());
+    $this->assertGreaterThanOrEqual(20, $performance_data->getCacheGetCount());
+    $this->assertLessThanOrEqual(28, $performance_data->getCacheGetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
 
     // Test user profile page.
@@ -132,7 +133,6 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertSame(28, $performance_data->getCacheGetCount());
     $this->assertLessThanOrEqual(2, $performance_data->getCacheSetCount());
     $this->assertGreaterThanOrEqual(1, $performance_data->getCacheSetCount());
-    $this->assertSame(1, $performance_data->getCacheSetCount());
     $this->assertSame(1, $performance_data->getCacheDeleteCount());
   }
 
