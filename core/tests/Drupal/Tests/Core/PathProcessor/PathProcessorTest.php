d912bb8c510a6ed456b854a930e0c7bf87dd4bfd
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2023-12-07 21:58:42 +0000

    Issue #2494703: Deprecate dependency setters and getters on LanguageNegotiatorInterface

--- core/tests/Drupal/Tests/Core/PathProcessor/PathProcessorTest.php
+++ core/tests/Drupal/Tests/Core/PathProcessor/PathProcessorTest.php
@@ -130,10 +130,6 @@ class PathProcessorTest extends UnitTestCase {
       ->method('getNegotiationMethodInstance')
       ->willReturn($method);
 
-    // Create a user stub.
-    $current_user = $this->getMockBuilder('Drupal\Core\Session\AccountInterface')
-      ->getMock();
-
     // Create a config event subscriber stub.
     $config_subscriber = $this->getMockBuilder('Drupal\language\EventSubscriber\ConfigSubscriber')
       ->disableOriginalConstructor()
@@ -143,7 +139,7 @@ class PathProcessorTest extends UnitTestCase {
     $alias_processor = new AliasPathProcessor($alias_manager);
     $decode_processor = new PathProcessorDecode();
     $front_processor = new PathProcessorFront($config_factory_stub);
-    $language_processor = new PathProcessorLanguage($config_factory_stub, $this->languageManager, $negotiator, $current_user, $config_subscriber);
+    $language_processor = new PathProcessorLanguage($config_factory_stub, $this->languageManager, $negotiator, $config_subscriber);
 
     // First, test the processor manager with the processors in the incorrect
     // order. The alias processor will run before the language processor, meaning
