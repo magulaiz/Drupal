577d4787c7a8c17cd6ff330026cd768a49bb4938
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2023-12-07 17:37:09 +0000

    Issue #2494703: Deprecate dependency setters and getters on LanguageNegotiatorInterface

--- core/modules/language/src/LanguageNegotiatorInterface.php
+++ core/modules/language/src/LanguageNegotiatorInterface.php
@@ -115,6 +115,8 @@ interface LanguageNegotiatorInterface {
    *   The current active user.
    *
    * @deprecated Deprecated in 10.3.0 and is removed from 11.0.0. 
+   * Inject the current user through a constructor instead.
+   * 
    * @see https://www.drupal.org/project/drupal/issues/2494703
    */
   public function setCurrentUser(AccountInterface $current_user);
