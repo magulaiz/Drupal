099cd1307983af91b04238d0b1572489c336c3ff
Alex Pott <alex.a.pott@googlemail.com>
2023-12-10 18:37:34 +0000

    Add a test

--- core/tests/Drupal/KernelTests/Core/Config/ConfigImporterTest.php
+++ core/tests/Drupal/KernelTests/Core/Config/ConfigImporterTest.php
@@ -978,7 +978,8 @@ class ConfigImporterTest extends KernelTestBase {
   /**
    * Tests events and collections during a config import.
    */
-  public function testEventsAndCollectionsImport(): void {
+  public function testEventsAndCollectionsImport(): void
+  {
     $collections = [
       'another_collection',
       'collection.test1',
@@ -1011,6 +1012,41 @@ class ConfigImporterTest extends KernelTestBase {
       $event_collections[] = $event['current_config_data']['collection'];
     }
     $this->assertSame($collections, $event_collections);
+  }
+
+  /**
+   * Tests the target storage caching during configuration import.
+   */
+  public function testStorageComparerTargetStorage(): void {
+    $this->installConfig(['config_events_test']);
+    $this->copyConfig($this->container->get('config.storage'), $this->container->get('config.storage.sync'));
+    $this->assertTrue($this->container->get('module_installer')->uninstall(['config_test']));
+    \Drupal::state()->set('config_events_test.all_events', []);
+    \Drupal::state()->set('config_test_install.foo_value', 'transient');
+
+    // Prime the active config cache. If the ConfigImporter and StorageComparer
+    // do not manager the target storage correctly this cache can pollute the
+    // data.
+    \Drupal::configFactory()->get('config_test.system');
+
+    // Import the configuration. This results in a save event with the value
+    // changing from foo to bar.
+    $this->configImporter()->import();
+    $all_events = \Drupal::state()->get('config_events_test.all_events');
+
+    // Test that the values change as expected during the configuration import.
+    $this->assertCount(3, $all_events[ConfigEvents::SAVE]['config_test.system']);
+    // First, the values are set by the module installer using the configuration
+    // import's source storage.
+    $this->assertSame([], $all_events[ConfigEvents::SAVE]['config_test.system'][0]['original_config_data']);
+    $this->assertSame('bar', $all_events[ConfigEvents::SAVE]['config_test.system'][0]['current_config_data']['foo']);
+    // Next, the config_test_install() function changes the value.
+    $this->assertSame('bar', $all_events[ConfigEvents::SAVE]['config_test.system'][1]['original_config_data']['foo']);
+    $this->assertSame('transient', $all_events[ConfigEvents::SAVE]['config_test.system'][1]['current_config_data']['foo']);
+    // Last, the config importer processes all the configuration in the source
+    // storage and ensures the values are as expected.
+    $this->assertSame('transient', $all_events[ConfigEvents::SAVE]['config_test.system'][2]['original_config_data']['foo']);
+    $this->assertSame('bar', $all_events[ConfigEvents::SAVE]['config_test.system'][2]['current_config_data']['foo']);
   }
 
   /**
