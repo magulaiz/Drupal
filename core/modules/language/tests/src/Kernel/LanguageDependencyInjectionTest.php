45e5d3f05e2603ddaba54dfe1d89f3fd0ce63400
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2023-12-10 21:03:41 +0000

    Issue #2494703: Deprecate dependency setters and getters on LanguageNegotiatorInterface

--- core/modules/language/tests/src/Kernel/LanguageDependencyInjectionTest.php
+++ core/modules/language/tests/src/Kernel/LanguageDependencyInjectionTest.php
@@ -34,6 +34,7 @@ class LanguageDependencyInjectionTest extends LanguageTestBase {
     $fr = ConfigurableLanguage::createFromLangcode('fr');
     $fr->save();
     $this->config('system.site')->set('default_langcode', 'fr')->save();
+    \Drupal::languageManager()->reset();
 
     // The language system creates a Language object which contains the
     // same properties as the new default language object.
