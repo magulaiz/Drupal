2863ef544b94dc0ae0e0422ede521211c1d264d6
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-12-11 15:45:04 +0530

    Fixed theme related issue with tests

--- core/modules/menu_ui/tests/src/FunctionalJavascript/MenuUiJavascriptTest.php
+++ core/modules/menu_ui/tests/src/FunctionalJavascript/MenuUiJavascriptTest.php
@@ -142,8 +142,10 @@ class MenuUiJavascriptTest extends WebDriverTestBase {
       'menu_parent' => $menu_id . ':' . $parent,
       'weight[0][value]' => $weight,
     ];
-    $this->getSession()->getPage()->find('css', '[data-drupal-selector="edit-menu-link-description"]')->pressButton('Description');
-    $this->getSession()->getPage()->find('css', '[data-drupal-selector="edit-menu-link-display-settings"]')->pressButton('Display settings');
+    if($this->config('system.theme')->get('default') !== 'stark') {
+      $this->getSession()->getPage()->find('css', '[data-drupal-selector="edit-menu-link-description"]')->pressButton('Description');
+      $this->getSession()->getPage()->find('css', '[data-drupal-selector="edit-menu-link-display-settings"]')->pressButton('Display settings');
+    }
 
     // Add menu link.
     $this->submitForm($edit, 'Save');
