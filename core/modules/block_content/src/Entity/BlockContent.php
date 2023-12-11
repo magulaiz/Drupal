4d77ea555bfaae2ad1c9848510692ea9e2bafcd6
Stephen Mustgrave <smustgrave@gmail.com>
2023-12-11 16:49:54 -0500

    Fix bug with disappearing tasks

--- core/modules/block_content/src/Entity/BlockContent.php
+++ core/modules/block_content/src/Entity/BlockContent.php
@@ -50,7 +50,7 @@ use Drupal\block_content\BlockContentInterface;
  *   revision_data_table = "block_content_field_revision",
  *   show_revision_ui = TRUE,
  *   links = {
- *     "canonical" = "/admin/content/block/{block_content}",
+ *     "canonical" = "/admin/content/block/{block_content}/edit",
  *     "delete-form" = "/admin/content/block/{block_content}/delete",
  *     "edit-form" = "/admin/content/block/{block_content}/edit",
  *     "collection" = "/admin/content/block",
