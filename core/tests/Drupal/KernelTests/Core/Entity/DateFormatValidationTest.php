c0f0a2005a980fdd2f45ff709293efd967762fd0
Joris Vercammen <botchcake@gmail.com>
2023-12-12 19:57:37 +0100

    Move test coverage

--- core/tests/Drupal/KernelTests/Core/Entity/DateFormatValidationTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/DateFormatValidationTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\KernelTests\Core\Entity;
 
+use Drupal\Core\Config\Schema\SchemaIncompleteException;
 use Drupal\Core\Datetime\Entity\DateFormat;
 use Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase;
 
@@ -25,6 +26,50 @@ class DateFormatValidationTest extends ConfigEntityValidationTestBase {
       'pattern' => 'Y-m-d',
     ]);
     $this->entity->save();
+  }
+
+  /**
+   * @dataProvider provideLocked
+   */
+  public function testDateFormatNotNull($locked) {
+    $entity_values['id'] = $entity_values['label'] = $this->randomMachineName();
+    $entity_values['locked'] = $locked;
+    $entity_values['pattern'] = NULL;
+    $this->expectException(SchemaIncompleteException::class);
+    $entity = DateFormat::create($entity_values);
+    $entity->save();
+  }
+
+  /**
+   * @dataProvider provideLocked
+   */
+  public function testDateFormatNotBlank($locked) {
+    $entity_values['id'] = $entity_values['label'] = $this->randomMachineName();
+    $entity_values['locked'] = $locked;
+    $entity_values['pattern'] = '';
+    $this->expectException(SchemaIncompleteException::class);
+    $entity = DateFormat::create($entity_values);
+    $entity->save();
+  }
+
+  /**
+   * @dataProvider provideLocked
+   */
+  public function testDateFormatNeedsAtLeastOneDateChar($locked) {
+    $entity_values['id'] = $entity_values['label'] = $this->randomMachineName();
+    $entity_values['locked'] = $locked;
+    $entity_values['pattern'] = 'k';
+    $this->expectException(SchemaIncompleteException::class);
+    $this->expectExceptionMessageMatches('/At least one of the characters should format this into a date/');
+    $entity = DateFormat::create($entity_values);
+    $entity->save();
+  }
+
+  public function provideLocked(): array {
+    return [
+      [TRUE],
+      [FALSE],
+    ];
   }
 
 }
