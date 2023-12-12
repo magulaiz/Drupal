9fd752b4878ca6445b0a1911500efcb05dc83ee8
Sebastian Leu <sebastian.leu@protonmail.com>
2023-12-12 21:14:21 +0700

    Issue #3168966: Post update hook to create the ckeditor_inline view mode for media entities

--- core/modules/ckeditor5/ckeditor5.post_update.php
+++ core/modules/ckeditor5/ckeditor5.post_update.php
@@ -5,6 +5,8 @@
  * Post update functions for CKEditor 5.
  */
 
+use Drupal\Core\Config\FileStorage;
+
 // cspell:ignore multiblock
 
 /**
@@ -19,4 +21,17 @@ function ckeditor5_removed_post_updates() {
     'ckeditor5_post_update_list_multiblock' => '11.0.0',
     'ckeditor5_post_update_list_start_reversed' => '11.0.0',
   ];
+}
+
+/**
+ * Creates the ckeditor_inline view mode for media entities.
+ */
+function ckeditor5_post_update_create_ckeditor_inline_view_mode() {
+  if (\Drupal::moduleHandler()->moduleExists('media')) {
+    $config_path = \Drupal::service('extension.list.module')->getPath('ckeditor5') . '/config/optional';
+    $source = new FileStorage($config_path);
+    \Drupal::entityTypeManager()->getStorage('entity_view_mode')
+      ->create($source->read('core.entity_view_mode.media.ckeditor_inline'))
+      ->save();
+  }
 }
