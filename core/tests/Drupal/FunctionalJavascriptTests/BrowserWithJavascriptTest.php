332f4e3338a92412b146fb7834606de5f927dfca
Michael Strelan <mstrelan@gmail.com>
2023-12-12 16:20:55 +1000

    Update one test

--- core/tests/Drupal/FunctionalJavascriptTests/BrowserWithJavascriptTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/BrowserWithJavascriptTest.php
@@ -27,7 +27,7 @@ class BrowserWithJavascriptTest extends WebDriverTestBase {
     $this->drupalGet('<front>');
     $session = $this->getSession();
 
-    $session->resizeWindow(400, 300);
+    $session->resizeWindow(400, 400);
     $javascript = <<<JS
     (function(){
         var w = window,
