6fc4aaa1ec1473b5c6e32a0e9262ef909dff1427
Lennard Swaneveld <lennard@ezcompany.nl>
2023-12-12 07:56:38 +0100

    Update the exception

--- core/modules/views/src/Exception/InvalidViewsDataException.php
+++ core/modules/views/src/Exception/InvalidViewsDataException.php
@@ -6,8 +6,8 @@ namespace Drupal\views\Exception;
  * Thrown when invalid views data is encountered.
  *
  * For example, this can occur when in provided views data the relationship
- * table points to itself or into an infinite loop or to a non existing table.
+ * table points to itself or into an infinite loop or to a non-existing table.
  */
-class InvalidViewsDataException extends \Exception {
+class InvalidViewsDataException extends \RuntimeException {
 
 }
