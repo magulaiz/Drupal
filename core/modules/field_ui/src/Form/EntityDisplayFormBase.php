3383ea8c7946d939433b0b3e9cd6e82eb55f6d6a
Joao Paulo <joaopauloctga@gmail.com>
2023-12-13 11:16:34 -0300

    Fixing drag icon on Manage form display and Manage display

--- core/modules/field_ui/src/Form/EntityDisplayFormBase.php
+++ core/modules/field_ui/src/Form/EntityDisplayFormBase.php
@@ -184,6 +184,7 @@ abstract class EntityDisplayFormBase extends EntityForm {
       '#attributes' => [
         'class' => ['field-ui-overview'],
         'id' => 'field-display-overview',
+        'data-drag-orientation' => 'drag-y',
       ],
       '#tabledrag' => [
         [
