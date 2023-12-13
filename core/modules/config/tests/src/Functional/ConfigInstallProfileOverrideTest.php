8b2114126b3ef4904308932133fa8b4911d16101
Stephen Mustgrave <smustgrave@gmail.com>
2023-12-13 18:47:33 -0500

    Address feedback

--- core/modules/config/tests/src/Functional/ConfigInstallProfileOverrideTest.php
+++ core/modules/config/tests/src/Functional/ConfigInstallProfileOverrideTest.php
@@ -84,8 +84,9 @@ class ConfigInstallProfileOverrideTest extends BrowserTestBase {
     $action = Action::load('user_block_user_action');
     $this->assertEquals('Overridden block the selected user(s)', $action->label());
     $action = Action::load('user_cancel_user_action');
-    $this->assertEquals('Cancel the selected user account(s)', $action->label(), 'Default configuration that is not overridden.');
+    $this->assertEquals('Cancel the selected user account(s)', $action->label(), 'Default configuration that is not overridden is not affected.');
 
+    // Ensure that optional configuration can be overridden.
     $config_test = \Drupal::entityTypeManager()->getStorage('entity_test_bundle')->load('install_test');
     $this->assertEquals('Optional configuration can be overridden.', $config_test->label());
     $config_test = \Drupal::entityTypeManager()->getStorage('entity_test_bundle')->load('install_test2');
