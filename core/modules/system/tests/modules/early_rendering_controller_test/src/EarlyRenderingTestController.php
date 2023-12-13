63d4287668adc68ee2b44ef43c4cddfa4a9c4636
Marco Villegas <git@marvil07.net>
2023-12-13 14:02:26 -0500

    Fix errors thrown from EarlyRenderingControllerTest

--- core/modules/system/tests/modules/early_rendering_controller_test/src/EarlyRenderingTestController.php
+++ core/modules/system/tests/modules/early_rendering_controller_test/src/EarlyRenderingTestController.php
@@ -96,7 +96,7 @@ class EarlyRenderingTestController extends ControllerBase {
 
   public function responseEarly() {
     $render_array = $this->earlyRenderContent();
-    return new Response($this->renderer->render($render_array));
+    return new Response((string) $this->renderer->render($render_array));
   }
 
   public function responseWithAttachments() {
@@ -105,7 +105,7 @@ class EarlyRenderingTestController extends ControllerBase {
 
   public function responseWithAttachmentsEarly() {
     $render_array = $this->earlyRenderContent();
-    return new AttachmentsTestResponse($this->renderer->render($render_array));
+    return new AttachmentsTestResponse((string) $this->renderer->render($render_array));
   }
 
   public function cacheableResponse() {
@@ -114,7 +114,7 @@ class EarlyRenderingTestController extends ControllerBase {
 
   public function cacheableResponseEarly() {
     $render_array = $this->earlyRenderContent();
-    return new CacheableTestResponse($this->renderer->render($render_array));
+    return new CacheableTestResponse((string) $this->renderer->render($render_array));
   }
 
   public function domainObject() {
