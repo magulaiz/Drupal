799cc7ea0d85d94c3665616c2829eed09a9f4f64
Andrew Wilson <awilson8@uw.edu>
2023-12-13 16:16:54 -0800

    Cease encoding then immediately decoding replaced self token value.
    The subsequent call to `PluginBase::viewsTokenReplace()` encodes the
    value, anyway. Other field plugins in e.g. `taxonomy` and `user` modules
    also skip encoding.

--- core/modules/views/src/Plugin/views/field/EntityField.php
+++ core/modules/views/src/Plugin/views/field/EntityField.php
@@ -987,7 +987,7 @@ class EntityField extends FieldPluginBase implements CacheableDependencyInterfac
 
         if (is_array($raw)) {
           if (isset($raw[$id]) && is_scalar($raw[$id])) {
-            $tokens['{{ ' . $this->options['id'] . '__' . $id . ' }}'] = Html::decodeEntities(Xss::filterAdmin($raw[$id]));
+            $tokens['{{ ' . $this->options['id'] . '__' . $id . ' }}'] = raw[$id];
           }
           else {
             // Make sure that empty values are replaced as well.
@@ -1000,7 +1000,7 @@ class EntityField extends FieldPluginBase implements CacheableDependencyInterfac
           // Check if TypedDataInterface is implemented so we know how to render
           // the item as a string.
           if (!empty($property) && $property instanceof TypedDataInterface) {
-            $tokens['{{ ' . $this->options['id'] . '__' . $id . ' }}'] = Html::decodeEntities(Xss::filterAdmin($property->getString()));
+            $tokens['{{ ' . $this->options['id'] . '__' . $id . ' }}'] = $property->getString();
           }
           else {
             // Make sure that empty values are replaced as well.
