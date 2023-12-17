e1d37694a6c17cbc72b95a81f4afd0644b313e9c
David Barratt <david@davidwbarratt.com>
2023-12-17 14:47:10 -0500

    Create mechanism for custom transports without addding the Transport class to DI

--- core/tests/Drupal/Tests/Core/Mail/Plugin/Mail/SymfonyMailerTest.php
+++ core/tests/Drupal/Tests/Core/Mail/Plugin/Mail/SymfonyMailerTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\Core\Mail\Plugin\Mail;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Mail\MailFormatHelper;
 use Drupal\Core\Mail\Plugin\Mail\SymfonyMailer;
+use Drupal\Core\Mail\TransportFactoryManagerInterface;
 use Drupal\Tests\UnitTestCase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
@@ -51,7 +52,10 @@ class SymfonyMailerTest extends UnitTestCase {
     /** @var \Psr\Log\LoggerInterface|\PHPUnit\Framework\MockObject\MockObject */
     $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
-    $plugin = new SymfonyMailer($logger, $mailer);
+    /** @var \Drupal\Core\Mail\TransportFactoryManagerInterface|\PHPUnit\Framework\MockObject\MockObject */
+    $factoryManager = $this->getMockBuilder(TransportFactoryManagerInterface::class)->getMock();
+
+    $plugin = new SymfonyMailer($logger, $factoryManager, $mailer);
     $message = $plugin->format($message);
 
     $expect = MailFormatHelper::wrapMail($plain . "\n\n" . strtr($template, $variables) . "\n");
@@ -134,7 +138,10 @@ class SymfonyMailerTest extends UnitTestCase {
     /** @var \Psr\Log\LoggerInterface|\PHPUnit\Framework\MockObject\MockObject */
     $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
-    $plugin = new SymfonyMailer($logger, $mailer);
+    /** @var \Drupal\Core\Mail\TransportFactoryManagerInterface|\PHPUnit\Framework\MockObject\MockObject */
+    $factoryManager = $this->getMockBuilder(TransportFactoryManagerInterface::class)->getMock();
+
+    $plugin = new SymfonyMailer($logger, $factoryManager, $mailer);
     $this->assertTrue($plugin->mail($message));
   }
 
