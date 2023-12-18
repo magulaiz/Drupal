1d94cd8d756e639e62b61a0eb2618a0fca04cbfc
Gary Gao <gary.gao@salesforce.com>
2023-12-17 19:36:02 -0800

    2811043-125 review feedback
    https://www.drupal.org/project/drupal/issues/2811043#comment-15284927
    
    Some other fixup:
    - assign and move up $file_url_generator
    - clean up "public" vs "private" test paths in new assertions

--- core/modules/file/file.post_update.php
+++ core/modules/file/file.post_update.php
@@ -9,6 +9,7 @@ use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\user\RoleInterface;
 
 /**
  * Implements hook_removed_post_updates().
