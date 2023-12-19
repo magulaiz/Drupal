5c376cd10ea3cfd74a4649d7f7aea26b805308c7
Alexander Yureskul <54681-chesn0k@users.noreply.drupalcode.org>
2023-12-19 16:34:20 +0000

    Fix

--- core/lib/Drupal/Core/DependencyInjection/ClassResolver.php
+++ core/lib/Drupal/Core/DependencyInjection/ClassResolver.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\Core\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\Exception\AutowiringFailedException;
 
 /**
  * Implements the class resolver interface supporting class names and services.
