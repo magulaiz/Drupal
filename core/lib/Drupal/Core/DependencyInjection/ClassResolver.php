8a109e4b280dee3712cf52172fa7e063f46274f9
Alexander Yureskul <54681-chesn0k@users.noreply.drupalcode.org>
2023-12-19 16:23:54 +0000

    Added automatically wiring dependencies if ContainerInjectionInterface is not implemented.

--- core/lib/Drupal/Core/DependencyInjection/ClassResolver.php
+++ core/lib/Drupal/Core/DependencyInjection/ClassResolver.php
@@ -27,6 +27,26 @@ class ClassResolver implements ClassResolverInterface, ContainerAwareInterface {
       if (is_subclass_of($definition, 'Drupal\Core\DependencyInjection\ContainerInjectionInterface')) {
         $instance = $definition::create($this->container);
       }
+      elseif (method_exists($definition, '__construct')) {
+        $constructor = new \ReflectionMethod($definition, '__construct');
+        $args = [];
+
+        foreach ($constructor->getParameters() as $parameter) {
+          $service = (string) $parameter->getType();
+
+          foreach ($parameter->getAttributes(Autowire::class) as $attribute) {
+            $service = (string) $attribute->newInstance()->value;
+          }
+
+          if (!$this->container->has($service)) {
+            throw new AutowiringFailedException($service, sprintf('Cannot autowire service "%s": argument "$%s" of method "%s::_construct()", you should configure its value explicitly.', $service, $parameter->getName(), static::class));
+          }
+
+          $args[] = $this->container->get($service);
+        }
+
+        $instance = new $definition(...$args);
+      }
       else {
         $instance = new $definition();
       }
