19874ecf61d954868673cdbe8d55395b19d7ee91
omkar podey <omkar.podey@acquia.com>
2023-12-20 15:16:47 +0530

    fix the last test

--- core/modules/node/tests/src/Functional/NodeTypeTranslationTest.php
+++ core/modules/node/tests/src/Functional/NodeTypeTranslationTest.php
@@ -170,14 +170,11 @@ class NodeTypeTranslationTest extends BrowserTestBase {
 
     // Add an email field.
     $this->drupalGet("admin/structure/types/manage/{$type}/fields/add-field");
-    $this->submitForm([
-      'new_storage_type' => 'email',
-    ], 'Continue');
+    $this->clickLink('Email');
     $this->submitForm([
       'label' => 'Email',
       'field_name' => 'email',
     ], 'Continue');
-    $this->submitForm([], 'Update settings');
     $this->submitForm([], 'Save settings');
 
     $type = $this->randomMachineName(16);
