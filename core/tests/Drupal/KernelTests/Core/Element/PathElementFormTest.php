1fe7e8f7e5a9948f2e7dcb26ff0c606811bdfb2e
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2023-12-20 16:15:40 +0600

    remove unwanted changes

--- core/tests/Drupal/KernelTests/Core/Element/PathElementFormTest.php
+++ core/tests/Drupal/KernelTests/Core/Element/PathElementFormTest.php
@@ -115,7 +115,7 @@ class PathElementFormTest extends KernelTestBase implements FormInterface {
 
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => 'Submit',
+      '#value' => t('Submit'),
     ];
 
     return $form;
