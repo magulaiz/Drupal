2b23f7ff4ace2c61d6736d078c105c7d3e54da3f
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2023-12-28 16:04:27 +0530

    Added the email conditionally

--- core/modules/user/src/AccountForm.php
+++ core/modules/user/src/AccountForm.php
@@ -95,14 +95,15 @@ abstract class AccountForm extends ContentEntityForm implements TrustedCallbackI
     // and the user performing the edit has 'administer users' permission.
     // This allows users without email address to be edited and deleted.
     // Also see \Drupal\user\Plugin\Validation\Constraint\UserMailRequired.
-    $form['account']['mail'] = [
-      '#type' => 'email',
-      '#title' => $this->t('Email address'),
-      '#description' => $this->t('A valid email address. All emails from the system will be sent to this address. The email address is not made public and will only be used if you wish to receive a new password or wish to receive certain news or notifications by email.'),
-      '#required' => !(!$account->getEmail() && $user->hasPermission('administer users')),
-      '#default_value' => (!$register ? $account->getEmail() : ''),
-      '#access' => $account->mail->access('edit'),
-    ];
+    if ($register) {
+      $form['account']['mail'] = [
+        '#type' => 'email',
+        '#title' => $this->t('Email address'),
+        '#description' => $this->t('The email address is not made public. It will only be used if you need to be contacted about your account or for opted-in notifications.'),
+        '#required' => !(!$account->getEmail() && $user->hasPermission('administer users')),
+        '#default_value' => (!$register ? $account->getEmail() : ''),
+      ];
+    }
 
     // Only show name field on registration form or user can change own username.
     $form['account']['name'] = [
