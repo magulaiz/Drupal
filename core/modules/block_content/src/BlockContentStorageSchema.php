b31133f9bfe75fe2e7f55df2b708eddbb7a45bf3
Stephen Mustgrave <smustgrave@gmail.com>
2024-01-02 16:36:25 -0500

    Address feedback

--- core/modules/block_content/src/BlockContentStorageSchema.php
+++ core/modules/block_content/src/BlockContentStorageSchema.php
@@ -17,11 +17,9 @@ class BlockContentStorageSchema extends SqlContentEntityStorageSchema {
     $schema = parent::getSharedTableFieldSchema($storage_definition, $table_name, $column_mapping);
     $field_name = $storage_definition->getName();
 
-    if ($table_name === $this->storage->getDataTable()) {
-      // Add index to 'reusable' to improve performance when filtering on this field
-      if ($field_name === 'reusable') {
-        $this->addSharedTableFieldIndex($storage_definition, $schema);
-      }
+    // Add index to 'reusable' to improve performance when filtering on this field
+    if ($table_name === $this->storage->getDataTable() && $field_name === 'reusable') {
+      $this->addSharedTableFieldIndex($storage_definition, $schema);
     }
 
     return $schema;
