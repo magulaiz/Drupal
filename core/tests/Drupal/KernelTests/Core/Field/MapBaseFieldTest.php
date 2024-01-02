c3d446b6d8aa9d3a397220a1eab8e2e858410e68
Brad Jones <brad@bradjonesllc.com>
2024-01-02 15:58:17 -0700

    Only do serialization in the storage

--- core/tests/Drupal/KernelTests/Core/Field/MapBaseFieldTest.php
+++ core/tests/Drupal/KernelTests/Core/Field/MapBaseFieldTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\KernelTests\Core\Field;
 
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\entity_test_update\Entity\EntityTestUpdate;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 
@@ -44,44 +43,23 @@ class MapBaseFieldTest extends EntityKernelTestBase {
   }
 
   /**
-   * Data provider for testMapItemBaseField().
-   */
-  public function provideMapItemBaseFieldData() {
-    return [
-      'single item cardinality, stored in base table' => [1],
-      'multiple item cardinality, stored in dedicated table' => [FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED],
-    ];
-  }
-
-  /**
    * Tests uninstalling map item base field.
-   *
-   * @dataProvider provideMapItemBaseFieldData
    */
-  public function testMapItemBaseField(int $cardinality): void {
+  public function testUninstallMapItemBaseField(): void {
     $definitions['data_map'] = BaseFieldDefinition::create('map')
       ->setLabel(t('Data'))
-      ->setCardinality($cardinality)
       ->setRequired(TRUE);
 
     $this->state->set('entity_test_update.additional_base_field_definitions', $definitions);
 
     $this->entityDefinitionUpdateManager->installFieldStorageDefinition('data_map', 'entity_test_update', 'entity_test', $definitions['data_map']);
 
-    $dataMapValue = [
-      'key' => 'value',
-      'another' => ['array', 'indexed' => 'value'],
-    ];
-
     $entity = EntityTestUpdate::create([
-      'data_map' => $dataMapValue,
+      'data_map' => [
+        'key' => 'value',
+      ],
     ]);
     $entity->save();
-    $entityId = $entity->id();
-
-    $storage = \Drupal::entityTypeManager()->getStorage('entity_test_update');
-    $entity = $storage->loadUnchanged($entityId);
-    $this->assertSame($dataMapValue, $entity->get('data_map')->first()->getValue());
 
     $this->entityDefinitionUpdateManager->uninstallFieldStorageDefinition($definitions['data_map']);
   }
