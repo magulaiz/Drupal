cc4b3b3d55664bc10ea527fe979b47647d1659ff
Sebastian Leu <sebastian.leu@protonmail.com>
2024-01-03 11:32:52 +0700

    Issue #3168966: Update hook to avoid stripping of drupal-media-inline tags by filter_html

--- core/modules/ckeditor5/ckeditor5.post_update.php
+++ core/modules/ckeditor5/ckeditor5.post_update.php
@@ -43,3 +43,21 @@ function ckeditor5_post_update_create_ckeditor_inline_view_mode() {
       ->save();
   }
 }
+
+/**
+ * Amends the filter format settings to allow the drupal-media-inline tag.
+ */
+function ckeditor5_post_update_allow_inline_media_element() {
+  $formats = filter_formats();
+  foreach ($formats as $format_id => $format) {
+    $config = \Drupal::configFactory()->getEditable('filter.format.' . $format_id);
+    $filters = $config->get('filters');
+    if (\array_key_exists('media_embed', $filters)
+      && \array_key_exists('filter_html', $filters)
+      && !\str_contains($filters['filter_html']['settings']['allowed_html'], 'drupal-media-inline')) {
+      $filters['filter_html']['settings']['allowed_html'] .= ' <drupal-media-inline data-entity-type data-entity-uuid alt data-view-mode data-caption data-align>';
+      $config->set('filters', $filters);
+      $config->save();
+    }
+  }
+}
