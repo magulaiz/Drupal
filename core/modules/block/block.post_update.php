5adee3c770dad780233c2c34ebfd5e8ee7e57b51
Kunal Sachdev <kunal.sachdev@acquia.com>
2024-01-03 17:17:24 +0530

    use ConfigEntityUpdater in post update

--- core/modules/block/block.post_update.php
+++ core/modules/block/block.post_update.php
@@ -5,6 +5,10 @@
  * Post update functions for Block.
  */
 
+use Consolidation\Config\ConfigInterface;
+use Drupal\block\BlockInterface;
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -20,25 +24,20 @@ function block_removed_post_updates() {
 /**
  * Add 'base_route_title' setting for page title blocks.
  */
-function block_post_update_add_base_route_title_page_title() {
-  $config_factory = \Drupal::configFactory();
-  foreach ($config_factory->listAll('block.block.') as $block_config_name) {
-    if (!str_contains($block_config_name, 'page_title')) {
-      continue;
-    }
-    $block = $config_factory->getEditable($block_config_name);
-
-    $settings = $block->get('settings');
-    if (str_contains($block->getName(), 'claro')) {
-      $settings['base_route_title'] = TRUE;
-    }
-    else {
-      $settings['base_route_title'] = FALSE;
-    }
-    $block->set('settings', $settings);
-
-    // Mark the resulting configuration as trusted data. This avoids issues with
-    // future schema changes.
-    $block->save(TRUE);
-  }
+function block_post_update_add_base_route_title_page_title(&$sandbox = NULL) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)
+    ->update($sandbox, 'block', function (BlockInterface $block) {
+      if ($block->get('plugin') == 'page_title_block') {
+        $settings = $block->get('settings');
+        if ($block->get('theme') == 'claro') {
+          $settings['base_route_title'] = TRUE;
+        }
+        else {
+          $settings['base_route_title'] = FALSE;
+        }
+        $block->set('settings', $settings);
+        return TRUE;
+      }
+      return FALSE;
+    });
 }
