0e31aa05bb1f475a495f34218f234d0ac164d66e
catch <catch@35733.no-reply.drupal.org>
2024-01-03 10:08:05 +0000

    Unused use statements.

--- core/modules/jsonapi/tests/src/Functional/JsonApiPatchRegressionTest.php
+++ core/modules/jsonapi/tests/src/Functional/JsonApiPatchRegressionTest.php
@@ -12,17 +12,10 @@ use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Url;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItem;
 use Drupal\entity_test\Entity\EntityTest;
-use Drupal\entity_test\Entity\EntityTestMapField;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\node\Entity\Node;
-use Drupal\node\Entity\NodeType;
-use Drupal\shortcut\Entity\Shortcut;
 use Drupal\taxonomy\Entity\Term;
-use Drupal\taxonomy\Entity\Vocabulary;
-use Drupal\user\Entity\Role;
-use Drupal\user\Entity\User;
-use Drupal\user\RoleInterface;
 use GuzzleHttp\RequestOptions;
 
 // cspell:ignore llamalovers catcuddlers Cuddlers
@@ -35,7 +28,7 @@ use GuzzleHttp\RequestOptions;
  *
  * @internal
  */
-class JsonApiRegressionTest extends JsonApiFunctionalTestBase {
+class JsonApiPatchRegressionTest extends JsonApiFunctionalTestBase {
 
   use CommentTestTrait;
 
