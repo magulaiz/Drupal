289c928c4419b4f25f99f96a7bcfa00a9cb1a51e
Kunal Sachdev <kunal.sachdev@acquia.com>
2024-01-04 18:07:12 +0530

    update upgrade path test

--- core/modules/block/tests/src/Functional/Update/BlockTitleUpdateTest.php
+++ core/modules/block/tests/src/Functional/Update/BlockTitleUpdateTest.php
@@ -15,31 +15,9 @@ class BlockTitleUpdateTest extends UpdatePathTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['block', 'language'];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp(): void {
-    parent::setUp();
-
-    // Add the page title block to the page.
-    $this->drupalPlaceBlock('page_title_block', ['id' => 'stark_page_title']);
-
-    // Create administrative user.
-    $admin_user = $this->drupalCreateUser([
-      'administer blocks',
-      'administer themes',
-    ]);
-    $this->drupalLogin($admin_user);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   protected function setDatabaseDumpFiles() {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.bare.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
     ];
   }
 
@@ -52,7 +30,7 @@ class BlockTitleUpdateTest extends UpdatePathTestBase {
   public function providerTestPostUpdateAddContextualizePageTitle() {
     return [
       'Stark theme' => ['stark', FALSE],
-      // For claro the 'contextual_title' configuration is enabled by default.
+      // For claro the 'base_route_title' configuration is enabled by default.
       'Claro theme' => ['claro', TRUE],
       'Olivero theme' => ['olivero', FALSE],
     ];
@@ -64,29 +42,14 @@ class BlockTitleUpdateTest extends UpdatePathTestBase {
    * @dataProvider providerTestPostUpdateAddContextualizePageTitle
    */
   public function testPostUpdateAddContextualizePageTitle(string $theme, bool $contextual_title_enabled): void {
+    $block_settings = $this->config('block.block.' . $theme . '_page_title')->get('settings');
+    $this->assertArrayNotHasKey('base_route_title', $block_settings);
+
     $this->runUpdates();
 
-    $system_theme_config = $this->container->get('config.factory')
-      ->getEditable('system.theme');
-    $system_theme_config
-      ->set('default', $theme)
-      ->save();
-    \Drupal::service('theme_installer')->install([$theme]);
-    $edit['admin_theme'] = $theme;
-    $this->drupalGet('admin/appearance');
-    $this->submitForm($edit, 'Save configuration');
-    $this->drupalGet('admin/structure/block');
-    $this->drupalGet('admin/structure/block/manage/' . $theme . '_page_title');
-
-    // If the title block is configured to show contextualized title then the value of the
-    // configuration will be 1 otherwise 0.
-    // @see \Drupal\Core\Block\Plugin\Block\PageTitleBlock::blockForm()
-    if ($contextual_title_enabled) {
-      $this->assertSession()->fieldValueEquals('settings[base_route_title]', 1);
-    }
-    else {
-      $this->assertSession()->fieldValueEquals('settings[base_route_title]', 0);
-    }
+    $block_settings = $this->config('block.block.' . $theme . '_page_title')->get('settings');
+    $this->assertArrayHasKey('base_route_title', $block_settings);
+    $this->assertSame($block_settings['base_route_title'], $contextual_title_enabled);
   }
 
 }
