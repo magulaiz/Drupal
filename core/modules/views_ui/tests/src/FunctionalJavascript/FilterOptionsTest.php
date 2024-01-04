3e7852f57cccb89e7db48380888dd2b0937a7a3b
Daniel Lobo <info@danlobo.co.uk>
2024-01-04 13:53:59 +0000

    - reroll bugfix for views modal dialogue

--- core/modules/views_ui/tests/src/FunctionalJavascript/FilterOptionsTest.php
+++ core/modules/views_ui/tests/src/FunctionalJavascript/FilterOptionsTest.php
@@ -36,6 +36,8 @@ class FilterOptionsTest extends WebDriverTestBase {
 
     $admin_user = $this->drupalCreateUser([
       'administer views',
+      'administer site configuration',
+      'access site in maintenance mode',
     ]);
     $this->drupalLogin($admin_user);
   }
@@ -83,6 +85,36 @@ class FilterOptionsTest extends WebDriverTestBase {
     });
     $this->assertFalse($page->findField('name[views.views_test_field_2]')->isVisible());
     $this->assertFalse($page->findField('name[views.views_test_field_1]')->isVisible());
+  }
+
+  /**
+   * Tests filtering options in the 'Add fields' dialog.
+   */
+  public function testFilterOptionsAddFieldsWithMaintenanceMode() {
+    // Test popup when in maintenance mode. Copied from SiteMaintenanceTest.php.
+    $edit = [
+      'maintenance_mode' => 1,
+    ];
+    $this->drupalGet('admin/config/development/maintenance');
+    $this->assertSession()->pageTextContains('Visitors will only see the maintenance mode message. Only users with the "Use the site in maintenance mode" permission will be able to access the site. Authorized users can log in directly via the user login page.');
+    $this->submitForm($edit, 'Save configuration');
+
+    $this->drupalGet('admin/structure/views/view/content');
+
+    $session = $this->getSession();
+    $web_assert = $this->assertSession();
+    $page = $session->getPage();
+
+    $add_link = $page->findById('views-add-field');
+    $this->assertTrue($add_link->isVisible(), 'Add fields button found.');
+    $page->clickLink('views-add-field');
+
+    // Wait for the popup to open and the search field to be available.
+    $web_assert->waitForField('override[controls][options_search]');
+    $this->assertSession()->pageTextContains('Operating in maintenance mode. Go online.');
+    // Test that the both special fields are visible.
+    $this->assertTrue($page->findField('name[views.views_test_field_1]')->isVisible());
+    $this->assertTrue($page->findField('name[views.views_test_field_2]')->isVisible());
   }
 
 }
