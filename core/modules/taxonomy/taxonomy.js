2313778e6c2aaa63c60735153c2a3a6430ed3c3f
omkar-pd <omkarpd109@gmail.com>
2024-01-05 21:22:11 +0530

    fix pipeline 2

--- core/modules/taxonomy/taxonomy.js
+++ core/modules/taxonomy/taxonomy.js
@@ -44,8 +44,9 @@
           tableBody.rows[backStep - 1].classList.add(
             'taxonomy-term-divider-top',
           );
-          tableBody.rows[backStep].
-            classList.add('taxonomy-term-divider-bottom');
+          tableBody.rows[backStep].classList.add(
+            'taxonomy-term-divider-bottom',
+          );
         }
 
         if (forwardStep) {
