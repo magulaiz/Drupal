bdde4012fba8ba27cc25e51c91d9106432aea168
Phéna Proxima <adam@phenaproxima.net>
2024-01-04 22:55:49 -0500

    Expand test coverage

--- core/tests/Drupal/KernelTests/Core/Entity/DateFormatValidationTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/DateFormatValidationTest.php
@@ -28,19 +28,23 @@ class DateFormatValidationTest extends ConfigEntityValidationTestBase {
   }
 
   /**
-   * Tests that the pattern of a date format can never be blank.
+   * Tests that the pattern of a date format is validated.
    *
+   * @param string $pattern
+   *   The pattern to set.
    * @param bool $locked
    *   Whether the date format entity is locked or not.
+   * @param string $expected_error
+   *   The error message that should be flagged for the invalid pattern.
    *
-   * @testWith [true]
-   *   [false]
+   * @testWith ["q", true, "This value is not valid."]
+   *   ["", true, "This value should not be blank."]
+   *   ["q", false, "This value is not valid."]
+   *   ["", false, "This value should not be blank."]
    */
-  public function testPatternCannotBeBlank(bool $locked): void {
-    $this->entity->setPattern('')->set('locked', $locked);
-    $this->assertValidationErrors([
-      'pattern' => 'This value should not be blank.',
-    ]);
+  public function testPatternIsValidated(string $pattern, bool $locked, string $expected_error): void {
+    $this->entity->setPattern($pattern)->set('locked', $locked);
+    $this->assertValidationErrors(['pattern' => $expected_error]);
   }
 
 }
