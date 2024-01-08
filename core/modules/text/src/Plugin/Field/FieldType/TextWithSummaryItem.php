c32c81715a6a3c5e8c9fb6cb73614d156ae21ac6
Stephen Mustgrave <smustgrave@gmail.com>
2024-01-08 10:15:31 -0500

    Remove condition

--- core/modules/text/src/Plugin/Field/FieldType/TextWithSummaryItem.php
+++ core/modules/text/src/Plugin/Field/FieldType/TextWithSummaryItem.php
@@ -120,9 +120,6 @@ class TextWithSummaryItem extends TextItemBase {
           [
             ':input[name="settings[required_summary]"]' => ['checked' => FALSE],
           ],
-          [
-            ':input[name="settings[display_summary]"]' => ['checked' => FALSE],
-          ],
         ],
       ],
     ];
