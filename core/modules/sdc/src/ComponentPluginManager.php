881736eaa6e3378224ee12f1e8ab8a02044d3d6c
Olivier Deboyser <30763-oldeb@users.noreply.drupalcode.org>
2024-01-08 17:14:12 +0100

    Issue #3391978 by oldeb, pdureau: SDC: Add a method to retrieve only components not replaced by others

--- core/modules/sdc/src/ComponentPluginManager.php
+++ core/modules/sdc/src/ComponentPluginManager.php
@@ -161,6 +161,26 @@ final class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
+   * Gets all definitions except the ones that are replaced.
+   *
+   * @return mixed[]
+   *   An array of plugin definitions (empty array if no definitions were
+   *   found). Keys are plugin IDs.
+   *
+   * @internal
+   */
+  public function getActiveDefinitions(): array {
+    $definitions = $this->getDefinitions();
+    foreach ($definitions as $definition) {
+      if (!empty($definition['replaces'])) {
+        unset($definitions[$definition['replaces']]);
+      }
+    }
+
+    return $definitions;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function clearCachedDefinitions(): void {
