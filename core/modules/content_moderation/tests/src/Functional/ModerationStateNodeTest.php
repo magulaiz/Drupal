d153b88c73001d70f995d26f64961ed9588a6e91
Adam Bramley <adam.bramley@previousnext.com.au>
2024-01-09 14:44:39 +1100

    Revert remaining test changes

--- core/modules/content_moderation/tests/src/Functional/ModerationStateNodeTest.php
+++ core/modules/content_moderation/tests/src/Functional/ModerationStateNodeTest.php
@@ -84,7 +84,7 @@ class ModerationStateNodeTest extends ModerationStateTestBase {
   /**
    * Tests edit form destinations.
    */
-  public function testFormSaveDestination(): void {
+  public function testFormSaveDestination() {
     // Create new moderated content in draft.
     $this->drupalGet('node/add/moderated_content');
     $this->submitForm([
@@ -135,7 +135,7 @@ class ModerationStateNodeTest extends ModerationStateTestBase {
   /**
    * Tests pagers aren't broken by content_moderation.
    */
-  public function testPagers(): void {
+  public function testPagers() {
     // Create 51 nodes to force the pager.
     foreach (range(1, 51) as $delta) {
       Node::create([
@@ -158,7 +158,7 @@ class ModerationStateNodeTest extends ModerationStateTestBase {
   /**
    * Tests the workflow when a user has no Content Moderation permissions.
    */
-  public function testNoContentModerationPermissions(): void {
+  public function testNoContentModerationPermissions() {
     $session_assert = $this->assertSession();
 
     // Create a user with quite advanced node permissions but no content
