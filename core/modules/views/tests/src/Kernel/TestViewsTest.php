0ca5761fdf396ac151d2cbb8f244a537deb041cc
Phéna Proxima <adam@phenaproxima.net>
2024-01-09 08:51:10 -0500

    Final bits of feedback

--- core/modules/views/tests/src/Kernel/TestViewsTest.php
+++ core/modules/views/tests/src/Kernel/TestViewsTest.php
@@ -29,7 +29,6 @@ class TestViewsTest extends KernelTestBase {
    * @var array
    */
   protected static $modules = [
-    'image',
     'views',
     // `node.type.book` config entity is a config dependency.
     // @see core/modules/book/tests/modules/book_test_views/test_views/views.view.test_book_view.yml
@@ -122,6 +121,9 @@ class TestViewsTest extends KernelTestBase {
     // `history` is a module dependency.
     // @see core/modules/views/tests/modules/views_test_config/test_views/views.view.test_history.yml
     'history',
+    // The `image` module is required by at least one of the Node module's
+    // views.
+    'image',
   ];
 
   /**
