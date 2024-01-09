a5c90b32d7cbe4a98afbc532e073da07c5787d6f
Roman Paska <roman.paska@lembergsolutions.com>
2024-01-09 13:36:22 +0200

    And last fixes (I hope)

--- core/misc/jquery.passive-listener.js
+++ core/misc/jquery.passive-listener.js
@@ -8,26 +8,26 @@
 
 (($) => {
   $.event.special.touchstart = {
-    setup: function (_, ns, handle) {
+    setup: (_, ns, handle) => {
       this.addEventListener('touchstart', handle, {
-        passive: !ns.includes("noPreventDefault")
+        passive: !ns.includes('noPreventDefault'),
       });
     },
   };
   $.event.special.touchmove = {
-    setup: function (_, ns, handle) {
+    setup: (_, ns, handle) => {
       this.addEventListener('touchmove', handle, {
-        passive: !ns.includes('noPreventDefault')
+        passive: !ns.includes('noPreventDefault'),
       });
     },
   };
   $.event.special.wheel = {
-    setup: function (_, ns, handle) {
+    setup: (_, ns, handle) => {
       this.addEventListener('wheel', handle, { passive: true });
     },
   };
   $.event.special.mousewheel = {
-    setup: function (_, ns, handle) {
+    setup: (_, ns, handle) => {
       this.addEventListener('mousewheel', handle, { passive: true });
     },
   };
