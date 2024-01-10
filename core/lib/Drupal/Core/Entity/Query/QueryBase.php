9e4de0ec01dd43df4e0c22e29d3c7895b081bf10
Pieter Frenssen <pieter@frenssen.be>
2024-01-10 21:17:52 +0200

    Support resetting the range.

--- core/lib/Drupal/Core/Entity/Query/QueryBase.php
+++ core/lib/Drupal/Core/Entity/Query/QueryBase.php
@@ -193,10 +193,10 @@ abstract class QueryBase implements QueryInterface {
    * {@inheritdoc}
    */
   public function range($start = NULL, $length = NULL) {
-    $this->range = [
+    $this->range = $start || $length ? [
       'start' => $start ?? 0,
       'length' => $length,
-    ];
+    ] : [];
     return $this;
   }
 
