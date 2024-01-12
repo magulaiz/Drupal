657a44b14e4386c05da4b29b1503da60a996e39a
Marcin Grabias <mgrabias@ogtrading.eu>
2024-01-12 12:46:49 +0100

    Applied additional access logic to custom block creation only; removed leaked code.

--- core/modules/layout_builder/tests/src/Unit/BlockComponentRenderArrayTest.php
+++ core/modules/layout_builder/tests/src/Unit/BlockComponentRenderArrayTest.php
@@ -122,8 +122,6 @@ class BlockComponentRenderArrayTest extends UnitTestCase {
       '#base_plugin_id' => 'block_plugin_id',
       '#derivative_plugin_id' => NULL,
       'content' => $block_content,
-      '#attributes' => [],
-      '#contextual_links' => [],
       '#in_preview' => FALSE,
     ];
 
@@ -200,8 +198,6 @@ class BlockComponentRenderArrayTest extends UnitTestCase {
       '#base_plugin_id' => 'block_plugin_id',
       '#derivative_plugin_id' => NULL,
       'content' => $block_content,
-      '#attributes' => [],
-      '#contextual_links' => [],
       '#in_preview' => FALSE,
     ];
 
@@ -449,7 +445,6 @@ class BlockComponentRenderArrayTest extends UnitTestCase {
         ],
         'max-age' => -1,
       ],
-      '#contextual_links' => [],
     ];
 
     $subscriber->onBuildRender($event);
@@ -478,7 +473,6 @@ class BlockComponentRenderArrayTest extends UnitTestCase {
       '#cache' => [
         'tags' => ['empty_build_cache_test'],
       ],
-      '#contextual_links' => [],
     ];
     $block->build()->willReturn($block_content);
     $this->blockManager->createInstance('some_block_id', ['id' => 'some_block_id'])->willReturn($block->reveal());
