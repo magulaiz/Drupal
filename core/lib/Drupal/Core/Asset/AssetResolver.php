4f6a7b582254e981e0ffdd38fbebbd8add78a185
catch <catch@35733.no-reply.drupal.org>
2024-01-12 17:41:02 +0000

    Revert "Fix syntax error."
    This reverts commit e856c057473081a2ef3234f3f2146be6ee2bc98d.

--- core/lib/Drupal/Core/Asset/AssetResolver.php
+++ core/lib/Drupal/Core/Asset/AssetResolver.php
@@ -133,7 +133,7 @@ class AssetResolver implements AssetResolverInterface {
       return [];
     }
     $cid = 'css:' . $theme_info->getName() . ':' . $language->getId() . ':' . Crypt::hashBase64(serialize($libraries_to_load)) . ':' . (int) $optimize;
-    if ($cached = $this->cache->get($cid)) {
+    elseif ($cached = $this->cache->get($cid)) {
       return $cached->data;
     }
 
