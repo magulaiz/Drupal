3c86faf144479dddacc03d67a7235b481e49f559
catch <catch@35733.no-reply.drupal.org>
2024-01-12 11:14:44 +0000

    Fix extra space

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
@@ -32,7 +32,7 @@ class OpenTelemetryAuthenticatedPerformanceTest extends PerformanceTestBase {
     $this->drupalGet('<front>');
     $this->drupalGet('<front>');
 
-    $performance_data = $this->collectPerformanceData(function ()  {
+    $performance_data = $this->collectPerformanceData(function () {
       $this->drupalGet('<front>');
     }, 'authenticatedFrontPage');
     $this->assertSame(16, $performance_data->getQueryCount());
