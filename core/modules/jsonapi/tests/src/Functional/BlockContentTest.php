5f5f7dc964d15b9651017d74afee6e5b7bd78f45
hardik patel <hardikpatel@hardiks-MacBook-Pro.local>
2024-01-17 16:29:04 +0530

    Solving test failed cases

--- core/modules/jsonapi/tests/src/Functional/BlockContentTest.php
+++ core/modules/jsonapi/tests/src/Functional/BlockContentTest.php
@@ -220,7 +220,7 @@ class BlockContentTest extends ResourceTestBase {
     return match ($method) {
       'GET' => "The 'access block library' permission is required.",
       'PATCH' => "The 'edit any basic block content' permission is required.",
-      'POST' => "The following permissions are required: 'create basic block content' AND 'access block library'.",
+      'POST' => "The 'create basic block content' permission is required.",
       'DELETE' => "The 'delete any basic block content' permission is required.",
       default => parent::getExpectedUnauthorizedAccessMessage($method),
     };
