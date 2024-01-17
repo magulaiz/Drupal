fb370ed30112edcd07185d3cabf5cea82e3ff697
catch <catch@35733.no-reply.drupal.org>
2024-01-17 21:48:08 +0000

    Handle FALSE from curl_exec().

--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverCurlService.php
@@ -117,7 +117,10 @@ class WebDriverCurlService extends CurlService {
 
         curl_setopt($curl, CURLOPT_HTTPHEADER, $customHeaders);
 
-        $rawResult = trim(curl_exec($curl));
+        $result = curl_exec($curl);
+        if ($result !== FALSE) {
+          $rawResult = trim($result);
+        }
 
         $info = curl_getinfo($curl);
         $info['request_method'] = $requestMethod;
