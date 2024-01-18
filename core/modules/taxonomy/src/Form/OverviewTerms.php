ad2feed4f8c2deb8f40e09a3a8022912735a300e
Michael Strelan <mstrelan@gmail.com>
2024-01-18 15:18:43 +1000

    Revert "Fix core/modules/taxonomy/src/Form/OverviewTerms.php"
    This reverts commit 9e67fd7fbf6841b8eaa4d75e1f703c2f9402d210.

--- core/modules/taxonomy/src/Form/OverviewTerms.php
+++ core/modules/taxonomy/src/Form/OverviewTerms.php
@@ -510,9 +510,7 @@ class OverviewTerms extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Sort term order based on weight.
-    $terms = $form_state->getValue('terms');
-    uasort($terms, ['Drupal\Component\Utility\SortArray', 'sortByWeightElement']);
-    $form_state->setValue('terms', $terms);
+    uasort($form_state->getValue('terms'), ['Drupal\Component\Utility\SortArray', 'sortByWeightElement']);
 
     $vocabulary = $form_state->get(['taxonomy', 'vocabulary']);
     $changed_terms = [];
