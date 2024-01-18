faed3ecdd8297a21d89b8d8564486d4c6c55dc59
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-01-18 21:20:02 +0200

    fix phpcs errors

--- core/lib/Drupal/Core/Cache/CacheableMetadata.php
+++ core/lib/Drupal/Core/Cache/CacheableMetadata.php
@@ -123,7 +123,7 @@ class CacheableMetadata implements RefinableCacheableDependencyInterface {
     else {
       $result->cacheMaxAge = Cache::mergeMaxAges($this->cacheMaxAge, $other->cacheMaxAge);
     }
-    assert($result instanceof \Drupal\Core\Cache\CacheableMetadata);
+    assert($result instanceof CacheableMetadata);
     return $result;
   }
 
