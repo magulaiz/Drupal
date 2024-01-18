a148561f0b5a3ac7da44d05679bb03a47969d375
Michael Strelan <mstrelan@gmail.com>
2024-01-18 15:18:01 +1000

    Revert "Fix core/lib/Drupal/Core/Datetime/Element/Datetime.php"
    This reverts commit cdf2831749e14869641538d7f23a31d736e84def.

--- core/lib/Drupal/Core/Datetime/Element/Datetime.php
+++ core/lib/Drupal/Core/Datetime/Element/Datetime.php
@@ -346,8 +346,7 @@ class Datetime extends DateElementBase {
    */
   public static function validateDatetime(&$element, FormStateInterface $form_state, &$complete_form) {
     $input_exists = FALSE;
-    $values = $form_state->getValues();
-    $input = NestedArray::getValue($values, $element['#parents'], $input_exists);
+    $input = NestedArray::getValue($form_state->getValues(), $element['#parents'], $input_exists);
     if ($input_exists) {
 
       $title = static::getElementTitle($element, $complete_form);
