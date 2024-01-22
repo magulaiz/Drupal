1cc472d5e63ecd60febad65d4b02eea926f9a18b
catch <catch@35733.no-reply.drupal.org>
2024-01-22 11:30:41 +0000

    whitespace

--- core/modules/jsonapi/tests/src/Functional/FileUploadBehaviorTest.php
+++ core/modules/jsonapi/tests/src/Functional/FileUploadBehaviorTest.php
@@ -67,7 +67,7 @@ class FileUploadBehaviorTest extends BrowserTestBase {
    */
   protected static $postUri = '/jsonapi/entity_test/entity_test/field_rest_file_test';
 
- /**
+  /**
    * Whether the tested JSON:API resource is versionable.
    *
    * @var bool
