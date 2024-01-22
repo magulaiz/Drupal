09f7a1060c0770f8ca3de921b6f3a3ff7a37b3dd
Wim Leers <wim.leers@acquia.com>
2024-01-22 18:57:51 +0100

    Update setup for `EntityLinkSuggestionsTest`.

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/EntityLinkSuggestionsTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/EntityLinkSuggestionsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Drupal\Tests\ckeditor5\FunctionalJavascript;
 
@@ -78,7 +78,7 @@ class EntityLinkSuggestionsTest extends WebDriverTestBase {
           // @see \Drupal\ckeditor5\Plugin\CKEditor5Plugin\EntityLinkSuggestions::defaultConfiguration()
           'ckeditor5_link_entity_suggestions' => [
             'allow_download_links' => TRUE,
-            'suggestions' => NULL,
+            'suggester' => 'core.entity_link_suggester.everything',
           ],
         ],
       ],
