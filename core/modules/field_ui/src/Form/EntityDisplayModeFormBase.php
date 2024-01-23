8c78a5693220d227e694b6ddeb42d99b85857734
Kunal Sachdev <kunal.sachdev@acquia.com>
2024-01-23 18:42:37 +0530

    revert some changes in EntityDisplayModeFormBase.php

--- core/modules/field_ui/src/Form/EntityDisplayModeFormBase.php
+++ core/modules/field_ui/src/Form/EntityDisplayModeFormBase.php
@@ -115,6 +115,7 @@ abstract class EntityDisplayModeFormBase extends EntityForm {
     $definition = $this->entityTypeManager->getDefinition($this->entity->isNew() ? $this->targetEntityTypeId : $this->entity->getTargetType());
 
     $bundles_by_entity = [];
+    $defaults = [];
     foreach (array_keys($bundles[$definition->id()]) as $bundle) {
       $bundles_by_entity[$bundle] = $bundles[$definition->id()][$bundle]['label'];
       // Determine default display modes.
@@ -131,6 +132,7 @@ abstract class EntityDisplayModeFormBase extends EntityForm {
       '#title' => $this->t('Enable this @display-mode for the following @bundle-label types:', ['@display-mode' => $this->entityType->getSingularLabel(), '@bundle-label' => $definition->getLabel()]),
       '#description' => $this->t('This @display-mode will still be available for the rest of the @bundle-label types if not checked here, but it will not be enabled by default.', ['@bundle-label' => $definition->getLabel(), '@display-mode' => $this->entityType->getSingularLabel()]),
       '#options' => $bundles_by_entity,
+      '#default_value' => $defaults,
     ];
 
     return $form;
