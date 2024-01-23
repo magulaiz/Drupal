d7f468a6a223f01ad79a466fb3d0fca72c9b71a4
AKHIL BABU <akhilbabui1998@gmail.com>
2024-01-23 17:32:27 +0530

    Issues/3010895: Empty state is wrong for input when filled with whitespaces.

--- core/misc/states.js
+++ core/misc/states.js
@@ -535,11 +535,11 @@
       keyup() {
         // The function associated with that trigger returns the new value for
         // the state.
-        return this.val() === '';
+        return this.val().trim() === '';
       },
       // Listen to 'change' for number native "spinner" widgets.
       change() {
-        return this.val() === '';
+        return this.val().trim() === '';
       },
     },
 
