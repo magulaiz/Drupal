5c61e3b837f59efcff6b88e38c79c7f31c620aed
Kushal Agrawal <kushalagrawal.1996@gmail.com>
2024-01-23 15:42:45 +0530

    3406109: changing the text

--- core/misc/form.js
+++ core/misc/form.js
@@ -111,7 +111,7 @@
    *   (due to limitations of jQuery.serialize()). That is deemed to be
    *   acceptable, because if the user forgot to attach a file, then the size of
    *   HTTP payload will most likely be small enough to be fully passed to the
-   *   server endpoint within (milli)seconds. If a user mistakenly attached a
+   *   server endpoint within seconds, or even milliseconds. If a user mistakenly attached a
    *   wrong file and is technically versed enough to cancel the form submission
    *   (and HTTP payload) in order to attach a different file, then that
    *   edge-case is not supported here.
