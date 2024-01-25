11097165e5f2a2f2c43c7e8a72cb7fc262d2d607
Adam Bramley <adam.bramley@previousnext.com.au>
2024-01-25 13:17:24 +1100

    Remove cachePerPermissions when using cachePerUser

--- core/modules/media/src/MediaAccessControlHandler.php
+++ core/modules/media/src/MediaAccessControlHandler.php
@@ -70,7 +70,6 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
         }
         elseif ($account->hasPermission('view own unpublished media')) {
           $access_result = AccessResult::allowedIf($is_owner)
-            ->cachePerPermissions()
             ->cachePerUser()
             ->addCacheableDependency($entity);
           if (!$access_result->isAllowed()) {
@@ -79,7 +78,6 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
         }
         else {
           $access_result = AccessResult::neutral()
-            ->cachePerPermissions()
             ->cachePerUser()
             ->addCacheableDependency($entity)
             ->setReason("The user must be the owner and the 'view own unpublished media' permission is required when the media item is unpublished.");
@@ -96,10 +94,9 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
           return AccessResult::allowed()->cachePerPermissions()->addCacheableDependency($entity);
         }
         if (($account->hasPermission('edit own ' . $type . ' media') || $account->hasPermission('update media')) && $is_owner) {
-          return AccessResult::allowed()->cachePerPermissions()->cachePerUser()->addCacheableDependency($entity);
+          return AccessResult::allowed()->cachePerUser()->addCacheableDependency($entity);
         }
         return AccessResult::neutral("The following permissions are required: 'update any media' OR 'update own media' OR '$type: edit any media' OR '$type: edit own media'.")
-          ->cachePerPermissions()
           ->cachePerUser()
           ->addCacheableDependency($entity);
 
@@ -113,10 +110,9 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
           return AccessResult::allowed()->cachePerPermissions()->addCacheableDependency($entity);
         }
         if (($account->hasPermission('delete own ' . $type . ' media') || $account->hasPermission('delete media')) && $is_owner) {
-          return AccessResult::allowed()->cachePerPermissions()->cachePerUser()->addCacheableDependency($entity);
+          return AccessResult::allowed()->cachePerUser()->addCacheableDependency($entity);
         }
         return AccessResult::neutral("The following permissions are required: 'delete any media' OR 'delete own media' OR '$type: delete any media' OR '$type: delete own media'.")
-          ->cachePerPermissions()
           ->cachePerUser()
           ->addCacheableDependency($entity);
 
