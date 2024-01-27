6ecf6ffa268315447e0f818307066ead5ec547ed
Francisco Adame <fadamemu@emeal.nttdata.com>
2024-01-27 13:42:09 +0100

    Update MigrateFieldFormatterSettingsTest.php
    Adding new fields to expected array

--- core/modules/field/tests/src/Kernel/Migrate/d6/MigrateFieldFormatterSettingsTest.php
+++ core/modules/field/tests/src/Kernel/Migrate/d6/MigrateFieldFormatterSettingsTest.php
@@ -121,6 +121,7 @@ class MigrateFieldFormatterSettingsTest extends MigrateDrupal6TestBase {
       'url_plain' => TRUE,
       'rel' => '0',
       'target' => '0',
+      'external_target' => '',
     ];
     $component = $display->getComponent('field_test_link');
     $this->assertSame($expected, $component);
