41618f6f037dbfc1a9ffe7eb3185e161d34e9f3e
Lorenz Struyf <24558-lorenzs@users.noreply.drupalcode.org>
2024-01-29 10:56:51 -0500

    Issue #2931680 by joseph.olstad, mohit_aghera, kriboogh, ita08, lorenzs: Taxonomy term weight is not translatable, meaning you can't order term translations differently

--- core/modules/taxonomy/tests/src/Functional/TermTranslationTest.php
+++ core/modules/taxonomy/tests/src/Functional/TermTranslationTest.php
@@ -143,6 +143,46 @@ class TermTranslationTest extends TaxonomyTestBase {
   }
 
   /**
+   * Tests that weight field is translated and returns content as expected.
+   */
+  public function testTranslatedTermWeight() {
+    $this->drupalLogin($this->rootUser);
+
+    $i = 0;
+    foreach ($this->terms as $term) {
+      $term->setWeight($i)->save();
+      $i++;
+    }
+
+    /**
+     * Expected en tree:
+     * term[0] | weight: 0
+     * term[1] | weight: 1
+     * term[2] | weight: 2
+     */
+    $this->drupalget("/admin/structure/taxonomy/manage/{$this->vocabulary->id()}/overview");
+    foreach ($this->terms as $term) {
+      $this->assertSession()->fieldValueEquals("terms[tid:{$term->id()}:0][weight]", $term->getWeight());
+    }
+
+    /**
+     * Expected hu tree:
+     * term[0] | weight: 2
+     * term[1] | weight: 1
+     * term[2] | weight: 0
+     */
+    $i = 2;
+    foreach ($this->terms as $term) {
+      $term->getTranslation($this->translateToLangcode)->setWeight($i)->save();
+      $i--;
+    }
+    $this->drupalget("/{$this->translateToLangcode}/admin/structure/taxonomy/manage/{$this->vocabulary->id()}/overview");
+    foreach ($this->terms as $term) {
+      $this->assertSession()->fieldValueEquals("terms[tid:{$term->id()}:0][weight]", $term->getTranslation($this->translateToLangcode)->getWeight());
+    }
+  }
+
+  /**
    * Get the final (leaf) term in the hierarchy.
    *
    * @return \Drupal\taxonomy\Entity\Term
