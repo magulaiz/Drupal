a0d1e809e45b06ae06e93483ba1d1b787a67f91d
mondrake <mondrake.org@gmail.com>
2024-01-29 18:25:25 +0100

    fixes

--- core/tests/Drupal/Tests/PhpUnitCompatibilityTrait.php
+++ core/tests/Drupal/Tests/PhpUnitCompatibilityTrait.php
@@ -15,17 +15,13 @@ class_alias("Drupal\TestTools\PhpUnitCompatibility\PhpUnit" . RunnerVersion::get
 if (FALSE) {
 
   /**
-   * Makes Drupal's test API forward compatible with multiple versions of PHPUnit.
+   * Makes Drupal's test API forward compatible with future PHPUnit releases.
+   *
+   * This stub is present here to allow PHPStan identify the methods present
+   * in the version-bound trait.
    */
   trait PhpUnitCompatibilityTrait {
-
-    /**
-     * Get test name.
-     */
-    public function name(): string {
-      return $this->getName();
-    }
-
+    use Drupal\TestTools\PhpUnitCompatibility\PhpUnit9\TestCompatibilityTrait;
   }
 
 }
