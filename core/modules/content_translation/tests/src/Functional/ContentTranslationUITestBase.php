ecbfc7e874d6fedb0c173a01958c65fdd2a26542
Michael Strelan <mstrelan@gmail.com>
2024-01-30 12:47:42 +1000

    Fix ContentTranslationUITestBase

--- core/modules/content_translation/tests/src/Functional/ContentTranslationUITestBase.php
+++ core/modules/content_translation/tests/src/Functional/ContentTranslationUITestBase.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\content_translation\Functional;
 
-use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Entity\EntityChangedInterface;
 use Drupal\Core\Entity\EntityInterface;
@@ -578,7 +577,7 @@ abstract class ContentTranslationUITestBase extends ContentTranslationTestBase {
         $entity = $storage->load($this->entityId);
         $this->assertEquals(
           $entity->getChangedTimeAcrossTranslations(), $entity->getTranslation($langcode)->getChangedTime(),
-          new FormattableMarkup('Changed time for language %language is the latest change over all languages.', ['%language' => $language->getName()])
+          "Changed time for language {$language->getName()} is the latest change over all languages."
         );
       }
 
