ba093b1e59ea23a116ad5bdcc8d0c1274f25a7eb
Joris Vercammen <botchcake@gmail.com>
2024-01-31 09:43:04 +0100

    Add test coverage.

---
+++ core/tests/Drupal/Tests/Core/Config/TypeResolverTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\Core\Config;
+
+use Drupal\Core\Config\Schema\TypeResolver;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @covers \Drupal\Core\Config\Schema\TypeResolver
+ * @group config
+ */
+class TypeResolverTest extends UnitTestCase {
+
+  /**
+   * @dataProvider provideInvalidTypes
+   */
+  public function testInvalidType($name, $message, $data = []): void {
+    $this->expectException(\LogicException::class);
+    $this->expectExceptionMessage($message);
+    TypeResolver::resolveDynamicTypeName($name, $data);
+  }
+
+  public function provideInvalidTypes() {
+    return [
+      'invalid %variable' => [
+        '[foo.%bar.qux]',
+        'The only valid usages of a variable value with a % in it are %parent, %key, and %type in foo.%bar.qux',
+        ['foo' => 'foo'],
+      ],
+      'misspelling' => [
+        '[%paren.field_type]',
+        'The only valid usages of a variable value with a % in it are %parent, %key, and %type in %paren.field_type',
+      ],
+      'type without parent' => [
+        '[something.%type]',
+        '%type can only used when immediately proceeded by %parent in something.%type',
+        ['something' => 'something'],
+      ],
+    ];
+  }
+
+}
