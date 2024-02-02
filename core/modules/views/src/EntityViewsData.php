ed2f686a1cc0c85f3945f0558505a2eab09f010a
Scott Euser <scotteuser@gmail.com>
2024-02-02 12:30:37 +0000

    Add support for base fields

--- core/modules/views/src/EntityViewsData.php
+++ core/modules/views/src/EntityViewsData.php
@@ -453,6 +453,26 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
       $table_data[$schema_field_name]['entity field'] = $field_name;
       $first = FALSE;
     }
+
+    // Check if the field has a target entity entity.
+    $field_storage = $field_definition->getFieldStorageDefinition();
+    $target_entity_type_id = $field_storage->getSetting('target_type');
+    if ($target_entity_type_id) {
+      $target_entity_type = $this->entityTypeManager->getDefinition($target_entity_type_id);
+      if ($target_entity_type instanceof EntityTypeInterface) {
+        foreach ($table_data as $table_field_name => $table_field_data) {
+          if (isset($table_field_data['filter']) && $table_field_name != 'delta') {
+            // Create separate views data to allow use of the entity_reference
+            // filter. Numeric filter should still be available for use.
+            // @see core_field_views_data().
+            $entity_reference = $table_field_data;
+            $entity_reference['filter']['id'] = 'entity_reference';
+            $entity_reference['title'] = $entity_reference['title'] . ' ' . t('as a Reference filter');
+            $table_data[$table_field_name . '_reference'] = $entity_reference;
+          }
+        }
+      }
+    }
   }
 
   /**
