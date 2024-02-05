ee6f63a4560ae94f962137ebf5f03c4df23152af
Nicolas Beaucage <nicolas.beaucage@symetris.ca>
2024-02-05 13:47:41 -0500

    Issue #3418678: Remove PrimitiveBase::setValue() because it is identical to its parent implementation.

--- core/lib/Drupal/Core/TypedData/PrimitiveBase.php
+++ core/lib/Drupal/Core/TypedData/PrimitiveBase.php
@@ -21,15 +21,4 @@ abstract class PrimitiveBase extends TypedData implements PrimitiveInterface {
     return $this->value;
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setValue($value, $notify = TRUE) {
-    $this->value = $value;
-    // Notify the parent of any changes.
-    if ($notify && isset($this->parent)) {
-      $this->parent->onChange($this->name);
-    }
-  }
-
 }
