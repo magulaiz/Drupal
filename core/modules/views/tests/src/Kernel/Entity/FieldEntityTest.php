535eff0065bfa8071389535eac83691456cce11b
Alex Pott <alex.a.pott@googlemail.com>
2024-02-06 16:06:39 +0000

    Fix silly test. I'm not convinced the test is testing what it thinks it is.

--- core/modules/views/tests/src/Kernel/Entity/FieldEntityTest.php
+++ core/modules/views/tests/src/Kernel/Entity/FieldEntityTest.php
@@ -167,6 +167,10 @@ class FieldEntityTest extends ViewsKernelTestBase {
     // First row will be $node since the other is now deleted.
     $row = $view->result[0];
     $this->assertNull($view->field['nid']->getEntity($row));
+
+    // @todo This test should be a kernel test and swapping a service out this
+    //   way is not correct. Rebuild the container to fix it.
+    $this->rebuildContainer();
   }
 
 }
