377bb0620fa08ecdaf0ad512490593b3b914b89a
Alex Pott <alex.a.pott@googlemail.com>
2024-02-06 16:27:19 +0000

    Remove unused uses

--- core/modules/system/tests/src/Functional/Pager/PagerTest.php
+++ core/modules/system/tests/src/Functional/Pager/PagerTest.php
@@ -3,8 +3,6 @@
 namespace Drupal\Tests\system\Functional\Pager;
 
 use Behat\Mink\Element\NodeElement;
-use Drupal\Core\Database\Database;
-use Drupal\Core\Database\Query\PagerSelectExtender;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\system\Functional\Cache\AssertPageCacheContextsAndTagsTrait;
 
