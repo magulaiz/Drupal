aecd71629a6b249ba69eb1f4f96fa38eb3aacafc
Dave Long <dave@longwaveconsulting.com>
2024-02-07 23:44:31 +0000

    Remove factory services.

--- core/lib/Drupal/Core/Update/UpdateHookRegistry.php
+++ core/lib/Drupal/Core/Update/UpdateHookRegistry.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\Core\Update;
 
-use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 
 /**
  * Provides module updates versions handling.
@@ -52,14 +52,14 @@ class UpdateHookRegistry {
   /**
    * Constructs a new UpdateRegistry.
    *
-   * @param string[] $enabled_modules
-   *   A list of enabled modules.
-   * @param \Drupal\Core\KeyValueStore\KeyValueStoreInterface $key_value
+   * @param array $module_list
+   *   An associative array whose keys are the names of installed modules.
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $key_value_factory
    *   The key value store.
    */
-  public function __construct(array $enabled_modules, KeyValueStoreInterface $key_value) {
-    $this->enabledModules = $enabled_modules;
-    $this->keyValue = $key_value;
+  public function __construct(array $module_list, KeyValueFactoryInterface $key_value_factory) {
+    $this->enabledModules = array_keys($module_list);
+    $this->keyValue = $key_value_factory->get('system.schema');
   }
 
   /**
