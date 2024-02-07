b82e097292fc3bfbe9beb05416dbc869f59ffd65
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2024-02-07 23:18:38 +0000

    Update file ConditionTest.php

--- core/tests/Drupal/Tests/Core/Database/ConditionTest.php
+++ core/tests/Drupal/Tests/Core/Database/ConditionTest.php
@@ -169,7 +169,7 @@ class ConditionTest extends UnitTestCase {
 
     $condition = $connection->condition('AND');
     $condition->condition('name', 'value', $operator);
-    $this->expectError();
+    $this->expectWarning();
     $condition->compile($connection, $query_placeholder);
   }
 
