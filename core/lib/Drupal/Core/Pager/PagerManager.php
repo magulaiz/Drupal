89da0ea743978a5d168704816e7bfdae9030fdb1
Fabian Fiorotto <fabian_1945@yahoo.com.ar>
2024-02-08 17:27:10 -0300

    Issue #3420230 by fabianfiorotto: Handle numeric keys on view pagers.

--- core/lib/Drupal/Core/Pager/PagerManager.php
+++ core/lib/Drupal/Core/Pager/PagerManager.php
@@ -91,7 +91,7 @@ class PagerManager implements PagerManagerInterface {
     // from the current request. In case of collision, the parameters passed
     // into this function take precedence.
     if ($current_query = $this->pagerParams->getQueryParameters()) {
-      $query = array_merge($current_query, $query);
+      $query = $query + $current_query;
     }
     return $query;
   }
