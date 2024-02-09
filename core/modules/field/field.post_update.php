08b82fa5692ed660d90eeff1510630a5d303f371
Phéna Proxima <adam@phenaproxima.net>
2024-02-09 14:33:48 -0500

    Fix update path tests, hopefully

--- core/modules/field/field.post_update.php
+++ core/modules/field/field.post_update.php
@@ -6,23 +6,35 @@
  */
 
 use Drupal\Core\Config\Entity\ConfigEntityUpdater;
-use Drupal\Core\Field\FieldConfigBase;
+use Drupal\Core\Field\Entity\BaseFieldOverride;
+use Drupal\field\FieldConfigInterface;
 
 /**
  * Converts empty `description` on fields to NULL.
  */
-function field_post_update_set_empty_description_to_null(array &$sandbox): void {
-  $callback = function (FieldConfigBase $entity): bool {
-    if (trim($entity->getDescription()) === '') {
-      $entity->set('description', NULL);
-      return TRUE;
-    }
-    return FALSE;
-  };
-  /** @var \Drupal\Core\Config\Entity\ConfigEntityUpdater $updater */
-  $updater = \Drupal::classResolver(ConfigEntityUpdater::class);
-  $updater->update($sandbox, 'field_config', $callback);
-  $updater->update($sandbox, 'base_field_override', $callback);
+function field_post_update_set_field_config_empty_description_to_null(array &$sandbox): void {
+  \Drupal::classResolver(ConfigEntityUpdater::class)
+    ->update($sandbox, 'field_config', function (FieldConfigInterface $entity): bool {
+      if (trim($entity->getDescription()) === '') {
+        $entity->set('description', NULL);
+        return TRUE;
+      }
+      return FALSE;
+  });
+}
+
+/**
+ * Converts empty `description` on base field overrides to NULL.
+ */
+function field_post_update_set_base_field_override_empty_description_to_null(array &$sandbox): void {
+  \Drupal::classResolver(ConfigEntityUpdater::class)
+    ->update($sandbox, 'base_field_override', function (BaseFieldOverride $entity): bool {
+      if (trim($entity->getDescription()) === '') {
+        $entity->set('description', NULL);
+        return TRUE;
+      }
+      return FALSE;
+    });
 }
 
 /**
