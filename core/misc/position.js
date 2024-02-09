f67940355e5484f2b7924ce83558c968612c3e45
catch <catch@35733.no-reply.drupal.org>
2024-02-09 19:11:35 +0000

    Replace isWindow.

--- core/misc/position.js
+++ core/misc/position.js
@@ -54,7 +54,7 @@
         offset: { top: 0, left: 0 },
       };
     }
-    if ($.isWindow(raw)) {
+    if (raw && raw === raw.window) {
       return {
         width: elem.width(),
         height: elem.height(),
