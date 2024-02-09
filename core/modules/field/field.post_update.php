77d72d32226d7016a9e905bece4212a55d0e16c7
Phéna Proxima <adam@phenaproxima.net>
2024-02-09 13:11:59 -0500

    Add an update path for the field description

--- core/modules/field/field.post_update.php
+++ core/modules/field/field.post_update.php
@@ -5,6 +5,26 @@
  * Post update functions for Field module.
  */
 
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+use Drupal\Core\Field\FieldConfigBase;
+
+/**
+ * Converts empty `description` on fields to NULL.
+ */
+function field_post_update_set_empty_description_to_null(array &$sandbox): void {
+  $callback = function (FieldConfigBase $entity): bool {
+    if (trim($entity->getDescription()) === '') {
+      $entity->set('description', NULL);
+      return TRUE;
+    }
+    return FALSE;
+  };
+  /** @var \Drupal\Core\Config\Entity\ConfigEntityUpdater $updater */
+  $updater = \Drupal::classResolver(ConfigEntityUpdater::class);
+  $updater->update($sandbox, 'field_config', $callback);
+  $updater->update($sandbox, 'base_field_override', $callback);
+}
+
 /**
  * Implements hook_removed_post_updates().
  */
