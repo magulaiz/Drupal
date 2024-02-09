8778561ee03724a85e4b5cafdb1e86cb5504e2ad
Kunal Sachdev <kunal.sachdev@acquia.com>
2024-02-09 14:22:01 +0530

    fixes in password-unmask.js

--- core/misc/password-unmask.js
+++ core/misc/password-unmask.js
@@ -5,7 +5,7 @@
 (function (Drupal, once) {
   const hidePass = Drupal.t('Hide password');
   const showPass = Drupal.t('Show password');
-  const unmaskClickHandler = (element, trigger) =>  {
+  const unmaskClickHandler = (element, trigger) => {
     element.setAttribute(
       'type',
       element.getAttribute('type') === 'password' ? 'text' : 'password',
@@ -19,8 +19,12 @@
     const isPassword = element.getAttribute('type') === 'password';
     trigger.classList.toggle('action-link--icon-show', !isPassword)
     trigger.classList.toggle('action-link--icon-hide', isPassword)
-   const ariaDescription = element.getAttribute('type') === 'password' ? Drupal.t('Password is hidden') : Drupal.t('Password is visible');
-element.setAttribute('aria-description', ariaDescription);
+    const ariaDescription =
+      element.getAttribute('type') === 'password'
+        ? Drupal.t('Password is hidden')
+        : Drupal.t('Password is visible');
+    element.setAttribute('aria-description', ariaDescription);
+  };
   const unmaskButton = function unmaskButton(element) {
     const wrapperButton = Drupal.theme.buttonWrapper(element);
     element.insertAdjacentElement('afterend', wrapperButton);
