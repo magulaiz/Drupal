4f919082d185f4c0923bab7646e322bf8b2f3afb
catch <catch@35733.no-reply.drupal.org>
2024-02-09 10:37:27 +0000

    Try jquery instead.

--- core/tests/Drupal/KernelTests/Core/Asset/AttachedAssetsTest.php
+++ core/tests/Drupal/KernelTests/Core/Asset/AttachedAssetsTest.php
@@ -368,7 +368,8 @@ class AttachedAssetsTest extends KernelTestBase {
     // Verify that lighter JavaScript assets are rendered first.
     $this->assertLessThan(strpos($rendered_js, 'first.js'), strpos($rendered_js, 'lighter.js'));
     // Verify that a JavaScript file is rendered before jQuery.
-    $this->assertLessThan(strpos($rendered_js, 'misc/jquery.form.js'), strpos($rendered_js, 'before-jquery.js'));
+    $this->assertLessThan(strpos($rendered_js, 'core/assets/vendor/jquery/jquery.js
+'), strpos($rendered_js, 'before-jquery.js'));
   }
 
   /**
