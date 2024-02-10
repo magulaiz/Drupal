fd0956c6a6c52c1559bb249d142f6042b25a4344
Joris Vercammen <botchcake@gmail.com>
2024-02-10 21:55:38 +0100

    Add quotes as suggested by Wim

--- core/lib/Drupal/Core/Config/Schema/TypeResolver.php
+++ core/lib/Drupal/Core/Config/Schema/TypeResolver.php
@@ -89,10 +89,10 @@ class TypeResolver {
     // Process each value part, one at a time.
     while ($name = array_shift($parts)) {
       if (str_starts_with($name, '%') && !in_array($name, ['%parent', '%key', '%type'])) {
-        throw new \LogicException('The only valid usages of a variable value with a % in it are %parent, %key, and %type in ' . $expression);
+        throw new \LogicException('The only valid usages of a variable value with a `%` in it are `%parent`, `%key`, and `%type` in `' . $expression . '`');
       }
       if ($name === '%type' && $previous_name !== '%parent') {
-        throw new \LogicException('%type can only used when immediately proceeded by %parent in ' . $expression);
+        throw new \LogicException('`%type` can only used when immediately proceeded by `%parent` in `' . $expression .'`');
       }
       $previous_name = $name;
       if (!is_array($data) || !isset($data[$name])) {
--- core/tests/Drupal/Tests/Core/Config/TypeResolverTest.php
+++ core/tests/Drupal/Tests/Core/Config/TypeResolverTest.php
@@ -26,16 +26,16 @@ class TypeResolverTest extends UnitTestCase {
     return [
       'invalid %variable' => [
         '[foo.%bar.qux]',
-        'The only valid usages of a variable value with a % in it are %parent, %key, and %type in foo.%bar.qux',
+        'The only valid usages of a variable value with a `%` in it are `%parent`, `%key`, and `%type` in `foo.%bar.qux`',
         ['foo' => 'foo'],
       ],
       'misspelling' => [
         '[%paren.field_type]',
-        'The only valid usages of a variable value with a % in it are %parent, %key, and %type in %paren.field_type',
+        'The only valid usages of a variable value with a `%` in it are `%parent`, `%key`, and `%type` in `%paren.field_type`',
       ],
       'type without parent' => [
         '[something.%type]',
-        '%type can only used when immediately proceeded by %parent in something.%type',
+        '`%type` can only used when immediately proceeded by `%parent` in `something.%type`',
         ['something' => 'something'],
       ],
     ];
