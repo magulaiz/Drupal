d6940d963b0e6dba3735df7c1d33ed1769083eeb
dmitryb <dmitryb@imagexmedia.com>
2024-02-11 20:05:29 +0200

    3419621: Confirm that Claro prerender table callback is not executed for non-array class attribute.

--- core/tests/Drupal/KernelTests/Core/Theme/ClaroTableTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/ClaroTableTest.php
@@ -3,13 +3,15 @@
 namespace Drupal\KernelTests\Core\Theme;
 
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\claro\ClaroPreRender;
 
 /**
  * Tests Claro specific table functionality.
  *
  * @group Theme
  */
-class ClaroTableTest extends KernelTestBase {
+class ClaroTableTest extends KernelTestBase
+{
 
   /**
    * {@inheritdoc}
@@ -38,4 +40,34 @@ class ClaroTableTest extends KernelTestBase {
     $this->assertRaw('position-sticky');
   }
 
+  /**
+   * Confirm that Claro prerender table callback is not executed for non-array class attribute.
+   */
+  public function testThemeTablePositionStickyPreRender() {
+    // Enable the Claro theme.
+    \Drupal::service('theme_installer')->install(['claro']);
+    $this->config('system.theme')->set('default', 'claro')->save();
+    $header = ['one', 'two', 'three'];
+    $rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    $table = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#sticky' => TRUE,
+      '#attributes' => [
+        'class' => 'class'
+      ],
+      '#pre_render' => [
+        [ClaroPreRender::class, 'tablePositionSticky'],
+      ],
+    ];
+
+    try {
+      $renderedTable = \Drupal::service('renderer')->renderRoot($table);
+      // Confirm that table is rendered.
+      $this->assertStringContainsString('class="class"', $renderedTable);
+    } catch (\Error $e) {
+      $this->fail('PHP Error was thrown.');
+    }
+  }
 }
