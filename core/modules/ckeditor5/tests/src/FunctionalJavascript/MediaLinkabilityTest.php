44937271d82e31fdfc2826a1c1ec57b8a47ac074
Ben Mullins <benm@umich.edu>
2024-02-12 17:11:00 -0500

    use node id to partially populate xpath query

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaLinkabilityTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaLinkabilityTest.php
@@ -367,7 +367,7 @@ class MediaLinkabilityTest extends MediaTestBase {
     $this->assertSame('Zoo Party (Content - blog)', $preview_button->getText());
 
     $xpath = new \DOMXPath($this->getEditorDataAsDom());
-    $query = sprintf('//a[@href="entity:%s" and @data-entity-uuid="%s" and @data-entity-type="node" and @data-entity-metadata]/drupal-media', substr($content_to_add->toUrl('canonical')->toString(), 1), $content_to_add_uuid);
+    $query = sprintf('//a[@href="entity:node/%s" and @data-entity-uuid="%s" and @data-entity-type="node" and @data-entity-metadata]/drupal-media', $content_to_add->id(), $content_to_add_uuid);
     $this->assertCount(1, $xpath->query($query), "Search for $query");
 
     $page->pressButton('Save');
