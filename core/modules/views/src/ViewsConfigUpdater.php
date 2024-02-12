7095f630a5de02d2e66bb18ff68b1dde04fb8a41
Ilias Dimopoulos <idimopoulos@hotmail.com>
2024-02-12 21:39:41 +0200

    10.2.x version of #2381293.

--- core/modules/views/src/ViewsConfigUpdater.php
+++ core/modules/views/src/ViewsConfigUpdater.php
@@ -10,6 +10,7 @@ use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\TimestampFormatter;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\views\Plugin\views\field\BulkForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -524,6 +525,47 @@ class ViewsConfigUpdater implements ContainerInjectionInterface {
       $changed = TRUE;
     }
     return $changed;
+  }
+
+  /**
+   * Updates the bulk form fields by adding the actions order configuration.
+   *
+   * @param \Drupal\views\ViewEntityInterface $view
+   *   The View being updated.
+   *
+   * @return bool
+   *   Whether the handler was updated.
+   */
+  public function needsBulkFormActionsOrderUpdate(ViewEntityInterface $view): bool {
+    return $this->processDisplayHandlers($view, FALSE, function (array &$handler, string $handler_type): bool {
+      return $this->processBulkFormActionsOrderUpdate($handler, $handler_type);
+    });
+  }
+
+  /**
+   * Processes the bulk form fields by adding the actions order configuration.
+   *
+   * @param array $handler
+   *   A display handler.
+   * @param string $handler_type
+   *   The handler type.
+   *
+   * @return bool
+   *   Whether the handler was updated.
+   */
+  protected function processBulkFormActionsOrderUpdate(array &$handler, string $handler_type): bool {
+    if ($handler_type === 'field' && !isset($handler['actions_order']) && isset($handler['plugin_id'])) {
+      /** @var \Drupal\views\Plugin\ViewsPluginManager $plugin_manager */
+      $plugin_manager = \Drupal::service('plugin.manager.views.field');
+      if ($plugin_manager->hasDefinition($handler['plugin_id'])) {
+        $definition = $plugin_manager->getDefinition($handler['plugin_id']);
+        if (is_subclass_of($definition['class'], BulkForm::class)) {
+          $handler['actions_order'] = [];
+          return TRUE;
+        }
+      }
+    }
+    return FALSE;
   }
 
 }
--- core/modules/views/views.post_update.php
+++ core/modules/views/views.post_update.php
@@ -137,3 +137,14 @@ function views_post_update_taxonomy_filter_user_context(?array &$sandbox = NULL)
     return $view_config_updater->needsTaxonomyTermFilterUpdate($view);
   });
 }
+
+/**
+ * Add the actions order option to all bulk form field configurations.
+ */
+function views_post_update_bulk_form_action_order(?array &$sandbox = NULL): void {
+  /** @var \Drupal\views\ViewsConfigUpdater $view_config_updater */
+  $view_config_updater = \Drupal::classResolver(ViewsConfigUpdater::class);
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'view', function (ViewEntityInterface $view) use ($view_config_updater): bool {
+    return $view_config_updater->needsBulkFormActionsOrderUpdate($view);
+  });
+}
