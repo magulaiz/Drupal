089c19a218bb29bb06523878c66b327600aaa84b
Siju Mathew <65250-sijumpk@users.noreply.drupalcode.org>
2024-02-13 12:30:20 +0000

    Adding test only to 11.x branch, without fix

--- core/modules/media_library/src/Form/AddFormBase.php
+++ core/modules/media_library/src/Form/AddFormBase.php
@@ -675,12 +675,6 @@ abstract class AddFormBase extends FormBase implements BaseFormIdInterface, Trus
     foreach ($this->getAddedMediaItems($form_state) as $delta => $media) {
       EntityFormDisplay::collectRenderDisplay($media, 'media_library')
         ->extractFormValues($media, $form['media'][$delta]['fields'], $form_state);
-      foreach ($media->get($this->getSourceFieldName($media->bundle->entity)) as $file_item) {
-        // Checking whether display is NULL, if null set it as TRUE
-        if ($file_item->get('display')->getValue() == NULL) {
-          $file_item->set('display', TRUE);
-        }
-      }
       $this->prepareMediaEntityForSave($media);
       $media->save();
     }
