1404cb31b32c46714b29a9fc7bbe34bd3b3b1d05
Siju Mathew <65250-sijumpk@users.noreply.drupalcode.org>
2024-02-13 14:03:12 +0000

    adding fix and test case to new branch

--- core/modules/views/tests/src/Functional/Plugin/DisabledDisplayTest.php
+++ core/modules/views/tests/src/Functional/Plugin/DisabledDisplayTest.php
@@ -88,6 +88,11 @@ class DisabledDisplayTest extends ViewTestBase {
     $this->drupalGet('test-disabled-display-2');
     $this->assertSession()->elementTextEquals('xpath', '//h1[@class="page-title"]', 'test_disabled_display');
 
+    // Confirm that the block is available in the block administration UI.
+    $this->drupalGet('admin/structure/block/list/' . $this->config('system.theme')->get('default'));
+    $this->clickLink('Place block');
+    $this->assertSession()->pageTextContains('test_disabled_display');
+
     // Disable each disabled display and save the view.
     foreach ($display_ids as $display_id) {
       $view->getExecutable()->displayHandlers->get($display_id)->setOption('enabled', FALSE);
@@ -105,6 +110,11 @@ class DisabledDisplayTest extends ViewTestBase {
     // Check that the page_2 display is now disabled again.
     $this->drupalGet('test-disabled-display-2');
     $this->assertSession()->statusCodeEquals(404);
+
+    // Confirm that the block is not available in the block administration UI.
+    $this->drupalGet('admin/structure/block/list/' . $this->config('system.theme')->get('default'));
+    $this->clickLink('Place block');
+    $this->assertSession()->pageTextNotContains('test_disabled_display');
   }
 
 }
