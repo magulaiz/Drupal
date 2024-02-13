775568cf791aec9328ed048e971832e6fc5532bd
ehsann95 <psychoahsan@gmail.com>
2024-02-13 16:40:30 +0530

    refactor closeset function.

--- core/modules/system/tests/modules/js_message_test/js/js_message_test.js
+++ core/modules/system/tests/modules/js_message_test/js/js_message_test.js
@@ -42,10 +42,11 @@
         (e) => {
           const $target = $(e.currentTarget);
           const type = $target.attr('data-type');
+          const closestEl = e.currentTarget.closest(
+            '[data-drupal-messages-area]',
+          );
           const area =
-            $target
-              .closest('[data-drupal-messages-area]')
-              .attr('data-drupal-messages-area') || 'default';
+            $(closestEl).attr('data-drupal-messages-area') || 'default';
           const message = messageObjects[area].zone;
           const action = $target.attr('data-action');
 
--- core/modules/text/text.js
+++ core/modules/text/text.js
@@ -15,7 +15,8 @@
   Drupal.behaviors.textSummary = {
     attach(context, settings) {
       once('text-summary', '.js-text-summary', context).forEach((summary) => {
-        const $widget = $(summary).closest('.js-text-format-wrapper');
+        const widgetEl = summary.closest('.js-text-format-wrapper');
+        const $widget = $(widgetEl);
 
         const $summary = $widget.find('.js-text-summary-wrapper');
         const $summaryLabel = $summary.find('label').eq(0);
