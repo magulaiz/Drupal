ddde60784448d604e65dca5fe566fcc7138cec6c
quietone <quietone@2572884.no-reply.drupal.org>
2024-02-14 00:55:37 +1300

    install users_data in test

--- core/modules/user/tests/src/Kernel/UserMailNotifyTest.php
+++ core/modules/user/tests/src/Kernel/UserMailNotifyTest.php
@@ -29,6 +29,14 @@ class UserMailNotifyTest extends EntityKernelTestBase {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+    $this->installSchema('user', ['users_data']);
+  }
+
+  /**
    * Data provider for user mail testing.
    *
    * @return array
