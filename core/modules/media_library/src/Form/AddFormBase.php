705ec352e29a810f2f1325a95c78e58ee191ee5d
Siju Mathew <65250-sijumpk@users.noreply.drupalcode.org>
2024-02-13 12:57:32 +0000

    adding corrected fix

--- core/modules/media_library/src/Form/AddFormBase.php
+++ core/modules/media_library/src/Form/AddFormBase.php
@@ -676,8 +676,9 @@ abstract class AddFormBase extends FormBase implements BaseFormIdInterface, Trus
       EntityFormDisplay::collectRenderDisplay($media, 'media_library')
         ->extractFormValues($media, $form['media'][$delta]['fields'], $form_state);
       foreach ($media->get($this->getSourceFieldName($media->bundle->entity)) as $file_item) {
-        // Checking whether display is NULL, if null set it as TRUE
-        if ($file_item->get('display')->getValue() == NULL) {
+        // Checking for display property, and if null set it as TRUE
+        if (!empty($file_item->getProperties()['display'])
+          && $file_item->get('display')->getValue() == NULL) {
           $file_item->set('display', TRUE);
         }
       }
