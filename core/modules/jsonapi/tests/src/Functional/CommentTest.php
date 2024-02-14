4ce2bdefa33e48500aec9fb125e93ce0d394ab55
Michael Strelan <mstrelan@gmail.com>
2024-02-15 09:12:41 +1000

    Fix \Drupal\Tests\jsonapi\Functional\CommentTest::testPatchIndividual

--- core/modules/jsonapi/tests/src/Functional/CommentTest.php
+++ core/modules/jsonapi/tests/src/Functional/CommentTest.php
@@ -464,7 +464,7 @@ class CommentTest extends ResourceTestBase {
   /**
    * {@inheritdoc}
    */
-  public function testPatchIndividual() {
+  public function testPatchIndividual(): void {
     // Ensure ::getModifiedEntityForPatchTesting() can pick an alternative value
     // for the 'entity_id' field.
     EntityTest::create([
@@ -472,7 +472,7 @@ class CommentTest extends ResourceTestBase {
       'type' => 'bar',
     ])->save();
 
-    return parent::testPatchIndividual();
+    parent::testPatchIndividual();
   }
 
 }
