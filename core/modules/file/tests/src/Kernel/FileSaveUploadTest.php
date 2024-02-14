7875e733fbcd47ca2dfd638f4f6d25359abc7171
Kim Pepper <kim@previousnext.com.au>
2024-02-14 12:17:37 +1100

    Fix logic in preparing extension validation

--- core/modules/file/tests/src/Kernel/FileSaveUploadTest.php
+++ core/modules/file/tests/src/Kernel/FileSaveUploadTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\file\Kernel;
 
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,14 +30,14 @@ class FileSaveUploadTest extends KernelTestBase {
    * {@inheritdoc}
    */
   protected function setUp(): void {
-    \file_put_contents('test.txt', 'test');
+    \file_put_contents('test.bbb', 'test');
 
     parent::setUp();
     $request = new Request();
     $request->files->set('files', [
       'file' => new UploadedFile(
-        path: 'test.txt',
-        originalName: 'test.txt',
+        path: 'test.bbb',
+        originalName: 'test.bbb',
         mimeType: 'text/plain',
         error: \UPLOAD_ERR_OK,
         test: TRUE
@@ -53,9 +54,17 @@ class FileSaveUploadTest extends KernelTestBase {
    * Tests file_save_upload() with empty extensions.
    */
   public function testFileSaveUploadEmptyExtensions(): void {
+    // Allow all extensions.
     $validators = ['file_validate_extensions' => ''];
-    $file = file_save_upload('file', $validators, 'public://');
-    $this->assertCount(1, $file);
+    $this->expectDeprecation('\'file_validate_extensions\' is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use the \'FileExtension\' constraint instead. See https://www.drupal.org/node/3363700');
+    $files = file_save_upload('file', $validators);
+    $this->assertCount(1, $files);
+    $file = $files[0];
+    // @todo work out why move_uploaded_file() is failing.
+    $this->assertFalse($file);
+    $messages = \Drupal::messenger()->messagesByType(MessengerInterface::TYPE_ERROR);
+    $this->assertNotEmpty($messages);
+    $this->assertEquals('File upload error. Could not move uploaded file.', $messages[0]);
   }
 
 }
