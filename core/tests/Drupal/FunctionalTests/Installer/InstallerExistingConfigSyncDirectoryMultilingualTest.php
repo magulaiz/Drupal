ea91e3b7a7294a470b7bd553eed915e75220d696
Michael Strelan <mstrelan@gmail.com>
2024-02-15 09:21:40 +1000

    Fix \Drupal\FunctionalTests\Installer\InstallerExistingConfigSyncDirectoryMultilingualTest::setUpProfile

--- core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigSyncDirectoryMultilingualTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigSyncDirectoryMultilingualTest.php
@@ -31,11 +31,11 @@ class InstallerExistingConfigSyncDirectoryMultilingualTest extends InstallerExis
   /**
    * Installer step: Select installation profile.
    */
-  protected function setUpProfile() {
+  protected function setUpProfile(): void {
     // Ensure the site name 'Multilingual' appears as expected in the 'Use
     // existing configuration' radio description.
     $this->assertSession()->pageTextContains('Install Multilingual using existing configuration.');
-    return parent::setUpProfile();
+    parent::setUpProfile();
   }
 
   /**
