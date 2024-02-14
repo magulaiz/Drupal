e26f4eea8f6303dac0d5e90bb1ce2d0a44fdcabf
mondrake <mondrake.org@gmail.com>
2024-02-14 19:57:32 +0100

    fix

--- core/tests/Drupal/BuildTests/Composer/ComposerValidateTest.php
+++ core/tests/Drupal/BuildTests/Composer/ComposerValidateTest.php
@@ -16,7 +16,7 @@ class ComposerValidateTest extends BuildTestBase {
 
   public static function provideComposerJson() {
     $data = [];
-    $composer_json_finder = $this->getComposerJsonFinder(self::getDrupalRoot());
+    $composer_json_finder = self::getComposerJsonFinder(self::getDrupalRoot());
     foreach ($composer_json_finder->getIterator() as $composer_json) {
       $data[] = [$composer_json->getPathname()];
     }
