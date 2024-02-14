786b2773e411f359945252c4efdc56e52dfe9abc
ehsann95 <psychoahsan@gmail.com>
2024-02-14 17:25:18 +0530

    refactor closest function.

--- core/modules/filter/filter.js
+++ core/modules/filter/filter.js
@@ -26,15 +26,26 @@
           .show();
       }
 
-      $(once('filter-guidelines', '.js-filter-guidelines', context))
-        .find(':header')
-        .hide()
-        .closest('.js-filter-wrapper')
-        .find('select.js-filter-list')
-        .on('change.filterGuidelines', updateFilterGuidelines)
-        // Need to trigger the namespaced event to avoid triggering formUpdated
-        // when initializing the select.
-        .trigger('change.filterGuidelines');
+      // Define the closest function for finding the closest ancestor with a given selector
+      function closest(element, selector) {
+        while (element && !element.matches(selector)) {
+          element = element.parentNode;
+        }
+        return element;
+      }
+
+      const contextEl = document.querySelector('.js-filter-guidelines');
+      const closestWrapper = closest(contextEl, '.js-filter-wrapper');
+      if (closestWrapper) {
+        const $context = $(contextEl);
+        const selectElement = $(closestWrapper).find('select.js-filter-list');
+        $context.find(':header').hide();
+        selectElement
+          .on('change.filterGuidelines', updateFilterGuidelines)
+          // Need to trigger the namespaced event to avoid triggering formUpdated
+          // when initializing the select.
+          .trigger('change.filterGuidelines');
+      }
     },
   };
 })(jQuery, Drupal);
