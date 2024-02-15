367c9b2ed39c0943fb8fbbb8b8a58f24134b0a90
Amber Himes Matz <agentolivia@gmail.com>
2024-02-15 10:58:30 -0800

    Converts test plugins to use attribute. Removes trailing comma from attribute in HelpTopicSection plugin.

--- core/modules/help/src/Plugin/HelpSection/HelpTopicSection.php
+++ core/modules/help/src/Plugin/HelpSection/HelpTopicSection.php
@@ -27,7 +27,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
   id: 'help_topics',
   title: new TranslatableMarkup('Topics'),
   description: new TranslatableMarkup('Topics can be provided by modules or themes. Top-level help topics on your site:'),
-  weight: -10,
+  weight: -10
 )]
 class HelpTopicSection extends HelpSectionPluginBase implements ContainerFactoryPluginInterface, SearchableHelpInterface {
 
