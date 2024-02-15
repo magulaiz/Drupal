2e2a425f184baa9cb03262f70ca24004ce0194cc
catch <catch@35733.no-reply.drupal.org>
2024-02-15 18:15:20 +0000

    No less cache gets.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
@@ -37,7 +37,7 @@ class OpenTelemetryAuthenticatedPerformanceTest extends PerformanceTestBase {
     }, 'authenticatedFrontPage');
     $this->assertGreaterThanOrEqual(14, $performance_data->getQueryCount());
     $this->assertLessThanOrEqual(16, $performance_data->getQueryCount());
-    $this->assertSame(44, $performance_data->getCacheGetCount());
+    $this->assertSame(45, $performance_data->getCacheGetCount());
     $this->assertSame(0, $performance_data->getCacheSetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
   }
