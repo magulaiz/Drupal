680c628828377af4816fdb7c2b318604e8724b62
catch <catch@35733.no-reply.drupal.org>
2024-02-16 12:40:43 +0000

    Add the CacheTagsChecksumDecorator class.

---
+++ core/modules/system/tests/modules/performance_test/src/Cache/CacheTagsChecksumDecorator.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\performance_test\Cache;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Cache\CacheTagsChecksumInterface;
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
+use Drupal\performance_test\PerformanceDataCollector;
+
+/**
+ * Wraps an existing cache tags checksum invalidator to track calls separately.
+ */
+class CacheTagsChecksumDecorator implements CacheTagsChecksumInterface, CacheTagsInvalidatorInterface {
+
+  public function __construct(protected readonly CacheTagsChecksumInterface $checksumInvalidator, protected readonly PerformanceDataCollector $performanceDataCollector) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCurrentChecksum(array $tags) {
+    $start = microtime(TRUE);
+    $return = $this->checksumInvalidator->getCurrentChecksum($tags);
+    $stop = microtime(TRUE);
+    $this->logCacheTagOperation($tags, $start, $stop, 'getCurrentChecksum');
+    return $return;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isValid($checksum, array $tags) {
+    $start = microtime(TRUE);
+    $return = $this->checksumInvalidator->isValid($checksum, $tags);
+    $stop = microtime(TRUE);
+    $this->logCacheTagOperation($tags, $start, $stop, 'isValid');
+    return $return;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function invalidateTags(array $tags) {
+    $start = microtime(TRUE);
+    $return = $this->checksumInvalidator->invalidateTags($tags);
+    $stop = microtime(TRUE);
+    $this->logCacheTagOperation($tags, $start, $stop, 'invalidateTags');
+    return $return;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function reset() {
+    $this->checksumInvalidator->reset();
+  }
+
+  /**
+   * Logs a cache tag operation.
+   *
+   * @param array $tags
+   *   The cache tags.
+   * @param float $start
+   *   The start microtime.
+   * @param float $stop
+   *   The stop microtime.
+   * @param string $operation
+   *   The type of operation being logged.
+   *
+   * @return void
+   */
+  protected function logCacheTagOperation(array $tags, float $start, float $stop, string $operation): void {
+    $this->performanceDataCollector->addCacheTagOperation([
+      'operation' => $operation,
+      'tags' => implode(', ', $tags),
+      'start' => $start,
+      'stop' => $stop,
+    ]);
+  }
+
+}
