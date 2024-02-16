e62ad911b9e15b2eeff47b23295c41fb764de33d
catch <catch@35733.no-reply.drupal.org>
2024-02-16 18:15:02 +0000

    Increase upper range to 146.

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -59,7 +59,7 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertSame(137, $performance_data->getCacheGetCount());
     $this->assertSame(47, $performance_data->getCacheSetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
-    $this->assertCountBetween(143, 145, $performance_data->getCacheTagChecksumCount());
+    $this->assertCountBetween(143, 146, $performance_data->getCacheTagChecksumCount());
     $this->assertSame(177, $performance_data->getCacheTagIsValidCount());
     $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
 
