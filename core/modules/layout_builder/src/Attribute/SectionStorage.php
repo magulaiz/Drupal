5f2d0db4c9dc7136452187fd7970d40eaf17e981
Daniel Veza <daniel.veza@previousnext.com.au>
2024-02-16 10:21:56 +1000

    Add in the annotation param documentation to the attribute

--- core/modules/layout_builder/src/Attribute/SectionStorage.php
+++ core/modules/layout_builder/src/Attribute/SectionStorage.php
@@ -18,6 +18,26 @@ class SectionStorage extends Plugin {
 
   /**
    * Constructs a SectionStorage attribute.
+   *
+   * @param string $id
+   *   The plugin ID.
+   * @param int $weight
+   *   The plugin weight, optional.
+   *   When an entity with layout is rendered, section storage plugins are
+   *   checked, in order of their weight, to determine which one should be used
+   *   to render the layout.
+   * @param array $context_definitions
+   *   Any required context definitions, optional.
+   *   When an entity with layout is rendered, all section storage plugins which
+   *   match a particular set of contexts are checked, in order of their weight,
+   *   to determine which plugin should be used to render the layout.
+   *   @see \Drupal\layout_builder\SectionStorage\SectionStorageManagerInterface::findByContext()
+   * @param bool $handles_permission_check
+   *   Indicates that this section storage handles its own permission checking.
+   *   If FALSE, the 'configure any layout' permission will be required during
+   *   routing access. If TRUE, Layout Builder will not enforce any access
+   *   restrictions for the storage, so the section storage's implementation of
+   *   access() must perform the access checking itself.
    */
   public function __construct(
     public readonly string $id,
