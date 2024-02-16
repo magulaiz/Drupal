9762d00e0e20af466e60b9289ca58dc5f9a43736
catch <catch@35733.no-reply.drupal.org>
2024-02-16 19:20:06 +0000

    Add a range to the assertion.

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -87,7 +87,7 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertSame(81, $performance_data->getCacheGetCount());
     $this->assertSame(16, $performance_data->getCacheSetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
-    $this->assertSame(73, $performance_data->getCacheTagChecksumCount());
+    $this->assertCountBetween(73, 74, $performance_data->getCacheTagChecksumCount());
     $this->assertSame(129, $performance_data->getCacheTagIsValidCount());
     $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
   }
