449dd27b1d81fbd5a0f7d5a83757f041314357fb
Daniel Veza <daniel.veza@previousnext.com.au>
2024-02-16 12:13:02 +1000

    [#3385746] - Add fields to the hidden region when Layout Builder is enabled

--- core/modules/layout_builder/src/Entity/LayoutBuilderEntityViewDisplay.php
+++ core/modules/layout_builder/src/Entity/LayoutBuilderEntityViewDisplay.php
@@ -150,6 +150,10 @@ class LayoutBuilderEntityViewDisplay extends BaseEntityViewDisplay implements La
         uasort($components, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
         foreach ($components as $name => $component) {
           $this->setComponent($name, $component);
+          // Move all fields to the hidden region.
+          $this->content[$name]['region'] = 'hidden';
+          $this->hidden[$name] = $name;
+          unset($this->content[$name]);
         }
       }
       else {
