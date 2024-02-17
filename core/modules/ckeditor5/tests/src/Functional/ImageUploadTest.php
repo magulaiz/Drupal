ab62d830434420ef7c6718b1143b2e77f2cfe1f5
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-02-18 01:41:19 +0200

    Revert "add @group legacy to class"
    This reverts commit cea9a65ab21ee054f08f13c69ae25c51aa86610e.

--- core/modules/ckeditor5/tests/src/Functional/ImageUploadTest.php
+++ core/modules/ckeditor5/tests/src/Functional/ImageUploadTest.php
@@ -16,7 +16,6 @@ use GuzzleHttp\RequestOptions;
  * Test image upload.
  *
  * @group ckeditor5
- * @group legacy
  * @internal
  */
 class ImageUploadTest extends BrowserTestBase {
@@ -58,6 +57,8 @@ class ImageUploadTest extends BrowserTestBase {
 
   /**
    * Tests using the file upload route with a disallowed extension.
+   *
+   * @group legacy
    */
   public function testUploadFileExtension() {
     $this->expectDeprecation('Specifying roles in text formats is deprecated in drupal:10.3.0 and will not be supported starting in drupal:11.0.0. See https://www.drupal.org/node/3168851');
