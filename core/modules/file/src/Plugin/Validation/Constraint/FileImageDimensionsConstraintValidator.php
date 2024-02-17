16f8a8f813d5b1ef76382884030ea1083c56cbdb
Roger Codina <34447-rcodina@users.noreply.drupalcode.org>
2024-02-17 13:05:54 +0100

    Replace the use of switch with if

--- core/modules/file/src/Plugin/Validation/Constraint/FileImageDimensionsConstraintValidator.php
+++ core/modules/file/src/Plugin/Validation/Constraint/FileImageDimensionsConstraintValidator.php
@@ -63,50 +63,47 @@ class FileImageDimensionsConstraintValidator extends BaseFileConstraintValidator
       // Check that it is smaller than the given dimensions.
       [$width, $height] = explode('x', $maxDimensions);
       if ($image->getWidth() > $width || $image->getHeight() > $height) {
-        switch ($resizePolicy) {
-          case 'reject_larger_images_with_error':
-            $this->context->addViolation($constraint->messageImageTooLarge,
-              [
-                '%dimensions' => $maxDimensions,
-                '%width' => $image->getWidth(),
-                '%height' => $image->getHeight(),
-              ]);
-            break;
-
-          default:
-            // Try to resize the image to fit the dimensions.
-            if ($image->scale($width, $height)) {
-              $scaling = TRUE;
-              $image->save();
-              if (!empty($width) && !empty($height)) {
-                $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed dimensions of %dimensions pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
-                  [
-                    '%dimensions' => $maxDimensions,
-                    '%new_width' => $image->getWidth(),
-                    '%new_height' => $image->getHeight(),
-                  ]));
-              }
-              elseif (empty($width)) {
-                $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed height of %height pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
-                  [
-                    '%height' => $height,
-                    '%new_width' => $image->getWidth(),
-                    '%new_height' => $image->getHeight(),
-                  ]));
-              }
-              elseif (empty($height)) {
-                $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed width of %width pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
-                  [
-                    '%width' => $width,
-                    '%new_width' => $image->getWidth(),
-                    '%new_height' => $image->getHeight(),
-                  ]));
-              }
+        if ($resizePolicy == 'reject_larger_images_with_error') {
+          $this->context->addViolation($constraint->messageImageTooLarge,
+            [
+              '%dimensions' => $maxDimensions,
+              '%width' => $image->getWidth(),
+              '%height' => $image->getHeight(),
+            ]);
+        }
+        else {
+          // Try to resize the image to fit the dimensions.
+          if ($image->scale($width, $height)) {
+            $scaling = TRUE;
+            $image->save();
+            if (!empty($width) && !empty($height)) {
+              $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed dimensions of %dimensions pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
+                [
+                  '%dimensions' => $maxDimensions,
+                  '%new_width' => $image->getWidth(),
+                  '%new_height' => $image->getHeight(),
+                ]));
             }
-            else {
-              $this->context->addViolation($constraint->messageResizeFailed);
+            elseif (empty($width)) {
+              $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed height of %height pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
+                [
+                  '%height' => $height,
+                  '%new_width' => $image->getWidth(),
+                  '%new_height' => $image->getHeight(),
+                ]));
             }
-            break;
+            elseif (empty($height)) {
+              $this->messenger->addStatus($this->t('The image was resized to fit within the maximum allowed width of %width pixels. The new dimensions of the resized image are %new_widthx%new_height pixels.',
+                [
+                  '%width' => $width,
+                  '%new_width' => $image->getWidth(),
+                  '%new_height' => $image->getHeight(),
+                ]));
+            }
+          }
+          else {
+            $this->context->addViolation($constraint->messageResizeFailed);
+          }
         }
       }
     }
