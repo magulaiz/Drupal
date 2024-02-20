7b18a3a81fcf8a66049c4d4264ab6d799b279074
ehsann95 <psychoahsan@gmail.com>
2024-02-20 15:51:22 +0530

    revert tabledrag.js

--- core/themes/claro/js/tabledrag.js
+++ core/themes/claro/js/tabledrag.js
@@ -75,35 +75,15 @@
        */
       const createItemWrapBoundaries = (row) => {
         const $row = $(row);
-
-        function generateElements(html) {
-          const el = document.createElement('div');
-          el.innerHTML = html.trim();
-          return el.firstChild;
-        }
-
-        function wrapItem(el) {
-          const handle = el.querySelector('.tabledrag-handle');
-
-          const contentWrapper = generateElements(
-            Drupal.theme('tableDragCellContentWrapper'),
+        const $firstCell = $row
+          .find('td:first-of-type')
+          .eq(0)
+          .wrapInner(Drupal.theme('tableDragCellContentWrapper'))
+          .wrapInner(
+            $(Drupal.theme('tableDragCellItemsWrapper')).addClass(
+              'js-tabledrag-cell-content',
+            ),
           );
-          const itemsWrapper = generateElements(
-            Drupal.theme('tableDragCellItemsWrapper'),
-          );
-          itemsWrapper.classList.add('js-tabledrag-cell-content');
-          contentWrapper.innerHTML = el.innerHTML;
-          itemsWrapper.append(contentWrapper);
-          const r = itemsWrapper.querySelector('.tabledrag-handle');
-          r.remove();
-          el.innerHTML = '';
-          itemsWrapper.prepend(handle);
-          el.appendChild(itemsWrapper);
-        }
-
-        const $firstCell = $row.find('td:first-of-type').eq(0);
-        wrapItem($row.find('td:first-of-type').eq(0)[0]);
-
         const $targetElem = $firstCell.find('.js-tabledrag-cell-content');
 
         // Move handle into the '.js-tabledrag-cell-content' target.
