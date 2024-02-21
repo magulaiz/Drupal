44bb353c52163c6836586cc5ccc42ad4c1f741e6
Guy Elsmore-Paddock <guy@inveniem.com>
2024-02-21 17:16:48 -0500

    2796341 - MRF: Extract Logic for Obtaining ER Handle and Add TODO for DI
    After applying the patch for this issue, there were multiple places
    where the entity-reference-selection handler was being obtained with
    identical logic. The MR feedback was that these calls should be updated
    to use DI, but that's beyond the scope of this issue because it would
    require reworking how the form element plug-in gets instantiated.
    
    Instead, I've moved the logic for obtaining the handler to its own
    utility method and added a TODO to address the lack of DI as part of

--- core/lib/Drupal/Core/Entity/Element/EntityAutocomplete.php
+++ core/lib/Drupal/Core/Entity/Element/EntityAutocomplete.php
@@ -6,6 +6,7 @@ use Drupal\Component\Utility\Crypt;
 use Drupal\Component\Utility\Tags;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface;
+use Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManagerInterface;
 use Drupal\Core\Entity\EntityReferenceSelection\SelectionWithAutocompleteLabelsInterface;
 use Drupal\Core\Entity\EntityReferenceSelection\SelectionWithAutocreateInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -206,12 +207,7 @@ class EntityAutocomplete extends Textfield {
 
     // Check the value for emptiness, but allow the use of (string) "0".
     if (!empty($element['#value']) || (is_string($element['#value']) && strlen($element['#value']))) {
-      $options = $element['#selection_settings'] + [
-        'target_type' => $element['#target_type'],
-        'handler' => $element['#selection_handler'],
-      ];
-      /** @var \Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface $handler */
-      $handler = \Drupal::service('plugin.manager.entity_reference_selection')->getInstance($options);
+      $handler = self::getSelectionHandlerForElement($element);
       $autocreate = (bool) $element['#autocreate'] && $handler instanceof SelectionWithAutocreateInterface;
 
       // GET forms might pass the validated data around on the next request, in
@@ -380,13 +376,7 @@ class EntityAutocomplete extends Textfield {
 
     // Check to see if the Selection handler can supply entity labels.
     if ($element != NULL) {
-      $options = $element['#selection_settings'] + [
-          'target_type' => $element['#target_type'],
-          'handler' => $element['#selection_handler'],
-        ];
-      /** @var /Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface $handler */
-      $handler = \Drupal::service('plugin.manager.entity_reference_selection')
-        ->getInstance($options);
+      $handler = self::getSelectionHandlerForElement($element);
       if ($handler instanceof SelectionWithAutocompleteLabelsInterface) {
         $labels_data = $handler->getAutocompleteLabels($entities, $element);
       }
@@ -455,6 +445,33 @@ class EntityAutocomplete extends Textfield {
     }
 
     return $match;
+  }
+
+  /**
+   * Gets the entity reference selection plug-in for a form element.
+   *
+   * @param array $element
+   *   The render array for the entity auto-complete form element for which the
+   *   selection plug-in is desired. The settings of this form element will be
+   *   used to locate and initialize the handler instance.
+   *
+   * @return \Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface|false
+   *   Either the entity reference selection handler for the form element, or
+   *   FALSE if it could not be loaded.
+   */
+  protected static function getSelectionHandlerForElement(array $element) {
+    $options = $element['#selection_settings'] + [
+      'target_type' => $element['#target_type'],
+      'handler'     => $element['#selection_handler'],
+    ];
+
+    // @todo switch to using DI in
+    // https://www.drupal.org/project/drupal/issues/3423053. This method and the
+    // methods that call it will need to become instance methods.
+    $plugin_manager = \Drupal::service('plugin.manager.entity_reference_selection');
+    assert($plugin_manager instanceof SelectionPluginManagerInterface);
+
+    return $plugin_manager->getInstance($options);
   }
 
 }
