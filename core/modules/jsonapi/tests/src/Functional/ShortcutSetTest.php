b02befdb26795b6e9dec519e6a7b114ef7a5cd76
Wim Leers <wim.leers@acquia.com>
2024-02-21 19:17:20 +0100

    Make `ShortcutSet` creation via JSON:API work 🚀

--- core/modules/jsonapi/tests/src/Functional/ShortcutSetTest.php
+++ core/modules/jsonapi/tests/src/Functional/ShortcutSetTest.php
@@ -70,6 +70,9 @@ class ShortcutSetTest extends ConfigEntityResourceTestBase {
       case 'GET':
         return "The 'access shortcuts' permission is required.";
 
+      case 'POST':
+        return "The 'administer shortcuts' permission is required.";
+
       default:
         return parent::getExpectedUnauthorizedAccessMessage($method);
     }
@@ -124,9 +127,21 @@ class ShortcutSetTest extends ConfigEntityResourceTestBase {
   /**
    * {@inheritdoc}
    */
+  protected static $firstCreatedEntityId = 'special';
+
+  /**
+   * {@inheritdoc}
+   */
   protected function getPostDocument(): array {
-    // @todo Update in https://www.drupal.org/node/2300677.
-    return [];
+    return [
+      'data' => [
+        'type' => 'shortcut_set--shortcut_set',
+        'attributes' => [
+          'drupal_internal__id' => 'special',
+          'label' => 'My special shortcut set',
+        ],
+      ],
+    ];
   }
 
 }
