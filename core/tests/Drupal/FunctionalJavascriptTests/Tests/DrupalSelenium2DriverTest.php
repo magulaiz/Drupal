1b4608f1eb4c38a7d85deeb19669ff4c322200f3
Alex Pott <alex.a.pott@googlemail.com>
2024-02-22 00:05:38 +0000

    Lol

--- core/tests/Drupal/FunctionalJavascriptTests/Tests/DrupalSelenium2DriverTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Tests/DrupalSelenium2DriverTest.php
@@ -50,7 +50,7 @@ class DrupalSelenium2DriverTest extends WebDriverTestBase {
   public function testGetRemoteFilePath() {
     $web_driver = $this->getSession()->getDriver();
     $this->assertInstanceOf(Selenium2Driver::class, $web_driver);
-    $this->assertFalse($web_driver->isW3C(), 'Driver is operating in W3C mode');
+    $this->assertFalse($web_driver->isW3C(), 'Driver is not operating in W3C mode');
 
     $file_system = \Drupal::service('file_system');
     $entity = EntityTest::create();
