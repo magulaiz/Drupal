ebada7c3197c392c3752a0a30e2dbe4e3c4cbc4a
Stephen Mustgrave <smustgrave@gmail.com>
2024-02-23 09:50:23 -0500

    Not needed now

--- core/tests/Drupal/KernelTests/Core/Theme/Stable9LibraryOverrideTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/Stable9LibraryOverrideTest.php
@@ -26,7 +26,6 @@ class Stable9LibraryOverrideTest extends StableLibraryOverrideTestBase {
     'media/drupal.media-icon',
     'options/drupal.options-icon',
     'telephone/drupal.telephone-icon',
-    'tour/tour-styling',
   ];
 
   /**
