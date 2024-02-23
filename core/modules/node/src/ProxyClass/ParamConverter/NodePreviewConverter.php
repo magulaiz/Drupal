763ef8f9a665fcc1245377a58f38e8ba52f35127
Chris64 <65407-Chris64@users.noreply.drupalcode.org>
2024-02-24 00:43:16 +0100

    ??= B: multilines case exclude generated files + 1 error

--- core/modules/node/src/ProxyClass/ParamConverter/NodePreviewConverter.php
+++ core/modules/node/src/ProxyClass/ParamConverter/NodePreviewConverter.php
@@ -60,7 +60,9 @@ namespace Drupal\node\ProxyClass\ParamConverter {
          */
         protected function lazyLoadItself()
         {
-              $this->service ??= $this->container->get($this->drupalProxyOriginalServiceId);
+            if (!isset($this->service)) {
+                $this->service = $this->container->get($this->drupalProxyOriginalServiceId);
+            }
 
             return $this->service;
         }
--- core/modules/views_ui/src/ProxyClass/ParamConverter/ViewUIConverter.php
+++ core/modules/views_ui/src/ProxyClass/ParamConverter/ViewUIConverter.php
@@ -60,7 +60,9 @@ namespace Drupal\views_ui\ProxyClass\ParamConverter {
          */
         protected function lazyLoadItself()
         {
-              $this->service ??= $this->container->get($this->drupalProxyOriginalServiceId);
+            if (!isset($this->service)) {
+                $this->service = $this->container->get($this->drupalProxyOriginalServiceId);
+            }
 
             return $this->service;
         }
