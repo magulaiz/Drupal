b567336fd3694a94a04ffb589530bc4bdaba2f34
Chris64 <65407-Chris64@users.noreply.drupalcode.org>
2024-02-26 18:41:16 +0100

    ??= A: uni-line case: add missed cases, in core/lib, got with rector.

--- core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
+++ core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
@@ -271,7 +271,7 @@ class LibraryDiscoveryParser {
 
           // Set the 'minified' flag on JS file assets, default to FALSE.
           if ($type == 'js' && $options['type'] == 'file') {
-            $options['minified'] = $options['minified'] ?? FALSE;
+            $options['minified'] ??= FALSE;
           }
 
           $library[$type][] = $options;
