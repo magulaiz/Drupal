e8d0610d4f78feab5674e877b403eb639c1451af
Michael Strelan <28650-mstrelan@users.noreply.drupalcode.org>
2024-02-27 06:09:55 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/search/src/Attribute/SearchPlugin.php
+++ core/modules/search/src/Attribute/SearchPlugin.php
@@ -6,7 +6,7 @@ use Drupal\Component\Plugin\Attribute\Plugin;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
- * Defines a SearchPlugin type annotation object.
+ * Defines a SearchPlugin type attribute for plugin discovery.
  *
  * SearchPlugin classes define search types for the core Search module. Each
  * search type can be used to create search pages from the Search settings page.
