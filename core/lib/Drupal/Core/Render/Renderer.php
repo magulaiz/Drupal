cbdf6424c7f38013f1a110663197822879f0cf26
catch <catch@35733.no-reply.drupal.org>
2024-02-27 12:11:29 +0000

    Remove @todo.

--- core/lib/Drupal/Core/Render/Renderer.php
+++ core/lib/Drupal/Core/Render/Renderer.php
@@ -394,9 +394,9 @@ class Renderer implements RendererInterface {
     }
     // If instructed to create a placeholder, and a #lazy_builder callback is
     // present (without such a callback, it would be impossible to replace the
-    // placeholder), replace the current element with a placeholder.
-    // @todo remove the isMethodCacheable() check when
-    //   https://www.drupal.org/node/2367555 lands.
+    // placeholder), replace the current element with a placeholder. On
+    // uncacheable requests, always skip placeholdering to support form
+    // discovery.
     if (isset($elements['#create_placeholder']) && $elements['#create_placeholder'] === TRUE && $this->requestStack->getCurrentRequest()->isMethodCacheable()) {
       if (!isset($elements['#lazy_builder'])) {
         throw new \LogicException('When #create_placeholder is set, a #lazy_builder callback must be present as well.');
