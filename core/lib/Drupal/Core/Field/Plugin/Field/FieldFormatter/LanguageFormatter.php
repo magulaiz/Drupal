e8c34d336b6b155fb38eb693a5b83ac808a13199
Alex Pott <1732-alexpott@users.noreply.drupalcode.org>
2024-02-29 10:14:13 +0000

    Applied Alex suggestion

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/LanguageFormatter.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/LanguageFormatter.php
@@ -120,7 +120,7 @@ class LanguageFormatter extends StringFormatter {
     // \Drupal\Core\Language\LanguageInterface::LANGCODE_NOT_SPECIFIED
     // and \Drupal\Core\Language\LanguageInterface::LANGCODE_NOT_APPLICABLE are
     // not returned from the language manager above.
-    $value = '';
+    $value = [];
     if (isset($item->language)) {
       $name = isset($languages[$item->language->getId()]) ? $languages[$item->language->getId()]->getName() : $item->language->getId();
       $value = ['#plain_text' => $name];
