d53fe65b5791f6943a82be5594b4fb6c17a0c1d7
HitchShock <HitchShock@gmail.com>
2024-02-29 12:34:40 +0200

    [#3424593] Fixed owner access for media entities

--- core/modules/media/src/MediaAccessControlHandler.php
+++ core/modules/media/src/MediaAccessControlHandler.php
@@ -57,7 +57,7 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
     }
 
     $type = $entity->bundle();
-    $is_owner = ($account->id() && $account->id() === $entity->getOwnerId());
+    $is_owner = ($account->id() && $entity->getOwnerId() && $account->id() === (int) $entity->getOwnerId());
     switch ($operation) {
       case 'view':
         if ($entity->isPublished()) {
