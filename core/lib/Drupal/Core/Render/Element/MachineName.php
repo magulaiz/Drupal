9a87c96ccd69047bd19e3f5b65c8f6898aab6645
Svitlana Horoshanska <60884-shoroshanska@users.noreply.drupalcode.org>
2024-02-29 22:39:00 +0000

    Fix whitespace at the end of the line

--- core/lib/Drupal/Core/Render/Element/MachineName.php
+++ core/lib/Drupal/Core/Render/Element/MachineName.php
@@ -109,7 +109,7 @@ class MachineName extends Textfield {
       // valid input in programmatic form submissions.
       return is_scalar($input) ? (string) $input : '';
     }
-    
+
     return $element['#default_value'] ? (string) $element['#default_value'] : NULL;
   }
 
