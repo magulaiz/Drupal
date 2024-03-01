3c738bdd50936ff4f5a913e35d3eae057edcafdf
mondrake <mondrake.org@gmail.com>
2024-03-01 12:51:29 +0100

    fix

--- core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
+++ core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
@@ -1593,7 +1593,7 @@ PHP,
    * @return \Generator
    *   Test scenarios.
    */
-  public function providerTestDerivedPluginDefinitions(): \Generator {
+  public static function providerTestDerivedPluginDefinitions(): \Generator {
     // Defaults inherited from CKEditor5AspectsOfCKEditor5Plugin.
     $ckeditor5_aspects_defaults = get_class_vars(CKEditor5AspectsOfCKEditor5Plugin::class);
     // Defaults inherited from DrupalAspectsOfCKEditor5Plugin.
