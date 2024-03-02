dc115238492de0e9f5825e1f217edc7c87068372
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2024-03-02 22:02:16 +0000

    Issue #2265487: ConfigEntity based lists with items containing non-ascii characters are not sorted correctly

--- core/tests/Drupal/Tests/Core/Config/Entity/ConfigEntityBaseUnitTest.php
+++ core/tests/Drupal/Tests/Core/Config/Entity/ConfigEntityBaseUnitTest.php
@@ -529,7 +529,8 @@ class ConfigEntityBaseUnitTest extends UnitTestCase {
     $entity_b = $this->createMock(ConfigEntityBase::class);
     $entity_b->expects($this->atLeastOnce())
       ->method('label')
-      ->willReturn('åwesome'); // cspell:disable-line
+      // cSpell:disable-next-line
+      ->willReturn('åwesome');
 
     // Test sorting by label.
     $list = [$entity_a, $entity_b];
