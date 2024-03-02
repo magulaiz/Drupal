dfa9dc5c08d0f1eb318ca690bd1fc0b1268d5775
mondrake <mondrake.org@gmail.com>
2024-03-02 10:55:01 +0100

    fix

--- core/tests/Drupal/Tests/Core/Test/PhpUnitCliTest.php
+++ core/tests/Drupal/Tests/Core/Test/PhpUnitCliTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\Tests\Core\Test;
 
 use Drupal\Tests\UnitTestCase;
-use Drupal\TestTools\PhpUnitCompatibility\RunnerVersion;
 use Symfony\Component\Process\Process;
 
 /**
