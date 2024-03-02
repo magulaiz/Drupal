e8c33b3d04f218b996c219a011f4022b344acd7c
tavi toporjinschi <vasike@gmail.com>
2024-03-02 13:07:16 +0200

    Some tests fixes 2.

--- core/modules/views/tests/src/Kernel/ViewsTemplateTest.php
+++ core/modules/views/tests/src/Kernel/ViewsTemplateTest.php
@@ -57,13 +57,13 @@ class ViewsTemplateTest extends ViewsKernelTestBase {
     $output = $this->render($build);
     $extension = '.html.twig';
     $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * container--more-link--test-page-display--default' . $extension . PHP_EOL
-      . '   * container--more-link--default' . $extension . PHP_EOL
-      . '   * container--more-link--tag1' . $extension . PHP_EOL
-      . '   * container--more-link--tag2' . $extension . PHP_EOL
-      . '   * container--more-link--test-page-display' . $extension . PHP_EOL
-      . '   * container--more-link' . $extension . PHP_EOL
-      . '   x container' . $extension . PHP_EOL
+      . '   ▪️ container--more-link--test-page-display--default' . $extension . PHP_EOL
+      . '   ▪️ container--more-link--default' . $extension . PHP_EOL
+      . '   ▪️ container--more-link--tag1' . $extension . PHP_EOL
+      . '   ▪️ container--more-link--tag2' . $extension . PHP_EOL
+      . '   ▪️ container--more-link--test-page-display' . $extension . PHP_EOL
+      . '   ▪️ container--more-link' . $extension . PHP_EOL
+      . '   ✅ container' . $extension . PHP_EOL
       . '-->' . PHP_EOL;
     $this->assertStringContainsString($expected, $output, 'Views more link container suggestions found in Twig debug output');
   }
