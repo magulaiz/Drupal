40410cfecb9de1da0d7708baafcfeffecab14a88
Andrey Postnikov <2684-andypost@users.noreply.drupalcode.org>
2024-03-02 04:43:54 +0000

    Apply 2 suggestion(s) to 2 file(s)

--- core/modules/views/src/Attribute/ViewsExposedForm.php
+++ core/modules/views/src/Attribute/ViewsExposedForm.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\views\Attribute;
 
 use Drupal\Core\StringTranslation\TranslatableMarkup;
@@ -19,7 +21,7 @@ class ViewsExposedForm extends ViewsPluginAttributeBase {
    * Constructs a views exposed form attribute object.
    *
    * @param string $id
-   *   The attribute class ID.
+   *   The plugin ID.
    * @param \Drupal\Core\StringTranslation\TranslatableMarkup $title
    *   The plugin title used in the views UI.
    * @param \Drupal\Core\StringTranslation\TranslatableMarkup|null $short_title
@@ -28,16 +30,13 @@ class ViewsExposedForm extends ViewsPluginAttributeBase {
    *   A short help string; this is displayed in the views UI.
    * @param array|null $display_types
    *   The types of the display this plugin can be used with.
-   *
    *   For example the Feed display defines the type 'feed', so only rss style
    *   and row plugins can be used in the views UI.
    * @param array|null $base
    *   The base tables on which this exposed form plugin can be used.
-   *
    *   If no base table is specified the plugin can be used with all tables.
    * @param bool $no_ui
    *   Whether the plugin should be not selectable in the UI.
-   *
    *   If it's set to TRUE, you can still use it via the API in config files.
    * @param bool $register_theme
    *   (optional) Whether or not to register a theme function automatically.
