a650ba2a995f692c04e8eebb360d5416ce85c1fc
mondrake <mondrake.org@gmail.com>
2024-03-04 00:05:32 +0100

    debug

--- core/tests/Drupal/TestTools/Extension/HtmlLogging/HtmlOutputLogger.php
+++ core/tests/Drupal/TestTools/Extension/HtmlLogging/HtmlOutputLogger.php
@@ -48,8 +48,9 @@ final class HtmlOutputLogger {
    */
   public static function init(string $outputDirectory, bool $outputVerbose): void {
     if (self::$instance === NULL) {
-      dump([__METHOD__, 'is_dir', is_dir($outputDirectory)]);
-      dump([__METHOD__, 'is_writable', is_writable($outputDirectory)]);
+      dump([__METHOD__, $outputDirectory, 'is_dir', is_dir($outputDirectory)]);
+      dump([__METHOD__, $outputDirectory, 'is_writable', is_writable($outputDirectory)]);
+      dump([__METHOD__, $outputDirectory, 'file_exists', file_exists($outputDirectory)]);
       if (!is_dir($outputDirectory) || !is_writable($outputDirectory)) {
         dump([__METHOD__, 'not writable']);
         throw new \RuntimeException("HTML output directory {$outputDirectory} is not a writable directory.");
