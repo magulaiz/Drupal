f8e1aefa6308f4a13a44f1680c93369058cf35f5
Chris Tomasso <christomasso@symetris.ca>
2024-03-04 13:58:49 -0500

    Issue #3410037: Deprecate StorageComparerInterface

--- core/lib/Drupal/Core/Config/StorageComparer.php
+++ core/lib/Drupal/Core/Config/StorageComparer.php
@@ -134,7 +134,14 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets the configuration source storage.
+   *
+   * @param string $collection
+   *   (optional) The storage collection to use. Defaults to the
+   *   default collection.
+   *
+   * @return \Drupal\Core\Config\StorageInterface
+   *   Storage object used to read configuration.
    */
   public function getSourceStorage($collection = StorageInterface::DEFAULT_COLLECTION) {
     if (!isset($this->sourceStorages[$collection])) {
@@ -149,7 +156,14 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets the configuration target storage.
+   *
+   * @param string $collection
+   *   (optional) The storage collection to use. Defaults to the
+   *   default collection.
+   *
+   * @return \Drupal\Core\Config\StorageInterface
+   *   Storage object used to write configuration.
    */
   public function getTargetStorage($collection = StorageInterface::DEFAULT_COLLECTION) {
     if (!isset($this->targetStorages[$collection])) {
@@ -171,7 +185,13 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Changes the StorageComparer to write mode.
+   *
+   * Tells the StorageComparer that the target storage is going to be used for
+   * writing. This information allows implementations to optimize caching
+   * strategies for reading from or writing to the target storage.
+   *
+   * @return $this
    */
   public function writeMode(): static {
     if (!$this->writeMode) {
@@ -183,7 +203,10 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets an empty changelist.
+   *
+   * @return array
+   *   An empty changelist array.
    */
   public function getEmptyChangelist() {
     return [
@@ -195,7 +218,17 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets the list of differences to import.
+   *
+   * @param string $op
+   *   (optional) A change operation. Either delete, create or update. If
+   *   supplied the returned list will be limited to this operation.
+   * @param string $collection
+   *   (optional) The collection to get the changelist for. Defaults to the
+   *   default collection.
+   *
+   * @return array
+   *   An array of config changes that are yet to be imported.
    */
   public function getChangelist($op = NULL, $collection = StorageInterface::DEFAULT_COLLECTION) {
     if ($op) {
@@ -233,7 +266,10 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Calculates the differences.
+   *
+   * @return $this
+   *   An object which implements the StorageComparerInterface.
    */
   public function createChangelist() {
     foreach ($this->getAllCollectionNames() as $collection) {
@@ -389,7 +425,15 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Moves a rename operation to an update.
+   *
+   * @param string $rename
+   *   The rename name, as provided by ConfigImporter::createRenameName().
+   * @param string $collection
+   *   (optional) The collection where the configuration is stored. Defaults to
+   *   the default collection.
+   *
+   * @see \Drupal\Core\Config\ConfigImporter::createRenameName()
    */
   public function moveRenameToUpdate($rename, $collection = StorageInterface::DEFAULT_COLLECTION) {
     $names = $this->extractRenameNames($rename);
@@ -398,7 +442,10 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Recalculates the differences.
+   *
+   * @return $this
+   *   An object which implements the StorageComparerInterface.
    */
   public function reset() {
     $this->changelist = [StorageInterface::DEFAULT_COLLECTION => $this->getEmptyChangelist()];
@@ -410,7 +457,14 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Checks if there are any operations with changes to process.
+   *
+   * Until the changelist has been calculated this will always be FALSE.
+   *
+   * @return bool
+   *   TRUE if there are changes to process and FALSE if not.
+   *
+   * @see \Drupal\Core\Config\StorageComparerInterface::createChangelist()
    */
   public function hasChanges() {
     foreach ($this->getAllCollectionNames() as $collection) {
@@ -424,7 +478,10 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Validates that the system.site::uuid in the source and target match.
+   *
+   * @return bool
+   *   TRUE if identical, FALSE if not.
    */
   public function validateSiteUuid() {
     $source = $this->sourceStorage->read('system.site');
@@ -477,7 +534,18 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Extracts old and new configuration names from a configuration change name.
+   *
+   * @param string $name
+   *   The configuration change name, as provided by
+   *   ConfigImporter::createRenameName().
+   *
+   * @return array
+   *   An associative array of configuration names. The array keys are
+   *   'old_name' and 'new_name' representing the old and new configuration
+   *   object names during a rename operation.
+   *
+   * @see \Drupal\Core\Config\StorageComparer::createRenameNames()
    */
   public function extractRenameNames($name) {
     $names = explode('::', $name, 2);
@@ -488,7 +556,13 @@ class StorageComparer implements StorageComparerInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets the existing collections from both the target and source storage.
+   *
+   * @param bool $include_default
+   *   (optional) Include the default collection. Defaults to TRUE.
+   *
+   * @return array
+   *   An array of existing collection names.
    */
   public function getAllCollectionNames($include_default = TRUE) {
     $collections = array_unique(array_merge($this->sourceStorage->getAllCollectionNames(), $this->targetStorage->getAllCollectionNames()));
