1c1f270a1c8b614c456d5b93d08dc0fa0bfcc505
dinesh.b <dinesh.b@valuebound.com>
2024-03-04 13:02:17 +0530

    Add @property  to the DateTimeItem

--- core/modules/datetime/src/Plugin/Field/FieldType/DateTimeItem.php
+++ core/modules/datetime/src/Plugin/Field/FieldType/DateTimeItem.php
@@ -26,6 +26,8 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  *   list_class = "\Drupal\datetime\Plugin\Field\FieldType\DateTimeFieldItemList",
  *   constraints = {"DateTimeFormat" = {}}
  * )
+ * 
+ * @property \Drupal\Core\Datetime\DrupalDateTime $date
  */
 class DateTimeItem extends FieldItemBase implements DateTimeItemInterface {
 
