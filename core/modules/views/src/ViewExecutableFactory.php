7198ff0cf5537167bb2703cb6775505b933dc545
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-03-04 21:14:00 +0200

    switch to constructor property promotion

--- core/modules/views/src/ViewExecutableFactory.php
+++ core/modules/views/src/ViewExecutableFactory.php
@@ -41,13 +41,6 @@ class ViewExecutableFactory {
   protected $routeProvider;
 
   /**
-   * The display plugin manager.
-   *
-   * @var \Drupal\views\Plugin\ViewsPluginManager
-   */
-  protected ViewsPluginManager $displayPluginManager;
-
-  /**
    * Constructs a new ViewExecutableFactory.
    *
    * @param \Drupal\Core\Session\AccountInterface $user
@@ -58,19 +51,18 @@ class ViewExecutableFactory {
    *   The views data.
    * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
    *   The route provider.
-   * @param \Drupal\views\Plugin\ViewsPluginManager|null $display_plugin_manager
+   * @param \Drupal\views\Plugin\ViewsPluginManager|null $displayPluginManager
    *   The display plugin manager.
    */
-  public function __construct(AccountInterface $user, RequestStack $request_stack, ViewsData $views_data, RouteProviderInterface $route_provider, ViewsPluginManager $display_plugin_manager = NULL) {
+  public function __construct(AccountInterface $user, RequestStack $request_stack, ViewsData $views_data, RouteProviderInterface $route_provider, protected ?ViewsPluginManager $displayPluginManager = NULL) {
     $this->user = $user;
     $this->requestStack = $request_stack;
     $this->viewsData = $views_data;
     $this->routeProvider = $route_provider;
-    if ($display_plugin_manager === NULL) {
-      @trigger_error('Calling ' . __METHOD__ . ' without the $display_plugin_manager argument is deprecated in drupal:10.3.0 and it will be required in drupal:12.0.0. See https://www.drupal.org/node/3410349', E_USER_DEPRECATED);
-      $display_plugin_manager = \Drupal::service('plugin.manager.views.display');
+    if ($this->displayPluginManager === NULL) {
+      @trigger_error('Calling ' . __METHOD__ . ' without the $displayPluginManager argument is deprecated in drupal:10.3.0 and it will be required in drupal:12.0.0. See https://www.drupal.org/node/3410349', E_USER_DEPRECATED);
+      $this->displayPluginManager = \Drupal::service('plugin.manager.views.display');
     }
-    $this->displayPluginManager = $display_plugin_manager;
   }
 
   /**
