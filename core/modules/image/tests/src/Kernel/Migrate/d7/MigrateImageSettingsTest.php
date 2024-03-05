3dd82f937676352b10c10731a6f1b5be083712aa
Phéna Proxima <adam@phenaproxima.net>
2024-03-05 14:30:14 -0500

    Fix (sorta) one test by schema exclusion

--- core/modules/image/tests/src/Kernel/Migrate/d7/MigrateImageSettingsTest.php
+++ core/modules/image/tests/src/Kernel/Migrate/d7/MigrateImageSettingsTest.php
@@ -16,6 +16,17 @@ class MigrateImageSettingsTest extends MigrateDrupal7TestBase {
   /**
    * {@inheritdoc}
    */
+  protected static $configSchemaCheckerExclusions = [
+    // The schema of `image.settings` requires that the `preview_image` value
+    // be a file that actually exists, but that won't be the case in this test.
+    // Therefore, exclude `image.settings` from the schema check; we still
+    // assert that the expected value is migrated.
+    'image.settings',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
   protected function setUp(): void {
     parent::setUp();
     $this->executeMigration('d7_image_settings');
