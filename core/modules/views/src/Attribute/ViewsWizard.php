2bc34106002cdc907ab5a5394b87915dc92bc4e3
sorlov <sergey.kam.orlov@gmail.com>
2024-03-05 17:57:51 +0600

    3421000 Convert ViewsWizard plugin discovery to attributes

---
+++ core/modules/views/src/Attribute/ViewsWizard.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\views\Attribute;
+
+use Drupal\Component\Plugin\Attribute\Plugin;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Defines a Plugin attribute object for views wizard plugins.
+ *
+ * @see \Drupal\views\Plugin\views\wizard\WizardPluginBase
+ * @see \Drupal\views\Plugin\views\wizard\WizardInterface
+ *
+ * @ingroup views_wizard_plugins
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class ViewsWizard extends Plugin {
+
+  /**
+   * Constructs an ViewsWizard attribute.
+   *
+   * @param string $id
+   *   The plugin ID.
+   * @param \Drupal\Core\StringTranslation\TranslatableMarkup|null $title
+   *   The plugin title used in the views UI.
+   * @param \Drupal\Core\StringTranslation\TranslatableMarkup|null $short_title
+   *   (optional) The short title used in the views UI.
+   * @param string $base_table
+   *   The base table on which this wizard is used.
+   * @param class-string|null $deriver
+   *   (optional) The deriver class.
+   */
+  public function __construct(
+    public readonly string $id,
+    public readonly ?TranslatableMarkup $title = NULL,
+    public readonly ?TranslatableMarkup $short_title = NULL,
+    public readonly ?string $base_table = '',
+    public readonly ?string $deriver = NULL
+  ) {}
+
+}
--- core/modules/views/src/Plugin/views/wizard/Standard.php
+++ core/modules/views/src/Plugin/views/wizard/Standard.php
@@ -2,17 +2,20 @@
 
 namespace Drupal\views\Plugin\views\wizard;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\views\Attribute\ViewsWizard;
+use Drupal\views\Plugin\Derivative\DefaultWizardDeriver;
+
 /**
  * Standard Views wizard plugin.
  *
  * @ingroup views_wizard_plugins
- *
- * @ViewsWizard(
- *   id = "standard",
- *   deriver = "Drupal\views\Plugin\Derivative\DefaultWizardDeriver",
- *   title = @Translation("Default wizard")
- * )
  */
+#[ViewsWizard(
+  id: 'standard',
+  deriver: DefaultWizardDeriver::class,
+  title: new TranslatableMarkup('Default wizard')
+)]
 class Standard extends WizardPluginBase {
 
 }
