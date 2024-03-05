c1baa3f079044b00e8923a08ffd2b7ecc75d2688
Ashish Dwivedi <adwivedi008@gmail.com>
2024-03-05 09:46:48 +0530

    Issue #3424593: Media entity owner access check

--- core/modules/media/src/MediaAccessControlHandler.php
+++ core/modules/media/src/MediaAccessControlHandler.php
@@ -57,7 +57,7 @@ class MediaAccessControlHandler extends EntityAccessControlHandler implements En
     }
 
     $type = $entity->bundle();
-    $is_owner = ($account->id() && $entity->getOwnerId() && $account->id() === (int) $entity->getOwnerId());
+    $is_owner = $account->id() && (int) $account->id() === (int) $entity->getOwnerId();
     switch ($operation) {
       case 'view':
         if ($entity->isPublished()) {
