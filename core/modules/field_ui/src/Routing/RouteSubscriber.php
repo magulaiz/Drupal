c2e9042b745a85c6db3b2f2d7f789ef7c677eef2
omkar podey <omkarpodey@gmail.com>
2024-03-05 18:02:04 +0530

    controller and new route

--- core/modules/field_ui/src/Routing/RouteSubscriber.php
+++ core/modules/field_ui/src/Routing/RouteSubscriber.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Drupal\Core\Routing\RoutingEvents;
 use Drupal\field_ui\Controller\FieldConfigAddController;
+use Drupal\field_ui\Controller\FieldStorageAddController;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -94,7 +95,7 @@ class RouteSubscriber extends RouteSubscriberBase {
         $route = new Route(
           "$path/fields/add-field",
           [
-            '_form' => '\Drupal\field_ui\Form\FieldStorageAddForm',
+            '_controller' => FieldStorageAddController::class . '::getFieldSelectionLinks',
             '_title' => 'Add field',
           ] + $defaults,
           ['_permission' => 'administer ' . $entity_type_id . ' fields'],
@@ -104,7 +105,7 @@ class RouteSubscriber extends RouteSubscriberBase {
 
         $route = new Route(
           "$path/fields/add-field/{new_storage_type}",
-          [
+          [s
             '_form' => '\Drupal\field_ui\Form\FieldStorageAddForm',
             '_title' => 'Add field',
           ] + $defaults,
