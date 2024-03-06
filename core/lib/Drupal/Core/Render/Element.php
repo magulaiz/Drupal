0a92e7a601c7466eaa372794776f3f68cb2c41f4
David Galeano <galeanobarradodavid@gmail.com>
2024-03-06 11:21:39 +0100

    Issue #3083690 by gxleano: Update trigger error message

--- core/lib/Drupal/Core/Render/Element.php
+++ core/lib/Drupal/Core/Render/Element.php
@@ -95,7 +95,12 @@ class Element {
         // Only trigger an error if the value is not null.
         // @see https://www.drupal.org/node/1283892
         elseif (isset($value)) {
-          trigger_error(new FormattableMarkup('"@key" is an invalid render array key', ['@key' => $key]), E_USER_ERROR);
+          trigger_error(new FormattableMarkup(
+            'Expected an array but got a @type',
+            [
+              '@type' => gettype($value),
+            ]
+          ), E_USER_ERROR);
         }
       }
       $i++;
