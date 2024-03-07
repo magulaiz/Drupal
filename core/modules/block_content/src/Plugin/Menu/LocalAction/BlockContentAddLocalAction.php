b65df470d7b4d725c90eac1e70fe871bc1df5cdb
Adam Bramley <46227-acbramley@users.noreply.drupalcode.org>
2024-03-07 01:16:09 +0000

    Collapse + fix constructor.

--- core/modules/block_content/src/Plugin/Menu/LocalAction/BlockContentAddLocalAction.php
+++ core/modules/block_content/src/Plugin/Menu/LocalAction/BlockContentAddLocalAction.php
@@ -15,18 +15,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class BlockContentAddLocalAction extends LocalActionDefault {
 
   /**
-   * Constructs a LocalActionDefault object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteProviderInterface $routeProvider
-   *   The route provider to load routes by name.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-   *   The current request.
+   * Constructs a BlockContentAddLocalAction object.
    */
   public function __construct(
     array $configuration,
