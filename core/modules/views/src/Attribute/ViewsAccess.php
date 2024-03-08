5c88ceb43ae862b49d776fdb99ef8303093b9bb0
Alex Pott <1732-alexpott@users.noreply.drupalcode.org>
2024-03-08 11:25:55 +0000

    Apply 4 suggestion(s) to 1 file(s)

--- core/modules/views/src/Attribute/ViewsAccess.php
+++ core/modules/views/src/Attribute/ViewsAccess.php
@@ -31,26 +31,22 @@ class ViewsAccess extends Plugin {
    *   For example the Feed display defines the type 'feed', so only rss style
    *   and row plugins can be used in the views UI.
    * @param string[] $base
-   *   The base tables on which this access plugin can be used.
+   *   (optional) The base tables on which this access plugin can be used.
    *   If no base table is specified the plugin can be used with all tables.
    * @param bool $no_ui
-   *   Whether the plugin should be not selectable in the UI.
+   *   (optional) Whether the plugin should be not selectable in the UI.
    *   If set to TRUE, you can still use it via the API in config files.
-   * @param bool $register_theme
-   *   Whether to register a theme function automatically.
-   *   This property is optional and it does not need to be declared.
    * @param class-string|null $deriver
    *   (optional) The deriver class.
    */
   public function __construct(
     public readonly string $id,
-    public readonly ?TranslatableMarkup $title = NULL,
+    public readonly TranslatableMarkup $title,
     public readonly ?TranslatableMarkup $short_title = NULL,
     public readonly ?TranslatableMarkup $help = NULL,
     public readonly ?array $display_types = NULL,
     public readonly array $base = [],
     public readonly bool $no_ui = FALSE,
-    public readonly bool $register_theme = FALSE,
     public readonly ?string $deriver = NULL
   ) {}
 
