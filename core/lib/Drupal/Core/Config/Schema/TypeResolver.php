7549b06f5ec10a25c0c1b9573379aa33b2010058
Joris Vercammen <botchcake@gmail.com>
2024-03-08 14:08:24 +0100

    fix phpcs

--- core/lib/Drupal/Core/Config/Schema/TypeResolver.php
+++ core/lib/Drupal/Core/Config/Schema/TypeResolver.php
@@ -89,7 +89,7 @@ class TypeResolver {
     // Process each value part, one at a time.
     while ($name = array_shift($parts)) {
       if (str_starts_with($name, '%') && !in_array($name, ['%parent', '%key', '%type'])) {
-        throw new \LogicException('`' . $expression .'` is not a valid dynamic type expression. Dynamic type expressions must contain at least `%parent`, `%key`, or `%type`.`');
+        throw new \LogicException('`' . $expression . '` is not a valid dynamic type expression. Dynamic type expressions must contain at least `%parent`, `%key`, or `%type`.`');
       }
       if ($name === '%type' && $previous_name !== '%parent') {
         throw new \LogicException('`%type` can only used when immediately proceeded by `%parent` in `' . $expression . '`');
