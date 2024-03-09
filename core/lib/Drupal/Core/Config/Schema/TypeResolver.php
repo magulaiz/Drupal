46051e35cb383a87a5e42fe4713be297583128bb
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2024-03-09 08:37:12 +0000

    Apply suggestion by @phenaproxima

--- core/lib/Drupal/Core/Config/Schema/TypeResolver.php
+++ core/lib/Drupal/Core/Config/Schema/TypeResolver.php
@@ -88,7 +88,7 @@ class TypeResolver {
     $previous_name = NULL;
     // Process each value part, one at a time.
     while ($name = array_shift($parts)) {
-      if (str_starts_with($name, '%') && !in_array($name, ['%parent', '%key', '%type'])) {
+      if (str_starts_with($name, '%') && !in_array($name, ['%parent', '%key', '%type'], TRUE)) {
         throw new \LogicException('`' . $expression . '` is not a valid dynamic type expression. Dynamic type expressions must contain at least `%parent`, `%key`, or `%type`.`');
       }
       if ($name === '%type' && $previous_name !== '%parent') {
