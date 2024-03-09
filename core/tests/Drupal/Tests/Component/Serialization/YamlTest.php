c5fcd0e5f93e026de3b08f92d4d3b4642ab92fee
Geoff Appleby <geoff@gapple.ca>
2024-03-08 23:04:28 -0800

    Cleanup unused test data provider

--- core/tests/Drupal/Tests/Component/Serialization/YamlTest.php
+++ core/tests/Drupal/Tests/Component/Serialization/YamlTest.php
@@ -164,12 +164,9 @@ foo: !php/enum Drupal\Tests\Component\Serialization\EnumValue::Yes
 bar: !php/const Drupal\Tests\Component\Serialization\BackedEnumValue::Maybe->value
 
 YAML;
-    $symfony = YamlSymfony::decode($yaml_alternative);
-    $yaml = YamlPecl::decode($yaml_alternative);
-    $this->assertSame($symfony['foo'], EnumValue::Yes);
-    $this->assertSame($symfony['bar'], BackedEnumValue::Maybe->value);
-    $this->assertSame($yaml['foo'], EnumValue::Yes);
-    $this->assertSame($yaml['bar'], BackedEnumValue::Maybe->value);
+    $decoded = Yaml::decode($yaml_alternative);
+    $this->assertSame($decoded['foo'], EnumValue::Yes);
+    $this->assertSame($decoded['bar'], BackedEnumValue::Maybe->value);
   }
 
 }
