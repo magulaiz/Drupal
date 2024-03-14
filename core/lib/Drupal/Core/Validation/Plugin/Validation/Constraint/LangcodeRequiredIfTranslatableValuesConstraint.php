5ae621c0bce51f4da48dfefe0359d580e47dd636
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2024-03-14 09:17:19 +0000

    Language nits 👍

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/LangcodeRequiredIfTranslatableValuesConstraint.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/LangcodeRequiredIfTranslatableValuesConstraint.php
@@ -20,13 +20,13 @@ class LangcodeRequiredIfTranslatableValuesConstraint extends SymfonyConstraint {
    *
    * @var string
    */
-  public string $missingMessage = "The @name config object contains translatable values and must hence specify a 'langcode' key.";
+  public string $missingMessage = "The @name config object must specify a language code, because it contains translatable values.";
 
   /**
    * The error message if this config object contains a superfluous `langcode`.
    *
    * @var string
    */
-  public string $superfluousMessage = "The @name config object does not contain translatable values and should hence not specify a 'langcode' key.";
+  public string $superfluousMessage = "The @name config object does not contain any translatable values, so it should not specify a language code.";
 
 }
