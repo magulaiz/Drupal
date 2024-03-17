d97b858cf6e5bb613a9f8df3c4f57a0b014cf3ec
Sascha Grossenbacher <saschagros@gmail.com>
2024-03-17 22:24:02 +0100

    also remove tag from kernel class

--- core/tests/Drupal/KernelTests/KernelTestBase.php
+++ core/tests/Drupal/KernelTests/KernelTestBase.php
@@ -563,8 +563,7 @@ abstract class KernelTestBase extends TestCase implements ServiceProviderInterfa
       ->register('datetime.time', 'Drupal\Component\Datetime\Time');
     $container
       ->register('flood', 'Drupal\Core\Flood\MemoryBackend')
-      ->addArgument(new Reference('request_stack'))
-      ->addTag('cron');
+      ->addArgument(new Reference('request_stack'));
     $container
       ->register('lock', 'Drupal\Core\Lock\NullLockBackend');
     $container
