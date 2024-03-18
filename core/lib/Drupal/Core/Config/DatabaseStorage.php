8515dc0754a48b843eeea3ce3833f267e9aa3b0c
Phéna Proxima <adam@phenaproxima.net>
2024-03-18 10:52:33 -0400

    Refactor DatabaseStorage::read()

--- core/lib/Drupal/Core/Config/DatabaseStorage.php
+++ core/lib/Drupal/Core/Config/DatabaseStorage.php
@@ -92,10 +92,7 @@ class DatabaseStorage implements StorageInterface {
         $data = $this->decode($raw);
       }
     }
-    catch (\Exception $e) {
-      if ($this->connection->schema()->tableExists($this->table)) {
-        throw $e;
-      }
+    catch (SchemaObjectDoesNotExistException) {
       // If we attempt a read without actually having the table available,
       // return false so the caller can handle it.
     }
