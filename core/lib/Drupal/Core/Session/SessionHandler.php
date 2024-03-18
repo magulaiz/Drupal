dbf21036ac3f3b382f231a4ce7881a23b7a6e0c4
catch <catch@35733.no-reply.drupal.org>
2024-03-18 10:50:23 +0000

    Use more specific catch.

--- core/lib/Drupal/Core/Session/SessionHandler.php
+++ core/lib/Drupal/Core/Session/SessionHandler.php
@@ -5,6 +5,7 @@ namespace Drupal\Core\Session;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\SchemaObjectDoesNotExistException;
 use Drupal\Core\Database\SchemaObjectExistsException;
 use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -70,7 +71,7 @@ class SessionHandler extends AbstractProxy implements \SessionHandlerInterface {
         $data = (string) $query->fetchField();
       }
       // Swallow the error if the table hasn't been created yet.
-      catch (\Throwable $e) {
+      catch (SchemaObjectDoesNotExistException) {
       }
     }
     return $data;
@@ -94,9 +95,9 @@ class SessionHandler extends AbstractProxy implements \SessionHandlerInterface {
         ->fields($fields)
         ->execute();
     try {
-      $doWrite($sid, $fields, $this->connection);
+      $doWrite();
     }
-    catch (\Throwable $e) {
+    catch (SchemaObjectDoesNotExistException $e) {
       // If there was an exception, try to create the table.
       if (!$try_again = $this->ensureTableExists()) {
         // If the exception happened for other reason than the missing
@@ -106,7 +107,7 @@ class SessionHandler extends AbstractProxy implements \SessionHandlerInterface {
     }
     // Now that the bin has been created, try again if necessary.
     if ($try_again) {
-      $doWrite($sid, $fields, $this->connection);
+      $doWrite();
     }
 
     return TRUE;
@@ -130,7 +131,7 @@ class SessionHandler extends AbstractProxy implements \SessionHandlerInterface {
         ->execute();
     }
     // Swallow the error if the table hasn't been created yet.
-    catch (\Throwable $e) {
+    catch (SchemaObjectDoesNotExistException) {
     }
 
     return TRUE;
@@ -151,7 +152,7 @@ class SessionHandler extends AbstractProxy implements \SessionHandlerInterface {
         ->execute();
     }
     // Swallow the error if the table hasn't been created yet.
-    catch (\Throwable $e) {
+    catch (SchemaObjectDoesNotExistException) {
     }
     return FALSE;
   }
