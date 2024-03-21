3615b4557af9144f65076c1c30d0b921d523afa3
Alex Pott <alex.a.pott@googlemail.com>
2024-03-21 14:21:07 +0000

    This is not really true

--- core/tests/Drupal/Tests/BrowserTestBase.php
+++ core/tests/Drupal/Tests/BrowserTestBase.php
@@ -116,7 +116,7 @@ abstract class BrowserTestBase extends TestCase {
   /**
    * The profile to install as a basis for testing.
    *
-   * @var string|false
+   * @var string
    */
   protected $profile = 'testing';
 
