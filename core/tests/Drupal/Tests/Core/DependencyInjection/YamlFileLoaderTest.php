c3e4393f3c32fcbde2f63662996b412c7222a0d3
Dave Long <dave@longwaveconsulting.com>
2024-03-26 11:27:30 +0000

    Add YAML loader test for \!tagged_iterator.

--- core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
+++ core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
@@ -9,6 +9,7 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\YamlFileLoader;
 use Drupal\Tests\UnitTestCase;
 use org\bovigo\vfs\vfsStream;
+use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
 
 /**
  * @coversDefaultClass \Drupal\Core\DependencyInjection\YamlFileLoader
@@ -61,6 +62,7 @@ YAML;
     $this->assertFalse($builder->has('example_private_service'));
     $this->assertTrue($builder->has('Drupal\Core\ExampleClass'));
     $this->assertSame('Drupal\Core\ExampleClass', $builder->getDefinition('Drupal\Core\ExampleClass')->getClass());
+    $this->assertInstanceOf(TaggedIteratorArgument::class, $builder->getDefinition('example_tagged_iterator')->getArgument(0));
   }
 
   /**
