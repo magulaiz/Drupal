6c6a2b74884a735e19100b298cfe91386f8ed6ae
catch <catch@35733.no-reply.drupal.org>
2024-03-26 15:19:35 +0000

    Syntax error.

--- core/modules/user/src/Controller/UserAuthenticationController.php
+++ core/modules/user/src/Controller/UserAuthenticationController.php
@@ -198,7 +198,7 @@ class UserAuthenticationController extends ControllerBase implements ContainerIn
         throw new BadRequestHttpException('The user has not been activated or is blocked.');
       }
       if ($this->userAuth instanceof UserAuthenticationInterface) {
-        $authenticated = $this->userAuth->authenticateAccount($account, $credentials['pass'])?>id();
+        $authenticated = $this->userAuth->authenticateAccount($account, $credentials['pass'])?->id();
       }
       else {
         $authenticated = $this->userAuth->authenticateAccount($credentials['name'], $credentials['pass']);
