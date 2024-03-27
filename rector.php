6202baa36745b904a771b3a90d6ca004fcc7d090
Michael Strelan <mstrelan@gmail.com>
2024-03-27 14:21:38 +1000

    Add rector config

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Drupal\Core\Rector\StringTypeRector;
+
+return RectorConfig::configure()
+  ->withPaths([
+    'core/lib',
+    'core/modules',
+    'core/profiles',
+    'core/tests',
+    'core/themes',
+  ])
+  ->withSkipPath('core/lib/Drupal/Component/Annotation/Doctrine')
+  ->withFileExtensions(['php'])
+  ->withParallel(300)
+;
