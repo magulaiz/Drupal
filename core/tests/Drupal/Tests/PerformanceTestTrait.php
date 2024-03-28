d79a40bdd08b32f8231846ef281e60e05942e0a0
catch <catch@35733.no-reply.drupal.org>
2024-03-28 12:55:34 +0000

    handle subdirectories.

--- core/tests/Drupal/Tests/PerformanceTestTrait.php
+++ core/tests/Drupal/Tests/PerformanceTestTrait.php
@@ -371,13 +371,13 @@ trait PerformanceTestTrait {
     $style_elements = $page->findAll('xpath', '//link[@href and @rel="stylesheet"]');
     $style_urls = [];
     foreach ($style_elements as $element) {
-      $filename = parse_url(ltrim($element->getAttribute('href'), '/'), PHP_URL_PATH);
+      $filename = \Drupal::root() . parse_url($element->getAttribute('href'), PHP_URL_PATH);
       $stylesheet_bytes += filesize($filename);
     }
     $script_elements = $page->findAll('xpath', '//script[@src]');
     $script_urls = [];
     foreach ($script_elements as $element) {
-      $filename = parse_url(ltrim($element->getAttribute('src'), '/'), PHP_URL_PATH);
+      $filename = \Drupal::root() . parse_url($element->getAttribute('src'), PHP_URL_PATH);
       $script_bytes += filesize($filename);
     }
 
