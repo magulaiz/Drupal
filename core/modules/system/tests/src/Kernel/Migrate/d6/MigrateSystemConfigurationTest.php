03aaf397aa4be7cb3825a28538c0e4fd90b9fc2b
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-03-29 22:25:49 +0530

    Changed values

--- core/modules/system/tests/src/Kernel/Migrate/d6/MigrateSystemConfigurationTest.php
+++ core/modules/system/tests/src/Kernel/Migrate/d6/MigrateSystemConfigurationTest.php
@@ -23,7 +23,7 @@ class MigrateSystemConfigurationTest extends MigrateDrupal6TestBase {
         'requirements_error' => 1209600,
       ],
       // logging is not handled by the migration.
-      'logging' => 1,
+      'logging' => true,
     ],
     'system.date' => [
       'first_day' => 4,
