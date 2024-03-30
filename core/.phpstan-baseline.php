afd6501fff7e45481c35a37214b88436e864019e
Michael Lutz <mlutz531@gmail.com>
2024-03-30 18:36:33 -0400

    fix: adjust baseline

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -1137,6 +1137,11 @@ $ignoreErrors[] = [
   'path' => __DIR__ . '/modules/migrate/tests/src/Kernel/MigrateSourceTestBase.php',
 ];
 $ignoreErrors[] = [
+  'message' => '#^@dataProvider providerSource related method not found\\.$#',
+  'count' => 1,
+  'path' => __DIR__ . '/modules/migrate/tests/src/Kernel/MigrateSqlSourceTestBase.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Call to an undefined static method Drupal\\\\Tests\\\\migrate\\\\Kernel\\\\MigrateTestBase\\:\\:migrateDumpAlter\\(\\)\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/modules/migrate/tests/src/Kernel/MigrateTestBase.php',
