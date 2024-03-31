d31f6732d229d139b2e704d4538a3674a8102c12
Dave Long <dave@longwaveconsulting.com>
2024-03-31 20:52:19 +0100

    Deprecate NULL arguments in FormattableMarkup::__construct().

--- core/lib/Drupal/Component/Render/FormattableMarkup.php
+++ core/lib/Drupal/Component/Render/FormattableMarkup.php
@@ -88,6 +88,18 @@ class FormattableMarkup implements MarkupInterface, \Countable {
    * @see \Drupal\Component\Render\FormattableMarkup::placeholderFormat()
    */
   public function __construct($string, array $arguments) {
+    foreach ($arguments as $key => $value) {
+      if (is_null($value)) {
+        // It's probably a bug to provide a null value for the placeholder arg,
+        // and in D11 this will no longer be allowed. When this trigger_error
+        // is removed, also remove isset $value checks inside the switch{}
+        // below.
+        // phpcs:ignore Drupal.Semantics.FunctionTriggerError
+        @trigger_error(sprintf('Deprecated NULL placeholder value for key (%s) in: "%s". This will throw a PHP error in drupal:11.0.0. See https://www.drupal.org/node/3318826', (string) $key, (string) $string), E_USER_DEPRECATED);
+        $arguments[$key] = '';
+      }
+    }
+
     $this->string = (string) $string;
     $this->arguments = $arguments;
   }
