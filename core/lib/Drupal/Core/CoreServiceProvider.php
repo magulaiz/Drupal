815f271598d7fac2cbb53bb432619beba9233e2f
Dave Long <dave@longwaveconsulting.com>
2024-04-02 09:25:54 +0100

    Do not rely on autoconfiguration of plugin managers.

--- core/lib/Drupal/Core/CoreServiceProvider.php
+++ core/lib/Drupal/Core/CoreServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\Core;
 
-use Drupal\Component\Plugin\Discovery\CachedDiscoveryInterface;
 use Drupal\Core\Cache\Context\CacheContextsPass;
 use Drupal\Core\Cache\ListCacheBinsPass;
 use Drupal\Core\DependencyInjection\Compiler\AuthenticationProviderPass;
@@ -25,6 +24,7 @@ use Drupal\Core\DependencyInjection\Compiler\TwigExtensionPass;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use Drupal\Core\Plugin\PluginManagerPass;
 use Drupal\Core\Queue\QueueFactoryInterface;
 use Drupal\Core\Render\MainContent\MainContentRenderersPass;
 use Drupal\Core\Site\Settings;
@@ -100,6 +100,9 @@ class CoreServiceProvider implements ServiceProviderInterface, ServiceModifierIn
     $container->addCompilerPass(new CacheContextsPass());
     $container->addCompilerPass(new AuthenticationProviderPass());
 
+    // Register plugin managers.
+    $container->addCompilerPass(new PluginManagerPass());
+
     $container->addCompilerPass(new DeprecatedServicePass());
 
     $container->registerForAutoconfiguration(EventSubscriberInterface::class)
@@ -110,9 +113,6 @@ class CoreServiceProvider implements ServiceProviderInterface, ServiceModifierIn
 
     $container->registerForAutoconfiguration(QueueFactoryInterface::class)
       ->addTag('queue_factory');
-
-    $container->registerForAutoconfiguration(CachedDiscoveryInterface::class)
-      ->addTag('plugin_manager_cache_clear');
   }
 
   /**
--- core/lib/Drupal/Core/Plugin/PluginManagerPass.php
+++ core/lib/Drupal/Core/Plugin/PluginManagerPass.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Core\Plugin;
 
-@trigger_error('The ' . __NAMESPACE__ . '\PluginManagerPass is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/1234567', E_USER_DEPRECATED);
-
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -19,9 +17,9 @@ class PluginManagerPass implements CompilerPassInterface {
   public function process(ContainerBuilder $container): void {
     $cache_clearer_definition = $container->getDefinition('plugin.cache_clearer');
     foreach ($container->getDefinitions() as $service_id => $definition) {
-      if (str_starts_with($service_id, 'plugin.manager.') || $definition->hasTag('plugin_manager_cache_clear')) {
+      if (str_starts_with($service_id, 'plugin.manager.') && !$definition->hasTag('plugin_manager_cache_clear')) {
         if (is_subclass_of($definition->getClass(), '\Drupal\Component\Plugin\Discovery\CachedDiscoveryInterface')) {
-          $cache_clearer_definition->addMethodCall('addCachedDiscovery', [new Reference($service_id)]);
+          $definition->addTag('plugin_manager_cache_clear');
         }
       }
     }
