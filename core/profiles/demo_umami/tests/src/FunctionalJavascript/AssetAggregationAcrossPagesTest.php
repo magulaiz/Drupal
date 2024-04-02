41fd6ef8b6ae68561e4c990e7c35d5455eaf1e60
catch <catch@35733.no-reply.drupal.org>
2024-04-02 14:47:11 +0100

    Fix assertion.

---
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\demo_umami\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\PerformanceTestBase;
+
+/**
+ * Tests demo_umami profile performance.
+ *
+ * @group #slow
+ */
+class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'demo_umami';
+
+  /**
+   * Checks the asset requests made when the front and recipe pages are visited.
+   */
+  public function testFrontAndRecipesPages() {
+    $performance_data = $this->collectPerformanceData(function () {
+      $this->drupalGet('<front>');
+      $this->drupalGet('recipes');
+    }, 'umamiFrontAndRecipePages');
+    $this->assertSame(2, $performance_data->getStylesheetCount());
+    $this->assertSame(88793, $performance_data->getStylesheetBytes());
+    $this->assertSame(4, $performance_data->getScriptCount());
+    $this->assertSame(14150, $performance_data->getScriptBytes());
+  }
+
+}
