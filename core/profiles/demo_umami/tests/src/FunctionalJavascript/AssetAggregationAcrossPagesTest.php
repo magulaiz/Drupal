45f0753efe590912881451949c9a67571728d8f7
catch <catch@35733.no-reply.drupal.org>
2024-04-04 00:25:19 +0100

    Fix assertions.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -26,7 +26,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $performance_data = $this->doRequests();
     $this->assertSame(4, $performance_data->getStylesheetCount());
     $this->assertSame(90241, $performance_data->getStylesheetBytes());
-    $this->assertSame(2, $performance_data->getScriptCount());
+    $this->assertSame(1, $performance_data->getScriptCount());
     $this->assertSame(7075, $performance_data->getScriptBytes());
   }
 
@@ -38,7 +38,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $this->drupalLogin($user);
     $this->rebuildAll();
     $performance_data = $this->doRequests();
-    $this->assertSame(2, $performance_data->getStylesheetCount());
+    $this->assertSame(4, $performance_data->getStylesheetCount());
     $this->assertSame(45296, $performance_data->getStylesheetBytes());
     $this->assertSame(1, $performance_data->getScriptCount());
     $this->assertSame(132038, $performance_data->getScriptBytes());
