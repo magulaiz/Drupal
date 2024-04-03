d4ec2b5f47f4ba620efd9478154c526f1ac90ddf
Sudhir Krishna <40153-sudhir@users.noreply.drupalcode.org>
2024-04-03 16:17:11 +0530

    test case updated

--- core/modules/system/tests/src/Functional/Form/FormTest.php
+++ core/modules/system/tests/src/Functional/Form/FormTest.php
@@ -772,7 +772,9 @@ class FormTest extends BrowserTestBase {
     // All the elements should be marked as disabled, including the ones below
     // the disabled container.
     $actual_count = count($disabled_elements);
-    $expected_count = 44;
+    // Count has increased to 45 since managed_file attributes issue has been
+    // fixed.
+    $expected_count = 45;
     $this->assertEquals($expected_count, $actual_count, sprintf('Found %s elements with disabled property (expected %s).', count($disabled_elements), $expected_count));
 
     // Mink does not "see" hidden elements, so we need to set the value of the
