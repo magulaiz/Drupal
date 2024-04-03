f9b6c41e319a4a01f13a7bf945d79c85dbcd5b9b
catch <catch@35733.no-reply.drupal.org>
2024-04-04 00:44:48 +0100

    Reduce assertion.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
@@ -36,7 +36,7 @@ class OpenTelemetryAuthenticatedPerformanceTest extends PerformanceTestBase {
       $this->drupalGet('<front>');
     }, 'authenticatedFrontPage');
     $this->assertSame(2, $performance_data->getStylesheetCount());
-    $this->assertSame(47552, $performance_data->getStylesheetBytes());
+    $this->assertSame(45296, $performance_data->getStylesheetBytes());
     $this->assertSame(1, $performance_data->getScriptCount());
     $this->assertSame(132038, $performance_data->getScriptBytes());
 
