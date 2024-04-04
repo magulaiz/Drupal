ad8b8653ff8d8b1e78cf50d7e878fd9853722c4e
Adam Bramley <adam.bramley@previousnext.com.au>
2024-04-05 08:37:25 +1100

    Final perf test tweaks

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -196,7 +196,7 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertSame(1, $performance_data->getCacheSetCount());
     $this->assertSame(1, $performance_data->getCacheDeleteCount());
     $this->assertSame(1, $performance_data->getCacheTagChecksumCount());
-    $this->assertSame(28, $performance_data->getCacheTagIsValidCount());
+    $this->assertSame(30, $performance_data->getCacheTagIsValidCount());
     $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
   }
 
@@ -246,11 +246,11 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $recorded_queries = $performance_data->getQueries();
     $this->assertSame($expected_queries, $recorded_queries);
     $this->assertSame(17, $performance_data->getQueryCount());
-    $this->assertSame(107, $performance_data->getCacheGetCount());
+    $this->assertSame(110, $performance_data->getCacheGetCount());
     $this->assertSame(1, $performance_data->getCacheSetCount());
     $this->assertSame(1, $performance_data->getCacheDeleteCount());
     $this->assertSame(1, $performance_data->getCacheTagChecksumCount());
-    $this->assertSame(43, $performance_data->getCacheTagIsValidCount());
+    $this->assertSame(46, $performance_data->getCacheTagIsValidCount());
     $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
   }
 
