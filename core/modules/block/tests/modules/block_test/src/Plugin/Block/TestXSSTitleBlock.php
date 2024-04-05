a05188aba64ceb25cca69305dbe589664c5a9349
Pablo López // plopesc <plopesc@gmail.com>
2024-04-05 08:28:10 +0200

    Issue #3438623: Block Layout & Place Block pages are unsafe due to XSS vulnerabilities in title and category fields

--- core/modules/block/tests/modules/block_test/src/Plugin/Block/TestXSSTitleBlock.php
+++ core/modules/block/tests/modules/block_test/src/Plugin/Block/TestXSSTitleBlock.php
@@ -2,13 +2,16 @@
 
 namespace Drupal\block_test\Plugin\Block;
 
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
 /**
  * Provides a block to test XSS in title.
- *
- * @Block(
- *   id = "test_xss_title",
- *   admin_label = "<script>alert('XSS subject');</script>"
- * )
  */
+#[Block(
+  id: "test_xss_title",
+  admin_label: new TranslatableMarkup("<script>alert('XSS subject');</script>"),
+  category: new TranslatableMarkup("<script>alert('XSS category');</script>"),
+)]
 class TestXSSTitleBlock extends TestCacheBlock {
 }
