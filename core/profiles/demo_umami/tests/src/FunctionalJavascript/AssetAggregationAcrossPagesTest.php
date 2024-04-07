9ccc6c8722bd1f820d125215de0ee98c9beed26a
catch <catch@35733.no-reply.drupal.org>
2024-04-07 11:33:27 +0100

    More pages more bytes.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -25,7 +25,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
   public function testFrontAndRecipesPages() {
     $performance_data = $this->doRequests();
     $this->assertSame(6, $performance_data->getStylesheetCount());
-    $this->assertSame(90241, $performance_data->getStylesheetBytes());
+    $this->assertSame(137375, $performance_data->getStylesheetBytes());
     $this->assertSame(2, $performance_data->getScriptCount());
     $this->assertSame(14150, $performance_data->getScriptBytes());
   }
