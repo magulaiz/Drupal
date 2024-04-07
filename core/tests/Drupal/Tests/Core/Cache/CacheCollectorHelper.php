57eeaaf8d8b2a0e5c6598ab2dcac624855a52fe5
catch <catch@35733.no-reply.drupal.org>
2024-04-07 08:32:34 +0100

    Add comment.

--- core/tests/Drupal/Tests/Core/Cache/CacheCollectorHelper.php
+++ core/tests/Drupal/Tests/Core/Cache/CacheCollectorHelper.php
@@ -70,6 +70,7 @@ class CacheCollectorHelper extends CacheCollector {
    * Setter for the cacheCreated property for use in unit tests.
    *
    * @param int $cache_created
+   *   A unix timestamp.
    */
   public function setCacheCreated(int $cache_created):void {
     $this->cacheCreated = $cache_created;
