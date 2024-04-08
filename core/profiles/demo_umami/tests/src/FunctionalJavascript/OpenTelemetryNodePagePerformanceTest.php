9abf62f3323f567b0ebb6e99d2468ea1c7606f5b
catch <catch@35733.no-reply.drupal.org>
2024-04-08 20:27:18 +0100

    Don't use a forward slash with drupalGet().

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
@@ -29,7 +29,7 @@ class OpenTelemetryNodePagePerformanceTest extends PerformanceTestBase {
     $this->drupalGet('user/login');
     $this->rebuildAll();
     $this->collectPerformanceData(function () {
-      $this->drupalGet('/node/1');
+      $this->drupalGet('node/1');
     }, 'umamiNodePageColdCache');
     $this->assertSession()->pageTextContains('quiche');
   }
@@ -68,9 +68,9 @@ class OpenTelemetryNodePagePerformanceTest extends PerformanceTestBase {
     $this->drupalGet('node/1');
     $this->rebuildAll();
     // Now visit a non-node page to warm non-route-specific caches.
-    $this->drupalGet('/user/login');
+    $this->drupalGet('user/login');
     $this->collectPerformanceData(function () {
-      $this->drupalGet('/node/1');
+      $this->drupalGet('node/1');
     }, 'umamiNodePageCoolCache');
     $this->assertSession()->pageTextContains('quiche');
   }
@@ -86,9 +86,9 @@ class OpenTelemetryNodePagePerformanceTest extends PerformanceTestBase {
     $this->drupalGet('node/1');
     $this->rebuildAll();
     // Now visit a different node page to warm non-path-specific caches.
-    $this->drupalGet('/node/2');
+    $this->drupalGet('node/2');
     $this->collectPerformanceData(function () {
-      $this->drupalGet('/node/1');
+      $this->drupalGet('node/1');
     }, 'umamiNodePageWarmCache');
     $this->assertSession()->pageTextContains('quiche');
   }
