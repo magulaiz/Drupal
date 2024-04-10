0fbf7c276ceea9f6ae456f1dde5c987d87ddd48b
Sudhir Krishna <40153-sudhir@users.noreply.drupalcode.org>
2024-04-10 11:44:28 +0530

    spacing fix

--- core/modules/file/src/Entity/FileRouteProvider.php
+++ core/modules/file/src/Entity/FileRouteProvider.php
@@ -30,8 +30,8 @@ class FileRouteProvider implements EntityRouteProviderInterface {
 
     // Add a collection route to handle deletion.
     $collection_route = (new Route('/admin/content/files'))
-    ->setRequirement('_permission', 'access files overview')
-    ->setOption('_admin_route', TRUE);
+      ->setRequirement('_permission', 'access files overview')
+      ->setOption('_admin_route', TRUE);
     $route_collection->add('entity.file.collection', $collection_route);
 
     return $route_collection;
