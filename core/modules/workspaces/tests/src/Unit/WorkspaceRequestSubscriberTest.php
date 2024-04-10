efbbcd10e5ee1eb0f1b6c793566a80f70fc8163b
Andrei Mateescu <andrei@amateescu.me>
2024-04-10 18:00:39 +0300

    Remove hidden dependency on the path_alias module.

--- core/modules/workspaces/tests/src/Unit/WorkspaceRequestSubscriberTest.php
+++ core/modules/workspaces/tests/src/Unit/WorkspaceRequestSubscriberTest.php
@@ -17,19 +17,9 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 /**
  * @coversDefaultClass \Drupal\workspaces\EventSubscriber\WorkspaceRequestSubscriber
  *
- * @group workspace
+ * @group workspaces
  */
 class WorkspaceRequestSubscriberTest extends UnitTestCase {
-
-  /**
-   * @var \Drupal\path_alias\AliasManagerInterface
-   */
-  protected $aliasManager;
-
-  /**
-   * @var \Drupal\Core\Path\CurrentPathStack
-   */
-  protected $currentPath;
 
   /**
    * @var \Drupal\workspaces\WorkspaceManagerInterface
@@ -42,8 +32,6 @@ class WorkspaceRequestSubscriberTest extends UnitTestCase {
   protected function setUp(): void {
     parent::setUp();
 
-    $this->aliasManager = $this->prophesize(AliasManagerInterface::class)->reveal();
-    $this->currentPath = $this->prophesize(CurrentPathStack::class)->reveal();
     $this->workspaceManager = $this->prophesize(WorkspaceManagerInterface::class);
 
     $active_workspace = $this->prophesize(WorkspaceInterface::class);
@@ -62,7 +50,7 @@ class WorkspaceRequestSubscriberTest extends UnitTestCase {
     // Check that WorkspaceRequestSubscriber::onKernelRequest() calls
     // addExtraCacheKeyPart() on a route provider that implements
     // CacheableRouteProviderInterface.
-    $workspace_request_subscriber = new WorkspaceRequestSubscriber($this->aliasManager, $this->currentPath, $route_provider->reveal(), $this->workspaceManager->reveal());
+    $workspace_request_subscriber = new WorkspaceRequestSubscriber($route_provider->reveal(), $this->workspaceManager->reveal());
     $event = $this->prophesize(RequestEvent::class)->reveal();
     $this->assertNull($workspace_request_subscriber->onKernelRequest($event));
   }
@@ -76,7 +64,7 @@ class WorkspaceRequestSubscriberTest extends UnitTestCase {
     // Check that WorkspaceRequestSubscriber::onKernelRequest() doesn't call
     // addExtraCacheKeyPart() on a route provider that does not implement
     // CacheableRouteProviderInterface.
-    $workspace_request_subscriber = new WorkspaceRequestSubscriber($this->aliasManager, $this->currentPath, $route_provider->reveal(), $this->workspaceManager->reveal());
+    $workspace_request_subscriber = new WorkspaceRequestSubscriber($route_provider->reveal(), $this->workspaceManager->reveal());
     $event = $this->prophesize(RequestEvent::class)->reveal();
     $this->assertNull($workspace_request_subscriber->onKernelRequest($event));
   }
