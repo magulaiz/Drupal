aa504590cc29cc7dd594f090e20213efc12109e9
Nick Procopan <nick@optasy.com>
2024-04-11 00:25:46 +0300

    fix

--- core/modules/path/tests/src/Functional/PathContentModerationTest.php
+++ core/modules/path/tests/src/Functional/PathContentModerationTest.php
@@ -57,8 +57,6 @@ class PathContentModerationTest extends BrowserTestBase {
     'administer taxonomy',
     'administer languages',
     'administer site configuration',
-    'edit any page content',
-    'create page content',
     'administer url aliases',
     'create url aliases',
   ];
