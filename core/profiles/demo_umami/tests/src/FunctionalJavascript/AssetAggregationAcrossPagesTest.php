08782559a8343c7223d7a2248ef54ec1e8c103f2
catch <catch@35733.no-reply.drupal.org>
2024-04-11 09:31:23 +0100

    Even more.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -27,7 +27,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $this->assertSame(4, $performance_data->getStylesheetCount());
     $this->assertSame(82347, $performance_data->getStylesheetBytes());
     $this->assertSame(2, $performance_data->getScriptCount());
-    $this->assertSame(14150, $performance_data->getScriptBytes());
+    $this->assertSame(14134, $performance_data->getScriptBytes());
   }
 
   /**
@@ -41,7 +41,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $this->assertSame(4, $performance_data->getStylesheetCount());
     $this->assertSame(89167, $performance_data->getStylesheetBytes());
     $this->assertSame(2, $performance_data->getScriptCount());
-    $this->assertSame(264702, $performance_data->getScriptBytes());
+    $this->assertSame(264670, $performance_data->getScriptBytes());
   }
 
   /**
