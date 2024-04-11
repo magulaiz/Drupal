0ea096d8b68951712953e3b536d9bbf77348dd37
mark burdett <mfburdett@gmail.com>
2024-04-11 13:13:24 -0700

    Add locale kernel test

--- core/tests/Drupal/KernelTests/Core/DrupalKernel/DrupalKernelTest.php
+++ core/tests/Drupal/KernelTests/Core/DrupalKernel/DrupalKernelTest.php
@@ -295,4 +295,17 @@ class DrupalKernelTest extends KernelTestBase {
     $this->assertSame($request_stack, $container->get('request_stack'));
   }
 
+  /**
+   * Tests system locale.
+   */
+  public function testLocale(): void {
+    $utf8_string = 'äöüßαβγδεζηθικλμνξοσὠ';
+    // Test environment locale should be UTF-8.
+    $this->assertSame($utf8_string, escapeshellcmd($utf8_string));
+    $request = Request::createFromGlobals();
+    $kernel = $this->getTestKernel($request);
+    // Kernel environment locale should be UTF-8.
+    $this->assertSame($utf8_string, escapeshellcmd($utf8_string));
+  }
+
 }
