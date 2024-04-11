4f13a4af3cc6e1f83efcc06c103a595f2da47387
Andrei Ivnitskii <ivnish.dev@gmail.com>
2024-04-11 16:06:14 +0500

    Add update path

--- core/themes/olivero/olivero.post_update.php
+++ core/themes/olivero/olivero.post_update.php
@@ -13,3 +13,12 @@ function olivero_removed_post_updates() {
     'olivero_post_update_add_olivero_primary_color' => '11.0.0',
   ];
 }
+
+/**
+ * Sets the `comment_form_after_comments` value of Olivero's theme settings.
+ */
+function olivero_post_update_add_comment_form_after_comments() {
+  \Drupal::configFactory()->getEditable('olivero.settings')
+    ->set('comment_form_after_comments', FALSE)
+    ->save();
+}
