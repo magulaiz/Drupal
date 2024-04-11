fe150326ce6f858221a195e2afddbca2b166be33
maskedjellybean <benteegarden@gmail.com>
2024-04-11 15:30:45 -0700

    Media author views filter use user_name plugin.

--- core/modules/media/media.post_update.php
+++ core/modules/media/media.post_update.php
@@ -45,3 +45,10 @@ function media_post_update_set_blank_iframe_domain_to_null() {
       ->save(TRUE);
   }
 }
+
+/**
+ * Update media author views filter to use the user_name plugin.
+ */
+function media_post_update_media_author_views_filter_update(&$sandbox = NULL) {
+  // Empty update function to clear the Views data cache.
+}
