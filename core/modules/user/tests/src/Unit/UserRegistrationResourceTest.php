3cde098974326dd688c596116502b70a9cb8e9b6
Patrick Kenny <33362-ptmkenny@users.noreply.drupalcode.org>
2024-04-13 00:00:10 +0900

    use correct deprecation test

--- core/modules/user/tests/src/Unit/UserRegistrationResourceTest.php
+++ core/modules/user/tests/src/Unit/UserRegistrationResourceTest.php
@@ -148,7 +148,7 @@ class UserRegistrationResourceTest extends UnitTestCase {
    * @group legacy
    */
   public function testDeprecations() {
-    $this->expectDeprecation('Calling Drupal\user\Plugin\rest\resource\UserRegistrationResource::__construct() without the $password_generator argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0.');
+    $this->expectDeprecation('Calling Drupal\user\Plugin\rest\resource\UserRegistrationResource::__construct() without the $password_generator argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3405799');
     $this->expectException(BadRequestHttpException::class);
 
     $container = new ContainerBuilder();
