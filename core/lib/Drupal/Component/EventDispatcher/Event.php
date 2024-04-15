1918d12e8b726b258c87ab40a550a693f1621817
Michael Strelan <mstrelan@gmail.com>
2024-04-15 13:30:46 +1000

    Deprecate Event class

--- core/lib/Drupal/Component/EventDispatcher/Event.php
+++ core/lib/Drupal/Component/EventDispatcher/Event.php
@@ -4,6 +4,8 @@ namespace Drupal\Component\EventDispatcher;
 
 use Symfony\Contracts\EventDispatcher\Event as SymfonyEvent;
 
+trigger_error(__NAMESPACE__ . '\Event is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use \Symfony\Contracts\EventDispatcher\Event instead. See https://www.drupal.org/node/3440958', E_USER_DEPRECATED);
+
 /**
  * Provides a forward-compatibility layer for the Symfony 5 event class.
  *
@@ -11,5 +13,10 @@ use Symfony\Contracts\EventDispatcher\Event as SymfonyEvent;
  * In order to prepare for updates, code that wishes to extend Symfony's Event
  * class should extend this intermediary class, which will handle switching
  * from Symfony\Component to Symfony\Contracts without a further change.
+ *
+ * @deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use
+ *   \Symfony\Contracts\EventDispatcher\Event instead.
+ *
+ * @see https://www.drupal.org/node/3440958
  */
 class Event extends SymfonyEvent {}
