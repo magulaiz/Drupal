098a7d9b8f49dcabfe5a013504f0b9c05baa6bc8
Andrei Mateescu <andrei@amateescu.me>
2024-04-15 13:01:06 +0300

    Add a @see reference to the CR.

--- core/modules/workspaces/src/Form/WorkspaceFormInterface.php
+++ core/modules/workspaces/src/Form/WorkspaceFormInterface.php
@@ -11,5 +11,7 @@ use Drupal\Core\Form\FormInterface;
  *
  * @deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use
  *   \Drupal\Core\Form\WorkspaceSafeFormTrait instead.
+ *
+ * @see https://www.drupal.org/node/3229111
  */
 interface WorkspaceFormInterface extends FormInterface {}
