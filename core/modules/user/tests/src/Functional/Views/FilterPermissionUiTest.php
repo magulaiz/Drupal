5f86fc3f18eae9a269d2a0328fbf8d1c778e7a51
Sakthi Shanmuga Sundaram M <62943-sakthi_dev@users.noreply.drupalcode.org>
2024-04-15 14:47:31 +0000

    Issue#3427427: Update the permission title.

--- core/modules/user/tests/src/Functional/Views/FilterPermissionUiTest.php
+++ core/modules/user/tests/src/Functional/Views/FilterPermissionUiTest.php
@@ -54,7 +54,7 @@ class FilterPermissionUiTest extends ViewTestBase {
     $this->drupalGet('admin/structure/views/view/test_filter_permission/edit/default');
     // Verify that the handler summary is correctly displaying the selected
     // permission.
-    $this->assertSession()->linkExists('User: Permission (= Access user profiles)');
+    $this->assertSession()->linkExists('User: Permission (= Access Any User Profile)');
     $this->submitForm([], 'Save');
     // Verify that we can save the view.
     $this->assertSession()->pageTextNotContains('No valid values found on filter: User: Permission.');
