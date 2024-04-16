7713d7feaa08015d9d8a0ddb033c13a60ce6ec4f
catch <catch@35733.no-reply.drupal.org>
2024-04-16 22:44:30 +0100

    Lower assertion to reflect smaller filesize in HEAD.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
@@ -37,7 +37,7 @@ class OpenTelemetryAuthenticatedPerformanceTest extends PerformanceTestBase {
       $this->drupalGet('<front>');
     }, 'authenticatedFrontPage');
     $this->assertSame(2, $performance_data->getStylesheetCount());
-    $this->assertSame(42989, $performance_data->getStylesheetBytes());
+    $this->assertSame(42981, $performance_data->getStylesheetBytes());
     $this->assertSame(1, $performance_data->getScriptCount());
     $this->assertSame(132067, $performance_data->getScriptBytes());
 
