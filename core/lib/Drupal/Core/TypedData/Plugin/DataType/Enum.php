b8cfe6e07d8d6e238fe2f241b247ba5af761f8d2
Alex Pott <alex.a.pott@googlemail.com>
2024-04-18 12:31:27 +0100

    Improve data type

--- core/lib/Drupal/Core/TypedData/Plugin/DataType/Enum.php
+++ core/lib/Drupal/Core/TypedData/Plugin/DataType/Enum.php
@@ -25,7 +25,7 @@ class Enum extends TypedData {
   /**
    * Overrides \Drupal\Core\TypedData\TypedData::setValue().
    *
-   * @param array|null $value
+   * @param \UnitEnum|null $value
    *   The value.
    * @param bool $notify
    *   (optional) Whether to notify the parent object of the change. Defaults to
@@ -34,12 +34,17 @@ class Enum extends TypedData {
    */
   public function setValue($value, $notify = TRUE) {
     if (isset($value)) {
+      // @todo I think this should be a constraint.
+      $class = $this->getDataDefinition()['enum_class'] ?? '';
+      if (empty($class)) {
+        throw new \InvalidArgumentException('Enum data definitions must supply an "enum_class".');
+      }
+      // Ensure the value is of the provided class.
+      if (!$value instanceof $class) {
+        throw new \InvalidArgumentException(sprintf('Invalid value given. Value must be a "%s" but has "%s" instead.', $class, get_class($value)));
+      }
       if (!$value instanceof \UnitEnum) {
         throw new \InvalidArgumentException('Invalid value given. Value must be an enum.');
-      }
-      $class = $this->getDataDefinition()['enum_class'] ?? '';
-      if (!empty($class) && !($value instanceof $class)) {
-        throw new \InvalidArgumentException(sprintf('Invalid value given. Value must be a "%s" but have "%s" instead.', $class, get_class($value)));
       }
     }
     parent::setValue($value, $notify);
