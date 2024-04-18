729f0477db6a679df97189c519e374531782c345
Pradhuman Jain <pradhuman.j@opensenselabs.com>
2024-04-18 21:46:41 +0530

    Issue #3439831: Remove $usesSuperUserAccessPolicy Variable.

--- core/modules/config/tests/src/Functional/LanguageNegotiationFormOverrideTest.php
+++ core/modules/config/tests/src/Functional/LanguageNegotiationFormOverrideTest.php
@@ -18,14 +18,6 @@ class LanguageNegotiationFormOverrideTest extends BrowserTestBase {
 
   /**
    * {@inheritdoc}
-   *
-   * @todo Remove and fix test to not rely on super user.
-   * @see https://www.drupal.org/project/drupal/issues/3437620
-   */
-  protected bool $usesSuperUserAccessPolicy = TRUE;
-
-  /**
-   * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
 
