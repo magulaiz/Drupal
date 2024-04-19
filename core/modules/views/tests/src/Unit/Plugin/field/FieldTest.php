c5c902b6ac52aea457bf0ef98a8ab6b5806cdbff
Trent Crawford <trent.crawford@liip.ch>
2024-04-19 09:14:24 +0200

    Issue 2815881: Check for emptiness of the group_fields property rather than non-existence of the property as the property will always exist.

--- core/modules/views/tests/src/Unit/Plugin/field/FieldTest.php
+++ core/modules/views/tests/src/Unit/Plugin/field/FieldTest.php
@@ -578,7 +578,7 @@ class FieldTest extends UnitTestCase {
 
     $handler->query(TRUE);
 
-    $this->assertObjectNotHasProperty('group_fields', $handler);
+    $this->assertEmpty($handler->group_fields);
     $this->assertNull($handler->tableAlias);
   }
 
@@ -704,8 +704,7 @@ class FieldTest extends UnitTestCase {
     $this->executable->query = $query;
 
     $handler->query(TRUE);
-
-    $this->assertObjectNotHasProperty('group_fields', $handler);
+    $this->assertEmpty($handler->group_fields);
     $this->assertNull($handler->tableAlias);
   }
 
