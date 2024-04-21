cc7d43cf958856bf87c64d385634d958ed88cc5a
nicxvan <nic@nlightened.net>
2024-04-20 22:52:28 -0400

    Add basic html tags to filter

--- core/lib/Drupal/Core/EventSubscriber/RssResponseRelativeUrlFilter.php
+++ core/lib/Drupal/Core/EventSubscriber/RssResponseRelativeUrlFilter.php
@@ -63,7 +63,7 @@ class RssResponseRelativeUrlFilter implements EventSubscriberInterface {
         $html_markup = $node->nodeValue;
         if (!empty($html_markup)) {
           $html_markup = Html::transformRootRelativeUrlsToAbsolute($html_markup, $request->getSchemeAndHttpHost());
-          $html_markup = Xss::filter($html_markup);
+          $html_markup = Xss::filter($html_markup, ['a', 'abbr', 'acronym', 'address', 'b', 'bdo', 'big', 'blockquote', 'br', 'caption', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'dl', 'dt', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'ins', 'kbd', 'li', 'ol', 'p', 'pre', 'q', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'tt', 'ul', 'var']);
           $new_node = $rss_dom->createCDATASection($html_markup);
           $node->replaceChild($new_node, $node->firstChild);
         }
