6caa384590edfba10469ca479f3d705a9ae7d393
Lap Pham <phthlaap@gmail.com>
2024-04-21 16:59:18 +0700

    re-roll in 11x and added a tests to cover the issue.

--- core/modules/views/src/Plugin/views/filter/NumericFilter.php
+++ core/modules/views/src/Plugin/views/filter/NumericFilter.php
@@ -445,12 +445,15 @@ class NumericFilter extends FilterPluginBase implements FilterOperatorsInterface
       if (!empty($info[$this->operator]['values'])) {
         switch ($info[$this->operator]['values']) {
           case 1:
-            if ($value['value'] === '') {
+            if (!isset($value['value']) || $value['value'] === '') {
               return FALSE;
             }
             break;
 
           case 2:
+            if (!isset($value['min']) || !isset($value['max'])) {
+              return FALSE;
+            }
             if ($value['min'] === '' && $value['max'] === '') {
               return FALSE;
             }
