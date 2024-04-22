eabca9c2d54ddbd36ab7d1a7cd95f086153bc9b2
sandeeps79 <sandeep.sanwale@opensenselabs.com>
2024-04-22 10:53:08 +0530

    Issue #3442365: Removed deprecated code in taxonomy module fixed php coding standard issue

--- core/modules/taxonomy/src/Plugin/views/argument/IndexTidDepth.php
+++ core/modules/taxonomy/src/Plugin/views/argument/IndexTidDepth.php
@@ -43,6 +43,9 @@ class IndexTidDepth extends ArgumentPluginBase implements ContainerFactoryPlugin
     );
   }
 
+  /**
+   *
+   */
   protected function defineOptions() {
     $options = parent::defineOptions();
 
@@ -53,6 +56,9 @@ class IndexTidDepth extends ArgumentPluginBase implements ContainerFactoryPlugin
     return $options;
   }
 
+  /**
+   *
+   */
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
     $form['depth'] = [
       '#type' => 'weight',
@@ -89,6 +95,9 @@ class IndexTidDepth extends ArgumentPluginBase implements ContainerFactoryPlugin
     return $actions;
   }
 
+  /**
+   *
+   */
   public function query($group_by = FALSE) {
     $this->ensureMyTable();
 
@@ -106,12 +115,15 @@ class IndexTidDepth extends ArgumentPluginBase implements ContainerFactoryPlugin
     $this->addSubQueryJoin($tids);
   }
 
+  /**
+   *
+   */
   public function title() {
     $term = $this->entityRepository->getCanonical('taxonomy_term', $this->argument);
     if (!empty($term)) {
       return $term->label();
     }
-    // TODO review text
+    // @todo review text
     return $this->t('No name');
   }
 
