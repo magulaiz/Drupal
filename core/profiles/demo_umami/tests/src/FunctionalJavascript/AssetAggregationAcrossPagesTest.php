9c79e47602f31967e6f355be65e850778206024d
Sudhir Krishna <40153-sudhir@users.noreply.drupalcode.org>
2024-04-22 20:37:34 +0530

    updated metrics data

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -41,7 +41,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $this->assertSame(4, $performance_data->getStylesheetCount());
     $this->assertLessThan(89500, $performance_data->getStylesheetBytes());
     $this->assertSame(2, $performance_data->getScriptCount());
-    $this->assertSame(264160, $performance_data->getScriptBytes());
+    $this->assertSame(247034, $performance_data->getScriptBytes());
   }
 
   /**
