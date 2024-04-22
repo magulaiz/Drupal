94bb95b3a5037e155c9bf795cddf04152495f6bd
HitchShock <HitchShock@gmail.com>
2024-04-22 14:08:43 +0300

    [#3296190] Updated tests

--- core/modules/system/tests/src/Functional/Form/EmailTest.php
+++ core/modules/system/tests/src/Functional/Form/EmailTest.php
@@ -38,7 +38,7 @@ class EmailTest extends BrowserTestBase {
     $this->submitForm($edit, 'Submit');
     $this->assertSession()->pageTextContains('The email address invalid is not valid. Use the format user@example.com.');
     $this->assertSession()->pageTextContains('Address field is required.');
-    $this->assertSession()->pageTextContains('The email addresses invalid-first, invalid-second are not valid. Use the format user@example.com and separate the addresses with a comma.');
+    $this->assertSession()->pageTextContains('The email addresses invalid-first, invalid-second are not valid. Use the format user@example.com, and separate the addresses with a comma.');
 
     $edit = [];
     $edit['email_required'] = 'invalid-required';
@@ -46,7 +46,7 @@ class EmailTest extends BrowserTestBase {
     $this->drupalGet('form-test/email');
     $this->submitForm($edit, 'Submit');
     $this->assertSession()->pageTextContains('The email address invalid-required is not valid. Use the format user@example.com.');
-    $this->assertSession()->pageTextContains('The email address invalid-multiple is not valid. Use the format user@example.com and separate the addresses with a comma.');
+    $this->assertSession()->pageTextContains('The email address invalid-multiple is not valid. Use the format user@example.com, and separate the addresses with a comma.');
 
     $edit = [];
     $edit['email_required'] = 'example@drupal.org,example2@drupal.org';
