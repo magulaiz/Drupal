ff733165ca9d199a4d6586ecda15d40ecde3e698
Christian López Espínola <31980-penyaskito@users.noreply.drupalcode.org>
2024-04-23 14:18:09 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/tests/Drupal/Tests/Component/Utility/UrlHelperTest.php
+++ core/tests/Drupal/Tests/Component/Utility/UrlHelperTest.php
@@ -129,7 +129,7 @@ class UrlHelperTest extends TestCase {
    */
   public function testUncompressInvalidString() {
     // Pass an invalid string to ::uncompressQueryParameter() and ensure it
-    // doesn't result in a PHP warning.
+    // returns the passed string without resulting in a PHP warning.
     $this->assertSame('llama', UrlHelper::uncompressQueryParameter('llama'));
   }
 
