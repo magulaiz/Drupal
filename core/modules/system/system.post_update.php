08d2f374590211287dabfe3a8f3cdb2dd6ef9602
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-04-24 22:59:31 +0530

    upgrade path added

--- core/modules/system/system.post_update.php
+++ core/modules/system/system.post_update.php
@@ -94,6 +94,18 @@ function system_post_update_remove_rss_cdata_subscriber(): void {
 }
 
 /**
+ * Updates system.theme:admin config if it's still at the default.
+ */
+function system_post_update_set_theme_admin_to_null() {
+  $theme_settings = \Drupal::configFactory()->getEditable('system.theme');
+  if ($theme_settings->get('admin') === '') {
+    $theme_settings
+      ->set('admin', NULL)
+      ->save(TRUE);
+  }
+}
+
+/**
  * Remove path key in system.file.
  */
 function system_post_update_remove_path_key(): void {
