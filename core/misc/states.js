681877ad6f517ff9a284546379608ab02048a0bf
Nick Procopan <nick@optasy.com>
2024-04-25 16:52:34 +0300

    lint

--- core/misc/states.js
+++ core/misc/states.js
@@ -571,7 +571,8 @@
 
         // For single element, handle based on its type.
         const element = this[0];
-        if (element && element.tagName.toLowerCase() === 'select' && element.multiple) {
+        if (element &&
+          element.tagName.toLowerCase() === 'select' && element.multiple) {
           return Array.from(element.selectedOptions).map(
             (option) => option.value,
           );
@@ -589,7 +590,8 @@
 
         // For single element, handle based on its type.
         const element = this[0];
-        if (element && element.tagName.toLowerCase() === 'select' && element.multiple) {
+        if (element &&
+          element.tagName.toLowerCase() === 'select' && element.multiple) {
           return Array.from(element.selectedOptions).map(
             (option) => option.value,
           );
