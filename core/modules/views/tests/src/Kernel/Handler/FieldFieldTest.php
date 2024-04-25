5606cd54f9feb90efd46402a4337234248c5dec5
Stephen Mustgrave <smustgrave@gmail.com>
2024-04-25 10:43:35 -0400

    Remove unused  trait

--- core/modules/views/tests/src/Kernel/Handler/FieldFieldTest.php
+++ core/modules/views/tests/src/Kernel/Handler/FieldFieldTest.php
@@ -9,7 +9,6 @@ use Drupal\entity_test\Entity\EntityTest;
 use Drupal\entity_test\Entity\EntityTestRev;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\user\Entity\User;
 use Drupal\views\Plugin\views\field\EntityField;
 use Drupal\Tests\views\Kernel\ViewsKernelTestBase;
@@ -24,8 +23,6 @@ use Drupal\views\Views;
  * @group #slow
  */
 class FieldFieldTest extends ViewsKernelTestBase {
-
-  use UserCreationTrait;
 
   /**
    * {@inheritdoc}
