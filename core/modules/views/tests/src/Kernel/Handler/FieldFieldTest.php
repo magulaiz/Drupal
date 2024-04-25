85607a2e95fe520067d69ea9ae92f1059ec2aee7
Stephen Mustgrave <smustgrave@gmail.com>
2024-04-25 10:49:41 -0400

    Undo my change

--- core/modules/views/tests/src/Kernel/Handler/FieldFieldTest.php
+++ core/modules/views/tests/src/Kernel/Handler/FieldFieldTest.php
@@ -9,6 +9,7 @@ use Drupal\entity_test\Entity\EntityTest;
 use Drupal\entity_test\Entity\EntityTestRev;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\user\Entity\User;
 use Drupal\views\Plugin\views\field\EntityField;
 use Drupal\Tests\views\Kernel\ViewsKernelTestBase;
@@ -23,6 +24,8 @@ use Drupal\views\Views;
  * @group #slow
  */
 class FieldFieldTest extends ViewsKernelTestBase {
+
+  use UserCreationTrait;
 
   /**
    * {@inheritdoc}
