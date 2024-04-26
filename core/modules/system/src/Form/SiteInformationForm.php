f63b7361103d4b4d6cdbf19b282bee1540cb5386
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-04-26 21:47:16 +0530

    Configtarget used in SiteInformationForm

--- core/modules/system/src/Form/SiteInformationForm.php
+++ core/modules/system/src/Form/SiteInformationForm.php
@@ -108,7 +108,11 @@ class SiteInformationForm extends ConfigFormBase {
     $form['site_information']['site_slogan'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Slogan'),
-      '#config_target' => 'system.site:slogan',
+      '#config_target' => new ConfigTarget(
+        'system.site',
+        'slogan',
+        toConfig: fn(?string $value) => $value ?: NULL
+      ),
       '#description' => $this->t("How this is used depends on your site's theme."),
       '#maxlength' => 255,
     ];
@@ -167,25 +171,8 @@ class SiteInformationForm extends ConfigFormBase {
     // Get the normal path of the front page.
     $form_state->setValueForElement($form['front_page']['site_frontpage'], $this->aliasManager->getPathByAlias($form_state->getValue('site_frontpage')));
     // Validate front page path.
-    if (($value = $form_state->getValue('site_frontpage')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_frontpage', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_frontpage')]));
-
-    }
     if (!$this->pathValidator->isValid($form_state->getValue('site_frontpage'))) {
       $form_state->setErrorByName('site_frontpage', $this->t("Either the path '%path' is invalid or you do not have access to it.", ['%path' => $form_state->getValue('site_frontpage')]));
-    }
-    // Get the normal paths of both error pages.
-    if (!$form_state->isValueEmpty('site_403')) {
-      $form_state->setValueForElement($form['error_page']['site_403'], $this->aliasManager->getPathByAlias($form_state->getValue('site_403')));
-    }
-    if (!$form_state->isValueEmpty('site_404')) {
-      $form_state->setValueForElement($form['error_page']['site_404'], $this->aliasManager->getPathByAlias($form_state->getValue('site_404')));
-    }
-    if (($value = $form_state->getValue('site_403')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_403', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_403')]));
-    }
-    if (($value = $form_state->getValue('site_404')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_404', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_404')]));
     }
     // Validate 403 error path.
     if (!$form_state->isValueEmpty('site_403') && !$this->pathValidator->isValid($form_state->getValue('site_403'))) {
