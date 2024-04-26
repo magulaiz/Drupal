5b71e9f001d4c66ca231b2f1019c57fadf5f8cc0
Michael Strelan <28650-mstrelan@users.noreply.drupalcode.org>
2024-04-26 13:08:54 +0000

    Update server-setup.sh

--- .gitlab-ci/scripts/server-setup.sh
+++ .gitlab-ci/scripts/server-setup.sh
@@ -2,6 +2,6 @@
 
 ln -s $CI_PROJECT_DIR /var/www/html
 # sudo service apache2 start
-mkdir -p ./sites/simpletest ./sites/default/files ./build/logs/junit /var/www/.composer
-chown -R www-data:www-data ./sites ./build/logs/junit ./vendor /var/www/
-sudo -u www-data git config --global --add safe.directory $CI_PROJECT_DIR
+# mkdir -p ./sites/simpletest ./sites/default/files ./build/logs/junit /var/www/.composer
+# chown -R www-data:www-data ./sites ./build/logs/junit ./vendor /var/www/
+# sudo -u www-data git config --global --add safe.directory $CI_PROJECT_DIR
