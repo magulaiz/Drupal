b4254488a30e3ca6d5709035dc9eedd972391bd0
Brad Jones <brad@bradjonesllc.com>
2024-04-28 14:47:20 -0600

    Remove deprecations for 11.0

--- core/modules/serialization/src/Normalizer/NormalizerBase.php
+++ core/modules/serialization/src/Normalizer/NormalizerBase.php
@@ -106,15 +106,6 @@ abstract class NormalizerBase implements SerializerAwareInterface, CacheableNorm
   /**
    * {@inheritdoc}
    */
-  public function hasCacheableSupportsMethod(): bool {
-    @trigger_error(__METHOD__ . '() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Use getSupportedTypes() instead. See https://www.drupal.org/node/3359695', E_USER_DEPRECATED);
-
-    return FALSE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function getSupportedTypes(?string $format): array {
     return [
       '*' => FALSE,
