f32cb46145f3a32881e74ffbf5461a5bf0aaade9
Dave Long <dave@longwaveconsulting.com>
2024-04-29 16:47:25 +0100

    Listen for failure events in PerformanceDataCollector.

--- core/modules/system/tests/modules/performance_test/src/DatabaseEventEnabler.php
+++ core/modules/system/tests/modules/performance_test/src/DatabaseEventEnabler.php
@@ -3,6 +3,7 @@
 namespace Drupal\performance_test;
 
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\Event\StatementExecutionFailureEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +25,7 @@ class DatabaseEventEnabler implements HttpKernelInterface {
         // to the latter event.
         StatementExecutionStartEvent::class,
         StatementExecutionEndEvent::class,
+        StatementExecutionFailureEvent::class,
       ]);
     }
     return $this->httpKernel->handle($request, $type, $catch);
