b8c0567eb280a5531271c06b75acdc9bfc2b03d1
Michael Strelan <28650-mstrelan@users.noreply.drupalcode.org>
2024-05-02 06:39:39 +0000

    Update server-setup.sh

--- .gitlab-ci/scripts/server-setup.sh
+++ .gitlab-ci/scripts/server-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo ln -s $CI_PROJECT_DIR /var/www/html
-# sudo service apache2 start
+/usr/local/bin/frankenphp start --config /etc/caddy/Caddyfile
 sudo mkdir -p ./sites/simpletest ./sites/default/files ./build/logs/junit /var/www/.composer
 # chown -R www-data:www-data ./sites ./build/logs/junit ./vendor /var/www/
 # sudo -u www-data git config --global --add safe.directory $CI_PROJECT_DIR
