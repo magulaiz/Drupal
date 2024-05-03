14e8596a70c52679b043f285f811e1ebd8b643c3
Kristiaan Van den Eynde <kristiaan@factorial.io>
2024-05-03 11:38:14 +0200

    Update link to CR

--- core/lib/Drupal/Core/Theme/Registry.php
+++ core/lib/Drupal/Core/Theme/Registry.php
@@ -199,11 +199,11 @@ class Registry implements DestructableInterface {
     $this->moduleList = $module_list;
     $this->themeName = $theme_name;
     if (!isset($kernel)) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $kernel argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3441222', E_USER_DEPRECATED);
+      @trigger_error('Calling ' . __METHOD__ . '() without the $kernel argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3445054', E_USER_DEPRECATED);
       $this->kernel = \Drupal::service('kernel');
     }
     if (!isset($settings)) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $settings argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3441222', E_USER_DEPRECATED);
+      @trigger_error('Calling ' . __METHOD__ . '() without the $settings argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3445054', E_USER_DEPRECATED);
       $this->settings = \Drupal::service('settings');
     }
   }
