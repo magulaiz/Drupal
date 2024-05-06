6af45f0da6fa49ff5fbacf6bf1366b6f46d91340
binoli-lalani <binoliblalani@gmail.com>
2024-05-06 16:22:58 +0530

    Issue #3183766 by guilhermevp, alecsmrekar, paulocs, djsagar, Abhijith S, jshimota01, ilgnerfagundes: Views Pager - 0 Items is confusing

--- core/modules/views/src/Plugin/views/pager/None.php
+++ core/modules/views/src/Plugin/views/pager/None.php
@@ -33,7 +33,7 @@ class None extends PagerPluginBase {
 
   public function summaryTitle() {
     if (!empty($this->options['offset'])) {
-      return $this->t('All items, skip @skip', ['@skip' => $this->options['offset']]);
+      return $this->t('Except first @skip', ['@skip' => $this->options['offset']]);
     }
     return $this->t('All items');
   }
