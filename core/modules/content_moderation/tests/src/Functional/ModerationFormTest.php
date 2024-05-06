3feeca431a23bdade14ab9dd2159936a2d22c97f
Sudhir Krishna <40153-sudhir@users.noreply.drupalcode.org>
2024-05-06 11:24:03 +0530

    reverted unwanted change

--- core/modules/content_moderation/tests/src/Functional/ModerationFormTest.php
+++ core/modules/content_moderation/tests/src/Functional/ModerationFormTest.php
@@ -200,7 +200,6 @@ class ModerationFormTest extends ModerationStateTestBase {
    */
   public function testNonBundleModerationForm() {
     $this->drupalLogin($this->rootUser);
-    $this->drupalGet('/admin/reports/dblog');
     $this->workflow->getTypePlugin()->addEntityTypeAndBundle('entity_test_mulrevpub', 'entity_test_mulrevpub');
     $this->workflow->save();
 
