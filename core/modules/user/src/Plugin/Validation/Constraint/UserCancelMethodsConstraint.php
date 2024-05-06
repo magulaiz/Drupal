0c6085525ef68dd997bc8c6c87d7b80084a4cfc4
omkar podey <omkarpodey@gmail.com>
2024-05-06 09:22:26 +0530

    alternative

--- core/modules/user/src/Plugin/Validation/Constraint/UserCancelMethodsConstraint.php
+++ core/modules/user/src/Plugin/Validation/Constraint/UserCancelMethodsConstraint.php
@@ -20,7 +20,7 @@ class UserCancelMethodsConstraint implements ContainerFactoryPluginInterface {
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): Choice {
-    $configuration['choices'] = array_keys(get_user_cancel_methods_array());
+    $configuration['choices'] = array_keys(user_cancel_methods()['#options']);
     return new Choice($configuration);
   }
 
