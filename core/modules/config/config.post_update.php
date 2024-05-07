02240a3853c97a556082aae5e2353e0f41ce1ace
omkar podey <omkarpodey@gmail.com>
2024-05-07 18:52:53 +0530

    event listener + post update

--- core/modules/config/config.post_update.php
+++ core/modules/config/config.post_update.php
@@ -6,14 +6,11 @@
  */
 
 /**
- * Fix core.menu.static_menu_link_overrides:definitions.*.parent value to null.
+ * Load the config and save to trigger event listener.
+ *
+ * @see \Drupal\config\EventSubscriber\MenuParentUpdate
  */
 function config_post_update_set_menu_parent_value_to_null(): void {
   $config = \Drupal::configFactory()->getEditable('core.menu.static_menu_link_overrides');
-  $all_overrides = $config->get('definitions') ?: [];
-  foreach ($all_overrides as $definition_key => $definition_value) {
-    if ($definition_value['parent'] === '') {
-      $config->set('definitions.' . $definition_key . '.parent', NULL)->save();
-    }
-  }
+  $config->save();
 }
---
+++ core/modules/config/src/EventSubscriber/MenuParentUpdate.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\config\EventSubscriber;
+
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigEvents;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Fix core.menu.static_menu_link_overrides:definitions.*.parent value to null.
+ */
+class MenuParentUpdate implements EventSubscriberInterface {
+
+  /**
+   * The config.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
+   * Constructs a MenuParentUpdate object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Fix core.menu.static_menu_link_overrides:definitions.*.parent value to null.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   The Event to process.
+   */
+  public function onSave(ConfigCrudEvent $event) {
+    $saved_config = $event->getConfig();
+    if ($saved_config->getName() === 'core.menu.static_menu_link_overrides') {
+      $all_overrides = $saved_config->get('definitions') ?: [];
+      foreach ($all_overrides as $definition_key => $definition_value) {
+        if ($definition_value['parent'] === '') {
+          $saved_config->set('definitions.' . $definition_key . '.parent', NULL)->save();
+        }
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    $events[ConfigEvents::SAVE][] = ['onSave'];
+    return $events;
+  }
+
+}
