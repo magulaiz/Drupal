8aacf23e7cd2bd4b526ee3bfba3a3a3ba38c782f
HARUMI JANG <harumi.jang@acquia.com>
2024-05-07 16:01:26 -0400

    Add react and react-dom as externals

--- core/themes/claro/js/react-test.js
+++ core/themes/claro/js/react-test.js
@@ -1,13 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import Babel from '@babel/standalone';
 // import Foo from 'foo';
 // import {
 //   createRoot
 // } from '../../../assets/vendor/react-dom/react-dom.development';
 
-const {parser, generator} = Babel.packages;
-const {transformFromAst, transform} = Babel;
+// const {parser, generator} = Babel.packages;
+// const {transformFromAst, transform} = Babel;
 
 const jsx = `<h1 style={{marginLeft: '80px'}} onClick={() => console.log('clicked')}>Hello world!</h1>`;
 
