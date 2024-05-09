29dbda9f9697072f2cee853384ef73509c7fac17
Mingsong Hu <mingsonghu@osb-macpro-v2nq.lan>
2024-05-10 02:14:09 +1000

    Fix SmartDefaultSettingsTest failure

--- core/modules/ckeditor5/src/Plugin/Validation/Constraint/SourceEditingRedundantTagsConstraintValidator.php
+++ core/modules/ckeditor5/src/Plugin/Validation/Constraint/SourceEditingRedundantTagsConstraintValidator.php
@@ -188,7 +188,7 @@ class SourceEditingRedundantTagsConstraintValidator extends ConstraintValidator 
     $message_string = '';
 
     foreach ($plugin_definitions as $definition) {
-      if ($definition->hasElements() && !$definition->hasConditions() && $definition->hasToolbarItems()) {
+      if ($definition->hasElements() && $definition->hasToolbarItems()) {
         $plugin_capabilities = HTMLRestrictions::fromString(implode(' ', $definition->getElements()));
 
         // Intersection in the overlap and the plugin definition,
