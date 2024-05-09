ce993958e50683814e3d34556bfe79dc15368a12
recrit <34538-recrit@users.noreply.drupalcode.org>
2024-05-09 15:23:25 -0400

    visit the english page to test it without assuming that is where we're at after saving a translation

--- core/modules/image/tests/src/Functional/ImageOnTranslatedEntityTest.php
+++ core/modules/image/tests/src/Functional/ImageOnTranslatedEntityTest.php
@@ -165,6 +165,7 @@ class ImageOnTranslatedEntityTest extends ImageFieldTestBase {
     $this->assertTrue($file->isPermanent(), 'First file still exists and is permanent.');
     // This inspects the HTML after the post of the translation, the image
     // should be displayed on the original node.
+    $this->drupalGet('node/' . $default_language_node->id());
     $this->assertSession()->responseContains('alt="Lost in translation image"');
     $this->assertSession()->responseContains('title="Lost in translation image title"');
     // View the translated node.
