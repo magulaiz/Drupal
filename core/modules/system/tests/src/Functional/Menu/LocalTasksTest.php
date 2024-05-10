f23d1b0d95e6274adf41828a21f6c952868f07b2
Tyler <66377-NexusNovaz@users.noreply.drupalcode.org>
2024-05-10 17:02:15 +0100

    Issue #3443748: Added administer account settings permission to testLocalTaskBlockCache

--- core/modules/system/tests/src/Functional/Menu/LocalTasksTest.php
+++ core/modules/system/tests/src/Functional/Menu/LocalTasksTest.php
@@ -263,6 +263,7 @@ class LocalTasksTest extends BrowserTestBase {
     $this->drupalLogin($this->drupalCreateUser([
       'administer content types',
       'administer permissions',
+      'administer account settings'
     ]));
     $this->drupalCreateContentType(['type' => 'page']);
 
