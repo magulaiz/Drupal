5febfd150e293d1627eb2caa3b674f6667541c87
Sudhir Krishna <40153-sudhir@users.noreply.drupalcode.org>
2024-05-13 15:06:56 +0530

    test refactored

--- core/modules/system/tests/modules/form_test/src/Form/JavascriptStatesForm.php
+++ core/modules/system/tests/modules/form_test/src/Form/JavascriptStatesForm.php
@@ -730,17 +730,12 @@ class JavascriptStatesForm extends FormBase {
       '#title' => 'Enable textarea',
     ];
 
-    $form['password_generate'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Generate password'),
-      '#default_value' => 1,
-    ];
     $form['password_confirm'] = [
       '#title' => $this->t('Enter password'),
       '#type' => 'password_confirm',
       '#states' => [
         'visible' => [
-          ':input[name="password_generate"]' => ['checked' => FALSE],
+          ':input[name="checkbox_trigger"]' => ['checked' => FALSE],
         ],
       ],
     ];
