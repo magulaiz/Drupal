0c01832c0eec99eb38fc5c88d1e7b0a453034f2b
Kristiaan Van den Eynde <kristiaan@factorial.io>
2024-05-13 13:52:44 +0200

    Change order of parameters so it's nicer post-deprecation

--- core/lib/Drupal/Core/Theme/Registry.php
+++ core/lib/Drupal/Core/Theme/Registry.php
@@ -181,14 +181,14 @@ class Registry implements DestructableInterface {
    *   The cache backend interface to use for the runtime theme registry data.
    * @param \Drupal\Core\Extension\ModuleExtensionList $module_list
    *   The module list.
-   * @param string $theme_name
-   *   (optional) The name of the theme for which to construct the registry.
    * @param \Symfony\Component\HttpKernel\HttpKernelInterface|null $kernel
    *   The kernel.
    * @param \Drupal\Core\Site\Settings|null $settings
    *   The settings.
+   * @param string $theme_name
+   *   (optional) The name of the theme for which to construct the registry.
    */
-  public function __construct($root, CacheBackendInterface $cache, LockBackendInterface $lock, ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler, ThemeInitializationInterface $theme_initialization, CacheBackendInterface $runtime_cache, ModuleExtensionList $module_list, $theme_name = NULL, protected ?HttpKernelInterface $kernel = NULL, protected ?Settings $settings = NULL) {
+  public function __construct($root, CacheBackendInterface $cache, LockBackendInterface $lock, ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler, ThemeInitializationInterface $theme_initialization, CacheBackendInterface $runtime_cache, ModuleExtensionList $module_list, protected ?HttpKernelInterface $kernel = NULL, protected ?Settings $settings = NULL, $theme_name = NULL) {
     $this->root = $root;
     $this->cache = $cache;
     $this->lock = $lock;
@@ -198,14 +198,6 @@ class Registry implements DestructableInterface {
     $this->runtimeCache = $runtime_cache;
     $this->moduleList = $module_list;
     $this->themeName = $theme_name;
-    if (!isset($kernel)) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $kernel argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3445054', E_USER_DEPRECATED);
-      $this->kernel = \Drupal::service('kernel');
-    }
-    if (!isset($settings)) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $settings argument is deprecated in drupal:10.3.0 and will be required in drupal:11.0.0. See https://www.drupal.org/node/3445054', E_USER_DEPRECATED);
-      $this->settings = \Drupal::service('settings');
-    }
   }
 
   /**
