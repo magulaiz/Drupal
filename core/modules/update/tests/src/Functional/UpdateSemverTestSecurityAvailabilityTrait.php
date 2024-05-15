bcfc913e2fb9d165181847757f661e8a2d89b6a4
quietone <quietone@2572884.no-reply.drupal.org>
2024-05-15 18:27:45 +1200

    Fix fixture and modify test for edge case

--- core/modules/update/tests/src/Functional/UpdateSemverTestSecurityAvailabilityTrait.php
+++ core/modules/update/tests/src/Functional/UpdateSemverTestSecurityAvailabilityTrait.php
@@ -26,7 +26,12 @@ trait UpdateSemverTestSecurityAvailabilityTrait {
   public function testSecurityUpdateAvailability($site_patch_version, array $expected_security_releases, $expected_update_message_type, $fixture) {
     $this->setProjectInstalledVersion("8.$site_patch_version");
     $this->refreshUpdateStatus([$this->updateProject => $fixture]);
-    $this->assertSecurityUpdates("{$this->updateProject}-8", $expected_security_releases, $expected_update_message_type, $this->updateTableLocator);
+    $multiple_security_no_insecure = FALSE;
+    if ($fixture == 'sec.8.0.0_8.0.2') {
+      // This is testing an edge case.
+      $multiple_security_no_insecure = TRUE;
+    }
+    $this->assertSecurityUpdates("{$this->updateProject}-8", $expected_security_releases, $expected_update_message_type, $this->updateTableLocator, $multiple_security_no_insecure);
   }
 
   /**
--- core/modules/update/tests/src/Functional/UpdateTestBase.php
+++ core/modules/update/tests/src/Functional/UpdateTestBase.php
@@ -110,8 +110,14 @@ abstract class UpdateTestBase extends BrowserTestBase {
    *   The type of update message expected.
    * @param string $update_element_css_locator
    *   The CSS locator for the page element that contains the security updates.
+   * @param bool $multiple_security_no_insecure
+   *   A flag to indicate that there are multiple security releases and none of
+   *   tagged is 'Insecure'. That is an edge case that should not happen. When
+   *   there are multiple security releases all but the latest one should be
+   *   tagged 'Insecure'. This flag allows testing of this edge case. Defaults
+   *   to FALSE.
    */
-  protected function assertSecurityUpdates($project_path_part, array $expected_security_releases, $expected_update_message_type, $update_element_css_locator) {
+  protected function assertSecurityUpdates($project_path_part, array $expected_security_releases, $expected_update_message_type, $update_element_css_locator, $multiple_security_no_insecure = FALSE): void {
     $assert_session = $this->assertSession();
     $page = $this->getSession()->getPage();
     $this->standardTests();
@@ -141,8 +147,17 @@ abstract class UpdateTestBase extends BrowserTestBase {
         $this->assertContains($release_url, $all_security_release_urls, "Release $release_url is a security release link.");
         $assert_session->linkByHrefExists($release_url);
       }
-      // Ensure no other links are shown as security releases.
-      $this->assertEquals([], array_diff($all_security_release_urls, $expected_release_urls));
+      if ($multiple_security_no_insecure) {
+        $security_releases = [
+          'http://example.com/drupal-8-0-1-release',
+          'http://example.com/drupal-8-0-1-release',
+        ];
+        $this->assertEquals($security_releases, array_values(array_diff($all_security_release_urls, $expected_release_urls)));
+      }
+      else {
+        // Ensure no other links are shown as security releases.
+        $this->assertEquals([], array_diff($all_security_release_urls, $expected_release_urls));
+      }
     }
     else {
       // Ensure there were no security links.
