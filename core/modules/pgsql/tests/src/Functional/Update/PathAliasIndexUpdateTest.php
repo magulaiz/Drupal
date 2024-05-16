fc82accad27866ff0a86687bc028b496a119d301
M Parker <mparker17@536298.no-reply.drupal.org>
2024-05-16 16:08:53 -0400

    PathAliasIndexUpdateTest needs updated path to test fixture.

--- core/modules/pgsql/tests/src/Functional/Update/PathAliasIndexUpdateTest.php
+++ core/modules/pgsql/tests/src/Functional/Update/PathAliasIndexUpdateTest.php
@@ -20,7 +20,7 @@ class PathAliasIndexUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles(): void {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz',
     ];
   }
 
