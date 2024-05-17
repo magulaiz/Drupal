363a648c1648104bd8b91cb6b3c7919ed94601fb
Andrew Chappell <acha5066@gmail.com>
2024-05-17 16:20:20 +1000

    Revert "Disable run in separate process to try and see actual error message"
    This reverts commit 236da4f955c993fcc5a7adcf3a6974f915d83f93.

--- core/modules/migrate/tests/src/Kernel/MigrateEventsTest.php
+++ core/modules/migrate/tests/src/Kernel/MigrateEventsTest.php
@@ -41,11 +41,6 @@ class MigrateEventsTest extends KernelTestBase {
   protected static $modules = ['migrate', 'migrate_events_test'];
 
   /**
-   * Temporary disable to see actual error.
-   */
-  protected $runTestInSeparateProcess = FALSE;
-
-  /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
