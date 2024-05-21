42f8ee0da61ddd0eda55d7c8b7207ea7693d9a0a
Julian Pustkuchen <3110-Anybody@users.noreply.drupalcode.org>
2024-05-21 15:08:53 +0000

    Added missing break and comment to handle empty path prefix.

--- core/modules/language/src/Plugin/LanguageNegotiation/LanguageNegotiationUrl.php
+++ core/modules/language/src/Plugin/LanguageNegotiation/LanguageNegotiationUrl.php
@@ -73,9 +73,11 @@ class LanguageNegotiationUrl extends LanguageNegotiationMethodBase implements In
             $langcode = $negotiated_language->getId();
           }
 
+          // Handle empty path prefix:
           if ($negotiated_language === FALSE && (($no_prefix_langcode = array_search('', $config['prefixes'])) !== FALSE)) {
             $langcode = $no_prefix_langcode;
           }
+          break;
 
         case LanguageNegotiationUrl::CONFIG_DOMAIN:
           // Get only the host, not the port.
