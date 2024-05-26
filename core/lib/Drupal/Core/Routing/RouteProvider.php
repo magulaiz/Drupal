9aff2ffaddcde8c62ac554176d972b446e636644
Brad Jones <brad@bradjonesllc.com>
2024-05-26 00:58:28 -0600

    Use all() directly.

--- core/lib/Drupal/Core/Routing/RouteProvider.php
+++ core/lib/Drupal/Core/Routing/RouteProvider.php
@@ -481,10 +481,8 @@ class RouteProvider implements CacheableRouteProviderInterface, PreloadableRoute
    */
   protected function getQueryParametersCacheIdPart(Request $request) {
     $request_query_params = [];
-    foreach ($request->query->keys() as $key) {
-      // Must call ::all() here instead of ::get() to handle arrays.
-      $val = $request->query->all($key);
-      $request_query_params[] = $key . '=' . json_encode($val);
+    foreach ($request->query->all() as $key => $val) {
+      $request_query_params[] = $key . '=' . (is_string($val) ? $val : json_encode($val));
     }
     return implode(',', array_filter([$request->getQueryString(), implode('&', $request_query_params)]));
   }
