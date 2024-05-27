15b633f7d25664909e3cd970d933afa9cfad92c5
Stefan Borchert <stefan@undpaul.de>
2024-05-27 08:45:04 +0200

    Issue #3449991 by stBorchert: Upsert::preExecute always returns TRUE which leads to Exception on empty insert

--- core/tests/Drupal/KernelTests/Core/Database/UpsertTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/UpsertTest.php
@@ -59,6 +59,21 @@ class UpsertTest extends DatabaseTestBase {
   }
 
   /**
+   * Confirms executing the query without values causes no exception.
+   */
+  public function testUpsertNoValues(): void {
+    $connection = Database::getConnection();
+
+    // Define fields only but no values.
+    $upsert = $connection->upsert('test_people')
+      ->key('job')
+      ->fields(['job', 'age', 'name']);
+
+    $result = $upsert->execute();
+    $this->assertNull($result);
+  }
+
+  /**
    * Confirms that we can upsert records with keywords successfully.
    */
   public function testUpsertWithKeywords() {
