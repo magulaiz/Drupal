9efa79c226eeade12ebbbe2928e2a9698f3ef864
Kim Pepper <kim@previousnext.com.au>
2024-05-29 15:58:36 +1000

    Fix syntax

--- core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
+++ core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
@@ -42,7 +42,7 @@ services:
     arguments: [!tagged_iterator foo.bar]"
   example_service_closure:
     class: \Drupal\Core\ExampleClass
-    arguments: [!service_closure foo.bar]"
+    arguments: [!service_closure '@example_service_1']"
 YAML;
 
     vfsStream::setup('drupal', NULL, [
