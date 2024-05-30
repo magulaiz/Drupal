fcdf7672ceda120a3b65db167839d9f40a1e6e5d
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2024-05-30 09:03:05 +0000

    Applied Adam's suggestions

--- core/modules/system/system.post_update.php
+++ core/modules/system/system.post_update.php
@@ -110,12 +110,12 @@ function system_post_update_remove_path_key(): void {
 /**
  * Updates all entity_form_mode configuration entities to set description from empty string to null.
  */
-function system_post_update_convert_empty_string_entity_form_modes_to_null(array &$sandbox): void {
+function system_post_update_convert_empty_description_entity_form_modes_to_null(array &$sandbox): void {
   \Drupal::classResolver(ConfigEntityUpdater::class)
     ->update($sandbox, 'entity_form_mode', function (EntityFormModeInterface $form_mode): bool {
       // Entity form mode's `description` field must be stored as NULL at the
       // config level if they are empty.
-      if (trim($form_mode->getDescription()) === '') {
+      if (trim($form_mode->get('description')) === '') {
         $form_mode->set('description', NULL);
         return TRUE;
       }
