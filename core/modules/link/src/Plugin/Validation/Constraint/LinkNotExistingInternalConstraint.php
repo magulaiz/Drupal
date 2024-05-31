1bbdc2df547d3ce8c6173680013b12f12cb13bf2
Benji Fisher <benji@FisherFam.org>
2024-05-31 12:40:03 -0400

    Issue #3262935, Comment #34

--- core/modules/link/src/Plugin/Validation/Constraint/LinkNotExistingInternalConstraint.php
+++ core/modules/link/src/Plugin/Validation/Constraint/LinkNotExistingInternalConstraint.php
@@ -15,6 +15,25 @@ use Symfony\Component\Validator\Constraint as SymfonyConstraint;
 )]
 class LinkNotExistingInternalConstraint extends SymfonyConstraint {
 
-  public $message = "The path '@uri' is invalid.";
+  /**
+   * Message when the path doesn't exist.
+   *
+   * @var string
+   */
+  public string $notFoundMessage = "The path '@uri' doesn't exist.";
+
+  /**
+   * Message when a parameter is not valid.
+   *
+   * @var string
+   */
+  public string $invalidParameterMessage = "The path '@uri' has an invalid parameter.";
+
+  /**
+   * Message when the path is missing mandatory parameters.
+   *
+   * @var string
+   */
+  public string $missingParameterMessage = "The path '@uri' is missing a required parameter.";
 
 }
