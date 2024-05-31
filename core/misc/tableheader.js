783b86e4987e55d05212a0a862a2723769cc8329
Yash Rode <yashrode774@gmail.com>
2024-05-31 14:55:59 +0530

    used Drupal.behaviors

--- core/misc/tableheader.js
+++ core/misc/tableheader.js
@@ -1,32 +1,24 @@
-
-// Example Drupal theme function for sticky header checkbox
+(function (Drupal, once) {
 Drupal.theme.stickyHeaderCheckbox = function () {
   return `<div class="tableheader-toggle-sticky">
             <label>
               <input type="checkbox" data-drupal-toggle-sticky-header checked/>
-              Sticky Header
+              ${Drupal.t('Sticky Header')}
             </label>
           </div>`;
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-  const stickyHeaderElement = document.getElementsByClassName('views-table')[0];
-
-  if (stickyHeaderElement) {
-    stickyHeaderElement.insertAdjacentHTML('beforebegin', Drupal.theme.stickyHeaderCheckbox());
-
-    const checkbox = document.querySelector('[data-drupal-toggle-sticky-header]');
-
-    checkbox.addEventListener('change', function () {
-      if (checkbox.checked) {
-        if (!stickyHeaderElement.classList.contains('sticky-header')) {
-          stickyHeaderElement.classList.add('sticky-header');
-        }
-      } else {
-         if (stickyHeaderElement.classList.contains('sticky-header')) {
-           stickyHeaderElement.classList.remove('sticky-header');
-         }
-      }
-    });
-  }
-});
+Drupal.behaviors.makeStikcyOptional = {
+  attach(context) {
+    const stickyHeaderElement = once('makeStikcyOptional', '.views-table', context).shift();
+    if (stickyHeaderElement) {
+      stickyHeaderElement.insertAdjacentHTML('beforebegin', Drupal.theme.stickyHeaderCheckbox());
+      const previousElement = stickyHeaderElement.previousElementSibling;
+      const checkbox = previousElement.querySelector('input[type="checkbox"]');
+      checkbox.addEventListener('change', () => {
+        stickyHeaderElement.classList.toggle('sticky-header', checkbox.checked);
+      });
+    }
+  },
+};
+}(Drupal, once));
