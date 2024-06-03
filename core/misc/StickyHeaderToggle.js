9685fc7c980ea608b3832f890ac4feade7292023
yashrode774 <yashrode774@gmail.com>
2024-06-03 16:31:19 +0530

    lint fix

--- core/misc/StickyHeaderToggle.js
+++ core/misc/StickyHeaderToggle.js
@@ -1,32 +1,45 @@
 (function (Drupal, once) {
-Drupal.theme.stickyHeaderCheckbox = function () {
-  return `<div class="tableheader-toggle-sticky">
+  Drupal.theme.stickyHeaderCheckbox = function () {
+    return `<div class="tableheader-toggle-sticky">
             <label>
               <input type="checkbox" data-drupal-toggle-sticky-header checked/>
               ${Drupal.t('Sticky Header')}
             </label>
           </div>`;
-};
+  };
 
-Drupal.behaviors.makeStikcyOptional = {
-  attach(context) {
-    const stickyHeaderElement = once('makeStikcyOptional', '.views-table', context).shift();
-    if (stickyHeaderElement) {
-      stickyHeaderElement.insertAdjacentHTML('beforebegin', Drupal.theme.stickyHeaderCheckbox());
-      const previousElement = stickyHeaderElement.previousElementSibling;
-      const checkbox = previousElement.querySelector('input[type="checkbox"]');
+  Drupal.behaviors.makeStikcyOptional = {
+    attach(context) {
+      const stickyHeaderElement = once(
+        'makeStikcyOptional',
+        '.views-table',
+        context,
+      ).shift();
+      if (stickyHeaderElement) {
+        stickyHeaderElement.insertAdjacentHTML(
+          'beforebegin',
+          Drupal.theme.stickyHeaderCheckbox(),
+        );
+        const previousElement = stickyHeaderElement.previousElementSibling;
+        const checkbox = previousElement.querySelector(
+          'input[type="checkbox"]',
+        );
 
-      const storedValue = localStorage.getItem('stickyHeaderEnabled');
-      const isStickyHeaderEnabled = storedValue === null ? true : storedValue === 'true';
-      checkbox.checked = isStickyHeaderEnabled;
-      stickyHeaderElement.classList.toggle('sticky-header', isStickyHeaderEnabled);
+        const storedValue = localStorage.getItem('stickyHeaderEnabled');
+        const isStickyHeaderEnabled =
+          storedValue === null ? true : storedValue === 'true';
+        checkbox.checked = isStickyHeaderEnabled;
+        stickyHeaderElement.classList.toggle(
+          'sticky-header',
+          isStickyHeaderEnabled,
+        );
 
-      checkbox.addEventListener('change', () => {
-        const isChecked = checkbox.checked;
-        stickyHeaderElement.classList.toggle('sticky-header', isChecked);
-        localStorage.setItem('stickyHeaderEnabled', isChecked);
-      });
-    }
-  },
-};
-}(Drupal, once));
+        checkbox.addEventListener('change', () => {
+          const isChecked = checkbox.checked;
+          stickyHeaderElement.classList.toggle('sticky-header', isChecked);
+          localStorage.setItem('stickyHeaderEnabled', isChecked);
+        });
+      }
+    },
+  };
+})(Drupal, once);
