97b98c28120a435296d6a7349757820b20af9ae8
Joe Parsons <20385-joegraduate@users.noreply.drupalcode.org>
2024-06-05 01:33:15 +0000

    Remove deprecated legacy interface.

--- core/lib/Drupal/Core/File/MimeType/FallbackMimeTypeGuesser.php
+++ core/lib/Drupal/Core/File/MimeType/FallbackMimeTypeGuesser.php
@@ -2,20 +2,12 @@
 
 namespace Drupal\Core\File\MimeType;
 
-use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface as LegacyMimeTypeGuesserInterface;
 use Symfony\Component\Mime\MimeTypeGuesserInterface;
 
 /**
  * Fallback MIME type guesser that always returns 'application/octet-stream'.
  */
-class FallbackMimeTypeGuesser implements MimeTypeGuesserInterface, LegacyMimeTypeGuesserInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function guess($path) {
-    return $this->guessMimeType($path);
-  }
+class FallbackMimeTypeGuesser implements MimeTypeGuesserInterface {
 
   /**
    * {@inheritdoc}
