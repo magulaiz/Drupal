668eb9efd889f246562a87cd30537551d5c20037
Greg Anderson <greg.anderson@greenknowe.org>
2024-06-06 12:56:55 -0700

    We do not need to calculate the expected driver, it is always mysql.

--- core/modules/mysql/tests/src/Kernel/mysql/SqlModeDeprecationTest.php
+++ core/modules/mysql/tests/src/Kernel/mysql/SqlModeDeprecationTest.php
@@ -22,22 +22,14 @@ class SqlModeDeprecationTest extends KernelTestBase {
     // initialization so to save cycles.
     $this->root = static::getDrupalRoot();
     $connectionInfo = $this->getDatabaseConnectionInfo();
-    $test_class_parts = explode('\\', get_class($this));
-    $expected_provider = $test_class_parts[2] ?? '';
-    for ($i = 3; $i < count($test_class_parts); $i++) {
-      if ($test_class_parts[$i] === 'Kernel') {
-        $expected_driver = $test_class_parts[$i + 1] ?? '';
-        break;
-      }
-    }
-    if ($connectionInfo['default']['driver'] !== $expected_driver) {
-      $this->markTestSkipped("This test only runs for the database driver '$expected_driver'. Current database driver is '{$connectionInfo['default']['driver']}'.");
+    if ($connectionInfo['default']['driver'] !== 'mysql') {
+      $this->markTestSkipped("This test only runs for the database driver 'mysql'. Current database driver is '{$connectionInfo['default']['driver']}'.");
     }
 
     // We expect that getConnection() should trigger a deprecation warning
     // when called, if 'sql_mode' is in use.
-    $this->assertDeprecation('there should be a deprecation message hee');
-    $this->connection = Database::getConnection();
+    $this->expectDeprecation('there should be a deprecation message hee');
+    $connection = Database::getConnection();
   }
 
   /**
