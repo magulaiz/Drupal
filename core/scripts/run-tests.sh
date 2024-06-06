859eb2cf336956b21a081ce99240b5b7c8491415
quietone <33605-quietone@users.noreply.drupalcode.org>
2024-06-06 06:16:40 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: quietone <33605-quietone@users.noreply.drupalcode.org>

--- core/scripts/run-tests.sh
+++ core/scripts/run-tests.sh
@@ -934,7 +934,7 @@ function simpletest_script_get_test_list() {
         $parts = explode('\\', $class);
         return implode('\\', array_slice($parts, 3));
       };
-      return (int) $slice($a) > $slice($b);
+      return $slice($a) > $slice($b) ? 1 : -1;
     });
     // If the tests are not being run in parallel, then ensure slow tests run all
     // together first.
