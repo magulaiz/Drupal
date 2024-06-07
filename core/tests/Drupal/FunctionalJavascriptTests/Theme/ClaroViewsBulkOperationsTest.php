d31172b08f362e3692874a1d82d285ad046ac583
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2024-06-07 10:47:59 +0530

    Refactored tests

--- core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroViewsBulkOperationsTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroViewsBulkOperationsTest.php
@@ -102,7 +102,7 @@ class ClaroViewsBulkOperationsTest extends WebDriverTestBase {
   }
 
   /**
-   * Tests the view bulk operation alter.
+   * Tests the presence of a custom button added by the view bulk operation alteration.
    */
   public function testViewBulkOperationAlter() {
     $this->drupalGet('admin/content');
@@ -114,14 +114,6 @@ class ClaroViewsBulkOperationsTest extends WebDriverTestBase {
     // Assert that the button does not exist.
     $custom_button = $page->find('css', '#edit-custom-action');
     $this->assertEmpty($custom_button);
-
-    \Drupal::service('module_installer')->install(['claro_bulk_view_operation_test']);
-    drupal_flush_all_caches();
-    $this->drupalGet('admin/content');
-    $select_all = $page->find('css', '.select-all > input');
-    $select_all->check();
-    // Assert the button exist after the module is installed.
-    $assert_session->buttonExists('Custom button');
   }
 
 }
