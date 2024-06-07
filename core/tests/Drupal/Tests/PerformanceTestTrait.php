253b0a6f9bc2a2d6a791d38bd340bf382e4a39fb
Dave Long <24510-longwave@users.noreply.drupalcode.org>
2024-06-07 22:41:33 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/tests/Drupal/Tests/PerformanceTestTrait.php
+++ core/tests/Drupal/Tests/PerformanceTestTrait.php
@@ -614,7 +614,7 @@ trait PerformanceTestTrait {
    *   Whether the event was triggered by the database cache implementation.
    */
   protected static function isDatabaseCache(DatabaseEvent $event): bool {
-    // If there is no class, then this is called from a procedural function
+    // If there is no class, then this is called from a procedural function.
     if (isset($event->caller['class'])) {
       $class = str_replace('\\\\', '\\', $event->caller['class']);
       return is_a($class, '\Drupal\Core\Cache\DatabaseBackend', TRUE) || is_a($class, '\Drupal\Core\Cache\DatabaseCacheTagsChecksum', TRUE);
