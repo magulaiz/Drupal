3d47ec88bce75ef74eca5d3236a624eed7433b04
Jerome Schneider <jerome.schneider@tcs.ch>
2024-06-13 14:29:18 +0200

    Issue #3450525: Update eslint report errors

--- core/modules/views/js/ajax_view.js
+++ core/modules/views/js/ajax_view.js
@@ -25,18 +25,35 @@
 
     $('input[name="use_ajax"]', context).each(function () {
       $(this).on('change', function () {
-        if (!$(this).is(':checked')) {
-          $('input[name="use_ajax_options[use_ajax_paging]"]', context).prop('checked', false);
-          $('input[name="use_ajax_options[use_ajax_sorting]"]', context).prop('checked', false);
-          $('input[name="use_ajax_options[use_ajax_exposed_filters]"]', context).prop('checked', false);
+        if (!this.matches(':checked')) {
+          $('input[name="use_ajax_options[use_ajax_paging]"]', context).prop(
+            'checked',
+            false,
+          );
+          $('input[name="use_ajax_options[use_ajax_sorting]"]', context).prop(
+            'checked',
+            false,
+          );
+          $(
+            'input[name="use_ajax_options[use_ajax_exposed_filters]"]',
+            context,
+          ).prop('checked', false);
         } else {
-          $('input[name="use_ajax_options[use_ajax_paging]"]', context).prop('checked', true);
-          $('input[name="use_ajax_options[use_ajax_sorting]"]', context).prop('checked', true);
-          $('input[name="use_ajax_options[use_ajax_exposed_filters]"]', context).prop('checked', true);
+          $('input[name="use_ajax_options[use_ajax_paging]"]', context).prop(
+            'checked',
+            true,
+          );
+          $('input[name="use_ajax_options[use_ajax_sorting]"]', context).prop(
+            'checked',
+            true,
+          );
+          $(
+            'input[name="use_ajax_options[use_ajax_exposed_filters]"]',
+            context,
+          ).prop('checked', true);
         }
       });
     });
-
   };
   Drupal.behaviors.ViewsAjaxView.detach = (context, settings, trigger) => {
     if (trigger === 'unload') {
