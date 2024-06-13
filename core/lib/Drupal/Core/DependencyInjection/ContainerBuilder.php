1a5909b89ffb6b1952f384846c96dee2ee14afee
Alex Pott <alex.a.pott@googlemail.com>
2024-06-13 15:08:47 +0100

    Better fix

--- core/lib/Drupal/Core/DependencyInjection/ContainerBuilder.php
+++ core/lib/Drupal/Core/DependencyInjection/ContainerBuilder.php
@@ -57,8 +57,8 @@ class ContainerBuilder extends SymfonyContainerBuilder implements ContainerInter
    */
   public function setAlias($alias, $id): Alias {
     $alias = parent::setAlias($alias, $id);
-    if ($id instanceof Alias && $id->isPublic()) {
-      $alias->setPublic(TRUE);
+    if ($id instanceof Alias) {
+      $alias->setPublic($id->isPublic());
     }
     elseif ($this->hasDefinition($id)) {
       $alias->setPublic($this->getDefinition($id)->isPublic());
