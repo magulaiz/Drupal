a7fc4ad1a0bb7b733187e29be68ba38b7ea9afb2
Kristiaan Van den Eynde <kristiaan@factorial.io>
2024-06-14 10:17:48 +0200

    First attempt, let's see what CI thinks of it.

--- core/lib/Drupal/Core/Breadcrumb/BreadcrumbBuilderInterface.php
+++ core/lib/Drupal/Core/Breadcrumb/BreadcrumbBuilderInterface.php
@@ -14,15 +14,27 @@ interface BreadcrumbBuilderInterface {
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match.
+   * phpcs:disable Drupal.Commenting
+   * @todo Uncomment new method parameters before drupal:12.0.0.
+   * @see https://www.drupal.org/project/drupal/issues/7654321
+   *
+   * @param \Drupal\Core\Cache\CacheableMetadata $cacheable_metadata
+   *   The cacheable metadata to add to. If your result varies by something, you
+   *   must specify the cache contexts that represent said variability. This
+   *   will automatically be added to the outcome of build().
+   * phpcs:enable
    *
    * @return bool
    *   TRUE if this builder should be used or FALSE to let other builders
    *   decide.
    */
-  public function applies(RouteMatchInterface $route_match);
+  public function applies(RouteMatchInterface $route_match /* , CacheableMetadata $cacheable_metadata */);
 
   /**
    * Builds the breadcrumb.
+   *
+   * There is no need to add any cacheable metadata that was already added in
+   * applies() as that will be automatically added for you.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match.
