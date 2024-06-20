366e9e8e7838ce47846b253878a97decc8e0cbbf
kalash jain <62080-kalashjain72@users.noreply.drupalcode.org>
2024-06-20 12:12:59 +0000

    Update file LinkGenerator.php

--- core/lib/Drupal/Core/Utility/LinkGenerator.php
+++ core/lib/Drupal/Core/Utility/LinkGenerator.php
@@ -124,7 +124,7 @@ class LinkGenerator implements LinkGeneratorInterface {
       // Add a "data-drupal-link-system-path" attribute to let the
       // drupal.active-link library know the path in a standardized manner.
       if ($url->isRouted() && !isset($variables['options']['attributes']['data-drupal-link-system-path'])) {
-        //   see https://www.drupal.org/project/drupal/issues/3443759.
+       
         // Get the route match from the URL.
         $routeMatch = $url->getRouteMatch();
         if ($routeMatch) {
