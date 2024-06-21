dd55f7f9fe57b46ae4b285e13b880a82e10ca74d
James Williams <james.williams@computerminds.co.uk>
2024-06-21 16:03:26 +0100

    Improve code quality

--- core/modules/file/src/Plugin/rest/resource/FileUploadResource.php
+++ core/modules/file/src/Plugin/rest/resource/FileUploadResource.php
@@ -7,7 +7,6 @@ use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\File\Event\FileUploadSanitizeNameEvent;
 use Drupal\Core\File\Exception\FileException;
 use Drupal\Core\File\Exception\FileExistsException;
 use Drupal\Core\File\FileExists;
