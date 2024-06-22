3876014e918ce0064473fcaf7b6be9996df18065
Martin Anderson-Clutz <46883-mandclu@users.noreply.drupalcode.org>
2024-06-22 16:35:42 +0000

    Rebase placeBlock changes onto core

--- core/tests/Drupal/KernelTests/Core/Config/Action/ConfigActionTest.php
+++ core/tests/Drupal/KernelTests/Core/Config/Action/ConfigActionTest.php
@@ -24,7 +24,7 @@ class ConfigActionTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['config_test'];
+  protected static $modules = ['config_test', 'block', 'system'];
 
   /**
    * @see \Drupal\Core\Config\Action\Plugin\ConfigAction\EntityCreate
@@ -319,6 +319,223 @@ class ConfigActionTest extends KernelTestBase {
     $this->expectException(PluginNotFoundException::class);
     $this->expectExceptionMessageMatches('/^The "does_not_exist" plugin does not exist/');
     $this->container->get('plugin.manager.config_action')->applyAction('does_not_exist', 'config_test.system', ['foo' => 'Yay!']);
+  }
+
+  /**
+   * @see \Drupal\Core\Config\Action\Plugin\ConfigAction\PlaceBlock
+   */
+  public function testPlaceBlock(): void {
+    /** @var \Drupal\Core\Config\Action\ConfigActionManager $manager */
+    $manager = $this->container->get('plugin.manager.config_action');
+    // Empty configuration should trigger an error.
+    try {
+      $manager->applyAction('placeBlock', 'block.block.block_test', NULL);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.block_test cannot be created because no configuration was provided', $e->getMessage());
+    }
+
+    // Empty configuration should trigger an error.
+    try {
+      $manager->applyAction('placeBlock', 'block.block.block_test', 'Dummy string');
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.block_test cannot be created because provided configuration is not an array', $e->getMessage());
+    }
+
+    // Non-block configuration should trigger an error.
+    try {
+      $manager->applyAction('placeBlock', 'config_test.dynamic.action_test', ['label' => 'Action test', 'protected_property' => '']);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Provided config entity config_test.dynamic.action_test is not a block', $e->getMessage());
+    }
+
+    // Define an example set of valid configuration.
+    $valid_block = [
+      'theme' => 'olivero',
+      'region' => 'content',
+      'weight' => 0,
+      'provider' => NULL,
+      'id' => 'config_action_test',
+      'plugin' => 'local_tasks_block',
+      'settings' => [
+        'id' => 'local_tasks_block',
+        'label' => 'Additional tabs',
+        'provider' => 'core',
+        'primary' => FALSE,
+        'secondary' => TRUE,
+      ],
+      'visibility' => [],
+    ];
+
+    // Block configuration with no theme specified should trigger an error.
+    $no_theme = $valid_block;
+    unset($no_theme['theme']);
+    try {
+      $manager->applyAction('placeBlock', 'block.block.no_theme', $no_theme);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.no_theme cannot be created because of missing theme identifier', $e->getMessage());
+    }
+
+    // Block configuration with invalid theme specified should trigger an error.
+    $bad_theme = $valid_block;
+    $bad_theme['theme'] = 'mongoose';
+    try {
+      $manager->applyAction('placeBlock', 'block.block.bad_theme', $bad_theme);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.bad_theme cannot be created because the specified theme is missing', $e->getMessage());
+    }
+
+    // Tests below require a theme installed.
+    \Drupal::service('theme_installer')->install(['olivero']);
+    $config = \Drupal::configFactory()->getEditable('system.theme');
+    $config->set('default', 'olivero')->save();
+
+    // Block configuration with no region specified should trigger an error.
+    $no_region = $valid_block;
+    unset($no_region['region']);
+    try {
+      $manager->applyAction('placeBlock', 'block.block.no_region', $no_region);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.no_region cannot be created because a region must be specified', $e->getMessage());
+    }
+
+    // Block configuration with invalid region specified must trigger an error.
+    $bad_region = $valid_block;
+    $bad_region['id'] = 'bad_region';
+    $bad_region['region'] = 'platypus';
+    try {
+      $manager->applyAction('placeBlock', 'block.block.bad_region', $bad_region);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.bad_region could not identify a valid region', $e->getMessage());
+    }
+
+    $bad_region['regions'] = ['nonexistent', 'bogus', 'nonsense'];
+    try {
+      $manager->applyAction('placeBlock', 'block.block.bad_region', $bad_region);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.bad_region could not identify a valid region', $e->getMessage());
+    }
+
+    // Validate that the block can be placed with a valid backup region.
+    $bad_region['regions'][] = 'content';
+    try {
+      $manager->applyAction('placeBlock', 'block.block.bad_region', $bad_region);
+      $placed_blocks = \Drupal::entityTypeManager()->getStorage('block')->loadByProperties([
+        'id' => 'bad_region',
+      ]);
+      $this->assertCount(1, $placed_blocks, 'There is 1 matching block entity');
+    }
+    catch (ConfigActionException $e) {
+      $this->fail('Unexpected exception while placing block');
+    }
+
+    // Validate that the block can be placed with 'default' theme specified.
+    $default_theme_block = $valid_block;
+    $default_theme_block['theme'] = 'default';
+    $default_theme_block['id'] = 'default_theme_block';
+    try {
+      $manager->applyAction('placeBlock', 'block.block.default_theme_block', $default_theme_block);
+      $placed_blocks = \Drupal::entityTypeManager()->getStorage('block')->loadByProperties([
+        'id' => 'olivero_theme_block',
+      ]);
+      $this->assertCount(1, $placed_blocks, 'There is 1 matching block entity');
+      $found_block = array_pop($placed_blocks);
+      $this->assertSame('olivero', $found_block->get('theme'));
+    }
+    catch (ConfigActionException $e) {
+      $this->fail('Unexpected exception while placing block: ' . $e->getMessage());
+    }
+
+    // Block configuration with no plugin specified should trigger an error.
+    $no_plugin = $valid_block;
+    unset($no_plugin['plugin']);
+    try {
+      $manager->applyAction('placeBlock', 'block.block.no_plugin', $no_plugin);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.no_plugin requires a plugin value', $e->getMessage());
+    }
+
+    // Validate that the block can be placed.
+    try {
+      $manager->applyAction('placeBlock', 'block.block.config_action_test', $valid_block);
+      $placed_blocks = \Drupal::entityTypeManager()->getStorage('block')->loadByProperties([
+        'id' => 'config_action_test',
+      ]);
+      $this->assertCount(1, $placed_blocks, 'There is 1 matching block entity');
+    }
+    catch (ConfigActionException $e) {
+      $this->fail('Unexpected exception while placing block');
+    }
+
+    // Validate that the block can be placed without an id, with a valid name.
+    $no_id = $valid_block;
+    unset($no_id['id']);
+    // First, verify that it fails without an available fallback.
+    try {
+      $manager->applyAction('placeBlock', 'block.block.', $no_id);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Unable to determine a valid id for block block.block.', $e->getMessage());
+    }
+    try {
+      $manager->applyAction('placeBlock', 'block.block.no_id', $no_id);
+      $placed_blocks = \Drupal::entityTypeManager()->getStorage('block')->loadByProperties([
+        'id' => 'no_id',
+      ]);
+      $this->assertCount(1, $placed_blocks, 'There is 1 matching block entity');
+    }
+    catch (ConfigActionException $e) {
+      $this->fail('Unexpected exception while placing block');
+    }
+
+    // Placing the same block again should fail.
+    try {
+      $manager->applyAction('placeBlock', 'block.block.config_action_test', $valid_block);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (ConfigActionException $e) {
+      $this->assertSame('Block block.block.config_action_test already exists and cannot be created', $e->getMessage());
+    }
+
+    // Validate that placing blocks first and last works as expected.
+    try {
+      $first_block = $last_block = $valid_block;
+      $last_block['weight'] = 'last';
+      $last_block['id'] = 'config_action_last';
+      $manager->applyAction('placeBlock', 'block.block.config_action_last', $last_block);
+      $first_block['weight'] = 'first';
+      $first_block['id'] = 'config_action_first';
+      $manager->applyAction('placeBlock', 'block.block.config_action_first', $first_block);
+      $placed_blocks = \Drupal::entityTypeManager()->getStorage('block')->loadByProperties([
+        'theme' => 'olivero',
+        'region' => 'content',
+      ]);
+      // These would be out of order if not for the weight keywords.
+      uasort($placed_blocks, 'Drupal\block\Entity\Block::sort');
+      $this->assertSame('config_action_first', array_key_first($placed_blocks));
+      $this->assertSame('config_action_last', array_key_last($placed_blocks));
+    }
+    catch (ConfigActionException $e) {
+      $this->fail('Unexpected exception while placing first and last blocks');
+    }
   }
 
 }
