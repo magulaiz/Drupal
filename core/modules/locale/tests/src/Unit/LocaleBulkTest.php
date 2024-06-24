785a0fe576a450a56669cf9df795c75bde37cdd0
sayan goswami <sayan.goswami@acquia.com>
2024-06-24 21:02:23 +0530

    Fix tests

--- core/modules/locale/tests/src/Unit/LocaleBulkTest.php
+++ core/modules/locale/tests/src/Unit/LocaleBulkTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\locale\Unit;
 
+use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -15,6 +16,16 @@ class LocaleBulkTest extends UnitTestCase {
 
   protected function setUp(): void {
     parent::setUp();
+
+    $container = new ContainerBuilder();
+    $container->set('locale.config_manager', $this->createMock('Drupal\locale\LocaleConfigManager'));
+    $this->languageManager = $this->createMock('Drupal\Core\Language\LanguageManagerInterface');
+    $this->languageManager->expects($this->any())
+      ->method('getDefaultLanguage')
+      ->willReturn($this->createMock('Drupal\Core\Language\LanguageInterface'));
+    $container->set('language_manager', $this->languageManager);
+
+    \Drupal::setContainer($container);
 
     include_once DRUPAL_ROOT . '/core/modules/locale/locale.bulk.inc';
   }
@@ -30,7 +41,7 @@ class LocaleBulkTest extends UnitTestCase {
     $this->expectDeprecation('locale_config_batch_refresh_name() is deprecated in drupal:10.2.3 and is removed from drupal:11.0.0. Use locale_config_batch_update_config_translations() instead. See https://www.drupal.org/project/drupal/issues/3422977');
     $names = ['English', 'German'];
     $langcodes = ['en', 'de'];
-    \locale_config_batch_refresh_name($names, $langcodes, $context);
+    locale_config_batch_refresh_name($names, $langcodes, $context);
   }
 
   /**
@@ -41,7 +52,7 @@ class LocaleBulkTest extends UnitTestCase {
    * @see locale_config_batch_set_config_langcodes()
    */
   public function testDeprecatedLocaleConfigBatchSetConfigLangcodes() {
-    $this->expectDeprecation('locale_config_batch_set_config_langcodes() is deprecated in drupal:10.2.3 and is removed from drupal:11.0.0. Use locale_config_batch_update_config_translations() instead. See https://www.drupal.org/project/drupal/issues/3422977');
+    $this->expectDeprecation('locale_config_batch_set_config_langcodes() is deprecated in drupal:10.2.3 and is removed from drupal:11.0.0. Use locale_config_batch_update_default_config_langcodes() instead. See https://www.drupal.org/project/drupal/issues/3422977');
     $context = [];
     locale_config_batch_set_config_langcodes($context);
   }
