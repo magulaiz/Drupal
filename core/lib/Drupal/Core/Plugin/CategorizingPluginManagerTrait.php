ef53dea208eed41810c58f37ace3eace98cfdeaf
Ethan Teague <13453-EthanT@users.noreply.drupalcode.org>
2024-06-27 20:55:21 +0000

    Update CategorizingPluginManagerTrait.php

--- core/lib/Drupal/Core/Plugin/CategorizingPluginManagerTrait.php
+++ core/lib/Drupal/Core/Plugin/CategorizingPluginManagerTrait.php
@@ -90,7 +90,7 @@ trait CategorizingPluginManagerTrait {
     $definitions = $definitions ?? $this->getDefinitions();
     uasort($definitions, function ($a, $b) use ($label_key) {
       if ((string) $a['category'] != (string) $b['category']) {
-        return strnatcasecmp($a['category'], $b['category']);
+        return strnatcasecmp((string) $a['category'], (string) $b['category']);
       }
       return strnatcasecmp($a[$label_key], $b[$label_key]);
     });
