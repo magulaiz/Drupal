99ba6a99f6ceb06fb6577bc5421d67e9c25b31ab
Pooja Sharma <62756-pooja_sharma@users.noreply.drupalcode.org>
2024-06-27 15:50:41 +0000

    issue/3457651: Remove todo views should expect and store a leading.

--- core/modules/views/src/Plugin/views/field/FieldPluginBase.php
+++ core/modules/views/src/Plugin/views/field/FieldPluginBase.php
@@ -1346,8 +1346,6 @@ abstract class FieldPluginBase extends HandlerBase implements FieldHandlerInterf
           $more_link_path = mb_substr($more_link_path, mb_strlen($base_path));
         }
 
-        // @todo Views should expect and store a leading /. See
-        //   https://www.drupal.org/node/2423913.
         $options = [
           'attributes' => [
             'class' => [
@@ -1484,8 +1482,6 @@ abstract class FieldPluginBase extends HandlerBase implements FieldHandlerInterf
 
     if (empty($alter['url'])) {
       if (!parse_url($path, PHP_URL_SCHEME)) {
-        // @todo Views should expect and store a leading /. See
-        //   https://www.drupal.org/node/2423913.
         $alter['url'] = CoreUrl::fromUserInput('/' . ltrim($path, '/'));
       }
       else {
