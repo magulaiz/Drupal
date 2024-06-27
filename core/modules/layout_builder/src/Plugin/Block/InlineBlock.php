515578a79731e5a2cea841776d7a5da4e91a9dab
Ethan Teague <13453-EthanT@users.noreply.drupalcode.org>
2024-06-27 20:44:44 +0000

    Update InlineBlock.php

--- core/modules/layout_builder/src/Plugin/Block/InlineBlock.php
+++ core/modules/layout_builder/src/Plugin/Block/InlineBlock.php
@@ -215,7 +215,7 @@ class InlineBlock extends BlockBase implements ContainerFactoryPluginInterface, 
    */
   public function build() {
     $block = $this->getEntity();
-    if (empty($block)) {
+    if (is_null($block)) {
       return [];
     }
     return $this->entityTypeManager->getViewBuilder($block->getEntityTypeId())->view($block, $this->configuration['view_mode']);
