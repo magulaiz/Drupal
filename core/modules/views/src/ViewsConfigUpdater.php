6bb7a9fc79a8d0e2790a39b13b4f71748b1bca48
Lennard Swaneveld <lennard@ezcompany.nl>
2024-06-27 15:38:44 +0200

    Clean up CS

--- core/modules/views/src/ViewsConfigUpdater.php
+++ core/modules/views/src/ViewsConfigUpdater.php
@@ -473,7 +473,7 @@ class ViewsConfigUpdater implements ContainerInjectionInterface {
    *   default_argument_skip_url removed.
    */
   public function needsDefaultArgumentSkipUrlUpdate(ViewEntityInterface $view) {
-    return $this->processDisplayHandlers($view, TRUE, function (&$handler, $handler_type) use ($view)  {
+    return $this->processDisplayHandlers($view, TRUE, function (&$handler, $handler_type) use ($view) {
       return $this->processDefaultArgumentSkipUrlUpdate($handler, $handler_type, $view);
     });
   }
