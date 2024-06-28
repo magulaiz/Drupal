2b888c7b7d65d6b6ddf23bbb2fa647b8980e0421
joseph.olstad <joseph.olstad@1321830.no-reply.drupal.org>
2024-06-28 17:08:41 -0400

    Revert "Issue #2107455 by kksandr - Base fields can also have default images, for example they can be easily defined using the core.base_field_override.*.* configuration, so it is better to use entity_field.manager here."
    This reverts commit 5158f6f62a72d5c95fd900c9ef646d90f563a2a4.

--- core/modules/image/src/ImageFieldManager.php
+++ core/modules/image/src/ImageFieldManager.php
@@ -86,7 +86,7 @@ class ImageFieldManager {
         // configuration IDs for quick lookup.
         $defaults = [];
         $fields = $this->entityTypeManager
-          ->getStorage('entity_field.manager')
+          ->getStorage('field_config')
           ->loadMultiple();
 
         foreach ($fields as $field) {
