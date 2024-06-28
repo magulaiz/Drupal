5475113c0a4886150cd23cb179c46fb0f6350f8f
Andy Postnikov <apostnikov@gmail.com>
2024-06-28 16:16:39 +0200

    fix remaining phpcs issues

--- core/modules/node/tests/src/Functional/NodeRevisionsTest.php
+++ core/modules/node/tests/src/Functional/NodeRevisionsTest.php
@@ -209,13 +209,15 @@ class NodeRevisionsTest extends NodeTestBase {
       $prefixed_table = $connection->getPrefix() . 'node';
       $connection->getConnection()->{$prefixed_table}->updateMany(
         [],
-        ['$set' =>
-          ["node_all_revisions.$[revision].revision_timestamp" => new UTCDateTime($old_revision_date * 1000)],
+        [
+          '$set' =>
+            ["node_all_revisions.$[revision].revision_timestamp" => new UTCDateTime($old_revision_date * 1000)],
         ],
-        ['arrayFilters' =>
-          [
-            ["revision.vid" => (int) $nodes[2]->getRevisionId()],
-          ],
+        [
+          'arrayFilters' =>
+            [
+              ["revision.vid" => (int) $nodes[2]->getRevisionId()],
+            ],
         ],
       );
     }
