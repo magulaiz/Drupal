b741da961d7ee810cd3d6cda0f8e5c03f1194210
Claudiu Cristea <clau.cristea@gmail.com>
2024-07-02 00:02:54 +0300

    Use new fixture

--- core/modules/file/tests/src/Functional/Update/FileDescriptionRequiredUpdateTest.php
+++ core/modules/file/tests/src/Functional/Update/FileDescriptionRequiredUpdateTest.php
@@ -20,7 +20,7 @@ class FileDescriptionRequiredUpdateTest extends UpdatePathTestBase {
   protected function setDatabaseDumpFiles() {
     $this->databaseDumpFiles = [
       // Using the 'filled' fixture as 'base' contains no file fields.
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz',
     ];
   }
 
