8953c58e35349c3be9617a32a1ce8e808855d455
Alex Pott <alex.a.pott@googlemail.com>
2024-07-01 13:31:39 +0100

    Add index to locales_location

---
+++ core/modules/locale/tests/src/Functional/LocalesLocationAddIndexUpdateTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\locale\Functional;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+
+/**
+ * Tests Locale update functions.
+ */
+class LocalesLocationAddIndexUpdateTest extends UpdatePathTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles() {
+    $this->databaseDumpFiles[] = $this->root . '/core/modules/system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz';
+  }
+
+  /**
+   * Tests locale_update_10101().
+   *
+   * @see locale_update_10101
+   */
+  public function testIndex(): void {
+    $this->assertFalse(\Drupal::database()
+      ->schema()
+      ->indexExists('locales_location', 'type_name'));
+
+    // Run updates and test them.
+    $this->runUpdates();
+
+    $this->assertTrue(\Drupal::database()
+      ->schema()
+      ->indexExists('locales_location', 'type_name'));
+  }
+
+}
