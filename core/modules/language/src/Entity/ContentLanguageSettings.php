bb0d39d3ccd05229c20785a1879af9f1d6129222
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-07-01 20:31:25 +0530

    Validation added

--- core/modules/language/src/Entity/ContentLanguageSettings.php
+++ core/modules/language/src/Entity/ContentLanguageSettings.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\language\ContentLanguageSettingsException;
 use Drupal\language\ContentLanguageSettingsInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * Defines the ContentLanguageSettings entity.
@@ -238,4 +239,27 @@ class ContentLanguageSettings extends ConfigEntityBase implements ContentLanguag
     ]);
   }
 
+  /**
+   * Validates id of language.content_settings.*.* schema.
+   *
+   * To be used with the `Callback` constraint.
+   *
+   * @param string $value
+   *   The string to validate.
+   * @param \Symfony\Component\Validator\Context\ExecutionContextInterface $context
+   *   The validation execution context.
+   *
+   * @see \Symfony\Component\Validator\Constraints\CallbackValidator
+   * @see core/modules/language/config/schema/language.schema.yml
+   */
+  public static function validateId(string $value, ExecutionContextInterface $context): void {
+    $config_being_validated = $context->getRoot();
+    $target_entity_type_id = $config_being_validated->get('target_entity_type_id')->getValue();
+    $target_bundle = $config_being_validated->get('target_bundle')->getValue();
+    if ($target_entity_type_id . '.' . $target_bundle !== $value) {
+      $context->addViolation('The id %value must be a combination of $target_entity_type_id.$target_bundle.', [
+        '%value' => $value,
+      ]);
+    }
+  }
 }
