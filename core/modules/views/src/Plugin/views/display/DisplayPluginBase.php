53910b66c8236ed955d345e663d6dc4c6b98fde7
catch <6915-catch@users.noreply.drupalcode.org>
2024-07-01 19:08:03 +0100

    Remove more to make phpcs happy.

--- core/modules/views/src/Plugin/views/display/DisplayPluginBase.php
+++ core/modules/views/src/Plugin/views/display/DisplayPluginBase.php
@@ -875,15 +875,11 @@ abstract class DisplayPluginBase extends PluginBase implements DisplayPluginInte
         // If aggregation is on, the group type might override the actual
         // handler that is in use. This piece of code checks that and,
         // if necessary, sets the override handler.
-        $override = NULL;
         if ($this->useGroupBy() && !empty($info['group_type'])) {
           if (empty($this->view->query)) {
             $this->view->initQuery();
           }
           $aggregate = $this->view->query->getAggregationInfo();
-          if (!empty($aggregate[$info['group_type']]['handler'][$type])) {
-            $override = $aggregate[$info['group_type']]['handler'][$type];
-          }
         }
 
         if (!empty($types[$type]['type'])) {
