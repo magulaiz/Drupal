d88d6e0cea8dbbed753c833aa84b7607a3d098cb
Alex Pott <alex.a.pott@googlemail.com>
2024-07-02 17:04:34 +0100

    Add defensive coding

--- core/modules/locale/tests/src/Functional/LocalesLocationAddIndexUpdateTest.php
+++ core/modules/locale/tests/src/Functional/LocalesLocationAddIndexUpdateTest.php
@@ -43,4 +43,72 @@ class LocalesLocationAddIndexUpdateTest extends UpdatePathTestBase {
       ->indexExists('locales_location', 'type_name'));
   }
 
+  /**
+   * Tests locale_update_10300().
+   *
+   * @see locale_update_10300
+   */
+  public function testExistingIndex(): void {
+    $spec = [];
+    $spec['locales_location'] = [
+      'description' => 'Location information for source strings.',
+      'fields' => [
+        'lid' => [
+          'type' => 'serial',
+          'not null' => TRUE,
+          'description' => 'Unique identifier of this location.',
+        ],
+        'sid' => [
+          'type' => 'int',
+          'not null' => TRUE,
+          'description' => 'Unique identifier of this string.',
+        ],
+        'type' => [
+          'type' => 'varchar_ascii',
+          'length' => 50,
+          'not null' => TRUE,
+          'default' => '',
+          'description' => 'The location type (file, config, path, etc).',
+        ],
+        'name' => [
+          'type' => 'varchar',
+          'length' => 255,
+          'not null' => TRUE,
+          'default' => '',
+          'description' => 'Type dependent location information (file name, path, etc).',
+        ],
+        'version' => [
+          'type' => 'varchar_ascii',
+          'length' => 20,
+          'not null' => TRUE,
+          'default' => 'none',
+          'description' => 'Version of Drupal where the location was found.',
+        ],
+      ],
+      'primary key' => ['lid'],
+      'foreign keys' => [
+        'locales_source' => [
+          'table' => 'locales_source',
+          'columns' => ['sid' => 'lid'],
+        ],
+      ],
+      'indexes' => [
+        'string_type' => ['sid', 'type'],
+        'type_name' => ['type', 'name'],
+      ],
+    ];
+    \Drupal::database()->schema()->addIndex('locales_location', 'type_name', ['type', 'name', 'sid'], $spec['locales_location']);
+
+    // Run updates and test them.
+    $this->runUpdates();
+
+    $schema = \Drupal::database()->schema();
+    $this->assertTrue($schema->indexExists('locales_location', 'type_name'));
+
+    // Ensure that index specification matches our expectations.
+    $introspect_index_schema = new \ReflectionMethod(get_class($schema), 'introspectIndexSchema');
+    $index_schema = $introspect_index_schema->invoke($schema, 'locales_location');
+    $this->assertSame(['type', 'name'], $index_schema['indexes']['type_name']);
+  }
+
 }
