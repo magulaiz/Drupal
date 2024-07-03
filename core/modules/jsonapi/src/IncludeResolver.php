7380791fe03749052f1ec32e8b708092d45f3b47
Marcus McDowell <marcusml@axis.com>
2024-07-03 22:56:32 +0200

    Try simpler approach

--- core/modules/jsonapi/src/IncludeResolver.php
+++ core/modules/jsonapi/src/IncludeResolver.php
@@ -148,23 +148,10 @@ class IncludeResolver {
 
           // Support entity reference fields that don't have the referenced
           // target type stored in settings.
-          $target_type = $field_item->entity->getEntityTypeId();
-          $field_item_properties = $field_item->getProperties();
-          if (array_key_exists('target_revision_id', $field_item_properties)) {
-            // Load the revision id.
-            $references[$target_type . ':revision_ids'][] = $field_item->get('target_revision_id')->getValue();
-          }
-          else {
-            $references[$target_type . ':ids'][] = $field_item->get($field_item::mainPropertyName())->getValue();
-          }
+          $references[$field_item->entity->getEntityTypeId()][$field_item->entity->id()] = $field_item->entity;
         }
       }
-      foreach ($references as $target_type_and_rev => $ids) {
-        [$target_type, $revision_type] = explode(':', $target_type_and_rev) + [1 => NULL];
-        $entity_storage = $this->entityTypeManager->getStorage($target_type);
-        $targeted_entities = ($revision_type === 'revision_ids')
-          ? $entity_storage->loadMultipleRevisions($ids)
-          : $entity_storage->loadMultiple(array_unique($ids));
+      foreach ($references as $targeted_entities) {
         $access_checked_entities = array_map(function (EntityInterface $entity) {
           return $this->entityAccessChecker->getAccessCheckedResourceObject($entity);
         }, $targeted_entities);
