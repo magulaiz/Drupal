4b4b618919fba624019006698c9f039b6a6340f9
Roman Paska <40929-Taran2L@users.noreply.drupalcode.org>
2024-07-03 20:46:05 +0000

    Update file LinkPostUpdateTest.php

--- core/modules/link/tests/src/Functional/Update/LinkPostUpdateTest.php
+++ core/modules/link/tests/src/Functional/Update/LinkPostUpdateTest.php
@@ -23,7 +23,7 @@ class LinkPostUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles(): void {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz',
     ];
   }
 
