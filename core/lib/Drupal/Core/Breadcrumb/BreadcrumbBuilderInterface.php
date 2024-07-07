6f7eb1fc77fbab7782f87ea80e489f11e1beb71f
a.barrio <a.barrio@1xinternet.de>
2024-07-07 14:49:52 +0200

    Issue #3459277: Remove cache context when it is not needed and convert parameter on BreadcrumbBuilder required

--- core/lib/Drupal/Core/Breadcrumb/BreadcrumbBuilderInterface.php
+++ core/lib/Drupal/Core/Breadcrumb/BreadcrumbBuilderInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Core\Breadcrumb;
 
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Routing\RouteMatchInterface;
 
 /**
@@ -14,21 +15,17 @@ interface BreadcrumbBuilderInterface {
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match.
-   * phpcs:disable Drupal.Commenting
-   * @todo Uncomment new method parameters before drupal:12.0.0, see
-   *   https://www.drupal.org/project/drupal/issues/3459277.
    *
    * @param \Drupal\Core\Cache\CacheableMetadata $cacheable_metadata
    *   The cacheable metadata to add to if your check varies by or depends
    *   on something. Anything you specify here does not have to be repeated in
    *   the build() method as it will be merged in automatically.
-   * phpcs:enable
    *
    * @return bool
    *   TRUE if this builder should be used or FALSE to let other builders
    *   decide.
    */
-  public function applies(RouteMatchInterface $route_match /* , CacheableMetadata $cacheable_metadata */);
+  public function applies(RouteMatchInterface $route_match, CacheableMetadata $cacheable_metadata);
 
   /**
    * Builds the breadcrumb.
