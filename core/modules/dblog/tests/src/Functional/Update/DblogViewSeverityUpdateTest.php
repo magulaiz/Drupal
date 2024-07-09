9b9eb7912c19b19e19ed61a7c2597ef82d0806f9
Mariano D'Agostino <dagmar@154086.no-reply.drupal.org>
2024-07-09 15:36:18 -0300

    More upgrade fixes

--- core/modules/dblog/tests/src/Functional/Update/DblogViewSeverityUpdateTest.php
+++ core/modules/dblog/tests/src/Functional/Update/DblogViewSeverityUpdateTest.php
@@ -16,7 +16,7 @@ class DblogViewSeverityUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles() {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz',
     ];
   }
 
