511b9aab79e788e182c1e0638640867b97669804
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-07-10 14:26:28 +0100

    Added to docs.

--- sites/example.sites.php
+++ sites/example.sites.php
@@ -19,9 +19,10 @@
  * correct when the site is deployed to a live server.
  *
  * If a test site from a Functional test has been preserved after a test, the
- * site may be accessed by setting a site directory to the test site directory,
- * in the form 'simpletest/TEST_PREFIX'. 'TEST_PREFIX' is the numeric prefix of
- * the test which is also used as part of the test database table prefix.
+ * site may be accessed by setting a site directory (or indeed the main site) to
+ * the test site directory, in the form 'simpletest/TEST_PREFIX'. 'TEST_PREFIX'
+ * is the numeric prefix of the test which is also used as part of the test
+ * database table prefix.
  *
  * To activate this feature, copy and rename it such that its path plus
  * filename is 'sites/sites.php'.
