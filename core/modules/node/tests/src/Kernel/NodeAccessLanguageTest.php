4c6e3e6bae70abd369c24cc7f2991754bef3f850
Pooja Sharma <62756-pooja_sharma@users.noreply.drupalcode.org>
2024-07-12 06:28:12 +0000

    issue/3439903: Updated comments NodeAccessLanguageTest

--- core/modules/node/tests/src/Kernel/NodeAccessLanguageTest.php
+++ core/modules/node/tests/src/Kernel/NodeAccessLanguageTest.php
@@ -184,8 +184,7 @@ class NodeAccessLanguageTest extends NodeAccessTestBase {
     // Create a normal authenticated user.
     $web_user = $this->drupalCreateUser(['access content']);
 
-    // Load the user 1 user for later use as an admin user with permission to
-    // see everything.
+    // Create a user with permission bypass node access to see everything.
     $admin_user = $this->drupalCreateUser([
       'bypass node access',
     ]);
