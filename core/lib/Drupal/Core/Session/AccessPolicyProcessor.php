8ce9d52c04c1bec2d58be99f609f2eba716fa448
Dieter Holvoet <dieter.holvoet@gmail.com>
2024-07-14 11:35:04 +0200

    Fix infinite loop caused by 'user.permissions' cache context.
    If the cacheability metadata includes 'user.permissions', remove it to prevent an infinite loop in PermissionsHashGenerator::getCacheableMetadata().

--- core/lib/Drupal/Core/Session/AccessPolicyProcessor.php
+++ core/lib/Drupal/Core/Session/AccessPolicyProcessor.php
@@ -134,6 +134,14 @@ class AccessPolicyProcessor implements AccessPolicyProcessorInterface {
       // we had stored a CalculatedPermissions object, we would no longer be
       // able to ask for its cache contexts.
       $cacheability = CacheableMetadata::createFromObject($calculated_permissions);
+
+      // If the cacheability metadata contains the 'user.permissions' cache context,
+      // remove it. This cache key invokes PermissionsHashGenerator::getCacheableMetadata(),
+      // which in turn invokes this method, creating an infinite loop.
+      if (in_array('user.permissions', $cacheability->getCacheContexts(), TRUE)) {
+        $cacheability->setCacheContexts(array_diff($cacheability->getCacheContexts(), ['user.permissions']));
+      }
+
       $this->variationCache->set($cache_keys, $calculated_permissions, $cacheability, $initial_cacheability);
 
       // Then convert the calculated permissions to an immutable value object
