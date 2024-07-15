41e69ac27d4bb982ade472769290df5c8c8e9f43
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-07-15 13:07:57 +0530

    phpcs

--- core/modules/block/tests/src/Kernel/BlockValidationTest.php
+++ core/modules/block/tests/src/Kernel/BlockValidationTest.php
@@ -34,7 +34,6 @@ class BlockValidationTest extends ConfigEntityValidationTestBase {
 
   /**
    * {@inheritdoc}
-   *
    */
   protected static array $propertiesWithOptionalValues = [
     'provider',
