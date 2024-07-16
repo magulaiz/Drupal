d2c8be62846c89e0b0f502256518b8bf5a321fc6
catch <6915-catch@users.noreply.drupalcode.org>
2024-07-16 10:07:08 +0100

    Fix deprecation message.

--- core/lib/Drupal/Core/PathProcessor/PathProcessorDecode.php
+++ core/lib/Drupal/Core/PathProcessor/PathProcessorDecode.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
  *   controllers should be required to implement this logic themselves. If we
  *   decide to keep this code, remove this TODO.
  *
- * @deprecated in drupal:10.4.0 and is removed in drupal:12.0.0. There is no
+ * @deprecated in drupal:10.4.0 and is removed from drupal:12.0.0. There is no
  *   replacement.
  */
 class PathProcessorDecode implements InboundPathProcessorInterface {
@@ -26,7 +26,7 @@ class PathProcessorDecode implements InboundPathProcessorInterface {
    * {@inheritdoc}
    */
   public function processInbound($path, Request $request) {
-    trigger_error('Deprecated in drupal:10.4.0 and is removed in drupal:12.0.0. There is no replacement.', E_USER_DEPRECATED);
+    trigger_error('Deprecated in drupal:10.4.0 and is removed from drupal:12.0.0. There is no replacement.', E_USER_DEPRECATED);
     return urldecode($path);
   }
 
