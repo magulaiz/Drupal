852d1a023e1f9fb0e8ea83feee309797b19f239a
catch <6915-catch@users.noreply.drupalcode.org>
2024-07-16 09:56:58 +0100

    Deprecate PathProcessorDecode

--- core/lib/Drupal/Core/PathProcessor/PathProcessorDecode.php
+++ core/lib/Drupal/Core/PathProcessor/PathProcessorDecode.php
@@ -16,6 +16,9 @@ use Symfony\Component\HttpFoundation\Request;
  * @todo Revisit whether or not this logic is appropriate for here or if
  *   controllers should be required to implement this logic themselves. If we
  *   decide to keep this code, remove this TODO.
+ *
+ * @deprecated in drupal:10.4.0 and is removed from drupal:12.0.0. There is no
+ *   replacement.
  */
 class PathProcessorDecode implements InboundPathProcessorInterface {
 
@@ -23,8 +26,8 @@ class PathProcessorDecode implements InboundPathProcessorInterface {
    * {@inheritdoc}
    */
   public function processInbound($path, Request $request) {
-    // Ensure that any URL decoded whitespace is trimmed from the URL.
-    return trim(urldecode($path));
+    trigger_error('Deprecated in drupal:10.4.0 and is removed from drupal:12.0.0. There is no replacement.', E_USER_DEPRECATED);
+    return urldecode($path);
   }
 
 }
