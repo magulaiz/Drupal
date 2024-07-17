f918e1812471781730cf6070ece630242c439d1a
aman.mansuri <aman@cmsminds.com>
2024-07-17 13:22:45 +0530

    phpcs issue for the file

--- core/modules/block_content/src/Controller/BlockContentController.php
+++ core/modules/block_content/src/Controller/BlockContentController.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- *
+ * The controller class.
  */
 class BlockContentController extends ControllerBase {
 
@@ -126,9 +126,9 @@ class BlockContentController extends ControllerBase {
       'type' => $block_content_type->id(),
     ]);
     if (($theme = $request->query->get('theme')) && in_array($theme, array_keys($this->themeHandler->listInfo()))) {
-      // We have navigated to this page from the block library and will keep track
-      // of the theme for redirecting the user to the configuration page for the
-      // newly created block in the given theme.
+      // We have navigated to this page from the block library and will keep
+      // track of the theme for redirecting the user to the configuration page
+      // for the newly created block in the given theme.
       $block->setTheme($theme);
     }
     return $this->entityFormBuilder()->getForm($block);
