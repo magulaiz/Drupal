4e39fb450494511005bbf03c1fef8d5a0c36e4fe
Joe Parsons <parsonsj@arizona.edu>
2024-07-16 17:58:04 -0700

    More test fixes.

--- core/modules/content_moderation/tests/src/Kernel/StateFormatterTest.php
+++ core/modules/content_moderation/tests/src/Kernel/StateFormatterTest.php
@@ -36,6 +36,7 @@ class StateFormatterTest extends KernelTestBase {
 
     $this->installEntitySchema('entity_test_rev');
     $this->installEntitySchema('content_moderation_state');
+    $this->installEntitySchema('action');
     $this->installConfig('content_moderation');
 
     $workflow = $this->createEditorialWorkflow();
