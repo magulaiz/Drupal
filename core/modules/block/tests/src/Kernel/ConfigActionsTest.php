649eea5d849516048748d7346c86e149a8be5ab2
Phéna Proxima <adam@phenaproxima.net>
2024-07-17 11:19:34 -0400

    Test coverage

--- core/modules/block/tests/src/Kernel/ConfigActionsTest.php
+++ core/modules/block/tests/src/Kernel/ConfigActionsTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\block\Kernel;
 
+use Drupal\block\Entity\Block;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ThemeInstallerInterface;
 use Drupal\Core\Recipe\RecipeRunner;
 use Drupal\FunctionalTests\Core\Recipe\RecipeTestTrait;
 use Drupal\KernelTests\KernelTestBase;
@@ -19,12 +22,36 @@ class ConfigActionsTest extends KernelTestBase {
   use RecipeTestTrait;
 
   /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['block', 'user', 'system'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->container->get(ThemeInstallerInterface::class)->install([
+      'olivero',
+      'claro',
+    ]);
+    $this->config('system.theme')
+      ->set('default', 'olivero')
+      ->set('admin', 'claro')
+      ->save();
+
+    // Delete the "powered by" blocks, which we'll restore in our tests.
+    $storage = $this->container->get(EntityTypeManagerInterface::class)
+      ->getStorage('block');
+    $blocks = $storage->loadByProperties(['plugin' => 'system_powered_by_block']);
+    $storage->delete($blocks);
+  }
+
+  /**
    * @testWith ["placeBlockInDefaultTheme"]
    *   ["placeBlockInAdminTheme"]
    */
   public function testActionOnlyWorksOnBlocks(string $action): void {
-    $this->enableModules(['user']);
-
     $recipe = $this->createRecipe(<<<YAML
 name: Targeting the wrong entity type
 config:
@@ -38,10 +65,27 @@ YAML
     RecipeRunner::processRecipe($recipe);
   }
 
-  public function testPlaceBlockInDefaultTheme(): void {
-  }
+  /**
+   * @testWith ["placeBlockInDefaultTheme", "olivero"]
+   *   ["placeBlockInAdminTheme", "claro"]
+   */
+  public function testPlaceBlockInTheme(string $action, string $expected_theme): void {
+    $recipe = $this->createRecipe(<<<YAML
+name: Placing a block
+config:
+  actions:
+    block.block.test_block:
+      $action:
+        plugin: system_powered_by_block
+        region: header
+YAML
+    );
+    RecipeRunner::processRecipe($recipe);
 
-  public function testPlaceBlockInAdminTheme(): void {
+    $block = Block::load('test_block');
+    $this->assertInstanceOf(Block::class, $block);
+    $this->assertSame('system_powered_by_block', $block->getPluginId());
+    $this->assertSame($expected_theme, $block->getTheme());
   }
 
 }
