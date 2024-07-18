e9564777a6a0dd26aadb716d20ae9e5d7fa12c3d
Phéna Proxima <adam@phenaproxima.net>
2024-07-18 16:55:09 -0400

    Fix a few

--- core/modules/block/tests/src/Functional/BlockWeightUpdateTest.php
+++ core/modules/block/tests/src/Functional/BlockWeightUpdateTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Drupal\Tests\block\Functional\Update;
+namespace Drupal\Tests\block\Functional;
 
 use Drupal\block\Entity\Block;
 use Drupal\FunctionalTests\Update\UpdatePathTestBase;
