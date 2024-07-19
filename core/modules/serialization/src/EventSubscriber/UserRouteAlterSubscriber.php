76397df5fa6335633a0beddcb53658cac0649707
Arne Jørgensen <arne@arnested.dk>
2024-07-19 15:27:45 +0200

    Issue #3359649: Deprecated function: explode(): Passing null to parameter #2 ($string) of type string is deprecated in Drupal\serialization\EventSubscriber\UserRouteAlterSubscriber->onRoutingAlterAddFormats()

--- core/modules/serialization/src/EventSubscriber/UserRouteAlterSubscriber.php
+++ core/modules/serialization/src/EventSubscriber/UserRouteAlterSubscriber.php
@@ -52,9 +52,12 @@ class UserRouteAlterSubscriber implements EventSubscriberInterface {
     $routes = $event->getRouteCollection();
     foreach ($route_names as $route_name) {
       if ($route = $routes->get($route_name)) {
-        $formats = explode('|', $route->getRequirement('_format'));
+        $formats = array_filter(explode('|', $route->getRequirement('_format') ?? ''));
         $formats = array_unique(array_merge($formats, $this->serializerFormats));
-        $route->setRequirement('_format', implode('|', $formats));
+
+        if (count($formats) > 0) {
+          $route->setRequirement('_format', implode('|', $formats));
+        }
       }
     }
   }
