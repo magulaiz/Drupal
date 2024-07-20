eecbe2868713b337e5d8e2cc55832cedf28d197d
iberdinsky <iberdinsky@skilld.cloud>
2024-07-20 16:05:22 +0200

    Tests again

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
@@ -65,9 +65,9 @@ class OpenTelemetryFrontPagePerformanceTest extends PerformanceTestBase {
     $this->assertSame(1, $performance_data->getCacheTagIsValidCount());
     $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
     $this->assertSame(1, $performance_data->getScriptCount());
-    $this->assertLessThan(12000, $performance_data->getScriptBytes());
+    $this->assertLessThan(12200, $performance_data->getScriptBytes());
     $this->assertSame(2, $performance_data->getStylesheetCount());
-    $this->assertLessThan(42000, $performance_data->getStylesheetBytes());
+    $this->assertLessThan(44000, $performance_data->getStylesheetBytes());
   }
 
   /**
