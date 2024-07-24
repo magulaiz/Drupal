722aa6bdcda260a2f74b929b046d513a584d4546
Matt Glaman <matt.glaman@acquia.com>
2024-07-24 10:46:28 -0500

    register file_system

--- core/modules/sqlite/src/Driver/Database/sqlite/Install/Tasks.php
+++ core/modules/sqlite/src/Driver/Database/sqlite/Install/Tasks.php
@@ -74,7 +74,9 @@ class Tasks extends InstallTasks {
         $connection_info = Database::getConnectionInfo();
         $database = $connection_info['default']['database'];
 
-        $connection_info['default']['database'] = tempnam(sys_get_temp_dir(), 'sqlite');
+        // We cannot use \Drupal::service('file_system')->getTempDirectory()
+        // here because we haven't yet successfully connected to the database.
+        $connection_info['default']['database'] = \Drupal::service('file_system')->tempnam(sys_get_temp_dir(), 'sqlite');
 
         // In order to change the Database::$databaseInfo array, need to remove
         // the active connection, then re-add it with the new info.
