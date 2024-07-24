4aa289700083d90c4a8d948141b82645c699e2a1
ehsann95 <psychoahsan@gmail.com>
2024-07-24 12:51:33 +0530

    3250234: Add progress bar around button.

--- core/themes/olivero/js/back-to-top.js
+++ core/themes/olivero/js/back-to-top.js
@@ -4,23 +4,38 @@
  */
 
 ((Drupal) => {
+  const scrollContainer = document.querySelector('.scroll-top');
   const backToTop = document.querySelector('.scroll-top__button');
+  const progressCircle = document.querySelector('.scroll-top__progress');
 
   // Function to toggle visibility of the button based on scroll position
   const toggleButtonVisibility = () => {
     if (window.scrollY > 200) {
       // Adjust the value as needed
-      backToTop.style.display = 'block';
+      scrollContainer.style.display = 'block';
     } else {
-      backToTop.style.display = 'none';
+      scrollContainer.style.display = 'none';
     }
   };
 
-  // Add the scroll event listener to toggle button visibility
-  window.addEventListener('scroll', toggleButtonVisibility);
+  // Function to update the progress bar based on scroll position
+  const updateProgressBar = () => {
+    const scrollTop = window.scrollY;
+    const docHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    progressCircle.style.background = `conic-gradient(#1475AD ${scrollPercent}%, transparent ${scrollPercent}%)`;
+  };
+
+  // Add the scroll event listener to toggle button visibility and update progress bar
+  window.addEventListener('scroll', () => {
+    toggleButtonVisibility();
+    updateProgressBar();
+  });
 
   // Initial check in case the user has already scrolled down
   toggleButtonVisibility();
+  updateProgressBar();
 
   backToTop.addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
