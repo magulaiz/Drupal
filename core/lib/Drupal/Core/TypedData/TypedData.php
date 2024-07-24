984b276db1b66b153e1d069cb209a3c7fd0678db
samit.khulve1 <samit.khulve1@srijan.net>
2024-07-24 16:52:09 +0530

    3368250: fix Deprecated dynamic property

--- core/lib/Drupal/Core/TypedData/TypedData.php
+++ core/lib/Drupal/Core/TypedData/TypedData.php
@@ -41,6 +41,13 @@ abstract class TypedData implements TypedDataInterface, PluginInspectionInterfac
   protected $parent;
 
   /**
+   * The data value.
+   *
+   * @var array
+   */
+  protected $value;
+
+  /**
    * {@inheritdoc}
    */
   public static function createInstance($definition, $name = NULL, ?TraversableTypedDataInterface $parent = NULL) {
