12f026067e74cd811b4da76b73db68766be610df
Brian Suttis <bsuttis@shaw.ca>
2024-07-29 11:06:27 -0700

    Update test status code

--- core/modules/user/tests/src/Functional/UserPermissionsTest.php
+++ core/modules/user/tests/src/Functional/UserPermissionsTest.php
@@ -323,12 +323,13 @@ class UserPermissionsTest extends BrowserTestBase {
     $this->drupalGet('/admin/structure/comment/manage/comment/display');
     $assert_session->statusCodeEquals(200);
     $this->drupalGet('/admin/structure/comment/manage/comment/permissions');
-    $assert_session->statusCodeEquals(403);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('No permissions found.');
 
     // Ensure there are no warnings in the log.
     $this->drupalGet('/admin/reports/dblog');
     $assert_session->statusCodeEquals(200);
-    $assert_session->pageTextContains('access denied');
+    $assert_session->pageTextContains('Session opened');
     $assert_session->pageTextNotContains("Entity view display 'node.article.default': Component");
   }
 
