eeb7e4f9e6429929406c3464aa640a42d9934f85
Phéna Proxima <adam@phenaproxima.net>
2024-07-30 11:28:19 -0400

    Remove prepare(), it is up to calling code to remove things that are meaningless in their specific use case

--- core/lib/Drupal/Core/Config/Entity/ComparatorBase.php
+++ core/lib/Drupal/Core/Config/Entity/ComparatorBase.php
@@ -10,23 +10,13 @@ class ComparatorBase implements ComparatorInterface {
    * {@inheritdoc}
    */
   public function isEquivalent(array $a, array $b): bool {
-    static::prepare($a);
-    static::prepare($b);
-
-    return $a == $b;
-  }
-
-  protected static function prepare(array &$data): void {
-    unset($data['uuid'], $data['_core']);
-
-    if (empty($data['dependencies'])) {
-      unset($data['dependencies']);
-    }
-
     // Ensure we don't get a false mismatch due to differing key order.
     // @todo When https://www.drupal.org/project/drupal/issues/3230826 is
     //   fixed in core, use that API instead to sort the config data.
-    static::recursiveSortByKey($data);
+    static::recursiveSortByKey($a);
+    static::recursiveSortByKey($b);
+
+    return $a == $b;
   }
 
   protected static function recursiveSortByKey(array &$data): void {
