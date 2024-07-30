6ecca4f1a303765c5758fb7f8700769e684f651b
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2024-07-30 08:23:32 +0000

    Update 2 files
    - /core/tests/Drupal/Tests/AutowireProperty.php
    - /core/tests/Drupal/Tests/AutowirePropertyTrait.php

--- core/tests/Drupal/Tests/AutowireProperty.php
+++ core/tests/Drupal/Tests/AutowireProperty.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests;
 
+use Symfony\Component\DependencyInjection\Argument\ArgumentInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 /**
@@ -11,5 +12,16 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
  */
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 final class AutowireProperty extends Autowire {
+
+  public function __construct(
+    string|array|ArgumentInterface|NULL $value = NULL,
+    ?string $service = NULL,
+    ?string $expression = NULL,
+    ?string $env = NULL,
+    ?string $param = NULL,
+    bool|string|array $lazy = false,
+  ) {
+    parent::__construct($value, $service, $expression, $env, $param, $lazy);
+  }
 
 }
