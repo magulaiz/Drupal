6d48c60cfd079ae75d17ef00bb1b85ccb4c89b61
quietone <quietone@2572884.no-reply.drupal.org>
2024-07-31 15:58:30 +1200

    ViewsConfigUpdater.php

--- core/modules/views/src/ViewsConfigUpdater.php
+++ core/modules/views/src/ViewsConfigUpdater.php
@@ -255,6 +255,7 @@ class ViewsConfigUpdater implements ContainerInjectionInterface {
     $deprecations_triggered = &$this->triggeredDeprecations['2640994'][$view->id()];
     if ($this->deprecationsEnabled && $changed && !$deprecations_triggered) {
       $deprecations_triggered = TRUE;
+      // phpcs:ignore Drupal.Semantics.FunctionTriggerError.TriggerErrorTextLayoutRelaxed
       @\trigger_error(\sprintf('The update to convert "numeric" arguments to "entity_target_id" for entity reference fields for view "%s" is deprecated in drupal:10.3.0 and is removed from drupal:12.0.0. Profile, module and theme provided configuration should be updated. See https://www.drupal.org/node/3441945', $view->id()), E_USER_DEPRECATED);
     }
 
