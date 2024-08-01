6180f832b3dca8b594a62718f21540e6eab2fd15
Mark Conroy <mark@mark.ie>
2024-08-01 18:45:58 +0100

    simplify CSS/JS to give same experience on mobile + desktop

--- core/profiles/demo_umami/themes/umami/js/components/language-switcher/language-switcher.js
+++ core/profiles/demo_umami/themes/umami/js/components/language-switcher/language-switcher.js
@@ -7,13 +7,13 @@
   Drupal.behaviors.umamiLanguageSwitcherBlocks = {
     attach: function (context) {
 
-      let windowWidth = window.innerWidth;
-
       function handleLanguageSwitcher() {
         const languageSwitcherBlocks = context.querySelectorAll('.umami-language-switcher');
         languageSwitcherBlocks.forEach((block) => {
           const toggleButton = block.querySelector('.umami-language-switcher__toggle');
           const languageList = block.querySelector('.umami-language-switcher__content');
+          const languageListId = languageList.getAttribute('id');
+          toggleButton.setAttribute('aria-controls', languageListId);
           toggleButton.removeAttribute('hidden');
           languageList.setAttribute('hidden', 'hidden');
           toggleButton.addEventListener('click', () => {
@@ -24,37 +24,7 @@
         });
       }
 
-      function handleReset() {
-        const languageSwitcherBlocks = context.querySelectorAll('.umami-language-switcher');
-        languageSwitcherBlocks.forEach((block) => {
-          const toggleButton = block.querySelector('.umami-language-switcher__toggle');
-          const languageList = block.querySelector('.umami-language-switcher__content');
-          toggleButton.setAttribute('hidden', 'hidden');
-          languageList.removeAttribute('hidden');
-          toggleButton.setAttribute('aria-expanded', 'false');
-        });
-      }
-
-      // Initial check.
-      if (windowWidth <= 768) {
-        setTimeout(handleLanguageSwitcher, 250);
-      }
-
-      // Check after window resize.
-      function handleCheckIfWindowActuallyResized() {
-        if (window.innerWidth === windowWidth) {
-          return
-        } else {
-          windowWidth = window.innerWidth;
-          if (windowWidth <= 768) {
-            handleLanguageSwitcher();
-          } else {
-            handleReset();
-          }
-        }
-      }
-
-      window.addEventListener('resize', Drupal.debounce(handleCheckIfWindowActuallyResized, 50, false));
+      setTimeout(handleLanguageSwitcher, 50);
 
     }
   };
