d8eeb8ef50f8a66e21be8708fddd3a3d7488212e
Ramil Gonzaga <rgonzaga@cs.ubc.ca>
2024-08-02 15:09:00 -0700

    #3457963 validate ajaxview arguments

--- core/modules/views/src/Controller/ViewAjaxController.php
+++ core/modules/views/src/Controller/ViewAjaxController.php
@@ -127,18 +127,33 @@ class ViewAjaxController implements ContainerInjectionInterface {
    */
   public function ajaxView(Request $request) {
     $name = $request->get('view_name');
-    $display_id = $request->get('view_display_id');
-    if (isset($name) && isset($display_id)) {
-      $args = $request->get('view_args', '');
-      $args = $args !== '' ? explode('/', Html::decodeEntities($args)) : [];
+    if (!$this->isValid($name)) {
+      throw new NotFoundHttpException();
+    }
 
-      // Arguments can be empty, make sure they are passed on as NULL so that
-      // argument validation is not triggered.
-      $args = array_map(function ($arg) {
-        return ($arg == '' ? NULL : $arg);
-      }, $args);
+    $display_id = $request->get('view_display_id');
+    if (!$this->isValid($display_id)) {
+      throw new NotFoundHttpException();
+    }
+
+    if (isset($name) && isset($display_id)) {
+      $args = [];
+      $args_raw = $request->get('view_args', '');
+      if ($args_raw !== '' && is_scalar($args_raw)) {
+        $args = explode('/', Html::decodeEntities($args_raw));
+
+        // Arguments can be empty, make sure they are passed on as NULL so that
+        // argument validation is not triggered.
+        $args = array_map(function ($arg) {
+          return ($arg == '' ? NULL : $arg);
+        }, $args);
+      }
 
       $path = $request->get('view_path');
+      if (!$this->isValid($path)) {
+        throw new NotFoundHttpException();
+      }
+
       $dom_id = $request->get('view_dom_id');
       $dom_id = isset($dom_id) ? preg_replace('/[^a-zA-Z0-9_-]+/', '-', $dom_id) : NULL;
       $pager_element = $request->get('pager_element');
@@ -227,6 +242,20 @@ class ViewAjaxController implements ContainerInjectionInterface {
     else {
       throw new NotFoundHttpException();
     }
+  }
+
+  /**
+   * Validates given value to protect from unexpected requests.
+   *
+   * @param mixed $value
+   *   Given value for validation.
+   *
+   * @return bool
+   *   Returns TRUE if value valid, otherwise FALSE
+   */
+  private function isValid(mixed $value): bool {
+    return is_scalar($value)
+      && mb_check_encoding($value, 'ASCII');
   }
 
 }
