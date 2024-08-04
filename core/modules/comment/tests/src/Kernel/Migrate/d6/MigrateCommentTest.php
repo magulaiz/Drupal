5212b03c7ec52438770c6f00003b413594647505
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-04 19:24:29 +0900

    More kernel test rebalancing.
    (cherry picked from commit b771eb9634b3d8a7d1f243ac6184ae23cfd2b3e8)

--- core/modules/comment/tests/src/Kernel/Migrate/d6/MigrateCommentTest.php
+++ core/modules/comment/tests/src/Kernel/Migrate/d6/MigrateCommentTest.php
@@ -14,6 +14,7 @@ use Drupal\node\NodeInterface;
  *
  * @group comment
  * @group migrate_drupal_6
+ * @group #slow
  */
 class MigrateCommentTest extends MigrateDrupal6TestBase {
 
--- core/modules/path/tests/src/Kernel/Migrate/d7/MigrateUrlAliasTest.php
+++ core/modules/path/tests/src/Kernel/Migrate/d7/MigrateUrlAliasTest.php
@@ -8,6 +8,7 @@ namespace Drupal\Tests\path\Kernel\Migrate\d7;
  * Tests URL alias migration.
  *
  * @group path
+ * @group #slow
  */
 class MigrateUrlAliasTest extends MigrateUrlAliasTestBase {
 
