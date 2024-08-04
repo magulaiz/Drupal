0b0395cb56b5ca63cb3137eaed7a0ec24cfd7027
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-04 19:45:56 +0900

    Rebalance even more kernel tests.

--- core/modules/field/tests/src/Kernel/Migrate/d7/MigrateFieldFormatterSettingsTest.php
+++ core/modules/field/tests/src/Kernel/Migrate/d7/MigrateFieldFormatterSettingsTest.php
@@ -12,6 +12,7 @@ use Drupal\Tests\migrate_drupal\Kernel\d7\MigrateDrupal7TestBase;
  * Tests migration of D7 field formatter settings.
  *
  * @group field
+ * @group #slow
  */
 class MigrateFieldFormatterSettingsTest extends MigrateDrupal7TestBase {
 
--- core/modules/node/tests/src/Kernel/Migrate/d7/MigrateNodeRevisionTest.php
+++ core/modules/node/tests/src/Kernel/Migrate/d7/MigrateNodeRevisionTest.php
@@ -12,6 +12,7 @@ use Drupal\Tests\migrate_drupal\Kernel\d7\MigrateDrupal7TestBase;
  * Tests node revision migrations.
  *
  * @group node
+ * @group #slow
  */
 class MigrateNodeRevisionTest extends MigrateDrupal7TestBase {
 
