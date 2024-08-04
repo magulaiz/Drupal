d4573dec32fdd65d10e031c4cbebfa722d099cd1
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-04 15:24:49 +0900

    Add @group #slow to FieldNumericTest.

--- core/modules/views/tests/src/Kernel/Handler/FieldNumericTest.php
+++ core/modules/views/tests/src/Kernel/Handler/FieldNumericTest.php
@@ -11,6 +11,7 @@ use Drupal\views\Views;
  * Tests the core Drupal\views\Plugin\views\field\Numeric handler.
  *
  * @group views
+ * @group #slow
  * @see \Drupal\views\Plugin\views\field\Numeric
  */
 class FieldNumericTest extends ViewsKernelTestBase {
