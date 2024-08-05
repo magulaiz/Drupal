c6f1eacef4278fd359c10cb9f62e0f17f2e23fb5
quietone <quietone@2572884.no-reply.drupal.org>
2024-08-05 22:44:37 +1200

    fix deprecation msgs

--- core/modules/node/tests/src/Kernel/NodeDeprecatedRouteProcessorTest.php
+++ core/modules/node/tests/src/Kernel/NodeDeprecatedRouteProcessorTest.php
@@ -5,7 +5,7 @@ namespace Drupal\Tests\node\Kernel;
 use Drupal\Core\Routing\RouteObjectInterface;
 use Drupal\Core\Url;
 use Drupal\KernelTests\KernelTestBase;
-use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
+use Drupal\TestTools\Extension\DeprecationBridge\ExpectDeprecationTrait;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -31,7 +31,7 @@ class NodeDeprecatedRouteProcessorTest extends KernelTestBase {
    * Test URL generation using 'node.add_page' generates deprecated warning.
    */
   public function testNodeAddPageDeprecatedRoute(): void {
-    $this->expectDeprecation("The 'node.add_page' route is deprecated in drupal:11.0.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_page' route instead. See https://www.drupal.org/node/2940083");
+    $this->expectDeprecation("The 'node.add_page' route is deprecated in drupal:11.1.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_page' route instead. See https://www.drupal.org/node/2940083");
     $url_for_current_route = Url::fromRoute('entity.node.add_page')->toString(TRUE)->getGeneratedUrl();
     $url_for_bc_route = Url::fromRoute('node.add_page')->toString(TRUE)->getGeneratedUrl();
     $this->assertSame($url_for_current_route, $url_for_bc_route);
@@ -41,7 +41,7 @@ class NodeDeprecatedRouteProcessorTest extends KernelTestBase {
    * Test URL generation using 'node.add' generates deprecated warning.
    */
   public function testNodeAddDeprecatedRoute(): void {
-    $this->expectDeprecation("The 'node.add' route is deprecated in drupal:11.0.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_form' route instead. See https://www.drupal.org/node/2940083");
+    $this->expectDeprecation("The 'node.add' route is deprecated in drupal:11.1.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_form' route instead. See https://www.drupal.org/node/2940083");
     $url_for_current_route = Url::fromRoute('entity.node.add_form', ['node_type' => 'page'])->toString(TRUE)->getGeneratedUrl();
     $url_for_bc_route = Url::fromRoute('node.add', ['node_type' => 'page'])->toString(TRUE)->getGeneratedUrl();
     $this->assertSame($url_for_current_route, $url_for_bc_route);
@@ -51,7 +51,7 @@ class NodeDeprecatedRouteProcessorTest extends KernelTestBase {
    * Test route matcher generates deprecated warning for 'node.add_page' route.
    */
   public function testNodeAddPageDeprecatedRouteMatcher(): void {
-    $this->expectDeprecation("The 'node.add_page' route is deprecated in drupal:11.0.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_page' route instead. See https://www.drupal.org/node/2940083");
+    $this->expectDeprecation("The 'node.add_page' route is deprecated in drupal:11.1.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_page' route instead. See https://www.drupal.org/node/2940083");
     $url_for_current_route = Url::fromRoute('entity.node.add_page')->toString(TRUE)->getGeneratedUrl();
     $sub_request = new Request();
     $sub_request->attributes->set(RouteObjectInterface::ROUTE_NAME, 'node.add_page');
@@ -66,7 +66,7 @@ class NodeDeprecatedRouteProcessorTest extends KernelTestBase {
    * Test route matcher generates deprecated warning for 'node.add' route.
    */
   public function testNodeAddDeprecatedRouteMatcher(): void {
-    $this->expectDeprecation("The 'node.add' route is deprecated in drupal:11.0.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_form' route instead. See https://www.drupal.org/node/2940083");
+    $this->expectDeprecation("The 'node.add' route is deprecated in drupal:11.1.0 and is removed in drupal:12.0.0. Use the 'entity.node.add_form' route instead. See https://www.drupal.org/node/2940083");
     $url_for_current_route = Url::fromRoute('entity.node.add_form', ['node_type' => 'page'])->toString(TRUE)->getGeneratedUrl();
     $sub_request = new Request();
     $sub_request->attributes->set(RouteObjectInterface::ROUTE_NAME, 'node.add');
