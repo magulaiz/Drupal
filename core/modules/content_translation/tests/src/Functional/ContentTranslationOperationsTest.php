2e5094040c77e389c2cdae4795907f36caf1eaab
Kris Booghmans <kris@calibrate.be>
2024-08-05 12:32:22 +0200

    Fix drupalPostForm decprecation.

--- core/modules/content_translation/tests/src/Functional/ContentTranslationOperationsTest.php
+++ core/modules/content_translation/tests/src/Functional/ContentTranslationOperationsTest.php
@@ -209,7 +209,7 @@ class ContentTranslationOperationsTest extends NodeTestBase {
     $this->drupalPlaceBlock('local_tasks_block');
     $this->drupalGet('node/' . $node->id());
     $this->assertSession()->linkByHrefExists('node/' . $node->id() . '/translations');
-    $this->drupalPostForm('admin/config/regional/content-language', ['settings[node][article][translatable]' => FALSE], 'Save configuration');
+    $this->submitForm(['settings[node][article][translatable]' => FALSE], 'Save configuration');
     $this->drupalGet('node/' . $node->id());
     $this->assertSession()->linkByHrefNotExists('node/' . $node->id() . '/translations');
   }
