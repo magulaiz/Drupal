50a50281663e8758e8d15905a341c43d87e62772
Dezső BICZÓ <dezso.biczo@pronovix.com>
2024-08-06 14:17:57 +0200

    CS fix

--- core/tests/Drupal/Tests/Core/Access/AccessResultTest.php
+++ core/tests/Drupal/Tests/Core/Access/AccessResultTest.php
@@ -449,13 +449,13 @@ class AccessResultTest extends UnitTestCase {
 
   public static function assertAndOrIfClosureResult(AccessResult $access_result, bool $is_closure_call_expected, bool $is_merge_expected = TRUE): void {
     $expected_cache_contexts = $is_closure_call_expected  && $is_merge_expected ? Cache::mergeContexts($access_result->getCacheContexts(), AndOrIfClosureSpy::CACHE_CONTEXTS) : $access_result->getCacheContexts();
-    $expectedCacheContextsMessage = $expected_cache_contexts === [] ? 'No cache contexts bubbled up from closure.' : 'Expected cache contexts bubbled up from the closure.';
+    $expected_cache_contexts_message = $expected_cache_contexts === [] ? 'No cache contexts bubbled up from closure.' : 'Expected cache contexts bubbled up from the closure.';
     $expected_cache_tags = $is_closure_call_expected && $is_merge_expected ? Cache::mergeTags($access_result->getCacheTags(), AndOrIfClosureSpy::CACHE_TAGS) : $access_result->getCacheTags();
-    $expectedCacheTagsMessage = $expected_cache_tags === [] ? 'No cache tags bubbled up from closure.' : 'Expected cache tags bubbled up from the closure.';
+    $expected_cache_tags_message = $expected_cache_tags === [] ? 'No cache tags bubbled up from closure.' : 'Expected cache tags bubbled up from the closure.';
     $expected_max_age = $is_closure_call_expected && $is_merge_expected ? Cache::mergeMaxAges($access_result->getCacheMaxAge(), AndOrIfClosureSpy::CACHE_MAX_AGE) : $access_result->getCacheMaxAge();
 
-    self::assertEquals($expected_cache_contexts, $access_result->getCacheContexts(), $expectedCacheContextsMessage);
-    self::assertEquals($expected_cache_tags, $access_result->getCacheTags(), $expectedCacheTagsMessage);
+    self::assertEquals($expected_cache_contexts, $access_result->getCacheContexts(), $expected_cache_contexts_message);
+    self::assertEquals($expected_cache_tags, $access_result->getCacheTags(), $expected_cache_tags_message);
     self::assertEquals($expected_max_age, $access_result->getCacheMaxAge());
   }
 
