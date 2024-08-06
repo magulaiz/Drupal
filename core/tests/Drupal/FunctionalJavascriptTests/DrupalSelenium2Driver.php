fcdfcd16595bb09b0f8f24812fa7fc4b195842f4
Alex Pott <alex.a.pott@googlemail.com>
2024-08-06 12:23:34 +0100

    Continue to use WebDriverCurlService in non W3c mode

--- core/tests/Drupal/FunctionalJavascriptTests/DrupalSelenium2Driver.php
+++ core/tests/Drupal/FunctionalJavascriptTests/DrupalSelenium2Driver.php
@@ -9,11 +9,24 @@ use Behat\Mink\Exception\DriverException;
 use WebDriver\Element;
 use WebDriver\Exception;
 use WebDriver\Exception\UnknownError;
+use WebDriver\ServiceFactory;
 
 /**
  * Provides a driver for Selenium testing.
  */
 class DrupalSelenium2Driver extends Selenium2Driver {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($browserName = 'firefox', $desiredCapabilities = NULL, $wdHost = 'http://localhost:4444/wd/hub') {
+    parent::__construct($browserName, $desiredCapabilities, $wdHost);
+    dump($desiredCapabilities);
+    $is_w3c = $desiredCapabilities['goog:chromeOptions']['w3c'] ?? $desiredCapabilities['w3c'] ?? FALSE;
+    if (!$is_w3c) {
+      ServiceFactory::getInstance()->setServiceClass('service.curl', WebDriverCurlService::class);
+    }
+  }
 
   /**
    * Uploads a file to the Selenium instance and returns the remote path.
