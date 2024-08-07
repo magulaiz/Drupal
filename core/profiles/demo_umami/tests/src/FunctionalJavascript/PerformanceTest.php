a690a07970492d924a7901fbee0e857ad552329c
Mark Conroy <mark@mark.ie>
2024-08-07 17:02:06 +0100

    updates test

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/PerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/PerformanceTest.php
@@ -37,10 +37,10 @@ class PerformanceTest extends PerformanceTestBase {
   }
 
   /**
-   * Load the front page as a user with access to Toolbar.
+   * Load the front page as a user with access to the content overview page.
    */
   public function testFrontPagePerformance(): void {
-    $admin_user = $this->drupalCreateUser(['access toolbar']);
+    $admin_user = $this->drupalCreateUser(['access content overview']);
     $this->drupalLogin($admin_user);
     $performance_data = $this->collectPerformanceData(function () {
       $this->drupalGet('<front>');
