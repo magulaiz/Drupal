a56bcaf844ec8261b336695ff894fa2192f56790
Martin Anderson-Clutz <mandclu@gmail.com>
2024-08-10 05:48:39 -0400

    Roll recent patch into an MR

--- core/modules/views/src/Plugin/Derivative/ViewsLocalTask.php
+++ core/modules/views/src/Plugin/Derivative/ViewsLocalTask.php
@@ -7,6 +7,7 @@ use Drupal\Core\State\StateInterface;
 use Drupal\Component\Plugin\Derivative\DeriverBase;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\views\ViewExecutable;
 use Drupal\views\Views;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -16,46 +17,25 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ViewsLocalTask extends DeriverBase implements ContainerDeriverInterface {
 
   /**
-   * The route provider.
-   *
-   * @var \Drupal\Core\Routing\RouteProviderInterface
-   */
-  protected $routeProvider;
-
-  /**
-   * The state key value store.
-   *
-   * @var \Drupal\Core\State\StateInterface
-   */
-  protected $state;
-
-  /**
-   * The view storage.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageInterface
-   */
-  protected $viewStorage;
-
-  /**
    * Constructs a \Drupal\views\Plugin\Derivative\ViewsLocalTask instance.
    *
-   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
+   * @param \Drupal\Core\Routing\RouteProviderInterface $routeProvider
    *   The route provider.
    * @param \Drupal\Core\State\StateInterface $state
    *   The state key value store.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $view_storage
+   * @param \Drupal\Core\Entity\EntityStorageInterface $viewStorage
    *   The view storage.
    */
-  public function __construct(RouteProviderInterface $route_provider, StateInterface $state, EntityStorageInterface $view_storage) {
-    $this->routeProvider = $route_provider;
-    $this->state = $state;
-    $this->viewStorage = $view_storage;
-  }
+  public function __construct(
+    protected RouteProviderInterface $routeProvider,
+    protected StateInterface $state,
+    protected EntityStorageInterface $viewStorage
+  ) {}
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, $base_plugin_id) {
+  public static function create(ContainerInterface $container, $base_plugin_id): static {
     return new static(
       $container->get('router.route_provider'),
       $container->get('state'),
@@ -66,7 +46,7 @@ class ViewsLocalTask extends DeriverBase implements ContainerDeriverInterface {
   /**
    * {@inheritdoc}
    */
-  public function getDerivativeDefinitions($base_plugin_definition) {
+  public function getDerivativeDefinitions($base_plugin_definition): array|null {
     $this->derivatives = [];
 
     $view_route_names = $this->state->get('views.view_route_names');
@@ -93,10 +73,24 @@ class ViewsLocalTask extends DeriverBase implements ContainerDeriverInterface {
           'title' => $menu['title'],
         ] + $base_plugin_definition;
 
-        // Default local tasks have themselves as root tab.
         if ($menu['type'] == 'default tab') {
-          $this->derivatives[$plugin_id]['base_route'] = $route_name;
+          $tab_options = $executable->display_handler->getOption('tab_options');
+
+          // If the user has chosen a "Menu tab" as the parent for the default
+          // tab, then it must also be created and the parent relationship must
+          // be established.
+          if (!empty($tab_options['type']) && $tab_options['type'] == 'tab') {
+            $parent_id = $plugin_id . '.parent';
+            $this->derivatives[$parent_id] = [
+              'route_name' => $route_name,
+              'weight' => $tab_options['weight'],
+              'title' => $tab_options['title'],
+            ] + $base_plugin_definition;
+            $this->derivatives[$plugin_id]['parent_id'] = $parent_id;
+          }
         }
+        // Default local tasks have themselves as root tab.
+        $this->derivatives[$plugin_id]['base_route'] = $route_name;
       }
     }
     return $this->derivatives;
@@ -117,7 +111,7 @@ class ViewsLocalTask extends DeriverBase implements ContainerDeriverInterface {
       $menu = $executable->display_handler->getOption('menu');
 
       // We already have set the base_route for default tabs.
-      if (in_array($menu['type'], ['tab'])) {
+      if ($menu['type'] == 'tab') {
         $plugin_id = 'view.' . $executable->storage->id() . '.' . $display_id;
         $view_route_name = $view_route_names[$executable->storage->id() . '.' . $display_id];
 
@@ -129,19 +123,70 @@ class ViewsLocalTask extends DeriverBase implements ContainerDeriverInterface {
 
         // Find out the parent route.
         // @todo Find out how to find both the root and parent tab.
-        $path = $executable->display_handler->getPath();
-        $split = explode('/', $path);
-        array_pop($split);
-        $path = implode('/', $split);
-
-        $pattern = '/' . str_replace('%', '{}', $path);
-        if ($routes = $this->routeProvider->getRoutesByPattern($pattern)) {
-          foreach ($routes->all() as $name => $route) {
-            $local_tasks['views_view:' . $plugin_id]['base_route'] = $name;
-            // Skip after the first found route.
-            break;
+        $this->applyBaseRoute($executable, 'views_view:' . $plugin_id, $local_tasks);
+      }
+      // Although the base route for the default tab has been taken care of, we
+      // would still have to adjust the parent tab, if it's being created.
+      elseif ($menu['type'] == 'default tab') {
+        $tab_options = $executable->display_handler->getOption('tab_options');
+        if ($tab_options['type'] == 'tab') {
+          $parent_id = 'view.' . $executable->storage->id() . '.' . $display_id . '.parent';
+          $this->applyBaseRoute($executable, 'views_view:' . $parent_id, $local_tasks);
+        }
+        elseif ($tab_options['type'] == 'none') {
+          $plugin_id = 'view.' . $executable->storage->id() . '.' . $display_id;
+          $route_name = $view_route_names[$executable->storage->id() . '.' . $display_id];
+          if (!($route_name != $plugin_id)) {
+            $this->applyBaseRoute($executable, 'views_view:' . $plugin_id, $local_tasks);
           }
         }
+      }
+    }
+  }
+
+  /**
+   * Apply the proper base route to a local task.
+   *
+   * @param \Drupal\views\ViewExecutable $view
+   *   The View executable providing the local task.
+   * @param string $task_id
+   *   The identifier for the local task.
+   * @param array $local_tasks
+   *   The set of all local tasks, which will be adjusted.
+   */
+  protected function applyBaseRoute(ViewExecutable $view, string $task_id, array &$local_tasks) {
+    // Find out the parent route.
+    // @todo Find out how to find both the root and parent tab the best way.
+    $path = $view->display_handler->getPath();
+    $split = explode('/', $path);
+    array_pop($split);
+    $path = implode('/', $split);
+    $pattern = '/' . str_replace('%', '{}', $path);
+    if ($routes = $this->routeProvider->getRoutesByPattern($pattern)) {
+      foreach ($routes->all() as $name => $route) {
+        // Add parent for default menu tab with type = tab.
+        $parent_id = 'views_view:' . $name . '.parent';
+        $menu = $view->display_handler->getOption('menu');
+        if (!empty($local_tasks[$parent_id]) && $parent_id != $task_id) {
+          $local_tasks[$task_id]['parent_id'] = $parent_id;
+        }
+        else {
+          $local_tasks[$task_id]['base_route'] = $name;
+          if ($menu['type'] == 'default tab') {
+            $tab_options = $view->display_handler->getOption('tab_options');
+            if ($tab_options['type'] == 'tab') {
+              $split = explode('.', $task_id);
+              array_pop($split);
+              $parent_id = implode('.', $split);
+              $local_tasks[$parent_id]['parent_id'] = $task_id;
+            }
+            elseif ($tab_options['type'] == 'none') {
+              $local_tasks[$task_id]['parent_id'] = $name;
+            }
+          }
+        }
+        // Skip after the first found route.
+        break;
       }
     }
   }
