c9a3cba776bfb215e62aa821c1e3ec5447b8654a
Bharath <bharath@specbee.com>
2024-08-13 02:51:32 +0530

    Addressed review comments.

--- core/modules/views/src/Plugin/views/filter/NumericFilter.php
+++ core/modules/views/src/Plugin/views/filter/NumericFilter.php
@@ -429,9 +429,6 @@ class NumericFilter extends FilterPluginBase {
             break;
 
           case 2:
-            if (!isset($value['min']) || !isset($value['max'])) {
-              return FALSE;
-            }
             if ($value['min'] === '' && $value['max'] === '') {
               return FALSE;
             }
