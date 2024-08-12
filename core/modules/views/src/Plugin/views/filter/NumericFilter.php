9c677897d2ad49819a44b5f71d3701ef2607751d
Bharath <bharath@specbee.com>
2024-08-13 02:59:34 +0530

    Addressed review comments - removed extra isset.

--- core/modules/views/src/Plugin/views/filter/NumericFilter.php
+++ core/modules/views/src/Plugin/views/filter/NumericFilter.php
@@ -450,9 +450,6 @@ class NumericFilter extends FilterPluginBase implements FilterOperatorsInterface
             break;
 
           case 2:
-            if (!isset($value['min']) || !isset($value['max'])) {
-              return FALSE;
-            }
             if ($value['min'] === '' && $value['max'] === '') {
               return FALSE;
             }
