cdceba5c13962ca049d36994479428edf4106ddb
Andrei Ivnitskii <ivnish.dev@gmail.com>
2024-08-13 14:28:01 +0500

    Fix kernel tests

--- core/modules/jsonapi/tests/src/Kernel/Normalizer/JsonApiDocumentTopLevelNormalizerTest.php
+++ core/modules/jsonapi/tests/src/Kernel/Normalizer/JsonApiDocumentTopLevelNormalizerTest.php
@@ -322,7 +322,7 @@ class JsonApiDocumentTopLevelNormalizerTest extends JsonapiKernelTestBase {
       'width' => 10,
       'height' => 11,
       'drupal_internal__target_id' => $this->file->id(),
-      'display' => null,
+      'display' => NULL,
     ], $normalized['data']['relationships']['field_image']['data']['meta']);
     $this->assertSame('node--article', $normalized['data']['type']);
     $this->assertEquals([
