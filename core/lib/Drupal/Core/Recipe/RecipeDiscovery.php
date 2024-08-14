b03ffea0d3eeb6f5bf4cb4a6dce55a481ff728d6
Jesse Nicola <nicolaj@ohsu.edu>
2024-08-14 16:59:43 -0700

    Issue# 3446354 -- RecipeDiscovery class and tests

---
+++ core/lib/Drupal/Core/Recipe/RecipeDiscovery.php
@@ -0,0 +1,172 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Core\Recipe;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Logger\LoggerChannelTrait;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * This class facilitates discovering recipes.
+ *
+ * Borrows method naming convention from:
+ * Drupal\Core\Extension\ExtensionDiscovery.
+ */
+final class RecipeDiscovery {
+  use LoggerChannelTrait;
+  use StringTranslationTrait;
+
+  /**
+   * Array of directories we will search.
+   */
+  const string CORE_RECIPE_DIR = 'core/recipes';
+
+  /**
+   * The directories to search.
+   *
+   * @var array|string[]
+   */
+  protected array $directoriesToSearch = [];
+
+  /**
+   * Logger for storing messages.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected LoggerInterface $logger;
+
+  /**
+   * Constructs a recipe discovery object.
+   *
+   * @param string|null $path
+   *   (OPTIONAL) path should be a folder containing directories that contain a
+   *   recipe.yml file There will be no traversal further into the directory
+   *   structure. Core recipe will be added. Ideally you'll pass a value,
+   *   relative to Drupal core, where all recipes are expected to be installed
+   *   by Composer; for example DRUPAL_ROOT . '/recipes'.
+   * @param bool $include_core_recipes
+   *   (optional) Whether or not to include core recipes. If you're requesting
+   *   a specific path, you may not want to include core recipes.
+   */
+  public function __construct(string $path = NULL, bool $include_core_recipes = TRUE) {
+    if (is_null($path)) {
+      $path = self::getComposerRecipePath();
+    }
+
+    if ($include_core_recipes) {
+      $this->directoriesToSearch[] = DRUPAL_ROOT . '/' . self::CORE_RECIPE_DIR;
+    }
+
+    if ($path !== FALSE) {
+      if (is_dir($path)) {
+        $this->directoriesToSearch[] = $path;
+      }
+      else {
+        throw new DirectoryNotFoundException();
+      }
+    }
+
+    $this->logger = $this->getLogger('recipe');
+
+  }
+
+  /**
+   * Gets the path at which composer has installed recipes.
+   *
+   * This will work when Drupal core composer has an install-paths
+   * value of type:drupal-recipe.
+   *
+   * @todo Can composer recipe path come from InstalledVersions or similar?
+   *
+   * @return string|bool
+   *   Path to composer recipes, FALSE if no composer path.
+   */
+  private static function getComposerRecipePath() : string|bool {
+    $composer_values = Json::decode(file_get_contents(DRUPAL_ROOT . '/composer.json'));
+    $installer_types_and_paths = $composer_values['extra']['installer-paths'];
+
+    foreach ($installer_types_and_paths as $path => $installer_type) {
+      if (reset($installer_type) == 'type:drupal-recipe') {
+        $recipe_path = explode('/', $path);
+        array_pop($recipe_path);
+        $recipe_path = implode('/', $recipe_path);
+        return DRUPAL_ROOT . '/' . $path;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Scans the site for recipes in the specified search directories.
+   *
+   * @return array
+   *   An array of recipe paths, as discovered in the provided paths.
+   */
+  public function getAllRecipePaths(): array {
+    $recipes = [];
+
+    foreach ($this->directoriesToSearch as $search_dir) {
+      $recipes = array_merge($recipes, $this->scanDirectory($search_dir));
+    }
+
+    return $recipes;
+  }
+
+  /**
+   * Scans the site for recipes and returns an array of loaded Recipe objects.
+   *
+   * WARNING: Recipes can be sizable so be careful using this method!
+   *
+   * @return Recipe[]
+   *   An array of recipe objects.
+   */
+  public function getAllRecipes() : array {
+    $valid_recipes = [];
+
+    foreach ($this->getAllRecipePaths() as $recipePath) {
+      try {
+        $valid_recipes[$recipePath] = Recipe::createFromDirectory($recipePath);
+      }
+      catch (RecipeFileException $e) {
+        // Invalid recipes aren't really our problem, but are worth logging.
+        $this->logger->warning(
+          $this->t("Attempted to load recipe at path '@path', but got a RecipeFileException of: @message",
+            ['@path' => $recipePath, '@message' => $e->getMessage()]
+        ));
+      }
+    }
+
+    return $valid_recipes;
+  }
+
+  /**
+   * Scans a directory and returns all found recipes by path.
+   *
+   * @param string $directory
+   *   The directory to search.
+   *
+   * @return array
+   *   All recipes found, by path.
+   */
+  protected function scanDirectory(string $directory) : array {
+    $recipes = [];
+
+    $finder = Finder::create()
+      ->files()
+      ->name('recipe.yml')
+      ->depth(1)
+      ->in($directory);
+
+    foreach ($finder as $file) {
+      $recipes[] = $file->getPath();
+    }
+
+    return $recipes;
+  }
+
+}
---
+++ core/tests/Drupal/KernelTests/Core/Recipe/RecipeDiscoveryTest.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\KernelTests\Core\Recipe;
+
+use Drupal\Core\Recipe\Recipe;
+use Drupal\Core\Recipe\RecipeDiscovery;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Recipe\RecipeDiscovery
+ * @group Recipe
+ */
+class RecipeDiscoveryTest extends KernelTestBase {
+
+  /**
+   * The recipe fixture path, including DRUPAL_ROOT.
+   *
+   * @var string
+   */
+  private string $recipeFixturePath = DRUPAL_ROOT . '/core/tests/fixtures/recipes';
+
+  /**
+   * An array of invalid test recipe paths. IE: Throw exceptions on load.
+   *
+   * @var string[]
+   */
+  private array $invalidExpectedRecipePaths = [
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/config_rollback_exception",
+  ];
+
+  /**
+   * Array of expected test recipe paths. IE: doesn't throw exceptions.
+   *
+   * @var string[]
+   */
+  private array $validExpectedRecipePaths = [
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/recipe_depend_on_invalid_config_and_valid_modules",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/unmet_config_dependencies",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/config_from_module_and_recipe",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/no_extensions",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/config_actions",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/recipe_depend_on_invalid",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/config_from_module",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/install_node_with_config",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/invalid_config",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/install_two_modules",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/recipe_include",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/config_wildcard",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/base_theme_and_views",
+    DRUPAL_ROOT . "/core/tests/fixtures/recipes/theme_with_module_dependencies",
+  ];
+
+  /**
+   * Check that core recipe paths are present.
+   *
+   * @covers ::getAllRecipePaths
+   */
+  public function testRecipeDiscoveryGetAllRecipePathsCore(): void {
+    $recipeDiscovery = new RecipeDiscovery();
+    $found_recipe_paths = $recipeDiscovery->getAllRecipePaths();
+    $this->assertNotEmpty($found_recipe_paths);
+  }
+
+  /**
+   * Check that all recipe paths in our custom path are as expected.
+   *
+   * NOTE: Does not check if recipes are valid.
+   *
+   * @covers ::getAllRecipePaths
+   */
+  public function testRecipeDiscoveryGetAllRecipePathsTestData(): void {
+    // Get recipes.
+    $recipeDiscovery = new RecipeDiscovery(
+      $this->recipeFixturePath,
+       FALSE
+    );
+    $found_recipe_paths = $recipeDiscovery->getAllRecipePaths();
+
+    $expected_recipes = array_merge($this->invalidExpectedRecipePaths, $this->validExpectedRecipePaths);
+
+    $this->assertSame(
+      ksort($expected_recipes),
+      ksort($found_recipe_paths)
+    );
+  }
+
+  /**
+   * Checks that recipe objects are returned from test recipes.
+   *
+   * NOTE: Checks an invalid recipe is removed.
+   * NOTE: Confirms array keys (paths) and that values are Recipe objects.
+   *
+   * @covers ::getAllRecipePaths
+   */
+  public function testRecipeDiscoveryGetAllRecipes(): void {
+    $recipeDiscovery = new RecipeDiscovery($this->recipeFixturePath, FALSE
+    );
+
+    $found_recipes = $recipeDiscovery->getAllRecipes();
+    $this->assertSame($this->validExpectedRecipePaths, array_keys($found_recipes));
+
+    foreach ($found_recipes as $found_recipe) {
+      $this->assertTrue($found_recipe instanceof Recipe);
+    }
+  }
+
+}
