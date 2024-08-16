7f062f5a33d232d489875b008021514100644f6d
Arunkumar Kuppuswamy <3594-arunkumark@users.noreply.drupalcode.org>
2024-08-16 07:45:32 +0530

    Revert code related to Date

--- core/modules/datetime/src/Plugin/Field/FieldFormatter/DateTimeCustomFormatter.php
+++ core/modules/datetime/src/Plugin/Field/FieldFormatter/DateTimeCustomFormatter.php
@@ -69,7 +69,7 @@ class DateTimeCustomFormatter extends DateTimeFormatterBase {
     $form['date_format'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Date/time format'),
-      '#description' => $this->t('See <a href="!url" target="_blank">the documentation for PHP date formats</a>.', ['!url' => 'https://www.php.net/manual/datetime.format.php#refsect1-datetime.format-parameters']),
+      '#description' => $this->t('See <a href="https://www.php.net/manual/datetime.format.php#refsect1-datetime.format-parameters" target="_blank">the documentation for PHP date formats</a>.'),
       '#default_value' => $this->getSetting('date_format'),
     ];
 
