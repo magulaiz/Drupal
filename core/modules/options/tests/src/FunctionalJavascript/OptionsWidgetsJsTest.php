876ab6a0a355f194846933eb943695026e2d794e
Leon Röthemeyer <64041-LRWebks@users.noreply.drupalcode.org>
2024-08-20 10:41:31 +0000

    Correcting mistakes in test comments

--- core/modules/options/tests/src/FunctionalJavascript/OptionsWidgetsJsTest.php
+++ core/modules/options/tests/src/FunctionalJavascript/OptionsWidgetsJsTest.php
@@ -46,18 +46,18 @@ class OptionsWidgetsJsTest extends WebDriverTestBase {
   }
 
   /**
-   * Tests the 'options_buttons' widget (multiple select).
+   * Tests the 'options_buttons' widget for multiple select.
    */
   public function testCheckboxes(): void {
     $this->createContentType(['type' => 'article', 'name' => 'Article']);
-    // Create field:
+    // Create field.
     FieldStorageConfig::create([
       'field_name' => 'field_fences_test',
       'entity_type' => 'node',
       'type' => 'text',
       'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
     ])->save();
-    // Create field instance:
+    // Create field instance.
     FieldConfig::create([
       'label' => 'Field Fences Test',
       'field_name' => 'field_fences_test',
@@ -67,7 +67,7 @@ class OptionsWidgetsJsTest extends WebDriverTestBase {
     ])->save();
     /** @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $display_repository */
     $display_repository = \Drupal::service('entity_display.repository');
-    // Enable form display and set check_all_enabled to TRUE:
+    // Enable form display and set check_all_enabled to TRUE.
     $display_repository->getFormDisplay('node', 'article')
       ->setComponent('field_fences_test', [
         'type' => 'text_textfield',
@@ -76,10 +76,10 @@ class OptionsWidgetsJsTest extends WebDriverTestBase {
         ],
       ])
       ->save();
-    // Go to the Create Article page and check that the button is there:
+    // Go to the Create Article page and check that the button is there.
     $this->drupalGet('node/add/article');
     $this->getSession()->getPage()->hasButton('Check all / none');
-    // Change the setting to FALSE:
+    // Change the setting to FALSE.
     $display_repository->getFormDisplay('node', 'article')
       ->setComponent('field_fences_test', [
         'type' => 'text_textfield',
@@ -88,7 +88,7 @@ class OptionsWidgetsJsTest extends WebDriverTestBase {
         ],
       ])
       ->save();
-    // Go to the Create Article page again and check that the button is gone:
+    // Reload the create Article page and confirm there is no 'check all' button.
     $this->drupalGet('node/add/article');
     $this->assertFalse($this->getSession()->getPage()->hasButton('Check all / none'));
   }
