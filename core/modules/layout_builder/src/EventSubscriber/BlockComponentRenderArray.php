9a183f6b4b3464823c62eee0b1ae766ac5e577f2
piratskul <piratskul@gmail.com>
2024-08-20 17:38:09 -0500

    Issue #3292520 - Handle block_build and block_view alter hooks

--- core/modules/layout_builder/src/EventSubscriber/BlockComponentRenderArray.php
+++ core/modules/layout_builder/src/EventSubscriber/BlockComponentRenderArray.php
@@ -9,6 +9,7 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Render\PreviewFallbackInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\layout_builder\Access\LayoutPreviewAccessAllowed;
 use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
@@ -32,16 +33,26 @@ class BlockComponentRenderArray implements EventSubscriberInterface {
    *
    * @var \Drupal\Core\Session\AccountInterface
    */
-  protected $currentUser;
+  protected AccountInterface $currentUser;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected ModuleHandlerInterface $moduleHandler;
 
   /**
    * Creates a BlockComponentRenderArray object.
    *
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface|null $module_handler
+   *   The module handler.
    */
-  public function __construct(AccountInterface $current_user) {
+  public function __construct(AccountInterface $current_user, ?ModuleHandlerInterface $module_handler = NULL) {
     $this->currentUser = $current_user;
+    $this->moduleHandler = $module_handler;
   }
 
   /**
@@ -150,6 +161,23 @@ class BlockComponentRenderArray implements EventSubscriberInterface {
         $build['#contextual_links'] = $content['#contextual_links'];
       }
       $build['content'] = $content;
+
+      if (isset($this->moduleHandler)) {
+        $base_id = $block->getBaseId();
+
+        // If an alter hook wants to modify the block contents, it can append
+        // another #pre_render hook.
+        $this->moduleHandler->alter([
+          'block_view',
+          "block_view_$base_id",
+        ], $build, $block);
+
+        // Allow altering of cacheability metadata or setting #create_placeholder.
+        $this->moduleHandler->alter([
+          'block_build',
+          "block_build_$base_id",
+        ], $build, $block);
+      }
 
       if ($event->inPreview()) {
         if ($block instanceof PreviewFallbackInterface) {
