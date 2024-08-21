6f333b6320d90c0717c9ed92560fc9f6ad207664
Sergey Bortkevich <serg.bortkevich@gmail.com>
2024-08-21 22:14:37 +0200

    Issue #3460831 by vasyapledov: Add alter for  - fix test 2

--- core/modules/system/tests/modules/router_route_callback_primary_test/src/Controller/PrimaryControllerTest.php
+++ core/modules/system/tests/modules/router_route_callback_primary_test/src/Controller/PrimaryControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\router_route_callback_primary_test\Controller;
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * Controller routines for testing the route_callbacks.
  */
-class PrimaryControllerTest {
+class PrimaryControllerTest extends TestCase {
 
   /**
    * Test function.
--- core/modules/system/tests/modules/router_route_callback_secondary_test/src/Controller/SecondaryControllerTest.php
+++ core/modules/system/tests/modules/router_route_callback_secondary_test/src/Controller/SecondaryControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\router_route_callback_secondary_test\Controller;
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * Controller routines for testing the route_callbacks.
  */
-class SecondaryControllerTest {
+class SecondaryControllerTest extends TestCase {
 
   /**
    * Test function.
