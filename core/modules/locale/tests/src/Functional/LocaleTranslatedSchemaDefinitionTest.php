dd52fe752dd92c7f8983d4df95b6f93b5ecd3741
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-21 18:33:31 +0900

    Apply https://www.drupal.org/project/drupal/issues/3469309

--- core/modules/locale/tests/src/Functional/LocaleTranslatedSchemaDefinitionTest.php
+++ core/modules/locale/tests/src/Functional/LocaleTranslatedSchemaDefinitionTest.php
@@ -33,6 +33,11 @@ class LocaleTranslatedSchemaDefinitionTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
+  protected bool $useOneTimeLoginLinks = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
   protected function setUp(): void {
     parent::setUp();
     ConfigurableLanguage::createFromLangcode('fr')->save();
