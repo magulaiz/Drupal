800d2d5b712b910065a7f16b20bfd363e39fdaad
Fran Rouco <fran.rouco@metadrop.net>
2024-08-22 09:48:48 +0200

    Issue #3367077: Add missing dependencies

--- core/modules/link/src/Plugin/Field/FieldFormatter/LinkFormatter.php
+++ core/modules/link/src/Plugin/Field/FieldFormatter/LinkFormatter.php
@@ -3,6 +3,7 @@
 namespace Drupal\link\Plugin\Field\FieldFormatter;
 
 use Drupal\Component\Utility\Unicode;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
