cf74982efd49ff430e91aeb5e5099837e08ca7d8
Sergey Bortkevich <serg.bortkevich@gmail.com>
2024-08-22 11:25:21 +0200

    Issue #3460831 by vasyapledov: Add alter for - retrun original test-only.sh

--- .gitlab-ci/scripts/test-only.sh
+++ .gitlab-ci/scripts/test-only.sh
@@ -6,13 +6,13 @@ echo "ℹ️ Changes from ${BASELINE}"
 git diff ${BASELINE} --name-only
 echo "If this list contains more files than what you changed, then you need to rebase your branch."
 
-#echo "1️⃣ Reverting non test changes"
-#if [[ $(git diff ${BASELINE} --diff-filter=DM --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh) ]]; then
-#git diff ${BASELINE} --diff-filter=DM --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh|while read file;do
-#  echo "↩️ Reverting $file";
-#  git checkout ${BASELINE} -- $file;
-#done
-#fi
+echo "1️⃣ Reverting non test changes"
+if [[ $(git diff ${BASELINE} --diff-filter=DM --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh) ]]; then
+git diff ${BASELINE} --diff-filter=DM --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh|while read file;do
+  echo "↩️ Reverting $file";
+  git checkout ${BASELINE} -- $file;
+done
+fi
 if [[ $(git diff ${BASELINE} --diff-filter=A --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh) ]]; then
 git diff ${BASELINE} --diff-filter=A --name-only|grep -Ev "*/tests/*"|grep -v .gitlab-ci|grep -v scripts/run-tests.sh|while read file;do
   echo "🗑️️ Deleting $file";
