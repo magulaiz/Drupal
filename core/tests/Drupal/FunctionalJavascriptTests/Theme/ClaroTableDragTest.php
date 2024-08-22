5353b86417025c8a6fc22a203610ff3687e1e57d
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-22 15:45:36 +0900

    Remove @group #slow from three functional js tests.

--- core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroTableDragTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroTableDragTest.php
@@ -10,7 +10,6 @@ use Drupal\FunctionalJavascriptTests\TableDrag\TableDragTest;
  * Tests draggable tables with Claro theme.
  *
  * @group claro
- * @group #slow
  *
  * @see \Drupal\FunctionalJavascriptTests\TableDrag\TableDragTest
  */
