9a99eb2defd20e94d6d7fd5157d53a73c3e7ff26
quietone <quietone@2572884.no-reply.drupal.org>
2024-08-23 14:07:25 +1200

    adjust comments

--- core/scripts/run-tests.sh
+++ core/scripts/run-tests.sh
@@ -1063,7 +1063,8 @@ function simpletest_script_get_test_list() {
  * slow can be added to the '#slow' group so they are placed first in each
  * test run regardless of the number of methods.
  *
- * @param string[] an array of test class names.
+ * @param string[] $tests
+ *   An array of test class names.
  */
 function sort_tests_by_public_method_count(array &$tests): void {
   usort($tests, function ($a, $b) {
@@ -1077,13 +1078,24 @@ function sort_tests_by_public_method_count(array &$tests): void {
 
 /**
  * Distribute tests into bins.
+ *
+ * The given array of tests is split into the available bins. The distribution
+ * starts with the first test, placing the first test in the first bin, the
+ * second test in the second bin and so on. This results each bin having a
+ * similar number of test methods to run in total.
+ *
+ * @param string[] $tests
+ *   An array of test class names.
+ * @param int $bin_count
+ *   The number of bins available.
+ *
+ * @return array
+ *   An associative array of bins and the test class names in each bin.
  */
-function place_tests_into_bins(array $tests, int $bin_count) {
+ function place_tests_into_bins(array $tests, int $bin_count) {
   // Create a bin corresponding to each parallel test job.
   $bins = array_fill(0, $bin_count, []);
-  // Go through each test, already sorted from most methods to least, and
-  // add them to one bin at a time. This results each bin having a similar
-  // number of test methods to run in total.
+  // Go through each test and add them to one bin at a time.
   foreach ($tests as $key => $test) {
     $bins[($key % $bin_count)][] = $test;
   }
