32065d40b63efb0540f12482d9db8e94f17d7676
Phéna Proxima <adam@phenaproxima.net>
2024-08-27 10:44:13 -0400

    Remove unnecessary nullsafe

--- core/lib/Drupal/Core/Recipe/ConsoleInputCollector.php
+++ core/lib/Drupal/Core/Recipe/ConsoleInputCollector.php
@@ -60,7 +60,7 @@ final class ConsoleInputCollector implements InputCollectorInterface {
   private function getInputFromOptions(): array {
     $options = [];
     try {
-      foreach ($this->input?->getOption(static::INPUT_OPTION) ?? [] as $option) {
+      foreach ($this->input->getOption(static::INPUT_OPTION) ?? [] as $option) {
         [$key, $value] = explode('=', $option, 2);
         $options[$key] = $value;
       }
