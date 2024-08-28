7bd45f4b7a040fd6c022e3fc6b82be02597a8cd4
recrit <34538-recrit@users.noreply.drupalcode.org>
2024-08-28 08:59:31 -0400

    making the error message more descriptive, remove '|' since system logs are often delimited by '|'

--- core/modules/media/src/Plugin/media/Source/OEmbed.php
+++ core/modules/media/src/Plugin/media/Source/OEmbed.php
@@ -250,10 +250,12 @@ class OEmbed extends MediaSourceBase implements OEmbedInterface {
         $this->messenger->addError(Markup::create($e->getMessage() . " Update media entity: " . $link->toString()));
       }
       $this->logger->error(
-        $e->getMessage() . ' Resource URL: %resource_url | Media ID: %media_id',
+       'An error occurred while fetching an oEmbed resource for Media ID: %media_id, Embed URL: %media_url, oEmebed Resource URL: %resource_url. Error: @error',
         [
           '%resource_url' => $resource_url ?? 'unknown',
+          '%media_url' => $media_url,
           '%media_id' => $media->id(),
+          '@error' =>  $e->getMessage(),
         ]
       );
       return NULL;
