78b306666eb5ab25cf5b3f9ee814891909990f14
recrit <34538-recrit@users.noreply.drupalcode.org>
2024-08-28 09:00:27 -0400

    check empty for resource url

--- core/modules/media/src/Plugin/media/Source/OEmbed.php
+++ core/modules/media/src/Plugin/media/Source/OEmbed.php
@@ -252,7 +252,7 @@ class OEmbed extends MediaSourceBase implements OEmbedInterface {
       $this->logger->error(
        'An error occurred while fetching an oEmbed resource for Media ID: %media_id, Embed URL: %media_url, oEmebed Resource URL: %resource_url. Error: @error',
         [
-          '%resource_url' => $resource_url ?? 'unknown',
+          '%resource_url' => !empty($resource_url) ? $resource_url : 'unknown',
           '%media_url' => $media_url,
           '%media_id' => $media->id(),
           '@error' =>  $e->getMessage(),
