f1614a583071208d7c61e058228e8bad1832a0d5
Andy Postnikov <apostnikov@gmail.com>
2024-09-06 02:19:39 +0200

    fix Component/Diff/DiffFormatter

--- core/lib/Drupal/Component/Diff/DiffFormatter.php
+++ core/lib/Drupal/Component/Diff/DiffFormatter.php
@@ -134,7 +134,7 @@ class DiffFormatter {
         $this->_changed($edit->orig, $edit->closing);
       }
       else {
-        trigger_error('Unknown edit type', E_USER_ERROR);
+        throw new \RuntimeException('Unknown edit type: '. $edit->type);
       }
     }
     $this->_end_block();
