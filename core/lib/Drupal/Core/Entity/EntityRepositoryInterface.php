a9690880fc18f0f56a9526b7cc094efcf54566de
Roger Codina <34447-rcodina@users.noreply.drupalcode.org>
2024-09-16 10:48:09 +0200

    Fix phpcs errors

--- core/lib/Drupal/Core/Entity/EntityRepositoryInterface.php
+++ core/lib/Drupal/Core/Entity/EntityRepositoryInterface.php
@@ -106,7 +106,7 @@ interface EntityRepositoryInterface {
    * @return \Drupal\Core\Entity\EntityInterface|null
    *   An entity object variant or NULL if the entity does not exist.
    */
-  public function getActive($entity_type_id, $entity_id, array $contexts = NULL);
+  public function getActive($entity_type_id, $entity_id, ?array $contexts = NULL);
 
   /**
    * Retrieves the active entity variants matching the specified context.
@@ -125,7 +125,7 @@ interface EntityRepositoryInterface {
    *
    * @see getActive()
    */
-  public function getActiveMultiple($entity_type_id, array $entity_ids, array $contexts = NULL);
+  public function getActiveMultiple($entity_type_id, array $entity_ids, ?array $contexts = NULL);
 
   /**
    * Retrieves the canonical entity variant matching the specified context.
@@ -152,7 +152,7 @@ interface EntityRepositoryInterface {
    * @return \Drupal\Core\Entity\EntityInterface|null
    *   An entity object variant or NULL if the entity does not exist.
    */
-  public function getCanonical($entity_type_id, $entity_id, array $contexts = NULL);
+  public function getCanonical($entity_type_id, $entity_id, ?array $contexts = NULL);
 
   /**
    * Retrieves the canonical entity variants matching the specified context.
@@ -171,6 +171,6 @@ interface EntityRepositoryInterface {
    *
    * @see getCanonical()
    */
-  public function getCanonicalMultiple($entity_type_id, array $entity_ids, array $contexts = NULL);
+  public function getCanonicalMultiple($entity_type_id, array $entity_ids, ?array $contexts = NULL);
 
 }
