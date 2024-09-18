50e98816d323a0ec7353d6b94b35da4c2e979c36
Pavlos Daniel <pavlos.daniel@acquia.com>
2024-09-17 20:16:12 -0700

    Update test to do a change that keeps the entity in the result and we can observe the revision ID changing.

--- core/tests/Drupal/KernelTests/Core/Entity/EntityQueryTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityQueryTest.php
@@ -393,16 +393,17 @@ class EntityQueryTest extends EntityKernelTestBase {
     // Update an entity.
     $entity = EntityTestMulRev::load(4);
     $entity->setNewRevision();
-    $entity->$figures->color = 'red';
+    $entity->name->value .= 'x';
     $entity->save();
 
+    // Updated entity should now have revision ID 24.
     $results = $this->queryResults = $this->storage
       ->getQuery()
       ->latestRevision()
       ->notExists("$figures.color")
       ->accessCheck(TRUE)
       ->execute();
-    $expected = [8 => '8', 20 => '12'];
+    $expected = [24 => '4', 8 => '8', 20 => '12'];
     $this->assertSame($expected, $results);
 
     // Check that a query on the latest revisions without any condition returns
@@ -414,7 +415,7 @@ class EntityQueryTest extends EntityKernelTestBase {
       ->sort('id')
       ->sort('revision_id')
       ->execute();
-    $expected = [1 => '1', 2 => '2', 3 => '3', 16 => '4', 17 => '5', 18 => '6', 19 => '7', 8 => '8', 9 => '9', 10 => '10', 11 => '11', 20 => '12', 21 => '13', 22 => '14', 23 => '15'];
+    $expected = [1 => '1', 2 => '2', 3 => '3', 24 => '4', 17 => '5', 18 => '6', 19 => '7', 8 => '8', 9 => '9', 10 => '10', 11 => '11', 20 => '12', 21 => '13', 22 => '14', 23 => '15'];
     $this->assertSame($expected, $results);
   }
 
