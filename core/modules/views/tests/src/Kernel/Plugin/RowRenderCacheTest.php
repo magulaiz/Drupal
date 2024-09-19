2d0f17e33dfe2c750129436a35ea876ee86011bc
iberdinsky-skilld <iberdinsky@skilld.cloud>
2024-09-19 17:32:47 +0200

    Fixed test

--- core/modules/views/tests/src/Kernel/Plugin/RowRenderCacheTest.php
+++ core/modules/views/tests/src/Kernel/Plugin/RowRenderCacheTest.php
@@ -183,12 +183,13 @@ class RowRenderCacheTest extends ViewsKernelTestBase {
       $expected = $access ? "<a href=\"$node_url/delete?destination=/\" hreflang=\"en\">delete</a>" : "";
       $output = $view->style_plugin->getField($index, 'delete_node');
       $this->assertSame($expected, (string) $output);
-      $expected = $access ? '  <div class="dropbutton-wrapper" data-drupal-ajax-container><div class="dropbutton-widget"><ul class="dropbutton">' .
-        '<li><a href="' . $node_url . '/edit?destination=/" aria-label="Edit ' . $node->label() . '" hreflang="en">Edit</a></li>' .
-        '<li><a href="' . $node_url . '/delete?destination=/" aria-label="Delete ' . $node->label() . '" class="use-ajax" data-dialog-type="modal" data-dialog-options="' . Html::escape(Json::encode(['width' => 880])) . '" hreflang="en">Delete</a></li>' .
-        '</ul></div></div>' : '';
-      $output = $view->style_plugin->getField($index, 'operations');
-      $this->assertSame($expected, (string) $output);
+      $expected = $access ? preg_replace('/\s+/', '', '
+        <div class="dropbutton-wrapper" data-drupal-ajax-container><div class="dropbutton-widget"><ul class="dropbutton">
+          <li><a href="' . $node_url . '/edit?destination=/" aria-label="Edit ' . $node->label() . '" hreflang="en">Edit</a></li>
+          <li><a href="' . $node_url . '/delete?destination=/" aria-label="Delete ' . $node->label() . '" class="use-ajax" data-dialog-type="modal" data-dialog-options="' . Html::escape(Json::encode(["width" => 880])) . '" hreflang="en">Delete</a></li>
+        </ul></div></div>') : '';
+      $output = preg_replace('/\s+/', '', (string) $view->style_plugin->getField($index, 'operations'));
+      $this->assertSame(trim($expected), (string) trim($output));
 
       if ($check_cache) {
         $keys = $cache_plugin->getRowCacheKeys($view->result[$index]);
