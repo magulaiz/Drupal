88a44c9f41ce4efec9302bcfb5f5f9ed07102825
Julian Pustkuchen <3110-Anybody@users.noreply.drupalcode.org>
2024-09-25 15:50:58 +0000

    Pass original error object with details to console.error()

--- core/misc/ajax.js
+++ core/misc/ajax.js
@@ -1142,11 +1142,10 @@
           // eslint-disable-next-line no-console
           console.error(
             Drupal.t(
-              'An error occurred during the execution of the Ajax response: !error',
-              {
-                '!error': error,
-              },
+              'An error occurred during the execution of the Ajax response:'
             ),
+            // Pass original error object with details:
+            error
           ),
         )
     );
