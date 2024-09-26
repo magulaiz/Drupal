7cf15772cd0577a69a756658fa30375a29ac415f
Pieter Frenssen <pieter@frenssen.be>
2024-09-26 15:30:40 +0300

    Check performance metrics using a warm browser cache and cold Drupal cache.

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
@@ -28,6 +28,7 @@ class OpenTelemetryFrontPagePerformanceTest extends PerformanceTestBase {
     $this->testFrontPageColdCache();
     $this->testFrontPageCoolCache();
     $this->testFrontPageHotCache();
+    $this->testFrontPageColdDrupalCache();
   }
 
   /**
@@ -38,23 +39,11 @@ class OpenTelemetryFrontPagePerformanceTest extends PerformanceTestBase {
     //   first request in a test, so warm it up.
     //   https://www.drupal.org/project/drupal/issues/3379750
     $this->drupalGet('user/login');
-    // Clear caches to ensure a cold cache.
     $this->rebuildAll();
-    $performance_data = $this->collectPerformanceData(function () {
+    $this->collectPerformanceData(function () {
       $this->drupalGet('<front>');
     }, 'umamiFrontPageColdCache');
     $this->assertSession()->pageTextContains('Umami');
-    $this->assertCountBetween(320, 360, $performance_data->getQueryCount());
-    $this->assertCountBetween(600, 750, $performance_data->getCacheGetCount());
-    $this->assertCountBetween(410, 450, $performance_data->getCacheSetCount());
-    $this->assertSame(2, $performance_data->getCacheDeleteCount());
-    $this->assertCountBetween(250, 275, $performance_data->getCacheTagChecksumCount());
-    $this->assertCountBetween(40, 90, $performance_data->getCacheTagIsValidCount());
-    $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
-    $this->assertSame(1, $performance_data->getScriptCount());
-    $this->assertCountBetween(7000, 7100, $performance_data->getScriptBytes());
-    $this->assertSame(2, $performance_data->getStylesheetCount());
-    $this->assertCountBetween(40200, 40250, $performance_data->getStylesheetBytes());
   }
 
   /**
@@ -107,6 +96,36 @@ class OpenTelemetryFrontPagePerformanceTest extends PerformanceTestBase {
     $this->collectPerformanceData(function () {
       $this->drupalGet('<front>');
     }, 'umamiFrontPageCoolCache');
+  }
+
+  /**
+   * Check performance metrics with a cold Drupal cache.
+   */
+  protected function testFrontPageColdDrupalCache(): void {
+    // Request the page twice so that all assets are cached in the browser. We
+    // will be checking performance metrics and want to avoid additional
+    // requests made by the browser influencing the results.
+    $this->drupalGet('<front>');
+    $this->drupalGet('<front>');
+
+    // Clear caches to ensure a cold cache.
+    $this->rebuildAll();
+
+    $performance_data = $this->collectPerformanceData(function () {
+      $this->drupalGet('<front>');
+    }, 'umamiFrontPageColdCache');
+    $this->assertSession()->pageTextContains('Umami');
+    $this->assertCountBetween(315, 330, $performance_data->getQueryCount());
+    $this->assertCountBetween(540, 565, $performance_data->getCacheGetCount());
+    $this->assertCountBetween(420, 440, $performance_data->getCacheSetCount());
+    $this->assertSame(2, $performance_data->getCacheDeleteCount());
+    $this->assertCountBetween(245, 260, $performance_data->getCacheTagChecksumCount());
+    $this->assertCountBetween(70, 80, $performance_data->getCacheTagIsValidCount());
+    $this->assertSame(0, $performance_data->getCacheTagInvalidationCount());
+    $this->assertSame(1, $performance_data->getScriptCount());
+    $this->assertCountBetween(7000, 7100, $performance_data->getScriptBytes());
+    $this->assertSame(2, $performance_data->getStylesheetCount());
+    $this->assertCountBetween(40200, 40300, $performance_data->getStylesheetBytes());
   }
 
   /**
