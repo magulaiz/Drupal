26047bc6b4cbbd851da8a45247f7d3531150c274
samit.khulve1 <samit.khulve1@srijan.net>
2024-09-27 15:12:22 +0530

    3476166: Deprecate RoleStorage::isPermissionInRoles()

--- core/modules/user/src/RoleStorage.php
+++ core/modules/user/src/RoleStorage.php
@@ -13,6 +13,7 @@ class RoleStorage extends ConfigEntityStorage implements RoleStorageInterface {
    * {@inheritdoc}
    */
   public function isPermissionInRoles($permission, array $rids) {
+    @trigger_error(__METHOD__ . '() is deprecated in drupal:11.1.0 and is removed from drupal:12.0.0. There is no replacement. See https://www.drupal.org/node/3477235', E_USER_DEPRECATED);
     foreach ($this->loadMultiple($rids) as $role) {
       /** @var \Drupal\user\RoleInterface $role */
       if ($role->hasPermission($permission)) {
--- core/modules/user/src/RoleStorageInterface.php
+++ core/modules/user/src/RoleStorageInterface.php
@@ -12,6 +12,11 @@ interface RoleStorageInterface extends ConfigEntityStorageInterface {
   /**
    * Returns whether a permission is in one of the passed in roles.
    *
+   *
+   * @deprecated in drupal:11.1.0 and is removed from drupal:12.0.0. There is no
+   *   replacement.
+   * @see https://www.drupal.org/node/3477235
+   *
    * @param string $permission
    *   The permission.
    * @param array $rids
