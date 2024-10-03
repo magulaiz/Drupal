94c864d60338f465dac547febada67d49050a9bc
Nayana Ramakrishnan <nayanamvr@gmail.com>
2024-10-03 18:08:46 +0530

    3475842: Fix build issues.

--- core/modules/ckeditor5/js/ckeditor5.js
+++ core/modules/ckeditor5/js/ckeditor5.js
@@ -432,12 +432,16 @@
 
           // Set the max-width of the dropdown toolbar panel
           // based the main toolbar panel width.
-          editor.ui.view.stickyPanel.element
-            .querySelector('.ck-toolbar__grouped-dropdown .ck-dropdown__panel')
-            .style.setProperty(
+          const dropdownpanel =
+            editor.ui.view.stickyPanel.element.querySelector(
+              '.ck-toolbar__grouped-dropdown .ck-dropdown__panel',
+            );
+          if (dropdownpanel) {
+            dropdownpanel.style.setProperty(
               '--ck-toolbar-dropdown-max-width',
               `${calculateToolbarPanelWidth()}vw`,
             );
+          }
 
           // CKEditor 4 had a feature to remove the required attribute
           // see: https://www.drupal.org/project/drupal/issues/1954968
