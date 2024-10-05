44c2a4dbea0c9feb41cbaf913dce7e119da34ea1
andrew farquharson <farquharson.a@gmail.com>
2024-10-05 03:51:47 +0100

    add test assertions for tabs

--- core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroRevisionsTabsUiTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Theme/ClaroRevisionsTabsUiTest.php
@@ -91,6 +91,9 @@ class ClaroRevisionsTabsUiTest extends WebDriverTestBase {
     $this->drupalGet('node/' . $this->nodes[0]->id() . '/revisions');
     $assert_session = $this->assertSession();
     $assert_session->elementExists('css', 'ul.tabs.tabs--primary.clearfix');
+    $assert_session->pageContains('node/' . $this->nodes[0]->id() . '/delete');
+    $assert_session->pageContains('node/' . $this->nodes[0]->id() . '/edit');
+    $assert_session->pageContains('node/' . $this->nodes[0]->id() . '/view');
   }
 
 }
