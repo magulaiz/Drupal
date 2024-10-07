cf2efb6addb649d9456e7dbc4757e70bd32baf6a
Abdulrahman Alshami <67413-Alshami@users.noreply.drupalcode.org>
2024-10-07 09:47:57 +0000

    Update file TwigNodeVisitor.php

--- core/lib/Drupal/Core/Template/TwigNodeVisitor.php
+++ core/lib/Drupal/Core/Template/TwigNodeVisitor.php
@@ -47,11 +47,7 @@ class TwigNodeVisitor implements NodeVisitorInterface {
       $class = get_class($node);
       $line = $node->getTemplateLine();
       return new $class(
-        new FunctionExpression(
-          new TwigFunction('render_var', [$env->getExtension(\Drupal::service('twig.extension')::class), 'renderVar']),
-          new Node([$node->getNode('expr')]),
-          $line
-        ),
+        new FunctionExpression('render_var', new Node([$node->getNode('expr')]), $line),
         $line
       );
     }
