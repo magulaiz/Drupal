2a5babd469ee092261d9e7c99691831cc9f8dd31
Ankit Pathak <1972-ankitv18@users.noreply.drupalcode.org>
2024-10-08 17:30:25 +0000

    Update file ConfigFormBase.php

--- core/lib/Drupal/Core/Form/ConfigFormBase.php
+++ core/lib/Drupal/Core/Form/ConfigFormBase.php
@@ -19,6 +19,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * -specific validation logic. To do that, override copyFormValuesToConfig().
  */
 abstract class ConfigFormBase extends FormBase {
+
   use ConfigFormBaseTrait;
 
   /**
