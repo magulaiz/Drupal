22ff414d817dc60e169e7b0b96e6205af89e2882
Stephen Mustgrave <smustgrave@gmail.com>
2024-10-09 15:54:00 -0400

    missing return

--- core/tests/Drupal/KernelTests/AssertContentTrait.php
+++ core/tests/Drupal/KernelTests/AssertContentTrait.php
@@ -729,7 +729,7 @@ trait AssertContentTrait {
    *   \Drupal\Component\Render\FormattableMarkup unless you cast the object to
    *   a string. If left blank, a default message will be displayed.
    */
-  protected function assertNoTitle($title, $message = '') {
+  protected function assertNoTitle($title, $message = ''): bool {
     $actual = (string) current($this->xpath('//title'));
     if (!$message) {
       $message = new FormattableMarkup('Page title @actual is not equal to @unexpected.', [
