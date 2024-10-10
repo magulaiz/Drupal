e952e29294329b102a669c42bd73565b5cc447a3
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2024-10-10 19:53:06 +0200

    kill 'em all!

--- core/modules/block/tests/src/Functional/BlockCacheTest.php
+++ core/modules/block/tests/src/Functional/BlockCacheTest.php
@@ -80,10 +80,6 @@ class BlockCacheTest extends BrowserTestBase {
    * Tests a cacheable block without any additional cache context.
    */
   public function testCachePermissions(): void {
-    // user.permissions is a required context, so a user with different
-    // permissions will see a different version of the block.
-    \Drupal::state()->set('block_test.cache_contexts', ['user.roles']);
-
     $current_content = $this->randomMachineName();
     \Drupal::state()->set('block_test.content', $current_content);
 
@@ -100,7 +96,6 @@ class BlockCacheTest extends BrowserTestBase {
 
     // Block content not served from cache.
     $this->drupalLogout();
-    $this->drupalGet('<front>');
     $this->assertSession()->pageTextContains($current_content);
   }
 
