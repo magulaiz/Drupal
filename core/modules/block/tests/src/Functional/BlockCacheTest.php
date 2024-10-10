d867bbd7e4b67a06b28ae95266c6b4eef63af2d3
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2024-10-10 19:40:52 +0200

    Cache  context 'user.roles'

--- core/modules/block/tests/src/Functional/BlockCacheTest.php
+++ core/modules/block/tests/src/Functional/BlockCacheTest.php
@@ -82,7 +82,7 @@ class BlockCacheTest extends BrowserTestBase {
   public function testCachePermissions(): void {
     // user.permissions is a required context, so a user with different
     // permissions will see a different version of the block.
-    \Drupal::state()->set('block_test.cache_contexts', ['user.permissions']);
+    \Drupal::state()->set('block_test.cache_contexts', ['user.roles']);
 
     $current_content = $this->randomMachineName();
     \Drupal::state()->set('block_test.content', $current_content);
