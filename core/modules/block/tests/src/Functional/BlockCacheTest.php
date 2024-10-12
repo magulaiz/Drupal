855094bd8a42a6d2422fe589e7e4a641672e95ec
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2024-10-12 17:56:43 +0200

    Reinstate \Drupal\Tests\block\Functional\BlockCacheTest::testCachePermissions() (kinda...)

--- core/modules/block/tests/src/Functional/BlockCacheTest.php
+++ core/modules/block/tests/src/Functional/BlockCacheTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\block\Functional;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -16,7 +17,7 @@ class BlockCacheTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['block_test'];
+  protected static $modules = ['block', 'block_test', 'test_page_test'];
 
   /**
    * {@inheritdoc}
@@ -24,19 +25,64 @@ class BlockCacheTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
-   * Tests a cacheable block without any additional cache context.
+   * A user with permission to administer blocks.
+   *
+   * @var object
    */
-  public function testCachePermissions(): void {
-    $adminUser = $this->drupalCreateUser([
+  protected $adminUser;
+
+  /**
+   * An authenticated user to test block caching.
+   *
+   * @var object
+   */
+  protected $normalUser;
+
+  /**
+   * Another authenticated user to test block caching.
+   *
+   * @var object
+   */
+  protected $normalUserAlt;
+
+  /**
+   * The block used by this test.
+   *
+   * @var \Drupal\block\BlockInterface
+   */
+  protected $block;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Create an admin user, log in and enable test blocks.
+    $this->adminUser = $this->drupalCreateUser([
       'administer blocks',
       'access administration pages',
     ]);
+    $this->drupalLogin($this->adminUser);
+
+    // Create additional users to test caching modes.
+    $this->normalUser = $this->drupalCreateUser();
+    $this->normalUserAlt = $this->drupalCreateUser();
+    // Sync the roles, since drupalCreateUser() creates separate roles for
+    // the same permission sets.
+    $this->normalUserAlt->roles = $this->normalUser->getRoles();
+    $this->normalUserAlt->save();
+
+    // Enable our test block.
+    $this->block = $this->drupalPlaceBlock('test_cache');
+  }
+
+  /**
+   * Tests a cacheable block without any additional cache context.
+   */
+  public function testCachePermissions(): void {
     $current_content = $this->randomMachineName();
     \Drupal::state()->set('block_test.content', $current_content);
-
-    $this->drupalPlaceBlock('test_cache');
-
-    $this->drupalLogin($adminUser);
 
     $this->assertSession()->pageTextContains($current_content);
 
