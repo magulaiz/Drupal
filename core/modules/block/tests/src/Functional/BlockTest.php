0cf4c3d8fcfc69bba6f74029ade9c394e2e90d6b
tyler <tyler@zoocha.com>
2024-10-13 00:31:32 +0100

    Issue #2912703: Fix some tests in BlockTest class.

--- core/modules/block/tests/src/Functional/BlockTest.php
+++ core/modules/block/tests/src/Functional/BlockTest.php
@@ -48,7 +48,7 @@ class BlockTest extends BlockTestBase {
     $this->assertSession()->checkboxChecked('edit-visibility-request-path-negate-0');
 
     $this->submitForm($edit, 'Save block');
-    $this->assertSession()->statusMessageContains('The block configuration has been saved.', 'status');
+    $this->assertSession()->statusMessageContains('The '. $block_name . ' block configuration has been saved.', 'status');
 
     $this->clickLink('Configure');
     $this->assertSession()->checkboxChecked('edit-visibility-request-path-negate-1');
@@ -138,7 +138,7 @@ class BlockTest extends BlockTestBase {
     // authenticated users.
     $this->drupalGet('admin/structure/block/add/' . $block_name . '/' . $default_theme);
     $this->submitForm($edit, 'Save block');
-    $this->assertSession()->statusMessageContains('The block configuration has been saved.', 'status');
+    $this->assertSession()->statusMessageContains('The ' . $block_name . ' block configuration has been saved.', 'status');
 
     // Confirm that block was not displayed according to block visibility
     // rules.
@@ -233,7 +233,7 @@ class BlockTest extends BlockTestBase {
       'id' => $block['id'],
       'region' => $block['region'],
     ], 'Save block');
-    $this->assertSession()->statusMessageContains('The block configuration has been saved.', 'status');
+    $this->assertSession()->statusMessageContains('The ' . $block['settings[label]'] . ' block configuration has been saved.', 'status');
     // Check to see if the block was created by checking its configuration.
     $instance = Block::load($block['id']);
 
@@ -313,7 +313,7 @@ class BlockTest extends BlockTestBase {
       $block['region'] = 'content';
       $this->drupalGet('admin/structure/block/add/system_powered_by_block');
       $this->submitForm($block, 'Save block');
-      $this->assertSession()->statusMessageContains('The block configuration has been saved.', 'status');
+      $this->assertSession()->statusMessageContains('The ' . $title . ' block configuration has been saved.', 'status');
       $this->assertSession()->addressEquals('admin/structure/block/list/' . $theme . '?block-placement=' . Html::getClass($block['id']));
 
       // Set the default theme and ensure the block is placed.
@@ -357,7 +357,7 @@ class BlockTest extends BlockTestBase {
     ];
     $this->drupalGet('admin/structure/block/add/' . $block_name . '/' . $default_theme);
     $this->submitForm($edit, 'Save block');
-    $this->assertSession()->statusMessageContains('The block configuration has been saved.', 'status');
+    $this->assertSession()->statusMessageContains('The ' . $block_name . 'block configuration has been saved.', 'status');
 
     // Confirm that the block is not displayed by default.
     $this->drupalGet('user');
