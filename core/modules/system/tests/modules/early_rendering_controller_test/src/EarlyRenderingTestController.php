3ad4a2f2fda7e0a76e24d9decefdc753bbd1e956
Abi أب <el@elabee.me>
2024-10-18 22:32:47 +0700

    add return type

--- core/modules/system/tests/modules/early_rendering_controller_test/src/EarlyRenderingTestController.php
+++ core/modules/system/tests/modules/early_rendering_controller_test/src/EarlyRenderingTestController.php
@@ -147,7 +147,7 @@ class EarlyRenderingTestController extends ControllerBase {
     return new CacheableTestDomainObject();
   }
 
-  public function withArgument(string $argument) {
+  public function withArgument(string $argument): Response {
     return new Response($argument);
   }
 
