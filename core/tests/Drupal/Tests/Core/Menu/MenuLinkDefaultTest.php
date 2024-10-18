55201245222697c945dc7aa3d5468c4cbea9eb80
MANIBHARTHIER <manibharthi.er@materialplus.io>
2024-10-18 09:16:44 +0530

    PHPCS Fixes

--- core/tests/Drupal/Tests/Core/Menu/MenuLinkDefaultTest.php
+++ core/tests/Drupal/Tests/Core/Menu/MenuLinkDefaultTest.php
@@ -50,7 +50,7 @@ class MenuLinkDefaultTest extends UnitTestCase {
       'menu_name' => 'admin',
       'parent' => '',
       'weight' => 10,
-      'promoted' => 1
+      'promoted' => 1,
     ];
     $expected_plugin_definition = $plugin_definition;
     $expected_plugin_definition['weight'] = -10;
