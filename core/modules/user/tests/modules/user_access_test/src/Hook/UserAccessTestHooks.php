e3da38a9b562dcbb64ad0dcd28108dd62932e155
nicxvan <nic@nlightened.net>
2024-10-19 16:14:26 -0400

    Fix formatting

--- core/modules/user/tests/modules/user_access_test/src/Hook/UserAccessTestHooks.php
+++ core/modules/user/tests/modules/user_access_test/src/Hook/UserAccessTestHooks.php
@@ -17,7 +17,7 @@ class UserAccessTestHooks {
    * Implements hook_ENTITY_TYPE_access() for entity type "user".
    */
   #[Hook('access_test_user_access', module: 'user')]
-    public function userAccess(User $entity, $operation, $account) {
+  public function userAccess(User $entity, $operation, $account) {
     if ($entity->getAccountName() == "no_edit" && $operation == "update") {
       // Deny edit access.
       return AccessResult::forbidden();
@@ -31,38 +31,38 @@ class UserAccessTestHooks {
       return AccessResult::allowedIfHasPermission($account, 'sub-admin');
     }
     return AccessResult::neutral();
-    }
+  }
 
-    /**
-     * Implements hook_entity_create_access().
-     */
-    #[Hook('entity_create_access')]
-    public function entityCreateAccess(AccountInterface $account, array $context, $entity_bundle) {
-      if ($context['entity_type_id'] != 'user') {
-        return AccessResult::neutral();
-      }
-      // Account with role sub-admin can create users.
-      return AccessResult::allowedIfHasPermission($account, 'sub-admin');
-    }
-
-    /**
-     * Implements hook_entity_field_access().
-     */
-    #[Hook('entity_field_access')]
-    public function entityFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, ?FieldItemListInterface $items = \NULL) {
-      // Account with role sub-admin can view the status, init and mail fields for
-      // user with no roles.
-      if ($field_definition->getTargetEntityTypeId() == 'user' && $operation === 'view' && \in_array($field_definition->getName(), ['status', 'init', 'mail'])) {
-        if ($items == \NULL || \count($items->getEntity()->getRoles()) == 1) {
-          return AccessResult::allowedIfHasPermission($account, 'sub-admin');
-        }
-      }
-      if (\Drupal::state()->get('user_access_test_forbid_mail_edit', \FALSE)) {
-        if ($operation === 'edit' && $items && $items->getEntity()->getEntityTypeId() === 'user' && $field_definition->getName() === 'mail') {
-          return AccessResult::forbidden();
-        }
-      }
+  /**
+   * Implements hook_entity_create_access().
+   */
+  #[Hook('entity_create_access')]
+  public function entityCreateAccess(AccountInterface $account, array $context, $entity_bundle) {
+    if ($context['entity_type_id'] != 'user') {
       return AccessResult::neutral();
     }
+    // Account with role sub-admin can create users.
+    return AccessResult::allowedIfHasPermission($account, 'sub-admin');
+  }
+
+  /**
+   * Implements hook_entity_field_access().
+   */
+  #[Hook('entity_field_access')]
+  public function entityFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, ?FieldItemListInterface $items = \NULL) {
+    // Account with role sub-admin can view the status, init and mail fields for
+    // user with no roles.
+    if ($field_definition->getTargetEntityTypeId() == 'user' && $operation === 'view' && \in_array($field_definition->getName(), ['status', 'init', 'mail'])) {
+      if ($items == \NULL || \count($items->getEntity()->getRoles()) == 1) {
+        return AccessResult::allowedIfHasPermission($account, 'sub-admin');
+      }
+    }
+    if (\Drupal::state()->get('user_access_test_forbid_mail_edit', \FALSE)) {
+      if ($operation === 'edit' && $items && $items->getEntity()->getEntityTypeId() === 'user' && $field_definition->getName() === 'mail') {
+        return AccessResult::forbidden();
+      }
+    }
+    return AccessResult::neutral();
+  }
 
 }
--- core/modules/user/tests/modules/user_form_test/src/Hook/UserFormTestHooks.php
+++ core/modules/user/tests/modules/user_form_test/src/Hook/UserFormTestHooks.php
@@ -12,9 +12,9 @@ class UserFormTestHooks {
    * Implements hook_form_FORM_ID_alter() for user_cancel_form().
    */
   #[Hook('form_test_form_user_cancel_form_alter', module: 'user')]
-    public function formUserCancelFormAlter(&$form, &$form_state) {
+  public function formUserCancelFormAlter(&$form, &$form_state) {
     $form['user_cancel_confirm']['#default_value'] = \FALSE;
     $form['access']['#value'] = \Drupal::currentUser()->hasPermission('cancel other accounts');
-    }
+  }
 
 }
