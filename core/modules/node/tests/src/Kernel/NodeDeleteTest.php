88de7e4bdea5e9eee308642be0830d4778fa9c33
James Shields <57457-lostcarpark@users.noreply.drupalcode.org>
2024-10-21 12:00:05 +0000

    Create kernel test to delete a node

---
+++ core/modules/node/tests/src/Kernel/NodeDeleteTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\node\Kernel;
+
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+
+/**
+ * Tests node validation constraints.
+ *
+ * @group node
+ */
+class NodeDeleteTest extends EntityKernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['node'];
+
+  /**
+   * Set the default field storage backend for fields created during tests.
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Create a node type for testing.
+    $type = NodeType::create(['type' => 'page', 'name' => 'page']);
+    $type->save();
+  }
+
+  /**
+   * Tests the node validation constraints.
+   */
+  public function testDelete(): void {
+    $this->createUser();
+    $node = Node::create(['type' => 'page', 'title' => 'test', 'uid' => 1]);
+    $node->save();
+    $node->delete();
+  }
+}
