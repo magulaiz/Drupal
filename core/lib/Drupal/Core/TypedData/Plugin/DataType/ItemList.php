45c08407bad3b7f97bde8a2f5e5e457b0d43d84d
Stephen Mustgrave <38930-smustgrave@users.noreply.drupalcode.org>
2024-10-21 17:22:57 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/lib/Drupal/Core/TypedData/Plugin/DataType/ItemList.php
+++ core/lib/Drupal/Core/TypedData/Plugin/DataType/ItemList.php
@@ -306,7 +306,7 @@ class ItemList extends TypedData implements \IteratorAggregate, ListInterface {
   /**
    * {@inheritdoc}
    */
-  public function last(): ?TypedDataInterface{
+  public function last(): ?TypedDataInterface {
     return $this->get($this->count() - 1);
   }
 
