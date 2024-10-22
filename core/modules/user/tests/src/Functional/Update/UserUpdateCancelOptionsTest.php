586d62ecd701c0b6209e6e28895ddf865445df6e
Eduardo Morales <eduardo.morales@metadrop.net>
2024-10-22 12:18:30 +0200

    Issue #3384789 by eduardo morales alberti: Allow user admins to choose the user cancellation method on the user deletion confirmation form (rather than having a single sitewide setting)

--- core/modules/user/tests/src/Functional/Update/UserUpdateCancelOptionsTest.php
+++ core/modules/user/tests/src/Functional/Update/UserUpdateCancelOptionsTest.php
@@ -18,7 +18,7 @@ class UserUpdateCancelOptionsTest extends UpdatePathTestBase {
    * {@inheritdoc}
    */
   protected function setDatabaseDumpFiles(): void {
-    $this->databaseDumpFiles[] = __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.bare.standard.php.gz';
+    $this->databaseDumpFiles[] = __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.bare.standard.php.gz';
   }
 
   /**
