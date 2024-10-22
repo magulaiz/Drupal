cdf5c555e50a93f77098b1c5ac56e64c9fe970e7
Your Name <you@example.com>
2024-10-22 22:54:10 +0200

    Issue #2265487 by sleitner, tanuj.: ConfigEntity based lists with items containing non-ascii characters are not sorted correctly

--- core/tests/Drupal/Tests/Core/Config/Entity/ConfigEntityBaseUnitTest.php
+++ core/tests/Drupal/Tests/Core/Config/Entity/ConfigEntityBaseUnitTest.php
@@ -540,22 +540,22 @@ class ConfigEntityBaseUnitTest extends UnitTestCase {
 
     // Test sorting by label.
     $list = [$entity_a, $entity_b];
-    ConfigEntityBase::sortEntities($list, $collator);
+    $list = ConfigEntityBase::sortEntities($list, $collator);
     $this->assertSame($entity_b, reset($list));
 
     $list = [$entity_b, $entity_a];
-    ConfigEntityBase::sortEntities($list, $collator);
+    $list = ConfigEntityBase::sortEntities($list, $collator);
     $this->assertSame($entity_b, reset($list));
 
     // Test sorting by weight.
     $entity_a->weight = 0;
     $entity_b->weight = 1;
     $list = [$entity_b, $entity_a];
-    ConfigEntityBase::sortEntities($list, $collator);
+    $list = ConfigEntityBase::sortEntities($list, $collator);
     $this->assertSame($entity_a, reset($list));
 
     $list = [$entity_a, $entity_b];
-    ConfigEntityBase::sortEntities($list, $collator);
+    $list = ConfigEntityBase::sortEntities($list, $collator);
     $this->assertSame($entity_a, reset($list));
   }
 
