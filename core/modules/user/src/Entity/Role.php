fd50858ab33d1b23fcd88b1e104637fdf7d8fc8e
Your Name <you@example.com>
2024-10-23 01:04:15 +0200

    Issue #2265487 by sleitner, tanuj.: ConfigEntity based lists with items containing non-ascii characters are not sorted correctly

--- core/modules/user/src/Entity/Role.php
+++ core/modules/user/src/Entity/Role.php
@@ -170,9 +170,9 @@ class Role extends ConfigEntityBase implements RoleInterface {
    */
   public static function postLoad(EntityStorageInterface $storage, array &$entities) {
     parent::postLoad($storage, $entities);
-    // Sort the queried roles by their weight.
-    // See \Drupal\Core\Config\Entity\ConfigEntityBase::sort().
-    uasort($entities, [static::class, 'sort']);
+    // Sort the entities using the entity class's sortEntities() method.
+    $collator = \Collator::create((!extension_loaded('intl')) ? ('en') : (\Drupal::service('language_manager')->getCurrentLanguage()->getId()));
+    static::class::sortEntities($entities, $collator);
   }
 
   /**
