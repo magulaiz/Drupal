8cb8896e38e6ac958f938e622ea8ef4b5eab3cac
andrew farquharson <farquharson.a@gmail.com>
2024-10-24 00:59:27 +0100

    edit error message, edit error message in test

--- core/modules/media_library/tests/src/FunctionalJavascript/WidgetOEmbedTest.php
+++ core/modules/media_library/tests/src/FunctionalJavascript/WidgetOEmbedTest.php
@@ -130,7 +130,7 @@ class WidgetOEmbedTest extends MediaLibraryTestBase {
     // assertWaitOnAjaxRequest() required for input "id" attributes to
     // consistently match their label's "for" attribute.
     $assert_session->assertWaitOnAjaxRequest();
-    $this->waitForText('Could not retrieve the oEmbed resource.');
+    $this->waitForText('Check the url. It does not correspond to any remote oEmbed resource.');
 
     // Select a media item to check if the selection is persisted when adding
     // new items.
@@ -316,7 +316,7 @@ class WidgetOEmbedTest extends MediaLibraryTestBase {
     // assertWaitOnAjaxRequest() required for input "id" attributes to
     // consistently match their label's "for" attribute.
     $assert_session->assertWaitOnAjaxRequest();
-    $this->waitForText('Could not retrieve the oEmbed resource.');
+    $this->waitForText('Check the url. It does not correspond to any remote oEmbed resource.');
 
     // Select a media item to check if the selection is persisted when adding
     // new items.
