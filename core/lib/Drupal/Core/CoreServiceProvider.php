0a4620b14a717f17fc2d4ceb4168b4b372534692
catch <6915-catch@users.noreply.drupalcode.org>
2024-10-24 18:37:05 +0100

    Autoconfigure!

--- core/lib/Drupal/Core/CoreServiceProvider.php
+++ core/lib/Drupal/Core/CoreServiceProvider.php
@@ -26,6 +26,7 @@ use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
 use Drupal\Core\Extension\ModuleUninstallValidatorInterface;
 use Drupal\Core\Plugin\PluginManagerPass;
+use Drupal\Core\PreWarm\CachePreWarmerInterface;
 use Drupal\Core\Queue\QueueFactoryInterface;
 use Drupal\Core\Render\MainContent\MainContentRenderersPass;
 use Drupal\Core\Site\Settings;
@@ -114,6 +115,9 @@ class CoreServiceProvider implements ServiceProviderInterface, ServiceModifierIn
 
     $container->registerForAutoconfiguration(QueueFactoryInterface::class)
       ->addTag('queue_factory');
+
+    $container->registerForAutoconfiguration(CachePreWarmerInterface::class)
+      ->addTag('cache_prewarmable');
 
     $container->registerForAutoconfiguration(ModuleUninstallValidatorInterface::class)
       ->addTag('module_install.uninstall_validator');
