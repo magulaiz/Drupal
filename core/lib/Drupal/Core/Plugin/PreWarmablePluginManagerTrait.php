7c775b4b233530fdc33737963f162f18e46dfad9
catch <6915-catch@users.noreply.drupalcode.org>
2024-10-24 07:49:57 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Alexander Varwijk <22619-Kingdutch@users.noreply.drupalcode.org>

--- core/lib/Drupal/Core/Plugin/PreWarmablePluginManagerTrait.php
+++ core/lib/Drupal/Core/Plugin/PreWarmablePluginManagerTrait.php
@@ -4,6 +4,9 @@ namespace Drupal\Core\Plugin;
 
 /**
  * Provides a trait for Drupal\Core\PreWarm\PreWarmableInterface.
+ *
+ * @phpstan-require-implements \Drupal\Component\Plugin\Discovery\DiscoveryInterface
+ * @phpstan-require-implements \Drupal\Core\PreWarm\PreWarmableInterface
  */
 trait PreWarmablePluginManagerTrait {
 
