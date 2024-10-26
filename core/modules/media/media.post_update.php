b6c445490bd80fd97037202b330552b18100a9c3
Chris Green <chrisgreen@arizona.edu>
2024-10-25 21:14:18 -0700

    Move update from .install to .post_update.php and added sandbox batch loop

--- core/modules/media/media.post_update.php
+++ core/modules/media/media.post_update.php
@@ -7,6 +7,7 @@
 
 use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\media\MediaConfigUpdater;
 
 /**
@@ -44,4 +45,47 @@ function media_post_update_set_blank_iframe_domain_to_null() {
       ->set('iframe_domain', NULL)
       ->save(TRUE);
   }
+}
+
+/**
+ * Disable contextual links for media embeds in all text formats.
+ */
+function media_post_update_add_show_contextual_links_as_falsse(&$sandbox = NULL): TranslatableMarkup {
+  // Initialize batch variables if this is the first run.
+  if (!isset($sandbox['total'])) {
+    $query = \Drupal::entityQuery('filter_format');
+    $sandbox['ids'] = $query->execute();
+    $sandbox['total'] = count($sandbox['ids']);
+    $sandbox['progress'] = 0;
+
+    // In case there are no entities to process.
+    if ($sandbox['total'] == 0) {
+      $sandbox['total'] = 1;
+      $sandbox['progress'] = 1;
+    }
+  }
+  // Load the filter formats in chunks of 50.
+  $ids = array_splice($sandbox['ids'], 0, 50);
+  $filter_formats = \Drupal\filter\Entity\FilterFormat::loadMultiple($ids);
+  /** @var \Drupal\filter\Entity\FilterFormat $filter_format */
+  foreach ($filter_formats as $filter_format) {
+    $filters = $filter_format->get('filters');
+    if (isset($filters['media_embed'])) {
+      $media_embed_settings = $filters['media_embed'];
+      if (empty($media_embed_settings['settings']['show_contextual_links'])) {
+        $media_embed_settings['settings']['show_contextual_links'] = FALSE;
+        $filter_format->setFilterConfig('media_embed', $media_embed_settings);
+        $filter_format->save();
+      }
+    }
+    $sandbox['progress']++;
+  }
+
+  // Determine if the batch process is complete.
+  $sandbox['#finished'] = ($sandbox['progress'] / $sandbox['total']);
+
+  return new TranslatableMarkup('Processed Filter Formats (@count/@total)', [
+    '@count' => $sandbox['progress'],
+    '@total' => $sandbox['total'],
+  ]);
 }
