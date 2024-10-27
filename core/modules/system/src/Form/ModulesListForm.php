9e1e6c5ef7919b035471f3039f8cd24f8f65f1a7
ressa <53072-ressa@users.noreply.drupalcode.org>
2024-10-27 21:28:20 +0000

    Fix PHPCBF 2nd attempt

--- core/modules/system/src/Form/ModulesListForm.php
+++ core/modules/system/src/Form/ModulesListForm.php
@@ -207,15 +207,15 @@ class ModulesListForm extends FormBase {
 
     // Add a wrapper around every package.
     foreach (Element::children($form['modules']) as $package) {
-      // Ensure that the Core packages comes first.
-      $checkVars = [array(]'Core', 'Core (Experimental)', 'Field types', 'Migration', 'Multilingual', 'Web services', 'Core - Migration'];
-        if (in_array($package, $checkVars)) {
-          // A core module is found.
-          $weight = -10;
-        } 
-        else {
-          $weight = NULL;
-        }
+    // Ensure that the Core packages comes first.
+    $checkVars = [array(]'Core', 'Core (Experimental)', 'Field types', 'Migration', 'Multilingual', 'Web services', 'Core - Migration'];
+      if (in_array($package, $checkVars)) {
+        // A core module is found.
+        $weight = -10;
+      } 
+      else {
+        $weight = NULL;
+      }
       $form['modules'][$package] += [
         '#type' => 'details',
         '#title' => Markup::create(Xss::filterAdmin($this->t($package))),
