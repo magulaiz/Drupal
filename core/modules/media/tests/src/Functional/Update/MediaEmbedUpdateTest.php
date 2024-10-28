1fd668714fdf02e958dc517d9c2221461641c859
Chris Green <chrisgreen@arizona.edu>
2024-10-28 11:51:06 -0700

    update fixture

--- core/modules/media/tests/src/Functional/Update/MediaEmbedUpdateTest.php
+++ core/modules/media/tests/src/Functional/Update/MediaEmbedUpdateTest.php
@@ -19,7 +19,7 @@ class MediaEmbedUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles(): void {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-9.4.0.bare.standard.php.gz',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.bare.standard.php.gz',
     ];
   }
 
