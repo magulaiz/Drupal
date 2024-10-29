3472d9b0bfee4734f4a35c15fd48903ac40d9bb8
Ghost Of Drupal Past <drupal@negyesi.net>
2024-10-29 17:10:10 +0100

    pageAttachments

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -40621,6 +40621,12 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/modules/system/src/Hook/SystemHooks.php',
 ];
 $ignoreErrors[] = [
+        // identifier: missingType.return
+        'message' => '#^Method Drupal\\\\system\\\\Hook\\\\SystemHooks\\:\\:pageAttachments\\(\\) has no return type specified\\.$#',
+        'count' => 1,
+        'path' => __DIR__ . '/modules/system/src/Hook/SystemHooks.php',
+];
+$ignoreErrors[] = [
 	// identifier: missingType.return
 	'message' => '#^Method Drupal\\\\system\\\\Hook\\\\SystemHooks\\:\\:pageTop\\(\\) has no return type specified\\.$#',
 	'count' => 1,
