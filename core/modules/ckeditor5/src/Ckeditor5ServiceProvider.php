76c7e538bd3cb58c2ede555657dd9a24e0cd74c1
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-10-29 10:33:21 -0400

    Conversion

--- core/modules/ckeditor5/src/Ckeditor5ServiceProvider.php
+++ core/modules/ckeditor5/src/Ckeditor5ServiceProvider.php
@@ -13,7 +13,7 @@ class Ckeditor5ServiceProvider extends ServiceProviderBase {
    */
   public function alter(ContainerBuilder $container) {
     HookOrder::after($container, 'form_filter_format_form_alter',
-      'Drupal\\ckeditor5\\Hook\\Ckeditor5Hooks::formFilterFormatFormAlter' ,
+      'Drupal\\ckeditor5\\Hook\\Ckeditor5Hooks::formFilterFormatFormAlter',
       'Drupal\\editor\\Hook\\EditorHooks::formFilterFormatFormAlter',
       'Drupal\\media\\Hook\\MediaHooks::formFilterFormatFormAlter');
   }
