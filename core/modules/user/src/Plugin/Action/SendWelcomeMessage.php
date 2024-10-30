e853f5536e7524e99477e83bdaaa0f60f1ff6c9b
Bhanu <50266-Bhanu951@users.noreply.drupalcode.org>
2024-10-30 17:03:54 +0530

    Issue #5688 : Resend welcome message from admin user/edit.

---
+++ core/modules/user/src/Plugin/Action/SendWelcomeMessage.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\user\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Send welcome message to a user.
+ *
+ * @Action(
+ *   id = "user_welcome_message_action",
+ *   label = @Translation("Send welcome message to selected users"),
+ *   type = "user"
+ * )
+ */
+class SendWelcomeMessage extends ActionBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new SendWelcomeMessage object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($account = NULL) {
+    if (empty($account) || empty($account->getEmail())) {
+      return;
+    }
+
+    $op = 'register_pending_approval';
+    if ($account->isActive()) {
+      // Determine the user approval method.
+      switch ($this->configFactory->get('user.settings')->get('register')) {
+        case UserInterface::REGISTER_ADMINISTRATORS_ONLY:
+          $op = 'register_admin_created';
+          break;
+
+        case UserInterface::REGISTER_VISITORS:
+        default:
+          $op = 'register_no_approval_required';
+      }
+    }
+
+    _user_mail_notify($op, $account);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\user\UserInterface $object */
+    $access = $object->status->access('edit', $account, TRUE)
+      ->andIf($object->access('update', $account, TRUE));
+
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+}
--- core/modules/user/user.post_update.php
+++ core/modules/user/user.post_update.php
@@ -5,6 +5,10 @@
  * Post update functions for User module.
  */
 
+use Drupal\system\Entity\Action;
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+use Drupal\user\Entity\Role;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -15,4 +19,29 @@ function user_removed_post_updates(): array {
     'user_post_update_sort_permissions' => '11.0.0',
     'user_post_update_sort_permissions_again' => '11.0.0',
   ];
+}
+
+/**
+ * Ensure permissions stored in role configuration are sorted using the schema.
+ */
+function user_post_update_sort_permissions(&$sandbox = NULL) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'user_role', function (Role $role) {
+    $permissions = $role->getPermissions();
+    sort($permissions);
+    return $permissions !== $role->getPermissions();
+  });
+}
+
+/**
+ * Add an action to send activation emails to multiple users.
+ */
+function user_post_update_add_send_action() {
+  $action = Action::create([
+    'id' => 'user_welcome_message_action',
+    'type' => 'user',
+    'label' => 'Send the welcome message to the selected user(s)',
+    'configuration' => [],
+    'plugin' => 'user_welcome_message_action',
+  ]);
+  $action->trustData()->save();
 }
