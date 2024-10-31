439db106566c617a42ae219e713a7c55f25cc34d
Bhanu <50266-Bhanu951@users.noreply.drupalcode.org>
2024-10-31 15:08:06 +0530

    Attributes used in plugin.

--- core/modules/user/src/Plugin/Action/SendWelcomeMessage.php
+++ core/modules/user/src/Plugin/Action/SendWelcomeMessage.php
@@ -6,63 +6,66 @@ namespace Drupal\user\Plugin\Action;
 
 use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Action\Attribute\Action;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\user\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Send welcome message to a user.
- *
- * @Action(
- *   id = "user_welcome_message_action",
- *   label = @Translation("Send welcome message to selected users"),
- *   type = "user"
- * )
+ * Provides a Send welcome message to selected users action.
  */
-class SendWelcomeMessage extends ActionBase implements ContainerFactoryPluginInterface {
+#[Action(
+  id: 'user_welcome_message_action',
+  label: new TranslatableMarkup('Send welcome message to selected users'),
+  category: new TranslatableMarkup('Custom'),
+  type: 'user',
+)]
+final class SendWelcomeMessage extends ActionBase implements ContainerFactoryPluginInterface {
 
   /**
-   * The config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   * {@inheritdoc}
    */
-  protected $configFactory;
-
-  /**
-   * Constructs a new SendWelcomeMessage object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory.
-   */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config_factory) {
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly ConfigFactoryInterface $configFactory,
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->configFactory = $config_factory;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('config.factory')
+      $container->get('config.factory'),
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function execute($account = NULL): void {
+  public function access($entity, ?AccountInterface $account = NULL, $return_as_object = FALSE): AccessResultInterface|bool {
+    /** @var \Drupal\user\UserInterface $object */
+    $access = $object->status->access('edit', $account, TRUE)
+      ->andIf($object->access('update', $account, TRUE));
+
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(?ContentEntityInterface $entity = NULL): void {
+
     if (empty($account) || empty($account->getEmail())) {
       return;
     }
@@ -82,17 +85,6 @@ class SendWelcomeMessage extends ActionBase implements ContainerFactoryPluginInt
     }
 
     _user_mail_notify($op, $account);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE): AccessResultInterface|bool {
-    /** @var \Drupal\user\UserInterface $object */
-    $access = $object->status->access('edit', $account, TRUE)
-      ->andIf($object->access('update', $account, TRUE));
-
-    return $return_as_object ? $access : $access->isAllowed();
   }
 
 }
