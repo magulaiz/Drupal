53b6aa1b491d8495d57b0887e1bff5f045cd3383
Lee Rowlands <lee.rowlands@previousnext.com.au>
2024-10-31 15:04:28 +1000

    Fix last test

--- core/modules/node/src/NodeForm.php
+++ core/modules/node/src/NodeForm.php
@@ -9,6 +9,7 @@ use Drupal\Core\Entity\EntityMalformedException;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\Core\Url;
@@ -330,7 +331,7 @@ class NodeForm extends ContentEntityForm {
    */
   protected function buildRedirectUrl(NodeInterface $node): Url {
     try {
-      $node_url = $node->toUrl();
+      $node_url = $node->getTranslation(LanguageInterface::LANGCODE_DEFAULT)->toUrl();
 
       if ($node_url->access()) {
         return $node_url;
