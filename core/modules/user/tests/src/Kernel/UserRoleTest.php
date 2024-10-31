9c7663c54168d97c7c17edd491ec6227f8fc63cd
Claudiu Cristea <7931-claudiucristea@users.noreply.drupalcode.org>
2024-10-31 08:42:09 +0000

    Test coverage

---
+++ core/modules/user/tests/src/Kernel/UserRoleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\user\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\user\Traits\UserCreationTrait;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
+
+/**
+ * @group user
+ * @coversDefaultClass \Drupal\user\Entity\User
+ */
+class UserRoleTest extends KernelTestBase {
+
+  use UserCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['user'];
+
+  /**
+   * @covers ::isAdmin
+   */
+  public function testUserIsAdmin(): void {
+    $role1 = Role::create(['id' => 'role1']);
+    $role2 = Role::create(['id' => 'role2']);
+    /** @var \Drupal\user\UserInterface $account */
+    $account = User::create([
+      'name' => $this->randomMachineName(),
+      'roles' => [$role1, $role2],
+    ]);
+    $this->assertFalse($account->isAdmin());
+    $role3 = Role::create(['id' => 'role3', 'is_admin' => TRUE]);
+    $account->get('roles')->appendItem($role3);
+    $this->assertTrue($account->isAdmin());
+  }
+
+}
