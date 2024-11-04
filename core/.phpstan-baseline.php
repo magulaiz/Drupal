6a26faad670987b4a4472439331a318d114048ff
James Williams <james.williams@computerminds.co.uk>
2024-11-04 16:20:13 +0000

    Issue #3091336: Update baseline for phpstan.

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -1546,7 +1546,7 @@ $ignoreErrors[] = [
 $ignoreErrors[] = [
 	// identifier: property.notFound
 	'message' => '#^Access to an undefined property Drupal\\\\path\\\\Plugin\\\\Field\\\\FieldType\\\\PathItem\\:\\:\\$alias\\.$#',
-	'count' => 4,
+	'count' => 5,
 	'path' => __DIR__ . '/modules/path/src/Plugin/Field/FieldType/PathItem.php',
 ];
 $ignoreErrors[] = [
