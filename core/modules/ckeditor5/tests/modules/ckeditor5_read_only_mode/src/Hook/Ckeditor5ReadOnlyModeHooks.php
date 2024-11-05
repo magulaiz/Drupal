7e525c1cee861ae786d85d28f093e523b1225d4f
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-11-05 10:28:06 -0500

    Fix ckeditor readonly

--- core/modules/ckeditor5/tests/modules/ckeditor5_read_only_mode/src/Hook/Ckeditor5ReadOnlyModeHooks.php
+++ core/modules/ckeditor5/tests/modules/ckeditor5_read_only_mode/src/Hook/Ckeditor5ReadOnlyModeHooks.php
@@ -13,9 +13,9 @@ use Drupal\Core\Hook\Attribute\Hook;
 class Ckeditor5ReadOnlyModeHooks {
 
   /**
-   * Implements hook_form_alter().
+   * Implements hook_form_FORM_ID_alter().
    */
-  #[Hook('form_alter', module: 'ckeditor5_read_only_mode_form_node_page')]
+  #[Hook('form_node_page_form_alter')]
   public function formNodePageFormAlter(array &$form, FormStateInterface $form_state, string $form_id) : void {
     $form['body']['#disabled'] = \Drupal::state()->get('ckeditor5_read_only_mode_body_enabled', FALSE);
     $form['field_second_ckeditor5_field']['#disabled'] = \Drupal::state()->get('ckeditor5_read_only_mode_second_ckeditor5_field_enabled', FALSE);
