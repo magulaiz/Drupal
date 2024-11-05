4dd789d488bb3c689f16a9695b156e83320fda4a
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2024-11-05 00:25:27 +0000

    Update filter.admin.js Revert to .is

--- core/modules/filter/js/filter.admin.js
+++ core/modules/filter/js/filter.admin.js
@@ -61,7 +61,7 @@
             // Hide the table labels if there are no visible rows.
             let visibleRows = false;
             $table.find('tr').each(function () {
-              if ($(this).matches(':visible')) {
+              if ($(this).is(':visible')) {
                 visibleRows = true;
               }
             });
