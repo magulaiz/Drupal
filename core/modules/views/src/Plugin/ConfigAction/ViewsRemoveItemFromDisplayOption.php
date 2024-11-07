32a7e479898b376d35e35d634bff75ff8c6bb56a
Artem  Dmitriiev <a.dmitriiev@1xinternet.de>
2024-11-07 11:46:01 +0100

    Issue #3305859 by a.dmitriiev: Add config actions for views

---
+++ core/modules/views/src/Plugin/ConfigAction/ViewsRemoveItemFromDisplayOption.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\views\Plugin\ConfigAction;
+
+use Drupal\Core\Config\Action\Attribute\ConfigAction;
+use Drupal\Core\Config\Action\ConfigActionException;
+use Drupal\Core\Config\Action\ConfigActionPluginInterface;
+use Drupal\Core\Config\ConfigManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\views\ViewEntityInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Config action for removing item from display option.
+ */
+#[ConfigAction(
+  id: 'view:removeItemFromDisplayOption',
+  admin_label: new TranslatableMarkup('Views remove item from display option'),
+  entity_types: ['view'],
+)]
+class ViewsRemoveItemFromDisplayOption implements ConfigActionPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs instance of ViewsAddItemToDisplayOption.
+   *
+   * @param \Drupal\Core\Config\ConfigManagerInterface $configManager
+   *   The configuration manager.
+   */
+  public function __construct(
+    protected readonly ConfigManagerInterface $configManager
+  ) {
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($container->get('config.manager'));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function apply(string $configName, mixed $value): void {
+    if (empty($value)) {
+      throw new ConfigActionException(sprintf('View %s cannot be updated because no option settings were provided', $configName));
+    }
+    if (empty($value['option'])) {
+      throw new ConfigActionException('No view display option provided');
+    }
+    $option = $value['option'];
+    if (empty($value['item'])) {
+      throw new ConfigActionException(sprintf('No view display %s item name provided', $option));
+    }
+    $item = $value['item'];
+    // Load the views executable.
+    $entity = $this->configManager->loadConfigEntityByName($configName);
+    if (empty($entity)) {
+      throw new ConfigActionException(sprintf('View %s does not exist', $configName));
+    }
+    if (!$entity instanceof ViewEntityInterface) {
+      throw new ConfigActionException(sprintf('%s is not view', $configName));
+    }
+    $view = $entity->getExecutable();
+    $display_id = NULL;
+    if (!empty($value['display_id'])) {
+      $display_id = $value['display_id'];
+    }
+    $override = FALSE;
+    if (!empty($value['override'])) {
+      $override = TRUE;
+    }
+    $view->setDisplay($display_id);
+    $option_settings = $view->displayHandlers->get('default')->getOption($option);
+    if (!empty($option_settings[$item])) {
+      unset($option_settings[$item]);
+      if ($override) {
+        $view->displayHandlers->get('default')->overrideOption($option, $option_settings);
+      }
+      else {
+        $view->displayHandlers->get('default')->setOption($option, $option_settings);
+      }
+      $errors = $view->validate();
+      if (!empty($errors)) {
+        throw new ConfigActionException(sprintf('Validation of the view ended with following errors: %s', implode(', ', $errors)));
+      }
+      $view->save();
+    }
+  }
+
+}
---
+++ core/modules/views/src/Plugin/ConfigAction/ViewsSetDisplayOption.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\views\Plugin\ConfigAction;
+
+use Drupal\Core\Config\Action\Attribute\ConfigAction;
+use Drupal\Core\Config\Action\ConfigActionException;
+use Drupal\Core\Config\Action\ConfigActionPluginInterface;
+use Drupal\Core\Config\ConfigManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\views\ViewEntityInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Config action for setting display option.
+ */
+#[ConfigAction(
+  id: 'view:setDisplayOption',
+  admin_label: new TranslatableMarkup('Views set display option'),
+  entity_types: ['view'],
+)]
+class ViewsSetDisplayOption implements ConfigActionPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs instance of ViewsSetDisplayOption.
+   *
+   * @param \Drupal\Core\Config\ConfigManagerInterface $configManager
+   *   The configuration manager.
+   */
+  public function __construct(
+    protected readonly ConfigManagerInterface $configManager
+  ) {
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($container->get('config.manager'));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function apply(string $configName, mixed $value): void {
+    if (empty($value)) {
+      throw new ConfigActionException(sprintf('View %s cannot be updated because no option settings were provided', $configName));
+    }
+    if (empty($value['option'])) {
+      throw new ConfigActionException('No view display option provided');
+    }
+    $option = $value['option'];
+    if (empty($value['settings'])) {
+      throw new ConfigActionException('No view display option settings provided');
+    }
+    $settings = $value['settings'];
+    // Load the views executable.
+    $entity = $this->configManager->loadConfigEntityByName($configName);
+    if (empty($entity)) {
+      throw new ConfigActionException(sprintf('View %s does not exist', $configName));
+    }
+    if (!$entity instanceof ViewEntityInterface) {
+      throw new ConfigActionException(sprintf('%s is not view', $configName));
+    }
+    $view = $entity->getExecutable();
+    $display_id = NULL;
+    if (!empty($value['display_id'])) {
+      $display_id = $value['display_id'];
+    }
+    $override = FALSE;
+    if (!empty($value['override'])) {
+      $override = TRUE;
+    }
+    $view->setDisplay($display_id);
+    if ($override) {
+      $view->displayHandlers->get('default')->overrideOption($option, $settings);
+    }
+    else {
+      $view->displayHandlers->get('default')->setOption($option, $settings);
+    }
+    $errors = $view->validate();
+    if (!empty($errors)) {
+      throw new ConfigActionException(sprintf('Validation of the view ended with following errors: %s', implode(', ', $errors)));
+    }
+    $view->save();
+  }
+
+}
