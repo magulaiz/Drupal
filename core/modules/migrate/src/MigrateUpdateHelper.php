8f523a815eb124681472e3d00bb301305f0d2ebd
Edouard Cunibil <edouard@happyculture.coop>
2024-11-07 13:38:20 +0100

    Improve upgrade path to handle shared tables case.

--- core/modules/migrate/src/MigrateUpdateHelper.php
+++ core/modules/migrate/src/MigrateUpdateHelper.php
@@ -3,6 +3,7 @@
 namespace Drupal\migrate;
 
 use Drupal\Core\Database\Connection;
+use Drupal\migrate\Plugin\migrate\id_map\Sql;
 
 /**
  * Helper for migrate update hook.
@@ -37,22 +38,46 @@ class MigrateUpdateHelper {
     $migration_plugin_manager = \Drupal::service('plugin.manager.migration');
     $definitions = $migration_plugin_manager->getDefinitions();
     // Loop though all existing migrations and check if there are existing
-    // tables for this migration. If so, then update the table names.
+    // tables for this migration. If so, then create the new tables and copy
+    // over the legacy data before removing legacy tables.
+    $copied_table_names = [];
     foreach ($definitions as $definition) {
       $legacy_table_names = $this->getLegacyTableNames($definition['id']);
       if (in_array($legacy_table_names[0], $current_map_table_names)) {
         /** @var \Drupal\migrate\Plugin\Migration $migration */
         $migration = $migration_plugin_manager->createStubMigration($definition);
         $id_map = $migration->getIdMap();
-        if ($legacy_table_names[0] != $id_map->mapTableName()) {
-          $this->database->schema()
-            ->renameTable($legacy_table_names[0], $id_map->mapTableName());
+        // The upgrade only affects sql mapping.
+        if (!($id_map instanceof Sql)) {
+          continue;
         }
-        if ($legacy_table_names[1] != $id_map->messageTableName()) {
-          $this->database->schema()
-            ->renameTable($legacy_table_names[1], $id_map->messageTableName());
+
+        // Create new tables.
+        $id_map->getDatabase();
+
+        // Copy legacy data to new tables.
+        $tables_names = [
+          $legacy_table_names[0] => $id_map->mapTableName(),
+          $legacy_table_names[1] => $id_map->messageTableName(),
+        ];
+        foreach ($tables_names as $legacy_name => $new_name) {
+          if ($legacy_name === $new_name) {
+            continue;
+          }
+          $select_query = $this->database->select($legacy_name)->execute();
+          $insert_query = $this->database->insert($new_name);
+          while ($row = $select_query->fetchAssoc()) {
+            $insert_query->fields($row);
+          }
+          $insert_query->execute();
+          $copied_table_names[] = $legacy_name;
         }
       }
+    }
+
+    // Remove legacy tables that have been copied.
+    foreach ($copied_table_names as $table_name) {
+      $this->database->schema()->dropTable($table_name);
     }
   }
 
