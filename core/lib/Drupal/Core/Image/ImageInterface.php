ebb4e9b479a1cf2d1a013bca905ffef6151063db
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2024-11-08 20:52:15 +0000

    First commit

--- core/lib/Drupal/Core/Image/ImageInterface.php
+++ core/lib/Drupal/Core/Image/ImageInterface.php
@@ -10,6 +10,7 @@ interface ImageInterface {
   /**
    * Checks if the image is valid.
    *
+   * @return-type-will-be-added in drupal:12.0.0
    * @return bool
    *   TRUE if the image object contains a valid image, FALSE otherwise.
    */
@@ -18,6 +19,7 @@ interface ImageInterface {
   /**
    * Returns the height of the image.
    *
+   * @return-type-will-be-added in drupal:12.0.0
    * @return int|null
    *   The height of the image, or NULL if the image is invalid.
    */
@@ -26,6 +28,7 @@ interface ImageInterface {
   /**
    * Returns the width of the image.
    *
+   * @return-type-will-be-added in drupal:12.0.0
    * @return int|null
    *   The width of the image, or NULL if the image is invalid.
    */
