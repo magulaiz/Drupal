3d07f58c88abeba93fd2ba5c8fed79171d4ef157
Arunkumar Kuppuswamy <3594-arunkumark@users.noreply.drupalcode.org>
2024-11-08 16:19:54 +0530

    Do not require a 'title' field

--- core/modules/node/src/Entity/Node.php
+++ core/modules/node/src/Entity/Node.php
@@ -278,6 +278,7 @@ class Node extends EditorialContentEntityBase implements NodeInterface {
       ->setRequired(TRUE)
       ->setTranslatable(TRUE)
       ->setRevisionable(TRUE)
+      ->setStorageRequired(FALSE)
       ->setSetting('max_length', 255)
       ->setDisplayOptions('view', [
         'label' => 'hidden',
