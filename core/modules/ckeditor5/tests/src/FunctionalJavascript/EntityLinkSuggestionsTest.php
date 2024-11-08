467f272034e1da978a4003fad851dcfe7532a55a
Dang Tran <dang.tran@portlandwebworks.com>
2024-11-08 15:46:07 -0600

    fix JS functional test EntityLinkSuggestionsTest.php

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/EntityLinkSuggestionsTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/EntityLinkSuggestionsTest.php
@@ -150,10 +150,9 @@ class EntityLinkSuggestionsTest extends CKEditor5TestBase {
 
     // Find all the autocomplete results.
     $results = $page->findAll('css', '.entity-link-suggestions-result-line.ui-menu-item');
-    $this->assertCount(3, $results);
+    $this->assertCount(2, $results);
     $this->assertSame('Foo', $results[0]->find('css', '.entity-link-suggestions-result-line--title')->getText());
     $this->assertSame('Information about screaming hairy armadillo', $results[1]->find('css', '.entity-link-suggestions-result-line--title')->getText());
-    $this->assertSame('Sofie', $results[2]->find('css', '.entity-link-suggestions-result-line--title')->getText());
 
     // Make the search term longer to narrow down the results.
     $autocomplete_field->setValue('fo');
