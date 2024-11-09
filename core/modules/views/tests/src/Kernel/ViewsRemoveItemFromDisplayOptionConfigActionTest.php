2c5cd623b4a9bab11bc97a1eb24e594985f6f30c
Artem  Dmitriiev <a.dmitriiev@1xinternet.de>
2024-11-09 19:51:00 +0100

    Add tests

---
+++ core/modules/views/tests/src/Kernel/ViewsRemoveItemFromDisplayOptionConfigActionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\Tests\views\Kernel;
+
+use Drupal\Core\Recipe\RecipeRunner;
+use Drupal\FunctionalTests\Core\Recipe\RecipeTestTrait;
+use Drupal\views\Views;
+
+/**
+ * @covers \Drupal\views\Plugin\ConfigAction\ViewsRemoveItemFromDisplayOption
+ *
+ * @group Recipe
+ * @group views
+ */
+class ViewsRemoveItemFromDisplayOptionConfigActionTest extends ViewsKernelTestBase {
+
+  use RecipeTestTrait;
+
+  public static $testViews = ['entity_test_fields'];
+
+  /**
+   * Tests removing field from a default display.
+   */
+  public function testRemoveFieldFromDefaultDisplay() : void {
+    $view = Views::getView('entity_test_fields');
+    $fields = $view->displayHandlers->get('default')->getOption('fields');
+    // Check that field type is not part of default display.
+    $this->assertArrayHasKey('name', $fields);
+    // Apply config action that removes field name from default display.
+    $this->applyAction('views.view.entity_test_fields');
+    $view = Views::getView('entity_test_fields');
+    $fields = $view->displayHandlers->get('default')->getOption('fields');
+    // Check that field type now exists.
+    $this->assertArrayNotHasKey('name', $fields);
+  }
+
+  /**
+   * Applies a recipe with the addItemToDisplayOption action.
+   *
+   * @param string $config_name
+   *   The name of the config object which should run the addItemToDisplayOption
+   *   action.
+   */
+  private function applyAction(string $config_name): void {
+    $contents = <<<YAML
+name: Remove name field from view
+config:
+  actions:
+    $config_name:
+      removeItemFromDisplayOption:
+        option: fields
+        item: name
+YAML;
+    $recipe = $this->createRecipe($contents);
+    RecipeRunner::processRecipe($recipe);
+  }
+
+
+}
---
+++ core/modules/views/tests/src/Kernel/ViewsSetDisplayOptionConfigActionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\views\Kernel;
+
+use Drupal\Core\Recipe\RecipeRunner;
+use Drupal\FunctionalTests\Core\Recipe\RecipeTestTrait;
+use Drupal\views\Views;
+
+/**
+ * @covers \Drupal\views\Plugin\ConfigAction\ViewsSetDisplayOption
+ *
+ * @group Recipe
+ * @group views
+ */
+class ViewsSetDisplayOptionConfigActionTest extends ViewsKernelTestBase {
+
+  use RecipeTestTrait;
+
+  public static $testViews = ['entity_test_fields'];
+
+  /**
+   * Tests removing field from a default display.
+   */
+  public function testSetDefaultDisplay() : void {
+    $view = Views::getView('entity_test_fields');
+    $pager = $view->displayHandlers->get('default')->getOption('pager');
+    // Check that pager type is full.
+    $this->assertSame('full', $pager['type']);
+    // Apply config action that set pager to mini for default display.
+    $this->applyAction('views.view.entity_test_fields');
+    $view = Views::getView('entity_test_fields');
+    $pager = $view->displayHandlers->get('default')->getOption('pager');
+    // Check that pager type is mini.
+    $this->assertSame('mini', $pager['type']);
+  }
+
+  /**
+   * Applies a recipe with the addItemToDisplayOption action.
+   *
+   * @param string $config_name
+   *   The name of the config object which should run the addItemToDisplayOption
+   *   action.
+   */
+  private function applyAction(string $config_name): void {
+    $contents = <<<YAML
+name: Set mini pager
+config:
+  actions:
+    $config_name:
+      setDisplayOption:
+        option: pager
+        settings:
+          type: mini
+          options:
+            items_per_page: 5
+YAML;
+    $recipe = $this->createRecipe($contents);
+    RecipeRunner::processRecipe($recipe);
+  }
+
+
+}
