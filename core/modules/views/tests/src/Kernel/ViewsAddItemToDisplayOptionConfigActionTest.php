c6394a68884646f0a00a7e558ff152675eeae7b9
Artem  Dmitriiev <a.dmitriiev@1xinternet.de>
2024-11-10 08:39:40 +0100

    Fix coding standards in tests

--- core/modules/views/tests/src/Kernel/ViewsAddItemToDisplayOptionConfigActionTest.php
+++ core/modules/views/tests/src/Kernel/ViewsAddItemToDisplayOptionConfigActionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\Tests\views\Kernel;
 
 use Drupal\Core\Config\Action\ConfigActionException;
@@ -77,6 +79,5 @@ YAML;
     $recipe = $this->createRecipe($contents);
     RecipeRunner::processRecipe($recipe);
   }
-
 
 }
--- core/modules/views/tests/src/Kernel/ViewsRemoveItemFromDisplayOptionConfigActionTest.php
+++ core/modules/views/tests/src/Kernel/ViewsRemoveItemFromDisplayOptionConfigActionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\Tests\views\Kernel;
 
 use Drupal\Core\Recipe\RecipeRunner;
@@ -54,6 +56,5 @@ YAML;
     $recipe = $this->createRecipe($contents);
     RecipeRunner::processRecipe($recipe);
   }
-
 
 }
