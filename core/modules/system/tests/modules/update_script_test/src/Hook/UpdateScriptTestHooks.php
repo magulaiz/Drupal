976faf744bcafd230a6e15395522ae29c498399f
DDEV User <nobody@example.com>
2024-11-10 23:46:15 +0000

    post conversion

---
+++ core/modules/system/tests/modules/update_script_test/src/Hook/UpdateScriptTestHooks.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\update_script_test\Hook;
+
+use Drupal\Core\Extension\Extension;
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for update_script_test.
+ */
+class UpdateScriptTestHooks {
+
+  /**
+   * Implements hook_system_info_alter().
+   */
+  #[Hook('system_info_alter')]
+  public function systemInfoAlter(array &$info, Extension $file, $type) {
+    $new_info = \Drupal::state()->get('update_script_test.system_info_alter');
+    if ($new_info) {
+      if ($file->getName() == 'update_script_test') {
+        $info = $new_info + $info;
+      }
+    }
+  }
+
+}
