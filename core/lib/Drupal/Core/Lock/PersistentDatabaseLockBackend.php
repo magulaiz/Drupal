fefc885727f61cd8d12fa368fe80f29e10ff1880
Brad Jones <brad@bradjonesllc.com>
2024-11-11 15:10:31 -0700

    Adjust typehint in the persistent db lock backend.

--- core/lib/Drupal/Core/Lock/PersistentDatabaseLockBackend.php
+++ core/lib/Drupal/Core/Lock/PersistentDatabaseLockBackend.php
@@ -2,7 +2,8 @@
 
 namespace Drupal\Core\Lock;
 
-use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\DatabaseConnectionInterface;
+use Drupal\Core\Database\NonTransactionalConnection;
 
 /**
  * Defines the persistent database lock backend.
@@ -16,13 +17,15 @@ class PersistentDatabaseLockBackend extends DatabaseLockBackend {
   /**
    * Constructs a new PersistentDatabaseLockBackend.
    *
-   * @param \Drupal\Core\Database\Connection $database
+   * @param \Drupal\Core\Database\DatabaseConnectionInterface $database
    *   The database connection.
    */
-  public function __construct(Connection $database) {
+  public function __construct(protected DatabaseConnectionInterface $database) {
     // Do not call the parent constructor to avoid registering a shutdown
     // function that releases all the locks at the end of a request.
-    $this->database = $database;
+    if (!($this->database instanceof NonTransactionalConnection) && $this->database->databaseType() !== 'sqlite') {
+      @trigger_error('Calling ' . __METHOD__ . '() with a transactional database connection is deprecated in drupal:10.4.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3310017', E_USER_DEPRECATED);
+    }
     // Set the lockId to a fixed string to make the lock ID the same across
     // multiple requests. The lock ID is used as a page token to relate all the
     // locks set during a request to each other.
