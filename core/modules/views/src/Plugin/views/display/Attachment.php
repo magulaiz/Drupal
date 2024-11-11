a5e0bb15c068df5c08d4294bc30a0fea71d0694b
Saiyad Zaryab Haider <66608-zaryab_drupal@users.noreply.drupalcode.org>
2024-11-11 10:15:22 +0530

    Issue #2833734 by Tаo, Daniel Kulbe, Mirroar, pvsureshmca, rensingh99: Allow views attachment display to use its own pager options

--- core/modules/views/src/Plugin/views/display/Attachment.php
+++ core/modules/views/src/Plugin/views/display/Attachment.php
@@ -176,7 +176,7 @@ class Attachment extends DisplayPluginBase {
         $form['render_pager'] = [
           '#type' => 'checkbox',
           '#title' => $this->t('Render'),
-          '#description' => $this->t('Should this display render the pager values? This is only meaningful if inheriting a pager.'),
+          '#description' => $this->t('Should this display render the pager values?'),
           '#default_value' => $this->getOption('render_pager'),
         ];
         break;
@@ -254,6 +254,10 @@ class Attachment extends DisplayPluginBase {
       $view->display_handler->usesPager = $this->view->displayHandlers->get($display_id)->usesPager();
       $view->display_handler->setOption('pager', $this->view->displayHandlers->get($display_id)->getOption('pager'));
     }
+    elseif ($this->getOption('render_pager')) {
+      $view->display_handler->usesPager = TRUE;
+      $view->display_handler->setOption('pager', $view->displayHandlers->get($view->current_display)->getOption('pager'));
+    }
 
     $attachment = $view->buildRenderable($this->display['id'], $args);
 
@@ -272,6 +276,12 @@ class Attachment extends DisplayPluginBase {
         break;
     }
 
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function usesPager() {
+    return $this->getOption('render_pager');
   }
 
   /**
