5afda343026009b2e77823157c42d9097a2403f6
Brad Jones <brad@bradjonesllc.com>
2024-11-11 15:33:51 -0700

    Additional typehint updates for non-transactional services

--- core/lib/Drupal/Core/Queue/QueueDatabaseFactory.php
+++ core/lib/Drupal/Core/Queue/QueueDatabaseFactory.php
@@ -2,7 +2,8 @@
 
 namespace Drupal\Core\Queue;
 
-use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\DatabaseConnectionInterface;
+use Drupal\Core\Database\NonTransactionalConnection;
 
 /**
  * Defines the queue factory for the database backend.
@@ -10,20 +11,15 @@ use Drupal\Core\Database\Connection;
 class QueueDatabaseFactory implements QueueFactoryInterface {
 
   /**
-   * The database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $connection;
-
-  /**
    * Constructs this factory object.
    *
-   * @param \Drupal\Core\Database\Connection $connection
+   * @param \Drupal\Core\Database\DatabaseConnectionInterface $connection
    *   The Connection object containing the queue table.
    */
-  public function __construct(Connection $connection) {
-    $this->connection = $connection;
+  public function __construct(protected DatabaseConnectionInterface $connection) {
+    if (!($connection instanceof NonTransactionalConnection) && $connection->databaseType() !== 'sqlite') {
+      @trigger_error('Calling ' . __METHOD__ . '() with a transactional database connection is deprecated in drupal:10.4.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3310017', E_USER_DEPRECATED);
+    }
   }
 
   /**
