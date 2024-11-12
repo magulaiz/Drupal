4333bf59ccd4300dd7eaf21cedb46f5e2320e3f0
Dang Tran <51879-sea2709@users.noreply.drupalcode.org>
2024-11-12 18:56:38 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Dave Long <24510-longwave@users.noreply.drupalcode.org>

--- core/modules/filter/src/Plugin/Filter/EntityLinks.php
+++ core/modules/filter/src/Plugin/Filter/EntityLinks.php
@@ -57,7 +57,7 @@ class EntityLinks extends FilterBase implements ContainerFactoryPluginInterface 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
     return new static(
       $configuration,
       $plugin_id,
