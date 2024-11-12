b4d78c7ab85f9c5e34f47fbecb92342b0e69f70d
catch <6915-catch@users.noreply.drupalcode.org>
2024-11-12 12:33:07 +0000

    Don't use a sub request for 400 exceptions.

--- core/lib/Drupal/Core/EventSubscriber/DefaultExceptionHtmlSubscriber.php
+++ core/lib/Drupal/Core/EventSubscriber/DefaultExceptionHtmlSubscriber.php
@@ -93,6 +93,18 @@ class DefaultExceptionHtmlSubscriber extends HttpExceptionSubscriberBase {
   }
 
   /**
+   * Handles a 400 error for HTML.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\ExceptionEvent $event
+   *   The event to process.
+   */
+  public function on400(ExceptionEvent $event) {
+    if ($exception = $event->getThrowable()) {
+      throw $exception;
+    }
+  }
+
+  /**
    * Handles a 401 error for HTML.
    *
    * @param \Symfony\Component\HttpKernel\Event\ExceptionEvent $event
