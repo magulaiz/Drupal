28362488fbb5647f56fd7078889d83e16f3c23bd
Kumud Vadher <kumud.vadher@srijan.net>
2024-11-13 11:53:03 +0530

    drupal-3293287: view access label rendered only if the user has both view label and view access to the entity.

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/EntityReferenceLabelFormatter.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/EntityReferenceLabelFormatter.php
@@ -62,9 +62,12 @@ class EntityReferenceLabelFormatter extends EntityReferenceFormatterBase {
 
     foreach ($this->getEntitiesToView($items, $langcode) as $delta => $entity) {
       $label = $entity->label();
+      // Check if the user has "view label" access.
+      if ($entity->access('view label')) {
+        $uri = NULL;
       // If the link is to be displayed and the entity has a uri, display a
       // link.
-      if ($output_as_link && !$entity->isNew()) {
+      if ($output_as_link && !$entity->isNew() && $entity->access('view')) {
         try {
           $uri = $entity->toUrl();
         }
@@ -100,6 +103,7 @@ class EntityReferenceLabelFormatter extends EntityReferenceFormatterBase {
       $elements[$delta]['#entity'] = $entity;
       $elements[$delta]['#cache']['tags'] = $entity->getCacheTags();
     }
+  }
 
     return $elements;
   }
