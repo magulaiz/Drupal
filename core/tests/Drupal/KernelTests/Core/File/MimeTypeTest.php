c80354e03c7baa270b1fe5e1a8d6d67ca62cf02f
Kim Pepper <kim@pepper.id.au>
2024-11-15 10:25:50 +1100

    Add another test case

--- core/tests/Drupal/KernelTests/Core/File/MimeTypeTest.php
+++ core/tests/Drupal/KernelTests/Core/File/MimeTypeTest.php
@@ -45,6 +45,7 @@ class MimeTypeTest extends FileTestBase {
       'foobar.tar' => 'application/x-tar',
       'foobar.tar.z' => 'application/x-tarz',
       'foobar.0.zip' => 'application/zip',
+      'foobar..zip' => 'application/zip',
     ];
 
     $guesser = $this->container->get('file.mime_type.guesser');
