abd9112b1f9414d04b51facfe40e8f099592583c
Kim Pepper <kim@pepper.id.au>
2024-11-15 09:54:44 +1100

    Simplify and add a test

--- core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
+++ core/lib/Drupal/Core/File/MimeType/ExtensionMimeTypeGuesser.php
@@ -915,17 +915,9 @@ class ExtensionMimeTypeGuesser implements MimeTypeGuesserInterface {
     // then iterate over the file parts, trying to find a match.
     // For 'my.awesome.image.jpeg', we try: 'awesome.image.jpeg', then
     // 'image.jpeg', then 'jpeg'.
-    while (TRUE) {
-      $maybeExtension = array_shift($file_parts);
-
-      if ($maybeExtension === NULL) {
-        // Strictly check for NULL because "0" (zero)
-        // can be a part of the file name.
-        break;
-      }
-
+    // We explicitly check for NULL because '0' is a valid value.
+    while (array_shift($file_parts) !== NULL) {
       $extension = strtolower(implode('.', $file_parts));
-
       if (isset($this->mapping['extensions'][$extension])) {
         return $this->mapping['mimetypes'][$this->mapping['extensions'][$extension]];
       }
