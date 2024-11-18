e7191a5c8cb6a9cfad34d7295372c77519bba145
nicxvan <29861-nicxvan@users.noreply.drupalcode.org>
2024-11-18 16:09:36 +0000

    Update comments

--- core/lib/Drupal/Core/Hook/Attribute/Hook.php
+++ core/lib/Drupal/Core/Hook/Attribute/Hook.php
@@ -5,87 +5,10 @@ declare(strict_types=1);
 namespace Drupal\Core\Hook\Attribute;
 
 /**
- * Attribute for defining a class method as a hook implementation.
+ * This class will not have an effect until Drupal 11.1.0.
  *
- * Hook implementations in classes need to be marked with this attribute,
- * using one of the following techniques:
- * - On a method, use this attribute with the hook name:
- *   @code
- *   #[Hook('user_cancel')]
- *   public method userCancel(...)
- *   @endcode
- * - On a class, specifying the method name:
- *   @code
- *   #[Hook('user_cancel', method: 'userCancel')]
- *   class Hooks {
- *     method userCancel(...) {}
- *   }
- *   @endcode
- * - On a class with an __invoke method, which is taken to be the hook
- *   implementation:
- *   @code
- *   #[Hook('user_cancel')]
- *   class Hooks {
- *     method __invoke(...) {}
- *   }
- *   @endcode
- *
- * Ordering hook implementations can be done by specifying the 'priority' on the
- * attribute, or by manipulating the kernel listeners in service alter
- * providers. See \Drupal\Core\Hook\HookOrder for details.
- *
- * Classes that use this annotation on the class or on their methods are
- * automatically registered as autowired services with the class name as the
- * service ID. If autowire does not suffice, they can be registered manually as
- * well.
- *
- * Implementing a hook on behalf of another module can be done by by specifying
- * the 'module' parameter in the attribute.
- *
- * @section sec_multiple_implementations Multiple implementations
- *
- * Multiple implementations are allowed on multiple axes:
- * - One method can implement multiple hooks by adding a Hook attribute for each
- *   method.
- * - One module can implement a particular hook multiple times in multiple
- *   classes, although see below for some exceptions. This allows, for example,
- *   adding hook_form_alter() implementations firing on other conditions than
- *   form ID without modifying any existing implementations.
- *
- * The following hooks may not have multiple implementations by a single module:
- * - hook_library_build_info()
- * - hook_mail()
- * - hook_help()
- * - hook_node_update_index()
- *
- * @section sec_procedural Procedural hooks
- *
- * The following hooks can only have procedural hook implementations:
- *
- * Install hooks:
- * - hook_install()
- * - hook_module_preinstall()
- * - hook_module_preuninstall()
- * - hook_modules_installed()
- * - hook_modules_uninstalled()
- * - hook_post_update_NAME()
- * - hook_schema()
- * - hook_uninstall()
- * - hook_update_last_removed()
- * - hook_update_N()
- *
- * Theme hooks:
- * - hook_preprocess_HOOK()
- * - hook_process_HOOK()
- *
- * @section sec_backwards_compatibility Backwards-compatibility
- *
- * To allow hook implementations to work on older versions of Drupal as well,
- * add both an attribute-based hook implementation and a procedural hook
- * implementation, with the \Drupal\Core\Hook\Attribute\LegacyHook attribute on
- * the procedural hook implementations.
- *
- * See \Drupal\Core\Hook\Attribute\LegacyHook for additional information.
+ * This class is included in earlier Drupal versions to prevent phpstan errors
+ * for modules implementing object oriented hooks using the #Hook and #LegacyHook attributes.
  */
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class Hook {
