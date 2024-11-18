cf71e407688df731f7b373259a50ae7610198727
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-11-18 08:16:34 +0300

    apply patch 3039354-26

--- core/modules/user/src/Plugin/views/argument_default/CurrentUser.php
+++ core/modules/user/src/Plugin/views/argument_default/CurrentUser.php
@@ -4,9 +4,11 @@ namespace Drupal\user\Plugin\views\argument_default;
 
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\views\Attribute\ViewsArgumentDefault;
 use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Default argument plugin to extract the current user.
@@ -20,10 +22,45 @@ use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
 class CurrentUser extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
 
   /**
+   * Gets the current active user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * CurrentUser constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxyInterface $current_user = NULL) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    if ($current_user === NULL) {
+      @trigger_error('Calling' . __CLASS__ . '::__construct() without the $current_user argument is deprecated in drupal:10.1.0 and is removed in drupal:11.0.0. See https://www.drupal.org/node/3347878', E_USER_DEPRECATED);
+      $current_user = \Drupal::currentUser();
+    }
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('current_user'));
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getArgument() {
-    return \Drupal::currentUser()->id();
+    return $this->currentUser->id();
   }
 
   /**
