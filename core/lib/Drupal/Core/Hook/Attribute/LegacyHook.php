37ec31688f466f8c4b53ea00137a67e00002d160
nicxvan <29861-nicxvan@users.noreply.drupalcode.org>
2024-11-18 16:11:14 +0000

    Fix line length

--- core/lib/Drupal/Core/Hook/Attribute/LegacyHook.php
+++ core/lib/Drupal/Core/Hook/Attribute/LegacyHook.php
@@ -8,7 +8,8 @@ namespace Drupal\Core\Hook\Attribute;
  * This class will not have an effect until Drupal 11.1.0.
  *
  * This class is included in earlier Drupal versions to prevent phpstan errors
- * for modules implementing object oriented hooks using the #Hook and #LegacyHook attributes.
+ * for modules implementing object oriented hooks using the #Hook and
+ * #LegacyHook attributes.
  */
 #[\Attribute(\Attribute::TARGET_FUNCTION)]
 class LegacyHook {
