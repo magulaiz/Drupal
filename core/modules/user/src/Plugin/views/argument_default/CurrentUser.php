f11dd48f8ab97eee23c2ce9c3222f477038b96b5
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-11-18 10:01:35 +0300

    update message

--- core/modules/user/src/Plugin/views/argument_default/CurrentUser.php
+++ core/modules/user/src/Plugin/views/argument_default/CurrentUser.php
@@ -43,7 +43,7 @@ class CurrentUser extends ArgumentDefaultPluginBase implements CacheableDependen
   public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxyInterface $current_user = NULL) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     if ($current_user === NULL) {
-      @trigger_error('Calling' . __CLASS__ . '::__construct() without the $current_user argument is deprecated in drupal:10.1.0 and is removed in drupal:11.0.0. See https://www.drupal.org/node/3347878', E_USER_DEPRECATED);
+      @trigger_error('Calling ' . __CLASS__ . '::__construct() without the $current_user argument is deprecated in drupal:11.1.0 and is required in drupal:12.0.0. See https://www.drupal.org/node/3347878', E_USER_DEPRECATED);
       $current_user = \Drupal::currentUser();
     }
     $this->currentUser = $current_user;
