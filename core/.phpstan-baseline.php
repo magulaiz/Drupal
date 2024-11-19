27337ac50e0865ff227cd5bd00d8381688779892
Bhanu <50266-Bhanu951@users.noreply.drupalcode.org>
2024-11-19 14:28:39 +0530

    baseline updated.

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -39424,7 +39424,6 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/modules/system/tests/src/Functional/Menu/BreadcrumbTest.php',
 ];
 $ignoreErrors[] = [
-	// identifier: variable.undefined
 	'message' => '#^Variable \\$parent might not be defined\\.$#',
 	'identifier' => 'variable.undefined',
 	'count' => 3,
