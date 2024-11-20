3b9e802bff7dae98c5e902384a972c4caeae5a40
catch <6915-catch@users.noreply.drupalcode.org>
2024-11-20 22:52:58 +0000

    syntax

--- core/modules/ckeditor5/tests/src/Nightwatch/Tests/ckEditor5EditorHeightTest.js
+++ core/modules/ckeditor5/tests/src/Nightwatch/Tests/ckEditor5EditorHeightTest.js
@@ -54,7 +54,7 @@ module.exports = {
         )
         // Add the body field to the new content type.
         .drupalRelativeURL('/admin/structure/types/manage/test/fields/reuse')
-        .waitForElementVisible('input[name=body]');
+        .waitForElementVisible('input[name="body"]')
         .click('input[name="body"]')
         .waitForElementVisible('[data-drupal-selector="edit-label"]')
         .submitForm('input[type="submit"]')
