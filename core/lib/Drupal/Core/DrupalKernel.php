1b0bd0fcf6426b81c19c036ced26c24482ccf314
catch <6915-catch@users.noreply.drupalcode.org>
2024-11-20 16:06:14 +0000

    Add a DatabaseApcuFileCacheBackend implementation and set it as the default.

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -493,7 +493,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
       // @todo Use extension_loaded('apcu') for non-testbot
       //   https://www.drupal.org/node/2447753.
       if (function_exists('apcu_fetch')) {
-        $configuration['default']['cache_backend_class'] = '\Drupal\Component\FileCache\ApcuFileCacheBackend';
+        $configuration['default']['cache_backend_class'] = '\Drupal\Core\FileCache\DatabaseApcuFileCacheBackend';
       }
     }
     FileCacheFactory::setConfiguration($configuration);
