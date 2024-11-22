10a9858e48ed97fe6beaa2bda4adf023c6ac7dea
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-11-22 13:43:40 +0300

    refactor

---
+++ core/modules/link/tests/src/Kernel/LinkItemUrlDisplayTest.php
@@ -0,0 +1,203 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\link\Kernel;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Tests\field\Kernel\FieldKernelTestBase;
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\link\LinkItemInterface;
+
+/**
+ * Tests the default 'link' formatter with complex query parameters.
+ *
+ * @group link
+ */
+class LinkItemUrlDisplayTest extends FieldKernelTestBase {
+
+  /**
+   * The test entity.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface
+   */
+  protected EntityInterface $entity;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['link'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    FieldStorageConfig::create([
+      'entity_type' => 'entity_test',
+      'field_name' => 'field_test',
+      'type' => 'link',
+    ])->save();
+
+    FieldConfig::create([
+      'entity_type' => 'entity_test',
+      'field_name' => 'field_test',
+      'bundle' => 'entity_test',
+      'settings' => ['link_type' => LinkItemInterface::LINK_GENERIC],
+    ])->save();
+
+    // Create an entity with link field values provided.
+    $this->entity = EntityTest::create();
+    $links = $this->getLinkFieldsValues();
+    $this->entity->field_test->setValue($links);
+    $this->entity->save();
+  }
+
+  /**
+   * Test rendered entity field with complex internal url.
+   *
+   * @param array $display_settings
+   *   Display settings for link field formatter.
+   * @param array $expected_results
+   *   Render result using these display settings.
+   *
+   * @dataProvider getTestCases
+   */
+  public function testInternalLinksRender(array $display_settings, array $expected_results): void {
+    /** @var \Drupal\Core\Render\RendererInterface $renderer */
+    $renderer = $this->container->get('renderer');
+
+    // Render link field using display settings.
+    $render_array = $this->entity->field_test->view(['settings' => $display_settings]);
+    $output = (string) $renderer->renderRoot($render_array);
+
+    // Check results.
+    foreach ($expected_results as $expected_result) {
+      $this->assertStringContainsString($expected_result, $output);
+
+      // With url_plain should be no links.
+      if (!empty($display_settings['url_only']) && !empty($display_settings['url_plain'])) {
+        $this->assertStringNotContainsString('<a href="', $output);
+      }
+    }
+
+  }
+
+  /**
+   * Get array of url with complex query parameters for render check.
+   *
+   * @return array
+   *   The URLs to test.
+   */
+  public static function getTestingUrls(): array {
+    $test_urls = [
+      [
+        'input' => 'internal:?a[]=1&a[]=2',
+        // Result link: '?a[0]=1&a[1]=2'.
+        'expected_href'  => '?a%5B0%5D=1&amp;a%5B1%5D=2',
+      ],
+      [
+        'input' => 'internal:?b[0]=1&b[1]=2',
+        // Result link: '?b[0]=1&b[1]=2'.
+        'expected_href'  => '?b%5B0%5D=1&amp;b%5B1%5D=2',
+      ],
+      // UrlHelper::buildQuery will change order of params.
+      [
+        'input' => 'internal:?c[]=1&d=3&c[]=2',
+        // Result link: '?c[0]=1&c[1]=2&d=3'.
+        'expected_href'  => '?c%5B0%5D=1&amp;c%5B1%5D=2&amp;d=3',
+      ],
+      [
+        'input' => 'internal:?e[f][g]=h',
+        // Result link: '?e[f][g]=h'.
+        'expected_href'  => '?e%5Bf%5D%5Bg%5D=h',
+      ],
+      [
+        'input' => 'internal:?i[j[k]]=l',
+        // Result link: '?i[j[k]]=l'.
+        'expected_href'  => '?i%5Bj%5Bk%5D=l',
+      ],
+
+      // Query string replace value.
+      [
+        'input' => 'internal:?x=1&x=2',
+        'expected_href'  => '?x=2',
+      ],
+      [
+        'input' => 'internal:?z[0]=1&z[0]=2',
+        // Result link: '?z[0]=2'.
+        'expected_href'  => '?z%5B0%5D=2',
+      ],
+    ];
+    return $test_urls;
+  }
+
+  /**
+   * Link field display settings and expected results.
+   *
+   * @return array
+   *   Test cases.
+   */
+  public static function getTestCases(): array {
+    return [
+      'default settings' => [
+        'display settings' => [],
+        'results' => array_map(function ($values) {
+          return '<a href="' . $values['expected_href'] . '">' . $values['expected_href'] . '</a>';
+        }, self::getTestingUrls()),
+      ],
+      'trim title to 6' => [
+        'display settings' => ['trim_length' => 6],
+        'results' => array_map(function ($values) {
+          $title = Unicode::truncate($values['expected_href'], 6, FALSE, TRUE);
+          return '<a href="' . $values['expected_href'] . '">' . $title . '</a>';
+        }, self::getTestingUrls()),
+      ],
+      'attribute rel' => [
+        'display settings' => ['rel' => 'nofollow'],
+        'results' => array_map(function ($values) {
+          return '<a href="' . $values['expected_href'] . '" rel="nofollow">' . $values['expected_href'] . '</a>';
+        }, self::getTestingUrls()),
+      ],
+      'attribute target' => [
+        'display settings' => ['target' => '_blank'],
+        'results' => array_map(function ($values) {
+          return '<a href="' . $values['expected_href'] . '" target="_blank">' . $values['expected_href'] . '</a>';
+        }, self::getTestingUrls()),
+      ],
+      'url_only' => [
+        'display settings' => ['url_only' => TRUE],
+        'results' => array_map(function ($values) {
+          return '<a href="' . $values['expected_href'] . '">' . $values['expected_href'] . '</a>';
+        }, self::getTestingUrls()),
+      ],
+      'url_only and url_plain' => [
+        'display settings' => ['url_only' => TRUE, 'url_plain' => TRUE],
+        'results' => array_map(function ($values) {
+          return $values['expected_href'];
+        }, self::getTestingUrls()),
+      ],
+    ];
+  }
+
+  /**
+   * Prepare values for link field.
+   *
+   * @return array
+   *   Values to use at link field setter.
+   */
+  public static function getLinkFieldsValues(): array {
+    $links = [];
+    foreach (self::getTestingUrls() as $key => $test_url) {
+      $links[$key] = [
+        'uri' => $test_url['input'],
+      ];
+    }
+    return $links;
+  }
+
+}
