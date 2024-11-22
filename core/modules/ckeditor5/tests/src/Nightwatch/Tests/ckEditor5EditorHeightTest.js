c6972470bff1c093c0c8d745b350751d023b44d5
catch <6915-catch@users.noreply.drupalcode.org>
2024-11-22 18:01:15 +0000

    Switch to testing profile.

--- core/modules/ckeditor5/tests/src/Nightwatch/Tests/ckEditor5EditorHeightTest.js
+++ core/modules/ckeditor5/tests/src/Nightwatch/Tests/ckEditor5EditorHeightTest.js
@@ -4,8 +4,9 @@ module.exports = {
   '@tags': ['core', 'ckeditor5'],
   before(browser) {
     browser
-      .drupalInstall({ installProfile: 'minimal' })
+      .drupalInstall({ installProfile: 'testing' })
       .drupalInstallModule('ckeditor5', true)
+      .drupalInstallModule('node')
       .drupalInstallModule('field_ui');
 
     // Set fixed (desktop-ish) size to ensure a maximum viewport.
