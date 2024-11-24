c22326dcee4e3e6a1870a2ad205d0d18ff7e543e
mondrake <mondrake.org@gmail.com>
2024-11-24 21:36:26 +0100

    fix

--- core/lib/Drupal/Core/Database/FetchModeTrait.php
+++ core/lib/Drupal/Core/Database/FetchModeTrait.php
@@ -104,7 +104,7 @@ trait FetchModeTrait {
    */
   protected function assocToColumn(array $rowAssoc, array $columnNames, int $columnIndex): mixed {
     if (!isset($columnNames[$columnIndex])) {
-      throw new \OutOfBoundsException("Column with index {$columnIndex} does not exist");
+      throw new \ValueError("Column with index {$columnIndex} does not exist");
     }
     return $rowAssoc[$columnNames[$columnIndex]];
   }
