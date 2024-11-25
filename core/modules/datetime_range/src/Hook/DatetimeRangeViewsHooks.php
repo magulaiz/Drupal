833be2f40c1a5c5d4f4c3ec02540535e4bc69503
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-11-25 10:29:50 -0500

    No need to load .inc file

--- core/modules/datetime_range/src/Hook/DatetimeRangeViewsHooks.php
+++ core/modules/datetime_range/src/Hook/DatetimeRangeViewsHooks.php
@@ -15,9 +15,6 @@ class DatetimeRangeViewsHooks {
    */
   #[Hook('field_views_data')]
   public function fieldViewsData(FieldStorageConfigInterface $field_storage) {
-    // Include datetime.views.inc file in order for helper function
-    // datetime_type_field_views_data_helper() to be available.
-    \Drupal::moduleHandler()->loadInclude('datetime', 'inc', 'datetime.views');
     // Get datetime field data for value and end_value.
     $data = datetime_type_field_views_data_helper($field_storage, [], 'value');
     $data = datetime_type_field_views_data_helper($field_storage, $data, 'end_value');
