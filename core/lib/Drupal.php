815b9ecd2a4c5d8053a1ee2cf2c9b890daa3859d
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2024-11-26 10:03:01 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Alex Pott <1732-alexpott@users.noreply.drupalcode.org>

--- core/lib/Drupal.php
+++ core/lib/Drupal.php
@@ -81,7 +81,6 @@ class Drupal {
    * @see https://www.drupal.org/node/3481012
    */
   const VERSION = '11.0-dev';
-  @trigger_error('Using the constant \Drupal::VERSION is deprecated in drupal:11.0.9 and will be required in drupal:12.0.0. Use \Composer\InstalledVersions::getVersion instead. See https://www.drupal.org/node/3481012', E_USER_DEPRECATED);
 
   /**
    * Core API compatibility.
