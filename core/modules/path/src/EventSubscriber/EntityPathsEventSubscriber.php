62132c3aa956b38febbc45fea5537ad448bcad11
dpi <pro@danielph.in>
2024-11-26 21:58:30 +0800

    dpi: view modes for path variants

--- core/modules/path/src/EventSubscriber/EntityPathsEventSubscriber.php
+++ core/modules/path/src/EventSubscriber/EntityPathsEventSubscriber.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Drupal\path\EventSubscriber;
 
+use Drupal\Core\Config\Entity\ConfigEntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\path\Event\EntityPathsEvent;
+use Drupal\path\Event\PathVariantEvent;
 use Drupal\path\PathVariant\PathVariant;
+use Drupal\path\PathVariant\PathVariantRepositoryInterface;
 use Drupal\path\PathVariant\PlaceHolderInternalPath;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -13,6 +17,15 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  * Responds with internal paths provided by core.
  */
 final class EntityPathsEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Constructs an event subscriber for entity paths.
+   */
+  public function __construct(
+    private EntityTypeManagerInterface $entityTypeManager,
+    private PathVariantRepositoryInterface $pathVariantRepository,
+  ) {
+  }
 
   /**
    * Subscriber for canonical internal path.
@@ -25,12 +38,50 @@ final class EntityPathsEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Subscriber for computing internal paths for entity view modes.
+   */
+  public function entityViewModeInternalPaths(EntityPathsEvent $event): void {
+    $entity = $event->getEntity();
+
+    foreach ($this->pathVariantRepository->getPathVariantsForEntity($entity) as $variant) {
+      $variantStringed = $variant->getVariantStringed();
+      if (FALSE === \str_starts_with($variantStringed, 'view_mode:')) {
+        continue;
+      }
+
+      [$entityTypeId, $mode] = \explode('.', \substr($variantStringed, 10), 2);
+
+      if ($mode === 'full') {
+        // Full mode is handled by canonicalInternalPath().
+        continue;
+      }
+
+      /** @var \Drupal\Core\Entity\EntityViewModeInterface $entityViewMode */
+      $entityViewMode = $this->entityViewModeStorage()->load(sprintf('%s.%s', $entityTypeId, $mode)) ?? throw new \LogicException(sprintf('Mode is expected to exist since it came from %s', PathVariantEvent::class));
+
+      $event->addInternalPath(
+        $entity->isNew() ? PlaceHolderInternalPath::create() : '/' . $entity->toUrl()->getInternalPath() . '/' . $entityViewMode->getPath(),
+        $variant,
+      );
+    }
+  }
+
+  /**
+   * Get entity storage for `entity_view_mode` config entities.
+   */
+  private function entityViewModeStorage(): ConfigEntityStorageInterface {
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorageInterface */
+    return $this->entityTypeManager->getStorage('entity_view_mode');
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents(): array {
     return [
       EntityPathsEvent::class => [
         ['canonicalInternalPath'],
+        ['entityViewModeInternalPaths'],
       ],
     ];
   }
--- core/modules/path/src/EventSubscriber/PathVariantEventSubscriber.php
+++ core/modules/path/src/EventSubscriber/PathVariantEventSubscriber.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\path\EventSubscriber;
 
+use Drupal\Core\Config\Entity\ConfigEntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\path\Event\PathVariantEvent;
 use Drupal\path\PathVariant\PathVariant;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -14,10 +17,56 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 final class PathVariantEventSubscriber implements EventSubscriberInterface {
 
   /**
+   * Constructs an event subscriber for path variants.
+   */
+  public function __construct(
+    public EntityTypeManagerInterface $entityTypeManager,
+  ) {
+  }
+
+  /**
    * The default variant.
    */
   public function defaultVariant(PathVariantEvent $event): void {
+    // Full mode is always present.
     $event->addVariant(PathVariant::createDefault());
+  }
+
+  /**
+   * Subscriber adding path variants for entity view modes.
+   */
+  public function entityViewModePathVariants(PathVariantEvent $event): void {
+    $entityTypeId = $event->getEntityTypeId();
+
+    // Determine view modes with a path.
+    $viewModeIds = $this->entityViewModeStorage()
+      ->getQuery()
+      ->exists('path')
+      ->condition('id', sprintf('%s.', $entityTypeId), 'STARTS_WITH')
+      ->condition('id', sprintf('%s.%s', $entityTypeId, 'full'), '<>')
+      ->accessCheck(FALSE)
+      ->execute();
+
+    // Determine which bundles have a disabled page display:
+    $hiddenDisplayIds = $this->entityViewDisplayStorage()->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('targetEntityType', $entityTypeId)
+      ->condition('bundle', $event->getBundle())
+      ->condition('pageDisplay', FALSE)
+      ->execute();
+
+    // Remove disabled view modes:
+    $viewModeIds = \array_diff($viewModeIds, \array_map(
+      static fn (string $view_display_id) => \sprintf('%s.%s', $entityTypeId, \explode('.', $view_display_id)[2]),
+      $hiddenDisplayIds,
+    ));
+
+    foreach ($this->entityViewModeStorage()->loadMultiple($viewModeIds) as $viewMode) {
+      $event->addVariant(PathVariant::create(
+        sprintf('view_mode:%s.%s', $entityTypeId, $viewMode->getMode()),
+        new TranslatableMarkup('@label view mode', ['@label' => $viewMode->label()]),
+      ));
+    }
   }
 
   /**
@@ -25,8 +74,27 @@ final class PathVariantEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents(): array {
     return [
-      PathVariantEvent::class => 'defaultVariant',
+      PathVariantEvent::class => [
+        ['defaultVariant'],
+        ['entityViewModePathVariants'],
+      ],
     ];
+  }
+
+  /**
+   * Get entity storage for `entity_view_display` config entities.
+   */
+  private function entityViewDisplayStorage(): ConfigEntityStorageInterface {
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorageInterface */
+    return $this->entityTypeManager->getStorage('entity_view_display');
+  }
+
+  /**
+   * Get entity storage for `entity_view_mode` config entities.
+   */
+  private function entityViewModeStorage(): ConfigEntityStorageInterface {
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorageInterface */
+    return $this->entityTypeManager->getStorage('entity_view_mode');
   }
 
 }
