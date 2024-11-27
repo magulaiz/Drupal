c641eef4e567dab425e61f4ae9f2bd18ef683f77
arunsahijpal <arunsahijpal111@gmail.com>
2024-11-27 18:04:29 +0530

    Issue #3488937: Added presave method for trimming URI.

--- core/modules/link/src/Plugin/Field/FieldType/LinkItem.php
+++ core/modules/link/src/Plugin/Field/FieldType/LinkItem.php
@@ -204,4 +204,19 @@ class LinkItem extends FieldItemBase implements LinkItemInterface {
     parent::setValue($values, $notify);
   }
 
+  /**
+  * {@inheritdoc}
+  */
+  public function preSave() {
+    if (!empty($this->uri)) {
+      $this->uri = trim($this->uri);
+    }
+    
+    if (!empty($this->title)) {
+      $this->title = trim($this->title);
+    }
+
+    parent::preSave();
+  }
+
 }
