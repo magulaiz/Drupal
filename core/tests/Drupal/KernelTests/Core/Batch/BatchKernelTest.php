d55bd94b47261e7210c6d8f60e2748bb591bd482
mondrake <mondrake.org@gmail.com>
2024-12-01 11:56:53 +0100

    fix

--- core/tests/Drupal/KernelTests/Core/Batch/BatchKernelTest.php
+++ core/tests/Drupal/KernelTests/Core/Batch/BatchKernelTest.php
@@ -39,6 +39,9 @@ class BatchKernelTest extends KernelTestBase {
     $this->assertEquals(FALSE, _batch_needs_update());
   }
 
+  /**
+   * Tests BatchStorage::getId().
+   */
   public function testGetStorageId(): void {
     $storage = $this->container->get('batch.storage');
     $this->assertIsNumeric($storage->getId());
