d3a9309a50e86bc498dae725f5190b9f15c6daa1
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-02 09:12:20 -0500

    Test failures

--- core/modules/help/tests/modules/help_page_test/src/Hook/HelpPageTestHooks.php
+++ core/modules/help/tests/modules/help_page_test/src/Hook/HelpPageTestHooks.php
@@ -21,10 +21,10 @@ class HelpPageTestHooks {
       case 'help.page.help_page_test':
         // Make the help text conform to core standards. See
         // \Drupal\system\Tests\Functional\GenericModuleTestBase::assertHookHelp().
-        return t('Read the <a href=":url">online documentation for the Help Page Test module</a>.', [':url' => 'http://www.example.com']);
+        return '<p>' . t('Read the <a href=":url">online documentation for the Help Page Test module</a>.', [':url' => 'http://www.example.com']) . '</p>';
 
       case 'help_page_test.has_help':
-        return t('I have help!');
+        return '<p>' . t('I have help!') . '</p>';
 
       case 'help_page_test.test_array':
         return 'Help text from help_page_test_help module.';
