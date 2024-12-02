f65eaf36c0d223bfc4094272d70a897e221a22f2
Jeroen Tubex <19699-JeroenT@users.noreply.drupalcode.org>
2024-12-02 11:44:50 +0000

    Update file Condition.php

--- core/lib/Drupal/Core/Config/Entity/Query/Condition.php
+++ core/lib/Drupal/Core/Config/Entity/Query/Condition.php
@@ -170,7 +170,7 @@ class Condition extends ConditionBase {
 
     if (isset($value)) {
       // We always want a case-insensitive match.
-      if (!is_bool($condition['value']) && !empty($condition['value'])) {
+      if (is_string($value)) {
         $value = mb_strtolower($value);
       }
 
