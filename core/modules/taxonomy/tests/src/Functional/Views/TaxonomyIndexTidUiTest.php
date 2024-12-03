8ec27f5db5617862b97a1e7ef5af31420616e219
Chris Green <42483-trackleft2@users.noreply.drupalcode.org>
2024-12-03 07:22:44 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/taxonomy/tests/src/Functional/Views/TaxonomyIndexTidUiTest.php
+++ core/modules/taxonomy/tests/src/Functional/Views/TaxonomyIndexTidUiTest.php
@@ -476,7 +476,7 @@ class TaxonomyIndexTidUiTest extends UITestBase {
   /**
    * Tests using the TaxonomyIndexTid in a filter group.
    *
-   * @greoup legacy
+   * @group legacy
    */
   public function testFilterGrouping() {
     $this->expectDeprecation("The 'vid' key in 'views.filter.taxonomy_index_tid' config schema is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. Update your view to use the 'vids' key instead. See https://www.drupal.org/node/3162414");
