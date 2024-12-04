aba466a0d11374e4edbdee020fdc038d052b5ad9
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-04 11:35:11 -0500

    Search theme suggestions

---
+++ core/modules/search/src/Hook/SearchThemeSuggestionHook.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\search\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for search.
+ */
+class SearchThemeSuggestionHook {
+
+  /**
+   * Implements hook_theme_suggestions_HOOK().
+   */
+  #[Hook('theme_suggestions_search_result')]
+  public function themeSuggestionsSearchResult(array $variables) {
+    return ['search_result__' . $variables['plugin_id']];
+  }
+
+}
