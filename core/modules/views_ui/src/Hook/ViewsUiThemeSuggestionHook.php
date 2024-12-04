c9248de956bbe2a1e3804f9c1677a33802b86648
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-04 11:48:34 -0500

    views ui theme suggestions

---
+++ core/modules/views_ui/src/Hook/ViewsUiThemeSuggestionHook.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\views_ui\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for views_ui.
+ */
+class ViewsUiThemeSuggestionHook {
+
+  /**
+   * Implements hook_theme_suggestions_HOOK().
+   */
+  #[Hook('theme_suggestions_views_ui_view_preview_section')]
+  public function themeSuggestionsViewsUiViewPreviewSection(array $variables) {
+    return ['views_ui_view_preview_section__' . $variables['section']];
+  }
+
+}
