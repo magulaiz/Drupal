6c80c5ecc360aad9d260c6d5197d1a138b668ab6
binoli-lalani <binoliblalani@gmail.com>
2024-12-04 18:32:13 +0530

    Issue #3067677: TextTrimmedFormatter is tightly coupled to TextSummaryOrTrimmedFormatter

--- core/modules/text/src/Plugin/Field/FieldFormatter/TextSummaryOrTrimmedFormatter.php
+++ core/modules/text/src/Plugin/Field/FieldFormatter/TextSummaryOrTrimmedFormatter.php
@@ -3,7 +3,10 @@
 namespace Drupal\text\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\Attribute\FieldFormatter;
+use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\Plugin\DataType\StringData;
+use Drupal\text\Plugin\Field\FieldType\TextWithSummaryItem;
 
 /**
  * Plugin implementation of the 'text_summary_or_trimmed' formatter.
@@ -15,4 +18,24 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
     'text_with_summary',
   ],
 )]
-class TextSummaryOrTrimmedFormatter extends TextTrimmedFormatter {}
+class TextSummaryOrTrimmedFormatter extends TextTrimmedFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createSummary(array &$element, FieldItemInterface $item) {
+    if (!$item instanceof TextWithSummaryItem) {
+      parent::createSummary($element, $item);
+    }
+    else {
+      $summary = $item->get('summary');
+      if ($summary instanceof StringData && $summaryValue = $summary->getValue()) {
+        $element['#text'] = $summaryValue;
+      }
+      else {
+        parent::createSummary($element, $item);
+      }
+    }
+  }
+
+}
