7fe5914fd2ac08a88ac7f7b911c34b329418c57d
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-04 11:44:57 -0500

    theme test theme suggestions

---
+++ core/modules/system/tests/modules/theme_test/src/Hook/ThemeTestThemeSuggestionHook.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\theme_test\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for theme_test.
+ */
+class ThemeTestThemeSuggestionHook {
+
+  /**
+   * Implements hook_theme_suggestions_HOOK().
+   */
+  #[Hook('theme_suggestions_theme_test_preprocess_suggestions')]
+  public function themeSuggestionsThemeTestPreprocessSuggestions($variables) {
+    return ['theme_test_preprocess_suggestions__' . $variables['foo']];
+  }
+
+  /**
+   * Implements hook_theme_suggestions_HOOK().
+   */
+  #[Hook('theme_suggestions_theme_test_suggestion_provided')]
+  public function themeSuggestionsThemeTestSuggestionProvided(array $variables) {
+    return ['theme_test_suggestion_provided__foo'];
+  }
+
+  /**
+   * Implements hook_theme_suggestions_HOOK().
+   */
+  #[Hook('theme_suggestions_node')]
+  public function themeSuggestionsNode(array $variables) {
+    $xss = '<script type="text/javascript">alert(\'yo\');</script>';
+    $suggestions[] = 'node__' . $xss;
+  
+    return $suggestions;
+  }
+
+}
