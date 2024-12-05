14640f9cf81aa59a897ec41801ff07f482357eb0
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-05 13:20:14 +0000

    Revert "check that the module handler is available before loading modules."
    This reverts commit 67b08b4e5985c735f30c8d679aac76350f1b122a.

--- core/tests/Drupal/Tests/BrowserTestBase.php
+++ core/tests/Drupal/Tests/BrowserTestBase.php
@@ -375,9 +375,7 @@ abstract class BrowserTestBase extends TestCase {
     $this->initBrowserOutputFile();
 
     // Ensure modules are loaded for API calls within the test.
-    if ($this->container->has('module_handler')) {
-      $this->container->get('module_handler')->reload();
-    }
+    $this->container->get('module_handler')->reload();
   }
 
   /**
