a8d02a2ee5c1f5877cf1416b6fa9cd31493f7df9
Ramprassad <ramprassad.suresh@materialplus.io>
2024-12-05 03:08:32 +0000

    Added methods to implement DataDefinitionInterface

--- core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
+++ core/lib/Drupal/Core/Field/Entity/BaseFieldOverride.php
@@ -275,4 +275,43 @@ class BaseFieldOverride extends FieldConfigBase {
     return parent::__sleep();
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function setDataType($type) {
+    $this->definition['type'] = $type;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setReadOnly($read_only) {
+    $this->definition['read-only'] = $read_only;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setComputed($computed) {
+    $this->definition['computed'] = $computed;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setClass($class) {
+    $this->definition['class'] = $class;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setInternal($internal) {
+    $this->definition['internal'] = $internal;
+    return $this;
+  }
 }
