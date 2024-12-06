283d36e7a1ae1064205345a03d422029cb7b5f4f
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-06 22:23:51 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Derek Wright <12223-dww@users.noreply.drupalcode.org>

--- core/lib/Drupal/Core/Config/ConfigImporter.php
+++ core/lib/Drupal/Core/Config/ConfigImporter.php
@@ -880,7 +880,7 @@ class ConfigImporter {
    * @param string|array $names
    *   The name or names of the extension(s) to process.
    */
-  protected function processExtension(string $type, string $op, string|array $names) {
+  protected function processExtension(string $type, string $op, string|array $names): void {
     $names = (array) $names;
     // Set the config installer to use the sync directory instead of the
     // extensions own default config directories.
