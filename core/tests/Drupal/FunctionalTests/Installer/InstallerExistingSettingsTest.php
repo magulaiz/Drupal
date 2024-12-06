db57711f5922842f2a46c4c407961d8db842ad52
mondrake <mondrake.org@gmail.com>
2024-12-06 15:27:23 +0100

    config table is now created earlier

--- core/tests/Drupal/FunctionalTests/Installer/InstallerExistingSettingsTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerExistingSettingsTest.php
@@ -62,8 +62,10 @@ class InstallerExistingSettingsTest extends InstallerTestBase {
   protected function visitInstaller(): void {
     // Should redirect to the installer.
     $this->drupalGet($GLOBALS['base_url']);
-    // Ensure no database tables have been created yet.
-    $this->assertSame([], Database::getConnection()->schema()->findTables('%'));
+    // Ensure only the config table has been created yet.
+    $this->assertSame([
+      'config' => 'config',
+    ], Database::getConnection()->schema()->findTables('%'));
     $this->assertSession()->addressEquals($GLOBALS['base_url'] . '/core/install.php');
   }
 
