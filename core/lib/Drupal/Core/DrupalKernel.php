f7366a048ea72aafdb4e0b0ddc3abab2abb78f81
Lee Rowlands <23667-larowlan@users.noreply.drupalcode.org>
2024-12-08 02:28:11 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: godotislate <15754-godotislate@users.noreply.drupalcode.org>

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -922,9 +922,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
 
     $this->attachSynthetic($container);
 
-    if ($this->container !== NULL) {
-      $this->container->reset();
-    }
+    $this->container?->reset();
     $this->container = $container;
     if ($session_started) {
       $this->container->get('session')->start();
