7afda54fe6c789f0ed3b4df2dd7dfb57a6ce82e4
mondrake <mondrake.org@gmail.com>
2024-12-08 22:51:22 +0100

    fix

--- core/tests/Drupal/Tests/Core/Database/DatabaseEventsTest.php
+++ core/tests/Drupal/Tests/Core/Database/DatabaseEventsTest.php
@@ -10,7 +10,6 @@ use Drupal\Core\Database\Event\StatementEvent;
 use Drupal\Core\Database\Event\StatementExecutionEndEvent;
 use Drupal\Core\Database\Event\StatementExecutionFailureEvent;
 use Drupal\Core\Database\Event\StatementExecutionStartEvent;
-use Drupal\Core\Database\Exception\EventException;
 use Drupal\Tests\Core\Database\Stub\StubConnection;
 use Drupal\Tests\Core\Database\Stub\StubPDO;
 use Drupal\Tests\UnitTestCase;
