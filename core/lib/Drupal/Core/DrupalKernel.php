51dcbb699e67c25d0831fa95b6d2f8252fb9949f
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2024-12-09 08:47:57 +0000

    go synthetic

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -4,7 +4,7 @@ namespace Drupal\Core;
 
 use Composer\Autoload\ClassLoader;
 use Drupal\Component\EventDispatcher\Event;
-use Drupal\Component\EventDispatcher\EventFactory;
+use Drupal\Component\EventDispatcher\EventDispatcherFactory;
 use Drupal\Component\FileCache\FileCacheFactory;
 use Drupal\Component\Serialization\PhpSerialize;
 use Drupal\Component\Utility\UrlHelper;
@@ -1139,7 +1139,6 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
    * {@inheritdoc}
    */
   public function rebuildContainer() {
-    EventDispatcherFactory::reset();
     // Empty module properties and for them to be reloaded from scratch.
     $this->moduleList = NULL;
     $this->moduleData = [];
@@ -1195,6 +1194,8 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
 
     // Set the class loader which was registered as a synthetic service.
     $this->container->set('class_loader', $this->classLoader);
+
+    $this->container->set('event_dispatcher', EventDispatcherFactory::getInstance());
 
     if ($reload_module_handler) {
       $this->container->get('module_handler')->reload();
@@ -1274,6 +1275,9 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
 
     // Set the class loader which was registered as a synthetic service.
     $container->set('class_loader', $this->classLoader);
+
+    $container->set('event_dispatcher', EventDispatcherFactory::getInstance());
+
     return $container;
   }
 
@@ -1337,6 +1341,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
     $container->register('class_loader')->setSynthetic(TRUE);
     $container->register('kernel', 'Symfony\Component\HttpKernel\KernelInterface')->setSynthetic(TRUE);
     $container->register('service_container', 'Symfony\Component\DependencyInjection\ContainerInterface')->setSynthetic(TRUE);
+    $container->register('event_dispatcher', 'Symfony\Component\EventDispatcher\EventDispatcherInterface')->setSynthetic(TRUE);
 
     // Register aliases of synthetic services for autowiring.
     $container->setAlias(DrupalKernelInterface::class, 'kernel');
