a4603f56a135e0920e5ab1c15274f902b6b83c03
Dezső BICZÓ <dezso.biczo@pronovix.com>
2024-12-11 09:26:46 +0100

    DI new dependency with deprecation

--- core/modules/node/src/NodeViewsData.php
+++ core/modules/node/src/NodeViewsData.php
@@ -2,13 +2,69 @@
 
 namespace Drupal\node;
 
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Sql\SqlEntityStorageInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\views\EntityViewsData;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the views data for the node entity type.
  */
 class NodeViewsData extends EntityViewsData {
+
+  /**
+   * The module extension list.
+   *
+   * @var \Drupal\Core\Extension\ModuleExtensionList
+   */
+  protected $moduleExtensionList;
+
+  /**
+   * Constructs an NodeViewsData object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type to provide views integration for.
+   * @param \Drupal\Core\Entity\Sql\SqlEntityStorageInterface $storage_controller
+   *   The storage handler used for this entity type.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $translation_manager
+   *   The translation manager.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
+   * @param \Drupal\Core\Extension\ModuleExtensionList|null $moduleExtensionList
+   *   The module extension list.
+   */
+  public function __construct(EntityTypeInterface $entity_type, SqlEntityStorageInterface $storage_controller, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, TranslationInterface $translation_manager, EntityFieldManagerInterface $entity_field_manager, ?ModuleExtensionList $moduleExtensionList = NULL) {
+    parent::__construct($entity_type, $storage_controller, $entity_type_manager, $module_handler, $translation_manager, $entity_field_manager);
+    if ($this->moduleExtensionList === NULL) {
+      @trigger_error('Calling ' . __METHOD__ . '() without the $moduleExtensionList argument is deprecated in drupal:10.4.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3493129', E_USER_DEPRECATED);
+      $this->moduleExtensionList = \Drupal::service('extension.list.module');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('entity_type.manager'),
+      $container->get('module_handler'),
+      $container->get('string_translation'),
+      $container->get('entity_field.manager'),
+      $container->get('extension.list.module')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -378,7 +434,7 @@ class NodeViewsData extends EntityViewsData {
   private function getNodeAccessImplementations(): array {
     $implementations = [];
     if ($this->moduleHandler->hasImplementations('node_grants')) {
-      $module_data = \Drupal::getContainer()->get('extension.list.module')->getAllInstalledInfo();
+      $module_data = $this->moduleExtensionList->getAllInstalledInfo();
       foreach (['node_grants', 'node_grants_alter'] as $hook) {
         $this->moduleHandler->invokeAllWith(
           $hook,
