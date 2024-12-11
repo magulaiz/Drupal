54e48dbaae63f3e1dfce155e376692c7f866422c
Artem  Dmitriiev <a.dmitriiev@1xinternet.de>
2024-12-11 12:47:04 +0100

    Add test with override display option

--- core/modules/views/tests/src/Kernel/Plugin/ConfigAction/SetDisplayOptionConfigActionTest.php
+++ core/modules/views/tests/src/Kernel/Plugin/ConfigAction/SetDisplayOptionConfigActionTest.php
@@ -146,4 +146,60 @@ class SetDisplayOptionConfigActionTest extends ViewsKernelTestBase {
     $this->assertArrayHasKey('user_id', $fields);
   }
 
+  /**
+   * Tests adding field to a new display with override.
+   */
+  public function testAddFieldToOverridenDisplay() : void {
+    $view = Views::getView('test_disabled_display');
+    $view->setDisplay();
+    $fields = $view->displayHandlers->get('default')->getOption('fields');
+    // Check that field type is not part of default display.
+    $this->assertArrayNotHasKey('type', $fields);
+    // Create a new display.
+    $config_action_settings = [
+      'old_display_id' => 'page_1',
+      'new_display_type' => 'page',
+    ];
+    $this->container->get('plugin.manager.config_action')->applyAction('test_disabled_display', 'views.view.entity_test_fields', $config_action_settings);
+    $view = Views::getView('test_disabled_display');
+    // Confirm that new display was created.
+    $this->assertTrue($view->displayHandlers->has('page_3'));
+    // Apply config action that adds field type to page_3 display only.
+    $config_action_settings = [
+      'display_id' => 'page_3',
+      'option' => 'fields',
+      'item' => 'type',
+      'override' => TRUE,
+      'settings' => [
+        'id' => 'type',
+        'table' => 'entity_test',
+        'field' => 'type',
+        'entity_type' => 'entity_test',
+        'entity_field' => 'type',
+        'plugin_id' => 'field',
+        'exclude' => FALSE,
+        'alter' => [
+          'alter_text' => FALSE,
+        ],
+        'element_class' => '',
+        'empty' => '',
+        'hide_empty' => FALSE,
+        'empty_zero' => FALSE,
+        'hide_alter_empty' => TRUE,
+      ],
+    ];
+    $this->container->get('plugin.manager.config_action')->applyAction('setDisplayOption', 'views.view.test_disabled_display', $config_action_settings);
+    // Check that field was added to page_3 and not to default.
+    $view = Views::getView('test_disabled_display');
+    $view->setDisplay();
+    $fields = $view->displayHandlers->get('default')->getOption('fields');
+    // Check that field type is not part of default display.
+    $this->assertArrayNotHasKey('type', $fields);
+    $view->setDisplay('page_3');
+    $fields = $view->displayHandlers->get('page_3')->getOption('fields');
+    // Check that field type is not part of default display.
+    $this->assertArrayHasKey('type', $fields);
+
+  }
+
 }
