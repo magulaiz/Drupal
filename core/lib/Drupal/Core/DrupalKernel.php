7e59b7a134810b243593d9f6d15ce91167f30719
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2024-12-13 12:04:50 +0000

    add  preBootstrapSubscribers

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -15,6 +15,8 @@ use Drupal\Component\DependencyInjection\ReverseContainer;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
 use Drupal\Core\DependencyInjection\YamlFileLoader;
+use Drupal\Core\EventDispatcher\EventDispatcherFactory;
+use Drupal\Core\EventDispatcher\EventDispatcherFactoryInterface;
 use Drupal\Core\EventDispatcher\EventDispatcherFactoryStage;
 use Drupal\Core\Extension\Extension;
 use Drupal\Core\Extension\ExtensionDiscovery;
@@ -72,8 +74,8 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
   protected $defaultBootstrapContainerDefinition = [
     'parameters' => [],
     'services' => [
-      'Drupal\Core\EventDispatcher\EventDispatcherFactoryInterface' => [
-        'class' => 'Drupal\Core\EventDispatcher\EventDispatcherFactory',
+      EventDispatcherFactoryInterface::class => [
+        'class' => EventDispatcherFactory::class,
       ],
       'event_dispatcher' => [
         'class' => 'Symfony\Component\EventDispatcher\EventDispatcherInterface',
--- core/lib/Drupal/Core/EventDispatcher/EventDispatcherFactory.php
+++ core/lib/Drupal/Core/EventDispatcher/EventDispatcherFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Core\EventDispatcher;
 
+use Drupal\Core\Database\EventSubscriber\StatementExecutionSubscriber;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -15,9 +16,25 @@ class EventDispatcherFactory implements EventDispatcherFactoryInterface {
   private static EventDispatcherInterface $eventDispatcher;
   private static EventDispatcherFactoryStage $stage;
 
+  /**
+   * Subscribers to be registered for early event dispatcher.
+   *
+   * Note that the early event dispatcher will be purged during container
+   * bootstrap, so any subscriber/listener needed in later stages have to be
+   * added also in the service container definition.
+   */
+  private readonly static array $preBootstrapSubscribers = [
+    new StatementExecutionSubscriber();
+  ];
+
   public static function createInstance(EventDispatcherFactoryStage|string $stage = EventDispatcherFactoryStage::PreBootstrap): EventDispatcherInterface {
     self::$stage = is_string($stage) ? EventDispatcherFactoryStage::from($stage) : $stage;
     self::$eventDispatcher = new EventDispatcher();
+    if (self::$stage !== EventDispatcherFactoryStage::FullContainer) {
+      foreach (self::$preBootstrapSubscribers as $subscriber) {
+        self::$eventDispatcher->addSubscriber($subscriber);
+      }
+    }
     return self::$eventDispatcher;
   }
 
