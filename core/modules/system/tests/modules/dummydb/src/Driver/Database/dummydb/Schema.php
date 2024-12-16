2d7e9206fef5b327870b1ea6549d213b2ccae47c
David Bekker <david.bekker@finalist.nl>
2024-12-16 13:59:00 +0100

    2 added dummy classes for fixing the PHPStan errors

---
+++ core/modules/system/tests/modules/dummydb/src/Driver/Database/dummydb/Schema.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\dummydb\Driver\Database\dummydb;
+
+use Drupal\Core\Database\Schema as DatabaseSchema;
+
+/**
+ * DummyDB implementation of \Drupal\Core\Database\Schema.
+ */
+class Schema extends DatabaseSchema {}
---
+++ core/modules/system/tests/modules/dummydb/src/Driver/Database/dummydb/Upsert.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\dummydb\Driver\Database\dummydb;
+
+use Drupal\Core\Database\Query\Upsert as QueryUpsert;
+
+/**
+ * DummyDB implementation of \Drupal\Core\Database\Query\Upsert.
+ */
+class Upsert extends QueryUpsert {}
