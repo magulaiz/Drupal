db8a65ee9741d98760d5e50e1c117d866b33f6c4
David Bekker <david.bekker@finalist.nl>
2024-12-16 14:23:25 +0100

    Added the abstract methods to DummyDB Schema

--- core/modules/system/tests/modules/dummydb/src/Driver/Database/dummydb/Schema.php
+++ core/modules/system/tests/modules/dummydb/src/Driver/Database/dummydb/Schema.php
@@ -11,4 +11,85 @@ use Drupal\Core\Database\Schema as DatabaseSchema;
 /**
  * DummyDB implementation of \Drupal\Core\Database\Schema.
  */
-class Schema extends DatabaseSchema {}
+class Schema extends DatabaseSchema {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFieldTypeMap(): array {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function renameTable($table, $new_name): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dropTable($table): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addField($table, $field, $spec, $keys_new = []): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dropField($table, $field): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function indexExists($table, $name): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addPrimaryKey($table, $fields): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dropPrimaryKey($table):bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addUniqueKey($table, $name, $fields): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dropUniqueKey($table, $name): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addIndex($table, $name, $fields, array $spec): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dropIndex($table, $name): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function changeField($table, $field, $field_new, $spec, $keys_new = []): void {}
+
+}
