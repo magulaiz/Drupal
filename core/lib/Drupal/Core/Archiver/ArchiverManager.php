a41230765467be9f8f4814808bf5a81c3cdbc754
Andreas Hennings <andreas@dqxtech.net>
2024-12-17 14:14:20 +0100

    Add some explicit return types in getInstance() implementations.
    It seems that PhpStan wants this.

--- core/lib/Drupal/Core/Archiver/ArchiverManager.php
+++ core/lib/Drupal/Core/Archiver/ArchiverManager.php
@@ -48,6 +48,9 @@ class ArchiverManager extends DefaultPluginManager {
 
   /**
    * {@inheritdoc}
+   *
+   * @return \Drupal\Core\Archiver\ArchiverInterface
+   *   The new plugin instance.
    */
   public function createInstance($plugin_id, array $configuration = []) {
     $plugin_definition = $this->getDefinition($plugin_id);
@@ -57,6 +60,9 @@ class ArchiverManager extends DefaultPluginManager {
 
   /**
    * {@inheritdoc}
+   *
+   * @return \Drupal\Core\Archiver\ArchiverInterface|null
+   *   The new plugin instance, or NULL if not found.
    */
   public function getInstance(array $options) {
     $filepath = $options['filepath'];
