c45353451f914318e9910de5b1519a185ffc7584
Andreas Hennings <andreas@dqxtech.net>
2024-12-17 03:36:18 +0100

    Reorder imports in files where new imports will be added.

--- core/modules/views/src/Plugin/ViewsHandlerManager.php
+++ core/modules/views/src/Plugin/ViewsHandlerManager.php
@@ -7,10 +7,10 @@ use Drupal\Component\Plugin\FallbackPluginManagerInterface;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
+use Drupal\views\Plugin\views\HandlerBase;
 use Drupal\views\Plugin\views\ViewsHandlerInterface;
 use Drupal\views\ViewsData;
 use Symfony\Component\DependencyInjection\Container;
-use Drupal\views\Plugin\views\HandlerBase;
 
 /**
  * Plugin type manager for all views handlers.
