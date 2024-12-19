42c0ee42b4dfb93b40fa9a6b18b6f5e2610fddc8
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-18 20:53:45 -0500

    remove non service only changes

--- core/lib/Drupal/Core/Hook/HookCollectorPass.php
+++ core/lib/Drupal/Core/Hook/HookCollectorPass.php
@@ -143,10 +143,7 @@ class HookCollectorPass implements CompilerPassInterface {
     $module_preg = '/^(?<function>(?<module>' . implode('|', $modules) . ')_(?!preprocess_)(?!update_\d)(?<hook>[a-zA-Z0-9_\x80-\xff]+$))/';
     $collector = new static();
     foreach ($module_filenames as $module => $info) {
-      $skip_procedural = FALSE;
-      if ($container?->hasParameter("$module.hooks_converted")) {
-        $skip_procedural = $container->getParameter("$module.hooks_converted");
-      }
+      $skip_procedural = isset($container) ? $container->hasParameter("$module.hooks_converted") : FALSE;
       $collector->collectModuleHookImplementations(dirname($info['pathname']), $module, $module_preg, $skip_procedural);
     }
     return $collector;
