f4f6aadf049b79dea625f940b96e724376e26e9a
andrew farquharson <farquharson.a@gmail.com>
2024-12-19 04:29:34 +0000

    replace strip_tags() with htmlspecialchars_decode()

--- core/modules/user/tests/src/Functional/UserPasswordResetTest.php
+++ core/modules/user/tests/src/Functional/UserPasswordResetTest.php
@@ -336,7 +336,7 @@ class UserPasswordResetTest extends BrowserTestBase {
     // Assume the most recent email.
     $_emails = $this->drupalGetMails();
     $email = end($_emails);
-    if (strip_tags($email['body']) === $email['body']) {
+    if (htmlspecialchars_decode($email['body']) === $email['body']) {
       return TRUE;
     }
     return FALSE;
