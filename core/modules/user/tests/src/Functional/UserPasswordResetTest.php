87630405e411cc83829710f52935a9e751bb8045
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2024-12-21 15:35:40 +0000

    Update UserPasswordResetTest.php cspell:ignore

--- core/modules/user/tests/src/Functional/UserPasswordResetTest.php
+++ core/modules/user/tests/src/Functional/UserPasswordResetTest.php
@@ -170,8 +170,9 @@ class UserPasswordResetTest extends BrowserTestBase {
 
     // Change the site name.
     // The site name token in the email will be replaced by this one.
+    // cspell:ignore L\'equipe de l\'Agriculture
     \Drupal::configFactory()->getEditable('system.site')->set('name', 'L\'equipe de l\'Agriculture')->save();
-    // Request a new password again using the email address.
+    // Request a new password using the email address.
     $this->drupalGet('user/password');
     $edit = ['name' => $this->account->getEmail()];
     $this->submitForm($edit, 'Submit');
