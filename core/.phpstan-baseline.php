3b89f8af59089e27a95efb9547bc9d51e151581f
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-21 20:46:44 -0500

    Convert Demo Umami

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -54772,30 +54772,6 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/profiles/demo_umami/demo_umami.install',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Function demo_umami_form_install_configure_submit\\(\\) has no return type specified\\.$#',
-	'identifier' => 'missingType.return',
-	'count' => 1,
-	'path' => __DIR__ . '/profiles/demo_umami/demo_umami.profile',
-];
-$ignoreErrors[] = [
-	'message' => '#^Function demo_umami_help\\(\\) has no return type specified\\.$#',
-	'identifier' => 'missingType.return',
-	'count' => 1,
-	'path' => __DIR__ . '/profiles/demo_umami/demo_umami.profile',
-];
-$ignoreErrors[] = [
-	'message' => '#^Function demo_umami_set_users_passwords\\(\\) has no return type specified\\.$#',
-	'identifier' => 'missingType.return',
-	'count' => 1,
-	'path' => __DIR__ . '/profiles/demo_umami/demo_umami.profile',
-];
-$ignoreErrors[] = [
-	'message' => '#^Function demo_umami_toolbar\\(\\) has no return type specified\\.$#',
-	'identifier' => 'missingType.return',
-	'count' => 1,
-	'path' => __DIR__ . '/profiles/demo_umami/demo_umami.profile',
-];
-$ignoreErrors[] = [
 	'message' => '#^Method Drupal\\\\demo_umami_content\\\\InstallHelper\\:\\:create\\(\\) has no return type specified\\.$#',
 	'identifier' => 'missingType.return',
 	'count' => 1,
