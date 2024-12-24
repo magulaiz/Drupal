9946eaafd8eb9d6430ec44e198c2e289436c20ef
Bram Driesen <bramdriesen94@gmail.com>
2024-12-24 11:17:42 +0000

    fix: phpstan

--- core/lib/Drupal/Core/StreamWrapper/CoreStreamWrapperManagerInterface.php
+++ core/lib/Drupal/Core/StreamWrapper/CoreStreamWrapperManagerInterface.php
@@ -8,9 +8,9 @@ namespace Drupal\Core\StreamWrapper;
  * @see \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface
  *
  * @internal
- *   These methods are necessary for a StreamWrapper manager to implement however
- *   unlike methods on StreamWrapperManagerInterface these methods generally
- *   should only be invoked by Drupal Core and not third parties.
+ *   These methods are necessary for a StreamWrapper manager to implement
+ *   however unlike methods on StreamWrapperManagerInterface these methods
+ *   generally should only be invoked by Drupal Core and not third parties.
  */
 interface CoreStreamWrapperManagerInterface {
 
@@ -24,7 +24,7 @@ interface CoreStreamWrapperManagerInterface {
    * @param string $scheme
    *   The scheme for which the wrapper should be registered.
    */
-  public function addStreamWrapper($service_id, $class, $scheme): void;
+  public function addStreamWrapper(string $service_id, string $class, string $scheme): void;
 
   /**
    * Registers the tagged stream wrappers.
