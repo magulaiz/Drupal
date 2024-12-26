b1084bc9d147cf46be0101605d9796cad4201ae4
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-26 11:13:48 -0500

    Use module handler

--- core/lib/Drupal/Core/Extension/ModuleHandler.php
+++ core/lib/Drupal/Core/Extension/ModuleHandler.php
@@ -66,6 +66,13 @@ class ModuleHandler implements ModuleHandlerInterface {
   protected array $invokeMap = [];
 
   /**
+   * Legacy implementations.
+   *
+   * @var array
+   */
+  protected array $hasLegacyHookAttribute = [];
+
+  /**
    * Constructs a ModuleHandler object.
    *
    * @param string $root
@@ -301,9 +308,20 @@ class ModuleHandler implements ModuleHandlerInterface {
   /**
    * {@inheritdoc}
    */
-  public function hasImplementations(string $hook, $modules = NULL): bool {
+  public function hasImplementations(string $hook, $modules = NULL, $legacy = FALSE): bool {
     $implementation_modules = array_keys($this->getHookListeners($hook));
-    return (bool) (isset($modules) ? array_intersect($implementation_modules, (array) $modules) : $implementation_modules);
+    $return = (bool) (isset($modules) ? array_intersect($implementation_modules, (array) $modules) : $implementation_modules);
+    if ($return) {
+      return TRUE;
+    }
+    if ($legacy && $modules) {
+      foreach ($modules as $module) {
+        if ($this->canLegacyInvoke($module, $hook)) {
+          return TRUE;
+        }
+      }
+    }
+    return FALSE;
   }
 
   /**
@@ -354,12 +372,22 @@ class ModuleHandler implements ModuleHandlerInterface {
    */
   protected function legacyInvoke($module, $hook, array $args = []) {
     $this->load($module);
-    $function = $module . '_' . $hook;
-    if (function_exists($function) && !(new \ReflectionFunction($function))->getAttributes(LegacyHook::class)) {
+    if ($function = $this->canLegacyInvoke($module, $hook)) {
       return $function(... $args);
     }
 
     return NULL;
+  }
+
+  protected function canLegacyInvoke($module, $hook): string|FALSE {
+    $function = $module . '_' . $hook;
+    if (!function_exists($function)) {
+      return FALSE;
+    }
+    if (!isset($this->hasLegacyHookAttribute[$function])) {
+      $this->hasLegacyHookAttribute[$function] = !(new \ReflectionFunction($function))->getAttributes(LegacyHook::class);
+    }
+    return $this->hasLegacyHookAttribute[$function] ? $function : FALSE;
   }
 
   /**
@@ -576,7 +604,7 @@ class ModuleHandler implements ModuleHandlerInterface {
           else {
             $callable = $listener;
           }
-          if (isset($this->moduleList[$module])) {
+          if (isset($this->moduleList[$module]) || $module === 'template') {
             $this->invokeMap[$hook][$module][] = $callable;
           }
         }
