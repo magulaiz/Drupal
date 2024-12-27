71326cc1c9ff3dfafadee83970c13932053cc059
Heissen Lopez <heissen.lopez@gmail.com>
2024-12-27 11:20:59 -0500

    #3493872: Standardize HttpKernel property name across middleware classes
    - Rename property 'app' to 'httpKernel' in NegotiationMiddleware
    - Rename property 'kernel' to 'httpKernel' in StackedHttpKernel
    - Update constructor parameter names from 'kernel' to 'http_kernel'

--- core/lib/Drupal/Core/StackMiddleware/NegotiationMiddleware.php
+++ core/lib/Drupal/Core/StackMiddleware/NegotiationMiddleware.php
@@ -16,7 +16,7 @@ class NegotiationMiddleware implements HttpKernelInterface {
    *
    * @var \Symfony\Component\HttpKernel\HttpKernelInterface
    */
-  protected $app;
+  protected $httpKernel;
 
   /**
    * Contains a hashmap of format as key and mimetype as value.
@@ -28,11 +28,11 @@ class NegotiationMiddleware implements HttpKernelInterface {
   /**
    * Constructs a new NegotiationMiddleware.
    *
-   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $app
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel
    *   The wrapper HTTP kernel
    */
-  public function __construct(HttpKernelInterface $app) {
-    $this->app = $app;
+  public function __construct(HttpKernelInterface $http_kernel) {
+    $this->httpKernel = $http_kernel;
   }
 
   /**
