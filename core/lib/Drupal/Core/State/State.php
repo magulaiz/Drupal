e17d9ccae34ea437cf7f322fde633191614c0ae6
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-27 20:54:36 +0000

    Less lock waits.

--- core/lib/Drupal/Core/State/State.php
+++ core/lib/Drupal/Core/State/State.php
@@ -99,14 +99,6 @@ class State extends CacheCollector implements StateInterface {
     // CacheCollector::set() to work.
     parent::set($key, $value);
     $this->persist($key);
-
-    // Attempt to acquire the lock, and wait if it's not acquired, before
-    // calling ::updateCache(), to maximize the chance that this process
-    // invalidates the cache regardless of what is going on in other processes.
-    $lock_name = $this->getCid() . ':' . CacheCollector::class;
-    if (!$this->lock->acquire($lock_name)) {
-      $this->lock->wait($lock_name);
-    }
     static::updateCache();
   }
 
