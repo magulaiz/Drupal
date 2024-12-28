872504be87eb872afa53bcd9a946d3c84c931451
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-28 09:21:43 +0000

    More lock fun.

--- core/lib/Drupal/Core/State/State.php
+++ core/lib/Drupal/Core/State/State.php
@@ -101,8 +101,34 @@ class State extends CacheCollector implements StateInterface {
     $this->persist($key);
     $lock_name = $this->getCid() . ':' . CacheCollector::class;
     $lock_acquired = $this->lock->acquire($lock_name);
-    usleep(10000);
+    if (!$lock_acquired) {
+      // Wait for the lock to become available for a maximum of one second, then
+      // attempt to acquire the lock again. If we can't acquire the lock, then
+      // the one second that has passed should have given most processes time to
+      // complete anyway.
+      $this->lock->wait($lock_name, 1);
+    }
+    else {
+      // Cache items are stored with millisecond precision, and are compared by
+      // created time in CacheCollector. This allows for a race condition where:
+      // Process A: writes a cache item.
+      // Process B: reads the cache item.
+      // Process C: (this process) writes a new cache item in the same
+      // millisecond.
+      // Process B: reaches CacheCollector::destruct(), and the race condition
+      // protection logic compares the created timestamps of two different cache
+      // items and finds them the same. By sleeping for 10 milliseconds both
+      // prior to and after writing the cache item, we ensure that this
+      // situation  doesn't occur as long as the lock was acquired.
+      // @todo: this still doesn't account for the case where due to a clock
+      // offset between servers, identical timestamps are recorded despite
+      // happening at different times. Consider a more unique indentifier in
+      // cache collector.
+      usleep(10000);
+    }
     $this->cache->set($this->getCid(), [$key => $value], CacheBackendInterface::CACHE_PERMANENT, $this->tags);
+    // Sleep for another 10 milliseconds so that a process doesn't immediately
+    // overwrite this cache item with one with an identical timestamp.
     usleep(10000);
     if ($lock_acquired) {
       $this->lock->release($lock_name);
