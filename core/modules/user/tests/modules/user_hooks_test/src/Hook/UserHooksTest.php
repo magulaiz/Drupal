d4ef2e14eb7132c366fabc32a6c558132c5f9297
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2024-12-28 09:09:24 +0100

    2500x 4 classes key value

--- core/modules/user/tests/modules/user_hooks_test/src/Hook/UserHooksTest.php
+++ core/modules/user/tests/modules/user_hooks_test/src/Hook/UserHooksTest.php
@@ -6,15 +6,15 @@ namespace Drupal\user_hooks_test\Hook;
 
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\State\StateInterface;
 
 /**
  * Contains hook implementations.
  */
 class UserHooksTest {
 
-  public function __construct(protected StateInterface $state) {
+  public function __construct(protected KeyValueStoreInterface $keyValue) {
   }
 
   /**
@@ -29,8 +29,8 @@ class UserHooksTest {
    */
   #[Hook('user_format_name_alter')]
   public function userFormatNameAlter(&$name, AccountInterface $account): void {
-    if ($this->state->get('user_hooks_test_user_format_name_alter', FALSE)) {
-      if ($this->state->get('user_hooks_test_user_format_name_alter_safe', FALSE)) {
+    if ($this->keyValue->get('user_hooks_test')->get('user_format_name_alter', FALSE)) {
+      if ($this->keyValue->get('user_hooks_test')->get('user_format_name_alter_safe', FALSE)) {
         $name = new FormattableMarkup('<em>@uid</em>', ['@uid' => $account->id()]);
       }
       else {
