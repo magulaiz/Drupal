f9d97adf1e187d4413d1213e34db37005b321afa
Benji Fisher <benji@FisherFam.org>
2024-12-28 17:01:46 -0500

    Revert "3036368 - Fix PHP Notice in `\Drupal\migrate\Row::changed`"
    This reverts commit 3bf2ccad2ff4651fbebc5928bc8d727fb78787f2.
    
    In an earlier version, this change was needed since rowChanged() was
    called before prepareRow(). The current version does not do that.

--- core/modules/migrate/src/Row.php
+++ core/modules/migrate/src/Row.php
@@ -399,10 +399,7 @@ class Row {
    *   called, this always returns FALSE.
    */
   public function changed() {
-    $original_hash = $this->idMap['original_hash'] ?? NULL;
-    $current_hash = $this->idMap['hash'] ?? NULL;
-
-    return $original_hash != $current_hash;
+    return $this->idMap['original_hash'] != $this->idMap['hash'];
   }
 
   /**
