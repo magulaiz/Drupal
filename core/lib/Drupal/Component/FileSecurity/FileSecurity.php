b3cefe34fd28a726416aa45cd8834e740c038aa8
Kent Richards <22302-kentr@users.noreply.drupalcode.org>
2024-12-27 20:00:12 -0700

    Issue #3136788: Change REQUEST_URI regex
    Uses the regex from the PHP-FPM wiki page.
    
    See https://cwiki.apache.org/confluence/display/httpd/PHP-FPM

--- core/lib/Drupal/Component/FileSecurity/FileSecurity.php
+++ core/lib/Drupal/Component/FileSecurity/FileSecurity.php
@@ -72,7 +72,7 @@ SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
   # PHP-FPM SetHandler is inside an If
   # which is applied after Files. Need to be inside another If to override.
   # only block php, not other files e.g. advagg css
-  <If "%{REQUEST_URI} =~ /.+\.ph(?:ar|p|tml)$/">
+  <If "%{REQUEST_URI} =~ /.+\.ph(ar|p|tml)$/">
     SetHandler Drupal_Security_Do_Not_Remove_See_SA_2013_003
   </If>
 </Files>
