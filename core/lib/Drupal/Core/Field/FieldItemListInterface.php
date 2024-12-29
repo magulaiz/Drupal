5b281057f80c4375038b91b1657df4ae306733ab
mondrake <mondrake.org@gmail.com>
2024-12-29 22:20:47 +0100

    adds PHPDoc annotations

--- core/lib/Drupal/Core/Field/FieldItemListInterface.php
+++ core/lib/Drupal/Core/Field/FieldItemListInterface.php
@@ -23,8 +23,9 @@ use Drupal\Core\TypedData\ListInterface;
  * When implementing this interface which extends Traversable, make sure to list
  * IteratorAggregate or Iterator before this interface in the implements clause.
  *
- * Note: phpstan-drupal stubs this interface to make it a generic of
- * type <FieldItemInterface>.
+ * @template T of \Drupal\Core\Field\FieldItemInterface
+ * @extends \Drupal\Core\TypedData\ListInterface<T>
+ * @property mixed $value
  *
  * @see \Drupal\Core\Field\FieldItemInterface
  */
