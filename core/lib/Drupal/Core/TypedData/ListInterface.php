1038e5a2a6f934bd812037585a2d27255e56e5b5
mondrake <mondrake.org@gmail.com>
2024-12-29 16:46:48 +0100

    rev

--- core/lib/Drupal/Core/TypedData/ListInterface.php
+++ core/lib/Drupal/Core/TypedData/ListInterface.php
@@ -13,10 +13,6 @@ namespace Drupal\Core\TypedData;
  *
  * @see \Drupal\Core\TypedData\ListDefinitionInterface
  *
- * @extends \ArrayAccess<int<0,max>,\Drupal\Core\TypedData\TypedDataInterface|null>
- * @extends \Traversable<int<0,max>,\Drupal\Core\TypedData\TypedDataInterface|null>
- * @extends iterable<int<0,max>,\Drupal\Core\TypedData\TypedDataInterface|null>
- *
  * @ingroup typed_data
  */
 interface ListInterface extends TraversableTypedDataInterface, \ArrayAccess, \Countable {
