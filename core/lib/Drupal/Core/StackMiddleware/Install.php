89bcd9a7a654d28cb6c5b9e65786b48b8f08c3dd
Darren Oh <darrenoh@30772.no-reply.drupal.org>
2024-12-31 13:09:56 -0500

    Fix invalid caches

--- core/lib/Drupal/Core/StackMiddleware/Install.php
+++ core/lib/Drupal/Core/StackMiddleware/Install.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Core\StackMiddleware;
 
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\DrupalKernelInterface;
 use Drupal\Core\EventSubscriber\MainContentViewSubscriber;
 use Drupal\Core\State\StateInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -15,31 +17,20 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 class Install implements HttpKernelInterface {
 
   /**
-   * The wrapped HTTP kernel.
-   *
-   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
-   */
-  protected $httpKernel;
-
-  /**
-   * The Drupal state service.
-   *
-   * @var \Drupal\Core\State\StateInterface
-   */
-  protected $state;
-
-  /**
    * Constructs a new Install instance.
    *
-   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $httpKernel
    *   The wrapped HTTP kernel.
+   * @param \Drupal\Core\DrupalKernelInterface $drupalKernel
+   *   The main Drupal kernel.
    * @param \Drupal\Core\State\StateInterface $state
    *   The Drupal state service.
    */
-  public function __construct(HttpKernelInterface $http_kernel, StateInterface $state) {
-    $this->httpKernel = $http_kernel;
-    $this->state = $state;
-  }
+  public function __construct(
+    protected readonly HttpKernelInterface $httpKernel,
+    protected readonly DrupalKernelInterface $drupalKernel,
+    protected readonly StateInterface $state,
+  ) {}
 
   /**
    * {@inheritdoc}
@@ -47,12 +38,16 @@ class Install implements HttpKernelInterface {
   public function handle(Request $request, $type = self::MAIN_REQUEST, $catch = TRUE): Response {
     $task = $this->state->get('install_task', 'done');
     if ($task !== 'done') {
+      // Caches are not valid until installation is done.
+      foreach (Cache::getBins() as $cache_backend) {
+        $cache_backend->deleteAll();
+      }
+      $this->drupalKernel->invalidateContainer();
+      $this->state->resetCache();
       // Only redirect if this is an HTML response (i.e., a user trying to view
       // the site in a web browser before installing it).
       $format = $request->query->get(MainContentViewSubscriber::WRAPPER_FORMAT, $request->getRequestFormat());
       if ($format == 'html') {
-        // Make sure the next request uses the state set by the installer.
-        $this->state->resetCache();
         return new RedirectResponse($request->getBasePath() . '/core/install.php', 302, ['Cache-Control' => 'no-cache']);
       }
     }
