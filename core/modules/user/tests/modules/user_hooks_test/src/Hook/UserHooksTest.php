8625d2819bd01f18257e4e3e2ccb8f1f1bbe7c6a
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2025-01-02 08:56:40 +0100

    Revert "No DI"
    This reverts commit 74cecbd4412e0ad7f01de3cddf0a19fc5c520fff.

--- core/modules/user/tests/modules/user_hooks_test/src/Hook/UserHooksTest.php
+++ core/modules/user/tests/modules/user_hooks_test/src/Hook/UserHooksTest.php
@@ -14,15 +14,7 @@ use Drupal\Core\Session\AccountInterface;
  */
 class UserHooksTest {
 
-  /**
-   * The key value store implementation.
-   *
-   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
-   */
-  protected KeyValueStoreInterface $keyValue;
-
-  public function __construct(KeyValueStoreInterface $key_value) {
-    $this->keyValue = $key_value;
+  public function __construct(protected KeyValueStoreInterface $keyValue) {
   }
 
   /**
@@ -37,8 +29,8 @@ class UserHooksTest {
    */
   #[Hook('user_format_name_alter')]
   public function userFormatNameAlter(&$name, AccountInterface $account): void {
-    if ($this->keyValue->get('user_hooks_test')->get('user_format_name_alter', FALSE)) {
-      if ($this->keyValue->get('user_hooks_test')->get('user_format_name_alter_safe', FALSE)) {
+    if ($this->keyValue->get('user_format_name_alter', FALSE)) {
+      if ($this->keyValue->get('user_format_name_alter_safe', FALSE)) {
         $name = new FormattableMarkup('<em>@uid</em>', ['@uid' => $account->id()]);
       }
       else {
