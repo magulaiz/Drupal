1238f3870d7c9881ed3c07f0b6dedae68c00578d
Dezső BICZÓ <dezso.biczo@pronovix.com>
2025-01-03 11:29:38 +0100

    Handle potential null value of \Symfony\Component\HttpFoundation\RequestStack::getCurrentRequest()++

--- core/lib/Drupal/Core/Utility/UnroutedUrlAssembler.php
+++ core/lib/Drupal/Core/Utility/UnroutedUrlAssembler.php
@@ -168,18 +168,22 @@ class UnroutedUrlAssembler implements UnroutedUrlAssemblerInterface {
    *   The options to merge in the defaults.
    */
   protected function addOptionDefaults(array &$options) {
-    $request = $this->requestStack->getCurrentRequest();
-    $current_base_path = $request->getBasePath() . '/';
-    $current_script_path = '';
-    $base_path_with_script = $request->getBaseUrl();
+    $current_script_path = '/';
 
-    // If the current request was made with the script name (eg, index.php) in
-    // it, then extract it, making sure the leading / is gone, and a trailing /
-    // is added, to allow simple string concatenation with other parts.
-    if (!empty($base_path_with_script)) {
-      $script_name = $request->getScriptName();
-      if (str_contains($base_path_with_script, $script_name)) {
-        $current_script_path = ltrim(substr($script_name, strlen($current_base_path)), '/') . '/';
+    $request = $this->requestStack->getCurrentRequest();
+    if ($request) {
+      $current_base_path = $request->getBasePath() . '/';
+      $current_script_path = '';
+      $base_path_with_script = $request->getBaseUrl();
+
+      // If the current request was made with the script name (eg, index.php) in
+      // it, then extract it, making sure the leading / is gone, and a trailing /
+      // is added, to allow simple string concatenation with other parts.
+      if (!empty($base_path_with_script)) {
+        $script_name = $request->getScriptName();
+        if (str_contains($base_path_with_script, $script_name)) {
+          $current_script_path = ltrim(substr($script_name, strlen($current_base_path)), '/') . '/';
+        }
       }
     }
 
