2dfee07da33a6e3612603a8f46c9eaf19097788b
Spokje <39382-Spokje@users.noreply.drupalcode.org>
2025-01-03 10:42:01 +0100

    sleep 0.5s

--- core/modules/path/tests/src/Functional/PathAliasTest.php
+++ core/modules/path/tests/src/Functional/PathAliasTest.php
@@ -70,9 +70,9 @@ class PathAliasTest extends PathTestBase {
     // Visit the system path for the node and confirm a cache entry is
     // created.
     \Drupal::cache('data')->deleteAll();
-    $this->assertEmpty(\Drupal::cache('data')->get('preload-paths:' . $edit['path[0][value]']));
     // Make sure the path is not converted to the alias.
     $this->drupalGet(trim($edit['path[0][value]'], '/'), ['alias' => TRUE]);
+    usleep(500000);
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:' . $edit['path[0][value]']), 'Cache entry was created.');
 
     // Visit the alias for the node and confirm a cache entry is created.
@@ -80,6 +80,7 @@ class PathAliasTest extends PathTestBase {
     // @todo Remove this once https://www.drupal.org/node/2480077 lands.
     Cache::invalidateTags(['rendered']);
     $this->drupalGet(trim($edit['alias[0][value]'], '/'));
+    usleep(500000);
     $this->assertNotEmpty(\Drupal::cache('data')->get('preload-paths:' . $edit['path[0][value]']), 'Cache entry was created.');
   }
 
