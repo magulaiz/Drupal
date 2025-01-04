3cce992234a8996845f44a2d55f1e24aa27871b5
nicxvan <nic@nlightened.net>
2025-01-04 09:19:01 -0500

    Rename OverrideHook

--- core/modules/workspaces/src/Hook/EntityOperations.php
+++ core/modules/workspaces/src/Hook/EntityOperations.php
@@ -12,7 +12,7 @@ use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Entity\RevisionableInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Hook\Attribute\Hook;
-use Drupal\Core\Hook\Attribute\OverrideHook;
+use Drupal\Core\Hook\Attribute\ReOrderHook;
 use Drupal\Core\Hook\Order;
 use Drupal\Core\Hook\OrderBefore;
 use Drupal\content_moderation\Hook\ContentModerationHooks;
@@ -77,7 +77,7 @@ class EntityOperations {
    * Implements hook_entity_presave().
    */
   #[Hook('entity_presave', order: Order::First)]
-  #[OverrideHook('entity_presave',
+  #[ReOrderHook('entity_presave',
     class: ContentModerationHooks::class,
     method: 'entityPresave',
     order: new OrderBefore(['workspaces'])
