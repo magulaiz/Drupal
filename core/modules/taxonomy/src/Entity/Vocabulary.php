5597c629e2075d0ddc7196d57e84d131f0b13788
Ramprassad <ramprassad.suresh@materialplus.io>
2025-01-04 15:11:49 +0000

    3496939 - PHPCS fixes

--- core/modules/taxonomy/src/Entity/Vocabulary.php
+++ core/modules/taxonomy/src/Entity/Vocabulary.php
@@ -111,7 +111,7 @@ class Vocabulary extends ConfigEntityBundleBase implements VocabularyInterface {
   /**
    * {@inheritdoc}
    */
-  public function getName() {
+  public function getName(): string {
     return $this->name ?? '';
   }
 
