9d233a2ef514080877f706ad47e2beb7767fe698
mondrake <mondrake.org@gmail.com>
2025-01-05 11:31:50 +0100

    first stab

---
+++ core/lib/Drupal/Core/Test/PhpUnitTestDiscovery.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Drupal\Core\Test;
+
+use Composer\Autoload\ClassLoader;
+use Drupal\Core\Test\Exception\MissingGroupException;
+use PHPUnit\TextUI\Configuration\Builder;
+use PHPUnit\TextUI\Configuration\TestSuiteBuilder;
+
+/**
+ * Discovers available tests using the PHPUnit API.
+ *
+ * @internal
+ */
+class PhpUnitTestDiscovery {
+
+  /**
+   * The class loader.
+   */
+  protected ClassLoader $classLoader;
+
+  /**
+   * The app root.
+   */
+  private string $root;
+
+  private array $map = [
+    'PHPUnit-FunctionalJavascript' => 'functional-javascript',
+    'PHPUnit-Functional' => 'functional',
+    'PHPUnit-Kernel' => 'kernel',
+    'PHPUnit-Unit' => 'unit',
+    'PHPUnit-Build' => 'build',
+  ];
+
+  private array $reverseMap;
+
+  /**
+   * Constructs a new test discovery.
+   *
+   * @param string $root
+   *   The app root.
+   * @param class-string $class_loader
+   *   The class loader. Normally Composer's ClassLoader, as included by the
+   *   front controller, but may also be decorated.
+   */
+  public function __construct($root, $class_loader) {
+    $this->root = $root;
+    $this->classLoader = $class_loader;
+    $this->reverseMap = array_flip($this->map);
+  }
+
+  public function getTestClasses($extension = NULL, array $types = [], ?string $directory = NULL) {
+    $args = ['--configuration', $this->root . \DIRECTORY_SEPARATOR . 'core'];
+
+    if (!empty($types)) {
+      $tmp = [];
+      foreach ($types as $i) {
+        $tmp[] = $this->map[$i] ?? $i;
+      }
+      $args[] = '--testsuite=' . implode(',', $tmp);
+    }
+
+    $phpUnitConfiguration = (new Builder())->build($args);
+    $phpUnitTestSuite = (new TestSuiteBuilder())->build($phpUnitConfiguration);
+
+    $list = [];
+    foreach ($phpUnitTestSuite->tests() as $testSuite) {
+      foreach ($testSuite->tests() as $testClass) {
+#        $groups = array_filter($testClass->groups(), function (string $value): bool {
+#          return !str_starts_with($value, '__');
+#        });
+
+        $reflection = new \ReflectionClass($testClass->name());
+        $docComment = $reflection->getDocComment();
+
+        $annotations = [];
+        // Look for annotations, allow an arbitrary amount of spaces before the
+        // * but nothing else.
+        preg_match_all('/^[ ]*\* (\@[^\s]*)(.*)/m', $docComment, $matches);
+        if (isset($matches[1])) {
+          foreach ($matches[1] as $key => $annotation) {
+            $annotations[$annotation][] = substr($matches[2][$key], 1);
+          }
+        }
+
+        $groups = $annotations['@group'] ?? [];
+        // @todo add failure if missing
+
+        if (isset($annotations['@coversDefaultClass'][0])) {
+          $description = sprintf('Tests %s.', $annotations['@coversDefaultClass'][0]);
+        }
+        else {
+          $description = TestDiscovery::parseTestClassSummary($docComment);
+        }
+
+        $item = [
+          'name' => $testClass->name(),
+          'group' => $groups[0],
+          'groups' => $groups,
+          'type' => $this->reverseMap[$testSuite->name()] ?? $testSuite->name(),
+          'description' => $description,
+        ];
+
+        foreach ($groups as $group) {
+          $list[$group][$testClass->name()] = $item;
+        }
+      }
+    }
+
+    // Sort the groups and tests within the groups by name.
+    uksort($list, 'strnatcasecmp');
+    foreach ($list as &$tests) {
+      uksort($tests, 'strnatcasecmp');
+    }
+
+    return $list;
+  }
+
+}
---
+++ core/tests/Drupal/KernelTests/Core/Test/PhpUnitApiGetTestClassesTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\KernelTests\Core\Test;
+
+use Drupal\Core\Test\PhpUnitTestDiscovery;
+use Drupal\Core\Test\TestDiscovery;
+use Drupal\KernelTests\KernelTestBase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\TextUI\Configuration\Builder;
+use PHPUnit\TextUI\Configuration\TestSuiteBuilder;
+
+/**
+ * Tests ::getTestClasses() between TestDiscovery and PhpPUnitTestDiscovery.
+ *
+ * PhpPUnitTestDiscovery uses PHPUnit API to build the list of test classes,
+ * while TestDiscovery uses Drupal legacy code.
+ *
+ * @group TestSuites
+ * @group Test
+ * @group #slow
+ */
+class PhpUnitApiGetTestClassesTest extends KernelTestBase {
+
+  /**
+   * Checks that Drupal legacy and PHPUnit API based discoveries are equal.
+   */
+  #[DataProvider('argumentsProvider')]
+  public function testEquality(array $suites): void {
+    $testDiscovery = new TestDiscovery(
+      $this->container->getParameter('app.root'),
+      $this->container->get('class_loader')
+    );
+    $internalList = $testDiscovery->getTestClasses(NULL, $suites);
+
+    $phpUnitTestDiscovery = new PhpUnitTestDiscovery(
+      $this->container->getParameter('app.root'),
+      $this->container->get('class_loader')
+    );
+    $phpUnitList = $phpUnitTestDiscovery->getTestClasses(NULL, $suites);
+
+    $this->assertEquals(array_filter($internalList), $phpUnitList);
+  }
+
+  /**
+   * Provides test data to ::testEquality.
+   */
+  public static function argumentsProvider(): \Generator {
+    yield 'All tests' => ['suites' => []];
+    yield 'Testsuite: functional-javascript' => ['suites' => ['PHPUnit-FunctionalJavascript']];
+    yield 'Testsuite: functional' => ['suites' => ['PHPUnit-Functional']];
+    yield 'Testsuite: kernel' => ['suites' => ['PHPUnit-Kernel']];
+    yield 'Testsuite: unit' => ['suites' => ['PHPUnit-Unit']];
+    yield 'Testsuite: build' => ['suites' => ['PHPUnit-Build']];
+  }
+
+}
