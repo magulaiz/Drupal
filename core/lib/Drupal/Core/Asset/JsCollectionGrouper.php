0bbdc3516ad73fec32b8165d771383cd9292dcdf
catch <6915-catch@users.noreply.drupalcode.org>
2025-01-06 09:51:14 +0000

    Cs

--- core/lib/Drupal/Core/Asset/JsCollectionGrouper.php
+++ core/lib/Drupal/Core/Asset/JsCollectionGrouper.php
@@ -28,7 +28,7 @@ class JsCollectionGrouper implements AssetCollectionGrouperInterface {
     $index = -1;
     foreach ($js_assets as $item) {
       switch ($item['type']) {
-      case 'file':
+        case 'file':
           // Group file items if their 'preprocess' flag is TRUE.
           // Help ensure maximum reuse of aggregate files by only grouping
           // together items that share the same 'group' value.
