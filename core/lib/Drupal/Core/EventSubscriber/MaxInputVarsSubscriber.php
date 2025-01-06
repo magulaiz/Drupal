842006aa8d5064ce2ef87d94d4e94f23abc8b5ed
Doug Green <douggreen@douggreenconsulting.com>
2025-01-06 10:38:07 -0500

    Add MaxInputVarsSubscriber to detect PHP error.

---
+++ core/lib/Drupal/Core/EventSubscriber/MaxInputVarsSubscriber.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\Core\EventSubscriber;
+
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Max Input Var subscriber to catch error early.
+ */
+class MaxInputVarsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a new MaxInputVarSubscriber.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    // This should happen first.
+    $events[KernelEvents::REQUEST][] = ['onRequest', 2000];
+    return $events;
+  }
+
+  /**
+   * Check for the max input vars early in the request.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The request event.
+   */
+  public function onRequest(RequestEvent $event) {
+    $error = error_get_last();
+    if (!empty($error['message']) && strstr($error['message'], 'Input variables exceeded') !== FALSE) {
+      // Add the error message as a Drupal Error.
+      $this->messenger->addError($error['message']);
+
+      // Clear the existing output.
+      ob_clean();
+    }
+  }
+
+}
