5873f8db8844762e3b0490d453d228cb73762426
Bhanu <50266-Bhanu951@users.noreply.drupalcode.org>
2025-01-07 17:53:04 +0530

    Rebased to latest head.

---
+++ core/lib/Drupal/Core/Entity/EntityPermissionProviderInterface.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\Core\Entity;
+
+/**
+ * Provides an interface for entity permission providers.
+ */
+interface EntityPermissionProviderInterface {
+
+  /**
+   * Builds permissions for an entity type.
+   *
+   * @return array[]
+   *   An associative array of permission values, keyed by permission name.
+   *
+   * @see \Drupal\user\PermissionHandlerInterface::getPermissions()
+   */
+  public function buildPermissions();
+
+  /**
+   * Gets the name of the admin permission.
+   *
+   * @return string|null
+   */
+  public function getAdminPermission();
+
+  /**
+   * Gets the name of the collection permission.
+   *
+   * This permission is mostly used for providing access to the 'overview' page
+   * of an entity type. For other kind of listings, for example one that is
+   * provided by a HTTP REST API, the 'view any published' permission should be
+   * used instead.
+   *
+   * @return string|null
+   */
+  public function getCollectionPermission();
+
+  /**
+   * Gets the name of the 'view any|own published' permission.
+   *
+   * @param string $bundle
+   *   A bundle name or the entity type ID if the entity type does not have
+   *   bundles.
+   * @param string $scope
+   *   (optional) Whether the 'view any published' or 'view own published'
+   *   permission name should be returned. Defaults to 'any'.
+   *
+   * @return string|null
+   */
+  public function getViewPermission($bundle, $scope = 'any');
+
+  /**
+   * Gets the name of the 'view any|own unpublished' permission.
+   *
+   * Note that only the 'view own unpublished' permission is provided by Drupal
+   * core, but contrib can implement support for 'view own unpublished' in a
+   * custom permission provider handler.
+   *
+   * @param string $scope
+   *   (optional) Whether the 'view any unpublished' or 'view own unpublished'
+   *   permission name should be returned. Defaults to 'own'.
+   *
+   * @return string|null
+   */
+  public function getViewUnpublishedPermission($scope = 'own');
+
+  /**
+   * Gets the name of the 'edit any|own' permission.
+   *
+   * @param string $bundle
+   *   A bundle name or the entity type ID if the entity type does not have
+   *   bundles.
+   * @param string $scope
+   *   (optional) Whether the 'edit any' or 'edit own' permission name should be
+   *   returned. Defaults to 'any'.
+   *
+   * @return string|null
+   */
+  public function getUpdatePermission($bundle, $scope = 'any');
+
+  /**
+   * Gets the name of the 'delete any|own' permission.
+   *
+   * @param string $bundle
+   *   A bundle name or the entity type ID if the entity type does not have
+   *   bundles.
+   * @param string $scope
+   *   (optional) Whether the 'delete any' or 'delete own' permission name
+   *   should be returned. Defaults to 'any'.
+   *
+   * @return string|null
+   */
+  public function getDeletePermission($bundle, $scope = 'any');
+
+  /**
+   * Gets the name of the 'create' permission.
+   *
+   * @param string $bundle
+   *   A bundle name or the entity type ID if the entity type does not have
+   *   bundles.
+   *
+   * @return string|null
+   */
+  public function getCreatePermission($bundle);
+
+}
--- core/modules/user/tests/src/Unit/PermissionHandlerTest.php
+++ core/modules/user/tests/src/Unit/PermissionHandlerTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\user\Unit;
 
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Controller\ControllerResolverInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\Extension;
 use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
@@ -14,6 +18,7 @@ use Drupal\user\PermissionHandler;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStreamWrapper;
+use Prophecy\Argument;
 
 /**
  * Tests the permission handler.
@@ -31,6 +36,13 @@ class PermissionHandlerTest extends UnitTestCase {
    * @var \Drupal\user\PermissionHandler
    */
   protected $permissionHandler;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface|\Prophecy\Prophecy\ProphecyInterface
+   */
+  protected $entityTypeManager;
 
   /**
    * The mocked module handler.
@@ -61,6 +73,7 @@ class PermissionHandlerTest extends UnitTestCase {
 
     $this->stringTranslation = new TestTranslationManager();
     $this->callableResolver = $this->createMock('Drupal\Core\Utility\CallableResolver');
+    $this->entityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
   }
 
   /**
@@ -134,7 +147,7 @@ EOF
 
     $module_extension_list = $this->createMock(ModuleExtensionList::class);
 
-    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list);
+    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list, $this->entityTypeManager);
 
     $actual_permissions = $this->permissionHandler->getPermissions();
     $this->assertPermissions($actual_permissions);
@@ -196,7 +209,7 @@ EOF
       ->method('getModuleList')
       ->willReturn(array_flip($modules));
 
-    $permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list);
+    $permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list, $this->entityTypeManager);
     $actual_permissions = $permissionHandler->getPermissions();
     $this->assertEquals(['access_module_a4', 'access_module_a1', 'access_module_a2', 'access_module_a3'],
       array_keys($actual_permissions));
@@ -261,7 +274,7 @@ EOF
 
     $module_extension_list = $this->createMock(ModuleExtensionList::class);
 
-    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list);
+    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list, $this->entityTypeManager);
 
     $actual_permissions = $this->permissionHandler->getPermissions();
     $this->assertPermissions($actual_permissions);
@@ -306,7 +319,7 @@ EOF
 
     $module_extension_list = $this->createMock(ModuleExtensionList::class);
 
-    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list);
+    $this->permissionHandler = new PermissionHandler($this->moduleHandler, $this->stringTranslation, $this->callableResolver, $module_extension_list, $this->entityTypeManager);
 
     $actual_permissions = $this->permissionHandler->getPermissions();
 
@@ -337,6 +350,45 @@ EOF
     $this->assertEquals('module_c', $actual_permissions['access_module_c']['provider']);
     $this->assertTrue($actual_permissions['access_module_c']['restrict access']);
     $this->assertEquals('module_a', $actual_permissions['access module a via module b']['provider']);
+    $this->assertEquals($actual_permissions['access module a via module b']['provider'], 'module_a');
+  }
+
+  /**
+   * Sets up the entity type manager to be tested.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface[] $definitions
+   *   (optional) An array of entity type definitions.
+   */
+  protected function setUpEntityTypeDefinitions($definitions = []) {
+    $class = $this->getMockClass(EntityInterface::class);
+    foreach ($definitions as $key => $entity_type) {
+      // \Drupal\Core\Entity\EntityTypeInterface::getLinkTemplates() is called
+      // by \Drupal\Core\Entity\EntityManager::processDefinition() so it must
+      // always be mocked.
+      $entity_type->getLinkTemplates()->willReturn([]);
+
+      // Give the entity type a legitimate class to return.
+      $entity_type->getClass()->willReturn($class);
+
+      $definitions[$key] = $entity_type->reveal();
+    }
+
+    $this->entityTypeManager->getDefinition(Argument::cetera())
+      ->will(function ($args) use ($definitions) {
+        $entity_type_id = $args[0];
+        $exception_on_invalid = $args[1];
+        if (isset($definitions[$entity_type_id])) {
+          return $definitions[$entity_type_id];
+        }
+        elseif (!$exception_on_invalid) {
+          return NULL;
+        }
+        else {
+          throw new PluginNotFoundException($entity_type_id);
+        }
+      });
+    $this->entityTypeManager->getDefinitions()->willReturn($definitions);
+
   }
 
 }
