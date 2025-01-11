6c1f146877bd0c1e355de1abba7a977200fd1c49
mondrake <mondrake.org@gmail.com>
2025-01-11 17:51:12 +0100

    first stab at removing deprecation handling

--- core/tests/Drupal/TestTools/Extension/DeprecationBridge/DeprecationHandler.php
+++ core/tests/Drupal/TestTools/Extension/DeprecationBridge/DeprecationHandler.php
@@ -9,9 +9,9 @@ use PHPUnit\Framework\TestCase;
 /**
  * Drupal's PHPUnit extension to manage code deprecation.
  *
- * This class is a replacement for symfony/phpunit-bridge that does not
- * support PHPUnit 10. In the future this extension might be dropped if
- * PHPUnit adds support for all deprecation management needs.
+ * This class is a replacement for symfony/phpunit-bridge that is not
+ * supporting PHPUnit 10 and above. In the future this extension might be
+ * dropped if PHPUnit will support all deprecation management needs.
  *
  * @internal
  */
@@ -28,20 +28,6 @@ final class DeprecationHandler {
    * @var list<string>
    */
   private static array $deprecationIgnorePatterns = [];
-
-  /**
-   * A list of expected deprecation messages.
-   *
-   * @var list<string>
-   */
-  private static array $expectedDeprecations = [];
-
-  /**
-   * A list of deprecation messages collected during test run.
-   *
-   * @var list<string>
-   */
-  private static array $collectedDeprecations = [];
 
   /**
    * This class should not be instantiated.
@@ -131,80 +117,6 @@ final class DeprecationHandler {
   }
 
   /**
-   * Resets the extension.
-   *
-   * The extension should be reset at the beginning of each test run to ensure
-   * matching of expected and actual deprecations.
-   */
-  public static function reset(): void {
-    if (!self::isEnabled()) {
-      return;
-    }
-    self::$expectedDeprecations = [];
-    self::$collectedDeprecations = [];
-  }
-
-  /**
-   * Adds an expected deprecation.
-   *
-   * Tests will expect deprecations during the test execution; at the end of
-   * each test run, collected deprecations are checked against the expected
-   * ones.
-   *
-   * @param string $message
-   *   The expected deprecation message.
-   */
-  public static function expectDeprecation(string $message): void {
-    if (!self::isEnabled()) {
-      return;
-    }
-    self::$expectedDeprecations[] = $message;
-  }
-
-  /**
-   * Returns all expected deprecations.
-   *
-   * @return list<string>
-   *   The expected deprecation messages.
-   */
-  public static function getExpectedDeprecations(): array {
-    if (!self::isEnabled()) {
-      throw new \LogicException(__CLASS__ . ' is not initialized');
-    }
-    return self::$expectedDeprecations;
-  }
-
-  /**
-   * Collects an actual deprecation.
-   *
-   * Tests will expect deprecations during the test execution; at the end of
-   * each test run, collected deprecations are checked against the expected
-   * ones.
-   *
-   * @param string $message
-   *   The actual deprecation message triggered via trigger_error().
-   */
-  public static function collectActualDeprecation(string $message): void {
-    if (!self::isEnabled()) {
-      return;
-    }
-    self::$collectedDeprecations[] = $message;
-  }
-
-  /**
-   * Returns all collected deprecations.
-   *
-   * @return list<string>
-   *   The collected deprecation messages.
-   */
-  public static function getCollectedDeprecations(): array {
-    if (!self::isEnabled()) {
-      throw new \LogicException(__CLASS__ . ' is not initialized');
-    }
-    return self::$collectedDeprecations;
-  }
-
-  /**
    * Determines if an actual deprecation should be ignored.
    *
    * Deprecations that match the patterns included in the ignore file should
@@ -222,33 +134,6 @@ final class DeprecationHandler {
       throw new \RuntimeException(preg_last_error_msg());
     }
     return (bool) $result;
-  }
-
-  /**
-   * Determines if a test case is a deprecation test.
-   *
-   * Deprecation tests are those that are annotated with '@group legacy' or
-   * that have a '#[IgnoreDeprecations]' attribute.
-   *
-   * @param \PHPUnit\Framework\TestCase $testCase
-   *   The test case being executed.
-   */
-  public static function isDeprecationTest(TestCase $testCase): bool {
-    return $testCase->valueObjectForEvents()->metadata()->isIgnoreDeprecations()->isNotEmpty() || self::isTestInLegacyGroup($testCase);
-  }
-
-  /**
-   * Determines if a test case is part of the 'legacy' group.
-   *
-   * @param \PHPUnit\Framework\TestCase $testCase
-   *   The test case being executed.
-   */
-  private static function isTestInLegacyGroup(TestCase $testCase): bool {
-    $groups = [];
-    foreach ($testCase->valueObjectForEvents()->metadata()->isGroup() as $metadata) {
-      $groups[] = $metadata->groupName();
-    }
-    return in_array('legacy', $groups, TRUE);
   }
 
 }
--- core/tests/Drupal/TestTools/PhpUnitCompatibility/PhpUnit11/TestCompatibilityTrait.php
+++ core/tests/Drupal/TestTools/PhpUnitCompatibility/PhpUnit11/TestCompatibilityTrait.php
@@ -10,4 +10,34 @@ namespace Drupal\TestTools\PhpUnitCompatibility\PhpUnit11;
  * @internal
  */
 trait TestCompatibilityTrait {
+
+  /**
+   * Adds an expected deprecation.
+   *
+   * @param string $message
+   *   The expected deprecation message.
+   */
+  public function expectDeprecation(string $message): void {
+    $this->expectUserDeprecationMessageMatches($this->regularExpressionForFormatDescription('%A' . $message . '%A'));
+  }
+
+  private function regularExpressionForFormatDescription(string $string): string {
+    $string = strtr(preg_quote($string, '/'), [
+      '%%' => '%',
+      '%e' => preg_quote(\DIRECTORY_SEPARATOR, '/'),
+      '%s' => '[^\r\n]+',
+      '%S' => '[^\r\n]*',
+      '%a' => '.+?',
+      '%A' => '.*?',
+      '%w' => '\s*',
+      '%i' => '[+-]?\d+',
+      '%d' => '\d+',
+      '%x' => '[0-9a-fA-F]+',
+      '%f' => '[+-]?(?:\d+|(?=\.\d))(?:\.\d+)?(?:[Ee][+-]?\d+)?',
+      '%c' => '.',
+      '%0' => '\x00',
+    ]);
+    return '/^' . $string . '$/s';
+  }
+
 }
