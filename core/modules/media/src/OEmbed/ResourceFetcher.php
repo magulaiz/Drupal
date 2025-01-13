9cb53f9bbd6535459345e81ee9d0b05ff5261fe8
Aurora Norris <69621-aurora-norris@users.noreply.drupalcode.org>
2025-01-13 08:56:58 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: nicxvan <29861-nicxvan@users.noreply.drupalcode.org>

--- core/modules/media/src/OEmbed/ResourceFetcher.php
+++ core/modules/media/src/OEmbed/ResourceFetcher.php
@@ -40,7 +40,7 @@ class ResourceFetcher implements ResourceFetcherInterface {
   ) {
     if (empty($moduleHandler)) {
       $moduleHandler = \Drupal::moduleHandler();
-      @trigger_error('Passing NULL as the $module_handler parameter to ' . __METHOD__ . '() is deprecated in drupal:11.1.0 and is removed from drupal:12.0.0. See https://www.drupal.org/node/3042423', E_USER_DEPRECATED);
+      @trigger_error('Passing NULL as the $module_handler parameter to ' . __METHOD__ . '() is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. See https://www.drupal.org/node/3042423', E_USER_DEPRECATED);
     }
     $this->moduleHandler = $moduleHandler;
   }
