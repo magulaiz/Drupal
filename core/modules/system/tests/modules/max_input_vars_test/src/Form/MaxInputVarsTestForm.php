9549b91368255fcea4364b995e846f85f87f499e
Doug Green <douggreen@douggreenconsulting.com>
2025-01-13 11:51:38 -0500

    Add max_input_vars_test.

---
+++ core/modules/system/tests/modules/max_input_vars_test/src/Form/MaxInputVarsTestForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\max_input_vars_test\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+class MaxInputVarsTestForm extends FormBase implements FormInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    return 'max-input-vars-test-form';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $max_vars = ini_get('max_input_vars');
+    for ($i = 0; $i < $max_vars + 1; $i ++) {
+      $form["box-$i"] = [
+        '#type' => 'checkbox',
+        '#title' => $i,
+        '#default_value' => FALSE,
+      ];
+    }
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => 'Submit',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addError('This should not happen');
+  }
+
+}
---
+++ core/modules/system/tests/src/Functional/Form/MaxInputVarsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Drupal\Tests\BrowserTestBase;
+
+class MaxInputVarsTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'system',
+    'max_input_vars_test',
+  ];
+
+  /**
+   * Theme to enable.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests loading a module.
+   *
+   * @covers ::load
+   */
+  public function testMaxInputVars(): void {
+    $this->drupalGet('/max-input-vars-test');
+    $this->assertSession()->titleEquals('Max Input Vars Test | Drupal');
+
+    $max_vars = ini_get('max_input_vars');
+    $values = [];
+    for ($i = 0; $i < $max_vars + 1; $i++) {
+      $values["box-$i"] = 1;
+    }
+    $this->submitForm($values, 'Submit');
+    $this->assertSession()->statusMessageNotContains('This should not happen', 'error');
+    $this->assertSession()->statusMessageContains('Input variables exceeded', 'error');
+  }
+
+}
