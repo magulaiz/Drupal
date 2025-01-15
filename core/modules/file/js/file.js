955fb6f275ac3d983e4a28664f3444cb83cd2ca2
Oleksandr Syvyi <51502-aleksandr.s@users.noreply.drupalcode.org>
2025-01-15 17:21:58 +0000

    Change concatenation.

--- core/modules/file/js/file.js
+++ core/modules/file/js/file.js
@@ -158,9 +158,7 @@
       function humanFileSize(size) {
         const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
         return (
-          +(size / 1024 ** i).toFixed(2) +
-          ' '+ 
-          ['B', 'KB', 'MB', 'GB', 'TB'][i]
+          `${(size / 1024 ** i).toFixed(2)} ${['B', 'KB', 'MB', 'GB', 'TB'][i]}`
         );
       }
       // Remove any previous errors.
