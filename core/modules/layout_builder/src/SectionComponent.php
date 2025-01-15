6ca022eae24afdcfe07abbf4b1a36801114c55e4
Lucas Hedding <24691-lucashedding@users.noreply.drupalcode.org>
2025-01-15 19:38:46 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/modules/layout_builder/src/SectionComponent.php
+++ core/modules/layout_builder/src/SectionComponent.php
@@ -91,7 +91,7 @@ class SectionComponent implements ThirdPartySettingsInterface {
     $this->uuid = $uuid;
     $this->region = $region;
     $this->configuration = $configuration;
-    // @todo Remove below $additional code when the drupal:11.0.x branch is opened.
+    // @todo Remove below $additional code when the drupal:12.0.x branch is opened.
     // @see https://www.drupal.org/project/drupal/issues/3160644
     // @phpstan-ignore-next-line
     $this->additional = $additional;
