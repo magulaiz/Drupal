d7e441b4f27c098171c421a06145046c807187b5
Oleksandr Syvyi <51502-aleksandr.s@users.noreply.drupalcode.org>
2025-01-15 17:09:37 +0000

    Delete `·`

--- core/modules/file/js/file.js
+++ core/modules/file/js/file.js
@@ -159,7 +159,7 @@
         const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
         return (
           +(size / 1024 ** i).toFixed(2) +
-          ' ' + 
+          ' '+ 
           ['B', 'KB', 'MB', 'GB', 'TB'][i]
         );
       }
