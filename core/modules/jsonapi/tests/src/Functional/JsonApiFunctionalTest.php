fd5c595deb076e0b8c0c00e33290bba72b999739
bjorn <bjorn@swis.nl>
2025-01-17 14:39:53 +0100

    fix: fix test since message changed

--- core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalTest.php
+++ core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalTest.php
@@ -7,6 +7,7 @@ namespace Drupal\Tests\jsonapi\Functional;
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\Url;
 use Drupal\jsonapi\Query\OffsetPage;
+use Drupal\jsonapi_test_collection_count\ResourceType\CountableResourceType;
 use Drupal\node\Entity\Node;
 
 /**
@@ -634,7 +635,7 @@ class JsonApiFunctionalTest extends JsonApiFunctionalTestBase {
     $this->createDefaultContent(61, 5, TRUE, TRUE, static::IS_NOT_MULTILINGUAL);
 
     $this->container->get('module_installer')->install(['jsonapi_test_collection_count']);
-    $this->expectDeprecation("The Drupal\jsonapi_test_collection_count\ResourceType\CountableResourceType::includeCount() is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use ResourceTypeBuildEvent::setCollectionSizeMemberName('count') as a replacement.");
+    $this->expectDeprecation("The Drupal\jsonapi_test_collection_count\ResourceType\CountableResourceType::includeCount() is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use ResourceTypeBuildEvent::setCollectionSizeMemberName('count') as a replacement. See https://www.drupal.org/node/3246951");
     $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article'));
     $this->assertSession()->statusCodeEquals(200);
     $this->assertEquals(61, $collection_output['meta']['count']);
