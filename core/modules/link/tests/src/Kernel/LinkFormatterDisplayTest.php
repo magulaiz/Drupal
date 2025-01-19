5fc540a22fcc02b778201602acbb21ae9a0417c3
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2025-01-19 13:29:05 +0300

    move getTestCases to base

--- core/modules/link/tests/src/Kernel/LinkFormatterDisplayTest.php
+++ core/modules/link/tests/src/Kernel/LinkFormatterDisplayTest.php
@@ -54,53 +54,6 @@ class LinkFormatterDisplayTest extends LinkFormatterDisplayTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function getTestValues(): array {
-    return [
-      // External links.
-      0 => [
-        'uri' => 'http://www.example.com/content/articles/archive?author=John&year=2012#com',
-        // Note that title is empty.
-        'title' => '',
-      ],
-      1 => [
-        'uri' => 'http://www.example.org/content/articles/archive?author=John&year=2012#org',
-        'title' => 'A very long & strange example title that could break the nice layout of the site',
-      ],
-      2 => [
-        'uri' => 'internal:#net',
-        'title' => 'Fragment only',
-      ],
-
-      // Complex internal links.
-      // Result link: '?a[0]=1&a[1]=2'.
-      3 => ['uri' => 'internal:?a[]=1&a[]=2'],
-      4 => ['uri' => 'internal:?b[1]=1&b[0]=2'],
-      // UrlHelper::buildQuery will change order of params.
-      // Result link: '?c[0]=1&c[1]=2&d=3'.
-      5 => ['uri' => 'internal:?c[]=1&d=3&c[]=2'],
-      6 => ['uri' => 'internal:?e[f][g]=h'],
-      7 => ['uri' => 'internal:?i[j[k]]=l'],
-
-      // Query string replace value.
-      // Result link: '?x=1&x=2'.
-      8 => ['uri' => 'internal:?x=1&x=2'],
-      // Result link: '?z[0]=2'.
-      9 => ['uri' => 'internal:?z[0]=1&z[0]=2'],
-
-      // Special empty links.
-      10 => ['uri' => 'route:<none>'],
-      11 => ['uri' => 'route:<none>', 'title' => 'Title, no link'],
-      12 => ['uri' => 'route:<nolink>'],
-      13 => ['uri' => 'route:<nolink>', 'title' => 'Title, no link'],
-      14 => ['uri' => 'route:<button>'],
-      15 => ['uri' => 'route:<button>', 'title' => 'Title, button'],
-
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function getTestCases(): \Generator {
     $default = [
       0 => '<div><a href="http://www.example.com/content/articles/archive?author=John&amp;year=2012#com">http://www.example.com/content/articles/archive?author=John&amp;year=2012#com</a></div>',
