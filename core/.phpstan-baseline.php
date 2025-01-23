21e84e1c4c8a2076dc9f569cb1d09d092badb136
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-01-23 09:00:03 -0500

    Baseline

--- core/.phpstan-baseline.php
+++ core/.phpstan-baseline.php
@@ -35674,12 +35674,6 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/modules/system/tests/modules/module_test/module_test.file.inc',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Function module_test_hook_info\\(\\) has no return type specified\\.$#',
-	'identifier' => 'missingType.return',
-	'count' => 1,
-	'path' => __DIR__ . '/modules/system/tests/modules/module_test/module_test.module',
-];
-$ignoreErrors[] = [
 	'message' => '#^Function module_test_load\\(\\) has no return type specified\\.$#',
 	'identifier' => 'missingType.return',
 	'count' => 1,
