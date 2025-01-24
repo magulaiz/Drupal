2c4044960c503537684303d8d00a68ed5a0330c1
Víctor Dorado <victor.dorado.asensio@gmail.com>
2025-01-24 11:47:27 +0100

    Revert "typo"
    This reverts commit 4c01fe394a90f790cd4b5ae357af13705b0f0bd7.

--- core/modules/options/tests/src/Kernel/OptionsAllowedValuesCacheTest.php
+++ core/modules/options/tests/src/Kernel/OptionsAllowedValuesCacheTest.php
@@ -143,12 +143,12 @@ class OptionsAllowedValuesCacheTest extends FieldKernelTestBase {
       ->getFieldDefinitions('entity_test_with_bundle', 'bundle2')[$this->fieldName];
 
     // Get the allowed values for each bundle.
-    // Get the allowed values for the first bundle.
+    // Set the allowed values for the first bundle.
     $bundle1AllowedValues = options_allowed_values(
       $bundle1FieldDefinition->getFieldStorageDefinition(),
       $entity1
     );
-    // Get the allowed values for the second bundle.
+    // Set the allowed values for the second bundle.
     $bundle2AllowedValues = options_allowed_values(
       $bundle2FieldDefinition->getFieldStorageDefinition(),
       $entity2
