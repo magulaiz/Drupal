7cc45436014ea2673fa49637f1506962350e7a1b
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2025-01-24 21:26:15 +0000

    Edit PathValidatorTest.php Clarified the test comment.

--- core/tests/Drupal/Tests/Core/Path/PathValidatorTest.php
+++ core/tests/Drupal/Tests/Core/Path/PathValidatorTest.php
@@ -445,7 +445,7 @@ class PathValidatorTest extends UnitTestCase {
   }
 
   /**
-   * Tests the getUrlIfValidWithoutAccessCheck() method with an invalid path.
+   * Tests the behaviour of the getUrlIfValidWithoutAccessCheck() method when it receives invalid path parameters.
    *
    * @covers ::getUrlIfValidWithoutAccessCheck
    * @covers ::getUrl
