ce5877f93b565d304bc32015b7a2dc49a14ba72c
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2025-01-27 19:25:12 +0000

    Edit OverviewTerms.php Perfect!

--- core/modules/taxonomy/src/Form/OverviewTerms.php
+++ core/modules/taxonomy/src/Form/OverviewTerms.php
@@ -174,7 +174,7 @@ class OverviewTerms extends FormBase {
     $tree_index = 0;
     $complete_tree = NULL;
 
-    // Filter tree preserving each complete parent of matching terms.
+    // Filter tree preserving all parents of each matching term.
     $this->termFilter = $form_state->getValue('filter');
     if ($this->termFilter) {
 
