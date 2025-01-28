5efe4c5f9b4da4d2d933f5fcb0fe65e8c4abf599
David Bekker <david.bekker@finalist.nl>
2025-01-28 12:29:14 +0100

    Fixed style guide error

--- core/modules/views/tests/src/Kernel/Plugin/ExposedFormRenderTest.php
+++ core/modules/views/tests/src/Kernel/Plugin/ExposedFormRenderTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\views\Kernel\Plugin;
 
-use Drupal\Core\Database\Database;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Form\FormState;
 use Drupal\Core\Path\CurrentPathStack;
