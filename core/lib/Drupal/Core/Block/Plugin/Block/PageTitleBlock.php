7e372eeca124489c5855075e27523dac0bbf2699
Stephen Mulvihill <smulvih2@gmail.com>
2025-01-29 18:15:30 -0800

    Issue #2938129 by smulvih2: PageTitle block is non-functional when not handled directly by \Drupal\block\Plugin\DisplayVariant\BlockPageVariant

--- core/lib/Drupal/Core/Block/Plugin/Block/PageTitleBlock.php
+++ core/lib/Drupal/Core/Block/Plugin/Block/PageTitleBlock.php
@@ -6,6 +6,11 @@ use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\TitleBlockPluginInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Controller\TitleResolverInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Provides a block to display the page title.
@@ -17,14 +22,72 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
     'settings_tray' => FALSE,
   ]
 )]
-class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface {
+class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface, ContainerFactoryPluginInterface {
 
   /**
    * The page title: a string (plain title) or a render array (formatted title).
    *
-   * @var string|array
+   * @var string|array|null
    */
-  protected $title = '';
+  protected $title = NULL;
+
+  /**
+   * The request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * The title resolver.
+   *
+   * @var \Drupal\Core\Controller\TitleResolverInterface
+   */
+  protected $titleResolver;
+
+  /**
+   * Constructs a PageTitleBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   * @param \Drupal\Core\Controller\TitleResolverInterface $title_resolver
+   *   The title resolver.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Request $request, RouteMatchInterface $route_match, TitleResolverInterface $title_resolver) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->request = $request;
+    $this->routeMatch = $route_match;
+    $this->titleResolver = $title_resolver;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('request_stack')->getCurrentRequest(),
+      $container->get('current_route_match'),
+      $container->get('title_resolver')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -45,9 +108,25 @@ class PageTitleBlock extends BlockBase implements TitleBlockPluginInterface {
    * {@inheritdoc}
    */
   public function build() {
+    $route = $this->routeMatch->getRouteObject();
+    $isLayoutBuilder = $route->getOption('_layout_builder');
+    $isLayoutBuilderMove = $route->getDefault('_controller') ?? '';
+    if (($isLayoutBuilder || str_contains($isLayoutBuilderMove, 'MoveBlockController::build')) && $this->inPreview) {
+      $title = 'Title block';
+    }
+    else {
+      if (!is_null($this->title)) {
+        $title = $this->title;
+      }
+      else {
+        $title = $this->titleResolver->getTitle($this->request, $this->routeMatch->getRouteObject());
+      }
+    }
     return [
       '#type' => 'page_title',
-      '#title' => $this->title,
+      // @todo Always use the title resolver directly after
+      //   https://www.drupal.org/node/2359901 is resolved.
+      '#title' => $title,
     ];
   }
 
