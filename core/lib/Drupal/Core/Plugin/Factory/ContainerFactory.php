85b940e53731328ae4120d853b5130837b2094c3
Dave Long <dave@longwaveconsulting.com>
2025-02-01 11:57:30 +0000

    Handle case where class is altered after discovery.

--- core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
+++ core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
@@ -29,10 +29,19 @@ class ContainerFactory extends DefaultFactory {
     if (!is_null($services)) {
       $container = \Drupal::getContainer();
       $args = array_map(fn(Reference $service): object => $container->get((string) $service), $services);
-      if (is_subclass_of($plugin_class, ContainerFactoryPluginInterface::class)) {
-        @trigger_error(sprintf('Implementing ContainerFactoryPluginInterface in the \\%s plugin "%s" is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. The create() method can be safely removed. See https://www.drupal.org/node/7654321', $plugin_class, $plugin_id), E_USER_DEPRECATED);
+      try {
+        $plugin = new $plugin_class(...[$configuration, $plugin_id, $plugin_definition, ...$args]);
+        if (is_subclass_of($plugin_class, ContainerFactoryPluginInterface::class)) {
+          @trigger_error(sprintf('Implementing ContainerFactoryPluginInterface in the \\%s plugin "%s" is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. The create() method can be safely removed. See https://www.drupal.org/node/7654321', $plugin_class, $plugin_id), E_USER_DEPRECATED);
+        }
+        return $plugin;
       }
-      return new $plugin_class(...[$configuration, $plugin_id, $plugin_definition, ...$args]);
+      catch (\ArgumentCountError) {
+        // If the plugin class does not have a constructor that matches the
+        // services, fall back to the previous behavior, but issue a warning.
+        // @todo Trigger autowiring another way for this case?
+        @trigger_error(sprintf('Altering autowired plugins in the \\%s plugin "%s" is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. Update the constructor services list. See https://www.drupal.org/node/7654321', $plugin_class, $plugin_id), E_USER_DEPRECATED);
+      }
     }
 
     // If the plugin provides a factory method, pass the container to it.
