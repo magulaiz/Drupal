c3767f543508e3065ab20041fe7b6ab0970d94c0
Dave Long <dave@longwaveconsulting.com>
2025-02-01 11:26:29 +0000

    Improve message.

--- core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
+++ core/lib/Drupal/Core/Plugin/Factory/ContainerFactory.php
@@ -30,7 +30,7 @@ class ContainerFactory extends DefaultFactory {
       $container = \Drupal::getContainer();
       $args = array_map(fn(Reference $service): object => $container->get((string) $service), $services);
       if (is_subclass_of($plugin_class, ContainerFactoryPluginInterface::class)) {
-        @trigger_error(sprintf('Implementing ContainerFactoryPluginInterface in the plugin "%s" is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. The create() method can be safely removed. See https://www.drupal.org/node/7654321', $plugin_id), E_USER_DEPRECATED);
+        @trigger_error(sprintf('Implementing ContainerFactoryPluginInterface in the \\%s plugin "%s" is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. The create() method can be safely removed. See https://www.drupal.org/node/7654321', $plugin_class, $plugin_id), E_USER_DEPRECATED);
       }
       return new $plugin_class(...[$configuration, $plugin_id, $plugin_definition, ...$args]);
     }
