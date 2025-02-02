0c0d5ff16a2c3f548b95d84807b812f233e88e8a
Sascha Grossenbacher <saschagros@gmail.com>
2025-02-02 13:13:25 +0100

    coding standards

--- core/lib/Drupal/Core/Routing/RoutePreloader.php
+++ core/lib/Drupal/Core/Routing/RoutePreloader.php
@@ -5,7 +5,6 @@ namespace Drupal\Core\Routing;
 use Drupal\Core\State\StateInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\Route;
 
 /**
