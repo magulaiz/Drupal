133eb22b5b327799f1208626bd3ccec32930b0cf
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-02-03 12:46:17 -0500

    Code sniffing

--- core/lib/Drupal/Core/Extension/module.api.php
+++ core/lib/Drupal/Core/Extension/module.api.php
@@ -99,8 +99,6 @@ function hook_hook_info(): array {
  * modules will still need to maintain implementations with the #[LegacyHook]
  * attribute in order to support drupal versions older than 11.2.0.
  *
- * @see https://www.drupal.org/node/3496788
- *
  * Only procedural implementations are supported for this hook.
  *
  * This hook is invoked in \Drupal::moduleHandler()->getImplementationInfo().
