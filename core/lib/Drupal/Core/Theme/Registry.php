c6d1ad83aadc7db38db3d625cea5cf356ff0bcb1
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-02-03 16:11:18 -0500

    Add template

--- core/lib/Drupal/Core/Theme/Registry.php
+++ core/lib/Drupal/Core/Theme/Registry.php
@@ -644,7 +644,7 @@ class Registry implements DestructableInterface {
         // Preprocess variables for all theming hooks. Ensure they are arrays.
         if (!isset($info['preprocess functions']) || !is_array($info['preprocess functions'])) {
           $info['preprocess functions'] = [];
-          $prefixes = [];
+          $prefixes = ['template'];
           if ($type == 'module') {
             // Add template_preprocess_HOOK function, if no initial preprocess
             // callback is defined.
