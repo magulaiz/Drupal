daf7e9e0582f5dd7570dd72dead7e006acc45095
arunsahijpal <arunsahijpal111@gmail.com>
2025-02-04 16:07:38 +0530

    Issue #3499722: Used post_update and batch processing.

--- core/modules/block_content/block_content.post_update.php
+++ core/modules/block_content/block_content.post_update.php
@@ -18,3 +18,48 @@ function block_content_removed_post_updates(): array {
     'block_content_post_update_revision_type' => '11.0.0',
   ];
 }
+
+/**
+ * Removes the "area_text_custom" plugin from block content view configuration.
+ */
+function block_content_post_update_10301(?array &$sandbox = NULL): int {
+  $storage = \Drupal::entityTypeManager()->getStorage('view');
+  $view_ids = $storage->getQuery()->execute();
+
+  // Initialize batch processing if first run.
+  if (!isset($sandbox['total'])) {
+    $sandbox['total'] = count($view_ids);
+    $sandbox['current_index'] = 0;
+  }
+
+  // Process views in chunks of 10.
+  $view_ids = array_slice($view_ids, $sandbox['current_index'], 10);
+  foreach ($view_ids as $view_id) {
+    $view = $storage->load($view_id);
+    if ($view) {
+      $changed = FALSE;
+      foreach ($view->get('display') as $display_id => $display) {
+        if (
+          isset($display['display_options']['empty']['area_text_custom']) &&
+          $display['display_options']['empty']['area_text_custom']['content'] === 'There are no content blocks available.'
+        ) {
+          unset($display['display_options']['empty']['area_text_custom']);
+          $view->set('display.' . $display_id . '.display_options.empty.area_text_custom', NULL);
+          $changed = TRUE;
+        }
+      }
+      if ($changed) {
+        $view->save();
+      }
+    }
+  }
+
+  // Update progress.
+  $sandbox['current_index'] += 10;
+  if ($sandbox['current_index'] < $sandbox['total']) {
+    return t('Updating block content views (@current/@total)', [
+      '@current' => $sandbox['current_index'],
+      '@total' => $sandbox['total'],
+    ]);
+  }
+}
