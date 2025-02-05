cb4971a9f619487fb306cd29183b96671f70f46f
Chris Green <chrisgreen@arizona.edu>
2025-02-04 17:40:17 -0700

    Add code back to see if tests pass again

--- core/modules/file/src/Plugin/Field/FieldWidget/FileWidget.php
+++ core/modules/file/src/Plugin/Field/FieldWidget/FileWidget.php
@@ -546,19 +546,21 @@ class FileWidget extends WidgetBase {
    * @see file_managed_file_submit()
    */
   public static function submit($form, FormStateInterface $form_state) {
-    $button = $form_state->getTriggeringElement();
-    // Go one level up in the form, to the widgets container.
-    $element = NestedArray::getValue($form, array_slice($button['#array_parents'], 0, -1));
-    $field_name = $element['#field_name'];
-    $parents = $element['#field_parents'];
-
     // During the form rebuild, formElement() will create field item widget
     // elements using re-indexed deltas, so clear out FormState::$input to
     // avoid a mismatch between old and new deltas. The rebuilt elements will
     // have #default_value set appropriately for the current state of the field,
     // so nothing is lost in doing this.
-    $submitted_values = NestedArray::getValue($form_state->getValues(), array_slice($button['#parents'], 0, -2));
+    $button = $form_state->getTriggeringElement();
+    $parents = array_slice($button['#parents'], 0, -2);
+    NestedArray::setValue($form_state->getUserInput(), $parents, NULL);
 
+    // Go one level up in the form, to the widgets container.
+    $element = NestedArray::getValue($form, array_slice($button['#array_parents'], 0, -1));
+    $field_name = $element['#field_name'];
+    $parents = $element['#field_parents'];
+
+    $submitted_values = NestedArray::getValue($form_state->getValues(), array_slice($button['#parents'], 0, -2));
     foreach ($submitted_values as $delta => $submitted_value) {
       if (empty($submitted_value['fids'])) {
         unset($submitted_values[$delta]);
