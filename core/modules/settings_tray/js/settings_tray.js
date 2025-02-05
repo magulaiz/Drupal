d52d126011cd0ac2143d0de24a40204833052469
nikathone <29918-nikathone@users.noreply.drupalcode.org>
2025-02-05 09:42:22 -0500

    Using Optional chaining instead of ternary operator.

--- core/modules/settings_tray/js/settings_tray.js
+++ core/modules/settings_tray/js/settings_tray.js
@@ -168,7 +168,7 @@
           instance.options.data.dialogOptions = {};
         }
         instance.options.data.dialogOptions.settingsTrayActiveEditableId =
-          closestSettingsTray ? closestSettingsTray.id : undefined;
+          closestSettingsTray?.id;
         instance.progress = { type: 'fullscreen' };
       });
   }
