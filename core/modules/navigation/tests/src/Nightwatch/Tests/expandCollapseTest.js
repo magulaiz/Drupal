17bde410a5b67c831febc40cc91639c4752f9a60
Pablo López // plopesc <plopesc@gmail.com>
2025-02-06 07:31:10 +0100

    Issue #3504722: Navigation Expand/Collapse logic is not working properly in conjunction with Big Pipe

--- core/modules/navigation/tests/src/Nightwatch/Tests/expandCollapseTest.js
+++ core/modules/navigation/tests/src/Nightwatch/Tests/expandCollapseTest.js
@@ -15,6 +15,7 @@ module.exports = {
     browser
       .drupalInstall()
       .drupalInstallModule('navigation', true)
+      .drupalInstallModule('big_pipe', true)
       .setWindowSize(1220, 800);
   },
   after(browser) {
