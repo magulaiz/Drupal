2857482c2f260ac14a69158229d83b2b7d49c19f
Víctor Dorado <victor.dorado.asensio@gmail.com>
2025-02-06 21:22:04 +0100

    Do not simply silence a test

--- core/modules/file/tests/src/Kernel/FileItemValidationTest.php
+++ core/modules/file/tests/src/Kernel/FileItemValidationTest.php
@@ -106,11 +106,15 @@ class FileItemValidationTest extends KernelTestBase {
         'target_id' => $file->id(),
       ],
     ]);
+    // Enforce the file to be new as file size is checked only for new files.
+    $entity_test->field_test_file->entity->enforceIsNew();
     $result = $entity_test->validate();
-    $this->assertCount(1, $result);
+    $this->assertCount(2, $result);
 
     $this->assertEquals('field_test_file.0', $result->get(0)->getPropertyPath());
-    $this->assertEquals('Only files with the following extensions are allowed: <em class="placeholder">jpg|png</em>.', (string) $result->get(0)->getMessage());
+    $this->assertEquals('The file is <em class="placeholder">2.93 KB</em> exceeding the maximum file size of <em class="placeholder">2 KB</em>.', (string) $result->get(0)->getMessage());
+    $this->assertEquals('field_test_file.0', $result->get(1)->getPropertyPath());
+    $this->assertEquals('Only files with the following extensions are allowed: <em class="placeholder">jpg|png</em>.', (string) $result->get(1)->getMessage());
 
     // Refer to a file that does not exist.
     $entity_test = EntityTest::create([
