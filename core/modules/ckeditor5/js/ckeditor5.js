4bc1992f914827a491bb998e57dc14de03db6531
Vladimir Roudakov <v_roudakov@yahoo.com>
2025-02-09 23:07:29 +1000

    Issue #2741877 by nuez, larowlan, jastraat, mikhailkrainiuk, vakulrai, raman.b, ajv009, timodwhit, gauravvvv, kapilv, BinaryBlock, r_h-l: Nested modals don't work: opening a modal from a modal closes the original. Updated JS linting.

--- core/modules/ckeditor5/js/ckeditor5.js
+++ core/modules/ckeditor5/js/ckeditor5.js
@@ -417,9 +417,9 @@
           editor.ui.view.editable.element
             .closest('.ck-editor')
             .style.setProperty(
-            '--ck-min-height',
-            `${calculateLineHeight(rows)}px`,
-          );
+              '--ck-min-height',
+              `${calculateLineHeight(rows)}px`,
+            );
 
           // CKEditor 4 had a feature to remove the required attribute
           // see: https://www.drupal.org/project/drupal/issues/1954968
@@ -675,8 +675,7 @@
         Drupal.ckeditor5.saveCallback.has(selector)
       ) {
         Drupal.ckeditor5.saveCallback.get(selector)(values);
-      }
-      else {
+      } else {
         Drupal.ckeditor5.saveCallback(values);
       }
     }
@@ -693,8 +692,7 @@
         Drupal.ckeditor5.saveCallback.has(selector)
       ) {
         Drupal.ckeditor5.saveCallback.delete(selector);
-      }
-      else {
+      } else {
         Drupal.ckeditor5.saveCallback = null;
       }
     }
