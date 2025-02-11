a9d966fe7c9e32108b590b41b6c9c2b673951bcc
Alex Pott <alex.a.pott@googlemail.com>
2025-02-11 22:47:46 +0000

    Remove update function

--- core/modules/system/system.post_update.php
+++ core/modules/system/system.post_update.php
@@ -85,10 +85,3 @@ function system_post_update_sdc_uninstall() {
     \Drupal::service('module_installer')->uninstall(['sdc'], FALSE);
   }
 }
-
-/**
- * Rebuild the container to fix HTML in RSS feeds.
- */
-function system_post_update_remove_rss_cdata_subscriber(): void {
-  // Empty update to trigger container rebuild.
-}
