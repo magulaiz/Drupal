3db2f4d333c84afa2ea119d4272c85d402b62c55
Vladimir Roudakov <44088-VladimirAus@users.noreply.drupalcode.org>
2025-02-11 05:36:12 +0000

    Issue #3497021: ConfirmFormInterface::getFormName() serves no purpose in ConfirmFormBase

--- core/lib/Drupal/Core/Entity/EntityConfirmFormBase.php
+++ core/lib/Drupal/Core/Entity/EntityConfirmFormBase.php
@@ -45,11 +45,7 @@ abstract class EntityConfirmFormBase extends EntityForm implements ConfirmFormIn
    * {@inheritdoc}
    */
   public function getFormName(): string {
-    @trigger_error(
-    'ConfirmFormBase::getFormName() is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. 
-    Implementations should remove this method. See https://www.drupal.org/node/3505037',
-    E_USER_DEPRECATED
-    );
+    @trigger_error('ConfirmFormBase::getFormName() is deprecated in drupal:11.2.0 and will be removed in drupal:12.0.0. Implementations should remove this method. See https://www.drupal.org/node/3505037', E_USER_DEPRECATED);
     return 'confirm';
   }
 
