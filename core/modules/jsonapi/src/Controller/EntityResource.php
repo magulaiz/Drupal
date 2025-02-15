05127b5f58f241155eec88f47cdceb0e07c971db
bjorn <bjorn@swis.nl>
2025-02-15 13:58:00 +0100

    feat: When the config entity doesn't exist by id, also check the UUID since that can still overlap

--- core/modules/jsonapi/src/Controller/EntityResource.php
+++ core/modules/jsonapi/src/Controller/EntityResource.php
@@ -1243,13 +1243,14 @@ class EntityResource {
   protected function entityExists(EntityInterface $entity) {
     $entity_storage = $this->entityTypeManager->getStorage($entity->getEntityTypeId());
 
+    $return = FALSE;
     if ($entity instanceof ConfigEntityInterface) {
-      return !empty($entity_storage->loadByProperties([
+      $return = !empty($entity_storage->loadByProperties([
         'id' => $entity->id(),
       ]));
     }
 
-    return !empty($entity_storage->loadByProperties([
+    return $return || !empty($entity_storage->loadByProperties([
       'uuid' => $entity->uuid(),
     ]));
   }
