bc9f4cb910dd3c9151d87b0a8c85a5ce860020b1
nicxvan <nic@nlightened.net>
2025-02-15 23:12:49 -0500

    Language

--- core/modules/language/src/LanguageServiceRegister.php
+++ core/modules/language/src/LanguageServiceRegister.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Reference;
 /**
  * Overrides the language_manager service to point to language's module one.
  */
-class LanguageServices extends ServiceProviderBase {
+class LanguageServiceRegister extends ServiceProviderBase {
 
   const string CONFIG_PREFIX = 'language.entity.';
 
@@ -41,23 +41,6 @@ class LanguageServices extends ServiceProviderBase {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public function alter(ContainerBuilder $container): void {
-    $definition = $container->getDefinition('language_manager');
-    $definition->setClass('Drupal\language\ConfigurableLanguageManager')
-      ->addArgument(new Reference('config.factory'))
-      ->addArgument(new Reference('module_handler'))
-      ->addArgument(new Reference('language.config_factory_override'))
-      ->addArgument(new Reference('request_stack'))
-      ->addArgument(new Reference('cache.bootstrap'));
-    if ($default_language_values = $this->getDefaultLanguageValues()) {
-      $container->setParameter('language.default_values', $default_language_values);
-    }
-
-  }
-
-  /**
    * Checks whether the site is multilingual.
    *
    * @return bool
@@ -75,30 +58,6 @@ class LanguageServices extends ServiceProviderBase {
       return $config_id != $prefix . LanguageInterface::LANGCODE_NOT_SPECIFIED && $config_id != $prefix . LanguageInterface::LANGCODE_NOT_APPLICABLE;
     });
     return count($config_ids) > 1;
-  }
-
-  /**
-   * Gets the default language values.
-   *
-   * @return array|bool
-   *   Returns the default language values for the language configured in
-   *   system.site:default_langcode if the corresponding configuration entity
-   *   exists, otherwise FALSE.
-   */
-  protected function getDefaultLanguageValues(): array|FALSE {
-    $config_storage = BootstrapConfigStorageFactory::get();
-    $system = $config_storage->read('system.site');
-    // In Kernel tests it's possible this code is called before system.site
-    // exists. In such cases behave as though the corresponding language
-    // configuration entity does not exist.
-    if ($system === FALSE) {
-      return FALSE;
-    }
-    $default_language = $config_storage->read(static::CONFIG_PREFIX . $system['default_langcode']);
-    if (is_array($default_language)) {
-      return $default_language;
-    }
-    return FALSE;
   }
 
 }
