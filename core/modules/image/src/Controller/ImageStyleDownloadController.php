6e7a75fbf5667ec5d6c93c0bfac975628df69cc8
drobert <damien.robert@smile.fr>
2025-02-18 08:37:41 +0100

    add interface to ImageStyleDownloadController

--- core/modules/image/src/Controller/ImageStyleDownloadController.php
+++ core/modules/image/src/Controller/ImageStyleDownloadController.php
@@ -22,7 +22,7 @@ use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 /**
  * Defines a controller to serve image styles.
  */
-class ImageStyleDownloadController extends FileDownloadController {
+class ImageStyleDownloadController extends FileDownloadController implements ImageStyleDownloadInterface {
 
   /**
    * The lock backend.
@@ -85,28 +85,7 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Generates a derivative, given a style and image path.
-   *
-   * After generating an image, transfer it to the requesting agent.
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The request object.
-   * @param string $scheme
-   *   The file scheme, defaults to 'private'.
-   * @param \Drupal\image\ImageStyleInterface $image_style
-   *   The image style to deliver.
-   * @param string $required_derivative_scheme
-   *   The required scheme for the derivative image.
-   *
-   * @return \Symfony\Component\HttpFoundation\BinaryFileResponse|\Symfony\Component\HttpFoundation\Response
-   *   The transferred file as response or some error response.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-   *   Thrown when the file request is invalid.
-   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
-   *   Thrown when the user does not have access to the file.
-   * @throws \Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException
-   *   Thrown when the file is still being generated.
+   * {@inheritdoc}
    */
   public function deliver(Request $request, $scheme, ImageStyleInterface $image_style, string $required_derivative_scheme) {
     $target = $request->query->get('file');
@@ -209,17 +188,9 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Checks whether the provided source image exists.
-   *
-   * @param string $image_uri
-   *   The URI for the source image.
-   * @param bool $token_is_valid
-   *   Whether a valid image token was supplied.
-   *
-   * @return bool
-   *   Whether the source image exists.
+   * {@inheritdoc}
    */
-  private function sourceImageExists(string $image_uri, bool $token_is_valid): bool {
+  public function sourceImageExists(string $image_uri, bool $token_is_valid): bool {
     $exists = file_exists($image_uri);
 
     // If the file doesn't exist, we can stop here.
@@ -248,16 +219,7 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Get the file URI without the extension from any conversion image style.
-   *
-   * If the image style converted the image, then an extension has been added
-   * to the original file, resulting in filenames like image.png.jpeg.
-   *
-   * @param string $uri
-   *   The file URI.
-   *
-   * @return string
-   *   The file URI without the extension from any conversion image style.
+   * {@inheritdoc}
    */
   public static function getUriWithoutConvertedExtension(string $uri): string {
     $original_uri = $uri;
@@ -275,17 +237,9 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Factorization of ImageStyleDownloadController::deliver Line 114-124.
-   *
-   * @param string $scheme
-   *   Target Scheme.
-   * @param string $image_uri
-   *   Original image uri.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-   *   Thrown when the file request is invalid.
+   * {@inheritdoc}
    */
-  protected function checkNormalizedScheme($scheme, $image_uri): void {
+  public function checkNormalizedScheme($scheme, $image_uri): void {
 
     if ($this->streamWrapperManager->isValidScheme($scheme)) {
       $normalized_target = $this->streamWrapperManager->getTarget($image_uri);
@@ -301,23 +255,9 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Factorization of ImageStyleDownloadController::deliver Line 139-142.
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   HTTPS request.
-   * @param string $image_uri
-   *   Original image uri.
-   * @param string $scheme
-   *   Target Scheme.
-   * @param string $target
-   *   Target File.
-   * @param \Drupal\image\ImageStyleInterface $image_style
-   *   Image Style.
-   *
-   * @return bool
-   *   ITOK Token ins valid.
+   * {@inheritdoc}
    */
-  protected function checkToken(Request $request, $image_uri, $scheme, $target, ImageStyleInterface $image_style) {
+  public function checkToken(Request $request, $image_uri, $scheme, $target, ImageStyleInterface $image_style) : bool {
     $token = $request->query->get(IMAGE_DERIVATIVE_TOKEN, '');
     $token_is_valid = hash_equals($image_style->getPathToken($image_uri), $token)
       || hash_equals($image_style->getPathToken($scheme . '://' . $target), $token);
@@ -325,21 +265,9 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Factorization of ImageStyleDownloadController::deliver Line 126-152.
-   *
-   * @param \Drupal\image\ImageStyleInterface $image_style
-   *   ImageStyle used.
-   * @param string $scheme
-   *   Target Scheme.
-   * @param string $target
-   *   Target File.
-   * @param bool $token_is_valid
-   *   ITOK token is valid.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-   *   Thrown when the file request is invalid.
+   * {@inheritdoc}
    */
-  protected function authorizedDerivativeGeneration(ImageStyleInterface $image_style, $scheme, $target, $token_is_valid): void {
+  public function authorizedDerivativeGeneration(ImageStyleInterface $image_style, $scheme, $target, $token_is_valid): void {
 
     // Check that the style is defined and the scheme is valid.
     $valid = !empty($image_style) && $this->streamWrapperManager->isValidScheme($scheme);
@@ -367,19 +295,9 @@ class ImageStyleDownloadController extends FileDownloadController {
   }
 
   /**
-   * Factorization of ImageStyleDownloadController::deliver Line 157-165.
-   *
-   * @param bool $token_is_valid
-   *   ITOK token is valid.
-   * @param string $scheme
-   *   Target Scheme.
-   * @param string $derivative_scheme
-   *   Derivative Scheme.
-   *
-   * @return bool
-   *   Scheme is public.
+   * {@inheritdoc}
    */
-  protected function isSchemePublic($token_is_valid, $scheme, $derivative_scheme) {
+  public function isSchemePublic($token_is_valid, $scheme, $derivative_scheme) {
     if ($token_is_valid) {
       $is_public = ($scheme !== 'private');
     }
