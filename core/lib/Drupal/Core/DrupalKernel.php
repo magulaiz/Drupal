2800be7583693f4d19590e8ccc16c1f9c8783ea6
Ghost Of Drupal Past <drupal@negyesi.net>
2025-02-18 03:48:13 +0100

    aaaaaargh

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -1375,6 +1375,8 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
     foreach ($this->serviceYamls['app'] as $filename) {
       $yaml_loader->load($filename);
     }
+    // This variable is for BC only.
+    $alters = [];
     foreach ($container->findTaggedServiceIds('service_provider') as $id => $info) {
       $provider = $container->get($id);
       $class = get_class($provider);
@@ -1385,7 +1387,9 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
         $provider->register($container);
       }
       if ($provider instanceof ServiceModifierInterface) {
-        $this->serviceProviders['app'][$class] = $provider;
+        // $this->serviceProviders['app'][$class] = $provider; once BC is
+        // removed.
+        $alters[$class] = $provider;
       }
     }
     foreach ($this->serviceProviders['app'] as $provider) {
@@ -1399,6 +1403,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
         @trigger_error("Magic naming for $class is deprecated in drupal:11.2.0 and removed in drupal:12.0.0. Service providers are now tagged services with the service_provider tag. See https://www.drupal.org/node/2974194", E_USER_DEPRECATED);
       }
     }
+    $this->serviceProviders['app'] += $alters;
 
     // Register site-specific service overrides.
     foreach ($this->serviceYamls['site'] as $filename) {
