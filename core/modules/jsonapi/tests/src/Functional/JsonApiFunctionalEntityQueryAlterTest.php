293b6a38b621c9197b9fdd960ae780fd7357a4e6
Víctor Dorado <victor.dorado.asensio@gmail.com>
2025-02-20 21:02:37 +0100

    Fix tests

--- core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalEntityQueryAlterTest.php
+++ core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalEntityQueryAlterTest.php
@@ -7,17 +7,16 @@ namespace Drupal\Tests\jsonapi\Functional;
 use Drupal\Component\Serialization\Json;
 
 /**
- * General functional test class.
+ * JSON:API integration test for HOOK_entity_query_alter().
  *
  * @group jsonapi
- * @group legacy
  *
  * @internal
  */
 class JsonApiFunctionalEntityQueryAlterTest extends JsonApiFunctionalTestBase {
 
   /**
-   * @var string[]
+   * {@inheritdoc}
    */
   protected static $modules = [
     'jsonapi_test_entity_query_alter',
@@ -25,39 +24,39 @@ class JsonApiFunctionalEntityQueryAlterTest extends JsonApiFunctionalTestBase {
   ];
 
   /**
-   * Test the GET method.
+   * Test that the entity query alterer works as expected.
    */
-  public function testRead() {
+  public function testEntityQueryAlter(): void {
     $this->createDefaultContent(2, 2, TRUE, TRUE, static::IS_NOT_MULTILINGUAL, FALSE);
-    // 0. HEAD request allows a client to verify that JSON:API is installed and no major error is introduced
-    // by the query alterer.
+    // A HEAD request allows a client to verify that JSON:API is installed
+    // and no major error is introduced by the query alterer.
     $this->httpClient->request('HEAD', $this->buildUrl('/jsonapi/node/article'));
     $this->assertSession()->statusCodeEquals(200);
 
+    // A GET request with no custom_nid query parameter should return all nodes.
     $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article'));
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertEquals(2, count($collection_output['data']));
+    $this->assertCount(2, $collection_output['data']);
 
+    // A GET request with custom_nid = 1 should return only the node with nid 1.
     $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article', [
       'query' => [
-        'sort' => 'drupal_internal__nid',
         'custom_nid' => 1,
       ],
     ]));
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertEquals(1, count($collection_output['data']));
+    $this->assertCount(1, $collection_output['data']);
     $this->assertEquals(1, $collection_output['data'][0]['attributes']['drupal_internal__nid']);
 
+    // A GET request with custom_nid = 2 should return only the node with nid 2.
     $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article', [
       'query' => [
-        'sort' => 'drupal_internal__nid',
         'custom_nid' => 2,
       ],
     ]));
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertEquals(1, count($collection_output['data']));
+    $this->assertCount(1, $collection_output['data']);
     $this->assertEquals(2, $collection_output['data'][0]['attributes']['drupal_internal__nid']);
-
   }
 
 }
