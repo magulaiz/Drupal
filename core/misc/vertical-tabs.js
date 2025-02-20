165acb4c3d442a915d6bb3749e593ea3dd742eac
Maximo Mena <maximo.mena@lullabot.com>
2025-02-20 03:18:51 -0700

    Fix lint issues

--- core/misc/vertical-tabs.js
+++ core/misc/vertical-tabs.js
@@ -87,9 +87,12 @@
             const $summary = $that.find('> summary');
             // We're only interested in the summary's first child node to prevent
             // duplicate text in the title.
-            const title = $summary.length && $summary[0].childNodes.length ? $summary[0].firstChild.textContent : '';
+            const title =
+              $summary.length && $summary[0].childNodes.length
+                ? $summary[0].firstChild.textContent
+                : '';
             const verticalTab = new Drupal.verticalTab({
-              title: title,
+              title,
               details: $that,
             });
             tabList.append(verticalTab.item);
