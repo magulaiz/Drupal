3ebc03072081cb61323db33421d2294340f9d997
nicxvan <nic@nlightened.net>
2025-02-20 23:03:26 -0500

    Test update

--- core/modules/system/tests/src/Functional/Hook/HookCollectorPassTest.php
+++ core/modules/system/tests/src/Functional/Hook/HookCollectorPassTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\Tests\system\Functional\Hook;
 
 use Drupal\Tests\BrowserTestBase;
+use Drupal\language\Entity\ConfigurableLanguage;
 
 /**
  * Tests services in .module files.
@@ -44,15 +45,30 @@ class HookCollectorPassTest extends BrowserTestBase {
    * If this is removed then it needs to be moved to a test that installs modules through
    * admin/modules.
    */
-  public function testPreExistingConfigInstall(): void {
+  public function testContainerOutsideFunction(): void {
     $this->drupalLogin($this->adminUser);
 
+    // Install the config test module so that the configuration does actually
+    // exist.
     $this->drupalGet('admin/modules');
     $this->submitForm([
-      'modules[container_no_function][enable]' => TRUE,
+      'modules[config_test][enable]' => TRUE,
     ], 'Install');
+    $this->assertSession()->responseContains('Module <em class="placeholder">Configuration test</em> has been installed.');
 
-    $this->assertSession()->responseContains('Module <em class="placeholder">Container call outside function</em> has been installed.');
+    // Test that collection configuration clashes during a module install are
+    // reported correctly.
+    \Drupal::service('module_installer')->install(['language']);
+    $this->rebuildContainer();
+    ConfigurableLanguage::createFromLangcode('fr')->save();
+    \Drupal::languageManager()
+      ->getLanguageConfigOverride('fr', 'config_test.dynamic.dotted.default')
+      ->set('label', 'Je suis Charlie')
+      ->save();
+
+    $this->drupalGet('admin/modules');
+    $this->submitForm(['modules[config_install_fail_test][enable]' => TRUE], 'Install');
+    $this->assertSession()->responseContains('Unable to install Configuration install fail test, <em class="placeholder">config_test.dynamic.dotted.default, language/fr/config_test.dynamic.dotted.default</em> already exist in active configuration.');
   }
 
 }
