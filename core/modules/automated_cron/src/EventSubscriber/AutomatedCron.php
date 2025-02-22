049756a5bb83c532956b72c8dd8cdd76e04ec644
Kent Richards <22302-kentr@users.noreply.drupalcode.org>
2025-02-22 09:43:00 -0700

    Issue #3483996: Apply suggested changes, add test

--- core/modules/automated_cron/src/EventSubscriber/AutomatedCron.php
+++ core/modules/automated_cron/src/EventSubscriber/AutomatedCron.php
@@ -3,7 +3,6 @@
 namespace Drupal\automated_cron\EventSubscriber;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\CronInterface;
 use Drupal\Core\State\StateInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
@@ -15,42 +14,18 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class AutomatedCron implements EventSubscriberInterface {
 
   /**
-   * The cron service closure.
-   */
-  protected \Closure $cronClosure;
-
-  /**
    * The cron configuration.
    *
    * @var \Drupal\Core\Config\Config
    */
   protected $config;
 
-  /**
-   * The state key value store.
-   *
-   * @var \Drupal\Core\State\StateInterface
-   */
-  protected $state;
-
-  /**
-   * Constructs a new automated cron runner.
-   *
-   * @param \Drupal\Core\CronInterface|\Closure $cron
-   *   The cron service.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory.
-   * @param \Drupal\Core\State\StateInterface $state
-   *   The state key-value store service.
-   */
-  public function __construct(CronInterface|\Closure $cron, ConfigFactoryInterface $config_factory, StateInterface $state) {
-    if ($cron instanceof CronInterface) {
-      @trigger_error('Passing the $cron argument as an instance of type \Drupal\Core\CronInterface to ' . __METHOD__ . '() is deprecated in drupal:11.2.0 and type \Closure is required in drupal:12.0.0. Pass type \Closure instead. See https://www.drupal.org/node/3484001', E_USER_DEPRECATED);
-      $cron = fn($cron) => $cron;
-    }
-    $this->cronClosure = $cron;
+  public function __construct(
+    protected \Closure $cron_closure,
+    ConfigFactoryInterface $config_factory,
+    protected StateInterface $state,
+  ) {
     $this->config = $config_factory->get('automated_cron.settings');
-    $this->state = $state;
   }
 
   /**
@@ -64,7 +39,7 @@ class AutomatedCron implements EventSubscriberInterface {
     if ($interval > 0) {
       $cron_next = $this->state->get('system.cron_last', 0) + $interval;
       if ((int) $event->getRequest()->server->get('REQUEST_TIME') > $cron_next) {
-        $this->getCron()->run();
+        ($this->cron_closure)()->run();
       }
     }
   }
@@ -77,10 +52,6 @@ class AutomatedCron implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents(): array {
     return [KernelEvents::TERMINATE => [['onTerminate', 100]]];
-  }
-
-  protected function getCron(): CronInterface {
-    return ($this->cronClosure)();
   }
 
 }
