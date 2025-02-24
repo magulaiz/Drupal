191aeb406ba7086c5b185649c85378b81ec44926
bjorn <bjorn@swis.nl>
2025-02-24 12:07:11 +0100

    fix: MigrateContactCategoryTest should assume NULL not ''

--- core/modules/contact/tests/src/Kernel/Migrate/MigrateContactCategoryTest.php
+++ core/modules/contact/tests/src/Kernel/Migrate/MigrateContactCategoryTest.php
@@ -60,7 +60,7 @@ class MigrateContactCategoryTest extends MigrateDrupal6TestBase {
   public function testContactCategory(): void {
     $this->assertEntity('website_feedback', 'Website feedback', ['admin@example.com'], NULL, 0);
     $this->assertEntity('some_other_category', 'Some other category', ['test@example.com'], 'Thanks for contacting us, we will reply ASAP!', 1);
-    $this->assertEntity('a_category_much_longer_than_th', 'A category much longer than thirty two characters', ['fortyninechars@example.com'], '', 2);
+    $this->assertEntity('a_category_much_longer_than_th', 'A category much longer than thirty two characters', ['fortyninechars@example.com'], NULL, 2);
 
     // Test there are no duplicated roles.
     $contact_forms = [
