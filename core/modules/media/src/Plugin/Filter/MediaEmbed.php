de3fec62390707189a3b59e1b35e931ae51b276d
Ahmad Nawaz <ahmad.nawaz@paddle.be>
2025-02-27 19:19:42 +0500

    Removed un-used use statement

--- core/modules/media/src/Plugin/Filter/MediaEmbed.php
+++ core/modules/media/src/Plugin/Filter/MediaEmbed.php
@@ -7,7 +7,6 @@ use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceEntityFormatter;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
