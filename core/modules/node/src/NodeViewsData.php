4b9f81dfd59f6cb6440c361b3a9ff222606f550d
prabha14546 <prabhavathi@qed42.com>
2025-02-27 11:14:07 +0530

    Added suggested change for triggering error.

--- core/modules/node/src/NodeViewsData.php
+++ core/modules/node/src/NodeViewsData.php
@@ -36,10 +36,18 @@ class NodeViewsData extends EntityViewsData {
    * @param \Drupal\Core\Extension\ModuleExtensionList|null $moduleExtensionList
    *   The module extension list.
    */
-  public function __construct(EntityTypeInterface $entity_type, SqlEntityStorageInterface $storage_controller, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, TranslationInterface $translation_manager, EntityFieldManagerInterface $entity_field_manager, protected ?ModuleExtensionList $moduleExtensionList = NULL) {
+  public function __construct(
+    EntityTypeInterface $entity_type,
+    SqlEntityStorageInterface $storage_controller,
+    EntityTypeManagerInterface $entity_type_manager,
+    ModuleHandlerInterface $module_handler,
+    TranslationInterface $translation_manager,
+    EntityFieldManagerInterface $entity_field_manager,
+    protected ?ModuleExtensionList $moduleExtensionList = NULL
+    ) {
     parent::__construct($entity_type, $storage_controller, $entity_type_manager, $module_handler, $translation_manager, $entity_field_manager);
     if ($this->moduleExtensionList === NULL) {
-      @trigger_error('Calling ' . __METHOD__ . '() without the $moduleExtensionList argument is deprecated in drupal:10.4.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3493129', E_USER_DEPRECATED);
+      @trigger_error('Calling ' . __METHOD__ . '() without the $moduleExtensionList argument is deprecated in drupal:11.2.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3493129', E_USER_DEPRECATED);
       $this->moduleExtensionList = \Drupal::service('extension.list.module');
     }
   }
