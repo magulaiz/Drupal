bb9e6394ce691e3d4a50a7d9795be78eeb0f422f
nicxvan <29861-nicxvan@users.noreply.drupalcode.org>
2025-02-28 03:44:32 +0000

    Correct comments

--- core/lib/Drupal/Core/Hook/Attribute/LegacyModuleImplementsAlter.php
+++ core/lib/Drupal/Core/Hook/Attribute/LegacyModuleImplementsAlter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Drupal\Core\Hook\Attribute;
 
 /**
- * Defines a LegacyModuleImplementsAlter attribute object.
+ * Defines a LegacyModuleImplementsAlter object.
  *
  * This allows contrib and core to maintain legacy hook_module_implements_alter
  * alongside the new attribute-based ordering. This means that a contrib module
