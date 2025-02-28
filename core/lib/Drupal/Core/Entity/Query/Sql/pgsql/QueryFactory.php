2c84a79ab0564307e3ff65cf9239561ea6627cd6
David Bekker <david.bekker@finalist.nl>
2025-02-28 14:49:15 +0100

    Removing the deprecations from the classes

--- core/lib/Drupal/Core/Entity/Query/Sql/pgsql/QueryFactory.php
+++ core/lib/Drupal/Core/Entity/Query/Sql/pgsql/QueryFactory.php
@@ -4,8 +4,6 @@ namespace Drupal\Core\Entity\Query\Sql\pgsql;
 
 use Drupal\Core\Entity\Query\Sql\QueryFactory as BaseQueryFactory;
 
-@trigger_error('\Drupal\Core\Entity\Query\Sql\pgsql\QueryFactory is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. The PostgreSQL override of the entity query has been moved to the pgsql module. See https://www.drupal.org/node/3488580', E_USER_DEPRECATED);
-
 /**
  * PostgreSQL specific entity query implementation.
  *
