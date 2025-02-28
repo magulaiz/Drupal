4af03f4984153e17cdd9362c3e42a01aa2297d74
Brandon Lira <brandonlira98@gmail.com>
2025-02-28 08:00:21 -0300

    Issue #3447550: Remove unsupported @note

--- core/lib/Drupal/Core/Field/FieldItemInterface.php
+++ core/lib/Drupal/Core/Field/FieldItemInterface.php
@@ -252,10 +252,9 @@ interface FieldItemInterface extends ComplexDataInterface {
    * @return array
    *   A list of default settings, keyed by the setting name.
    *
-   * @note
-   *   Setting names defined by this method must not duplicate the setting names
-   *   returned by this plugin's implementation of defaultFieldSettings(), as both
-   *   lists of settings are merged.
+   * Setting names defined by this method must not duplicate the setting names
+   * returned by this plugin's implementation of defaultFieldSettings(), as both
+   * lists of settings are merged.
    */
   public static function defaultStorageSettings();
 
@@ -265,10 +264,9 @@ interface FieldItemInterface extends ComplexDataInterface {
    * @return array
    *   A list of default settings, keyed by the setting name.
    *
-   * @note
-   *   Setting names defined by this method must not duplicate the setting names
-   *   returned by this plugin's implementation of defaultStorageSettings(), as both
-   *   lists of settings are merged.
+   * Setting names defined by this method must not duplicate the setting names
+   * returned by this plugin's implementation of defaultStorageSettings(), as both
+   * lists of settings are merged.
    */
   public static function defaultFieldSettings();
 
