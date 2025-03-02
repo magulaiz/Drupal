ad55bf715bd4539fd79ef170ae25c13c3423002f
Andreas Hennings <andreas@dqxtech.net>
2025-03-03 00:40:38 +0100

    Change how empty arrays are removed.

--- core/lib/Drupal/Core/Hook/HookCollectorPass.php
+++ core/lib/Drupal/Core/Hook/HookCollectorPass.php
@@ -136,9 +136,21 @@ class HookCollectorPass implements CompilerPassInterface {
         // Remove the hook implementation for the defined class, method, and
         // hook.
         unset($implementations[$removeHook->hook][$module][$removeHook->class][$removeHook->method]);
+        // Remove empty arrays, after the entry was removed.
+        // Hook implementation removal is expected to be rare, therefore it will
+        // be faster to do it like this than cleaning the entire tree
+        // afterwards.
+        if (empty($implementations[$removeHook->hook][$module][$removeHook->class])) {
+          unset($implementations[$removeHook->hook][$module][$removeHook->class]);
+          if (empty($implementations[$removeHook->hook][$module])) {
+            unset($implementations[$removeHook->hook][$module]);
+            if (empty($implementations[$removeHook->hook])) {
+              unset($implementations[$removeHook->hook]);
+            }
+          }
+        }
       }
     }
-    $implementations = static::removeEmptyArraysRecursively($implementations);
 
     // Loop over all ReOrderHook attributes and gather order information
     // before registering the hooks. This must happen after all collection,
@@ -602,34 +614,6 @@ class HookCollectorPass implements CompilerPassInterface {
       }
     }
     $container->setParameter('hook_implementations_map', $map);
-  }
-
-  /**
-   * Removes empty sub-arrays recursively, preserving keys.
-   *
-   * This is different from NestedArray::filter() or regular array_filter():
-   *   - It only removes empty arrays, not other empty-ish values.
-   *   - It reliably removes empty parent arrays, after all children have been
-   *     removed. See https://www.drupal.org/project/drupal/issues/3381640.
-   *
-   * @param array $array
-   *   Array which may or may not contain other arrays as values.
-   *
-   * @return array
-   *   Filtered array, with empty arrays removed.
-   *
-   * @see \Drupal\Component\Utility\NestedArray::filter()
-   */
-  protected static function removeEmptyArraysRecursively(array $array): array {
-    foreach ($array as $key => $value) {
-      if (is_array($value)) {
-        $value = self::removeEmptyArraysRecursively($value);
-        if ($value === []) {
-          unset($array[$key]);
-        }
-      }
-    }
-    return $array;
   }
 
 }
