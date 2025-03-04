c74bee74c60a05261a18d52de477083ee2059b58
Riyas N R <riyas.nr@qed42.com>
2025-03-04 17:51:48 +0530

    Issue #3509141: Add post update hook and test coverage for update hook

---
+++ core/modules/views/tests/src/Functional/Update/AriaLabelUpdateTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\views\Functional\Update;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+use Drupal\views\Entity\View;
+
+/**
+ * Tests the upgrade path for adding aria labels to views field handler.
+ *
+ * @group Update
+ *
+ * @see views_post_update_add_aria_label()
+ */
+class AriaLabelUpdateTest extends UpdatePathTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles(): void {
+    $this->databaseDumpFiles = [
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-10.3.0.filled.standard.php.gz',
+    ];
+  }
+
+  /**
+   * Tests that numeric argument plugins are updated properly.
+   */
+  public function testAriaLabelPostUpdate(): void {
+    $view = View::load('files');
+    $data = $view->toArray();
+    $this->assertArrayHasKey('alter', $data['display']['default']['display_options']['fields']['filename']);
+    $this->assertArrayNotHasKey('aria_label', $data['display']['default']['display_options']['fields']['filename']['alter']);
+
+    $this->runUpdates();
+
+    $view = View::load('files');
+    $data = $view->toArray();
+    $this->assertEquals('', $data['display']['default']['display_options']['fields']['filename']['alter']['aria_label']);
+
+  }
+
+}
