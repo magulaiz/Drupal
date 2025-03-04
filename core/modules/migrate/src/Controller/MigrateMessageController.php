2ecb2404d812405c06cf3adf5c62a791bb1f6f51
David Bekker <david.bekker@finalist.nl>
2025-03-04 16:30:44 +0100

    Converted the migrate message controller to using a condition object

--- core/modules/migrate/src/Controller/MigrateMessageController.php
+++ core/modules/migrate/src/Controller/MigrateMessageController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\DatabaseConnectionRefusedException;
 use Drupal\Core\Database\DatabaseNotFoundException;
+use Drupal\Core\Database\Query\SelectInterface;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
@@ -189,10 +190,7 @@ class MigrateMessageController extends ControllerBase {
     $query->leftJoin($map_table, 'map', 'msg.source_ids_hash = map.source_ids_hash');
     $query->fields('msg');
     $query->fields('map');
-    $filter = $this->buildFilterQuery($request);
-    if (!empty($filter['where'])) {
-      $query->where($filter['where'], $filter['args']);
-    }
+    $this->addFilterToQuery($request, $query);
     $result = $query
       ->limit(50)
       ->orderByHeader($header)
@@ -240,52 +238,65 @@ class MigrateMessageController extends ControllerBase {
   /**
    * Builds a query for migrate message administration.
    *
+   * This method retrieves the session-based filters from the request and
+   * applies them to the provided query object. If no filters are present, the
+   * query is left unchanged.
+   *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The request.
-   *
-   * @return array|null
-   *   An associative array with keys 'where' and 'args' or NULL if there were
-   *   no filters set.
+   * @param \Drupal\Core\Database\Query\SelectInterface $query
+   *   The database query.
    */
-  protected function buildFilterQuery(Request $request): ?array {
+  protected function addFilterToQuery(Request $request, SelectInterface &$query): void {
     $session_filters = $request->getSession()->get('migration_messages_overview_filter', []);
     if (empty($session_filters)) {
-      return NULL;
+      return;
     }
 
-    // Build query.
-    $where = $args = [];
+    // Build the condition.
+    $condition_and = $query->getConnection()->condition('AND');
+    $condition_and_used = FALSE;
     foreach ($session_filters as $filter) {
-      $filter_where = [];
-
+      $condition_or = $query->getConnection()->condition('OR');
+      $condition_or_used = FALSE;
       switch ($filter['type']) {
         case 'array':
+          $values = [];
           foreach ($filter['value'] as $value) {
-            $filter_where[] = $filter['where'];
-            $args[] = $value;
+            if ($filter['field'] == 'msg.level') {
+              $values[] = (int) $value;
+            }
+            else {
+              $values[] = $value;
+            }
+          }
+          if (!empty($values)) {
+            $condition_or->condition($filter['field'], $values, 'IN');
+            $condition_or_used = TRUE;
           }
           break;
 
         case 'string':
-          $filter_where[] = $filter['where'];
-          $args[] = '%' . $filter['value'] . '%';
+          if (!empty($filter['value'])) {
+            $condition_or->condition($filter['field'], '%' . $filter['value'] . '%');
+            $condition_or_used = TRUE;
+          }
           break;
 
         default:
-          $filter_where[] = $filter['where'];
-          $args[] = $filter['value'];
+          if (!empty($filter['value'])) {
+            $condition_or->condition($filter['field'], $filter['value']);
+            $condition_or_used = TRUE;
+          }
       }
-
-      if (!empty($filter_where)) {
-        $where[] = '(' . implode(' OR ', $filter_where) . ')';
+      if ($condition_or_used) {
+        $condition_and->condition($condition_or);
+        $condition_and_used = TRUE;
       }
     }
-    $where = !empty($where) ? implode(' AND ', $where) : '';
-
-    return [
-      'where' => $where,
-      'args' => $args,
-    ];
+    if ($condition_and_used) {
+      $query->condition($condition_and);
+    }
   }
 
   /**
