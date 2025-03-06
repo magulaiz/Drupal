f9f1f2ba0f4b6534d80a7ce9c50dbb4fc5106846
catch <6915-catch@users.noreply.drupalcode.org>
2025-03-06 10:20:41 +0000

    Add extra cache context.

--- core/modules/node/src/NodeGrantDatabaseStorage.php
+++ core/modules/node/src/NodeGrantDatabaseStorage.php
@@ -121,6 +121,7 @@ class NodeGrantDatabaseStorage implements NodeGrantDatabaseStorageInterface {
     // know it for a fact.
     $set_cacheability = function (AccessResult $access_result) use ($operation) {
       $access_result->addCacheContexts(['user.node_grants:' . $operation]);
+      $access_result->addCacheContexts(['user.roles.authenticated']);
       if ($operation !== 'view') {
         $access_result->setCacheMaxAge(0);
       }
