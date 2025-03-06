20c84038f8bec0117f2153e11113e0e1d04e0056
znerol <znerol@63999.no-reply.drupal.org>
2025-03-06 15:17:54 +0100

    Use StringTranslationTrait in MailerHooks

--- core/modules/mailer/src/Hook/MailerHooks.php
+++ core/modules/mailer/src/Hook/MailerHooks.php
@@ -4,11 +4,14 @@ namespace Drupal\mailer\Hook;
 
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Hook implementations for mailer.
  */
 class MailerHooks {
+
+  use StringTranslationTrait;
 
   /**
    * Implements hook_help().
@@ -18,8 +21,8 @@ class MailerHooks {
     switch ($route_name) {
       case 'help.page.mailer':
         $output = '';
-        $output .= '<h3>' . t('About') . '</h3>';
-        $output .= '<p>' . t('The Mailer module provides an experimental API to build and deliver email messages based on Symfony mailer component. For more information, see the <a href=":mailer">online documentation for the Mailer module</a>.', [
+        $output .= '<h3>' . $this->t('About') . '</h3>';
+        $output .= '<p>' . $this->t('The Mailer module provides an experimental API to build and deliver email messages based on Symfony mailer component. For more information, see the <a href=":mailer">online documentation for the Mailer module</a>.', [
           ':mailer' => 'https://www.drupal.org/docs/core-modules-and-themes/experimental-extensions/experimental-modules/mailer',
         ]) . '</p>';
         return $output;
