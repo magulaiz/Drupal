a37b0bb3f5a1b5027bd006927cf8f5b27d41dd74
Michael Strelan <mstrelan@gmail.com>
2025-03-07 13:27:32 +1000

    Test coverage

--- core/modules/system/tests/modules/image_test/src/Plugin/ImageToolkit/TestToolkit.php
+++ core/modules/system/tests/modules/image_test/src/Plugin/ImageToolkit/TestToolkit.php
@@ -253,7 +253,11 @@ class TestToolkit extends ImageToolkitBase {
    *   IMAGETYPE_* constant (e.g. IMAGETYPE_JPEG, IMAGETYPE_PNG, etc.).
    */
   protected static function supportedTypes() {
-    return [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF];
+    $types = [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF];
+    if (\Drupal::state()->get('image_test.avif_enabled', FALSE)) {
+      $types[] = IMAGETYPE_AVIF;
+    }
+    return $types;
   }
 
   /**
