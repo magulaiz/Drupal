bd5f2f3deff14fa480c5be3e00779e08241531d8
David Cameron <david@cadeyrn.us>
2025-03-08 14:33:58 -0600

    Added the fix

--- core/misc/machine-name.js
+++ core/misc/machine-name.js
@@ -229,7 +229,7 @@
         // Preview the machine name in realtime when the human-readable name
         // changes, but only if there is no machine name yet; i.e., only upon
         // initial creation, not when editing.
-        if ($target[0].value === '') {
+        if (machine === '') {
           // Listen to the 'change' and 'input' events that are fired
           // immediately as the user is typing for faster response time. This is
           // safe because the event handler doesn't include any slow
