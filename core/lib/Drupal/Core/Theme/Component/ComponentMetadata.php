4da0a170e60ed219863e1ca87c8f4b3257be22c6
Pierre Rudloff <contact@rudloff.pro>
2025-03-10 23:30:24 +0100

    readonly property should be assigned in constructor

--- core/lib/Drupal/Core/Theme/Component/ComponentMetadata.php
+++ core/lib/Drupal/Core/Theme/Component/ComponentMetadata.php
@@ -125,7 +125,7 @@ class ComponentMetadata {
     $this->group = $metadata_info['group'] ?? $this->t('All Components');
 
     // Save the schemas.
-    $this->parseSchemaInfo($metadata_info);
+    $this->schema = $this->parseSchemaInfo($metadata_info);
     $this->slots = $metadata_info['slots'] ?? [];
   }
 
@@ -137,7 +137,7 @@ class ComponentMetadata {
    *
    * @throws \Drupal\Core\Render\Component\Exception\InvalidComponentException
    */
-  private function parseSchemaInfo(array $metadata_info): void {
+  private function parseSchemaInfo(array $metadata_info): ?array {
     if (empty($metadata_info['props'])) {
       if ($this->mandatorySchemas) {
         throw new InvalidComponentException(sprintf('The component "%s" does not provide schema information. Schema definitions are mandatory for components declared in modules. For components declared in themes, schema definitions are only mandatory if the "enforce_prop_schemas" key is set to "true" in the theme info file.', $metadata_info['id']));
@@ -155,8 +155,10 @@ class ComponentMetadata {
       $schema['additionalProperties'] = FALSE;
       // All props should also support "object" this allows deferring rendering
       // in Twig to the render pipeline.
-      $this->addObjectToSchemaRecursive($schema);
+      $schema = $this->addObjectToSchemaRecursive($schema);
     }
+
+    return $schema;
   }
 
   /**
