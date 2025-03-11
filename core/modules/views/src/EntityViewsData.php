sloaninnovations/selectbaseballteams_d11

Issue #2737619: Views entity reference relationships must join on langcode for translatable entities

core/modules/views/src/EntityViewsData.php
@@ -348,6 +348,12 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
             ['field' => 'deleted', 'value' => 0, 'numeric' => TRUE],
           ],
         ];
+        if ($data_table && $field_definition->isTranslatable()) {
+          $data[$table]['table']['join'][$views_base_table]['extra'][] = [
+            'field' => $entity_keys['langcode'],
+            'left_field' => $entity_keys['langcode'],
+          ];
+        }
 
         if ($revisionable) {
           $revision_table = $table_mapping->getDedicatedRevisionTableName($field_definition->getFieldStorageDefinition());
@@ -361,6 +367,12 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
               ['field' => 'deleted', 'value' => 0, 'numeric' => TRUE],
             ],
           ];
+          if ($revision_data_table && $field_definition->isTranslatable()) {
+            $data[$revision_table]['table']['join'][$views_revision_base_table]['extra'][] = [
+              'field' => $entity_keys['langcode'],
+              'left_field' => $entity_keys['langcode'],
+            ];
+          }
         }
       }
     }

magulaiz/Drupal

core/modules/views/src/EntityViewsData.php
@@ -365,6 +365,12 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
             ['field' => 'deleted', 'value' => 0, 'numeric' => TRUE],
           ],
         ];
+        if ($data_table && $field_storage_definition->isTranslatable()) {
+          $data[$table]['table']['join'][$views_base_table]['extra'][] = [
+            'field' => $entity_keys['langcode'],
+            'left_field' => $entity_keys['langcode'],
+          ];
+        }
 
         if ($revisionable) {
           $revision_table = $table_mapping->getDedicatedRevisionTableName($field_storage_definition);
@@ -378,6 +384,12 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
               ['field' => 'deleted', 'value' => 0, 'numeric' => TRUE],
             ],
           ];
+          if ($revision_data_table && $field_storage_definition->isTranslatable()) {
+            $data[$revision_table]['table']['join'][$views_revision_base_table]['extra'][] = [
+              'field' => $entity_keys['langcode'],
+              'left_field' => $entity_keys['langcode'],
+            ];
+          }
         }
       }
     }