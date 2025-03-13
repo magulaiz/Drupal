c98ac67abba26a1c2f046a5df5a823b9c9fc1e56
catch <6915-catch@users.noreply.drupalcode.org>
2025-03-13 11:31:28 +0000

    Send all tags at once to registerCacheTagsForPreload

--- core/lib/Drupal/Core/Cache/DatabaseBackend.php
+++ core/lib/Drupal/Core/Cache/DatabaseBackend.php
@@ -137,11 +137,13 @@ class DatabaseBackend implements CacheBackendInterface {
     // cache tags for all returned cache items for preloading, this allows the
     // cache tag service to optimize cache tag lookups.
     if ($this->checksumProvider instanceof CacheTagsChecksumPreloadInterface) {
+      $tags_for_preload = [];
       foreach ($result as $item) {
         if ($item->tags) {
-          $this->checksumProvider->registerCacheTagsForPreload(explode(' ', $item->tags));
+          $tags_for_preload[] = explode(' ', $item->tags);
         }
       }
+      $this->checksumProvider->registerCacheTagsForPreload(array_merge(...$tags_for_preload));
     }
     $cache = [];
     foreach ($result as $item) {
