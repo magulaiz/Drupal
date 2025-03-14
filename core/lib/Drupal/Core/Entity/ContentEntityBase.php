4c5f4f76a3b543bc1d372d0bdea6f6aba4e7da59
David Bekker <david.bekker@finalist.nl>
2025-03-14 12:58:40 +0100

    Remove change to the class ContentEntityBase

--- core/lib/Drupal/Core/Entity/ContentEntityBase.php
+++ core/lib/Drupal/Core/Entity/ContentEntityBase.php
@@ -345,14 +345,13 @@ abstract class ContentEntityBase extends EntityBase implements \IteratorAggregat
    * {@inheritdoc}
    */
   public function isDefaultRevision($new_value = NULL) {
-    $current_value = $this->isDefaultRevision;
-    // New entities should always ensure at least one default revision exists,
-    // creating an entity without a default revision is an invalid state.
-    if (isset($new_value) && (!$this->isNew() || $new_value === TRUE)) {
+    $return = $this->isDefaultRevision;
+    if (isset($new_value)) {
       $this->isDefaultRevision = (bool) $new_value;
     }
-
-    return (bool) $current_value;
+    // New entities should always ensure at least one default revision exists,
+    // creating an entity without a default revision is an invalid state.
+    return $this->isNew() || $return;
   }
 
   /**
