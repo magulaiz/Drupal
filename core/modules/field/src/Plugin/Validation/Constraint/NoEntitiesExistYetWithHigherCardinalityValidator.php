6a35c1d1657cf884b3b212c0185e98349c4a693e
bjorn <bjorn@swis.nl>
2025-03-14 15:37:33 +0100

    build: fix missing return type

--- core/modules/field/src/Plugin/Validation/Constraint/NoEntitiesExistYetWithHigherCardinalityValidator.php
+++ core/modules/field/src/Plugin/Validation/Constraint/NoEntitiesExistYetWithHigherCardinalityValidator.php
@@ -20,7 +20,7 @@ class NoEntitiesExistYetWithHigherCardinalityValidator extends ConstraintValidat
   ) {
   }
 
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container): self {
     return new static(
       $container->get('entity_type.manager')
     );
