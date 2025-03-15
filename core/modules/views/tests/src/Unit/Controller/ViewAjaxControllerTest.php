8cad40476135f48884d2e2019272430315bdaa81
Pierre Rudloff <contact@rudloff.pro>
2025-03-15 13:59:23 +0100

    Fix unit tests

--- core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
+++ core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Drupal\Tests\views\Unit\Controller;
 
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Path\PathValidatorInterface;
 use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Render\Renderer;
+use Drupal\Core\Url;
 use Drupal\Core\Utility\CallableResolver;
+use Drupal\Core\Utility\UnroutedUrlAssemblerInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\views\Ajax\ViewAjaxResponse;
 use Drupal\views\Controller\ViewAjaxController;
@@ -103,6 +106,20 @@ class ViewAjaxControllerTest extends UnitTestCase {
       ->disableOriginalConstructor()
       ->getMock();
     $this->redirectDestination = $this->createMock('\Drupal\Core\Routing\RedirectDestinationInterface');
+    $this->pathValidator = $this->createMock(PathValidatorInterface::class);
+    $this->pathValidator->expects($this->any())
+      ->method('getUrlIfValid')
+      /*
+       * Use a callback because container is not initialized yet
+       * so we can't generate a URL object right now.
+       */
+      ->willReturnCallback(function () {
+        return Url::fromUserInput('/foo');
+      });
+    $unroutedUrlAssembler = $this->createMock(UnroutedUrlAssemblerInterface::class);
+    $unroutedUrlAssembler->expects($this->any())
+      ->method('assemble')
+      ->willReturn('/foo');
 
     $this->viewAjaxController = new ViewAjaxController($this->viewStorage, $this->executableFactory, $this->renderer, $this->currentPath, $this->redirectDestination, $this->pathValidator);
 
@@ -129,6 +146,8 @@ class ViewAjaxControllerTest extends UnitTestCase {
     );
     $container = new ContainerBuilder();
     $container->set('renderer', $this->renderer);
+    $container->set('path.validator', $this->pathValidator);
+    $container->set('unrouted_url_assembler', $unroutedUrlAssembler);
     \Drupal::setContainer($container);
     // Not initialized in Unit tests.
     $GLOBALS['base_path'] = '/';
@@ -219,7 +238,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
 
     $this->assertSame($response->getView(), $executable);
 
-    $this->assertViewResultCommand($response);
+    $this->assertViewResultCommand($response, 1);
 
     // Test that the ajax controller for Views contains the
     // Drupal Settings.
@@ -256,7 +275,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
 
     $this->assertSame($response->getView(), $executable);
 
-    $this->assertViewResultCommand($response);
+    $this->assertViewResultCommand($response, 1);
   }
 
   /**
@@ -294,7 +313,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $response = $this->viewAjaxController->ajaxView($request);
     $this->assertInstanceOf(ViewAjaxResponse::class, $response);
 
-    $this->assertViewResultCommand($response);
+    $this->assertViewResultCommand($response, 1);
   }
 
   /**
@@ -315,7 +334,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $response = $this->viewAjaxController->ajaxView($request);
     $this->assertInstanceOf(ViewAjaxResponse::class, $response);
 
-    $this->assertViewResultCommand($response);
+    $this->assertViewResultCommand($response, 1);
   }
 
   /**
@@ -335,7 +354,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $response = $this->viewAjaxController->ajaxView($request);
     $this->assertInstanceOf(ViewAjaxResponse::class, $response);
 
-    $this->assertViewResultCommand($response);
+    $this->assertViewResultCommand($response, 1);
   }
 
   /**
@@ -374,7 +393,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $this->assertEquals('scrollTop', $commands[0]['command']);
     $this->assertEquals('.js-view-dom-id-' . $dom_id, $commands[0]['selector']);
 
-    $this->assertViewResultCommand($response, 1);
+    $this->assertViewResultCommand($response, 2);
 
     $this->assertInstanceOf(CacheableMetadata::class, $response->getCacheableMetadata());
   }
