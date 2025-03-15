70c9c16cfdfa0079f918ea869062ded48f09d48f
Sascha Grossenbacher <saschagros@gmail.com>
2025-03-15 12:31:56 +0100

    fix bugs

--- core/lib/Drupal/Core/Entity/ContentEntityStorageBase.php
+++ core/lib/Drupal/Core/Entity/ContentEntityStorageBase.php
@@ -793,13 +793,6 @@ abstract class ContentEntityStorageBase extends EntityStorageBase implements Con
       $revisions = $flipped_revision_ids;
     }
 
-    // Ensure that the returned array is ordered the same as the original
-    // $ids array if this was passed in and remove any invalid IDs.
-    if ($revision_ids) {
-      $flipped_ids = array_intersect_key(array_flip($revision_ids), $revisions);
-      $revisions = array_replace($flipped_ids, $revisions);
-    }
-
     return $revisions;
   }
 
@@ -884,7 +877,7 @@ abstract class ContentEntityStorageBase extends EntityStorageBase implements Con
 
     // Use the loaded revision instead of default one to check for data change.
     if (!$entity->isNew() && !$entity->getOriginal() && !$entity->wasDefaultRevision()) {
-      $original = $this->loadRevision($entity->getLoadedRevisionId());
+      $original = $this->loadRevisionUnchanged($entity->getLoadedRevisionId());
       $entity->setOriginal($original);
     }
 
--- core/lib/Drupal/Core/Entity/KeyValueStore/KeyValueContentEntityStorage.php
+++ core/lib/Drupal/Core/Entity/KeyValueStore/KeyValueContentEntityStorage.php
@@ -82,6 +82,9 @@ class KeyValueContentEntityStorage extends KeyValueEntityStorage implements Cont
     return NULL;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function resetRevisionCache(array $revision_ids): void {}
 
 }
