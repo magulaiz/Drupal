0cda27440af1debb23a6bb161c74a9bf943274d2
David Cameron <david@cadeyrn.us>
2025-03-16 21:30:04 -0500

    Check access to the reset password page in login warning

---
+++ core/modules/user/tests/modules/user_disable_password_reset/src/RouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\user_disable_password_reset;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Disables access to the password reset page.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    $route = $collection->get('user.pass');
+    $route->setRequirement('_access', 'FALSE');
+  }
+
+}
