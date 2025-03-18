5918481df43057e97774c0cf06a586f4d8c445f2
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-03-18 13:54:14 -0400

    Set Attribute for lists

--- core/lib/Drupal/Core/Template/TwigExtension.php
+++ core/lib/Drupal/Core/Template/TwigExtension.php
@@ -789,6 +789,18 @@ class TwigExtension extends AbstractExtension {
    *   The element with the given sanitized attribute's value.
    */
   public function setAttribute(array $element, string $name, mixed $value = NULL): array {
+    if (!\is_array($element)) {
+      return $element;
+    }
+    if (\array_is_list($element)) {
+      foreach ($element as $index => $item) {
+        if (!\is_array($item)) {
+          continue;
+        }
+        $element[$index] = $this->setAttribute($item, $name, $value);
+      }
+      return $element;
+    }
     $element['#attributes'] = AttributeHelper::mergeCollections(
       $element['#attributes'] ?? [],
       new Attribute([$name => $value])
@@ -796,7 +808,6 @@ class TwigExtension extends AbstractExtension {
 
     // Make sure element gets rendered again.
     unset($element['#printed']);
-
     return $element;
   }
 
