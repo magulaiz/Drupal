9a70ef0a332a5dec0715cabef7956dfb5d606f03
quietone <quietone@2572884.no-reply.drupal.org>
2025-03-18 19:59:01 +1300

    respond to feedback

--- core/lib/Drupal/Core/Config/Schema/Mapping.php
+++ core/lib/Drupal/Core/Config/Schema/Mapping.php
@@ -166,8 +166,8 @@ class Mapping extends ArrayElement {
     $all_type_definitions = $typed_data_manager->getDefinitions();
     $possible_type_definitions = array_intersect_key($all_type_definitions, array_fill_keys($possible_types, TRUE));
     // TRICKY: \Drupal\Core\Config\TypedConfigManager::getDefinition() does the
-    // necessary resolving, but TypedConfigManager::getDefinitions() does not!
-    // 🤷‍♂️.
+    // necessary resolving, but TypedConfigManager::getDefinitions() does not
+    // 🤷‍♂️!
     // @see \Drupal\Core\Config\TypedConfigManager::getDefinitionWithReplacements()
     // @see ::getValidKeys()
     $valid_keys_per_type = [];
