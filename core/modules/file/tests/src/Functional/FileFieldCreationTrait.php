e0b2d046793329c82c568efb962f491f49bd256f
Christopher Lewis <christopher.lewis@brandcode.de>
2025-03-20 09:10:03 +0100

    Revert out of scope changes

--- core/modules/file/tests/src/Functional/FileFieldCreationTrait.php
+++ core/modules/file/tests/src/Functional/FileFieldCreationTrait.php
@@ -6,7 +6,6 @@ namespace Drupal\Tests\file\Functional;
 
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\field\Entity\FieldConfig;
-use Drupal\field\FieldStorageConfigInterface;
 
 /**
  * Provides methods for creating file fields.
@@ -33,7 +32,7 @@ trait FileFieldCreationTrait {
    * @return \Drupal\field\FieldStorageConfigInterface
    *   The file field.
    */
-  protected function createFileField($name, $entity_type, $bundle, $storage_settings = [], $field_settings = [], $widget_settings = []): FieldStorageConfigInterface {
+  protected function createFileField($name, $entity_type, $bundle, $storage_settings = [], $field_settings = [], $widget_settings = []) {
     $field_storage = FieldStorageConfig::create([
       'entity_type' => $entity_type,
       'field_name' => $name,
@@ -62,7 +61,7 @@ trait FileFieldCreationTrait {
    * @param array $widget_settings
    *   A list of widget settings that will be added to the widget defaults.
    */
-  protected function attachFileField($name, $entity_type, $bundle, $field_settings = [], $widget_settings = []): void {
+  protected function attachFileField($name, $entity_type, $bundle, $field_settings = [], $widget_settings = []) {
     $field = [
       'field_name' => $name,
       'label' => $name,
