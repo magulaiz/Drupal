5ffc34e6e44af259a6a0eb7e2745c7d0de500626
nicxvan <29861-nicxvan@users.noreply.drupalcode.org>
2025-03-20 03:24:18 +0000

    Spelling

--- core/lib/Drupal/Core/Hook/Attribute/FormAlter.php
+++ core/lib/Drupal/Core/Hook/Attribute/FormAlter.php
@@ -33,7 +33,7 @@ class FormAlter extends Hook {
    *   parameter is omitted, the class must have an __invoke() method, which is
    *   taken as the hook implementation.
    * @param string|null $module
-   *   (optional) The module this implementation is for. This allows one modul
+   *   (optional) The module this implementation is for. This allows one module
    *   to implement a hook on behalf of another module. Defaults to the module
    *   the implementation is in.
    */
