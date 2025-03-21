8a4754f07a158a552a142fd73a1623c62f815f4c
Adam Bramley <adam.bramley@previousnext.com.au>
2025-03-21 11:42:39 +1100

    Fix double loop

--- core/modules/node/src/Plugin/views/wizard/Node.php
+++ core/modules/node/src/Plugin/views/wizard/Node.php
@@ -277,18 +277,13 @@ class Node extends WizardPluginBase {
     $tag_fields = [];
     foreach ($bundles as $bundle) {
       $display = $this->entityDisplayRepository->getFormDisplay($this->entityTypeId, $bundle);
-      $taxonomy_fields = array_filter($this->entityFieldManager->getFieldDefinitions($this->entityTypeId, $bundle), function (FieldDefinitionInterface $field_definition) {
-        return $field_definition->getType() == 'entity_reference' && $field_definition->getSetting('target_type') == 'taxonomy_term';
+      $tag_fields += array_filter($this->entityFieldManager->getFieldDefinitions($this->entityTypeId, $bundle), function (FieldDefinitionInterface $field_definition) use ($display) {
+        if ($field_definition->getType() == 'entity_reference' && $field_definition->getSetting('target_type') == 'taxonomy_term') {
+          $widget = $display->getComponent($field_definition->getName());
+          return isset($widget['type']) && $widget['type'] == 'entity_reference_autocomplete_tags';
+        }
+        return FALSE;
       });
-      foreach ($taxonomy_fields as $field_name => $field) {
-        $tag_fields += array_filter($this->entityFieldManager->getFieldDefinitions($this->entityTypeId, $bundle), function (FieldDefinitionInterface $field_definition) use ($display) {
-          if ($field_definition->getType() == 'entity_reference' && $field_definition->getSetting('target_type') == 'taxonomy_term') {
-            $widget = $display->getComponent($field_definition->getName());
-            return isset($widget['type']) && $widget['type'] == 'entity_reference_autocomplete_tags';
-          }
-          return FALSE;
-        });
-      }
     }
     if (!empty($tag_fields)) {
       // If there is more than one "tag-like" taxonomy field available to
