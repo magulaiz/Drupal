03b60ecac8b520cc01eda64eb001d6f486574b37
Mike McGovern <mcgovernm@566364.no-reply.drupal.org>
2025-03-21 11:19:20 -0400

    Fixing return on shouldMaintainIndexTable().

--- core/modules/taxonomy/src/Hook/TaxonomyEntityHooks.php
+++ core/modules/taxonomy/src/Hook/TaxonomyEntityHooks.php
@@ -32,6 +32,9 @@ class TaxonomyEntityHooks {
    */
   protected function shouldMaintainIndexTable(): bool {
     $taxonomy_config = $this->configFactory->get('taxonomy.settings');
+    if($taxonomy_config->get('maintain_index_table') == NULL){
+      return FALSE;
+    }
     return $taxonomy_config->get('maintain_index_table');
   }
 
