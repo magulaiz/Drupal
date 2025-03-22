00785fac818122458a0006a64803a90118345c81
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2025-03-22 16:35:22 +0000

    limit IdentifierType cases to the ones used at the moment

--- core/lib/Drupal/Core/Database/Identifier/IdentifierType.php
+++ core/lib/Drupal/Core/Database/Identifier/IdentifierType.php
@@ -8,16 +8,10 @@ namespace Drupal\Core\Database\Identifier;
  * Enumeration of database identifier types.
  */
 enum IdentifierType: string {
+
   case Database = 'database';
   case Schema = 'schema';
-  case Sequence = 'sequence';
   case Table = 'table';
-  case Column = 'column';
-  case Index = 'index';
-
-  case Alias = 'alias';
-
-  case Unknown = 'unknown';
 
   /**
    * Returns the value object class for an identifier type.
