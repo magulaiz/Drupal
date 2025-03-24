41066f626d7f580dc96921c3638203182d57af02
Stephen Mustgrave <smustgrave@gmail.com>
2025-03-24 10:31:34 -0400

    Address feedback

--- core/lib/Drupal/Core/Form/ConfigFormBase.php
+++ core/lib/Drupal/Core/Form/ConfigFormBase.php
@@ -65,13 +65,13 @@ abstract class ConfigFormBase extends FormBase {
    * @return \Drupal\Core\Config\TypedConfigManagerInterface
    *   The typed config manager service.
    *
-   * @deprecated in drupal:11.2.0 and is removed from drupal:12.0.0.
-   *   There is no replacement.
+   * @deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use
+   *   $this->typedConfigManager instead.
    *
-   * @see https://www.drupal.org/project/drupal/issues/3477616
+   * @see https://www.drupal.org/node/3515066
    */
   protected function typedConfigManager() {
-    @trigger_error('typedConfigManager() is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. This method is no longer in use instead $this->typedConfigManager to be used. See https://www.drupal.org/project/drupal/issues/3477616', E_USER_DEPRECATED);
+    @trigger_error( __METHOD__. 'is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use $this->typedConfigManager instead. See https://www.drupal.org/node/3515066', E_USER_DEPRECATED);
     return $this->typedConfigManager;
   }
 
