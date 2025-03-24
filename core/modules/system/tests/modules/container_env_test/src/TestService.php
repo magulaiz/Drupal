45cdcefc401a12e99e15d7d3aee8d1a213d89b8d
Guillaume Astier <astier.gui@gmail.com>
2025-03-24 15:49:17 +0100

    chore: add phpdoc to fix PHPCS

--- core/modules/system/tests/modules/container_env_test/src/TestService.php
+++ core/modules/system/tests/modules/container_env_test/src/TestService.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Drupal\container_env_test;
 
+/**
+ * Service class for testing container env.
+ */
 class TestService {
 
+  /**
+   * Constructs a new TestService instance.
+   */
   public function __construct(public string $parameter) {
   }
 
