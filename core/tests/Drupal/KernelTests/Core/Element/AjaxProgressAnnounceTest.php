5939b40e1849990fab5302771f5fc3cbbd88e440
Jeremy Jackson <jeremy@librarymarket.com>
2025-03-24 16:17:41 -0500

    Create kernel tests for announce settings

---
+++ core/tests/Drupal/KernelTests/Core/Element/AjaxProgressAnnounceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\KernelTest;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Core\Render\Element\RenderElementBase;
+
+/**
+ * Tests AJAX progress screen reader announcement defaults.
+ *
+ * @group system
+ */
+class AjaxProgressAnnounceTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['system', 'user', 'filter'];
+
+  /**
+   * Tests default progress settings for screen reader announcements.
+   */
+  public function testAjaxProgressAnnounceDefaults() {
+    $element = [
+      '#id' => 'ajax-progress-test',
+      '#type' => 'submit',
+      '#ajax' => [
+        'callback' => '::ajaxCallback',
+        'progress' => [],
+      ],
+    ];
+
+    // Simulate preRenderAjaxForm to attach AJAX settings.
+    $element = RenderElementBase::preRenderAjaxForm($element);
+
+    $settings = $element['#attached']['drupalSettings']['ajax'][$element['#id']]['progress'];
+
+    // Ensure default values are set when progress is defined.
+    $this->assertArrayHasKey('announce', $settings);
+    $this->assertArrayHasKey('announceDelay', $settings);
+    $this->assertArrayHasKey('announceIntervalTime', $settings);
+    $this->assertArrayHasKey('announceMessage', $settings);
+
+    // Check default values.
+    $this->assertTrue($settings['announce']);
+    $this->assertEquals(1000, $settings['announceDelay']);
+    $this->assertEquals(2000, $settings['announceIntervalTime']);
+    $this->assertEquals('Busy', (string) $settings['announceMessage']);
+  }
+
+}
