77c600829bc051dc8cea36e6feac9e40afd8ca2c
Pierre Rudloff <prudloff@insite.coop>
2025-03-25 12:04:05 +0100

    Latest fix changes the parameters order again

--- core/modules/views/tests/src/FunctionalJavascript/ExposedFilterAJAXTest.php
+++ core/modules/views/tests/src/FunctionalJavascript/ExposedFilterAJAXTest.php
@@ -115,7 +115,7 @@ class ExposedFilterAJAXTest extends WebDriverTestBase {
     $html = $session->getPage()->getHtml();
     $this->assertStringContainsString('Page Two', $html);
     $this->assertStringNotContainsString('Page One', $html);
-    $this->assertSession()->addressEquals('admin/content?title=Page%20Two&type=All&status=All');
+    $this->assertSession()->addressEquals('admin/content?type=All&status=All&title=Page%20Two');
 
     // Submit bulk actions form to ensure that the previous AJAX submit does not
     // break it.
--- core/modules/views/tests/src/FunctionalJavascript/PaginationAJAXTest.php
+++ core/modules/views/tests/src/FunctionalJavascript/PaginationAJAXTest.php
@@ -112,7 +112,7 @@ class PaginationAJAXTest extends WebDriverTestBase {
     $this->assertNoDuplicateAssetsOnPage();
 
     // Test that no unwanted parameters are added to the URL.
-    $this->assertEquals('?order=changed&sort=asc&status=All&type=All&title=&items_per_page=5&page=2', $link->getAttribute('href'));
+    $this->assertEquals('?status=All&type=All&items_per_page=5&title=&order=changed&sort=asc&page=2', $link->getAttribute('href'));
 
     $this->clickLink('Go to page 3');
     $session_assert->assertWaitOnAjaxRequest();
@@ -196,7 +196,7 @@ class PaginationAJAXTest extends WebDriverTestBase {
     $this->assertNoDuplicateAssetsOnPage();
 
     // Test that no unwanted parameters are added to the URL.
-    $this->assertEquals('?order=changed&sort=asc&status=All&type=All&title=default_value&items_per_page=5&page=0', $link->getAttribute('href'));
+    $this->assertEquals('?status=All&type=All&items_per_page=5&title=default_value&order=changed&sort=asc&page=0', $link->getAttribute('href'));
 
     // Set the title filter to empty string using the exposed pager.
     $page->fillField('title', '');
