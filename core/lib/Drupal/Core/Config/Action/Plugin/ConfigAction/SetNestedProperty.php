f4ce51481dc6e0c2c856e35c495add89469a4acb
Phéna Proxima <adam@phenaproxima.net>
2025-03-25 14:28:25 -0400

    Rename the plugin and make it work more like simpleConfigUpdate

--- core/lib/Drupal/Core/Config/Action/Plugin/ConfigAction/SetNestedProperty.php
+++ core/lib/Drupal/Core/Config/Action/Plugin/ConfigAction/SetNestedProperty.php
@@ -15,11 +15,11 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 #[ConfigAction(
-  id: 'setNested',
-  admin_label: new TranslatableMarkup('Set nested property'),
+  id: 'setProperties',
+  admin_label: new TranslatableMarkup('Set property of a config entity'),
   entity_types: ['*'],
 )]
-final class SetNestedProperty implements ConfigActionPluginInterface, ContainerFactoryPluginInterface {
+final class SetProperties implements ConfigActionPluginInterface, ContainerFactoryPluginInterface {
 
   public function __construct(
     private readonly ConfigManagerInterface $configManager,
@@ -42,24 +42,21 @@ final class SetNestedProperty implements ConfigActionPluginInterface, ContainerF
     assert($entity instanceof ConfigEntityInterface);
 
     assert(is_array($value));
-    $values = array_is_list($value) ? $value : [$value];
+    assert(!array_is_list($value));
 
-    foreach ($values as $value) {
-      $property_name = $value['property_name'];
-      assert(is_string($property_name));
-
-      // The property name has to be a nested property path.
-      if (!str_contains($property_name, '.')) {
-        throw new ConfigActionException("The setNested config action requires a nested property path.");
-      }
+    foreach ($values as $property_name => $value) {
       $parts = explode('.', $property_name);
 
-      $property_value = $entity->get($parts[0]) ?? [];
-      if (!is_array($property_value)) {
-        throw new ConfigActionException('The setNested config action can only work on array values.');
+      $property_value = $entity->get($parts[0]);
+      if (count($parts) > 1) {
+        if (!is_array($property_value)) {
+          throw new ConfigActionException('This config action can only set nested values on arrays.');
+        }
+        NestedArray::setValue($property_value, array_slice($parts, 1), $value['value']);
       }
-
-      NestedArray::setValue($property_value, array_slice($parts, 1), $value['value']);
+      else {
+        $property_value = $value;
+      }
       $entity->set($parts[0], $property_value);
     }
     $entity->save();
