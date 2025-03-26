405409b90ea7debd08cb32f7547e51bc292f24bd
Michael Strelan <mstrelan@gmail.com>
2025-03-26 12:56:13 +1000

    Resize the browser in drupalCreateRole instead

--- core/modules/toolbar/tests/src/Nightwatch/Tests/toolbarTest.js
+++ core/modules/toolbar/tests/src/Nightwatch/Tests/toolbarTest.js
@@ -14,8 +14,6 @@ module.exports = {
   '@tags': ['core'],
   before(browser) {
     browser
-      // We need to resize the browser in order to grant permissions.
-      .setWindowSize(1024, 768)
       .drupalInstall()
       .drupalInstallModule('toolbar', true)
       .drupalCreateUser({
