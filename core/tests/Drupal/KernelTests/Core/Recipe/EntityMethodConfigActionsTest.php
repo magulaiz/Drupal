3e5259d156ee11a7a0865d2296bfea2d1d8775de
Phéna Proxima <adam@phenaproxima.net>
2025-03-26 11:40:10 -0400

    Fix bad deprecation expectation

--- core/tests/Drupal/KernelTests/Core/Recipe/EntityMethodConfigActionsTest.php
+++ core/tests/Drupal/KernelTests/Core/Recipe/EntityMethodConfigActionsTest.php
@@ -233,7 +233,7 @@ class EntityMethodConfigActionsTest extends KernelTestBase {
       ->getViewDisplay('entity_test_with_bundle', 'test');
     $view_display->save();
 
-    $this->expectDeprecation('Using the simpleConfigUpdate config action on config entities is deprecated in drupal:11.2.0 and removed in drupal:12.0.0. Use the setNested action instead. See https://www.drupal.org/node/3439713');
+    $this->expectDeprecation('Using the simpleConfigUpdate config action on config entities is deprecated in drupal:11.2.0 and removed in drupal:12.0.0. Use the setProperties action instead. See https://www.drupal.org/node/3515543');
     $this->configActionManager->applyAction(
       'simpleConfigUpdate',
       $view_display->getConfigDependencyName(),
