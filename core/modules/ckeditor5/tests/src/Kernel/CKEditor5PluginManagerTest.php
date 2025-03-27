37a149afbaed4e3ebacd6f48980f64278a1b0eca
Guillaume Astier <astier.gui@gmail.com>
2025-03-27 11:30:47 +0100

    fix: resolve container.module parameter for testing

--- core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
+++ core/modules/ckeditor5/tests/src/Kernel/CKEditor5PluginManagerTest.php
@@ -167,7 +167,9 @@ YAML,
     // The exception to the above elegance: re-resolve the '%app_root%' param.
     // @see \Symfony\Component\DependencyInjection\Compiler\ResolveParameterPlaceHoldersPass
     // @see \Drupal\Core\DrupalKernel::guessApplicationRoot()
-    $container->getDefinition('module_handler')->setArgument(0, '%app.root%');
+    $container->getDefinition('module_handler')
+      ->setArgument(0, '%app.root%')
+      ->setArgument(1, '%container.modules%');
 
     // To discover per-test case config schema YAML files, work around the
     // static file cache in \Drupal\Core\Extension\ExtensionDiscovery. There is
