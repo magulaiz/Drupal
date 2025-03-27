f9c79d4c5a428c860375731c554a62a07a71bd8a
David Bekker <david.bekker@finalist.nl>
2025-03-27 16:20:11 +0100

    A couple of test fixes

--- core/modules/views/tests/src/Kernel/QueryGroupByTest.php
+++ core/modules/views/tests/src/Kernel/QueryGroupByTest.php
@@ -95,7 +95,7 @@ class QueryGroupByTest extends ViewsKernelTestBase {
     $view = Views::getView('test_aggregate_count_function');
     $this->executeView($view);
 
-    $this->assertEquals(7, $view->result[0]->id);
+    $this->assertEquals(7, $view->result[0]->entity_test_id);
     $this->assertCount(1, $view->result, 'Make sure the count of rows is one.');
   }
 
@@ -129,7 +129,12 @@ class QueryGroupByTest extends ViewsKernelTestBase {
     // Group by name to identify the right count.
     $results = [];
     foreach ($view->result as $item) {
-      $results[$item->entity_test_name] = $item->id;
+      if (isset($item->entity_test_id)) {
+        $results[$item->entity_test_name] = $item->entity_test_id;
+      }
+      else {
+        $results[$item->entity_test_name] = $item->id;
+      }
     }
     $aggregation_function ??= 'NULL';
     $this->assertEquals($values[0], $results['name1'], "Aggregation with $aggregation_function and group by name: name1 returned the expected amount of results");
@@ -161,7 +166,7 @@ class QueryGroupByTest extends ViewsKernelTestBase {
   /**
    * Tests the count aggregation function.
    */
-  public function testGroupByCount(): void {
+  public function testGroupByCount1(): void {
     $this->groupByTestHelper('count', [4, 3]);
   }
 
