fa94453e80c0a078d4901242d6b4c6bafce198e6
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-03-30 13:03:35 -0400

    Constant

--- core/modules/language/src/LanguageServiceAlter.php
+++ core/modules/language/src/LanguageServiceAlter.php
@@ -21,11 +21,11 @@ class LanguageServiceAlter extends ServiceProviderBase {
       ->addArgument(new Reference('config.factory'))
       ->addArgument(new Reference('module_handler'))
       ->addArgument(new Reference('language.config_factory_override'))
-      ->addArgument(new Reference('request_stack'));
+      ->addArgument(new Reference('request_stack'))
+      ->addArgument(new Reference('cache.bootstrap'));
     if ($default_language_values = $this->getDefaultLanguageValues()) {
       $container->setParameter('language.default_values', $default_language_values);
     }
-
   }
 
   /**
