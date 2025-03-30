5597e93f1f4d672a62de36c0294dc8ecca97e1b0
Michael Strelan <mstrelan@gmail.com>
2025-03-31 08:37:05 +1000

    Wait for block as well

--- core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayTestBase.php
+++ core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayTestBase.php
@@ -50,7 +50,8 @@ class SettingsTrayTestBase extends OffCanvasTestBase {
     // link is added.
     $this->assertNotEmpty($this->assertSession()->waitForElementVisible('css', '.dialog-off-canvas-main-canvas.js-settings-tray-edit-mode'));
 
-    $block = $this->getSession()->getPage()->find('css', $block_selector);
+    $block = $this->assertSession()->waitForElement('css', $block_selector);
+    $this->assertNotEmpty($block);
     $block->mouseOver();
     $block->click();
     $this->waitForOffCanvasToOpen();
