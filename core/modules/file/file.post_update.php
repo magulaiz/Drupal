8c2e68f7fdc04e7d33d8ac900621f4c7b72132c9
Christopher Lewis <christopher.lewis@brandcode.de>
2025-03-31 10:32:24 +0200

    Fix deprecation on PHP 8.4

--- core/modules/file/file.post_update.php
+++ core/modules/file/file.post_update.php
@@ -44,7 +44,7 @@ function file_post_update_add_playsinline(array &$sandbox = []): ?TranslatableMa
 /**
  * Add the preload configuration to existing media formatters.
  */
-function file_post_update_preload_setting(array &$sandbox = NULL): void {
+function file_post_update_preload_setting(array &$sandbox = []): void {
   $file_config_updater = \Drupal::classResolver(FileConfigUpdater::class);
   assert($file_config_updater instanceof FileConfigUpdater);
   \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'entity_view_display', function (EntityViewDisplayInterface $view_display) use ($file_config_updater): bool {
