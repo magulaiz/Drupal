b0c63bd5f388859b8ad8f683a17cec3a2369d46c
Michael Strelan <mstrelan@gmail.com>
2025-03-31 10:25:44 +1000

    Try wait for terminate

--- core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
+++ core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
@@ -6,6 +6,7 @@ namespace Drupal\Tests\settings_tray\FunctionalJavascript;
 
 use Drupal\settings_tray_test\Plugin\Block\SettingsTrayFormAnnotationIsClassBlock;
 use Drupal\settings_tray_test\Plugin\Block\SettingsTrayFormAnnotationNoneBlock;
+use Drupal\Tests\WaitTerminateTestTrait;
 use Drupal\user\Entity\Role;
 
 /**
@@ -14,6 +15,8 @@ use Drupal\user\Entity\Role;
  * @group settings_tray
  */
 class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
+
+  use WaitTerminateTestTrait;
 
   /**
    * {@inheritdoc}
@@ -44,6 +47,7 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
       'search content',
     ]);
     $this->drupalLogin($user);
+    $this->setWaitForTerminate();
   }
 
   /**
