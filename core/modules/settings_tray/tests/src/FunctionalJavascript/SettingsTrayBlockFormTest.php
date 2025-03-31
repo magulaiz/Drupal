76857eb37f7c3578fe50269b706e5f86cfb2892c
Michael Strelan <mstrelan@gmail.com>
2025-03-31 10:24:09 +1000

    Revert "Split edit mode test"
    This reverts commit 743df70d2db08448ff4cf750e8e8fe8b84b3cbab.

--- core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
+++ core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
@@ -50,6 +50,7 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
    * Tests opening off-canvas dialog by click blocks and elements in the blocks.
    */
   public function testBlocks(): void {
+    $this->markTestSkipped('Not interested in this one.');
     foreach ($this->getBlockTests() as $test) {
       call_user_func_array([$this, 'doTestBlocks'], array_values($test));
     }
@@ -225,6 +226,41 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
     }
 
     return $blocks;
+  }
+
+  /**
+   * Tests enabling and disabling Edit Mode.
+   */
+  public function testEditModeEnableDisable(): void {
+    foreach (static::getTestThemes() as $theme) {
+      $this->enableTheme($theme);
+      $block = $this->placeBlock('system_powered_by_block');
+      foreach (['contextual_link', 'toolbar_link'] as $enable_option) {
+        $this->drupalGet('user');
+        $this->assertEditModeDisabled();
+        switch ($enable_option) {
+          // Enable Edit mode.
+          case 'contextual_link':
+            $this->clickContextualLink($this->getBlockSelector($block), "Quick edit");
+            $this->waitForOffCanvasToOpen();
+            $this->assertEditModeEnabled();
+            break;
+
+          case 'toolbar_link':
+            $this->enableEditMode();
+            break;
+        }
+        $this->disableEditMode();
+
+        // Make another page request to ensure Edit mode is still disabled.
+        $this->drupalGet('user');
+        $this->assertEditModeDisabled();
+        // Make sure on this page request it also re-enables and disables
+        // correctly.
+        $this->enableEditMode();
+        $this->disableEditMode();
+      }
+    }
   }
 
   /**
