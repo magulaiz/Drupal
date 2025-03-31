e06778ca00900488b922418d2131de30b2e19e51
catch <6915-catch@users.noreply.drupalcode.org>
2025-03-31 14:10:54 +0100

    Only skip fetching from cache when is_root_call is true.

--- core/lib/Drupal/Core/Render/Renderer.php
+++ core/lib/Drupal/Core/Render/Renderer.php
@@ -280,10 +280,12 @@ class Renderer implements RendererInterface {
     // If the element was set to create a placeholder, we do not try to load it
     // from the cache, as \Drupal\Core\Render\Placeholder\CachedStrategy has an
     // optimization to load them all in one go, rather than individually here.
+    // This is only skipped when $is_root_call is false since placeholder
+    // strategies do not run at every level of a render array.
     // If the current request method is not cacheable, no placeholders are
     // generated, in that case, it's still better to fetch render cached
     // elements individually instead of fully building them again.
-    if (isset($elements['#cache']['keys']) && (empty($elements['#create_placeholder']) || !$this->requestStack->getCurrentRequest()->isMethodCacheable())) {
+    if (isset($elements['#cache']['keys']) && (empty($elements['#create_placeholder']) || !$this->requestStack->getCurrentRequest()->isMethodCacheable() || $is_root_call)) {
       $cached_element = $this->renderCache->get($elements);
       if ($cached_element !== FALSE) {
         $elements = $cached_element;
