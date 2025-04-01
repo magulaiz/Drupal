dc8151a56863ed975ebca45879fafc1020138c37
Kristiaan Van den Eynde <kristiaan@factorial.io>
2025-04-01 11:35:24 +0200

    Even fewer cache gets now

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryNodePagePerformanceTest.php
@@ -287,7 +287,7 @@ class OpenTelemetryNodePagePerformanceTest extends PerformanceTestBase {
 
     $expected = [
       'QueryCount' => 171,
-      'CacheGetCount' => 205,
+      'CacheGetCount' => 202,
       'CacheGetCountByBin' => [
         'page' => 1,
         'config' => 66,
@@ -296,7 +296,7 @@ class OpenTelemetryNodePagePerformanceTest extends PerformanceTestBase {
         'data' => 13,
         'entity' => 18,
         'dynamic_page_cache' => 1,
-        'render' => 24,
+        'render' => 21,
         'default' => 3,
         'menu' => 2,
       ],
