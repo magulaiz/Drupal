a0feea2eca4aa1890b162418c626ddcd89897bc5
Michael Strelan <mstrelan@gmail.com>
2025-04-01 15:24:42 +1000

    Test 180 second timeout

--- core/modules/package_manager/tests/modules/package_manager_test_api/src/ApiController.php
+++ core/modules/package_manager/tests/modules/package_manager_test_api/src/ApiController.php
@@ -108,7 +108,7 @@ class ApiController extends ControllerBase {
    *   as long as the stage needs to exist.
    */
   protected function createAndApplyStage(Request $request) : string {
-    $id = $this->stage->create();
+    $id = $this->stage->create(180);
     $this->stage->require(
       $request->get('runtime', []),
       $request->get('dev', [])
