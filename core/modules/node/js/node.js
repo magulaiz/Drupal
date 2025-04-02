d360e773d8759fec02b66dad53b0d33c746b49fb
Adam Bramley <adam.bramley@previousnext.com.au>
2025-04-02 13:22:48 +1100

    Lint

--- core/modules/node/js/node.js
+++ core/modules/node/js/node.js
@@ -20,9 +20,9 @@
         const nameElement = context.querySelector('.field--name-uid input');
         const name = nameElement?.value;
         const dateElement = context.querySelector('.field--name-created input');
-        const date = dateElement?.value && (
-          new Date(dateElement.value)
-        ).toLocaleDateString();
+        const date =
+          dateElement?.value &&
+          new Date(dateElement.value).toLocaleDateString();
 
         if (name && date) {
           return Drupal.t('By @name on @date', {
