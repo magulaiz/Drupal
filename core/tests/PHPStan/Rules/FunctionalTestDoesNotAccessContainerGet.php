f0a7c5db6ce99186a102cc04ac69658ce0d98081
Michael Strelan <mstrelan@gmail.com>
2025-04-02 15:06:39 +1000

    phpcs

--- core/tests/PHPStan/Rules/FunctionalTestDoesNotAccessContainerGet.php
+++ core/tests/PHPStan/Rules/FunctionalTestDoesNotAccessContainerGet.php
@@ -9,6 +9,7 @@ use Drupal\Tests\BrowserTestBase;
 use PhpParser\Node;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
@@ -33,19 +34,19 @@ final class FunctionalTestDoesNotAccessContainerGet implements Rule {
   public function processNode(Node $node, Scope $scope): array {
     $class = $scope->getClassReflection();
 
-    if ($class === null || !$class->isSubclassOf(BrowserTestBase::class) || $class->is(InstallerTestBase::class) || $class->isSubclassOf(InstallerTestBase::class)) {
+    if ($class === NULL || !$class->isSubclassOf(BrowserTestBase::class) || $class->is(InstallerTestBase::class) || $class->isSubclassOf(InstallerTestBase::class)) {
       return [];
     }
 
-    if ($node instanceof MethodCall && $node->name instanceof Node\Identifier && $node->name->name === 'get') {
+    if ($node instanceof MethodCall && $node->name instanceof Identifier && $node->name->name === 'get') {
       $var = $node->var;
-      if ($var instanceof PropertyFetch && $var->name instanceof Node\Identifier && $var->name->name === 'container') {
+      if ($var instanceof PropertyFetch && $var->name instanceof Identifier && $var->name->name === 'container') {
         return [
           RuleErrorBuilder::message('Functional tests should not access $this->container->get(). Use \Drupal::service() instead.')
             ->identifier('functionalTest.noContainerGet')
             ->line($node->getStartLine())
             ->build(),
-          ];
+        ];
       }
     }
 
