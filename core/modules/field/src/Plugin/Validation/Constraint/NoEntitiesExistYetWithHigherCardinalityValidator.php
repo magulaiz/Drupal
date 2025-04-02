848ad1c9226bafb7f107f5f457ce3b751dc25b41
bjorn <bjorn@swis.nl>
2025-04-02 14:54:04 +0200

    build: fix missing docblock

--- core/modules/field/src/Plugin/Validation/Constraint/NoEntitiesExistYetWithHigherCardinalityValidator.php
+++ core/modules/field/src/Plugin/Validation/Constraint/NoEntitiesExistYetWithHigherCardinalityValidator.php
@@ -41,6 +41,9 @@ class NoEntitiesExistYetWithHigherCardinalityValidator extends ConstraintValidat
   ) {
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container): self {
     return new static(
       $container->get('entity_type.manager')
