c647df59549b338cfb2c09d1fc717f6b0aac9f01
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2025-04-02 15:58:55 +0000

    refactor ExceptionHandlers

--- core/modules/mysql/src/Driver/Database/mysql/ExceptionHandler.php
+++ core/modules/mysql/src/Driver/Database/mysql/ExceptionHandler.php
@@ -23,38 +23,72 @@ class ExceptionHandler extends BaseExceptionHandler {
     if (!$exception instanceof \PDOException) {
       throw $exception;
     }
+    $this->rethrowNormalizedException($exception, $exception->getCode(), $exception->errorInfo[1] ?? NULL, $statement->getQueryString(), $arguments);
+  }
 
-    // Wrap the exception in another exception, because PHP does not allow
-    // overriding Exception::getMessage(). Its message is the extra database
-    // debug information.
-    $code = is_int($exception->getCode()) ? $exception->getCode() : 0;
+  /**
+   * Rethrows exceptions thrown during execution of statement objects.
+   *
+   * Wrap the exception in another exception, because PHP does not allow
+   * overriding Exception::getMessage(). Its message is the extra database
+   * debug information.
+   *
+   * @param \Exception $exception
+   *   The exception to be handled.
+   * @param int|string $sqlState
+   *   MySql SQLState error condition.
+   * @param int|null $errorCode
+   *   MySql error code.
+   * @param string $queryString
+   *   The SQL statement string.
+   * @param array $arguments
+   *   An array of arguments for the prepared statement.
+   *
+   * @throws \Drupal\Core\Database\DatabaseExceptionWrapper
+   * @throws \Drupal\Core\Database\IntegrityConstraintViolationException
+   * @throws \Drupal\Core\Database\Exception\SchemaPrimaryKeyMustBeDroppedException
+   * @throws \Drupal\Core\Database\Exception\SchemaTableColumnSizeTooLargeException
+   * @throws \Drupal\Core\Database\Exception\SchemaTableKeyTooLargeException
+   */
+  protected function rethrowNormalizedException(
+    \Exception $exception,
+    int|string $sqlState,
+    ?int $errorCode,
+    string $queryString,
+    array $arguments,
+  ): void {
+
+    // SQLState could be 'HY000' which cannot be used as a $code argument for
+    // exceptions. PDOException is contravariant in this case, but since we are
+    // rethrowing an exception that inherits from \Exception, we need to
+    // convert the code to an integer.
+    // @see https://www.php.net/manual/en/class.exception.php
+    // @see https://www.php.net/manual/en/class.pdoexception.php
+    $code = (int) $sqlState;
 
     // If a max_allowed_packet error occurs the message length is truncated.
     // This should prevent the error from recurring if the exception is logged
     // to the database using dblog or the like.
-    if (($exception->errorInfo[1] ?? NULL) === 1153) {
+    if ($errorCode === 1153) {
       $message = Unicode::truncateBytes($exception->getMessage(), Connection::MIN_MAX_ALLOWED_PACKET);
       throw new DatabaseExceptionWrapper($message, $code, $exception);
     }
 
-    $message = $exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
+    $message = $exception->getMessage() . ": " . $queryString . "; " . print_r($arguments, TRUE);
 
     // SQLSTATE 23xxx errors indicate an integrity constraint violation. Also,
     // in case of attempted INSERT of a record with an undefined column and no
     // default value indicated in schema, MySql returns a 1364 error code.
-    if (
-      substr($exception->getCode(), -6, -3) == '23' ||
-      ($exception->errorInfo[1] ?? NULL) === 1364
-    ) {
+    if (substr($sqlState, -6, -3) == '23' || $errorCode === 1364) {
       throw new IntegrityConstraintViolationException($message, $code, $exception);
     }
 
-    match ($exception->getCode()) {
-      'HY000' =>  match ($exception->errorInfo[1]) {
+    match ($sqlState) {
+      'HY000' =>  match ($errorCode) {
         4111 => throw new SchemaPrimaryKeyMustBeDroppedException($message, 0, $exception),
         default => throw new DatabaseExceptionWrapper($message, 0, $exception),
       },
-      '42000' =>  match ($exception->errorInfo[1]) {
+      '42000' =>  match ($errorCode) {
         1071 => throw new SchemaTableKeyTooLargeException($message, $code, $exception),
         1074 => throw new SchemaTableColumnSizeTooLargeException($message, $code, $exception),
         default => throw new DatabaseExceptionWrapper($message, 0, $exception),
--- core/modules/mysqli/src/Driver/Database/mysqli/ExceptionHandler.php
+++ core/modules/mysqli/src/Driver/Database/mysqli/ExceptionHandler.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\mysqli\Driver\Database\mysqli;
 
-use Drupal\Component\Utility\Unicode;
-use Drupal\Core\Database\DatabaseExceptionWrapper;
 use Drupal\Core\Database\ExceptionHandler as BaseExceptionHandler;
-use Drupal\Core\Database\Exception\SchemaPrimaryKeyMustBeDroppedException;
-use Drupal\Core\Database\Exception\SchemaTableColumnSizeTooLargeException;
-use Drupal\Core\Database\Exception\SchemaTableKeyTooLargeException;
-use Drupal\Core\Database\IntegrityConstraintViolationException;
 use Drupal\Core\Database\StatementInterface;
 
 /**
@@ -22,7 +16,6 @@ class ExceptionHandler extends BaseExceptionHandler {
    * {@inheritdoc}
    */
   public function handleExecutionException(\Exception $exception, StatementInterface $statement, array $arguments = [], array $options = []): void {
-
     // Close the client statement to release handles.
     if ($statement->hasClientStatement()) {
       $statement->getClientStatement()->close();
@@ -31,41 +24,7 @@ class ExceptionHandler extends BaseExceptionHandler {
     if (!($exception instanceof \mysqli_sql_exception)) {
       throw $exception;
     }
-
-    // Wrap the exception in another exception, because PHP does not allow
-    // overriding Exception::getMessage(). Its message is the extra database
-    // debug information.
-    $code = is_int($exception->getCode()) ? $exception->getCode() : 0;
-
-    // If a max_allowed_packet error occurs the message length is truncated.
-    // This should prevent the error from recurring if the exception is logged
-    // to the database using dblog or the like.
-    if ($code === 1153) {
-      $message = Unicode::truncateBytes($exception->getMessage(), Connection::MIN_MAX_ALLOWED_PACKET);
-      throw new DatabaseExceptionWrapper($message, $code, $exception);
-    }
-
-    $message = $exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
-
-    // SQLSTATE 23xxx errors indicate an integrity constraint violation. Also,
-    // in case of attempted INSERT of a record with an undefined column and no
-    // default value indicated in schema, MySql returns a 1364 error code.
-    if (substr($exception->getSqlState(), -6, -3) == '23' || $code === 1364) {
-      throw new IntegrityConstraintViolationException($message, $code, $exception);
-    }
-
-    match ($exception->getSqlState()) {
-      'HY000' =>  match ($code) {
-        4111 => throw new SchemaPrimaryKeyMustBeDroppedException($message, $code, $exception),
-        default => throw new DatabaseExceptionWrapper($message, $code, $exception),
-      },
-      '42000' =>  match ($code) {
-        1071 => throw new SchemaTableKeyTooLargeException($message, $code, $exception),
-        1074 => throw new SchemaTableColumnSizeTooLargeException($message, $code, $exception),
-        default => throw new DatabaseExceptionWrapper($message, $code, $exception),
-      },
-      default => throw new DatabaseExceptionWrapper($message, $code, $exception),
-    };
+    $this->rethrowNormalizedException($exception, $exception->getSqlState(), $exception->getCode(), $statement->getQueryString(), $arguments);
   }
 
 }
