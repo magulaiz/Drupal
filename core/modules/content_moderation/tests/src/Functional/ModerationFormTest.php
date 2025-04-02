efdf1b1985ccee0d9ae305254d933b2f34b67098
Adam Bramley <adam.bramley@previousnext.com.au>
2025-04-02 16:01:34 +1100

    Lint

--- core/modules/content_moderation/tests/src/Functional/ModerationFormTest.php
+++ core/modules/content_moderation/tests/src/Functional/ModerationFormTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\content_moderation\Functional;
 
-use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\Core\Url;
 use Drupal\Tests\content_translation\Traits\ContentTranslationTestTrait;
 
