ec3284058a991f10f01dcc66af54e015db44c3d9
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2025-04-03 11:16:13 +0000

    Revert

--- core/modules/shortcut/src/Entity/ShortcutSet.php
+++ core/modules/shortcut/src/Entity/ShortcutSet.php
@@ -132,7 +132,7 @@ class ShortcutSet extends ConfigEntityBundleBase implements ShortcutSetInterface
   public function getShortcuts() {
     $shortcuts = \Drupal::entityTypeManager()->getStorage('shortcut')->loadByProperties(['shortcut_set' => $this->id()]);
     // Sort the entities using the entity class's sortEntities() method.
-    self::sortEntities($shortcuts);
+    Shortcut::sortEntities($shortcuts);
     return $shortcuts;
   }
 
