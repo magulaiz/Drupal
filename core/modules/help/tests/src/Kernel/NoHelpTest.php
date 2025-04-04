ea8d21bde7eec8225294b03a9a9fffcd3f1bc5af
Michael Strelan <mstrelan@gmail.com>
2025-04-04 10:29:34 +1000

    NoHelpTest

--- core/modules/help/tests/src/Kernel/NoHelpTest.php
+++ core/modules/help/tests/src/Kernel/NoHelpTest.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\help\Functional;
 
-use Drupal\Tests\BrowserTestBase;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\HttpKernelUiHelperTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 
 /**
  * Verify no help is displayed for modules not providing any help.
  *
  * @group help
  */
-class NoHelpTest extends BrowserTestBase {
+class NoHelpTest extends KernelTestBase {
+
+  use HttpKernelUiHelperTrait;
+  use UserCreationTrait;
 
   /**
    * Modules to install.
@@ -20,12 +25,7 @@ class NoHelpTest extends BrowserTestBase {
    *
    * @var array
    */
-  protected static $modules = ['help', 'menu_test'];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected $defaultTheme = 'stark';
+  protected static $modules = ['help', 'menu_test', 'user', 'system'];
 
   /**
    * The user who will be created.
@@ -39,15 +39,15 @@ class NoHelpTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-    $this->adminUser = $this->drupalCreateUser(['access help pages']);
+    $this->installEntitySchema('user');
+    $this->adminUser = $this->createUser(['access help pages']);
+    $this->setCurrentUser($this->adminUser);
   }
 
   /**
    * Ensures modules not implementing help do not appear on admin/help.
    */
   public function testMainPageNoHelp(): void {
-    $this->drupalLogin($this->adminUser);
-
     $this->drupalGet('admin/help');
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->pageTextContains('Module overviews are provided by modules');
@@ -55,7 +55,15 @@ class NoHelpTest extends BrowserTestBase {
     // Make sure the test module menu_test does not display a help link on
     // admin/help.
     $this->assertSession()->pageTextNotContains(\Drupal::service('extension.list.module')->getName('menu_test'));
+  }
 
+  /**
+   * Test module overview help page.
+   *
+   * Ensure that the module overview help page for a module that does not
+   * implement hook_help() results in a 404.
+   */
+  public function testModuleOverviewPageNoHelp(): void {
     // Ensure that the module overview help page for a module that does not
     // implement hook_help() results in a 404.
     $this->drupalGet('admin/help/menu_test');
