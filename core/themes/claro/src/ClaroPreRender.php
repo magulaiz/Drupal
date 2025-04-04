f4583e7adddf3ee1cdc3a5edf3f19a0e5eb8cab6
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2025-04-04 18:14:51 +0530

    Issue #3515097: Audit use of the clearfix class.

--- core/themes/claro/src/ClaroPreRender.php
+++ core/themes/claro/src/ClaroPreRender.php
@@ -154,8 +154,6 @@ class ClaroPreRender implements TrustedCallbackInterface {
    * Prerender callback for text_format elements.
    */
   public static function textFormat($element) {
-    // Add clearfix for filter wrapper.
-    $element['format']['#attributes']['class'][] = 'clearfix';
     // Hide format select label visually.
     $element['format']['format']['#wrapper_attributes']['class'][] = 'form-item--editor-format';
     $element['format']['format']['#attributes']['class'][] = 'form-element--extrasmall';
