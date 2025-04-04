953b0befe3751d1be80d5a3cbc2d23a3c7e9de5d
bjorn <bjorn@swis.nl>
2025-04-04 14:44:42 +0200

    build: phpcs fix, missing . (grmbl)

--- core/lib/Drupal/Core/Config/TypedConfigManager.php
+++ core/lib/Drupal/Core/Config/TypedConfigManager.php
@@ -303,11 +303,11 @@ class TypedConfigManager extends TypedDataManager implements TypedConfigManagerI
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    *   Thrown if the config schema type definition is:
-   *   - incomplete (missing 'class' when defining a new type)
-   *   - its `class` does not subclass PrimitiveInterface for scalar types
-   *   - its `class` does not extend ArrayElement for list or complex types
-   *   - its `definition_class` does not implement DataDefinitionInterface
-   *   - it contains a circular type reference
+   *   - Incomplete (missing 'class' when defining a new type).
+   *   - Its `class` does not subclass PrimitiveInterface for scalar types.
+   *   - Its `class` does not extend ArrayElement for list or complex types.
+   *   - Its `definition_class` does not implement DataDefinitionInterface.
+   *   - It contains a circular type reference.
    */
   protected function validateType(array $definition, string $id): void {
     // If a config schema does not define a new type, but uses an existing one,
