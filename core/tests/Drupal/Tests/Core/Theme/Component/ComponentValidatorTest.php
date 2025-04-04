020145a6f10b919ee0f053b192eb4a9b069d9c4f
Christian López Espínola <penyaskito@computer.org>
2025-04-04 05:21:13 +0000

    Fix test for invalid component so it doesn't affect other tests

--- core/tests/Drupal/Tests/Core/Theme/Component/ComponentValidatorTest.php
+++ core/tests/Drupal/Tests/Core/Theme/Component/ComponentValidatorTest.php
@@ -119,9 +119,22 @@ class ComponentValidatorTest extends TestCase {
     ];
     yield 'invalid slot (type)' => [$cta_with_invalid_slot_type];
 
-    $invalid_variants = static::loadComponentDefinitionFromFs('invalid-variants-cta');
-    yield 'invalid variants' => [$invalid_variants];
-  }
+    $cta_with_invalid_variant_type = $valid_cta;
+    $cta_with_invalid_variant_type['variants'] = [
+      'valid_variant' => [
+        'title' => 'Valid variant',
+        'description' => 'Valid variant description',
+      ],
+      'invalid_variant' => [
+        'title' => [
+          'hello' => 'Invalid variant',
+          'world' => 'Invalid variant',
+        ],
+        'description' => 'Title must be string',
+      ],
+    ];
+    yield 'invalid variant (type)' => [$cta_with_invalid_variant_type];
+ }
 
   /**
    * Tests that valid props are handled properly.
