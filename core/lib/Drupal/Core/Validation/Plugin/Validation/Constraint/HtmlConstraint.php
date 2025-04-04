11571013f988d6672d954157e094dc5ddf5cae2c
bjorn <bjorn@swis.nl>
2025-04-04 14:37:24 +0200

    chore: unfortunately Symfony contraint sets the options with $this->$name = $value. Therefor readonly is not possible

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/HtmlConstraint.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/HtmlConstraint.php
@@ -20,7 +20,7 @@ use Drupal\Core\Validation\Attribute\Constraint;
 )]
 class HtmlConstraint extends SymfonyConstraint {
 
-  public function __construct(mixed $options = NULL, public readonly string $mode = 'fragment', ?array $groups = NULL, mixed $payload = NULL) {
+  public function __construct(mixed $options = NULL, public string $mode = 'fragment', ?array $groups = NULL, mixed $payload = NULL) {
     parent::__construct($options, $groups, $payload);
     if (!in_array($this->mode, ['fragment', 'document'])) {
       throw new InvalidArgumentException('Invalid HTML parsing mode. the `mode` argument must be "fragment" or "document".');
