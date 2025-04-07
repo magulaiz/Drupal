5654dcd01897ea42f1e160c3bc70227c189881f4
scambler <53589-rscambler@users.noreply.drupalcode.org>
2025-04-07 10:59:12 +0000

    by scambler: Fix grouped view filters to work with multivalue selection

--- core/modules/views/src/Plugin/views/filter/FilterPluginBase.php
+++ core/modules/views/src/Plugin/views/filter/FilterPluginBase.php
@@ -1491,7 +1491,7 @@ abstract class FilterPluginBase extends HandlerBase implements CacheableDependen
    * checkboxes widget, and this function will be called for each item
    * chosen in the checkboxes.
    */
-  public function convertExposedInput(&$input, $selected_group_id = NULL) {
+  public function convertExposedInput(&$input, $selected_group_id = NULL, bool $combine_options = FALSE) {
     if ($this->isAGroup()) {
       // If it is already defined the selected group, use it. Only valid
       // when the filter uses checkboxes for widget.
@@ -1518,7 +1518,14 @@ abstract class FilterPluginBase extends HandlerBase implements CacheableDependen
         // Value can be optional, For example for 'empty' and 'not empty'
         // filters.
         if (isset($selected_group_options['value']) && $selected_group_options['value'] !== '') {
-          $input[$this->options['group_info']['identifier']] = $selected_group_options['value'];
+          // If this is the first conversion for a grouped filter, then override with group values.
+          // For subsequent conversions, group options should be appended to the options list.
+          if (!$combine_options) {
+            $input[$this->options['group_info']['identifier']] = $selected_group_options['value'];
+          }
+          else {
+            $input[$this->options['group_info']['identifier']] += $selected_group_options['value'];
+          }
         }
 
         $this->group_info = $input[$this->options['group_info']['identifier']];
