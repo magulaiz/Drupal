ba6ef9e13de13040067506ab6605299ca63895c9
bjorn <bjorn@swis.nl>
2025-04-08 13:04:05 +0200

    fix: remove "please" from message since that word is now forbidden

--- core/lib/Drupal/Core/Config/Development/LenientConfigSchemaChecker.php
+++ core/lib/Drupal/Core/Config/Development/LenientConfigSchemaChecker.php
@@ -56,7 +56,7 @@ class LenientConfigSchemaChecker extends ConfigSchemaChecker {
       parent::onConfigSave($event);
     }
     catch (SchemaIncompleteException $exception) {
-      $message = sprintf('%s. These errors mean there is configuration that does not comply with it\'s schema. This is not a fatal error, but it is recommended to fix these issues. For more information on configuration schema\'s please check out <a href="%s">the documentation</a>.', $exception->getMessage(), 'https://www.drupal.org/docs/drupal-apis/configuration-api/configuration-schemametadata');
+      $message = sprintf('%s. These errors mean there is configuration that does not comply with it\'s schema. This is not a fatal error, but it is recommended to fix these issues. For more information on configuration schema\'s check out <a href="%s">the documentation</a>.', $exception->getMessage(), 'https://www.drupal.org/docs/drupal-apis/configuration-api/configuration-schemametadata');
 
       $this->messenger->addWarning($message);
       $this->logger->warning($message);
