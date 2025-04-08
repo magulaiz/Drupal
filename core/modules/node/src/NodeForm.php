8ead97947e8472e67e7288c87d766a36d42237e8
Adam Bramley <adam.bramley@previousnext.com.au>
2025-04-09 08:45:54 +1000

    Add deprecations in constructor

--- core/modules/node/src/NodeForm.php
+++ core/modules/node/src/NodeForm.php
@@ -2,6 +2,12 @@
 
 namespace Drupal\node;
 
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\node\Form\NodeForm as BaseNodeForm;
 
 /**
@@ -15,5 +21,13 @@ use Drupal\node\Form\NodeForm as BaseNodeForm;
  * @see https://www.drupal.org/node/3517871
  */
 class NodeForm extends BaseNodeForm {
+
+  /**
+   * Constructs a new NodeForm instance.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, PrivateTempStoreFactory $temp_store_factory, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, AccountInterface $current_user, DateFormatterInterface $date_formatter) {
+    @trigger_error(__CLASS__ . ' is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use \Drupal\node\Form\NodeForm instead. See https://www.drupal.org/node/3517871', E_USER_DEPRECATED);
+    parent::__construct($entity_repository, $temp_store_factory, $entity_type_bundle_info, $time, $current_user, $date_formatter);
+  }
 
 }
--- core/modules/node/src/NodeTypeForm.php
+++ core/modules/node/src/NodeTypeForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\node;
 
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\node\Form\NodeTypeForm as BaseNodeTypeForm;
 
 /**
@@ -15,5 +16,13 @@ use Drupal\node\Form\NodeTypeForm as BaseNodeTypeForm;
  * @see https://www.drupal.org/node/3517871
  */
 class NodeTypeForm extends BaseNodeTypeForm {
+
+  /**
+   * Constructs a new NodeTypeForm instance.
+   */
+  public function __construct(EntityFieldManagerInterface $entity_field_manager) {
+    @trigger_error(__CLASS__ . ' is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use \Drupal\node\Form\NodeTypeForm instead. See https://www.drupal.org/node/3517871', E_USER_DEPRECATED);
+    parent::__construct($entity_field_manager);
+  }
 
 }
